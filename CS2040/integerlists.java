bbe9af77d5c8d4d75b2b7954247f339c8d399ea5
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-11-19 11:27:48 +0800

    Create integerlists.java

---
+++ CS2040/integerlists.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.*;
+
+public class integerlists {
+    public static void main(String[] args) throws IOException {
+        var br = new BufferedReader(new InputStreamReader(System.in));
+        var pw = new PrintWriter(System.out);
+        int tests = Integer.parseInt(br.readLine());
+        while (tests-- > 0) {
+            var program = br.readLine();
+            int elements = Integer.parseInt(br.readLine());
+            if (elements == 0) {
+                var empty = br.readLine();
+                boolean error = false;
+                for (int i = 0; i < program.length(); i++)
+                    if (program.charAt(i) == 'D') {
+                        error = true;
+                        break;
+                    }
+                if (!error) pw.println(empty);
+                else pw.println("error");
+                continue;
+            }
+            var list = br.readLine().replaceAll("\\[","").replaceAll("]","");
+            var listArr = list.split(",");
+            var deque = new ArrayDeque<Integer>();
+            for (var s : listArr) {
+                int element = Integer.parseInt(s);
+                deque.add(element);
+            }
+            boolean reversed = false;
+            boolean empty = false;
+            for (int i = 0; i < program.length(); i++) {
+                if (program.charAt(i) == 'R') reversed = !reversed;
+                else {
+                    if (!deque.isEmpty()) {
+                        if (!reversed) deque.removeFirst();
+                        else deque.removeLast();
+                    }
+                    else {
+                        pw.println("error");
+                        empty = true;
+                        break;
+                    }
+                }
+            }
+            if (empty) continue;
+            int size = deque.size();
+            pw.print("[");
+            if (!reversed)
+                for (int i = 0; i < size; i++) {
+                    if (deque.size() != 1) pw.print(deque.removeFirst()+",");
+                    else pw.print(deque.removeFirst());
+                }
+            else
+                for (int i = 0; i < size; i++) {
+                    if (deque.size() != 1) pw.print(deque.removeLast() + ",");
+                    else pw.print(deque.removeLast());
+                }
+            pw.println("]");
+        }
+        pw.flush();
+    }
+}
