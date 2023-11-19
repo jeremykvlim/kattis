93b9c42df480e679915b3aecdd8ccda34a8d16d6
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-11-19 11:31:21 +0800

    Create kattissquest.java

---
+++ CS2040/kattissquest.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+
+public class kattissquest {
+    public static void main(String[] args) throws IOException {
+        var br = new BufferedReader(new InputStreamReader(System.in));
+        var pw = new PrintWriter(System.out);
+        int n = Integer.parseInt(br.readLine());
+        var set = new TreeSet<Quest>();
+        while (n-- > 0) {
+            var command = br.readLine().split(" ");
+            if (command[0].equals("add")) set.add(new Quest(Long.parseLong(command[1]), Long.parseLong(command[2]), n));
+            else {
+                long energy = Long.parseLong(command[1]);
+                long gold = 0;
+                while (set.floor(new Quest(energy, Integer.MAX_VALUE, Integer.MAX_VALUE)) != null) {
+                    var bestQuest = set.floor(new Quest(energy, Integer.MAX_VALUE, Integer.MAX_VALUE));
+                    energy -= bestQuest.consumption;
+                    gold += bestQuest.reward;
+                    set.remove(bestQuest);
+                }
+                pw.println(gold);
+            }
+        }
+        pw.flush();
+    }
+
+    static class Quest implements Comparable<Quest> {
+        long consumption, reward;
+        int index;
+
+        Quest(long c, long r, int i) {
+            consumption = c;
+            reward = r;
+            index = i;
+        }
+
+        @Override
+        public int compareTo(Quest q) {
+            if (consumption != q.consumption) return Long.compare(consumption, q.consumption);
+            else if (reward != q.reward) return Long.compare(reward, q.reward);
+            else return index - q.index;
+        }
+    }
+}
