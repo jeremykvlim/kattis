d7e7803aa3302b5dd14a755a49a1a1f147ea21be
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-11-19 12:57:54 +0800

    Create islands3.java

---
+++ CS2040/islands3.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+public class islands3 {
+    public static void main(String[] args) throws IOException {
+        var br = new BufferedReader(new InputStreamReader(System.in));
+        var input = br.readLine().split(" ");
+        int r = Integer.parseInt(input[0]), c = Integer.parseInt(input[1]);
+        var matrix = new char[r][c];
+        for (int i = 0; i < r; i++)
+            matrix[i] = br.readLine().toCharArray();
+
+        int ans = 0;
+        for (int i = 0; i < r; i++)
+            for (int j = 0; j < c; j++) 
+                if (matrix[i][j] == 'L') {
+                    ans++;
+                    dfs(matrix, i, j, r, c);
+                }
+            
+        System.out.println(ans);
+    }
+
+    static void dfs(char[][] matrix, int i, int j, int r, int c) {
+        if (i < 0 || j < 0 || i == r || j == c || matrix[i][j] == 'W')
+            return;
+        if (matrix[i][j] == 'L' || matrix[i][j] == 'C') {
+            matrix[i][j] = 'W';
+            dfs(matrix, i, j+1, r, c);
+            dfs(matrix, i, j-1, r, c);
+            dfs(matrix, i+1, j, r, c);
+            dfs(matrix, i-1, j, r, c);
+        }
+    }
+}
