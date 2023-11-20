1fed0d4f51592794168ce260770a763bd18622a3
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-11-20 12:26:06 +0800

    Create apaxiaaans.java

---
+++ CS2040/apaxiaaans.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class apaxiaaans {
+    public static void main(String[] args) throws Exception {
+        var dis = new DataInputStream(System.in);
+        var sb = new StringBuilder();
+        byte b = ' ', prev;
+        do {
+            prev = b;
+            b = dis.readByte();
+        } while (b <= ' ');
+        while (b > ' ') {
+            if (b != prev) sb.appendCodePoint(b);
+            prev = b;
+            b = dis.readByte();
+        }
+        System.out.println(sb);
+    }
+}
