e4f24ef280da5ccc313a53f5906f812e3bf51d34
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-11-20 15:17:49 +0800

    Create ferryloading4.java

---
+++ CS2040/ferryloading4.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class ferryloading4 {
+    public static void main(String[] args) throws IOException {
+        var br = new BufferedReader(new InputStreamReader(System.in));
+        int c = Integer.parseInt(br.readLine());
+        while (c-- > 0) {
+            var input = br.readLine().split(" ");
+            int l = Integer.parseInt(input[0]) * 100, m = Integer.parseInt(input[1]), count = 0;
+            var sides = new ArrayDeque[]{new ArrayDeque<Integer>(), new ArrayDeque<Integer>()};
+
+            while (m-- > 0) {
+                var car = br.readLine().split(" ");
+                sides[car[1].equals("right") ? 1 : 0].add(Integer.parseInt(car[0]));
+            }
+
+            var flag = false;
+            while (!sides[0].isEmpty() || !sides[1].isEmpty()) {
+                int length = 0;
+                while (!sides[flag ? 1 : 0].isEmpty() && (length + (Integer) sides[flag ? 1 : 0].peekFirst()) <= l)
+                    length += (Integer) sides[flag ? 1 : 0].removeFirst();
+                count++;
+                flag = !flag;
+            }
+            System.out.println(count);
+        }
+    }
+}
