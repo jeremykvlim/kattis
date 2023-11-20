e766979b29c117b58cea5b34ba8fd2025daa31f6
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-11-20 11:32:44 +0800

    Create numberfun.java

---
+++ CS2040/numberfun.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class numberfun {
+    public static void main(String[] args) {
+        var sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        while (n-- > 0) {
+            int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
+            if (a + b == c || a - b == c || a * b == c || c * b == a ||
+                b - a == c || c * a == b) System.out.println("Possible");
+            else System.out.println("Impossible");
+        }
+    }
+}
