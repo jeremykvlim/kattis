a3b8e031dc96b3e4509017a03ab380f357a6ec0e
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-11-20 11:23:07 +0800

    Create detaileddifferences.java

---
+++ CS2040/detaileddifferences.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class detaileddifferences {
+    public static void main(String[] args) throws IOException {
+        var br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        while (n-- > 0) {
+            String first = br.readLine(), second = br.readLine();
+            System.out.println(first+"\n"+second);
+            for (int i = 0; i < first.length(); i++)
+                System.out.print(first.charAt(i) != second.charAt(i) ? "*" : ".");
+            System.out.println("\n");
+        }
+    }
+}
