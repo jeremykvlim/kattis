e004fd577a108be5f6f12d0ace8d5c07498d4541
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-11-20 11:09:14 +0800

    Create lastfactorialdigit.java

---
+++ CS2040/lastfactorialdigit.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        var sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            switch (n) {
+                case 1: System.out.println(1); break;
+                case 2: System.out.println(2); break;
+                case 3: System.out.println(6); break;
+                case 4: System.out.println(4); break;
+                default: System.out.println(0); break;
+            }
+        }
+    }
+}
