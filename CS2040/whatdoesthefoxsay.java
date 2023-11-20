cf1c12bfab63b8f6c569c503bccb4ed7cb290c8c
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-11-20 15:38:28 +0800

    Create whatdoesthefoxsay.java

---
+++ CS2040/whatdoesthefoxsay.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class whatdoesthefoxsay {
+    public static void main(String[] args) throws IOException {
+        var br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        var set = new HashSet<String>();
+        for (int i =0 ; i < t; i++) {
+            var r = br.readLine().split(" ");
+            var line = br.readLine().split(" ");
+            while (!line[0].equals("what") ) {
+                set.add(line[2]);
+                line = br.readLine().split(" ");
+            }
+
+            var output = new StringBuilder();
+            for (var w : r)
+                if (!set.contains(w)) output.append(w).append(" ");
+            System.out.println(output);
+        }
+    }
+}
