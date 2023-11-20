991115aea313b446d4fc9ce3c8deac4b4960e99e
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-11-20 10:59:05 +0800

    Create twostones.java

---
+++ CS2040/twostones.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class twostones {
+    public static void main(String[] args) {
+        var sc = new Scanner(System.in);
+        if ((sc.nextInt() & 1) == 0) System.out.println("Bob");
+        else System.out.println("Alice");
+    }
+}
