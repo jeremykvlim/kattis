d0cb876969b57ac115b27093ad2f14813580e070
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-11-20 11:17:49 +0800

    Create pot.java

---
+++ CS2040/pot.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class pot {
+    public static void main(String[] args) throws IOException {
+        var br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine()), x = 0;
+        while (n-- > 0) {
+            int p = Integer.parseInt(br.readLine()), num = p / 10, pow = p % 10;
+            x += Math.pow(num, pow);
+        }
+        System.out.println(x);
+    }
+}
