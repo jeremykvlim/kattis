1d1ab2be9a88edd7c657b7e7238813d60b28edfa
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-11-20 16:22:35 +0800

    Create guessthedatastructure.java

---
+++ CS2040/guessthedatastructure.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+public class guessthedatastructure {
+    public static void main(String[] args) throws IOException {
+        var br = new BufferedReader(new InputStreamReader(System.in));
+        var pw = new PrintWriter(System.out);
+
+        String line;
+        while ((line = br.readLine()) != null) {
+            int n = Integer.parseInt(line), count = 0;
+            int[] command = new int[n], x = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                var input = br.readLine().split(" ");
+                command[i] = Integer.parseInt(input[0]);
+                x[i] = Integer.parseInt(input[1]);
+            }
+
+            var ans = "impossible";
+
+            var s = new Stack<Integer>();
+            for (int i = 0; i < command.length; i++) {
+                if (command[i] == 1) s.push(x[i]);
+                else if (s.isEmpty() || s.pop() != x[i]) break;
+                if (i == command.length - 1) {
+                    ans = "stack";
+                    count++;
+                }
+            }
+
+            var q = new ArrayDeque<Integer>();
+            for (int i = 0; i < command.length; i++) {
+                if (command[i] == 1) q.addLast(x[i]);
+                else if (q.isEmpty() || q.removeFirst() != x[i]) break;
+                if (i == command.length - 1) {
+                    ans = "queue";
+                    count++;
+                }
+            }
+
+            var pq = new PriorityQueue<Integer>(Collections.reverseOrder());
+            for (int i = 0; i < command.length; i++) {
+                if (command[i] == 1) pq.add(x[i]);
+                else if (pq.isEmpty() || pq.poll() != x[i]) break;
+                if (i == command.length - 1) {
+                    ans = "priority queue";
+                    count++;
+                }
+            }
+
+            pw.println(count > 1 ? "not sure" : ans);
+        }
+
+        pw.close();
+    }
+}
