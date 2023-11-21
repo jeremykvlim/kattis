4f37b785d81dfa4a5b958ab04a198e961993f3cd
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-11-21 15:21:52 +0800

    Update whatdoesthefoxsay.java

--- CS2040/whatdoesthefoxsay.java
+++ CS2040/whatdoesthefoxsay.java
@@ -6,7 +6,7 @@ public class whatdoesthefoxsay {
         var br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
         var set = new HashSet<String>();
-        for (int i = 0 ; i < t; i++) {
+        for (int i = 0; i < t; i++) {
             var r = br.readLine().split(" ");
             var line = br.readLine().split(" ");
             while (!line[0].equals("what") ) {
