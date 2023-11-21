ce90d0e3b224f425ead83cdd84f176a2e74d8416
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-11-21 15:21:03 +0800

    Update weakvertices.java

--- CS2040/weakvertices.java
+++ CS2040/weakvertices.java
@@ -4,9 +4,7 @@ import java.util.*;
 public class weakvertices {
     public static void main(String[] args) throws IOException {
         var br = new BufferedReader(new InputStreamReader(System.in));
-        for (;;) {
-            int n = Integer.parseInt(br.readLine());
-            if (n == -1) break;
+        for (int n = Integer.parseInt(br.readLine()); n != -1; n = Integer.parseInt(br.readLine())) {
             var matrix = new int[n][n];
             for (int i = 0; i < n; i++)
                 matrix[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
