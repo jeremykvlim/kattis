0f9b853244db82840918d0c8c918b08d74a88573
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-11-21 20:55:21 +0800

    Update superdoku.cpp

--- CS3233/superdoku.cpp
+++ CS3233/superdoku.cpp
@@ -23,7 +23,6 @@ int main() {
 
     vector<vector<int>> grid(n + 1, vector<int>(n + 1)), row(n + 1, vector<int>(n + 1)), col(n + 1, vector<int>(n + 1));
 
-
     for(int i = 1; i <= k; i++) {
         for(int j = 1; j <= n; j++) {
             cin >> grid[i][j];
