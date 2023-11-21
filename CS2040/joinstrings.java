1b19ffe9b08a8599f9edda382bc9a08c75fe5241
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-11-21 13:37:12 +0800

    Update joinstrings.java

--- CS2040/joinstrings.java
+++ CS2040/joinstrings.java
@@ -5,18 +5,16 @@ public class joinstrings {
     static PrintWriter pw = new PrintWriter(System.out);
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        var br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        String[] arr = new String[n];
+        var arr = new String[n];
         for (int i = 0; i < n; i++) arr[i] = br.readLine();
 
-        ArrayList<Integer>[] adjList = new ArrayList[n];
-        for (int i = 0; i < n; i++) adjList[i] = new ArrayList<>();
+        var adjList = new ArrayList[n];
+        for (int i = 0; i < n; i++) adjList[i] = new ArrayList<Integer>();
 
         int source = 0;
-
         for (int i = 0; i < n - 1; i++) {
             int[] operation = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
             source = operation[0] - 1;
@@ -29,7 +27,7 @@ public class joinstrings {
     }
 
     static void print(int idx, ArrayList<Integer>[] aL, String[] arr) {
-        for (int i : aL[idx]) {
+        for (var i : aL[idx]) {
             pw.print(arr[i]);
             print(i, aL, arr);
         }
