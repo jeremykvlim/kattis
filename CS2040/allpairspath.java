40a065ebf3a07edaef3f7c4ee98f60af3e28ebbe
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-11-22 18:38:02 +0800

    Update allpairspath.java

--- CS2040/allpairspath.java
+++ CS2040/allpairspath.java
@@ -10,33 +10,33 @@ public class allpairspath {
             var line = input.split(" ");
             int n = Integer.parseInt(line[0]), m = Integer.parseInt(line[1]), q = Integer.parseInt(line[2]);
 
-            var dp = new int[n][n];
-            for (var row : dp) Arrays.fill(row, Integer.MAX_VALUE);
-            for (int i = 0; i < n; i++) dp[i][i] = 0;
+            var adjMatrix = new int[n][n];
+            for (var row : adjMatrix) Arrays.fill(row, Integer.MAX_VALUE);
+            for (int i = 0; i < n; i++) adjMatrix[i][i] = 0;
 
             while (m-- > 0) {
                 var edge = br.readLine().split(" ");
                 int u = Integer.parseInt(edge[0]), v = Integer.parseInt(edge[1]), w = Integer.parseInt(edge[2]);
-                dp[u][v] = Math.min(dp[u][v], w);
+                adjMatrix[u][v] = Math.min(adjMatrix[u][v], w);
             }
 
-            for (int k = 0; k < n; k++)
-                for (int i = 0; i < n; i++)
-                    for (int j = 0; j < n; j++)
-                        if (dp[i][k] != Integer.MAX_VALUE && dp[k][j] != Integer.MAX_VALUE)
-                            dp[i][j] = Math.min(dp[i][j], dp[i][k] + dp[k][j]);
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    for (int k = 0; k < n; k++)
+                        if (adjMatrix[j][i] != Integer.MAX_VALUE && adjMatrix[i][k] != Integer.MAX_VALUE)
+                            adjMatrix[j][k] = Math.min(adjMatrix[j][k], adjMatrix[j][i] + adjMatrix[i][k]);
 
             for (int i = 0; i < n; i++)
-                if (dp[i][i] < 0)
+                if (adjMatrix[i][i] < 0)
                     for (int j = 0; j < n; j++)
                         for (int k = 0; k < n; k++)
-                            if (dp[j][i] != Integer.MAX_VALUE && dp[i][k] != Integer.MAX_VALUE)
-                                dp[j][k] = -Integer.MAX_VALUE;
+                            if (adjMatrix[j][i] != Integer.MAX_VALUE && adjMatrix[i][k] != Integer.MAX_VALUE)
+                                adjMatrix[j][k] = -Integer.MAX_VALUE;
 
 
             while (q-- > 0) {
                 var query = br.readLine().split(" ");
-                int u = Integer.parseInt(query[0]), v = Integer.parseInt(query[1]), w = dp[u][v];
+                int u = Integer.parseInt(query[0]), v = Integer.parseInt(query[1]), w = adjMatrix[u][v];
                 pw.println(w == Integer.MAX_VALUE ? "Impossible" : (w == -Integer.MAX_VALUE ? "-Infinity" : w));
             }
             pw.println();
