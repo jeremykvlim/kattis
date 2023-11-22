047def4bfba3ab021aaa22fa3b4fd25c1ac21896
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-11-22 22:11:55 +0800

    Create congaline.cpp

---
+++ CS2040C/congaline.cpp
@@ -0,0 +1,101 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    string name;
+    Node *next, *prev, *partner;
+};
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, q;
+    cin >> n >> q;
+
+    Node *head = nullptr, *tail = nullptr;
+
+    for (int i = 0; i < n; i++) {
+        auto *p1 = new Node(), *p2 = new Node();
+        cin >> p1->name >> p2->name;
+
+        p1->partner = p2;
+        p2->partner = p1;
+
+        if (!head) {
+            head = p1;
+            tail = p2;
+        } else {
+            tail->next = p1;
+            p1->prev = tail;
+            tail = p2;
+        }
+
+        p1->next = p2;
+        p2->prev = p1;
+    }
+
+    head->prev = tail;
+    tail->next = head;
+
+    auto *mic = head;
+
+    string instructions;
+    cin >> instructions;
+
+    for (char c : instructions) {
+        if (c == 'P') cout << mic->partner->name << "\n";
+        else if (c == 'F') mic = mic->prev;
+        else if (c == 'B') mic = mic->next;
+
+        else if (c == 'R') {
+            auto *prev = mic->prev, *next = mic->next;
+
+            if (mic == head) {
+                head = next;
+                tail = mic;
+            } else if (mic != tail) {
+                prev->next = next;
+                next->prev = prev;
+
+                tail->next = mic;
+                mic->prev = tail;
+                head->prev = mic;
+                mic->next = head;
+
+                tail = mic;
+            }
+
+            mic = next;
+
+        } else if (c == 'C') {
+            auto *next = mic->next, *prev = mic->prev, *partner = mic->partner, *partnerNext = partner->next;
+
+            if (prev != partner) {
+                prev->next = next;
+                next->prev = prev;
+
+                partner->next = mic;
+                mic->prev = partner;
+                partnerNext->prev = mic;
+                mic->next = partnerNext;
+
+                head = (mic == head) ? next : head;
+                tail = (mic == tail) ? prev : (partner == tail) ? mic : tail;
+            } else if (partner == tail) {
+                head = next;
+                tail = mic;
+            }
+
+            mic = next;
+        }
+    }
+
+    cout << "\n";
+
+    auto *it = head;
+    do {
+        cout << it->name << "\n";
+        it = it->next;
+    } while (it != head);
+}
