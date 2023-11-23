bc02ee024b52e6df9a1c5d97f06329315bcdc3cf
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-11-23 19:05:19 +0800

    Create perfectpowers.rs

---
+++ Self-practice/perfectpowers.rs
@@ -0,0 +1,24 @@
+use std::io::{self, BufRead, Write};
+
+fn main() {
+    let stdin = io::stdin();
+    let stdout = io::stdout();
+    let mut writer = io::BufWriter::new(stdout.lock());
+    for line in stdin.lock().lines() {
+        let mut x: i64 = line.unwrap().trim().parse().unwrap();
+        if x == 0 { break; }
+        let mut p: i64 = 1;
+        for b in 2.. {
+            if b * b > x.abs() { break; }
+            let mut a: i64 = b * b;
+            p = 2;
+            while a < x.abs() {
+                a *= b;
+                p += 1;
+            }
+            if a == x.abs() && (x > 0 || p % 2 == 1) { break; }
+            else { p = 1; }
+        }
+        writeln!(writer, "{}", p);
+    }
+}
