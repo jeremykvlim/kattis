3bf75551a948f72b5e3f40004c62cf8578aad791
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-11-24 01:03:21 +0800

    Create zbrka.cpp

---
+++ Self-practice/zbrka.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+constexpr int MODULO = 1e9 + 7;
+
+int f(int n, int c, vector<vector<int>>& dp) {
+    if (!c) return 1;
+    if (c < 0 || !n) return 0;
+    if (dp[n][c] != -1) return dp[n][c];
+
+    int r = (f(n, c - 1, dp) - f(n - 1, c - n, dp) + f(n - 1, c, dp)) % MODULO;
+    if (r < 0) r += MODULO;
+
+    return dp[n][c] = r;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n, c;
+    cin >> n >> c;
+
+    vector<vector<int>> dp(n + 1, vector<int>(c + 1, -1));
+
+    for (int i = 1; i <= n; i++) f(i, c, dp);
+    cout << f(n, c, dp);
+}
