687140a2cefd8076cfaa5884c7596f1b0ee1d59d
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-11-23 18:35:02 +0800

    Create sequences.cpp

---
+++ CS3233/sequences.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+constexpr int MODULO = 1e9 + 7;
+
+inline int mod(long long x) {
+    return x % MODULO;
+}
+
+int main() {
+    string input;
+    cin >> input;
+    long long sum = 0, count = 0, total = 1;
+    for (char c : input) {
+        if (c == '1') count = mod(count + total);
+        else if (c == '?') {
+            sum = mod(mod(sum * 2) + count);
+            count = mod(mod(count * 2) + total);
+            total = mod(total * 2);
+        }
+        else sum = mod(sum + count);
+    }
+    cout << sum << endl;
+}
