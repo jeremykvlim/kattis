bc3fc8dea751a51f4c81537241f15633d860087a
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-11-23 21:35:16 +0800

    Update factorfree.java

--- CS2040/factorfree.java
+++ CS2040/factorfree.java
@@ -1,34 +1,29 @@
 import java.util.*;
 import java.io.*;
 
-public class factorfree {
-    static int[] elements, factor, pos, prev, next, parent;
-    static ArrayList<Integer> primes;
-
+class factorfree {
     public static void main(String[] args) throws IOException {
         var br = new BufferedReader(new InputStreamReader(System.in));
         var pw = new PrintWriter(System.out);
+
         int n = Integer.parseInt(br.readLine());
-        elements = new int[n + 1];
+        var elements = new int[n + 1];
         var st = new StringTokenizer(br.readLine());
         int max = 0;
         for (int i = 1; i <= n; i++) {
             elements[i] = Integer.parseInt(st.nextToken());
             max = Math.max(max, elements[i]);
         }
-        factor = new int[max + 1];
-        primes = new ArrayList<>();
+        var factor = new int[max + 1];
+        var primes = new ArrayList<Integer>();
         primes.add(0);
-        sieve(max);
+        sieve(factor, primes, max);
 
         int biggest = -1;
         for (int i : factor)
             biggest = Math.max(biggest, i);
 
-        pos = new int[biggest + 1];
-        prev = new int[n + 1];
-        next = new int[n + 1];
-        parent = new int[n + 1];
+        int[] pos = new int[biggest + 1], prev = new int[n + 1], next = new int[n + 1], parent = new int[n + 1];
 
         Arrays.fill(pos, 1, primes.size(), -1);
         for (int i = 1; i <= n; i++) {
@@ -46,22 +41,22 @@ public class factorfree {
             for (int j = elements[i]; j > 1; j /= primes.get(factor[j]))
                 pos[factor[j]] = i;
         }
-        if (dfs(1, n, 0)) {
+        if (dfs(1, n, 0, prev, next, parent)) {
             for (int i = 1; i <= n; i++) pw.print(parent[i] + " ");
             pw.println();
         } else pw.println("impossible");
         pw.flush();
     }
 
-    static void sieve(int max) {
-        var prime = new BitSet(max + 1);
-        prime.set(2, max + 1);
-        for (int i = 2; i <= max; i++) {
+    static void sieve(int[] factor, ArrayList<Integer> primes, int max_element) {
+        var prime = new BitSet(max_element + 1);
+        prime.set(2, max_element + 1);
+        for (int i = 2; i <= max_element; i++) {
             if (prime.get(i)) {
                 primes.add(i);
                 factor[i] = primes.size() - 1;
             }
-            for (int j = 1, k; j < primes.size() && (k = i * primes.get(j)) <= max; j++) {
+            for (int j = 1, k; j < primes.size() && (k = i * primes.get(j)) <= max_element; j++) {
                 factor[k] = factor[i];
                 prime.clear(k);
                 if (i % primes.get(j) == 0) break;
@@ -69,18 +64,40 @@ public class factorfree {
         }
     }
 
-    static boolean dfs(int l, int r, int p) {
-        if (l > r) return true;
-        for (int left = l, right = r; left <= right; left++, right--) {
-            if (l >= prev[left] && r <= next[left]) {
-                parent[left] = p;
-                return dfs(l, left - 1, left) && dfs(left + 1, r, left);
-            }
-            if (l >= prev[right] && r <= next[right]) {
-                parent[right] = p;
-                return dfs(l, right - 1, right) && dfs(right + 1, r, right);
-            }
+    static boolean dfs(int l, int r, int p, int[] prev, int[] next, int[] parent) {
+        var s = new Stack<Integer>();
+        s.push(l);
+        s.push(r);
+        s.push(p);
+
+        while (!s.isEmpty()) {
+            p = s.pop();
+            r = s.pop();
+            l = s.pop();
+
+            if (l > r) continue;
+            
+            var found = false;
+            for (int left = l, right = r; left <= right; left++, right--) 
+                if ((l >= prev[left] && r <= next[left]) || (l >= prev[right] && r <= next[right])) {
+                    int mid = (l >= prev[left] && r <= next[left]) ? left : right;
+                    parent[mid] = p;
+
+                    s.push(mid + 1);
+                    s.push(r);
+                    s.push(mid);
+
+                    s.push(l);
+                    s.push(mid - 1);
+                    s.push(mid);
+
+                    found = true;
+                    break;
+                }
+            
+            if (!found) return false;
         }
-        return false;
+
+        return true;
     }
 }
