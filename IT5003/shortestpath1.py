226c9fad856c123b9f499282f09c4f1038090be2
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-11-24 12:13:37 +0800

    Create shortestpath1.py

---
+++ IT5003/shortestpath1.py
@@ -0,0 +1,36 @@
+import heapq, math
+
+while True:
+    n, m, q, s = map(int, input().split())
+    if m == n == q == s == 0: break
+
+    graph = [[] for i in range(n)]
+
+    for _ in range(m):
+        u, src, w = map(int, input().split())
+        graph[u].append((src, w))
+
+    d = [math.inf for i in range(n)]
+    pq = [(math.inf, i) for i in range(n)]
+    heapq.heapify(pq)
+
+    visited = [False for i in range(n)]
+    d[s] = 0
+    heapq.heappush(pq, (0, s))
+
+    for i in range(n):
+        while True:
+            _, src = heapq.heappop(pq)
+            if not visited[src]: break
+
+        visited[src] = True
+        for dest, weight in graph[src]:
+            if d[src] + weight < d[dest]:
+                d[dest] = d[src] + weight
+                heapq.heappush(pq, (d[dest], dest))
+
+    for _ in range(q):
+        src = int(input())
+        print("Impossible" if d[src] == math.inf else d[src])
+
+    print()
