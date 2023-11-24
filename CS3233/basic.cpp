cd522076c5f22be7f10a23ee7039edb5e8363645
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-11-24 20:10:03 +0800

    Update basic.cpp

--- CS3233/basic.cpp
+++ CS3233/basic.cpp
@@ -14,31 +14,33 @@ int main() {
     while (n--) {
         string line;
         cin >> line;
-        double radix = 0, integer = 0;
+        
+        double radix = 0, number = 0;
         int state = 1;
         bool valid = true;
+        
         for (char c : line) {
             if (!valid) break;
             int digit = ascii[c];
             switch (state) {
                 case 1:
                     valid = digit >= 0 && digit <= 9;
-                    if (valid) integer = digit, state = 2;
+                    if (valid) number = digit, state = 2;
                     break;
                 case 2:
-                    if (c == '#') valid = integer >= 2 && integer <= 16, radix = integer, integer = 0, state = 3;
-                    else valid = digit >= 0 && digit <= 9, integer = integer * 10 + digit;
+                    if (c == '#') valid = number >= 2 && number <= 16, radix = number, number = 0, state = 3;
+                    else valid = digit >= 0 && digit <= 9, number = number * 10 + digit;
                     break;
                 case 3:
                     valid = digit >= 0 && digit < radix;
-                    if (valid) integer = digit, state = 4;
+                    if (valid) number = digit, state = 4;
                     break;
                 case 4:
                     if (c == '#') state = 5;
-                    else valid = digit >= 0 && digit < radix, integer = integer * radix + digit;
+                    else valid = digit >= 0 && digit < radix, number = number * radix + digit;
                     break;
                 case 5:
-                    valid = c == '#' && integer >= 2 && integer <= 16, radix = integer, integer = 0, state = 3;
+                    valid = c == '#' && number >= 2 && number <= 16, radix = number, number = 0, state = 3;
                     break;
             }
         }
