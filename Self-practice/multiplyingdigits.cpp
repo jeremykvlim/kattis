66ceb06f5b18014b8c7edeaa1ad4b36e6dd17553
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-11-25 23:56:59 +0800

    Create multiplyingdigits.cpp

---
+++ Self-practice/multiplyingdigits.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long f(long long n, long long b, int size, vector<int> &factors, unordered_set<int> &primes) {
+    if (n < b) return n;
+    auto x = LLONG_MAX;
+    for (int i = size, depth = 0; i >= 0 && factors[i] * factors[i] >= b; i--) {
+        if (n % factors[i] ||
+        any_of(primes.begin(), primes.end(), [&](int p){return factors[i] * p < b && !(n % (factors[i] * p));})) continue;
+        auto next = f(n / factors[i], b, i, factors, primes) * b + factors[i];
+        if (next <= 0) continue;
+        x = min(x, next);
+        if (depth++ == 3) break;
+    }
+    return x;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long long b, n;
+    cin >> b >> n;
+
+    if (n == 1) {
+        cout << "1\n";
+        exit(0);
+    }
+
+    vector<int> factors;
+    for (int i = 2; i < b; i++) if (!(n % i)) factors.emplace_back(i);
+
+    unordered_set<int> prime_factors;
+    long long quotient = n;
+    for (int &factor: factors)
+        while (!(quotient % factor)) {
+            quotient /= factor;
+            prime_factors.insert(factor);
+        }
+
+    if (quotient > 1) {
+        cout << "impossible";
+        exit(0);
+    }
+
+    cout << f(n, b, factors.size() - 1, factors, prime_factors);
+}
