9ed8118d3a861408a186af224cb0b488c6c89eb1
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-11-27 21:14:25 +0800

    Create rollercoasterfun.cpp

---
+++ CS3233/rollercoasterfun.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n;
+    cin >> n;
+    vector<int> a(n), b(n), s(n);
+    for (int i = 0; i < n; i++) cin >> a[i] >> b[i] >> s[i];
+
+    vector<vector<long long>> dp(n + 1, vector<long long>(25001, 0));
+    for (int i = n - 1; i >= 0; i--)
+        for (int j = 0; j < 25001; j++) {
+            long long best = 0;
+            if (!b[i]) {
+                if (j >= s[i]) best = max(best, a[i] + dp[i][j - s[i]]);
+                best = max(best, dp[i + 1][j]);
+            } else {
+                long long sum = 0;
+                for (int k = 1; a[i] - (k - 1) * (k - 1) * b[i] > 0; k++) {
+                    sum += a[i] - (k - 1) * (k - 1) * b[i];
+                    if (k * s[i] <= j) best = max(best, sum + dp[i + 1][j - k * s[i]]);
+                    else break;
+                }
+                best = max(best, dp[i + 1][j]);
+            }
+            dp[i][j] = best;
+        }
+
+
+    int q;
+    cin >> q;
+    for (int i = 0; i < q; i++) {
+        int t;
+        cin >> t;
+        cout << dp[0][t] << "\n";
+    }
+}
