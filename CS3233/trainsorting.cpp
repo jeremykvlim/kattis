d15b5cf1a2c405c5ea38c0b7d5218354ae569aa6
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-11-27 14:42:21 +0800

    Create trainsorting.cpp

---
+++ CS3233/trainsorting.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n;
+    cin >> n;
+
+    vector<int> weight(n), lis(n, 1), lds(n, 1);
+
+    for (auto &w : weight) cin >> w;
+
+    int cars = 0;
+    for (int i = n - 1; i >= 0; --i) {
+        for (int j = i + 1; j < n; ++j) {
+            if (weight[j] > weight[i]) lis[i] = max(lis[i], lis[j] + 1);
+            if (weight[j] < weight[i]) lds[i] = max(lds[i], lds[j] + 1);
+        }
+        cars = max(cars, lis[i] + lds[i] - 1);
+    }
+
+    cout << cars;
+}
