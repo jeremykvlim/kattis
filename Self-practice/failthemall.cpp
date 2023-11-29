48b4cd3528cc314b54a0a2c4a8167fe66540529c
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-11-29 19:58:24 +0800

    Create failthemall.cpp

---
+++ Self-practice/failthemall.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool check(int i, char c, string& key, vector<string>& answers) {
+    if (key[i] != '.') return key[i] == c;
+    key[i] = c;
+    for (auto &s : answers)
+        if (s[i] == c)
+            for (int j = 0; j < s.size(); j++)
+                if (j != i && s[j] != 'X' && !check(j, s[j] ^ 'F' ^ 'T', key, answers)) return false;
+    return true;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n, k;
+    cin >> n >> k;
+
+    vector<string> answers(n);
+    for (auto &s : answers) cin >> s;
+
+    string key(k, '.');
+    bool exists = true;
+    for (int i = 0; i < k && exists; i++)
+        if (key[i] == '.')
+            for (int j = 0; exists; j++) {
+                string save = key;
+                if (check(i, "FT"[j], key, answers)) break;
+                key = save;
+                if (j) exists = false;
+            }
+
+    cout << (exists ? key : "-1");
+}
