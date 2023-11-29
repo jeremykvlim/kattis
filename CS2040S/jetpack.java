2aec898edda8cb9a5e88b7981620db591f9de3b9
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-11-29 23:02:48 +0800

    Create jetpack.java

---
+++ CS2040S/jetpack.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class jetpack {
+    static boolean dfs(int i, int j, int n, char[][] field, ArrayList<Integer> moves) {
+        if (field[i][j] == 'X') return false;
+        if (j == n - 1 || dfs(Math.min(9, i + 1), j + 1, n, field, moves)) return true;
+        field[i][j] = 'X';
+        if (dfs(Math.max(0, i - 1), j + 1, n, field, moves)) {
+            moves.add(j);
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) throws IOException {
+        var br = new BufferedReader(new InputStreamReader(System.in));
+        var pw = new PrintWriter(System.out);
+
+        int n = Integer.parseInt(br.readLine());
+        char[][] field = new char[10][n];
+        for (int i = 0; i < field.length; i++) field[i] = br.readLine().toCharArray();
+
+        var moves = new ArrayList<Integer>();
+        dfs(9, 0, n, field, moves);
+        Collections.reverse(moves);
+
+        pw.println(moves.size());
+        moves.forEach(t -> pw.println(t + " 1"));
+        pw.flush();
+    }
+}
