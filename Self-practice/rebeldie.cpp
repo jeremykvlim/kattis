813e8a22b2f7b089784e5dbe13bf3d237b35dbe2
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-11-29 18:41:14 +0800

    Update rebeldie.cpp

--- Self-practice/rebeldie.cpp
+++ Self-practice/rebeldie.cpp
@@ -6,36 +6,80 @@ using namespace __gnu_pbds;
 struct Die {
     int up, down, left, right, front, back, height, width;
 
-    bool operator==(const Die &s) const {
-        return tie(up, down, left, right, front, back, height, width) == tie(s.up, s.down, s.left, s.right, s.front, s.back, s.height, s.width);
+    bool operator==(const Die &d) const {
+        return tie(up, down, left, right, front, back, height, width) ==
+               tie(d.up, d.down, d.left, d.right, d.front, d.back, d.height, d.width);
     }
 
-    uint64_t key() const { return (((((((((((uint64_t(up) << 8) | down) << 8) | left) << 8) | right) << 8) | front) << 8) | back) << 16) | (height << 8) | width; }
+    uint64_t encode() const {
+        uint64_t encoded = 0;
+        encoded = (encoded << 8) | up;
+        encoded = (encoded << 8) | down;
+        encoded = (encoded << 8) | left;
+        encoded = (encoded << 8) | right;
+        encoded = (encoded << 8) | front;
+        encoded = (encoded << 8) | back;
+        encoded = (encoded << 16) | (height << 8) | width;
+        return encoded;
+    }
 };
 
 struct Hash {
-    static uint64_t h(uint64_t key) {
-        key += 0x9e3779b97f4a7c15;
-        key = (key ^ (key >> 30)) * 0xbf58476d1ce4e5b9;
-        key = (key ^ (key >> 27)) * 0x94d049bb133111eb;
-        return key ^ (key >> 31);
+     static uint64_t h(uint64_t key) {
+        uint64_t hash = key + 0x9e3779b97f4a7c15;
+        hash = (hash ^ (hash >> 30)) * 0xbf58476d1ce4e5b9;
+        hash = (hash ^ (hash >> 27)) * 0x94d049bb133111eb;
+        hash = hash ^ (hash >> 31);
+        return hash;
     }
 
     size_t operator()(const Die &d) const {
         static const uint64_t SEED = chrono::steady_clock::now().time_since_epoch().count();
-        return h(d.key() + SEED);
+        return h(d.encode() + SEED);
     }
 };
 
 void rotate(Die &s, char direction) {
     switch (direction) {
-        case 'N': swap(s.down, s.back), swap(s.back, s.up), swap(s.up, s.front); break;
-        case 'S': swap(s.front, s.up), swap(s.up, s.back), swap(s.back, s.down); break;
-        case 'E': swap(s.down, s.right), swap(s.right, s.up), swap(s.up, s.left); break;
-        case 'W': swap(s.left, s.up), swap(s.up, s.right), swap(s.right, s.down); break;
+        case 'N':
+            swap(s.down, s.back), swap(s.back, s.up), swap(s.up, s.front);
+            break;
+        case 'S':
+            swap(s.front, s.up), swap(s.up, s.back), swap(s.back, s.down);
+            break;
+        case 'E':
+            swap(s.down, s.right), swap(s.right, s.up), swap(s.up, s.left);
+            break;
+        case 'W':
+            swap(s.left, s.up), swap(s.up, s.right), swap(s.right, s.down);
+            break;
     }
 }
 
+void dfs(Die curr, vector<vector<int>> &board, gp_hash_table<Die, null_type, Hash> &visited,
+         vector<pair<pair<int, int>, char>> &directions) {
+
+    if (curr.height + 1 == board.size() && curr.width + 1 == board[0].size()) {
+        cout << "YES";
+        exit(0);
+    }
+
+    for (auto d : directions) {
+        Die next = curr;
+        next.height += d.first.first;
+        next.width += d.first.second;
+        if (next.height < 0 || next.width < 0 || next.height >= board.size() || next.width >= board[0].size()) continue;
+        rotate(next, d.second);
+
+        if (next.down == -1) next.down = board[next.height][next.width];
+        if (next.down != board[next.height][next.width]) continue;
+
+        if (visited.find(next) == visited.end()) {
+            visited.insert(next);
+            dfs(next, board, visited, directions);
+        }
+    }
+}
 
 int main() {
     ios::sync_with_stdio(false);
@@ -45,44 +89,18 @@ int main() {
     cin >> m >> n;
 
     vector<vector<int>> board(m, vector<int>(n));
-    for (auto &row : board)
-        for (int &square : row) cin >> square;
+    for (auto &row: board)
+        for (int &square: row) cin >> square;
 
-    vector<pair<pair<int, int>, char>> directions = {{{-1, 0},  'N'},
-                                                     {{1,  0},  'S'},
+    vector<pair<pair<int, int>, char>> directions = {{{-1, 0}, 'N'},
+                                                     {{1,  0}, 'S'},
                                                      {{0,  -1}, 'W'},
-                                                     {{0,  1},  'E'}};
-
+                                                     {{0,  1}, 'E'}};
 
     gp_hash_table<Die, null_type, Hash> visited;
-    stack<Die> dfs;
-    dfs.push({-1, board[0][0], -1, -1, -1, -1, 0, 0});
+    Die start = {-1, board[0][0], -1, -1, -1, -1, 0, 0};
+    visited.insert(start);
+    dfs(start, board, visited, directions);
 
-    while (!dfs.empty()) {
-        Die curr = dfs.top();
-        dfs.pop();
-
-        if (curr.height == m - 1 && curr.width == n - 1) {
-            cout << "YES";
-            exit(0);
-        }
-
-        for (auto d : directions) {
-            Die next = curr;
-            next.height += d.first.first;
-            next.width += d.first.second;
-            if (next.height < 0 || next.width < 0 || next.height >= m || next.width >= n) continue;
-            rotate(next, d.second);
-
-            if (next.down == -1) next.down = board[next.height][next.width];
-            if (next.down != board[next.height][next.width]) continue;
-
-            if (visited.find(next) == visited.end()) {
-                visited.insert(next);
-                dfs.push(next);
-            }
-        }
-    }
-  
     cout << "NO";
 }
