9ff8a75fb02dc050a3883e7ac9ea81fdd75df37e
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-07 18:27:12 +0800

    Update treehugging.cpp

--- Self-practice/treehugging.cpp
+++ Self-practice/treehugging.cpp
@@ -8,49 +8,48 @@ int main() {
     int n;
     cin >> n;
     vector<set<pair<int, int>>> adj_list(2 * n);
-    vector<int> leaves;
+    stack<int> leaves;
 
-    for (int e = 0; e < 2 * (n - 1); ++e) {
+    for (int e = 0; e < 2 * (n - 1); e++) {
         int u, v;
         cin >> u >> v;
         adj_list[v].insert({n + u, e});
         adj_list[n + u].insert({v, e});
     }
 
-    for (int u = 2; u < 2 * n; ++u) {
+    for (int u = 2; u < 2 * n; u++) {
         if (adj_list[u].empty()) {
             cout << "impossible";
             exit(0);
         }
-        if (adj_list[u].size() == 1) leaves.push_back(u);
+        if (adj_list[u].size() == 1) leaves.emplace(u);
     }
 
-    string solution(2*(n - 1), ' ');
+    string s(2 * (n - 1), ' ');
 
     while (!leaves.empty()) {
-        int u = leaves.back();
-        leaves.pop_back();
+        int u = leaves.top();
+        leaves.pop();
         if (adj_list[u].empty()) {
             cout << "impossible";
             exit(0);
         }
-        
+
         auto [v, e] = *adj_list[u].begin();
         adj_list[u].erase(adj_list[u].begin());
         adj_list[v].erase({u, e});
-        solution[e] = u <= n ? 'L' : 'R';
-        if (adj_list[v].size() == 1) leaves.push_back(v);
+        s[e] = u <= n ? 'L' : 'R';
+        if (adj_list[v].size() == 1) leaves.emplace(v);
     }
 
-    for (int u = 2; u < 2 * n; ++u)
+    for (int u = 2; u < 2 * n; u++)
         while (!adj_list[u].empty()) {
-            
             auto [v, e] = *adj_list[u].begin();
             adj_list[u].erase(adj_list[u].begin());
             adj_list[v].erase({u, e});
-            solution[e] = u <= n ? 'L' : 'R';
+            s[e] = u <= n ? 'L' : 'R';
             u = v;
         }
 
-    cout << solution;
+    cout << s;
 }
