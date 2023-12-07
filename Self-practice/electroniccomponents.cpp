836f9d48812ae9e388eed56afddafaaf76caa05d
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-08 00:43:37 +0800

    Create electroniccomponents.cpp

---
+++ Self-practice/electroniccomponents.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    vector<pair<int, long long>> components(n);
+    for (auto &c : components) cin >> c.first >> c.second;
+    sort(components.begin(), components.end(), [&](pair<int, int> p1, pair<int, int> p2) {
+            return p1.second > p2.second;
+    });
+
+    vector<vector<pair<int, long long>>> placements(max_element(components.begin(), components.end())->first + 1);
+    vector<long long> dp(max_element(components.begin(), components.end())->first + 1, LLONG_MAX);
+    dp[0] = 0;
+    for (int i = 0; i < n; i++) {
+        int f = components[i].first;
+        long long t = components[i].second;
+        for (int j = 0; j < dp.size(); j++) {
+            if (dp[j] == LLONG_MAX) continue;
+            placements[f + j - 2 * min(f, j)].emplace_back(f + j, dp[j] + (f - min(j, f)) * t);
+        }
+
+        for (int j = 0; j < 2; j++) {
+            priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
+            for (int k = j; k < dp.size(); k += 2) {
+                while (!pq.empty() && pq.top().second < k) pq.pop();
+                for (auto &p : placements[k]) pq.emplace(p.second - k / 2 * t, p.first);
+                placements[k].clear();
+                dp[k] = !pq.empty() ? pq.top().first + k / 2 * t : LLONG_MAX;
+            }
+        }
+    }
+
+    cout << *min_element(dp.begin(), dp.end());
+}
