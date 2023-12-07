ea3eaccd6584cf8fd885cf2322497202e81f7547
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-07 21:09:38 +0800

    Update orders.cpp

--- CS3233/orders.cpp
+++ CS3233/orders.cpp
@@ -7,33 +7,31 @@ int main() {
 
     int n, m;
     cin >> n;
-    vector<int> c(n);
-    for (int i = 0; i < n; i++) cin >> c[i];
+    vector<int> costs(n);
+    for (auto &c : costs) cin >> c;
 
     cin >> m;
-    vector<int> o(m);
-    for (int i = 0; i < m; i++) cin >> o[i];
-    m = *max_element(o.begin(), o.end());
+    vector<int> orders(m);
+    for (auto &s : orders) cin >> s;
+    m = *max_element(orders.begin(), orders.end());
     vector<long long> dp(m + 1, 0);
 
     dp[0] = 1;
     for (int i = 0; i < n; i++)
-        for (int j = c[i]; j <= m; j++)
-            dp[j] += dp[j - c[i]];
+        for (int j = costs[i]; j <= m; j++) dp[j] += dp[j - costs[i]];
 
-    for (int s : o) {
+    for (int s : orders) {
         if (!dp[s]) cout << "Impossible\n";
         else if (dp[s] > 1) cout << "Ambiguous\n";
         else {
             vector<int> v;
             for (int i = n - 1; i >= 0 && s > 0; i--)
-                while (s >= c[i] && dp[s - c[i]]) {
-                    v.push_back(i + 1);
-                    s -= c[i];
+                while (s >= costs[i] && dp[s - costs[i]]) {
+                    v.emplace_back(i + 1);
+                    s -= costs[i];
                 }
 
             for (int i = v.size() - 1; i >= 0; i--) cout << v[i] << " ";
-            cout << endl;
         }
     }
 }
