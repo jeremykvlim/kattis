424b572594c9d6bce0a69bc61fd925c7e10f4aa5
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-07 21:20:50 +0800

    Update wipeyourwhiteboards.cpp

--- CS3233/wipeyourwhiteboards.cpp
+++ CS3233/wipeyourwhiteboards.cpp
@@ -10,17 +10,17 @@ pair<long long, long long> bezout(long long a, long long b) {
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
+
     int t;
     cin >> t;
     while (t--) {
         long long R, S, Q;
         cin >> R >> S >> Q;
         auto [A, B] = bezout(R, -S);
-        long long l = INT_MIN, r = INT_MAX, m;
-        while (r - l > 1) {
-            m = l + (r - l) / 2;
-            if (Q / __gcd(R, -S) * A + m * S / __gcd(R, -S) && Q / __gcd(R, -S) * -B - m * R / __gcd(R, -S) > 0) l = m;
+        long long l = INT_MIN, r = INT_MAX;
+        while (l + 1 < r) {
+            long long m = l + (r - l) / 2;
+            if (Q / __gcd(R, -S) * A + m * S / __gcd(R, -S) > 0 && Q / __gcd(R, -S) * -B - m * R / __gcd(R, -S) > 0) l = m;
             else r = m;
         }
         cout << Q / __gcd(R, -S) * A + l * S / __gcd(R, -S) << ' ' << Q / __gcd(R, -S) * -B - l * R / __gcd(R, -S) << '\n';
