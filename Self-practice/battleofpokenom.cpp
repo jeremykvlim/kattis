742bcaf25d48abc13f23a69647be87a88a17bc08
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-09 07:15:46 +0800

    Create battleofpokenom.cpp

---
+++ Self-practice/battleofpokenom.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool valid(vector<char> v) {
+    return v.empty() || (sort(v.begin(), v.end()), v.front() == v.back());
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int k;
+        cin >> k;
+        vector<char> steps(k * 2);
+        for (auto &s : steps) cin >> s;
+        int s = 0;
+        vector<int> points{0, 0}, remaining{k, k}, prev{-1, -1};
+
+        for (int i = 0; i < k * 2 && points[0] + remaining[0] >= points[1] && points[1] + remaining[1] >= points[0]; i++, s++) {
+            if (steps[i] == 'E') points[i % 2]++;
+            remaining[i % 2]--;
+            if (points[0] + remaining[0] < points[1] || points[1] + remaining[1] < points[0]) prev[i % 2] = i;
+        }
+
+        for (int c = 0; c < s; c++) {
+            vector<char> a, b;
+            for (int i = c; i < s; i++)
+                if (prev[i & 1] != i) (i & 1 ? a : b).emplace_back(steps[i]);
+
+            if (valid(a) && valid(b)) {
+                cout << s << ' ' << c << "\n";
+                break;
+            }
+        }
+    }
+}
