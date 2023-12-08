28498073036f4f1700b36d7441a7876cf76626a4
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-08 22:41:48 +0800

    Update babynames.cpp

--- CS2040C/babynames.cpp
+++ CS2040C/babynames.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
 using namespace std;
+using namespace __gnu_pbds;
 
 int main() {
     ios::sync_with_stdio(false);
