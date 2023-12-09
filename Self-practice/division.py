6d5886e3c26d8003395d83192af77092059baf08
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-09 15:57:27 +0800

    Create division.py

---
+++ Self-practice/division.py
@@ -0,0 +1,13 @@
+import sys, math
+
+for line in sys.stdin:
+    t, a, b = map(int, line.split())
+    f = f"({t}^{a}-1)/({t}^{b}-1)"
+    if t < 2 or a % b or (a - b) * math.log10(t) >= 99:
+        print(f, "is not an integer with less than 100 digits.")
+    elif a == b:
+        print(f, 1)
+    elif (t ** a - 1) // (t ** b - 1) >= 10 ** 99:
+        print(f, "is not an integer with less than 100 digits.")
+    else:
+        print(f, (t ** a - 1) // (t ** b - 1))
