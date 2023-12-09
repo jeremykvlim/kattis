044c615358ec73834415ff378fb7eeeb9c6d947e
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-09 17:51:40 +0800

    Update excludescoring.py

--- Self-practice/excludescoring.py
+++ Self-practice/excludescoring.py
@@ -5,9 +5,8 @@ contests = [sorted(map(int, input().split()), reverse=True)[:4] for _ in range(m
 rank = sum(sum(scores) > sum(contests[0]) for scores in contests[1:]) + 1
 diff = sorted(sum(contests[0]) - sum(scores[:3]) + 1 for scores in contests[1:] if sum(scores) <= sum(contests[0]))
 
-l = 0
-r = len(diff) + 1
-while l < r - 1:
+l, r = 0, len(diff) + 1
+while l + 1 < r:
     m = l + (r - l) // 2
     contestants = total = 0
     for i in range(m):
