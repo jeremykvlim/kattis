3f00249dd7cf0bd5ee242e0f4b2aaa6e0bdd7c9b
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-09 17:09:19 +0800

    Create cestarine.cpp

---
+++ Self-practice/cestarine.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    vector<int> entrance(n), exit(n), p(3);
+    for (int i = 0; i < n; i++) cin >> entrance[i] >> exit[i];
+
+    sort(entrance.begin(), entrance.end());
+    sort(exit.begin(), exit.end());
+
+    long long largest = (long long) max(entrance.back(), exit.back()) * n;
+    vector<long long> dp(n + 1, largest);
+    dp[n] = 0;
+    for (int i = n - 1; i >= 0; i--)
+        for (int j = 1; j <= 3 && i + j <= n; j++) {
+            iota(p.begin(), p.begin() + j, 0);
+            do {
+                long long cost = 0;
+                for (int k = 0; k < j; k++) cost += (!abs(entrance[i + k] - exit[i + p[k]])) ? largest : abs(entrance[i + k] - exit[i + p[k]]);
+                dp[i] = min(dp[i], cost + dp[i + j]);
+            } while (next_permutation(p.begin(), p.begin() + j));
+        }
+
+    cout << dp[0];
+}
