0d6ce3336d8b0e6d1830e2317caf32c6507405f0
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-10 19:36:26 +0800

    Update electroniccomponents.cpp

--- Self-practice/electroniccomponents.cpp
+++ Self-practice/electroniccomponents.cpp
@@ -11,22 +11,23 @@ int main() {
     vector<pair<int, long long>> components(n);
     for (auto &c : components) cin >> c.first >> c.second;
     sort(components.begin(), components.end(), [&](pair<int, int> p1, pair<int, int> p2) {
-            return p1.second > p2.second;
+        return p1.second > p2.second;
     });
-
-    vector<vector<pair<int, long long>>> placements(max_element(components.begin(), components.end())->first + 1);
-    vector<long long> dp(max_element(components.begin(), components.end())->first + 1, LLONG_MAX);
+    int most = max_element(components.begin(), components.end())->first;
+    vector<vector<pair<int, long long>>> placements(most + 1);
+    vector<long long> dp(most + 1, LLONG_MAX);
+    
     dp[0] = 0;
     for (int i = 0; i < n; i++) {
         int f = components[i].first;
-        long long t = components[i].second;
+        auto t = components[i].second;
         for (int j = 0; j < dp.size(); j++) {
             if (dp[j] == LLONG_MAX) continue;
             placements[f + j - 2 * min(f, j)].emplace_back(f + j, dp[j] + (f - min(j, f)) * t);
         }
 
         for (int j = 0; j < 2; j++) {
-            priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
+            priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<>> pq;
             for (int k = j; k < dp.size(); k += 2) {
                 while (!pq.empty() && pq.top().second < k) pq.pop();
                 for (auto &p : placements[k]) pq.emplace(p.second - k / 2 * t, p.first);
