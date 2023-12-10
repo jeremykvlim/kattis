aeef1263fcf1d9884c0767a21a25064af99ee80a
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-10 20:50:45 +0800

    Update magicsequence.cpp

--- CS2040C/magicsequence.cpp
+++ CS2040C/magicsequence.cpp
@@ -22,14 +22,16 @@ int main() {
 
     int tc;
     cin >> tc;
+    
     while (tc--) {
         long long n, a, b, c, v = 0, x, y;
         cin >> n >> a >> b >> c >> x >> y;
+        
         vector<long long> s(n);
         s[0] = a;
         for (int i = 1; i < n; i++) s[i] = (s[i - 1]*b + a) % c;
         radix_sort(s);
-        for (int i = 0; i < n; i++) v = (v*x + s[i]) % y;
+        for (int i = 0; i < n; i++) v = (v * x + s[i]) % y;
         cout << v << "\n";
     }
 }
