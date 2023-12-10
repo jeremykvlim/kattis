3f3aea125b06a1a79c3c040462b35a86d39a6bc9
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-10 20:04:56 +0800

    Update greetings.cpp

--- Self-practice/greetings.cpp
+++ Self-practice/greetings.cpp
@@ -7,30 +7,32 @@ int main() {
 
     int n, k;
     cin >> n >> k;
-    vector<tuple<long long, long long, long long>> cards(n);
-    vector<long long> waste(1 << n);
-    vector<vector<long long>> dp(max(n, k), vector<long long>((1 << n), 1e16));
 
+    vector<tuple<int, int, int>> cards(n);
     for (auto &c : cards) cin >> get<0>(c) >> get<1>(c) >> get<2>(c);
 
+    vector<long long> waste(1 << n);
+    vector<vector<long long>> dp(max(n, k) + 1, vector<long long>(1 << n, 1e13));
+
     for (int i = 0; i < 1 << n; i++) {
-        long long widest = 0, highest = 0;
-        for (int j = 0; j < n; j++) 
+        int w = 0, h = 0;
+        for (int j = 0; j < n; j++)
             if (i & (1 << j)) {
-                widest = max(widest, get<0>(cards[j]));
-                highest = max(highest, get<1>(cards[j]));
+                w = max(w, get<0>(cards[j]));
+                h = max(h, get<1>(cards[j]));
             }
 
         waste[i] = 0;
         for (int j = 0; j < n; j++)
-            if (i & (1 << j)) waste[i] += (widest * highest - get<0>(cards[j]) * get<1>(cards[j])) * get<2>(cards[j]);
+            if (i & (1 << j)) waste[i] += ((long long) w * h - (long long) get<0>(cards[j]) * get<1>(cards[j])) * get<2>(cards[j]);
     }
-    
+
     dp[0][0] = 0;
     for (int i = 1; i <= k; i++)
         for (int j = 0; j < 1 << n; j++)
             for (int mask = j; mask > 0; mask = (mask - 1) & j)
                 dp[i][j] = min(dp[i][j], dp[i - 1][j & ~mask] + waste[mask]);
 
-    cout << dp[min(k, n)][(1 << n) - 1];
+
+    cout << dp[min(n, k)][(1 << n) - 1];
 }
