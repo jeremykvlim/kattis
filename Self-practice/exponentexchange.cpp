ecfc783e445812bb77cc716ddd42bcdd63f04dff
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-10 19:38:33 +0800

    Update exponentexchange.cpp

--- Self-practice/exponentexchange.cpp
+++ Self-practice/exponentexchange.cpp
@@ -4,10 +4,10 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
+
     int b, p;
     cin >> b >> p;
-    
+
     vector<int> digits(p);
     for (auto &x : digits) cin >> x;
 
@@ -16,12 +16,12 @@ int main() {
         vector<vector<pair<int, int>>> curr(2);
         for (int j = 0; j < 2; j++) {
             sort(prev[j].begin(), prev[j].end());
-            int min = INT_MAX;
+            int least = INT_MAX;
             for (auto [alice, bob]: prev[j]) {
-                if (bob >= min) continue;
-                min = bob;
-                curr[0].push_back({alice + (digits[i] + j), bob});
-                curr[1].push_back({alice, bob + (b - digits[i] - j)});
+                if (bob >= least) continue;
+                least = bob;
+                curr[0].emplace_back(alice + (digits[i] + j), bob);
+                curr[1].emplace_back(alice, bob + (b - digits[i] - j));
             }
         }
         prev.swap(curr);
