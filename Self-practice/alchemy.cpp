dc8ded5acf6e57ed7a428b752d0a5fa443ac1b89
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-11 18:01:37 +0800

    Create alchemy.cpp

---
+++ Self-practice/alchemy.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Circle {
+    int x, y, r, a, b, i;
+
+    Circle(int x = 0, int y = 0, int r = 0, int a = 0, int b = 0, int i = 0): x(x), y(y), r(r), a(a), b(b), i(i) {}
+
+    bool operator<(const Circle &c) const {
+        return r < c.r;
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    vector<Circle> circles(n);
+    for (int i = 0; i < n; i++) {
+        cin >> circles[i].x >> circles[i].y >> circles[i].r >> circles[i].a >> circles[i].b;
+        circles[i].i = i + 1;
+    }
+    sort(circles.begin(), circles.end());
+
+    vector<int> energy(n, 0), best(n, 0), ancestors(n, 0), parent(n, 0), required(n, 0);
+    int most = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++)
+            if (pow(circles[i].x - circles[j].x, 2) + pow(circles[i].y - circles[j].y, 2) < pow(circles[j].r, 2)) {
+                if (!parent[i]) parent[i] = j;
+                energy[i] += (++ancestors[i] & 1 ? circles[i].a : circles[i].b);
+                best[i] = max(best[i], energy[i]);
+            }
+
+        most += best[i];
+        int curr = energy[i];
+        for (int j = ancestors[i]; j >= 0; j--) {
+            if (curr == best[i]) required[i] = j;
+            curr -= (j & 1 ? circles[i].a : circles[i].b);
+        }
+    }
+
+    cout << most << "\n";
+
+    vector<bool> activated(n);
+    for (int i = 0; i < n; i++) {
+        if (i) cout << " ";
+        vector<bool> skip(n, false);
+        int draw = -1;
+        for (int j = 0; j < n; j++) {
+            if (skip[j]) skip[parent[j]] = true;
+            if (activated[j] || skip[j]) continue;
+            if (energy[j] == best[j] && (draw == -1 || circles[j].i < circles[draw].i)) draw = j;
+            if (ancestors[j] == required[j]) skip[parent[j]] = true;
+        }
+
+        activated[draw] = true;
+        for (int j = 0; j < draw; j++)
+            if (pow(circles[j].x - circles[draw].x, 2) + pow(circles[j].y - circles[draw].y, 2) < pow(circles[draw].r, 2)) energy[j] -= (ancestors[j]-- & 1 ? circles[j].a : circles[j].b);
+
+        cout << circles[draw].i;
+    }
+}
