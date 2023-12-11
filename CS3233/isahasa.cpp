1a2d9a750b57c64faf5845f28d212e00488639cd
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-11 16:24:14 +0800

    Create isahasa.cpp

---
+++ CS3233/isahasa.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+unordered_set<int> bfs(deque<int> q, vector<vector<int>> &adj_list) {
+    unordered_set<int> visited(q.begin(), q.end());
+    while (!q.empty()) {
+        int v = q.front();
+        q.pop_front();
+        for (int u : adj_list[v]) 
+            if (visited.emplace(u).second) q.emplace_back(u);
+    }
+    return visited;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+
+    unordered_map<string, int> indices;
+    vector<pair<int, int>> is, has;
+    for (int i = 0; i < n; i++) {
+        string c1, r, c2;
+        cin >> c1 >> r >> c2;
+        int a = indices.emplace(c1, indices.size()).first->second, b = indices.emplace(c2, indices.size()).first->second;
+        (r == "is-a" ? is : has).emplace_back(a, b);
+    }
+
+    int size = indices.size();
+    vector<vector<int>> is_adj_list(size), both_adj_list(size);
+    vector<unordered_set<int>> direct(size), transitive(size), superclasses(size), subclasses(size);
+
+    for (auto [a, b] : is) {
+        is_adj_list[a].emplace_back(b);
+        both_adj_list[a].emplace_back(b);
+    }
+
+    for (auto [a, b] : has) both_adj_list[a].emplace_back(b);
+
+    for (int i = 0; i < size; i++) {
+        superclasses[i] = bfs({i}, is_adj_list);
+        for (int c : superclasses[i]) subclasses[c].emplace(i);
+    }
+
+    for (auto [a, b] : has)
+        for (int c : subclasses[a]) direct[c].emplace(b);
+
+    for (int i = 0; i < size; i++) transitive[i] = bfs({direct[i].begin(), direct[i].end()}, both_adj_list);
+
+    for (int i = 1; i <= m; i++) {
+        string c1, r, c2;
+        cin >> c1 >> r >> c2;
+        cout << "Query " << i << ": " << ((r == "has-a" ? transitive : superclasses)[indices[c1]].count(indices[c2]) ? "true" : "false") << "\n";
+    }
+}
