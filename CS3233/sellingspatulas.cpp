e2fcadecc8f5a06e5c44bcb74ec63498588f4df6
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-12 00:48:14 +0800

    Create sellingspatulas.cpp

---
+++ CS3233/sellingspatulas.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    while (cin >> n && n) {
+        vector<int> times(n), pref(n);
+
+        for (int i = 0; i < n; i++) {
+            double p;
+            cin >> times[i] >> p;
+            pref[i] = i ? pref[i - 1] + round(p * 100) : round(p * 100);
+        }
+
+        int profit = -1, start = times.front(), end = times.back();
+        for (int i = 0; i < n; i++)
+            for (int j = i; j < n; j++) {
+                int curr = pref[j] - (i ? pref[i - 1] : 0) - (times[j] - times[i] + 1) * 8;
+                if (curr > profit || (curr == profit && times[j] - times[i] < end - start)) tie(start, end, profit) = make_tuple(times[i], times[j], curr);
+            }
+
+        if (profit > 0) cout << fixed << setprecision(2) << profit / 100.0 << " " << start << " " << end << "\n";
+        else cout << "no profit\n";
+    }
+}
