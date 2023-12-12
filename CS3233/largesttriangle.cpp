55dfa25c3ecfb77f7db144704ddc29bc34e18910
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-12 13:43:11 +0800

    Create largesttriangle.cpp

---
+++ CS3233/largesttriangle.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+double cross(pair<int, int> x, pair<int, int> y, pair<int, int> z) {
+    return static_cast<double>(z.first - x.first) * (y.second - x.second) - static_cast<double>(z.second - x.second) * (y.first - x.first);
+}
+
+int monotone(int n, vector<pair<int, int>> &points, vector<pair<int, int>> &convex) {
+    int size = 0;
+    sort(points.begin(), points.end());
+    points.erase(unique(points.begin(), points.end()), points.end());
+
+    for (int i = 0; i < n; i++) {
+        while (size > 1 && cross(convex[size - 2], convex[size - 1], points[i]) <= 0) size--;
+        convex[size++] = points[i];
+    }
+
+    int k = size;
+    for (int i = n - 2; i >= 0; i--) {
+        while (size > k && cross(convex[size - 2], convex[size - 1], points[i]) <= 0) size--;
+        convex[size++] = points[i];
+    }
+
+    return n > 1 ? size - 1 : size;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    vector<pair<int, int>> points(n), convex(n);
+    for (auto &[x, y] : points) cin >> x >> y;
+
+    int size = monotone(n, points, convex);
+    double area = 0;
+    if (size > 2)
+        for (int i = 0; i < size; i++)
+            for (int j = i + 1, k = j + 1; j < size; j++) {
+                while (k % size != i && cross(convex[i], convex[j], convex[(k + 1) % size]) >= cross(convex[i], convex[j], convex[k])) ++k %= size;
+                area = max(area, cross(convex[i], convex[j], convex[k % size]));
+            }
+
+    cout << fixed << setprecision(5) << area / 2.0;
+}
