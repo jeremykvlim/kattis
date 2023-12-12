de3ab0105d2bf93a7fed9e63df851dd475af8149
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-12 22:43:02 +0800

    Create distinctivecharacter.cpp

---
+++ CS3233/distinctivecharacter.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, k;
+    cin >> n >> k;
+
+    vector<bool> visited(1 << k);
+    queue<int> q;
+    while (n--) {
+        string s;
+        cin >> s;
+        int v = (int) bitset<20>(s).to_ulong();
+
+        if (!visited[v]) {
+            q.emplace(v);
+            visited[v] = true;
+        }
+    }
+
+    int tira;
+    while (!q.empty()) {
+        int v = q.front();
+        q.pop();
+
+        for (int i = 0; i < k; i++) {
+            int u = v ^ (1 << i);
+            if (!visited[u]) {
+                visited[u] = true;
+                q.emplace(u);
+                tira = u;
+            }
+        }
+    }
+
+    cout << bitset<20>(tira).to_string().substr(20 - k);
+}
