218e280ce3f6039b6923fc9f32d1c46b05261a79
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-15 05:47:30 +0800

    Update classy.java

--- CS2040/classy.java
+++ CS2040/classy.java
@@ -7,18 +7,18 @@ public class classy {
         int t = Integer.parseInt(br.readLine());
         while (t-- > 0) {
             int n = Integer.parseInt(br.readLine());
-            Person[] people = new Person[n];
+            var people = new Person[n];
 
             for (int i = 0; n > 0; n--, i++) {
-                String[] middle = new String[11];
+                var middle = new String[11];
                 Arrays.fill(middle, "middle");
                 var input = br.readLine().split(" ");
                 var name = input[0].replace(":", "");
                 var classes = input[1].split("-");
-                for (int j = classes.length - 1, k = 10; j >= 0; j--, k--)
-                    middle[k] = classes[j];
+                for (int j = classes.length - 1, k = 10; j >= 0; j--, k--) middle[k] = classes[j];
                 people[i] = new Person(name, middle);
             }
+            
             Arrays.sort(people);
             for (var p : people) System.out.println(p.name);
             for (int i = 0; i < 30; i++) System.out.print("=");
