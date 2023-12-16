26624b3fa1f8cf2e286d2c15e20c8e7cf4fc209d
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-17 01:01:10 +0800

    Create freecell.cpp

---
+++ Self-practice/freecell.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m, k;
+    cin >> n >> m >> k;
+
+    cout << ((k <= (n + 1) << m) ? "yes" : "no");
+}
