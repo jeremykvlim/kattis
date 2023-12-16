871151ac475ee3b5d9a5177de4c1302fe49b70c6
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-17 04:02:10 +0800

    Create meowfactor2.cpp

---
+++ Self-practice/meowfactor2.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    string s;
+    cin >> s;
+
+    vector<string> patterns = {"meow", "moew|meo|eow|me.?o?w|m.?ow", "me|eo|ow|m.?o|m.?.?w|e.?w", "m|e|o|w"};
+    int factor = 4;
+
+    for (int i = 0; i < patterns.size(); i++)
+        if (regex_search(s, regex(patterns[i]))) {
+            factor = min(factor, i);
+            break;
+        }
+
+    cout << factor;
+}
