a5a702480336b43996c82f31281c3f758d5d15d6
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-17 23:55:04 +0800

    Create costlycontest.cpp

---
+++ Self-practice/costlycontest.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m, k, t;
+    cin >> n >> m >> k >> t;
+
+    vector<int> slowness(n);
+    for (int &s : slowness) cin >> s;
+
+    bitset<(int) 1e5> solvable;
+    solvable[0] = true;
+    for (int i = 0; i < m; i++) {
+        int d;
+        cin >> d;
+
+        solvable |= solvable << d;
+    }
+
+    int total = t / *min_element(slowness.begin(), slowness.end()), threshold = INT_MAX;
+    while (!solvable[total]) total--;
+    if (total > 0) threshold = t / total + 1;
+
+    cout << max(k, (int) count_if(slowness.begin(), slowness.end(), [&](int s) {return s < threshold;}));
+}
