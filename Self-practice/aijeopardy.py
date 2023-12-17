65955d315d50eca962c63c70e9f07ad97e6e5cec
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-17 19:55:05 +0800

    Create aijeopardy.py

---
+++ Self-practice/aijeopardy.py
@@ -0,0 +1,24 @@
+x = int(input())
+(n, k) = (x, 1) if x > 1 else (0, 0)
+
+for i in range(2, min(x + 1, 200)):
+    l, r = i, x
+    while l + 1 < r:
+        m = l + (r - l) // 2
+        c = 1
+        for j in range(i):
+            c *= m - j
+            c //= j + 1
+            if c > x:
+                r = m
+                break
+        else:
+            if c == x:
+                (n, k) = min((n, k), (m, i))
+                break
+            elif c < x:
+                l = m
+            else:
+                r = m
+
+print(n, k)
