1243df8955b5678d6fed9860989a3a5f0c729bd8
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-17 16:40:25 +0800

    Create mixingdrinks.cpp

---
+++ Self-practice/mixingdrinks.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+constexpr int MODULO = 1e9 + 7;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, p;
+    cin >> n >> p;
+
+    vector<int> choice(n + 1, 0);
+    for (int i = 0; i < p; i++) {
+        int a, b;
+        cin >> a >> b;
+      
+        choice[max(a, b)] = max(choice[max(a, b)], min(a, b));
+    }
+
+    vector<long long> dp(n + 1, 1);
+    long long total = 1;
+    for (int i = 1, j = 0; i <= n; total += dp[i++]) {
+        while (j < choice[i]) total -= dp[j++];
+        total %= MODULO;
+        dp[i] = total;
+    }
+
+    cout << (dp.back() < 0 ? dp.back() + MODULO : dp.back());
+}
