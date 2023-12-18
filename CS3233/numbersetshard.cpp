acdefbcfce389e861b652dc904228d5f85a90309
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-18 15:43:49 +0800

    Create numbersetshard.cpp

---
+++ CS3233/numbersetshard.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> sieve(int n) {
+    vector<int> factors(n + 1), primes{2};
+    iota(factors.begin(), factors.end(), 0);
+    for (int p = 3; p <= n; p += 2)
+        if (factors[p] == p) {
+            primes.emplace_back(p);
+            for (auto i = (long long) p * p; i <= n; i += 2 * p)
+                if (factors[i] == i) factors[i] = p;
+        }
+
+    return primes;
+}
+
+int find(int p, vector<int> &sets) {
+    return (sets[p] == p) ? p : (sets[p] = find(sets[p], sets));
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    vector<int> primes = sieve(1e6);
+    for (int x = 1; x <= t; x++) {
+        long long a, b, p;
+        cin >> a >> b >> p;
+
+        long long size = b - a + 1;
+        vector<int> sets(size);
+        iota(sets.begin(), sets.end(), 0);
+
+        for (int i = primes.size() - 1; i >= 0 && primes[i] >= p; i--) {
+            auto l = a + (primes[i] - a % primes[i]) % primes[i];
+            for (auto r = l + primes[i]; r <= b; r += primes[i]) {
+                auto l_set = find(l - a, sets), r_set = find(r - a, sets);
+                if (l_set != r_set) {
+                    size--;
+                    sets[r_set] = sets[l_set];
+                }
+            }
+        }
+
+        cout << "Case #" << x << ": " << size << "\n";
+    }
+}
