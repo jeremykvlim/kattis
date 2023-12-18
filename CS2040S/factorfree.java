2bec0a351d28b37a960911a616efa53f87b0e650
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-18 14:50:41 +0800

    Update factorfree.java

--- CS2040S/factorfree.java
+++ CS2040S/factorfree.java
@@ -14,33 +14,29 @@ public class factorfree {
             elements[i] = Integer.parseInt(st.nextToken());
             max = Math.max(max, elements[i]);
         }
+
         var factor = new int[max + 1];
         var primes = new ArrayList<Integer>();
         primes.add(0);
         sieve(factor, primes, max);
 
-        int biggest = -1;
-        for (int i : factor)
-            biggest = Math.max(biggest, i);
-
+        int biggest = Arrays.stream(factor).max().orElse(-1);
         int[] pos = new int[biggest + 1], prev = new int[n + 1], next = new int[n + 1], parent = new int[n + 1];
 
         Arrays.fill(pos, 1, primes.size(), -1);
         for (int i = 1; i <= n; i++) {
             prev[i] = 1;
-            for (int j = elements[i]; j > 1; j /= primes.get(factor[j]))
-                prev[i] = Math.max(prev[i], pos[factor[j]] + 1);
-            for (int j = elements[i]; j > 1; j /= primes.get(factor[j]))
-                pos[factor[j]] = i;
+            for (int j = elements[i]; j > 1; j /= primes.get(factor[j])) prev[i] = Math.max(prev[i], pos[factor[j]] + 1);
+            for (int j = elements[i]; j > 1; j /= primes.get(factor[j])) pos[factor[j]] = i;
         }
+
         Arrays.fill(pos, 1, primes.size(), n + 1);
         for (int i = n; i > 0; i--) {
             next[i] = n;
-            for (int j = elements[i]; j > 1; j /= primes.get(factor[j]))
-                next[i] = Math.min(next[i], pos[factor[j]] - 1);
-            for (int j = elements[i]; j > 1; j /= primes.get(factor[j]))
-                pos[factor[j]] = i;
+            for (int j = elements[i]; j > 1; j /= primes.get(factor[j])) next[i] = Math.min(next[i], pos[factor[j]] - 1);
+            for (int j = elements[i]; j > 1; j /= primes.get(factor[j])) pos[factor[j]] = i;
         }
+
         if (dfs(1, n, 0, prev, next, parent)) {
             for (int i = 1; i <= n; i++) pw.print(parent[i] + " ");
             pw.println();
@@ -48,15 +44,16 @@ public class factorfree {
         pw.flush();
     }
 
-    static void sieve(int[] factor, ArrayList<Integer> primes, int max_element) {
-        var prime = new BitSet(max_element + 1);
-        prime.set(2, max_element + 1);
-        for (int i = 2; i <= max_element; i++) {
+    static void sieve(int[] factor, ArrayList<Integer> primes, int max) {
+        var prime = new BitSet(max + 1);
+        prime.set(2, max + 1);
+        for (int i = 2; i <= max; i++) {
             if (prime.get(i)) {
                 primes.add(i);
                 factor[i] = primes.size() - 1;
             }
-            for (int j = 1, k; j < primes.size() && (k = i * primes.get(j)) <= max_element; j++) {
+
+            for (int j = 1, k; j < primes.size() && (k = i * primes.get(j)) <= max; j++) {
                 factor[k] = factor[i];
                 prime.clear(k);
                 if (i % primes.get(j) == 0) break;
@@ -76,9 +73,9 @@ public class factorfree {
             l = s.pop();
 
             if (l > r) continue;
-            
+
             var found = false;
-            for (int left = l, right = r; left <= right; left++, right--) 
+            for (int left = l, right = r; left <= right; left++, right--)
                 if ((l >= prev[left] && r <= next[left]) || (l >= prev[right] && r <= next[right])) {
                     int mid = (l >= prev[left] && r <= next[left]) ? left : right;
                     parent[mid] = p;
@@ -94,7 +91,7 @@ public class factorfree {
                     found = true;
                     break;
                 }
-            
+
             if (!found) return false;
         }
 
