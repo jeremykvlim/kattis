13d72e9ebb86e70361ca16f9a9aa3c67ccc2b3bb
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-20 01:50:40 +0800

    Create arcaeapartners.cpp

---
+++ Self-practice/arcaeapartners.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void update(vector<vector<int>> &fenwick, int i, int j, int value) {
+    while (i < fenwick[j].size()) {
+        fenwick[j][i] = max(fenwick[j][i], value);
+        i += i & -i;
+    }
+}
+
+int query(vector<vector<int>> &fenwick, int i, int j) {
+    int value = 0;
+    while (i) {
+        value = max(value, fenwick[j][i]);
+        i ^= i & -i;
+    }
+    return value;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, k;
+    cin >> n >> k;
+
+    vector<vector<int>> values;
+    map<int, int> steps;
+    for (int i = 0; i < n; i++) {
+        int g, p, ga, pa;
+        cin >> g >> p >> ga >> pa;
+        
+        steps[p];
+        values.push_back({g, p, 0});
+        if (ga && pa) {
+            steps[pa];
+            values.push_back({ga, pa, 1});
+        }
+    }
+
+    int count = 0;
+    for (auto &p : steps) p.second = ++count;
+    for (auto &v : values) v[1] = steps[v[1]];
+    sort(values.begin(), values.end(), [&](auto v1, auto v2) {return v1[0] != v2[0] ? v1[0] > v2[0] : v1[1] < v2[1];});
+
+    vector<vector<int>> fenwick(max(n, k) + 1, vector<int>(count + 1, 0));
+    for (auto v : values)
+        if (!v[2])
+            for (int i = 0; i <= k; i++) update(fenwick, v[1], i, query(fenwick, v[1], i) + 1);
+        else
+            for (int i = k; i >= 1; i--) update(fenwick, v[1], i, query(fenwick, v[1], i - 1) + 1);
+
+    int diversity = 0;
+    for (int i = 1; i <= n; i++) diversity = max(diversity, query(fenwick, count, i));
+    cout << diversity;
+}
