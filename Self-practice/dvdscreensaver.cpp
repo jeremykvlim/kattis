93c553a8d8261746f98beee46cdf1efe5e124763
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-21 04:44:13 +0800

    Create dvdscreensaver.cpp

---
+++ Self-practice/dvdscreensaver.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+pair<long long, long long> bezout(long long a, long long b) {
+    if (!a) return {0, 1};
+    auto [x, y] = bezout(b % a, a);
+    return {y - (b / a) * x, x};
+}
+
+long long congruence(long long base, long long b, long long mod) {
+    long long g = __gcd(base, mod);
+    if (b % g) return -1;
+
+    b /= g;
+    base /= g;
+    mod /= g;
+    auto [x, y] = bezout(base, mod);
+    x %= mod;
+    if (x < 0) x += mod;
+    return b * x % mod;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T;
+    cin >> T;
+
+    while (T--) {
+        long long ws, hs, wl, hl, xl, yl;
+        cin >> ws >> hs >> wl >> hl >> xl >> yl;
+
+        ws -= wl;
+        hs -= hl;
+        long long base = ws % hs, b = ((hs - yl) % hs - ((ws - xl) % ws) % hs + hs) % hs, t = congruence(base, b, hs);
+        cout << ((t == -1) ? "Johnny will die waiting\n" : to_string(t * ws + (ws - xl) % ws) + "\n");
+    }
+}
