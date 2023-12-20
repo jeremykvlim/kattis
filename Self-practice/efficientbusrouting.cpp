150e327a47bd39a0cd5bde1be89e29f31fc03666
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-20 16:00:06 +0800

    Update efficientbusrouting.cpp

--- Self-practice/efficientbusrouting.cpp
+++ Self-practice/efficientbusrouting.cpp
@@ -1,14 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> dfs(int v, int parent, vector<vector<int>> &adj) {
+vector<int> dfs(int v, int parent, vector<vector<int>> &adj_list) {
     vector<int> leaves;
-    for (int u : adj[v]) {
+    for (int u : adj_list[v]) {
         if (u == parent) continue;
-        auto children = dfs(u, v, adj);
+        auto children = dfs(u, v, adj_list);
         leaves.insert(leaves.end(), children.begin(), children.end());
     }
-    
+
     if (leaves.empty()) leaves.emplace_back(v + 1);
     return leaves;
 }
@@ -20,19 +20,19 @@ int main() {
     int n;
     cin >> n;
 
-    vector<vector<int>> adj(n);
+    vector<vector<int>> adj_list(n);
     for (int i = 0; i < n - 1; i++) {
         int u, v;
         cin >> u >> v;
-        adj[u - 1].emplace_back(v - 1);
-        adj[v - 1].emplace_back(u - 1);
+        adj_list[u - 1].emplace_back(v - 1);
+        adj_list[v - 1].emplace_back(u - 1);
     }
 
-    int root = distance(adj.begin(), find_if(adj.begin(), adj.end(), [&](auto neighbors) {return neighbors.size() > 1;}));
-    auto leaves = dfs(root, root, adj);
+    int root = distance(adj_list.begin(), find_if(adj_list.begin(), adj_list.end(), [&](auto neighbors) {return neighbors.size() > 1;}));
+    auto leaves = dfs(root, root, adj_list);
 
     int size = leaves.size();
     cout << (size + 1) / 2 << "\n";
     for (int i = 0; i < size / 2; i++) cout << leaves[i] << " " << leaves[i + size / 2] << "\n";
-    if (size & 1) cout << leaves[0] << " " << leaves.back();
+    if (size & 1) cout << leaves.front() << " " << leaves.back();
 }
