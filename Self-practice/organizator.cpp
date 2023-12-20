9da71be88fc9b61356bf3fe178e5e3ec344a640e
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-20 23:56:26 +0800

    Create organizator.cpp

---
+++ Self-practice/organizator.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+struct Hash {
+    static uint64_t h(uint64_t key) {
+        uint64_t hash = key + 0x9e3779b97f4a7c15;
+        hash = (hash ^ (hash >> 30)) * 0xbf58476d1ce4e5b9;
+        hash = (hash ^ (hash >> 27)) * 0x94d049bb133111eb;
+        hash = hash ^ (hash >> 31);
+        return hash;
+    }
+
+    size_t operator()(const int &i) const {
+        uint64_t key = 0 ^ (i + 0x9e3779b9 + (key << 6) + (key >> 2));
+        return h(key);
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    gp_hash_table<int, int, Hash> members;
+    int biggest = 0;
+    while (n--) {
+        int m;
+        cin >> m;
+
+        members[m]++;
+        biggest = max(biggest, m);
+    }
+
+    long long finalists = -1;
+    for (int i = 1; i <= biggest; i++) {
+        long long curr = 0;
+        for (int j = i; j <= biggest; j += i) curr += members[j];
+        if (curr > 1) finalists = max(finalists, curr * i);
+    }
+
+    cout << finalists;
+}
