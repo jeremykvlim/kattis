7812657fe0a9768e07c54302fe4d01f2781ce5c5
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-21 01:06:34 +0800

    Create exponentiallyfun.cpp

---
+++ Self-practice/exponentiallyfun.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+constexpr int MODULO = 1e9 + 7;
+
+vector<int> sieve(int n) {
+    vector<int> factors(n + 1), primes{2};
+    iota(factors.begin(), factors.end(), 0);
+    for (int p = 3; p <= n; p += 2)
+        if (factors[p] == p) {
+            primes.emplace_back(p);
+            for (auto i = (long long) p * p; i <= n; i += 2 * p)
+                if (factors[i] == i) factors[i] = p;
+        }
+
+    return primes;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    vector<int> primes = sieve(n + 1);
+    vector<long long> dp(n + 1, -1);
+    dp[0] = 1;
+    for (int p : primes)
+        for (int i = 0; i <= n - p; i++)
+            if (dp[i] != -1) dp[i + p] = (min(dp[i] * p, dp[i + p] != -1 ? dp[i + p] : LLONG_MAX)) % MODULO;
+
+    auto smallest = dp[n] % MODULO, biggest = 1LL, power = 1LL;
+    while (n % 3) {
+        biggest = (biggest << 1) % MODULO;
+        n -= 2;
+    }
+    for (int i = 0; i < n / 3; i++) power = (power * 3) % MODULO;
+    biggest = (biggest * power) % MODULO;
+
+    cout << smallest << " " << biggest;
+}
