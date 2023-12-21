9c9078f86a2b34e712c9c1fff50968cc57a03619
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-22 00:40:10 +0800

    Update excludescoring.cpp

---
+++ Self-practice/excludescoring.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    vector<int> points = {100, 75, 60, 50, 45, 40, 36, 32, 29, 26, 24, 22, 20, 18, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    int n, M;
+    cin >> n >> M;
+
+    vector<vector<int>> contests(M, vector<int>(n - 1));
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < n - 1; j++) cin >> contests[i][j];
+        sort(contests[i].rbegin(), contests[i].rend());
+        contests[i].resize(4);
+    }
+
+    int rank = 1;
+    for (int i = 1; i < M; i++)
+        if (accumulate(contests[i].begin(), contests[i].end(), 0) > accumulate(contests[0].begin(), contests[0].end(), 0)) rank++;
+
+    vector<int> diff;
+    for (int i = 1; i < M; i++)
+        if (accumulate(contests[i].begin(), contests[i].end(), 0) <= accumulate(contests[0].begin(), contests[0].end(), 0))
+            diff.emplace_back(accumulate(contests[0].begin(), contests[0].end(), 0) - accumulate(contests[i].begin(), contests[i].begin() + 3, 0) + 1);
+    sort(diff.begin(), diff.end());
+    
+    int l = 0, r = diff.size() + 1, m;
+    while (l + 1 < r) {
+        m = l + (r - l) / 2;
+
+        int contestants = 0, total = 0;
+        for (int i = 0; i < m; i++) {
+            contestants++;
+            total += 1 + (m - i <= 30 ? points[m - i - 1] : 0);
+            if ((total + contestants - 1) / contestants >= diff[i]) total = contestants = 0;
+        }
+
+        if (contestants) r = m;
+        else l = m;
+    }
+
+    cout << rank + l;
+}
