7710a7640fdb82fd068fb3725ce9d1e3c7bab69c
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-22 01:55:38 +0800

    Update profitabletrip.cpp

---
+++ Self-practice/profitabletrip.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m, w;
+    cin >> n >> m >> w;
+
+    vector<vector<pair<int, int>>> adj_list(n + 1);
+    while (m--) {
+        int u, v, t;
+        cin >> u >> v >> t;
+
+        adj_list[u].emplace_back(v, t);
+    }
+
+    queue<int> q;
+    vector<int> profit(n + 1, INT_MIN);
+    profit[1] = 0;
+    q.emplace(1);
+    while (!q.empty()) {
+        int v = q.front();
+        q.pop();
+
+        for (auto &u : adj_list[v])
+            if (profit[u.first] < min(w, u.second + profit[v])) {
+                profit[u.first] = min(w, u.second + profit[v]);
+                q.emplace(u.first);
+            }
+    }
+
+    cout << profit[n];
+}
