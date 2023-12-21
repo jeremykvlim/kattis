e7e2ded58977d9c0db163f54ad266e6b5fd76876
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-22 01:44:24 +0800

    Update perfectpowers.cpp

---
+++ Self-practice/perfectpowers.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long long x;
+    while (cin >> x && x) {
+        int p = 1;
+        for (long long b = 2; b * b <= abs(x); b++) {
+            auto a = b * b;
+            p = 2;
+            while (a < abs(x)) {
+                a *= b;
+                p++;
+            }
+
+            if (a == abs(x) && (x > 0 || p & 1)) break;
+            else p = 1;
+        }
+
+        cout << p << "\n";
+    }
+}
