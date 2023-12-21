cb84df7b148b950ac3a3a1e7d8902120e505cde6
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-21 17:14:43 +0800

    Create hashing.cpp

---
+++ CS3233/hashing.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+pair<__int128, __int128> bezout(__int128 a, __int128 b) {
+    if (!a) return {0, 1};
+    auto [x, y] = bezout(b % a, a);
+    return {y - (b / a) * x, x};
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string s;
+    int q;
+    cin >> s >> q;
+
+    vector<__int128> pref(s.size() + 1, 0), inv(s.size() + 1, 1);
+    __int128 base = 1, b = 1e16 + 61, mod = (1ULL << 63) + 27;
+    for (int i = 0; i < s.size(); i++) {
+        pref[i + 1] = (pref[i] + base * s[i]) % mod;
+        base = base * b % mod;
+
+        auto [x, y] = bezout(base, mod);
+        if (x < 0) x += mod;
+        inv[i + 1] = x;
+    }
+
+    while (q--) {
+        int l, r;
+        cin >> l >> r;
+
+        cout << (long long) (((pref[r] - pref[l] + mod) % mod) * inv[l] % mod) << "\n";
+    }
+}
