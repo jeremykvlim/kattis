105fc29e3220df8910ccb7091d685b8971cb27a2
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-22 01:00:56 +0800

    Update safety.cpp

---
+++ Self-practice/safety.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    for (int x = 0; x < t; x++) {
+        int n;
+        cin >> n;
+
+        vector<double> p(n);
+        double sum_p = 0;
+        for (int i = 0; i < n; i++) {
+            cin >> p[i];
+            sum_p += p[i];
+        }
+
+        double curr = 2 * sum_p / n, prev = 201;
+        while (curr < prev) {
+            prev = curr;
+            double sum_s = 0, count = 0;
+            for (auto v : p)
+                if (v <= curr) {
+                    sum_s += v;
+                    count++;
+                }
+
+            curr = (sum_p + sum_s) / count;
+        }
+
+        cout << "Case #" << x + 1 << ":";
+        for (auto v : p) cout << " " << fixed << setprecision(5) << max((curr - v) / sum_p * 100, 0.0);
+        cout << "\n";
+    }
+}
--- Self-practice/safety.py
+++
@@ -1,6 +0,0 @@
-for x in range(int(input())):
-    n, *p = map(int, input().split())
-    curr, prev = 2 * sum(p) / n, 201
-    while curr < prev:
-        prev, curr = curr, (sum(p) + sum(s for s in p if s <= curr)) / sum(1 for s in p if s <= curr)
-    print(f'Case #{x + 1}:', *("{:.5f}".format(max((curr - s) / sum(p) * 100, 0)) for s in p))
