7aeb183bec6618a3d5f7192f508e659ca4607c4c
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-22 17:22:44 +0800

    Create help.cpp

---
+++ CS3233/help.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n{};
+    while (cin >> n && n) {
+        vector<int> money(n);
+        for (int &m : money) cin >> m;
+
+        sort(money.begin(), money.end());
+        for (int i = 0; i < n; i++) cout << money[(n - i) % n] << "-" << (i & 1 ? "B" : "A") << (i == n - 1 ? "\n" : " ");
+    }
+}
