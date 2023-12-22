296c3292b715d6fee6fba7e22cf50e0b6b3ff772
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-22 20:24:07 +0800

    Create trip.cpp

---
+++ Self-practice/trip.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    while (cin >> n && n) {
+        vector<int> costs(n);
+        int total = 0;
+        for (int i = 0; i < n; ++i) {
+            double spent;
+            cin >> spent;
+
+            costs[i] = round(100 * spent);
+            total += costs[i];
+        }
+
+        sort(costs.rbegin(), costs.rend());
+        int remaining = total % n;
+        total /= n;
+        
+        int money = 0;
+        for (int i = 0; i < n; i++) money += i < remaining ? abs(costs[i] - total - 1) : abs(costs[i] - total);
+        cout << "$" << fixed << setprecision(2) << money / 200.0 << "\n";
+    }
+}
