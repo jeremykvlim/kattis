02797a7f8bda119c65110968114119addaef44c5
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-22 19:08:09 +0800

    Update finalexam.cpp

--- Self-practice/finalexam.cpp
+++ Self-practice/finalexam.cpp
@@ -34,14 +34,14 @@ int main() {
         biggest = max(c, biggest);
     }
 
-    vector<vector<int>> GCD(sqrt(biggest) + 1, vector<int>(sqrt(biggest) + 1, 0));
-    for (int i = 1; i < GCD.size(); i++)
-        for (int j = 1; j < i; j++) GCD[i][j] = i % j ? GCD[j][i % j] : j;
+    vector<vector<int>> g(sqrt(biggest) + 1, vector<int>(sqrt(biggest) + 1, 0));
+    for (int i = 1; i < g.size(); i++)
+        for (int j = 1; j < i; j++) g[i][j] = i % j ? g[j][i % j] : j;
 
     long long triplets = 0;
     for (int m = 1; m * m <= biggest; m++)
         for (int n = 1; n < m && m * m + n * n <= biggest; n++) {
-            if (GCD[m][n] > 1 || (m & 1) == (n & 1)) continue;
+            if (g[m][n] > 1 || (m & 1) == (n & 1)) continue;
             int a = m * m - n * n, b = 2 * m * n, c = m * m + n * n;
             for (int ka = a, kb = b, kc = c; kc <= biggest; ka += a, kb += b, kc += c) triplets += (long long) (A[ka] * B[kb] + B[ka] * A[kb]) * C[kc];
         }
