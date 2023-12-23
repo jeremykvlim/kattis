25e55344aabd3f43fbcd9211e82e5f1af780b247
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-23 17:50:24 +0800

    Create pollygone.cpp

---
+++ CS3233/pollygone.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    double P;
+    cin >> n >> P;
+
+    vector<int> dp(10001, INT_MAX);
+    dp[0] = 0;
+    int j = 0;
+    while (n--) {
+        int e;
+        double p;
+        cin >> e >> p;
+
+        int prob = int(round(p * 10000));
+        for (int i = j; i >= 0; i--)
+            if (dp[i + prob] - e > dp[i]) {
+                dp[i + prob] = dp[i] + e;
+                j = max(j, i + prob);
+            }
+    }
+
+    cout << *min_element(dp.begin() + int(round(P * 10000)), dp.end());
+}
