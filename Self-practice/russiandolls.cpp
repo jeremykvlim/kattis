3fcc97ba9acc559b60e0f0b4c5268162f8f5af94
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-24 00:00:19 +0800

    Update russiandolls.cpp

--- Self-practice/russiandolls.cpp
+++ Self-practice/russiandolls.cpp
@@ -14,16 +14,19 @@ bool arrange(int doll1, int doll2, int counter, vector<Doll> &dolls, bool set, v
     int bigger = max(doll1, doll2);
     if (counter > (dolls.size() - 1) >> 1 || bigger - counter > (dolls.size() - 1) >> 1 || dp[doll1][doll2][counter]) return false;
     if (bigger++ == dolls.size() - 1) return true;
+    
     if ((dolls[bigger].h - 2 * dolls[bigger].w >= dolls[doll1].h && dolls[bigger].d - 2 * dolls[bigger].w >= dolls[doll1].d)
         && arrange(bigger, doll2, counter + 1, dolls, set, dp)) {
         if (set) cout << dolls[bigger].h << " " << dolls[bigger].d << " " << dolls[bigger].w << "\n";
         return true;
     }
+    
     if ((dolls[bigger].h - 2 * dolls[bigger].w >= dolls[doll2].h && dolls[bigger].d - 2 * dolls[bigger].w >= dolls[doll2].d)
         && arrange(doll1, bigger, counter, dolls, set, dp)) {
         if (!set) cout << dolls[bigger].h << " " << dolls[bigger].d << " " << dolls[bigger].w << "\n";
         return true;
     }
+    
     dp[doll1][doll2][counter] = true;
     return false;
 }
@@ -35,14 +38,14 @@ int main() {
 
     int n;
     while (cin >> n && n) {
+        vector<vector<vector<bool>>> dp(2 * n + 1, vector<vector<bool>>(2 * n + 1, vector<bool>(2 * n + 1, false)));
         vector<Doll> dolls(2 * n + 1);
-        vector<vector<vector<bool>>> memo(2 * n + 1, vector<vector<bool>>(2 * n + 1, vector<bool>(2 * n + 1, false)));
         for (int i = 1; i <= 2 * n; i++) cin >> dolls[i].h >> dolls[i].d >> dolls[i].w;
         sort(dolls.begin(), dolls.end());
 
-        arrange(0, 0, 0, dolls, false, memo);
+        arrange(0, 0, 0, dolls, false, dp);
         cout << "-\n";
-        arrange(0, 0, 0, dolls, true, memo);
+        arrange(0, 0, 0, dolls, true, dp);
         cout << "\n";
     }
 }
