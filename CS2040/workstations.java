4be85ebb873e39740ca801a3ba50b0fb1dcdabe8
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-23 18:22:37 +0800

    Update workstations.java

--- CS2040/workstations.java
+++ CS2040/workstations.java
@@ -1,26 +1,30 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class workstations {
     public static void main(String[] args) throws IOException {
         var br = new BufferedReader(new InputStreamReader(System.in));
         var input = br.readLine().split(" ");
-        int n = Integer.parseInt(input[0]), m = Integer.parseInt(input[1]), ans = 0;
+        int n = Integer.parseInt(input[0]), m = Integer.parseInt(input[1]);
+        
         int[] arrive = new int[n], leave = new int[n];
         for (int i = 0; i < n; i++) {
             var times = br.readLine().split(" ");
             arrive[i] = Integer.parseInt(times[0]);
             leave[i] = Integer.parseInt(times[1]) + arrive[i];
         }
+        
         Arrays.sort(arrive);
         Arrays.sort(leave);
+        int unlocks = 0;
         for (int i = 0, j = 0; i < n && j < n; i++) {
             while (arrive[i] > leave[j] + m) j++;
             if (arrive[i] >= leave[j] && arrive[i] <= leave[j] + m) {
-                ans++;
+                unlocks++;
                 j++;
             }
         }
-        System.out.println(ans);
+        
+        System.out.println(unlocks);
     }
 }
