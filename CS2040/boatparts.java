d00a74a240cddd13b4a8e725a317cce60aefcf3d
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-23 18:09:16 +0800

    Update boatparts.java

--- CS2040/boatparts.java
+++ CS2040/boatparts.java
@@ -1,16 +1,18 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class boatparts {
     public static void main(String[] args) throws IOException {
         var br = new BufferedReader(new InputStreamReader(System.in));
         var input = br.readLine().split(" ");
         int p = Integer.parseInt(input[0]), n = Integer.parseInt(input[1]), day = 1;
+        
         var parts = new HashSet<>();
         for (int i = 0; i < n; i++, day++) {
             parts.add(br.readLine());
             if (parts.size() == p) break;
         }
+        
         if (day > n) System.out.println("paradox avoided");
         else System.out.println(day);
     }
