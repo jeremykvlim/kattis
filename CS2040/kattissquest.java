ddd595a02703fadf58480c3489251f2923f7e50a
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-23 18:05:36 +0800

    Update kattissquest.java

--- CS2040/kattissquest.java
+++ CS2040/kattissquest.java
@@ -6,13 +6,13 @@ public class kattissquest {
         var br = new BufferedReader(new InputStreamReader(System.in));
         var pw = new PrintWriter(System.out);
         int n = Integer.parseInt(br.readLine());
+        
         var set = new TreeSet<Quest>();
         while (n-- > 0) {
             var command = br.readLine().split(" ");
             if (command[0].equals("add")) set.add(new Quest(Long.parseLong(command[1]), Long.parseLong(command[2]), n));
             else {
-                long energy = Long.parseLong(command[1]);
-                long gold = 0;
+                long energy = Long.parseLong(command[1]), gold = 0;
                 while (set.floor(new Quest(energy, Integer.MAX_VALUE, Integer.MAX_VALUE)) != null) {
                     var bestQuest = set.floor(new Quest(energy, Integer.MAX_VALUE, Integer.MAX_VALUE));
                     energy -= bestQuest.consumption;
