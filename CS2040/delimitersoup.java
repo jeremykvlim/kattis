c88628d7055ed9cfb7a44aba26d2d1850b7a3acb
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-23 17:55:47 +0800

    Update delimitersoup.java

--- CS2040/delimitersoup.java
+++ CS2040/delimitersoup.java
@@ -4,11 +4,12 @@ import java.util.*;
 public class delimitersoup {
     public static void main(String[] args) throws IOException {
         var br = new BufferedReader(new InputStreamReader(System.in));
-        int l = Integer.parseInt(br.readLine());
-        var arr = br.readLine().toCharArray();
+        int len = Integer.parseInt(br.readLine());
+        var l = br.readLine().toCharArray();
+        
         var s = new Stack<Character>();
-        for (int i = 0; i < l; i++) {
-            var c = arr[i];
+        for (int i = 0; i < len; i++) {
+            var c = l[i];
             if (c == '(' || c == '[' || c == '{') s.push(c);
             else if (c != ' ') {
                 if (s.isEmpty() || s.peek() == ')' || s.peek() == ']' || s.peek() == '}' ||
