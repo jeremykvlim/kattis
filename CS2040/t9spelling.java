cf1c4b65078de7ab02b3e7a3e78caaaa73062a58
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-23 18:08:28 +0800

    Update t9spelling.java

--- CS2040/t9spelling.java
+++ CS2040/t9spelling.java
@@ -1,15 +1,12 @@
 import java.io.*;
 
 public class t9spelling {
-
-    static int lastPressedNumber;
-
     public static void main(String[] args) throws IOException {
         var br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine()), count = 1;
-        lastPressedNumber = - 1;
-        int number, repetitions;
+        int n = Integer.parseInt(br.readLine());
 
+        int[] prev = {-1};
+        int number, repetitions, count = 1;
         while (n-- > 0) {
             System.out.print("Case #"+(count++)+": ");
             var line = br.readLine();
@@ -19,36 +16,36 @@ public class t9spelling {
                 if (letter == ' ') {
                     repetitions = 1;
                     number = 0;
-                    pressNumber(repetitions, number);
+                    pressNumber(repetitions, number, prev);
                 }
                 else if (letter == 's') {
                     repetitions = 4;
                     number = 7;
-                    pressNumber(repetitions, number);
+                    pressNumber(repetitions, number, prev);
                 }
                 else if (letter == 'z') {
                     repetitions = 4;
                     number = 9;
-                    pressNumber(repetitions, number);
+                    pressNumber(repetitions, number, prev);
                 }
                 else if (letter < 116) {
                     repetitions = (letter - 'a') % 3 + 1;
                     number = (letter - 'a') / 3 + 2;
-                    pressNumber(repetitions, number);
+                    pressNumber(repetitions, number, prev);
                 }
                 else if (letter < 123) {
                     repetitions = (letter - 'a' - 1) % 3 + 1;
                     number = (letter - 'a' - 1) / 3 + 2;
-                    pressNumber(repetitions, number);
+                    pressNumber(repetitions, number, prev);
                 }
             }
             System.out.println();
         }
     }
 
-    static void pressNumber(int repetitions, int number) {
-        if (lastPressedNumber == number) System.out.print(" ");
+    static void pressNumber(int repetitions, int number, int[] prev) {
+        if (prev[0] == number) System.out.print(" ");
         while (repetitions-- > 0) System.out.print(number);
-        lastPressedNumber = number;
+        prev[0] = number;
     }
 }
