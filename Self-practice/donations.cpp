862613fea7af4c3dbfbf2320ec61334f55cc4f5b
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-27 20:44:32 +0800

    Update donations.cpp

--- Self-practice/donations.cpp
+++ Self-practice/donations.cpp
@@ -9,16 +9,13 @@ int main() {
     double x, y, z, c, r;
     cin >> n >> x >> y >> z >> c >> r;
 
-    vector<vector<double>> dp(2, vector<double>(n + 1, 0.0));
+    vector<vector<double>> dp(2, vector<double>(n + 1, 0));
     dp[0][0] = x;
     for (int i = 0; i < n; i++) {
-        dp[(i + 1) % 2] = vector<double>(n + 1, 0.0);
+        dp[i & 1 ^ 1] = vector<double>(n + 1, 0);
         for (int j = i; j <= n; j++)
-            for (int k = j; k <= n; k++) {
-                auto donate = (k - j) * y;
-                dp[(i + 1) % 2][k] = max(dp[(i + 1) % 2][k], (dp[i % 2][j] - donate + max(0.0, donate - z) * c / 100.0) * (1.0 + r / 100.0));
-            }
+            for (int k = j; k <= n; k++) dp[i & 1 ^ 1][k] = max(dp[i & 1 ^ 1][k], (dp[i & 1][j] - (k - j) * y + max(0.0, (k - j) * y - z) * c / 100.0) * (1.0 + r / 100.0));
     }
 
-    cout << fixed << setprecision(6) << dp[n % 2][n];
+    cout << fixed << setprecision(6) << dp[n & 1][n];
 }
