9d733c57b256fcd3f5b93c50a033d73768367611
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-29 00:21:06 +0800

    Update unlockpattern2.cpp

--- Self-practice/unlockpattern2.cpp
+++ Self-practice/unlockpattern2.cpp
@@ -1,25 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool unvisited(int p1, int p2, int visited, vector<vector<int>> between) {
+bool valid(int p1, int p2, vector<bool> visited, vector<vector<int>> between) {
     if (p1 > p2) swap(p1, p2);
-    return !(between[p1][p2] && !(visited & (1 << between[p1][p2])));
+    return !between[p1][p2] || visited[between[p1][p2]];
 }
 
-void stroke(int curr, int prev, int visited, vector<vector<int>> &between, vector<pair<int, int>> &pos, vector<string> &patterns, string p = "") {
-    if (visited == (1 << 10) - 2) {
+void stroke(int curr, int prev, vector<bool> visited, vector<vector<int>> &between, vector<pair<int, int>> &pos, vector<string> &patterns, string p = "") {
+    if (all_of(visited.begin(), visited.end(), [&](auto b) {return b;})) {
         patterns.emplace_back(p);
         return;
     }
 
     pair<int, int> prev_dir = {pos[curr].first - pos[prev].first, pos[curr].second - pos[prev].second}, next_dir;
     for (int next = 1; next <= 9; next++)
-        if (!(visited & (1 << next)) && unvisited(curr, next, visited, between)) {
+        if (!visited[next] && valid(curr, next, visited, between)) {
             next_dir = {pos[next].first - pos[curr].first, pos[next].second - pos[curr].second};
             int cross = prev_dir.first * next_dir.second - prev_dir.second * next_dir.first;
             p += (cross < 0) ? 'R' : (cross > 0) ? 'L' : (prev_dir == next_dir) ? 'S' : 'A';
 
-            stroke(next, curr, visited | (1 << next), between, pos, patterns, p);
+            visited[next] = true;
+            stroke(next, curr, visited, between, pos, patterns, p);
+            visited[next] = false;
             p.pop_back();
         }
 }
@@ -30,7 +32,6 @@ int main() {
 
     string s;
     cin >> s;
-
 
     vector<vector<int>> between(10, vector<int>(10, 0));
     between[1][3] = 2;
@@ -44,8 +45,14 @@ int main() {
 
     vector<string> patterns;
     for (int i = 1; i <= 9; i++)
-        for (int j = 1; j <= 9; j++)
-            if (i != j && unvisited(i, j, 0, between)) stroke(i, j, (1 << i) | (1 << j), between, pos, patterns);
+        for (int j = 1; j <= 9; j++) {
+            vector<bool> visited(10, false);
+            visited[0] = true;
+            if (i != j && valid(i, j, visited, between)) {
+                visited[i] = visited[j] = true;
+                stroke(i, j, visited, between, pos, patterns);
+            }
+        }
 
     cout << count_if(patterns.begin(), patterns.end(), [&](auto p) {return equal(s.begin(), s.end(), p.begin(), [](char c1, char s1) {return c1 == '?' || c1 == s1;});});
 }
