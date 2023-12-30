e4a88b1ee37823ac8b6d54bb93a06e34407d430c
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2023-12-30 22:13:31 +0800

    Update intercept.cpp

--- CS3233/intercept.cpp
+++ CS3233/intercept.cpp
@@ -1,6 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void dijkstra(int s, bool t, vector<vector<pair<int, long long>>> &adj_list, vector<vector<pair<int, long long>>> &prev, vector<int> &stations) {
+    vector<long long> dist(prev.size(), LLONG_MAX);
+    priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<>> pq;
+    if (!t) dist[s] = 0;
+    pq.emplace(0, s);
+    while (!pq.empty()) {
+        auto [d, v] = pq.top();
+        pq.pop();
+
+        if (pq.empty() && t) stations.emplace_back(v);
+
+        for (auto [u, w] : adj_list[v])
+            if (dist[u] > d + w) {
+                dist[u] = d + w;
+                pq.emplace(d + w, u);
+                if (!t) prev[u] = vector<pair<int, long long>>(1, {v, w});
+            } else if (dist[u] == d + w && !t) prev[u].emplace_back(v, w);
+    }
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -8,7 +28,7 @@ int main() {
     int n, m;
     cin >> n >> m;
 
-    vector<vector<pair<int, int>>> adj_list(n);
+    vector<vector<pair<int, long long>>> adj_list(n);
     while (m--) {
         int u, v, w;
         cin >> u >> v >> w;
@@ -21,37 +41,9 @@ int main() {
     cin >> s >> t;
 
     vector<vector<pair<int, long long>>> prev(n);
-    vector<long long> dist(n, LLONG_MAX);
-    priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<>> pq;
-    dist[s] = 0;
-    pq.emplace(0, s);
-    while (!pq.empty()) {
-        auto [d, v] = pq.top();
-        pq.pop();
-
-        for (auto [u, w] : adj_list[v])
-            if (dist[u] > d + w) {
-                dist[u] = d + w;
-                pq.emplace(d + w, u);
-                prev[u] = vector<pair<int, long long>>(1, {v, w});
-            } else if (dist[u] == d + w) prev[u].emplace_back(v, w);
-    }
-
     vector<int> stations;
-    fill(dist.begin(), dist.end(), LLONG_MAX);
-    pq.emplace(0, t);
-    while (!pq.empty()) {
-        auto [d, v] = pq.top();
-        pq.pop();
-
-        if (pq.empty()) stations.emplace_back(v);
-
-        for (auto [u, w] : prev[v])
-            if (dist[u] > d + w) {
-                dist[u] = d + w ;
-                pq.emplace(d + w, u);
-            }
-    }
+    dijkstra(s, false, adj_list, prev, stations);
+    dijkstra(t, true, prev, prev, stations);
 
     sort(stations.begin(), stations.end());
     for (auto u : stations) cout << u << " ";
