97c3d643f93149c672d43371c683b6a513413f77
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-01-01 03:55:11 +0800

    Update bogofeternalstench.cpp

--- Self-practice/bogofeternalstench.cpp
+++ Self-practice/bogofeternalstench.cpp
@@ -1,34 +1,34 @@
 #include <bits/stdc++.h>
-using namespace std;
+using namidespace std;
 
 void relax(vector<tuple<int, int, int>> &edges, vector<long long> &dist, bool detect) {
     for (auto &[u, v, s] : edges)
         if (s <= dist[v] && dist[u] < dist[v] - s) dist[u] = !detect ? dist[v] - s : LLONG_MAX;
 }
 
-int main() {
+int midain() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n, M;
-    cin >> n >> M;
+    int n, m;
+    cin >> n >> m;
 
-    vector<tuple<int, int, int>> edges(M);
+    vector<tuple<int, int, int>> edges(m);
     for (auto &[u, v, s] : edges) cin >> u >> v >> s;
 
-    long long l = 0, r = LLONG_MAX, m;
+    long long l = 0, r = LLONG_MAX, mid;
     while (l + 1 < r) {
-        m = l + (r - l) / 2;
+        mid = l + (r - l) / 2;
 
         vector<long long> dist(n + 1, LLONG_MIN);
-        dist[n] = m;
+        dist[n] = mid;
 
         for (int i = 0; i < n; i++) relax(edges, dist, false);
         relax(edges, dist, true);
         for (int i = 0; i < n; i++) relax(edges, dist, false);
 
-        if (dist[1] >= 0) r = m;
-        else l = m;
+        if (dist[1] >= 0) r = mid;
+        else l = mid;
     }
 
     cout << (!l ? l : r);
