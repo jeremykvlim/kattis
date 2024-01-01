1250874cf3250254bf58dd49ad0ea85f112a92f2
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-01-02 02:23:18 +0800

    Update tugofwar.cpp

--- CS3233/tugofwar.cpp
+++ CS3233/tugofwar.cpp
@@ -8,28 +8,27 @@ int main() {
     int n;
     cin >> n;
 
-    vector<int> weights(n), team1((n >> 1) + 1), team2(n - (n >> 1) + 1);
+    vector<int> weights(n), team1(n / 2 + 1), team2(n - n / 2 + 1);
     for (int &w : weights) cin >> w;
 
-    int total1 = accumulate(weights.begin(), weights.begin() + (n >> 1), 0), total2 = accumulate(weights.begin() + (n >> 1), weights.end(), 0);
+    copy(weights.begin(), weights.begin() + n / 2, team1.begin());
+    copy(weights.begin() + n / 2, weights.end(), team2.begin());
+    int total1 = accumulate(team1.begin(), team1.end(), 0), total2 = accumulate(team2.begin(), team2.end(), 0);
 
-    copy(weights.begin(), weights.begin() + (n >> 1), team1.begin());
-    copy(weights.begin() + (n >> 1), weights.end(), team2.begin());
-
-    bool done;
+    bool change;
     do {
-        done = true;
-        for (int i = 0; i < n >> 1; i++)
-            for (int j = 0; j < n - (n >> 1); j++) {
+        change = false;
+        for (int i = 0; i < n / 2; i++)
+            for (int j = 0; j < n - n / 2; j++) {
                 int t1 = total1 - team1[i] + team2[j], t2 = total2 + team1[i] - team2[j];
                 if (abs(t1 - t2) < abs(total1 - total2)) {
-                    done = false;
+                    change = true;
                     swap(team1[i], team2[j]);
                     total1 = t1;
                     total2 = t2;
                 }
             }
-    } while (!done);
+    } while (change);
 
     cout << min(total1, total2) << " " << max(total1, total2);
 }
