0543fc0fc71e3469144e1ed1b0de1830c665a8fd
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-01-02 23:49:24 +0800

    Update beepers.py

--- IT5003/beepers.py
+++ IT5003/beepers.py
@@ -1,15 +1,15 @@
 import math
 
 def move(i, j):
-    mask = int(''.join(map(str, visited)), 2)
+    mask = int(''.join(str(int(v)) for v in reversed(visited)), 2)
     if dp[i][j][mask] != -1: return dp[i][j][mask]
 
     dp[i][j][mask] = math.inf
     for k in range(len(coordinates)):
         if not visited[k]:
-            visited[k] = 1
+            visited[k] = True
             dp[i][j][mask] = min(dp[i][j][mask], abs(i - coordinates[k][0]) + abs(j - coordinates[k][1]) + move(*coordinates[k]))
-            visited[k] = 0
+            visited[k] = False
 
     if dp[i][j][mask] == math.inf: dp[i][j][mask] = abs(i - start[0]) + abs(j - start[1])
     return dp[i][j][mask]
@@ -20,5 +20,5 @@ for _ in range(int(input())):
     start, beepers = list(map(int, input().split())), int(input())
     coordinates = [list(map(int, input().split())) for _ in range(beepers)]
 
-    dp, visited = [[[-1 for _ in range(1 << beepers)] for _ in range(y + 1)] for _ in range(x + 1)], [0] * beepers
+    dp, visited = [[[-1 for _ in range(1 << beepers)] for _ in range(y + 1)] for _ in range(x + 1)], [False] * beepers
     print(move(*start))
