ac2dd28bea04949f3c22433ac469dc25d8f5515a
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-01-02 22:15:31 +0800

    Create texassummers.py

---
+++ IT5003/texassummers.py
@@ -0,0 +1,25 @@
+import heapq, math
+
+n = int(input())
+points = [list(map(int, input().split())) for _ in range(n + 2)]
+d = [[(dx := xi - xj) * dx + (dy := yi - yj) * dy for xj, yj in points] for xi, yi in points]
+
+parent, dist = [-1] * (n + 2), [math.inf] * (n + 2)
+dist[n] = 0
+pq = [(0, n)]
+while pq:
+    w, v = heapq.heappop(pq)
+    if dist[v] < w: continue
+    for u in range(n + 2):
+        if dist[u] > w + d[v][u]:
+            dist[u] = w + d[v][u]
+            heapq.heappush(pq, (w + d[v][u], u))
+            parent[u] = v
+
+path = []
+curr = parent[-1]
+while parent[curr] != -1:
+    path.append(curr)
+    curr = parent[curr]
+
+print("-" if len(path) == 0 else "\n".join(map(str, path[::-1])))
