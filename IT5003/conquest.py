c1dc281bbc8ed1eae1a06cd147595528e02e690e
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-01-02 23:55:07 +0800

    Update conquest.py

--- IT5003/conquest.py
+++ IT5003/conquest.py
@@ -1,29 +1,25 @@
 import heapq
 
 n, m = map(int, input().split())
-adjList = [[] for _ in range(n)]
-visited = [0] * n
+adjList, visited = [[] for _ in range(n)], [False] * n
 
 for _ in range(m):
     u, v = map(int, input().split())
     adjList[u - 1].append(v - 1)
     adjList[v - 1].append(u - 1)
 
-s = [int(input()) for _ in range(n)]
-
-visited[0] = 1
-total = s[0]
-
-pq = [(s[v], v) for v in adjList[0]]
+size = [int(input()) for _ in range(n)]
+total = size[0]
+visited[0] = True
+pq = [(size[v], v) for v in adjList[0]]
 heapq.heapify(pq)
-
 while pq and pq[0][0] < total:
     _, v = heapq.heappop(pq)
     if not visited[v]:
-        total += s[v]
-        visited[v] = 1
+        total += size[v]
+        visited[v] = True
         for u in adjList[v]:
             if not visited[u]:
-                heapq.heappush(pq, (s[u], u))
+                heapq.heappush(pq, (size[u], u))
 
 print(total)
