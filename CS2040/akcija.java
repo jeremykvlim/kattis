354f06ae60013999093e3f56e45b2ffe1cb28808
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-01-04 04:13:04 +0800

    Update akcija.java

--- CS2040/akcija.java
+++ CS2040/akcija.java
@@ -4,6 +4,7 @@ public class akcija {
     public static void main(String[] args) {
         var sc = new Scanner(System.in);
         int n = sc.nextInt(), max = 0, total = 0, free = 0;
+        
         var prices = new int[n];
         for (int i = 0; i < n; i++) {
             prices[i] = sc.nextInt();
