9460625890171c6e8e54d642e8f4a1c6a427fa76
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-01-05 16:11:38 +0800

    Update carpool.cpp

--- CS3233/carpool.cpp
+++ CS3233/carpool.cpp
@@ -13,6 +13,7 @@ int main() {
         int u, v;
         long long w;
         cin >> u >> v >> w;
+
         dist[u][v] = dist[v][u] = min(dist[v][u], w);
     }
 
@@ -21,7 +22,7 @@ int main() {
         for (int j = 0; j < n + 2; j++)
             for (int k = 0; k < n + 2; k++) dist[j][k] = min(dist[j][k], dist[j][i] + dist[i][k]);
 
-    vector<long long> time(1 << n, INT_MAX), dp(1 << n, INT_MAX), temp(1 << n, INT_MAX);
+    vector<long long> time(1 << n, INT_MAX);
     for (int p1 = 1; p1 < n + 1; p1++)
         for (int p2 = 1; p2 < n + 1; p2++)
             for (int p3 = 1; p3 < n + 1; p3++)
@@ -29,16 +30,18 @@ int main() {
                     for (int p5 = 1; p5 < n + 1; p5++)
                         time[((1 << p1) | (1 << p2) | (1 << p3) | (1 << p4) | (1 << p5)) >> 1] =
                                 min(time[((1 << p1) | (1 << p2) | (1 << p3) | (1 << p4) | (1 << p5)) >> 1],
-                                    dist[0][p1] + dist[p1][p2] + dist[p2][p3] + dist[p3][p4] +
-                                    dist[p4][p5] + dist[p5][n + 1] + 5 * (1 + (p1 != p2) + (p2 != p3) + (p3 != p4) + (p4 != p5)));
-    
+                                    dist[0][p1] + dist[p1][p2] + dist[p2][p3] + dist[p3][p4] + dist[p4][p5] + dist[p5][n + 1]
+                                    + 5 * (1 + (p1 != p2) + (p2 != p3) + (p3 != p4) + (p4 != p5)));
+
+    vector<long long> dp(1 << n, INT_MAX);
     dp[0] = 0;
     for (int i = 0; i < ceil((double) n / 5); i++) {
+        auto temp = dp;
         for (int j = 0; j < 1 << n; j++)
             if (time[j] < INT_MAX)
                 for (int k = (1 << n) - 1; (k &= ~j) >= 0; k--) temp[j | k] = min(max(dp[k], time[j]), temp[j | k]);
-        dp.swap(temp);
+        dp = temp;
     }
-    
+
     cout << dp[(1 << n) - 1];
 }
