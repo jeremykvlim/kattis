ff55eab9851910e8b534d59f10c8ddae42a93f89
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-01-05 18:16:11 +0800

    Update cheatingluck.cpp

--- Self-practice/cheatingluck.cpp
+++ Self-practice/cheatingluck.cpp
@@ -1,25 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int play(int d, int n, int r, long total, vector<vector<vector<int>>> &bets, vector<vector<vector<int>>> &coins) {
+int play(int d, int n, int r, long total, vector<vector<vector<int>>> &bets, vector<vector<vector<int>>> &dp) {
     if (d >= total) return total;
     if (!d || !n || !r) return min(total, d * (1L << n));
-    if (coins[d][n][r]) return coins[d][n][r];
+    if (dp[d][n][r]) return dp[d][n][r];
 
-    coins[d][n][r] = d;
-    play(d - 1, n, r, total, bets, coins);
+    dp[d][n][r] = d;
+    play(d - 1, n, r, total, bets, dp);
 
     int least = bets[d - 1][n][r], most = min(d, (int) (total - d));
     least = min(least, most);
     bets[d][n][r] = least;
     for (int i = least; i <= most; i++) {
-        int max = min(play(d - i, n - 1, r - 1, total, bets, coins), play(d + i, n - 1, r, total, bets, coins));
-        if (max < coins[d][n][r]) break;
+        int max = min(play(d - i, n - 1, r - 1, total, bets, dp), play(d + i, n - 1, r, total, bets, dp));
+        if (max < dp[d][n][r]) break;
         bets[d][n][r] = i;
-        coins[d][n][r] = max;
+        dp[d][n][r] = max;
     }
 
-    return coins[d][n][r];
+    return dp[d][n][r];
 }
 
 int main() {
@@ -29,7 +29,7 @@ int main() {
     int d, g, n, k;
     cin >> d >> g >> n >> k;
 
-    vector<vector<vector<int>>> coins(d + g, vector<vector<int>>(n + 1, vector<int>(n + 1, 0))), bets(d + g, vector<vector<int>>(n + 1, vector<int>(n + 1, 0)));
+    vector<vector<vector<int>>> dp(d + g, vector<vector<int>>(n + 1, vector<int>(n + 1, 0))), bets(d + g, vector<vector<int>>(n + 1, vector<int>(n + 1, 0)));
     
-    cout << play(d, n, n - k, d + g, bets, coins);
+    cout << play(d, n, n - k, d + g, bets, dp);
 }
