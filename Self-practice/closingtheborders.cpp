d91b0e1565fd0701c8a6ef000eb6d2c0af6dfa1f
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-01-05 16:05:03 +0800

    Update closingtheborders.cpp

--- Self-practice/closingtheborders.cpp
+++ Self-practice/closingtheborders.cpp
@@ -23,7 +23,7 @@ int main() {
             double p = max(min(1.0, ((dest == n - 1 ? i : i + 1) - s[dest]) / double(t[dest] - s[dest])), 0.0);
             temp[dest] = max(dp[src] * (1 - p), temp[dest]);
         }
-        dp.swap(temp);
+        dp = temp;
         highest = max(dp.back(), highest);
     }
 
