79a0a31478c3e650e2fde168f3058aebf8b68594
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-01-08 01:28:05 +0800

    Create couplecompetition.cpp

---
+++ CS2040C/couplecompetition.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    vector<int> blocks(n);
+    int highest = 0;
+    for (auto &h : blocks) {
+        cin >> h;
+
+        highest = max(highest, h);
+    }
+
+    vector<vector<int>> adj_list(n);
+    auto construct = [&](stack<int> s, bool reverse) {
+        for (int i = (reverse ? n - 1 : 0); (reverse ? i >= 0 : i < n); (reverse ? i-- : i++)) {
+            while (!s.empty() && blocks[s.top()] < blocks[i]) {
+                adj_list[i].emplace_back(s.top());
+                s.pop();
+            }
+
+            s.emplace(i);
+        }
+    };
+
+    stack<int> s;
+    construct(s, true);
+    s = stack<int>();
+    construct(s, false);
+
+    queue<int> q;
+    vector<int> dist(n, -1);
+    for (int i = 0; i < n; i++)
+        if (blocks[i] == highest) {
+            q.emplace(i);
+            dist[i] = 0;
+        }
+
+    while (!q.empty()) {
+        int v = q.front();
+        q.pop();
+
+        for (int u : adj_list[v])
+            if (dist[u] == -1) {
+                dist[u] = dist[v] + 1;
+                q.emplace(u);
+            }
+    }
+
+    for (int i = 0; i < n; i++) cout << dist[i] << " ";
+}
