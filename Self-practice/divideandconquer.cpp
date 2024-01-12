a5ae6128e591d0a0b7b98052c1e03b20da9736df
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-01-12 23:58:54 +0800

    Create divideandconquer.cpp

---
+++ Self-practice/divideandconquer.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+__int128 pow(long long base, long long exponent, long long mod = 1) {
+    __int128 value = 1;
+    for (; exponent; exponent >>= 1) {
+        if (exponent & 1) value = (base * value) % mod;
+        base = (base * base) % mod;
+    }
+    return value;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long long b, d;
+    cin >> b >> d;
+
+    if (!(b % d)) {
+        cout << "no";
+        exit(0);
+    }
+
+    if (d == 2) {
+        cout << "yes";
+        exit(0);
+    }
+
+    auto order = d - 1;
+    while (!(order & 1) && pow(b, order >> 1, d) == 1) order >>= 1;
+    cout << (!(order & 1) ? "yes" : "no");
+}
