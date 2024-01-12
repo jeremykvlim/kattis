7b5298ed7c453e3fa8c7fdce3dd514fb04ea3113
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-01-12 19:21:13 +0800

    Update congaline.cpp

--- CS2040C/congaline.cpp
+++ CS2040C/congaline.cpp
@@ -13,7 +13,7 @@ int main() {
     int n, q;
     cin >> n >> q;
 
-    Person *head = nullptr, *tail = nullptr;
+    Person *front = nullptr, *back = nullptr;
     while (n--) {
         auto *p1 = new Person(), *p2 = new Person();
         cin >> p1->name >> p2->name;
@@ -21,23 +21,23 @@ int main() {
         p1->partner = p2;
         p2->partner = p1;
 
-        if (!head) {
-            head = p1;
-            tail = p2;
+        if (!front) {
+            front = p1;
+            back = p2;
         } else {
-            tail->next = p1;
-            p1->prev = tail;
-            tail = p2;
+            back->next = p1;
+            p1->prev = back;
+            back = p2;
         }
 
         p1->next = p2;
         p2->prev = p1;
     }
 
-    head->prev = tail;
-    tail->next = head;
+    front->prev = back;
+    back->next = front;
 
-    auto *mic = head;
+    auto *mic = front;
 
     string instructions;
     cin >> instructions;
@@ -50,25 +50,25 @@ int main() {
         else if (c == 'R') {
             auto *prev = mic->prev, *next = mic->next;
 
-            if (mic == head) {
-                head = next;
-                tail = mic;
-            } else if (mic != tail) {
+            if (mic == front) {
+                front = next;
+                back = mic;
+            } else if (mic != back) {
                 prev->next = next;
                 next->prev = prev;
 
-                tail->next = mic;
-                mic->prev = tail;
-                head->prev = mic;
-                mic->next = head;
+                back->next = mic;
+                mic->prev = back;
+                front->prev = mic;
+                mic->next = front;
 
-                tail = mic;
+                back = mic;
             }
 
             mic = next;
 
-        } else if (c == 'C') {
-            auto *next = mic->next, *prev = mic->prev, *partner = mic->partner, *partnerNext = partner->next;
+        } else {
+            auto *prev = mic->prev, *next = mic->next, *partner = mic->partner, *partner_next = partner->next;
 
             if (prev != partner) {
                 prev->next = next;
@@ -76,14 +76,14 @@ int main() {
 
                 partner->next = mic;
                 mic->prev = partner;
-                partnerNext->prev = mic;
-                mic->next = partnerNext;
+                partner_next->prev = mic;
+                mic->next = partner_next;
 
-                head = (mic == head) ? next : head;
-                tail = (mic == tail) ? prev : (partner == tail) ? mic : tail;
-            } else if (partner == tail) {
-                head = next;
-                tail = mic;
+                front = (mic == front) ? next : front;
+                back = (mic == back) ? prev : (partner == back) ? mic : back;
+            } else if (partner == back) {
+                front = next;
+                back = mic;
             }
 
             mic = next;
@@ -92,9 +92,9 @@ int main() {
 
     cout << "\n";
 
-    auto *it = head;
+    auto *person = front;
     do {
-        cout << it->name << "\n";
-        it = it->next;
-    } while (it != head);
+        cout << person->name << "\n";
+        person = person->next;
+    } while (person != front);
 }
