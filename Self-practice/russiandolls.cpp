9c5643e2f280c0d936882c0dacf3a3ffe93fc83a
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-01-13 23:46:17 +0800

    Update russiandolls.cpp

--- Self-practice/russiandolls.cpp
+++ Self-practice/russiandolls.cpp
@@ -10,24 +10,24 @@ struct Doll {
     }
 };
 
-bool arrange(int doll1, int doll2, int counter, vector<Doll> &dolls, bool set, vector<vector<vector<bool>>> &dp) {
+bool arrange(int doll1, int doll2, int count, vector<Doll> &dolls, bool set, vector<vector<vector<bool>>> &dp) {
     int bigger = max(doll1, doll2);
-    if (counter > (dolls.size() - 1) >> 1 || bigger - counter > (dolls.size() - 1) >> 1 || dp[doll1][doll2][counter]) return false;
+    if (2 * count > dolls.size() - 1 || 2 * (bigger - count) > dolls.size() - 1 || dp[doll1][doll2][count]) return false;
     if (bigger++ == dolls.size() - 1) return true;
     
     if ((dolls[bigger].h - 2 * dolls[bigger].w >= dolls[doll1].h && dolls[bigger].d - 2 * dolls[bigger].w >= dolls[doll1].d)
-        && arrange(bigger, doll2, counter + 1, dolls, set, dp)) {
+        && arrange(bigger, doll2, count + 1, dolls, set, dp)) {
         if (set) cout << dolls[bigger].h << " " << dolls[bigger].d << " " << dolls[bigger].w << "\n";
         return true;
     }
     
     if ((dolls[bigger].h - 2 * dolls[bigger].w >= dolls[doll2].h && dolls[bigger].d - 2 * dolls[bigger].w >= dolls[doll2].d)
-        && arrange(doll1, bigger, counter, dolls, set, dp)) {
+        && arrange(doll1, bigger, count, dolls, set, dp)) {
         if (!set) cout << dolls[bigger].h << " " << dolls[bigger].d << " " << dolls[bigger].w << "\n";
         return true;
     }
     
-    dp[doll1][doll2][counter] = true;
+    dp[doll1][doll2][count] = true;
     return false;
 }
 
@@ -38,11 +38,12 @@ int main() {
 
     int n;
     while (cin >> n && n) {
-        vector<vector<vector<bool>>> dp(2 * n + 1, vector<vector<bool>>(2 * n + 1, vector<bool>(2 * n + 1, false)));
         vector<Doll> dolls(2 * n + 1);
         for (int i = 1; i <= 2 * n; i++) cin >> dolls[i].h >> dolls[i].d >> dolls[i].w;
         sort(dolls.begin(), dolls.end());
 
+        
+        vector<vector<vector<bool>>> dp(2 * n + 1, vector<vector<bool>>(2 * n + 1, vector<bool>(2 * n + 1, false)));
         arrange(0, 0, 0, dolls, false, dp);
         cout << "-\n";
         arrange(0, 0, 0, dolls, true, dp);
