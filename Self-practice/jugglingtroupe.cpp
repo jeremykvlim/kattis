b4a5d213ce1ffee03d1012412fc567e94495be2c
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-01-27 01:29:01 +0800

    Update jugglingtroupe.cpp

--- Self-practice/jugglingtroupe.cpp
+++ Self-practice/jugglingtroupe.cpp
@@ -5,21 +5,21 @@ void perform(vector<int> &jugglers, stack<int> &zeroes, int i) {
     if (!jugglers[i]) zeroes.emplace(i);
 
     while (jugglers[i] > 1) {
-        int zero = zeroes.empty() ? -1 : zeroes.top(), dist = i - zero;
+        int zero = zeroes.empty() ? -1 : zeroes.top();
         if (!zeroes.empty()) zeroes.pop();
 
-        if (dist < jugglers[i]) {
-            jugglers[i] -= dist + 1;
-            if (i < jugglers.size() - 1) jugglers[i + 1] += dist;
-            if (zero >= 0) jugglers[zero] = 1;
+        int dist1 = i - zero, dist2 = jugglers[i] - 1;
+        if (dist1 < jugglers[i]) {
+            jugglers[i] -= dist1 + 1;
+            if (i < jugglers.size() - 1) jugglers[i + 1] += dist1;
+            if (~zero) jugglers[zero] = 1;
             if (!jugglers[i]) zeroes.emplace(i);
         } else {
-            int move = jugglers[i] - 1;
-            jugglers[i] -= move;
-            if (i < jugglers.size() - 1) jugglers[i + 1] += move;
-            zeroes.emplace(zero + move);
-            if (zero >= 0) jugglers[zero] = 1;
-            jugglers[zero + move] = 0;
+            jugglers[i] = 1;
+            if (i < jugglers.size() - 1) jugglers[i + 1] += dist2;
+            if (~zero) jugglers[zero] = 1;
+            jugglers[zero + dist2] = 0;
+            zeroes.emplace(zero + dist2);
         }
     }
 }
@@ -31,10 +31,11 @@ int main() {
 
     string s;
     cin >> s;
-    
+
     vector<int> jugglers(s.size());
-    stack<int> zeroes;
     for (int i = 0; i < s.size(); i++) jugglers[i] = s[i] - '0';
+
+    stack<int> zeroes;
     for (int i = 0; i < s.size(); i++) perform(jugglers, zeroes, i);
     for (int i : jugglers) cout << i;
 }
