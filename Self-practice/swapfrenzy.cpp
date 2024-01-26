0bec60f0d14ea3f8a529e823ba74bee1d4b4f491
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-01-26 22:32:31 +0800

    Update swapfrenzy.cpp

--- Self-practice/swapfrenzy.cpp
+++ Self-practice/swapfrenzy.cpp
@@ -1,7 +1,7 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-long long swap_digits(long long curr, int pos, int remaining, bool repeat, vector<int> &digits, vector<vector<unordered_set<long long>>> &visited) {
+long long swap(long long curr, int pos, int remaining, bool repeat, vector<int> &digits, vector<vector<unordered_set<long long>>> &visited) {
     if (pos < 0) {
         if (repeat || !(remaining & 1)) return curr;
         else return curr - digits[0] - 10 * digits[1] + 10 * digits[0] + digits[1];
@@ -9,22 +9,24 @@ long long swap_digits(long long curr, int pos, int remaining, bool repeat, vecto
 
     if (!remaining) return curr;
     if (visited[pos][remaining - 1].count(curr)) return -1;
-    visited[pos][remaining - 1].insert(curr);
+
+    visited[pos][remaining - 1].emplace(curr);
     int p = pos;
     vector<int> swap_indices;
-    for (int i = pos - 1; i >= 0; i--)
+    for (int i = pos - 1; ~i; i--)
         if (digits[p] < digits[i]) {
             p = i;
             swap_indices = {p};
         } else if (digits[p] == digits[i]) swap_indices.emplace_back(i);
 
-    long long largest = -1;
-    if (p == pos) largest = swap_digits(curr, pos - 1, remaining, repeat, digits, visited);
+    auto largest = -1LL;
+    if (p == pos) largest = swap(curr, pos - 1, remaining, repeat, digits, visited);
     for (int i : swap_indices) {
-        auto swapped = curr - llround(pow(10, pos)) * digits[pos] - llround(pow(10, i)) * digits[i] + llround(pow(10, pos)) * digits[i] + llround(pow(10, i)) * digits[pos];
+        auto next = curr - llround(pow(10, pos)) * digits[pos] - llround(pow(10, i)) * digits[i] + llround(pow(10, pos)) * digits[i] + llround(pow(10, i)) * digits[pos];
         swap(digits[pos], digits[i]);
-        largest = max(largest, swap_digits(swapped, pos - 1, remaining - 1, repeat, digits, visited));
-        swap(digits[pos], digits[i]);
+
+        largest = max(largest, swap(next, pos - 1, remaining - 1, repeat, digits, visited));
+        swap(digits[pos], digits[i]);   
     }
 
     return largest;
@@ -51,5 +53,5 @@ int main() {
         remainders.emplace(num % 10);
     }
 
-    cout << swap_digits(n, pos - 1, k, repeat, digits, visited);
+    cout << swap(n, pos - 1, k, repeat, digits, visited);
 }
