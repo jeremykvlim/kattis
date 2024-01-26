388f127bced873089793d53dd1d7538541ba5dec
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-01-26 21:19:59 +0800

    Update mububa.cpp

--- CS3233/mububa.cpp
+++ CS3233/mububa.cpp
@@ -11,17 +11,18 @@ int main() {
     vector<long long> pref(n + 1), dp(n + 1), prev(n + 1);
     for (int i = 1; i <= n; i++) {
         cin >> pref[i];
+        
         pref[i] += pref[i - 1];
     }
 
     for (int i = 1; i <= n; i++)
-        for (int j = i - 1; j >= 0; j--) {
+        for (int j = i - 1; ~j; j--) 
             if (prev[j] <= pref[i] - pref[j]) {
                 dp[i] = dp[j] + 1;
                 prev[i] = pref[i] - pref[j];
                 break;
             }
-        }
+        
 
     cout << dp[n];
 }
