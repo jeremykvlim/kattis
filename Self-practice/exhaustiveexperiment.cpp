250686d2ccbf67d7e550bf507bb1fae84f8017f7
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-01-27 01:48:56 +0800

    Update exhaustiveexperiment.cpp

--- Self-practice/exhaustiveexperiment.cpp
+++ Self-practice/exhaustiveexperiment.cpp
@@ -12,25 +12,24 @@ int main() {
     for (auto &c : components) {
         int x, y;
         char r;
+
         cin >> x >> y >> r;
-        c = make_tuple(2 * x + y, -2 * x + y, r);
+        c = {2 * x + y, -2 * x + y, r};
     }
     sort(components.begin(), components.end());
 
     int leak = 0, lowest = INT_MAX;
-    for (auto &c : components) {
-        int y = get<1>(c);
-        char r = get<2>(c);
-        if (y >= lowest) {
+    for (auto &[x, y, r] : components)
+        if (lowest <= y) {
             if (r == 'P') leak = -n - 1;
-            get<2>(c) = 'N';
+            r = 'N';
         } else if (r == 'N') lowest = y;
-    }
 
-    for (int i = n - 1, curr = INT_MIN, prev = INT_MIN; i >= 0; i--) {
-        int y = get<1>(components[i]);
-        char r = get<2>(components[i]);
+    reverse(components.begin(), components.end());
+    int curr = INT_MIN, prev = INT_MIN;
+    for (auto [x, y, r] : components) {
         if (r == 'N') continue;
+
         curr = max(curr, y);
         if (r == 'P' && y > prev) {
             leak++;
