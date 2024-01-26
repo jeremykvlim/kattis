9f5ace291bc4199fa280a18321a91fb57c60626b
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-01-27 02:53:25 +0800

    Update magicsequence.cpp

--- CS2040C/magicsequence.cpp
+++ CS2040C/magicsequence.cpp
@@ -5,14 +5,13 @@ void radix_sort(vector<long long> &s) {
     long long radix = 1 << 16, biggest = *max_element(s.begin(), s.end());
     auto msd = (long long) ceil((log(biggest)) / (log(radix)));
 
-    vector<int> bucket(radix, 0), v(s.size());
-
-    for (long long d = 0; d < msd; d++) {
+    vector<int> bucket(radix, 0), r(s.size());
+    for (auto d = 0LL; d < msd; d++) {
         fill(bucket.begin(), bucket.end(), 0);
-        for (long long i : s) bucket[(i >> (d * 16)) & (radix - 1)]++;
+        for (auto i : s) bucket[(i >> (d * 16)) & (radix - 1)]++;
         for (int i = 1; i < radix; i++) bucket[i] += bucket[i - 1];
-        for (int i = v.size() - 1; i >= 0; i--) v[--bucket[(s[i] >> (d * 16)) & (radix - 1)]] = s[i];
-        copy(v.begin(), v.end(), s.begin());
+        for (int i = r.size() - 1; ~i; i--) r[--bucket[(s[i] >> (d * 16)) & (radix - 1)]] = s[i];
+        copy(r.begin(), r.end(), s.begin());
     }
 }
 
@@ -22,16 +21,18 @@ int main() {
 
     int tc;
     cin >> tc;
-    
+
     while (tc--) {
-        long long n, a, b, c, v = 0, x, y;
+        long long n, a, b, c, x, y;
         cin >> n >> a >> b >> c >> x >> y;
-        
+
         vector<long long> s(n);
         s[0] = a;
-        for (int i = 1; i < n; i++) s[i] = (s[i - 1]*b + a) % c;
+        for (int i = 1; i < n; i++) s[i] = (s[i - 1] * b + a) % c;
         radix_sort(s);
-        for (int i = 0; i < n; i++) v = (v * x + s[i]) % y;
+
+        auto v = 0LL;
+        for (auto r : s) v = (v * x + r) % y;
         cout << v << "\n";
     }
 }
