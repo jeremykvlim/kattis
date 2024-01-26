96515a2cce8f227133bd12cf4eb31111427d5565
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-01-26 21:31:54 +0800

    Update celebritysplit.cpp

--- CS3233/celebritysplit.cpp
+++ CS3233/celebritysplit.cpp
@@ -1,13 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void dfs(int i, int jack, int jill, int sold, int n, vector<int> &mansions, vector<int> &suffix, int &value) {
-    if (sold >= value || abs(jack - jill) > suffix[i]) return;
-    if (jack == jill) value = min(value, sold + suffix[i]);
+void dfs(int i, int jack, int jill, int sold, int n, vector<int> &mansions, vector<int> &suff, int &value) {
+    if (sold >= value || abs(jack - jill) > suff[i]) return;
+    if (jack == jill) value = min(value, sold + suff[i]);
     if (i == n) return;
-    dfs(i + 1, jack + mansions[i], jill, sold, n, mansions, suffix, value);
-    if (jack != jill && jack) dfs(i + 1, jack, jill + mansions[i], sold, n, mansions, suffix, value);
-    dfs(i + 1, jack, jill, sold + mansions[i], n, mansions, suffix, value);
+    
+    dfs(i + 1, jack + mansions[i], jill, sold, n, mansions, suff, value);
+    if (jack != jill && jack) dfs(i + 1, jack, jill + mansions[i], sold, n, mansions, suff, value);
+    dfs(i + 1, jack, jill, sold + mansions[i], n, mansions, suff, value);
 }
 
 int main() {
@@ -19,7 +20,9 @@ int main() {
         vector<int> mansions(n), suff(n + 1, 0);
         for (int &v : mansions) cin >> v;
         sort(mansions.rbegin(), mansions.rend());
-        for (int i = n - 1; i >= 0; i--) suff[i] = suff[i + 1] + mansions[i];
+        
+        for (int i = n - 1; ~i; i--) suff[i] = suff[i + 1] + mansions[i];
+        
         int value = INT_MAX;
         dfs(0, 0, 0, 0, n, mansions, suff, value);
         cout << value << "\n";
