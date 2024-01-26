598a9aa87849e231e524d9e67888714a1c8f627b
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-01-27 04:06:48 +0800

    Update greetings.cpp

--- Self-practice/greetings.cpp
+++ Self-practice/greetings.cpp
@@ -9,13 +9,13 @@ int main() {
     cin >> n >> k;
 
     vector<tuple<int, int, int>> cards(n);
-    for (auto &c : cards) cin >> get<0>(c) >> get<1>(c) >> get<2>(c);
+    for (auto &[w, h, q] : cards) cin >> w >> h >> q;
 
     vector<long long> waste(1 << n);
     vector<vector<long long>> dp(max(n, k) + 1, vector<long long>(1 << n, 1e13));
 
     for (int i = 0; i < 1 << n; i++) {
-        int w = 0, h = 0;
+        auto w = 0, h = 0;
         for (int j = 0; j < n; j++)
             if (i & (1 << j)) {
                 w = max(w, get<0>(cards[j]));
@@ -30,9 +30,7 @@ int main() {
     dp[0][0] = 0;
     for (int i = 1; i <= k; i++)
         for (int j = 0; j < 1 << n; j++)
-            for (int mask = j; mask > 0; mask = (mask - 1) & j)
-                dp[i][j] = min(dp[i][j], dp[i - 1][j & ~mask] + waste[mask]);
-
+            for (int mask = j; mask; --mask &= j) dp[i][j] = min(dp[i][j], dp[i - 1][j & ~mask] + waste[mask]);
 
     cout << dp[min(n, k)][(1 << n) - 1];
 }
