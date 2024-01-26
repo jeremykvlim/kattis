2868705d61ce98b88375a4197ccdd58b1fdaeaae
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-01-27 03:09:59 +0800

    Update symmetricpolynomials.cpp

--- Self-practice/symmetricpolynomials.cpp
+++ Self-practice/symmetricpolynomials.cpp
@@ -42,7 +42,7 @@ int main() {
         }
 
         auto arg = 1.0;
-        for (int j = i - 1; j >= 0; j--)  {
+        for (int j = i - 1; ~j; j--)  {
             arg *= -t;
             curr[j] -= C(i , j) * arg * curr[i];
         }
