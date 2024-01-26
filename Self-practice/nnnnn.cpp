72cd7fc63b67f30205fd512054cade04101fd9c2
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-01-27 03:44:07 +0800

    Update nnnnn.cpp

--- Self-practice/nnnnn.cpp
+++ Self-practice/nnnnn.cpp
@@ -8,20 +8,22 @@ int main() {
     string s;
     cin >> s;
 
-    vector<int> L;
-    for (char c : s) L.emplace_back(c - '0');
+    if (s.size() == 1 && !stoi(s)) {
+        cout << 0;
+        exit(0);
+    }
 
-    int len = -1, remaining = 0;
-    for (int i = 0; i < L.size(); i++) {
-        remaining = 10 * remaining + L[i];
-        if (len == -1 && remaining >= L.size() - i) len = L.size() - i;
-        if (len > 0) {
-            L[i] = remaining / len;
-            remaining %= len;
+    vector<int> l;
+    for (char c : s) l.emplace_back(c - '0');
+
+    int len = 0;
+    for (int i = 0, j = l[i]; i < l.size(); i++, j = 10 * j + l[i]) {
+        if (!len && j >= l.size() - i) len = l.size() - i;
+        if (len) {
+            l[i] = j / len;
+            j %= len;
         }
     }
 
-    if (L == vector<int>{0}) cout << 0;
-    else
-        for (int i = L.size() - len; i < L.size(); i++) cout << L[i];
+    for (int i = l.size() - len; i < l.size(); i++) cout << l[i];
 }
