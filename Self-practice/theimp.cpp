b1e4a71a0d3a2820b1b2f30412deb99edc7a6d4f
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-01-27 02:42:44 +0800

    Update theimp.cpp

--- Self-practice/theimp.cpp
+++ Self-practice/theimp.cpp
@@ -13,13 +13,13 @@ int main() {
         cin >> n >> k;
 
         vector<pair<int, int>> items(n);
-        for (auto &p : items) cin >> p.first >> p.second;
+        for (auto &[v, c] : items) cin >> v >> c;
         sort(items.begin(), items.end());
 
         vector<vector<long long>> dp(k + 1, vector<long long>(n + 1, INT_MIN));
-        for (int i = n - 1; i >= 0; i--) dp[0][i] = max(dp[0][i + 1], (long long) items[i].first - items[i].second);
+        for (int i = n - 1; ~i; i--) dp[0][i] = max(dp[0][i + 1], (long long) items[i].first - items[i].second);
         for (int i = 1; i <= k; i++)
-            for (int j = n - 1; j >= 0; j--) dp[i][j] = max({min((long long) items[j].first - items[j].second, dp[i - 1][j + 1] - items[j].second), dp[i][j + 1], (i + j > n) ? INT_MIN : dp[i][j]});
+            for (int j = n - 1; ~j; j--) dp[i][j] = max({min((long long) items[j].first - items[j].second, dp[i - 1][j + 1] - items[j].second), dp[i][j + 1], (i + j > n) ? INT_MIN : dp[i][j]});
 
         cout << max(dp[k][0], 0LL) << "\n";
     }
