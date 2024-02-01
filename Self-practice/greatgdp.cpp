16115f32ec3ab1a99afe5d7dc40c4eab5694b588
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-02-02 02:52:44 +0800

    Update greatgdp.cpp

--- Self-practice/greatgdp.cpp
+++ Self-practice/greatgdp.cpp
@@ -15,6 +15,7 @@ void dfs(int v, int prev, vector<double> &cost, vector<double> &dp, vector<vecto
 bool contains(double m, vector<int> &gdp, vector<int> &population, vector<double> &cost, vector<double> &dp, vector<vector<int>> &adj_list) {
     for (int i = 0; i < cost.size(); i++) cost[i] = gdp[i] - m * population[i];
     dfs(0, -1, cost, dp, adj_list);
+    
     return dp[0] >= 0;
 }
 
@@ -33,13 +34,14 @@ int main() {
     for (int i = 0; i < n - 1; i++) {
         int u, v;
         cin >> u >> v;
+        
         adj_list[u - 1].emplace_back(v - 1);
         adj_list[v - 1].emplace_back(u - 1);
     }
 
     vector<double> cost(n), dp(n);
     double l = 0, r = INT_MAX, m;
-    while (l + EPSILON < r) {
+    while (l + 1e-6 < r) {
         m = l + (r - l) / 2;
         if (!contains(m, gdp, population, cost, dp, adj_list)) r = m;
         else l = m;
