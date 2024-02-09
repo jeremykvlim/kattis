a1aea8ca86f7b3277346d996e365780d6815513a
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-02-09 16:47:52 +0800

    Update fiat.cpp

--- CS3233/fiat.cpp
+++ CS3233/fiat.cpp
@@ -3,6 +3,15 @@ using namespace std;
 
 constexpr int MODULO = 1e9 + 7;
 
+long long pow(long long base, long long exponent, long long mod = 1) {
+    auto value = 1LL;
+    for (; exponent; exponent >>= 1) {
+        if (exponent & 1) value = (base * value) % mod;
+        base = (base * base) % mod;
+    }
+    return value;
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -14,10 +23,7 @@ int main() {
     for (int i = 1; i < n + 2; i++) {
         fact[i] = i * fact[i - 1] % MODULO;
 
-        for (long long exponent = MODULO - 2, base = fact[i]; exponent; exponent >>= 1) {
-            if (exponent & 1) a[i] = (base * a[i]) % MODULO;
-            base = (base * base) % MODULO;
-        }
+        a[i] *= pow(fact[i], MODULO - 2, MODULO);
     }
 
     for (int i = n + 2; i < fact.size(); i++) fact[i] = i * fact[i - 1] % MODULO;
