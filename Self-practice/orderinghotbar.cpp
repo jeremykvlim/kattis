1e0636ef81987bd56c3ed8e28a94450eb261b9d1
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-02-11 01:47:29 +0800

    Create orderinghotbar.cpp

---
+++ Self-practice/orderinghotbar.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    for (int &ai : a) cin >> ai;
+
+    vector<int> temp = a;
+    sort(temp.begin(), temp.end());
+    unordered_map<int, int> indices;
+    for (int i = 0; i < n; i++) indices[temp[i]] = i;
+    for (int &ai : a) ai = indices[ai];
+
+    vector<int> dp(n, INT_MIN);
+    for (int ai : a) {
+        dp[ai] = 1;
+        if (ai) dp[ai] = max(dp[ai], dp[ai - 1] + 1);
+    }
+
+    cout << n - *max_element(dp.begin(), dp.end());
+}
