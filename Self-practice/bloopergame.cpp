e2b7e25cd261c38deba2aa613a30895b7d8f94bb
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-02-10 23:03:12 +0800

    Create bloopergame.cpp

---
+++ Self-practice/bloopergame.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, l;
+    double p;
+    cin >> n >> l >> p;
+
+    priority_queue<double, vector<double>, greater<>> pq;
+    while (n--) {
+        double s;
+        cin >> s;
+
+        pq.emplace(1 / s);
+    }
+
+    double d = 1;
+    while (l) {
+        double prob = pq.top();
+        pq.pop();
+
+        int c = (l + round(n) - 1) / round(n);
+        pq.emplace(pow(prob, pow(p, c)));
+
+        l -= c;
+    }
+
+    while (!pq.empty()) {
+        d *= pq.top();
+        pq.pop();
+    }
+
+    cout << d;
+}
