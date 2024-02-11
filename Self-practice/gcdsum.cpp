68d3b4b20fb2366dc0c76474d9eef05b1e5c6739
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-02-11 23:24:15 +0800

    Create gcdsum.cpp

---
+++ Self-practice/gcdsum.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long long n;
+    cin >> n;
+
+    unordered_map<long long, int> pfs;
+    int p = 2;
+    for (; p <= sqrt(n); p == 2 ? p++ : p += 2)
+        while (!(n % p)) {
+            n /= p;
+            pfs[p]++;
+        }
+    if (n > 1) pfs[n]++;
+
+    auto sum = 1LL;
+    for (auto [pf, pow] : pfs) {
+        auto s = 1LL;
+        for (int i = 0; i < pow; i++) s = pf * s + 2 * i + 3;
+        sum *= s;
+    }
+
+    cout << sum;
+}
