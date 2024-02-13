8eca6bd306437d6ac5841af592c942299893ec91
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-02-13 18:41:18 +0800

    Update skysthelimit.cpp

--- Self-practice/skysthelimit.cpp
+++ Self-practice/skysthelimit.cpp
@@ -12,24 +12,24 @@ int main() {
     vector<double> houses(n + 2, 0);
     for (int i = 1; i <= n; i++) {
         cin >> houses[i];
-        
+
         houses[i] -= k * i * (n + 1 - i);
     }
 
-    deque<int> dq{0};
+    deque<int> convex{0};
     for (int i = 1; i <= n + 1; i++) {
-        while (dq.size() > 1 && (houses[i] - houses[dq[1]]) * (dq[0] - dq[1]) >= (houses[dq[0]] - houses[dq[1]]) * (i - dq[1])) 
-            dq.pop_front();
-        
-        dq.emplace_front(i);
+        while (convex.size() > 1 && (houses[i] - houses[convex[1]]) * (convex[0] - convex[1]) >= (houses[convex[0]] - houses[convex[1]]) * (i - convex[1]))
+            convex.pop_front();
+
+        convex.emplace_front(i);
     }
 
-    for (int i = dq.size() - 1; i; i--) {
-        int l = dq[i], r = dq[i - 1];
+    for (int i = convex.size() - 1; i; i--) {
+        int l = convex[i], r = convex[i - 1];
 
         for (int j = l; j < r; j++) houses[j] = (houses[l] * (r - j) + houses[r] * (j - l)) / (r - l);
     }
-    
+
     double height = 0;
     for (int i = 1; i <= n; i++) {
         houses[i] += k * i * (n + 1 - i);
