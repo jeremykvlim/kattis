b9aa9ed8cb8c5d1a132df08422e3ee17c95840c1
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-02-13 15:00:52 +0800

    Update largesttriangle.cpp

--- CS3233/largesttriangle.cpp
+++ CS3233/largesttriangle.cpp
@@ -5,7 +5,7 @@ double cross(pair<int, int> i, pair<int, int> j, pair<int, int> k) {
     return static_cast<double>(k.first - i.first) * (j.second - i.second) - static_cast<double>(k.second - i.second) * (j.first - i.first);
 }
 
-int monotone(vector<pair<int, int>> &points, vector<pair<int, int>> &convex) {
+void monotone(vector<pair<int, int>> &points, vector<pair<int, int>> &convex) {
     sort(points.begin(), points.end());
     points.erase(unique(points.begin(), points.end()), points.end());
 
@@ -21,8 +21,6 @@ int monotone(vector<pair<int, int>> &points, vector<pair<int, int>> &convex) {
         while (convex.size() > s && cross(convex[convex.size() - 2], convex.back(), p) <= 0) convex.pop_back();
         convex.emplace_back(p);
     }
-
-    return convex.size();
 }
 
 int main() {
@@ -35,13 +33,13 @@ int main() {
     vector<pair<int, int>> points(n), convex;
     for (auto &[x, y] : points) cin >> x >> y;
 
-    int size = monotone(points, convex);
+    monotone(points, convex);
     double area = 0;
-    if (size > 2)
-        for (int i = 0; i < size; i++)
-            for (int j = i + 1, k = j + 1; j < size; j++) {
-                while (k % size != i && cross(convex[i], convex[j], convex[(k + 1) % size]) >= cross(convex[i], convex[j], convex[k])) ++k %= size;
-                area = max(area, cross(convex[i], convex[j], convex[k % size]));
+    if (convex.size() > 2)
+        for (int i = 0; i < convex.size(); i++)
+            for (int j = i + 1, k = j + 1; j < convex.size(); j++) {
+                while (k % convex.size() != i && cross(convex[i], convex[j], convex[(k + 1) % convex.size()]) >= cross(convex[i], convex[j], convex[k])) ++k %= convex.size();
+                area = max(area, cross(convex[i], convex[j], convex[k % convex.size()]));
             }
 
     cout << fixed << setprecision(5) << area / 2;
