862ddfeef6fa190006b8c132f1c9c30fca730281
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-02-13 14:36:11 +0800

    Update largesttriangle.cpp

--- CS3233/largesttriangle.cpp
+++ CS3233/largesttriangle.cpp
@@ -5,23 +5,24 @@ double cross(pair<int, int> i, pair<int, int> j, pair<int, int> k) {
     return static_cast<double>(k.first - i.first) * (j.second - i.second) - static_cast<double>(k.second - i.second) * (j.first - i.first);
 }
 
-int monotone(int n, vector<pair<int, int>> &points, vector<pair<int, int>> &convex) {
-    int size = 0;
+int monotone(vector<pair<int, int>> &points, vector<pair<int, int>> &convex) {
     sort(points.begin(), points.end());
     points.erase(unique(points.begin(), points.end()), points.end());
 
-    for (int i = 0; i < n; i++) {
-        while (size > 1 && cross(convex[size - 2], convex[size - 1], points[i]) <= 0) size--;
-        convex[size++] = points[i];
+    for (auto p : points) {
+        while (convex.size() > 1 && cross(convex[convex.size() - 2], convex[convex.size() - 1], p) <= 0) convex.pop_back();
+        convex.emplace_back(p);
     }
 
-    int k = size;
-    for (int i = n - 2; ~i; i--) {
-        while (size > k && cross(convex[size - 2], convex[size - 1], points[i]) <= 0) size--;
-        convex[size++] = points[i];
+    int s = convex.size();
+    points.pop_back();
+    reverse(points.begin(), points.end());
+    for (auto p : points) {
+        while (convex.size() > s && cross(convex[convex.size() - 2], convex[convex.size() - 1], p) <= 0) convex.pop_back();
+        convex.emplace_back(p);
     }
 
-    return n > 1 ? size - 1 : size;
+    return convex.size();
 }
 
 int main() {
@@ -31,10 +32,10 @@ int main() {
     int n;
     cin >> n;
 
-    vector<pair<int, int>> points(n), convex(n);
+    vector<pair<int, int>> points(n), convex;
     for (auto &[x, y] : points) cin >> x >> y;
 
-    int size = monotone(n, points, convex);
+    int size = monotone(points, convex);
     double area = 0;
     if (size > 2)
         for (int i = 0; i < size; i++)
