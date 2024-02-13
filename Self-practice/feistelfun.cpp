b5e1528b9b7395160eaad3130848cfe52274b69e
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-02-13 18:29:12 +0800

    Update feistelfun.cpp

--- Self-practice/feistelfun.cpp
+++ Self-practice/feistelfun.cpp
@@ -1,6 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bitset<80> f(bitset<80> l, bitset<80> r, bitset<80> mask, long long shift, int m) {
+    r ^= ((l << shift) >> m) ^ (l << shift) & mask;
+    return r;
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -14,15 +19,15 @@ int main() {
 
     for (auto i = 0LL; i < 1 << upper; i++) {
         if (!seen.count((l_in << m) | r_in)) seen[(l_in << m) | r_in] = i;
-        r_in ^= ((l_in << 1) ^ (l_in >> (m - 1))) & mask;
+        r_in = f(l_in, r_in, mask, 1, m);
         swap(l_in, r_in);
     }
 
     auto rounds = LLONG_MAX;
     for (auto i = 0LL; i < 1 << upper; i++) {
         if (seen.count((r_out << m) | l_out)) rounds = min(rounds, seen[(r_out << m) | l_out] + (i << upper));
-        l_out ^= ((l_in << ((1 << upper) % m)) >> m) ^ (l_in << ((1 << upper) % m)) & mask;
-        r_out ^= ((r_in << ((1 << upper) % m)) >> m) ^ (r_in << ((1 << upper) % m)) & mask;
+        l_out = f(l_in, l_out, mask, ((1 << upper) % m), m);
+        r_out = f(r_in, r_out, mask, ((1 << upper) % m), m);
         swap(l_in, l_out);
         swap(r_in, r_out);
     }
