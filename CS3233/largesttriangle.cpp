fcab128c1de67f4443d526f79309f6b9867181d2
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-02-13 15:25:32 +0800

    Update largesttriangle.cpp

--- CS3233/largesttriangle.cpp
+++ CS3233/largesttriangle.cpp
@@ -5,21 +5,21 @@ double cross(pair<int, int> i, pair<int, int> j, pair<int, int> k) {
     return (double) (k.first - i.first) * (double) (j.second - i.second) - (double) (k.second - i.second) * (double) (j.first - i.first);
 }
 
-void monotone(vector<pair<int, int>> &points, vector<pair<int, int>> &convex) {
+void monotone(vector<pair<int, int>> &points, deque<pair<int, int>> &convex) {
     sort(points.begin(), points.end());
     points.erase(unique(points.begin(), points.end()), points.end());
 
     for (auto p : points) {
-        while (convex.size() > 1 && cross(convex[convex.size() - 2], convex.back(), p) <= 0) convex.pop_back();
-        convex.emplace_back(p);
+        while (convex.size() > 1 && cross(convex[1], convex[0], p) <= 0) convex.pop_front();
+        convex.emplace_front(p);
     }
 
     int s = convex.size();
     points.pop_back();
     reverse(points.begin(), points.end());
     for (auto p : points) {
-        while (convex.size() > s && cross(convex[convex.size() - 2], convex.back(), p) <= 0) convex.pop_back();
-        convex.emplace_back(p);
+        while (convex.size() > s && cross(convex[1], convex[0], p) <= 0) convex.pop_front();
+        convex.emplace_front(p);
     }
 }
 
@@ -30,16 +30,19 @@ int main() {
     int n;
     cin >> n;
 
-    vector<pair<int, int>> points(n), convex;
+    vector<pair<int, int>> points(n);
     for (auto &[x, y] : points) cin >> x >> y;
 
+    deque<pair<int, int>> convex;
     monotone(points, convex);
+    reverse(convex.begin(), convex.end());
+    int s = convex.size();
     double area = 0;
     if (convex.size() > 2)
-        for (int i = 0; i < convex.size(); i++)
-            for (int j = i + 1, k = j + 1; j < convex.size(); j++) {
-                while (k % convex.size() != i && cross(convex[i], convex[j], convex[(k + 1) % convex.size()]) >= cross(convex[i], convex[j], convex[k])) ++k %= convex.size();
-                area = max(area, cross(convex[i], convex[j], convex[k % convex.size()]));
+        for (int i = 0; i < s; i++)
+            for (int j = i + 1, k = j + 1; j < s; j++) {
+                while (k % s != i && cross(convex[i], convex[j], convex[(k + 1) % s]) >= cross(convex[i], convex[j], convex[k])) ++k %= s;
+                area = max(area, cross(convex[i], convex[j], convex[k % s]));
             }
 
     cout << fixed << setprecision(5) << area / 2;
