2a96be140b2f001493df0846e1708db97258a026
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-02-14 12:58:44 +0800

    Update reset.cpp

--- Self-practice/reset.cpp
+++ Self-practice/reset.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 bool complete(vector<pair<int, int>> tasks, long long total, long long &resets, long long attempts, int c) {
-    long long remaining = attempts * c;
+    auto remaining = attempts * c;
     for (auto [t, d] : tasks) {
         auto spent = min<long long>(t / d + (t % d != 0), attempts);
         if (remaining < spent) return false;
@@ -24,17 +24,14 @@ int main() {
     int n, c;
     cin >> n >> c;
 
-    long long total = 0;
     vector<pair<int, int>> tasks(n);
-    for (auto &t : tasks) {
-        cin >> t.first >> t.second;
-        total += t.first;
-    }
+    for (auto &[t, d] : tasks) cin >> t >> d;
 
-    long long l = 1, r = 1, m, resets = 0;
+    long long l = 1, r = 1, m, total = accumulate(tasks.begin(), tasks.end(), 0LL, [](auto t, auto p) {return t + p.first;}), resets = 0;
     while (!complete(tasks, total, resets, r, c)) r <<= 1;
     while (l + 1 < r) {
         m = l + (r - l) / 2;
+
         if (complete(tasks, total, resets, m, c)) r = m;
         else l = m;
     }
