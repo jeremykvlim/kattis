abc2fe623fe1a580ef3941cdb67c149660349bcc
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-02-14 17:25:39 +0800

    Update cardtrading.java

--- CS2040/cardtrading.java
+++ CS2040/cardtrading.java
@@ -7,7 +7,7 @@ public class cardtrading {
         var inputs = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         int t = inputs[1], k = inputs[2];
 
-        int[] cardCount = new int[t], cards = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray(), maintenance = new int[t];
+        int[] cardCount = new int[t], cards = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray(); 
         for (int i : cards) cardCount[i - 1]++;
 
         var cardPrices = new Card[t];
@@ -20,14 +20,15 @@ public class cardtrading {
         long profit = 0;
         for (int i = 0; i < cardCount.length; i++) profit += cardCount[i] * cardPrices[i].sell;
 
+        var opportunity = new int[t];
         for (int i = 0; i < t; i++) {
-            if (cardCount[i] == 0) maintenance[i] = cardPrices[i].buy * 2;
-            else if (cardCount[i] == 1) maintenance[i] = cardPrices[i].buy + cardPrices[i].sell;
-            else maintenance[i] = cardPrices[i].sell * 2;
+            if (cardCount[i] == 0) opportunity[i] = cardPrices[i].buy * 2;
+            else if (cardCount[i] == 1) opportunity[i] = cardPrices[i].buy + cardPrices[i].sell;
+            else opportunity[i] = cardPrices[i].sell * 2;
         }
 
-        Arrays.sort(maintenance);
-        for (int i = 0; i < k; i++) profit -= maintenance[i];
+        Arrays.sort(opportunity);
+        for (int i = 0; i < k; i++) profit -= opportunity[i];
 
         System.out.println(profit);
     }
