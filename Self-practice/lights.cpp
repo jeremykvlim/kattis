62964022473b72715690ceb661e058c4bcd092a5
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-02-16 23:05:03 +0800

    Update lights.cpp

--- Self-practice/lights.cpp
+++ Self-practice/lights.cpp
@@ -3,6 +3,15 @@ using namespace std;
 
 constexpr int MODULO = 10567201;
 
+long long pow(long long base, long long exponent, long long mod = 1) {
+    auto value = 1LL;
+    for (; exponent; exponent >>= 1) {
+        if (exponent & 1) value = (base * value) % mod;
+        base = (base * base) % mod;
+    }
+    return value;
+}
+
 pair<long long, long long> bezout(int a, int b) {
     if (!a) return {0, 1};
     auto [x, y] = bezout(b % a, a);
@@ -18,21 +27,18 @@ int main() {
         auto p2 = 1LL;
         while (n--) p2 = (p2 << 1) % MODULO;
 
-        vector<long long> C(m + 1, 1), ways(m + 1, 1);
-        for (int k = 1; k <= m; k++) {
-            auto [x, y] = bezout(k, MODULO);
-            if (x < 0) x += MODULO;
-            C[k] = ((__int128) C[k - 1] * (p2 + 1 - k) * x) % MODULO;
-        }
+        vector<long long> c(m + 1, 1), ways(m + 1, 1);
+        for (int i = 1; i <= m; i++)
+            c[i] = ((c[i - 1] * (p2 - i + 1)) % MODULO * pow(i, MODULO - 2, MODULO)) % MODULO;
 
         if (v) ways[0] = 0;
-        for (int k = 2; k <= m; k++) {
-            auto temp = ((__int128) ways[k - 2] * (p2 + 2 - k + MODULO)) % MODULO;
-            temp = (C[k - 1] - temp + MODULO) % MODULO;
+        for (int i = 2; i <= m; i++) {
+            auto temp = ((__int128) ways[i - 2] * (p2 - i + 2 + MODULO)) % MODULO;
+            temp = (c[i - 1] - temp + MODULO) % MODULO;
 
-            auto [x, y] = bezout(k, MODULO);
+            auto [x, y] = bezout(i, MODULO);
             if (x < 0) x += MODULO;
-            ways[k] = (temp * x) % MODULO;
+            ways[i] = (temp * x) % MODULO;
         }
 
         cout << ways[m] << "\n";
