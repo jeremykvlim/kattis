0a7b55d6e4b20da97f5f35d5448ac3aa96dee5db
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-02-18 22:05:11 +0800

    Update cardtrading.java

--- CS2040/cardtrading.java
+++ CS2040/cardtrading.java
@@ -7,34 +7,25 @@ public class cardtrading {
         var inputs = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         int t = inputs[1], k = inputs[2];
 
-        int[] cardCount = new int[t], cards = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray(); 
-        for (int i : cards) cardCount[i - 1]++;
+        int[] count = new int[t], cards = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        for (int i : cards) count[i - 1]++;
 
-        var cardPrices = new Card[t];
+        var cardPrices = new int[t][2];
 
         for (int i = 0; i < t; i++) {
             var input = br.readLine().split(" ");
-            cardPrices[i] = new Card(Integer.parseInt(input[0]), Integer.parseInt(input[1]));
+            cardPrices[i] = new int[]{Integer.parseInt(input[0]), Integer.parseInt(input[1])};
         }
 
         long profit = 0;
-        for (int i = 0; i < cardCount.length; i++) profit += cardCount[i] * cardPrices[i].sell;
+        for (int i = 0; i < count.length; i++) profit += count[i] * cardPrices[i][1];
 
         var opportunity = new int[t];
-        for (int i = 0; i < t; i++) opportunity[i] = cardPrices[i].buy * (2 - cardCount[i]) + cardPrices[i].sell * (cardCount[i]);
+        for (int i = 0; i < t; i++) opportunity[i] = cardPrices[i][0] * (2 - count[i]) + cardPrices[i][1] * (count[i]);
 
         Arrays.sort(opportunity);
         for (int i = 0; i < k; i++) profit -= opportunity[i];
 
         System.out.println(profit);
-    }
-
-    static class Card {
-        int buy, sell;
-
-        Card(int b, int s) {
-            buy = b;
-            sell = s;
-        }
     }
 }
