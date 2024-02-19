18f8eb1adc88748c89d0d85ed66ebf6fc1e64a41
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-02-19 13:11:16 +0800

    Update stringmultimatching.cpp

--- CS3233/stringmultimatching.cpp
+++ CS3233/stringmultimatching.cpp
@@ -2,19 +2,18 @@
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
     int n;
     while (cin >> n) {
         cin.ignore();
-        
-        vector<string> patterns(n);
-        for (auto &l : patterns) getline(cin, l);
+
+        vector<string> p(n);
+        for (auto &l : p) getline(cin, l);
+
         string t;
         getline(cin, t);
-        for (auto &l : patterns) {
-            size_t i = 0;
+
+        for (auto &l : p) {
+            int i = 0;
             while ((i = t.find(l, i)) != string::npos) cout << i++ << " ";
             cout << "\n";
         }
