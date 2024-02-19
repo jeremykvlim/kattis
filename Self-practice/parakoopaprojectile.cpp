d99a50fa61c96dde78ef39b577e0c3a25c6b2a2e
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-02-19 13:04:06 +0800

    Update parakoopaprojectile.cpp

--- Self-practice/parakoopaprojectile.cpp
+++ Self-practice/parakoopaprojectile.cpp
@@ -8,16 +8,15 @@ int main() {
     int n;
     cin >> n;
 
-    vector<pair<long double, long double>> coords(n);
+    vector<pair<double, double>> coords(n);
     for (auto &c : coords) cin >> c.first >> c.second;
     sort(coords.begin(), coords.end());
 
-    long double dist = LDBL_MAX;
+    auto dist = DBL_MAX;
     for (int i = 1; i < n; i++) {
         auto [x1, y1] = coords[i - 1];
         auto [x2, y2] = coords[i];
-        if (x1 == x2 || y1 * x2 - y2 * x1 <= 0) continue;
-        dist = min(dist, (y1 * x2 * x2 - y2 * x1 * x1) / (y1 * x2 - y2 * x1));
+        if (x1 != x2 && y1 * x2 - y2 * x1 > 0) dist = min(dist, (y1 * x2 * x2 - y2 * x1 * x1) / (y1 * x2 - y2 * x1));
     }
 
     cout << fixed << setprecision(6) << dist;
