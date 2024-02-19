53b1c1c934b38c167d1d217fbe4faa196ae6e717
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-02-19 14:59:56 +0800

    Update password.java

--- CS2040/password.java
+++ CS2040/password.java
@@ -5,14 +5,15 @@ public class password {
     public static void main(String[] args) throws IOException {
         var br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+        
         double total = 0;
-        var list = new ArrayList<Double>(n);
+        var p = new Double[n];
         for (int i = 0; i < n; i++) {
             var input = br.readLine().split(" ");
-            list.add(Double.parseDouble(input[1]));
+            p[i] = Double.parseDouble(input[1]);
         }
-        list.sort(Collections.reverseOrder());
-        for (int i = 1; i <= n; i++) total += list.get(i - 1) * i;
+        Arrays.sort(p, Collections.reverseOrder());
+        for (int i = 1; i <= n; i++) total += p[i - 1] * i;
         System.out.println(total);
     }
 }
