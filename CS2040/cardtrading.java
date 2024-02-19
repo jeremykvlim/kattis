a08c04d9dc3410cfc30b4d775b742c834bbb535e
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-02-19 12:39:54 +0800

    Update cardtrading.java

--- CS2040/cardtrading.java
+++ CS2040/cardtrading.java
@@ -10,18 +10,18 @@ public class cardtrading {
         int[] count = new int[t], cards = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         for (int i : cards) count[i - 1]++;
 
-        var cardPrices = new int[t][2];
+        var prices = new int[t][2];
 
         for (int i = 0; i < t; i++) {
             var input = br.readLine().split(" ");
-            cardPrices[i] = new int[]{Integer.parseInt(input[0]), Integer.parseInt(input[1])};
+            prices[i] = new int[]{Integer.parseInt(input[0]), Integer.parseInt(input[1])};
         }
 
         long profit = 0;
-        for (int i = 0; i < count.length; i++) profit += count[i] * cardPrices[i][1];
+        for (int i = 0; i < count.length; i++) profit += count[i] * prices[i][1];
 
         var opportunity = new int[t];
-        for (int i = 0; i < t; i++) opportunity[i] = cardPrices[i][0] * (2 - count[i]) + cardPrices[i][1] * (count[i]);
+        for (int i = 0; i < t; i++) opportunity[i] = prices[i][0] * (2 - count[i]) + prices[i][1] * (count[i]);
 
         Arrays.sort(opportunity);
         for (int i = 0; i < k; i++) profit -= opportunity[i];
