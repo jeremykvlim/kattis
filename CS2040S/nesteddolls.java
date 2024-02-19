57e2752b311a82f964e6734a3c55ec5833ef7d2e
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-02-20 02:35:14 +0800

    Update nesteddolls.java

--- CS2040S/nesteddolls.java
+++ CS2040S/nesteddolls.java
@@ -5,8 +5,8 @@ public class nesteddolls {
     public static void main(String[] args) throws IOException {
         var br = new BufferedReader(new InputStreamReader(System.in));
         var pw = new PrintWriter(System.out);
-        
         int t = Integer.parseInt(br.readLine());
+
         while (t-- > 0) {
             int m = Integer.parseInt(br.readLine());
 
@@ -15,19 +15,19 @@ public class nesteddolls {
             for (int i = 0; i < m; i++) dolls[i] = new Doll(Integer.parseInt(input[2 * i]), Integer.parseInt(input[2 * i + 1]));
             Arrays.sort(dolls, Comparator.reverseOrder());
 
-            var lnds = new ArrayList<Integer>();
-            for (var d : dolls) {
-                int l = 0, r = lnds.size(), mid;
-                while (l < r) {
-                    mid = l + (r - l) / 2;
-                    if (lnds.get(mid) > d.height) r = mid;
-                    else l = mid + 1;
-                }
-                if (l == lnds.size()) lnds.add(d.height);
-                else lnds.set(l, d.height);
+            var dp = new int[20010];
+            int d = 0;
+            for (int i = 0; i < m; i++) {
+                int j = d;
+                for (; j >= 0; j--)
+                    if (dp[j] <= dolls[i].height) {
+                        dp[++j] = dolls[i].height;
+                        break;
+                    }
+                d = Math.max(d, j);
             }
 
-            pw.println(lnds.size());
+            pw.println(d);
         }
         pw.flush();
     }
