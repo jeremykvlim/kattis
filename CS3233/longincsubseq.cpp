9aae55b2f8cee27ca0443092cdb6e8f3a0f90adf
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-02-20 21:07:06 +0800

    Update longincsubseq.cpp

--- CS3233/longincsubseq.cpp
+++ CS3233/longincsubseq.cpp
@@ -7,19 +7,19 @@ int main() {
 
     int n;
     while (cin >> n) {
-        vector<int> sequence(n), tail(n, 0), prev(n, -1);
-        for (int &e : sequence) cin >> e;
-        
+        vector<int> seq(n), tail(n, 0), prev(n, -1);
+        for (int &e : seq) cin >> e;
+
         int len = 1;
-        for (int i = 1; i < n; i++) 
-            if (sequence[i] > sequence[tail[len - 1]]) {
+        for (int i = 1; i < n; i++)
+            if (seq[i] > seq[tail[len - 1]]) {
                 prev[i] = tail[len - 1];
                 tail[len++] = i;
             } else {
                 int l = -1, r = len - 1, m;
                 while (l + 1 < r) {
                     m = l + (r - l) / 2;
-                    if (sequence[tail[m]] >= sequence[i]) r = m;
+                    if (seq[tail[m]] >= seq[i]) r = m;
                     else l = m;
                 }
 
@@ -27,7 +27,7 @@ int main() {
                 prev[i] = r ? tail[r - 1] : -1;
             }
         cout << len << "\n";
-        
+
         vector<int> lis(len);
         for (int i = tail[len - 1]; ~i; i = prev[i]) lis[--len] = i;
         for (auto &e : lis) cout << e << " ";
