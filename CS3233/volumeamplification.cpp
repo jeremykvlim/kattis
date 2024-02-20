ddf5310e5ea65b7d4d332da44f0caef3c7538380
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-02-20 20:43:12 +0800

    Update volumeamplification.cpp

--- CS3233/volumeamplification.cpp
+++ CS3233/volumeamplification.cpp
@@ -15,11 +15,12 @@ int main() {
         vector<int> description(n);
         for (int &a : description) cin >> a;
 
-        vector<int> volumes = {1};
+        vector<int> volumes{1};
         for (int i = 0; i < n; i++) {
             vector<int> amplified;
             for (auto v : volumes)
                 if (v * description[i] <= y) amplified.emplace_back(v * description[i]);
+            
             volumes.insert(volumes.end(), amplified.begin(), amplified.end());
             sort(volumes.begin(), volumes.end());
         }
