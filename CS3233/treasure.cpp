42351805dfcdd9d305c9fcaebe23f32aee41b083
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-02-20 20:32:40 +0800

    Update treasure.cpp

--- CS3233/treasure.cpp
+++ CS3233/treasure.cpp
@@ -13,11 +13,12 @@ int main() {
     for (int r = 0; r < n; r++)
         for (int c = 0; c < m; c++) {
             cin >> grid[r][c];
+
             if (grid[r][c] == 'S') s = {r, c};
         }
 
     vector<int> dr{0, 0, 1, -1}, dc{1, -1, 0, 0};
-    unordered_map<char, int> cost = {{'.', 1}, {'F', 2}, {'M', 3}, {'G', 1}};
+    unordered_map<char, int> cost{{'.', 1}, {'F', 2}, {'M', 3}, {'G', 1}};
     vector<vector<vector<int>>> dist(n, vector<vector<int>>(m, vector<int>(k + 1, INT_MAX)));
     dist[s.first][s.second][k] = 1;
     priority_queue<tuple<int, pair<int, int>, int>, vector<tuple<int, pair<int, int>, int>>, greater<>> pq;
@@ -25,27 +26,28 @@ int main() {
     while (!pq.empty()) {
         auto [d, p, stam] = pq.top();
         pq.pop();
-        
-        if (d > dist[p.first][p.second][stam]) continue;
-        
-        if (grid[p.first][p.second] == 'G') {
+        auto [x, y] = p;
+
+        if (d > dist[x][y][stam]) continue;
+
+        if (grid[x][y] == 'G') {
             cout << d;
             exit(0);
         }
 
         for (int i = 0; i < 4; i++) {
-            int r = p.first + dr[i], c = p.second + dc[i];
+            int r = x + dr[i], c = y + dc[i];
             if (r < 0 || r >= n || c < 0 || c >= m || grid[r][c] == '#' || grid[r][c] == 'S' || cost[grid[r][c]] > stam) continue;
 
-            if (dist[r][c][stam - cost[grid[r][c]]] > dist[p.first][p.second][stam]) {
-                dist[r][c][stam - cost[grid[r][c]]] = dist[p.first][p.second][stam];
+            if (dist[r][c][stam - cost[grid[r][c]]] > dist[x][y][stam]) {
+                dist[r][c][stam - cost[grid[r][c]]] = dist[x][y][stam];
                 pq.emplace(dist[r][c][stam - cost[grid[r][c]]], make_pair(r, c), stam - cost[grid[r][c]]);
             }
         }
 
-        if (dist[p.first][p.second][k] > dist[p.first][p.second][stam] + 1) {
-            dist[p.first][p.second][k] = dist[p.first][p.second][stam] + 1;
-            pq.emplace(dist[p.first][p.second][k], p, k);
+        if (dist[x][y][k] > dist[x][y][stam] + 1) {
+            dist[x][y][k] = dist[x][y][stam] + 1;
+            pq.emplace(dist[x][y][k], p, k);
         }
     }
 
