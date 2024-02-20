5dfb6e236371665366736135f64a22fb8513ac2c
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-02-20 20:51:55 +0800

    Update includescoring.cpp

--- CS2040C/includescoring.cpp
+++ CS2040C/includescoring.cpp
@@ -8,7 +8,7 @@ int main() {
     int n;
     cin >> n;
 
-    vector<int> points = {100, 75, 60, 50, 45, 40, 36, 32, 29, 26, 24, 22, 20, 18, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    vector<int> points{100, 75, 60, 50, 45, 40, 36, 32, 29, 26, 24, 22, 20, 18, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
     map<int, map<int, map<int, vector<int>>>> solved;
     vector<int> scores(n, 0);
     for (int i = 0; i < n; i++) {
