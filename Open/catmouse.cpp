8e3a8f93e36887b48f9a94a99cbadfbfc97aea51
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-02-28 18:35:56 +0800

    Create catmouse.cpp

---
+++ Open/catmouse.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int R, t, j;
+        cin >> R >> t >> j;
+
+        if (t >= j) {
+            cout << "YES\n";
+            continue;
+        }
+
+        double l = 0, r = M_PI_2, m;
+        while (l + 1e-10 < r) {
+            m = l + (r - l) / 2;
+
+            if (tan(m) - M_PI - m > 0) r = m;
+            else l = m;
+        }
+        cout << (t * (M_PI + m) / sin(m) >= j ? "YES\n" : "NO\n");
+    }
+}
