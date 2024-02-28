f2c8f05c1e95a1168955ba49650cfc3f5af7ec23
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-02-28 16:58:54 +0800

    Create sensor.cpp

---
+++ Open/sensor.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, d;
+    cin >> n >> d;
+
+    vector<pair<int, int>> coords(n);
+    vector<bitset<100>> valid(n, bitset<100>(0));
+    for (int i = 0; i < n; i++) {
+        cin >> coords[i].first >> coords[i].second;
+
+        for (int j = i; ~j; j--)
+            if (hypot((coords[i].first - coords[j].first), coords[i].second - coords[j].second) <= d)
+                valid[i][j] = valid[j][i] = true;
+    }
+
+    vector<int> indices(n);
+    iota(indices.begin(), indices.end(), 0);
+    bitset<100> sensors(0);
+    while (n--) {
+        shuffle(indices.begin(), indices.end(), mt19937(random_device()()));
+
+        auto temp = ~bitset<100>();
+        for (int i : indices)
+            if (temp[i]) temp &= valid[i];
+
+        if (sensors.count() < temp.count()) sensors = temp;
+    }
+
+    cout << sensors.count() << "\n";
+    for (int i = 0; i < 100; i++)
+        if (sensors[i]) cout << i + 1 << " ";
+}
