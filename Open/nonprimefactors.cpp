5a788e3d889c116bb60b458b837e55af9ccc97a9
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-02-29 14:06:20 +0800

    Create nonprimefactors.cpp

---
+++ Open/nonprimefactors.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    vector<bool> visited(2e6 + 1, false);
+    vector<int> npf(2e6 + 1, 0);
+    visited[1] = true;
+    for (int i = 1; i <= 2e6; i++)
+        if (visited[i])
+            for (int j = i; j <= 2e6; j += i) npf[j]++;
+        else
+            for (int j = i; j <= 2e6; j += i) visited[j] = true;
+
+    int q;
+    cin >> q;
+
+    while (q--) {
+        int i;
+        cin >> i;
+
+        cout << npf[i] << "\n";
+    }
+}
