44b8f5a094f29794bc180b0212d4ec94f48a5bc1
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-03-19 02:52:17 +0800

    Update sabor.cpp

--- CS3233/sabor.cpp
+++ CS3233/sabor.cpp
@@ -1,28 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void estimate(int mp, string &parties, vector<int> &arguments, vector<vector<int>> &adj_list) {
-    for (int v : adj_list[mp]) {
-        arguments[mp] += (parties[v] == parties[mp]) ? -1 : 1;
-        arguments[v] += (parties[v] == parties[mp]) ? -1 : 1;
+void dfs(int v, string &parties, vector<int> &degree, vector<vector<int>> &adj_list) {
+    for (int u : adj_list[v]) {
+        degree[v] += (parties[u] == parties[v]) ? -1 : 1;
+        degree[u] += (parties[u] == parties[v]) ? -1 : 1;
     }
 
-    parties[mp] ^= 'A' ^ 'B';
-    for (int i : adj_list[mp])
-        if (arguments[i] > 2) estimate(i, parties, arguments, adj_list);
+    parties[v] ^= 'A' ^ 'B';
+    for (int u : adj_list[v])
+        if (degree[u] > 2) dfs(u, parties, degree, adj_list);
 }
 
-int main () {
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
+
     int n;
     cin >> n;
 
     string parties(n, 'A');
-    vector<int> arguments(n);
+    vector<int> degree(n);
     vector<vector<int>> adj_list(n);
-
     for (int i = 0; i < 5; i++) {
         int p;
         cin >> p;
@@ -30,16 +29,16 @@ int main () {
         while (p--) {
             int k, l;
             cin >> k >> l;
-            
+
             adj_list[k - 1].emplace_back(l - 1);
             adj_list[l - 1].emplace_back(k - 1);
-            arguments[k - 1]++;
-            arguments[l - 1]++;
+            degree[k - 1]++;
+            degree[l - 1]++;
         }
     }
 
-    for (int i = 0; i < n; i++)
-        if (arguments[i] > 2) estimate(i, parties, arguments, adj_list);
+    for (int v = 0; v < n; v++)
+        if (degree[v] > 2) dfs(v, parties, degree, adj_list);
 
     cout << parties;
 }
