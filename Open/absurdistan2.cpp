b949844407c420f22c4ae2c5d174c8669cfb23db
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-03-19 03:15:56 +0800

    Update absurdistan2.cpp

--- Open/absurdistan2.cpp
+++ Open/absurdistan2.cpp
@@ -4,18 +4,18 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
+
     int n;
     cin >> n;
 
-    vector<double> probability(n + 1, 0), binomial(n + 1, 1);
+    vector<double> prob(n + 1, 0), C(n + 1, 1);
     for (int i = 1; i <= n; i++) {
-        probability[i] = pow(i - 1, i);
+        prob[i] = pow(i - 1, i);
         for (int j = 1; j < i; j++) {
-            binomial[j] = binomial[j - 1] * (i - j) / j;
-            if (j > 1) probability[i] -= pow(i - j - 1, i - j) * binomial[j - 1] * probability[j];
+            C[j] = C[j - 1] * (i - j) / j;
+            if (j > 1) prob[i] -= pow(i - j - 1, i - j) * C[j - 1] * prob[j];
         }
     }
 
-    cout << fixed << setprecision(8) << probability[n] / pow(n - 1, n);
+    cout << fixed << setprecision(8) << prob[n] / pow(n - 1, n);
 }
