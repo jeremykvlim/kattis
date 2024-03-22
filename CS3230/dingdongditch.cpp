20156aee626b07030aa2d97cb5d8807ba6f42d13
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-03-22 17:47:54 +0800

    Create dingdongditch.cpp

---
+++ CS3230/dingdongditch.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n, q;
+    cin >> n >> q;
+    
+    vector<int> a(n);
+    for (int &ai : a) cin >> ai;
+    sort(a.begin(), a.end());
+    
+    vector<long long> pref(n + 1, 0);
+    for (int i = 1; i <= n; i++) pref[i] = pref[i - 1] + a[i - 1];
+    
+    while (q--) {
+        int b;
+        cin >> b;
+        cout << pref[b] << "\n";
+    }
+}
