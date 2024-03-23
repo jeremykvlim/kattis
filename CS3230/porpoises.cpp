390e076cd4c6000052a8c6b77f6d65ca149dd2b4
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-03-23 16:56:24 +0800

    Update porpoises.cpp

--- CS3230/porpoises.cpp
+++ CS3230/porpoises.cpp
@@ -3,17 +3,28 @@ using namespace std;
 
 constexpr int MODULO = 1e9;
 
+vector<vector<long long>> I(int size) {
+    vector<vector<long long>> I(size, vector<long long>(size, 0));
+    for (int i = 0; i < size; i++) I[i][i] = 1;
+
+    return I;
+}
+
 vector<vector<long long>> matmul(vector<vector<long long>> &a, vector<vector<long long>> &b) {
-    vector<vector<long long>> c(2, vector<long long>(2, 0));
-    for (int i = 0; i < 2; i++)
-        for (int j = 0; j < 2; j++)
-            c[i][j] = (a[0][j] * b[i][0] + a[1][j] * b[i][1]) % MODULO;
+    int r1 = a.size(), r2 = b.size(), c2 = b[0].size();
+
+    vector<vector<long long>> c(r1, vector<long long>(c2, 0));
+    for (int i = 0; i < r1; i++)
+        for (int j = 0; j < c2; j++)
+            for (int k = 0; k < r2; k++) c[i][j] += (a[i][k] * b[k][j]) % MODULO;
 
     return c;
 }
 
 vector<vector<long long>> matpow(vector<vector<long long>> a, long long n) {
-    vector<vector<long long>> b{{1, 0}, {0, 1}};
+    int size = a.size();
+    auto b = I(size);
+
     for (; n; n >>= 1) {
         if (n & 1) b = matmul(a, b);
         a = matmul(a, a);
@@ -34,6 +45,6 @@ int main() {
         cin >> k >> y;
 
         vector<vector<long long>> a{{1, 1}, {1, 0}};
-        cout << k << " " << matpow(a, y)[0][1] << "\n";
+        cout << k << " " << matpow(a, y)[0][1] % MODULO << "\n";
     }
 }
