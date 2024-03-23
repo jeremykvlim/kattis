dd94e9a8729dc188e2cca7e2d9f8185dfde35fab
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-03-23 16:52:30 +0800

    Update languageinterpreter.cpp

--- CS3233/languageinterpreter.cpp
+++ CS3233/languageinterpreter.cpp
@@ -1,18 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<vector<unsigned>> I(int size) {
+    vector<vector<unsigned>> I(size, vector<unsigned>(size, 0));
+    for (int i = 0; i < size; i++) I[i][i] = 1;
+
+    return I;
+}
+
 vector<vector<unsigned>> matmul(vector<vector<unsigned>> &a, vector<vector<unsigned>> &b) {
-    vector<vector<unsigned>> c(33, vector<unsigned>(33, 0));
-    for (int i = 0; i < 33; i++)
-        for (int j = 0; j < 33; j++)
-            for (int k = 0; k < 33; k++) c[i][j] += a[i][k] * b[k][j];
+    int r1 = a.size(), r2 = b.size(), c2 = b[0].size();
+
+    vector<vector<unsigned>> c(r1, vector<unsigned>(c2, 0));
+    for (int i = 0; i < r1; i++)
+        for (int j = 0; j < c2; j++)
+            for (int k = 0; k < r2; k++) c[i][j] += a[i][k] * b[k][j];
 
     return c;
 }
 
 vector<vector<unsigned>> matpow(vector<vector<unsigned>> a, long long n) {
-    vector<vector<unsigned>> b(33, vector<unsigned>(33, 0));
-    for (int i = 0; i < 33; i++) b[i][i] = 1;
+    int size = a.size();
+    auto b = I(size);
 
     for (; n; n >>= 1) {
         if (n & 1) b = matmul(a, b);
@@ -33,10 +42,8 @@ int main() {
     for (int i = 0; i < k; i++) cin >> r[0][i];
     r[0][32] = 1;
 
-    vector<vector<unsigned>> temp(33, vector<unsigned>(33, 0));
-    for (int i = 0; i < 33; i++) temp[i][i] = 1;
     stack<pair<vector<vector<unsigned>>, long long>> st;
-    st.emplace(temp, 1);
+    st.emplace(I(33), 1);
     while (n--) {
         string s;
         cin >> s;
@@ -46,8 +53,7 @@ int main() {
             char _;
             cin >> _ >> x >> _ >> _ >> y >> _ >> _ >> z;
 
-            for (int i = 0; i < 33; i++)
-                for (int j = 0; j < 33; j++) temp[i][j] = (i == j);
+            auto temp = I(33);
             temp[x][x]--;
             temp[y][x]++;
             temp[z][x]++;
@@ -59,8 +65,7 @@ int main() {
             char _;
             cin >> _ >> x >> _ >> _ >> y >> _ >> c;
 
-            for (int i = 0; i < 33; i++)
-                for (int j = 0; j < 33; j++) temp[i][j] = (i == j);
+            auto temp = I(33);
             temp[x][x]--;
             temp[y][x]++;
             temp[32][x] += c;
@@ -71,8 +76,7 @@ int main() {
             char _;
             cin >> _ >> x >> _ >> _ >> y;
 
-            for (int i = 0; i < 33; i++)
-                for (int j = 0; j < 33; j++) temp[i][j] = (i == j);
+            auto temp = I(33);
             temp[x][x]--;
             temp[y][x]++;
 
@@ -83,8 +87,7 @@ int main() {
             char _;
             cin >> _ >> x >> _ >> c;
 
-            for (int i = 0; i < 33; i++)
-                for (int j = 0; j < 33; j++) temp[i][j] = (i == j);
+            auto temp = I(33);
             temp[x][x]--;
             temp[32][x] += c;
 
@@ -93,9 +96,7 @@ int main() {
             unsigned c;
             cin >> c;
 
-            for (int i = 0; i < 33; i++)
-                for (int j = 0; j < 33; j++) temp[i][j] = (i == j);
-            st.emplace(temp, c);
+            st.emplace(I(33), c);
         } else {
             auto temp2 = matpow(st.top().first, st.top().second);
 
