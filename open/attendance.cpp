d18987f8f55dab627b226dac1bf63f5bf5a1c66b
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-03-26 01:17:51 +0800

    Update attendance.cpp

--- open/attendance.cpp
+++ open/attendance.cpp
@@ -1,14 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void update(int i, int val, int inspections, vector<int> &fenwick, vector<bool> &attend) {
+void update(int i, int value, vector<int> &fenwick, int inspections, vector<bool> &attend) {
     if (i >= inspections) return;
-    
-    attend[i] = val != -1;
-    for (; i < fenwick.size(); i = i | (i + 1)) fenwick[i] += val;
+
+    attend[i] = value != -1;
+    for (; i < fenwick.size(); i = i | (i + 1)) fenwick[i] += value;
 }
 
-int pref_sum(int i, int inspections, vector<int> &fenwick) {
+int pref_sum(int i, vector<int> &fenwick, int inspections) {
     if (i >= inspections) i = inspections - 1;
 
     int sum = 0;
@@ -32,7 +32,8 @@ int main() {
     int curr = 0;
     for (int a : students)
         if (inspected[a - 1]) inspections.emplace_back(a - 1);
-        else while (!inspected[a - 1]) {
+        else
+            while (!inspected[a - 1]) {
                 inspected[order[curr] - 1] = true;
                 inspections.emplace_back(order[curr++] - 1);
             }
@@ -47,15 +48,15 @@ int main() {
     vector<int> fenwick(k);
     vector<bool> attend(k, false);
 
-    for (int i = 0; i < n; i++) update(index[i][0], 1, k, fenwick, attend);
+    for (int i = 0; i < n; i++) update(index[i][0], 1, fenwick, k, attend);
 
     curr = 0;
-    for (int i = 0; i != k; i++) {
+    for (int i = 0; i < k; i++) {
         while (!attend[curr]) curr++;
-        
+
         int next = *upper_bound(index[inspections[curr]].begin(), index[inspections[curr]].end(), curr);
-        update(curr, -1, k, fenwick, attend);
-        cout << pref_sum(next, k, fenwick) + 1 << " ";
-        update(next, 1, k, fenwick, attend);
+        update(curr, -1, fenwick, k, attend);
+        cout << pref_sum(next, fenwick, k) + 1 << " ";
+        update(next, 1, fenwick, k, attend);
     }
 }
