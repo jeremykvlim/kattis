ecdfdcc0f768e5303556b46bb22af309a2e2fd8c
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-03-25 13:47:48 +0800

    Update almostunionfind.java

--- CS2040/almostunionfind.java
+++ CS2040/almostunionfind.java
@@ -5,47 +5,53 @@ public class almostunionfind {
     public static void main(String[] args) throws IOException {
         var br = new BufferedReader(new InputStreamReader(System.in));
         var pw = new PrintWriter(System.out);
-        
+
         for (var line = br.readLine(); line != null; line = br.readLine()) {
             var input = line.split(" ");
             int n = Integer.parseInt(input[0]), m = Integer.parseInt(input[1]);
-            
-            int[] sets = new int[2 * n + 1], size = new int[2 * n + 1];
-            var sum = new long[2 * n + 1];
-            for (int i = 1, j = n + 1; i <= n; i++, j++) {
-                sets[i] = sets[j] = j;
-                sum[j] = i;
-                size[j] = 1;
+
+            var members = new HashSet[n + 1];
+            var id = new int[n + 1];
+            var sum = new long[n + 1];
+            for (int i = 1; i <= n; i++) {
+                members[i] = new HashSet<Integer>();
+                members[i].add(i);
+                id[i] = i;
+                sum[i] = i;
             }
-            
+
             while (m-- > 0) {
                 var command = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-                if (command[0] == 1) {
-                    int p = command[1], q = command[2], pSet = find(p, sets), qSet = find(q, sets);
-                    if (pSet != qSet) {
-                        size[pSet] += size[qSet];
-                        sum[pSet] += sum[qSet];
-                        sets[qSet] = pSet;
+                int o = command[0], p = command[1];
+                if (o == 1) {
+                    int q = command[2];
+                    if (id[p] != id[q]) {
+                        if (members[id[p]].size() < members[id[q]].size()) {
+                            var temp = p;
+                            p = q;
+                            q = temp;
+                        }
+
+                        sum[id[p]] += sum[id[q]];
+
+                        for (var t : members[id[q]]) {
+                            id[(int) t] = id[p];
+                            members[id[p]].add(t);
+                        }
                     }
-                } else if (command[0] == 2) {
-                    int p = command[1], q = command[2], pSet = find(p, sets), qSet = find(q, sets);
-                    if (pSet != qSet) {
-                        size[pSet]--;
-                        sum[pSet] -= p;
-                        size[qSet]++;
-                        sum[qSet] += p;
-                        sets[p] = qSet;
+                } else if (o == 2) {
+                    int q = command[2];
+                    if (id[p] != id[q]) {
+                        members[id[p]].remove(p);
+                        members[id[q]].add(p);
+                        sum[id[p]] -= p;
+                        sum[id[q]] += p;
+                        id[p] = id[q];
                     }
-                } else {
-                    int p = command[1], pSet = find(p, sets);
-                    pw.println(size[pSet] + " " + sum[pSet]);
-                }
+                } else pw.println(members[id[p]].size() + " " + sum[id[p]]);
             }
         }
+        
         pw.flush();
-    }
-
-    static int find(int p, int[] sets) {
-        return (sets[p] == p) ? p : (sets[p] = find(sets[p], sets));
     }
 }
