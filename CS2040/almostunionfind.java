0cf11363ed3b5a587b7cbf7e74e8919b1c0c3bdc
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-03-25 13:48:50 +0800

    Update almostunionfind.java

--- CS2040/almostunionfind.java
+++ CS2040/almostunionfind.java
@@ -10,13 +10,13 @@ public class almostunionfind {
             var input = line.split(" ");
             int n = Integer.parseInt(input[0]), m = Integer.parseInt(input[1]);
 
-            var members = new HashSet[n + 1];
-            var id = new int[n + 1];
+            var nodes = new HashSet[n + 1];
+            var sets = new int[n + 1];
             var sum = new long[n + 1];
             for (int i = 1; i <= n; i++) {
-                members[i] = new HashSet<Integer>();
-                members[i].add(i);
-                id[i] = i;
+                nodes[i] = new HashSet<Integer>();
+                nodes[i].add(i);
+                sets[i] = i;
                 sum[i] = i;
             }
 
@@ -25,33 +25,33 @@ public class almostunionfind {
                 int o = command[0], p = command[1];
                 if (o == 1) {
                     int q = command[2];
-                    if (id[p] != id[q]) {
-                        if (members[id[p]].size() < members[id[q]].size()) {
+                    if (sets[p] != sets[q]) {
+                        if (nodes[sets[p]].size() < nodes[sets[q]].size()) {
                             var temp = p;
                             p = q;
                             q = temp;
                         }
 
-                        sum[id[p]] += sum[id[q]];
+                        sum[sets[p]] += sum[sets[q]];
 
-                        for (var t : members[id[q]]) {
-                            id[(int) t] = id[p];
-                            members[id[p]].add(t);
+                        for (var t : nodes[sets[q]]) {
+                            sets[(int) t] = sets[p];
+                            nodes[sets[p]].add(t);
                         }
                     }
                 } else if (o == 2) {
                     int q = command[2];
-                    if (id[p] != id[q]) {
-                        members[id[p]].remove(p);
-                        members[id[q]].add(p);
-                        sum[id[p]] -= p;
-                        sum[id[q]] += p;
-                        id[p] = id[q];
+                    if (sets[p] != sets[q]) {
+                        nodes[sets[p]].remove(p);
+                        nodes[sets[q]].add(p);
+                        sum[sets[p]] -= p;
+                        sum[sets[q]] += p;
+                        sets[p] = sets[q];
                     }
-                } else pw.println(members[id[p]].size() + " " + sum[id[p]]);
+                } else pw.println(nodes[sets[p]].size() + " " + sum[sets[p]]);
             }
         }
-        
+
         pw.flush();
     }
 }
