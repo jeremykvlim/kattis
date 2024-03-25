dc7045a27b6d94b9781d518b0359bbe17ddabe04
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-03-26 01:03:50 +0800

    Update zuma.cpp

--- open/zuma.cpp
+++ open/zuma.cpp
@@ -1,17 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int find(int l, int r, int count, vector<vector<vector<int>>> &dp, vector<int> &colors, int k) {
+int marbles(int l, int r, int count, vector<vector<vector<int>>> &dp, vector<int> &colors, int k) {
     if (l > r) return 0;
     if (l == r) return k - 1 - count;
     if (dp[l][r][count] != -1) return dp[l][r][count];
 
     dp[l][r][count] = INT_MAX;
-    if (count < k - 1) dp[l][r][count] = find(l, r, count + 1, dp, colors, k) + 1;
-    if (count == k - 1) dp[l][r][count] = find(l + 1, r, 0, dp, colors, k);
+    if (count < k - 1) dp[l][r][count] = marbles(l, r, count + 1, dp, colors, k) + 1;
+    if (count == k - 1) dp[l][r][count] = marbles(l + 1, r, 0, dp, colors, k);
 
     for (int i = l + 1; i <= r; i++)
-        if (colors[i] == colors[l]) dp[l][r][count] = min(find(l + 1, i - 1, 0, dp, colors, k) + find(i, r, min(k - 1, count + 1), dp, colors, k), dp[l][r][count]);
+        if (colors[i] == colors[l]) 
+            dp[l][r][count] = min(dp[l][r][count], marbles(l + 1, i - 1, 0, dp, colors, k) + marbles(i, r, min(k - 1, count + 1), dp, colors, k));
 
     return dp[l][r][count];
 }
@@ -27,5 +28,5 @@ int main() {
     for (int &c : colors) cin >> c;
 
     vector<vector<vector<int>>> dp(n, vector<vector<int>>(n, vector<int>(k, -1)));
-    cout << find(0, n - 1, 0, dp, colors, k);
+    cout << marbles(0, n - 1, 0, dp, colors, k);
 }
