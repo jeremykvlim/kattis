9d70b26c9f89631cffad9372f8811f687a83573a
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-03-28 15:59:49 +0800

    Update fiat.cpp

--- nus/cs3233/fiat.cpp
+++ nus/cs3233/fiat.cpp
@@ -3,12 +3,18 @@ using namespace std;
 
 constexpr int MODULO = 1e9 + 7;
 
-long long pow(long long base, long long exponent, long long mod = LLONG_MAX) {
+long long mul(long long x, long long y, long long mod) {
+    auto product = x * y - mod * (long long) (1.L / mod * x * y);
+    return product + mod * (product < 0) - mod * (product >= mod);
+}
+
+long long pow(long long base, long long exponent, long long mod) {
     auto value = 1LL;
     for (; exponent; exponent >>= 1) {
-        if (exponent & 1) value = (base * value) % mod;
-        base = (base * base) % mod;
+        if (exponent & 1) value = mul(value, base, mod);
+        base = mul(base, base, mod);
     }
+
     return value;
 }
 
@@ -21,11 +27,11 @@ int main() {
 
     vector<long long> fact(2 * n + 1, 1), a(n + 2, 1);
     for (int i = 1; i < n + 2; i++) {
-        fact[i] = i * fact[i - 1] % MODULO;
+        fact[i] = mul(i, fact[i - 1], MODULO);
 
         a[i] *= pow(fact[i], MODULO - 2, MODULO);
     }
 
-    for (int i = n + 2; i < fact.size(); i++) fact[i] = i * fact[i - 1] % MODULO;
-    cout << fact[2 * n] * a[n] % MODULO * a[n + 1] % MODULO;
+    for (int i = n + 2; i < fact.size(); i++) fact[i] = mul(i, fact[i - 1], MODULO);
+    cout << mul(mul(fact[2 * n], a[n], MODULO), a[n + 1], MODULO);
 }
