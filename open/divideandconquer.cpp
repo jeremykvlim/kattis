86fcdc8e95df7a6e978c62ec02c959c3228bced2
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-03-28 16:20:09 +0800

    Update divideandconquer.cpp

--- open/divideandconquer.cpp
+++ open/divideandconquer.cpp
@@ -1,12 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-__int128 pow(long long base, long long exponent, long long mod = LLONG_MAX) {
+__int128 mul(__int128 x, __int128 y, long long mod) {
+    auto product = x * y - mod * (__int128) (1.L / mod * x * y);
+    return product + mod * (product < 0) - mod * (product >= mod);
+}
+
+__int128 pow(__int128 base, long long exponent, long long mod) {
     __int128 value = 1;
     for (; exponent; exponent >>= 1) {
-        if (exponent & 1) value = (base * value) % mod;
-        base = (base * base) % mod;
+        if (exponent & 1) value = mul(base, value, mod);
+        base = mul(base, base, mod);
     }
+    
     return value;
 }
 
@@ -29,5 +35,6 @@ int main() {
 
     auto order = d - 1;
     while (!(order & 1) && pow(b, order >> 1, d) == 1) order >>= 1;
+
     cout << (!(order & 1) ? "yes" : "no");
 }
