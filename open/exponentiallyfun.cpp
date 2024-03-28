b2c01ae741513dde18655206d35d7d357e5260dd
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-03-28 15:53:06 +0800

    Update exponentiallyfun.cpp

--- open/exponentiallyfun.cpp
+++ open/exponentiallyfun.cpp
@@ -16,12 +16,18 @@ vector<int> sieve(int n) {
     return primes;
 }
 
+long long mul(long long x, long long y, long long mod) {
+    auto product = x * y - mod * (long long) (1.L / mod * x * y);
+    return product + mod * (product < 0) - mod * (product >= mod);
+}
+
 long long pow(long long base, long long exponent, long long mod = LLONG_MAX) {
     auto value = 1LL;
     for (; exponent; exponent >>= 1) {
-        if (exponent & 1) value = (base * value) % mod;
-        base = (base * base) % mod;
+        if (exponent & 1) value = mul(value, base, mod);
+        base = mul(base, base, mod);
     }
+
     return value;
 }
 
