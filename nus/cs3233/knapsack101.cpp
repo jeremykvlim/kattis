17592e90f7af97d02c373b7b49c1eec5dd1efd8b
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-03-28 12:13:05 +0800

    Update knapsack101.cpp

--- nus/cs3233/knapsack101.cpp
+++ nus/cs3233/knapsack101.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-long long pow(long long base, long long exponent, long long mod = 1) {
+long long pow(long long base, long long exponent, long long mod = LLONG_MAX) {
     auto value = 1LL;
     for (; exponent; exponent >>= 1) {
         if (exponent & 1) value = (base * value) % mod;
