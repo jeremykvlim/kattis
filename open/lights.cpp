5476fdd5c639e003e3724cdf6e775efce159f666
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-03-28 16:05:37 +0800

    Update lights.cpp

--- open/lights.cpp
+++ open/lights.cpp
@@ -3,12 +3,18 @@ using namespace std;
 
 constexpr int MODULO = 10567201;
 
-long long pow(long long base, long long exponent, long long mod = LLONG_MAX) {
+long long mul(long long x, long long y, long long mod) {
+    long long product = (__int128) x * y - mod * (long long) (1.L / mod * x * y);
+    return product + mod * (product < 0) - mod * (product >= mod);
+}
+
+long long pow(long long base, long long exponent, long long mod) {
     auto value = 1LL;
     for (; exponent; exponent >>= 1) {
-        if (exponent & 1) value = (base * value) % mod;
-        base = (base * base) % mod;
+        if (exponent & 1) value = mul(value, base, mod);
+        base = mul(base, base, mod);
     }
+
     return value;
 }
 
@@ -29,16 +35,16 @@ int main() {
 
         vector<long long> C(m + 1, 1), ways(m + 1, 1);
         for (int k = 1; k <= m; k++)
-            C[k] = ((C[k - 1] * (p2 - k + 1)) % MODULO * pow(k, MODULO - 2, MODULO)) % MODULO;
+            C[k] = mul(mul(C[k - 1], p2 - k + 1, MODULO), pow(k, MODULO - 2, MODULO), MODULO);
 
         if (v) ways[0] = 0;
         for (int k = 2; k <= m; k++) {
-            auto temp = ((__int128) ways[k - 2] * (p2 - k + 2 + MODULO)) % MODULO;
+            auto temp = mul(ways[k - 2], p2 - k + 2 + MODULO, MODULO);
             temp = (C[k - 1] - temp + MODULO) % MODULO;
 
             auto [x, y] = bezout(k, MODULO);
             if (x < 0) x += MODULO;
-            ways[k] = (temp * x) % MODULO;
+            ways[k] = mul(temp, x, MODULO);
         }
 
         cout << ways[m] << "\n";
