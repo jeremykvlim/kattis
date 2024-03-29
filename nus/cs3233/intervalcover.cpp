6e4bdd99d7f14003d91a9aaae4533c484aeed829
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-03-29 22:56:46 +0800

    Update intervalcover.cpp

--- nus/cs3233/intervalcover.cpp
+++ nus/cs3233/intervalcover.cpp
@@ -5,12 +5,12 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    long double A, B;
+    double A, B;
     while (cin >> A >> B) {
         int n;
         cin >> n;
 
-        vector<tuple<long double, long double, int>> intervals(n);
+        vector<tuple<double, double, int>> intervals(n);
         for (int i = 0; i < n; i++) {
             long double a, b;
             cin >> a >> b;
@@ -19,16 +19,18 @@ int main() {
         }
         sort(intervals.begin(), intervals.end());
 
-        long double last = A, end = LLONG_MIN;
+        double last = A, end = LLONG_MIN;
         int curr = -1;
         vector<int> cover;
         for (auto [a, b, i] : intervals) {
             if (a > last) {
                 if (curr == -1 || end >= B) break;
+                
                 cover.emplace_back(curr);
                 last = end;
                 end = LLONG_MIN;
                 curr = -1;
+            
                 if (a > last) break;
             }
 
