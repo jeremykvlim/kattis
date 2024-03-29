7d6fe3acf74e4ef7c5a8ecfc6b7bfb19b1e74dbc
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-03-29 22:45:16 +0800

    Update mravihard.cpp

--- open/mravihard.cpp
+++ open/mravihard.cpp
@@ -1,16 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool dfs(long double l, int v, vector<long double> &liquid, vector<vector<int>> &adj_list, vector<vector<long double>> &flow,
-         vector<vector<bool>> &superpower, int parent = -1) {
+bool dfs(double l, int v, vector<double> &liquid, vector<vector<int>> &adj_list, vector<vector<double>> &flow, vector<vector<bool>> &superpower) {
     if (adj_list[v].empty() && liquid[v] != -1 && l < liquid[v]) return false;
-         
+
     for (int i = 0; i < adj_list[v].size(); i++) {
         int u = adj_list[v][i];
-        if (u == parent) continue;
+        if (u == v) continue;
+
         auto f = l + log(flow[v][i]);
-        if (!dfs(superpower[v][i] && f > 0 ? 2 * f : f, u, liquid, adj_list, flow, superpower, u)) return false;
+        if (!dfs(superpower[v][i] && f > 0 ? 2 * f : f, u, liquid, adj_list, flow, superpower)) return false;
     }
+
     return true;
 }
 
@@ -22,29 +23,30 @@ int main() {
     cin >> n;
 
     vector<vector<int>> adj_list(n);
-    vector<vector<long double>> flow(n);
+    vector<vector<double>> flow(n);
     vector<vector<bool>> superpower(n);
     for (int i = 0; i < n - 1; i++) {
         int a, b, x, t;
         cin >> a >> b >> x >> t;
-             
+
         adj_list[a - 1].emplace_back(b - 1);
         flow[a - 1].emplace_back(x / 100.0);
         superpower[a - 1].emplace_back(t);
     }
 
-    vector<long double> liquid(n);
+    vector<double> liquid(n);
     for (auto &k : liquid) {
         cin >> k;
+
         if (k > 0) k = log(k);
     }
 
-    long double inf = INT_MAX * log(INT_MAX), l = 0, r = inf + log(LLONG_MAX) * inf, m;
+    double l = 0, r = LLONG_MAX, m;
     while (l + 1 < r) {
         m = l + (r - l) / 2;
-        if (dfs(m / inf, 0, liquid, adj_list, flow, superpower)) r = m;
+        if (dfs(m / 1e14, 0, liquid, adj_list, flow, superpower)) r = m;
         else l = m;
     }
 
-    cout << fixed << setprecision(3) << exp(r / inf);
+    cout << fixed << setprecision(3) << exp(r / 1e14);
 }
