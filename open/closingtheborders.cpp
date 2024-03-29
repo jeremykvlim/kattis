a3a4b2415ef49fc21c043f7e789a14812e4b40f1
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-03-30 02:37:08 +0800

    Update closingtheborders.cpp

--- open/closingtheborders.cpp
+++ open/closingtheborders.cpp
@@ -12,17 +12,18 @@ int main() {
     for (int i = 0; i < n; i++) cin >> s[i] >> t[i];
 
     vector<pair<int, int>> flights(m);
-    for (auto &f : flights) cin >> f.first >> f.second;
+    for (auto &[src, dest] : flights) cin >> src >> dest;
 
     vector<double> dp(n);
     dp[0] = 1;
     double highest = 0;
     for (int i = 1; i <= n; i++) {
         vector<double> temp(n);
-        for (auto &[src, dest] : flights) {
-            double p = max(min(1.0, ((dest == n - 1 ? i : i + 1) - s[dest]) / double(t[dest] - s[dest])), 0.0);
+        for (auto [src, dest] : flights) {
+            double p = max(min(1.0, ((dest == n - 1 ? i : i + 1) - s[dest]) / (double) (t[dest] - s[dest])), 0.0);
             temp[dest] = max(dp[src] * (1 - p), temp[dest]);
         }
+
         dp = temp;
         highest = max(dp.back(), highest);
     }
