0da08922af94764dedd7f4eb4bfc727ae84c8097
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-03-29 22:54:26 +0800

    Update collidingtraffic.cpp

--- open/collidingtraffic.cpp
+++ open/collidingtraffic.cpp
@@ -9,27 +9,27 @@ int main() {
 
     int c;
     cin >> c;
-    
+
     while (c--) {
         int n;
         long double r;
         cin >> n >> r;
 
         vector<pair<long double, long double>> position(n), velocity(n);
-
         for (int i = 0; i < n; i++) {
             long double d, s;
             cin >> position[i].first >> position[i].second >> d >> s;
-            
-            velocity[i] = {s * cos((90 - d) / 180 * acos(-1)), s * sin((90 - d) / 180 * acos(-1))};
+
+            velocity[i] = {s * cos((90 - d) / 180 * M_PI), s * sin((90 - d) / 180 * M_PI)};
         }
 
-        long double time = LDBL_MAX;
+        auto time = LDBL_MAX;
         for (int i = 0; i < n; i++)
             for (int j = 0; j < i; j++) {
                 long double t;
-                auto boat1 = make_pair(position[i].first - position[j].first, position[i].second - position[j].second), v = make_pair(velocity[i].first - velocity[j].first, velocity[i].second - velocity[j].second);
-                
+                auto boat1 = make_pair(position[i].first - position[j].first, position[i].second - position[j].second),
+                         v = make_pair(velocity[i].first - velocity[j].first, velocity[i].second - velocity[j].second);
+
                 if (hypotl(boat1.first, boat1.second) < r + EPSILON) t = 0;
                 else if (hypotl(v.first, v.second) < EPSILON) continue;
                 else {
@@ -37,11 +37,12 @@ int main() {
                     if (t + EPSILON < 0) continue;
                     else {
                         auto boat2 = make_pair(boat1.first + v.first * t, boat1.second + v.second * t);
+
                         if (hypotl(boat2.first, boat2.second) > r) continue;
                         else t -= sqrt(r * r - (boat2.first * boat2.first + boat2.second * boat2.second)) / hypotl(v.first, v.second);
                     }
                 }
-                
+
                 time = min(time, t);
             }
 
