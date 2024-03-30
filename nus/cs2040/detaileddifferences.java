108b377ed4bcbe5b2269a7b88e481b157534c078
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-03-30 20:23:42 +0800

    Update detaileddifferences.java

--- nus/cs2040/detaileddifferences.java
+++ nus/cs2040/detaileddifferences.java
@@ -3,6 +3,7 @@ import java.io.*;
 public class detaileddifferences {
     public static void main(String[] args) throws IOException {
         var br = new BufferedReader(new InputStreamReader(System.in));
+        
         int n = Integer.parseInt(br.readLine());
         while (n-- > 0) {
             String first = br.readLine(), second = br.readLine();
