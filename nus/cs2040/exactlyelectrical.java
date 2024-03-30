f56bd03a8105841773ef1869b122a85ca485424d
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-03-30 20:36:03 +0800

    Update exactlyelectrical.java

--- nus/cs2040/exactlyelectrical.java
+++ nus/cs2040/exactlyelectrical.java
@@ -3,7 +3,9 @@ import java.util.*;
 public class exactlyelectrical {
     public static void main(String[] args) {
         var sc = new Scanner(System.in);
+        
         int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt(), d = sc.nextInt(), t = sc.nextInt(), dist = Math.abs(a - c) + Math.abs(b - d);
+        
         if (((t - dist) & 1) == 0 && (t - dist >= 0)) System.out.println("Y");
         else System.out.println("N");
     }
