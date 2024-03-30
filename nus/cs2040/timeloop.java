39758ee522a28e3ffe8124e6808b2052b7a7fe2a
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-03-30 20:32:44 +0800

    Update timeloop.java

--- nus/cs2040/timeloop.java
+++ nus/cs2040/timeloop.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class timeloop {
     public static void main(String[] args) {
         var sc = new Scanner(System.in);
+        
         int n = sc.nextInt();
         for (int i = 1; i <= n; i++) System.out.println(i + " Abracadabra");
     }
