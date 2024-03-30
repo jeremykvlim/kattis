1e6b7e597fc07bd5baa9eda22c5ab9ad299041fa
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-03-30 20:26:54 +0800

    Update grandpabernie.java

--- nus/cs2040/grandpabernie.java
+++ nus/cs2040/grandpabernie.java
@@ -5,23 +5,25 @@ public class grandpabernie {
     public static void main(String[] args) throws IOException {
         var br = new BufferedReader(new InputStreamReader(System.in));
         var pw = new PrintWriter(System.out);
+        
         int n = Integer.parseInt(br.readLine());
-        var map = new HashMap<String, ArrayList<Integer>>();
+        var years = new HashMap<String, ArrayList<Integer>>();
         while (n-- > 0) {
             var input = br.readLine().split(" ");
-            var list = map.getOrDefault(input[0], new ArrayList<>());
-            list.add(Integer.parseInt(input[1]));
-            map.put(input[0], list);
+            var y = years.getOrDefault(input[0], new ArrayList<>());
+            y.add(Integer.parseInt(input[1]));
+            years.put(input[0], y);
         }
 
-        for (var list : map.values()) Collections.sort(list);
+        for (var y : years.values()) Collections.sort(y);
 
         int q = Integer.parseInt(br.readLine());
         while (q-- > 0) {
             var input = br.readLine().split(" ");
-            var list = map.get(input[0]);
-            pw.println(list.get(Integer.parseInt(input[1]) - 1));
+            var y = years.get(input[0]);
+            pw.println(y.get(Integer.parseInt(input[1]) - 1));
         }
+        
         pw.flush();
     }
 }
