d4da8ed8449b1016723150e62af24703e4ca6173
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-03-30 20:36:12 +0800

    Update fizzbuzz.java

--- nus/cs2040/fizzbuzz.java
+++ nus/cs2040/fizzbuzz.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class fizzbuzz {
     public static void main(String[] args) throws Exception {
         var sc = new Scanner(System.in);
+        
         int x = sc.nextInt(), y = sc.nextInt(), n = sc.nextInt();
         for (int i = 1; i <= n; i++) {
             if (i % x == 0 && i % y == 0) System.out.println("FizzBuzz");
