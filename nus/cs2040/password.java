228e8b55172ee8cefe14e3de3a79600655d7a663
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-03-30 20:29:18 +0800

    Update password.java

--- nus/cs2040/password.java
+++ nus/cs2040/password.java
@@ -4,8 +4,9 @@ import java.util.*;
 public class password {
     public static void main(String[] args) throws IOException {
         var br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
         
+        int n = Integer.parseInt(br.readLine());
+
         double total = 0;
         var p = new Double[n];
         for (int i = 0; i < n; i++) {
@@ -13,6 +14,7 @@ public class password {
             p[i] = Double.parseDouble(input[1]);
         }
         Arrays.sort(p, Collections.reverseOrder());
+        
         for (int i = 1; i <= n; i++) total += p[i - 1] * i;
         System.out.println(total);
     }
