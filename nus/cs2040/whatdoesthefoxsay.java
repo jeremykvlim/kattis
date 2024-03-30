67a943e3c96a2d09096f73d7540cce58ce2a9910
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-03-30 20:34:03 +0800

    Update whatdoesthefoxsay.java

--- nus/cs2040/whatdoesthefoxsay.java
+++ nus/cs2040/whatdoesthefoxsay.java
@@ -4,10 +4,10 @@ import java.util.*;
 public class whatdoesthefoxsay {
     public static void main(String[] args) throws IOException {
         var br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
         
+        int t = Integer.parseInt(br.readLine());
         var words = new HashSet<String>();
-        for (int i = 0; i < t; i++) {
+        while (t-- > 0) {
             var r = br.readLine().split(" ");
             var line = br.readLine().split(" ");
             while (!line[0].equals("what") ) {
