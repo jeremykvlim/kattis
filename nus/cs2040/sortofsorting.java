13a876b02b007763b2b8a12488055efa78665641
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-03-30 20:30:20 +0800

    Update sortofsorting.java

--- nus/cs2040/sortofsorting.java
+++ nus/cs2040/sortofsorting.java
@@ -4,10 +4,12 @@ import java.util.*;
 public class sortofsorting {
     public static void main(String[] args) throws IOException {
         var br = new BufferedReader(new InputStreamReader(System.in));
+        
         for (int names = Integer.parseInt(br.readLine()); names != 0; names = Integer.parseInt(br.readLine())) {
             var arr = new String[names];
             for (int i = 0; names > 0; names--, i++) arr[i] = br.readLine();
             Arrays.sort(arr, Comparator.comparing(s -> s.substring(0, 2)));
+            
             for (var name : arr) System.out.println(name);
             System.out.println();
         }
