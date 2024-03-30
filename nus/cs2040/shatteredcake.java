4cc17bf55a9fa235c8564dd30ea34f323c4d58f5
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-03-30 20:30:01 +0800

    Update shatteredcake.java

--- nus/cs2040/shatteredcake.java
+++ nus/cs2040/shatteredcake.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class shatteredcake {
     public static void main(String[] args) {
         var sc = new Scanner(System.in);
+        
         int W = sc.nextInt(), n = sc.nextInt(), area = 0;
         while (n-- > 0) {
             int w = sc.nextInt(), l = sc.nextInt();
