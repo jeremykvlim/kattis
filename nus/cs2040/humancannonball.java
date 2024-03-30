729e9792ca6ffef92079048c65fcf3e8f3ea6f4b
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-03-30 20:37:11 +0800

    Update humancannonball.java

--- nus/cs2040/humancannonball.java
+++ nus/cs2040/humancannonball.java
@@ -17,12 +17,12 @@ public class humancannonball {
         var adjMatrix = new double[n + 2][n + 2];
 
         for (int i = 1; i < n + 2; i++)
-            adjMatrix[0][i] = Math.hypot(coords[0][0] - coords[i][0], coords[0][1] - coords[i][1])/5;
+            adjMatrix[0][i] = Math.hypot(coords[0][0] - coords[i][0], coords[0][1] - coords[i][1]) / 5;
 
         for (int i = 1; i < n + 1; i++)
             for (int j = 1; j < n + 2; j++)
-                adjMatrix[i][j] = Math.min(2 + Math.abs(Math.hypot(coords[i][0] - coords[j][0], coords[i][1] - coords[j][1]) - 50)/5,
-                        Math.hypot(coords[i][0] - coords[j][0], coords[i][1] - coords[j][1])/5);
+                adjMatrix[i][j] = Math.min(2 + Math.abs(Math.hypot(coords[i][0] - coords[j][0], coords[i][1] - coords[j][1]) - 50) / 5,
+                        Math.hypot(coords[i][0] - coords[j][0], coords[i][1] - coords[j][1]) / 5);
 
         var time = new double[n + 2];
         Arrays.fill(time, Double.POSITIVE_INFINITY);
