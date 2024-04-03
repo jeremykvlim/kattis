9c429dc55379c51eb48fc0f65fbf49f57e86ae25
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-04-03 21:29:29 +0800

    Update citytour.cpp

--- open/citytour.cpp
+++ open/citytour.cpp
@@ -8,32 +8,33 @@ int main() {
     int t;
     cin >> t;
 
-    for (int x = 0; x < t; x++) {
+    for (int x = 1; x <= t; x++) {
         int n;
         cin >> n;
 
-        vector<vector<int>> matrix(n, vector<int>(n, 0));
         vector<pair<int, int>> points(n - 3);
-        for (auto &p : points) cin >> p.first >> p.second;
+        for (auto &[a, b] : points) cin >> a >> b;
 
+        vector<vector<int>> adj_matrix(n, vector<int>(n, 0));
         for (int i = 0; i < 3; i++)
-            for (int j = i + 1; j < 3; j++) matrix[i][j] = 1;
+            for (int j = i + 1; j < 3; j++) adj_matrix[i][j] = 1;
 
-        for (int i = 0; i < points.size(); i++) matrix[points[i].first - 1][i + 3] = matrix[points[i].second - 1][i + 3] = 1;
+        for (int i = 0; i < points.size(); i++)
+            adj_matrix[points[i].first - 1][i + 3] = adj_matrix[points[i].second - 1][i + 3] = 1;
 
         int y = 0;
         for (int i = n - 1; i >= 2; i--) {
             int a = -1, b = -1;
             for (int j = 0; j < i; j++)
-                if (matrix[j][i]) {
+                if (adj_matrix[j][i]) {
                     if (a == -1) a = j;
                     else b = j;
                 }
 
-            y = max(y, matrix[a][b] + matrix[a][i] + matrix[b][i]);
-            matrix[a][b] = max(matrix[a][b], matrix[a][i] + matrix[b][i]);
+            y = max(y, adj_matrix[a][b] + adj_matrix[a][i] + adj_matrix[b][i]);
+            adj_matrix[a][b] = max(adj_matrix[a][b], adj_matrix[a][i] + adj_matrix[b][i]);
         }
 
-        cout << "Case #" << (x + 1) << ": " << y << "\n";
+        cout << "Case #" << x << ": " << y << "\n";
     }
 }
