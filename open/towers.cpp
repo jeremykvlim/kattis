2d23ab29ebf3037d629efeab7e822fbe8bbd1e58
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-04-03 17:15:13 +0800

    Update towers.cpp

--- open/towers.cpp
+++ open/towers.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 
 double scale(const vector<int> &t1, int t1_end, const vector<int> &t2, int t2_end) {
-    double exponent = 1, scale = 1;
+    double power = 1, scale = 1;
     for (int i = t2_end - 1; ~i; i--) {
         double a = i < t2_end ? t2[i] : 1, b = i < t1_end ? t1[i] : 1, ratio = scale * log(b) / log(a) - 1;
-        scale = abs(ratio) < 1e-9 ? 1 : pow(a, exponent * ratio);
+        scale = abs(ratio) < 1e-9 ? 1 : pow(a, power * ratio);
 
         if (scale > 50) return 2;
         else if (scale < 0.05) return 0;
 
-        exponent = pow(a, exponent);
+        power = pow(a, power);
     }
 
     return scale;
