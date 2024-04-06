79222f7c892c89f81d792894e4c30b9eba3cd320
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-04-06 17:21:25 +0800

    Update millionairemadness.java

--- nus/cs2040/millionairemadness.java
+++ nus/cs2040/millionairemadness.java
@@ -4,49 +4,62 @@ import java.util.*;
 public class millionairemadness {
     public static void main(String[] args) throws IOException {
         var br = new BufferedReader(new InputStreamReader(System.in));
-        
+
         var input = br.readLine().split(" ");
         int m = Integer.parseInt(input[0]), n = Integer.parseInt(input[1]);
 
-        var matrix = new int[m][n];
+        if (m == 1 && n == 1) {
+            System.out.println(0);
+            return;
+        }
+
+        var vault = new int[m][n];
+        for (int i = 0; i < m; i++) vault[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int l = -1, r = (int) 1e9, mid;
+        while (l + 1 < r) {
+            mid = l + (r - l) / 2;
+
+            if (bfs(mid, vault)) r = mid;
+            else l = mid;
+        }
+
+        System.out.println(r);
+    }
+
+    static boolean bfs(int len, int[][] vault) {
+        int[] dx = {-1, 0, 1, 0}, dy = {0, 1, 0, -1};
+        int m = vault.length, n = vault[0].length;
+
         var visited = new boolean[m][n];
-        for (int i = 0; i < m; i++) matrix[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-
-        var pq = new PriorityQueue<Pair>();
-        pq.add(new Pair(0, 0));
-        int len = 0;
-        int[] dx = {0, 0, 1, -1}, dy = {1, -1, 0, 0};
-        while (!pq.isEmpty()) {
-            var pair = pq.poll();
-            int cost = -pair.first, x = pair.second / n, y = pair.second % n;
-            if (visited[x][y]) continue;
-
-            visited[x][y] = true;
-            len = Math.max(len, cost);
-            if (x == m - 1 && y == n - 1) break;
+        visited[0][0] = true;
+        var q = new ArrayDeque<Pair>();
+        q.offer(new Pair(0, 0));
+        while (!q.isEmpty()) {
+            var curr = q.poll();
 
             for (int i = 0; i < 4; i++) {
-                int xi = x + dx[i], yi = y + dy[i];
-                if (0 <= xi && xi < m && 0 <= yi && yi < n && !visited[xi][yi])
-                    pq.add(new Pair(-Math.max(0, matrix[xi][yi] - matrix[x][y]), xi * n + yi));
+                int x = curr.first + dx[i], y = curr.second + dy[i];
+                if (!(0 <= x && x < m && 0 <= y && y < n) || visited[x][y]) continue;
+
+                int diff = Math.abs(vault[x][y] - vault[curr.first][curr.second]);
+                if (vault[x][y] <= vault[curr.first][curr.second] || diff <= len) {
+                    if (x == m - 1 && y == n - 1) return true;
+                    visited[x][y] = true;
+                    q.offer(new Pair(x, y));
+                }
             }
         }
 
-        System.out.println(len);
+        return false;
     }
 
-    static class Pair implements Comparable<Pair> {
+    static class Pair {
         int first, second;
 
         Pair(int f, int s) {
             first = f;
             second = s;
-        }
-
-        @Override
-        public int compareTo(Pair p) {
-            if (p.first != first) return p.first - first;
-            else return p.second - second;
         }
     }
 }
