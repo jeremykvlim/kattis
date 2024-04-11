53c64e7c3469bf7573b3e197613f6d15065943cc
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-04-11 21:23:53 +0800

    Update classicalcounting.cpp

--- nus/cs3233/classicalcounting.cpp
+++ nus/cs3233/classicalcounting.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 constexpr int MODULO1 = 29, MODULO2 = 34483;
 
-long long C(int n, int k, int p, vector<long long> &fact, vector<long long> &fact_inv) {
-    if (k < 0 || k > n) return 0;
-    if (n >= p || k >= p) return (C(n / p, k / p, p, fact, fact_inv) * C(n % p, k % p, p, fact, fact_inv)) % p;
-    return (fact[n] * fact_inv[k] % p * fact_inv[n - k] % p) % p;
+long long C(int n, int m, int p, vector<int> &fact, vector<int> &fact_inv) {
+    if (m < 0 || m > n) return 0;
+    if (n >= p || m >= p) return (C(n / p, m / p, p, fact, fact_inv) * C(n % p, m % p, p, fact, fact_inv)) % p;
+    return (fact[n] * fact_inv[m] % p * fact_inv[n - m] % p) % p;
 }
 
 int main() {
@@ -18,11 +18,13 @@ int main() {
 
     vector<int> fact1(MODULO1 + 1, 1), fact2(MODULO2 + 1, 1), fact_inv1(MODULO1 + 1, 1), fact_inv2(MODULO2 + 1, 1);
     auto prepare = [&](vector<int> &fact, vector<int> &fact_inv, int mod) {
-        vector<int> inv(mod + 1, 1);
-        for (int i = 2; i <= mod; i++) inv[i] = ((mod - mod / i) * inv[mod % i]) % mod;
+        vector<int> inv(mod, 1);
+        for (int i = 2; i < mod; i++) inv[i] = ((mod - mod / i) * inv[mod % i]) % mod;
 
-        for (int i = 1; i <= mod; i++) fact[i] = (i * fact[i - 1]) % mod;
-        for (int i = 1; i <= mod; i++) fact_inv[i] = (inv[i] * fact_inv[i - 1]) % mod;
+        for (int i = 1; i <= mod; i++) {
+            fact[i] = (i * fact[i - 1]) % mod;
+            fact_inv[i] = (inv[i] * fact_inv[i - 1]) % mod;
+        }
     };
 
     prepare(fact1, fact_inv1, MODULO1);
