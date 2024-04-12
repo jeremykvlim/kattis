4c42f631a0731824551dd38b4bf7155fd2f61f75
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-04-12 18:13:47 +0800

    Update dominos.java

--- nus/cs2040/dominos.java
+++ nus/cs2040/dominos.java
@@ -4,44 +4,50 @@ import java.util.*;
 public class dominos {
     public static void main(String[] args) throws IOException {
         var br = new BufferedReader(new InputStreamReader(System.in));
-        
+
         int t = Integer.parseInt(br.readLine());
         while (t-- > 0) {
-            var input = br.readLine().split(" ");
-            int n = Integer.parseInt(input[0]), m = Integer.parseInt(input[1]);
+            var inputs = br.readLine().split(" ");
+            int n = Integer.parseInt(inputs[0]), m = Integer.parseInt(inputs[1]);
 
             var adjList = new ArrayList[n + 1];
-            for (int i = 1; i <= n; i++) adjList[i] = new ArrayList<>();
+            for (int i = 1; i <= n; i++) adjList[i] = new ArrayList<Integer>();
+            
+            var indegree = new int[n + 1];
+            while (m-- > 0) {
+                var vertices = br.readLine().split(" ");
+                int x = Integer.parseInt(vertices[0]), y = Integer.parseInt(vertices[1]);
 
-            for (int i = 0; i < m; i++) {
-                input = br.readLine().split(" ");
-                adjList[Integer.parseInt(input[0])].add(Integer.parseInt(input[1]));
+                adjList[x].add(y);
+                indegree[y]++;
             }
 
-            var visited = new boolean[n + 1];
-            var s = new Stack<Integer>();
-            for (int i = 1; i <= n; i++)
-                if (!visited[i]) dfs(i, visited, adjList, s);
-
-            visited = new boolean[n + 1];
             int count = 0;
-            while (!s.isEmpty()) {
-                int v = s.pop();
-                
-                if (!visited[v]) {
-                    dfs(v, visited, adjList, s);
+            var visited = new boolean[n + 1];
+            for (int i = 1; i <= n; i++)
+                if (!visited[i] && indegree[i] == 0) {
                     count++;
+                    dfs(i, visited, adjList);
                 }
-            }
+
+            for (int i = 1; i <= n; i++)
+                if (!visited[i])
+                    for (var j : adjList[i]) adjList[(int) j].add(i);
+
+            for (int i = 1; i <= n; i++)
+                if (!visited[i]) {
+                    count++;
+                    dfs(i, visited, adjList);
+                }
 
             System.out.println(count);
         }
     }
 
-    static void dfs(int v, boolean[] visited, ArrayList<Integer>[] adjList, Stack<Integer> s) {
+    static void dfs(int v, boolean[] visited, ArrayList[] adjList) {
         visited[v] = true;
+
         for (var u : adjList[v])
-            if (!visited[u]) dfs(u, visited, adjList, s);
-        s.push(v);
+            if (!visited[(int) u]) dfs((int) u, visited, adjList);
     }
 }
