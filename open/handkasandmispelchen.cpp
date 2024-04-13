85b90d9f0bcaf71ea32fd5efc5e33ce6bf6d3c2f
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-04-13 22:25:06 +0800

    Create handkasandmispelchen.cpp

---
+++ open/handkasandmispelchen.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> h(k), m(k);
+    for (int i = 0; i < k; i++) cin >> h[i] >> m[i];
+
+    int switches = INT_MAX;
+    for (int i = 0; i < k; i++) {
+        auto temp_h = h, temp_m = m;
+        temp_h[i] = temp_m[i] = -1;
+        sort(temp_h.rbegin(), temp_h.rend());
+        sort(temp_m.rbegin(), temp_m.rend());
+
+
+        int n_h = n - h[i], n_m = n - m[i], s = 0;
+        auto sw = [&](int n, vector<int> temp) {
+            for (int j = 0; j < k && n > 0; j++) {
+                n -= temp[j];
+
+                if (n <= 0) s += j + 1;
+            }
+        };
+
+        sw(n_h, temp_h);
+        sw(n_m, temp_m);
+        switches = min(switches, s);
+    }
+
+    cout << switches;
+}
