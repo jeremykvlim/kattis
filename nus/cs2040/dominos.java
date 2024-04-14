a5f1ac986e53dd8570bfd98ec75ef5e08f96e550
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-04-14 21:31:51 +0800

    Update dominos.java

--- nus/cs2040/dominos.java
+++ nus/cs2040/dominos.java
@@ -12,42 +12,35 @@ public class dominos {
 
             var adjList = new ArrayList[n + 1];
             for (int i = 1; i <= n; i++) adjList[i] = new ArrayList<Integer>();
-            
+
             var indegree = new int[n + 1];
             while (m-- > 0) {
-                var vertices = br.readLine().split(" ");
-                int x = Integer.parseInt(vertices[0]), y = Integer.parseInt(vertices[1]);
+                var edge = br.readLine().split(" ");
+                int x = Integer.parseInt(edge[0]), y = Integer.parseInt(edge[1]);
 
                 adjList[x].add(y);
                 indegree[y]++;
             }
 
-            int count = 0;
             var visited = new boolean[n + 1];
-            for (int i = 1; i <= n; i++)
-                if (!visited[i] && indegree[i] == 0) {
-                    count++;
-                    dfs(i, visited, adjList);
-                }
-
-            for (int i = 1; i <= n; i++)
-                if (!visited[i])
-                    for (var j : adjList[i]) adjList[(int) j].add(i);
-
-            for (int i = 1; i <= n; i++)
-                if (!visited[i]) {
-                    count++;
-                    dfs(i, visited, adjList);
-                }
+            int count = 0;
+            for (int i = 1; i <= n; i++) {
+                if (dfs(visited, i, i, adjList)) indegree[i]--;
+                if (indegree[i] == 0) count++;
+            }
 
             System.out.println(count);
         }
     }
 
-    static void dfs(int v, boolean[] visited, ArrayList[] adjList) {
+    static boolean dfs(boolean[] visited, int v, int root, ArrayList[] adjList) {
         visited[v] = true;
 
-        for (var u : adjList[v])
-            if (!visited[(int) u]) dfs((int) u, visited, adjList);
+        for (var u : adjList[v]) {
+            if ((int) u == root) return true;
+            else if (!visited[(int) u]) return dfs(visited, (int) u, root, adjList);
+        }
+
+        return false;
     }
 }
