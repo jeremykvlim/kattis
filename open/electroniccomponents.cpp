220007da279f4dbb1fb77169b7ff9a49ebf4f9a4
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-04-19 23:32:43 +0800

    Update electroniccomponents.cpp

--- open/electroniccomponents.cpp
+++ open/electroniccomponents.cpp
@@ -9,24 +9,27 @@ int main() {
     cin >> n;
 
     vector<pair<int, long long>> components(n);
-    for (auto &c : components) cin >> c.first >> c.second;
+    int most = -1;
+    for (auto &[f, t] : components) {
+        cin >> f >> t;
+
+        most = max(most, f);
+    }
     sort(components.begin(), components.end(), [&](pair<int, int> p1, pair<int, int> p2) {return p1.second > p2.second;});
-    
-    int most = max_element(components.begin(), components.end())->first;
+
     vector<vector<pair<int, long long>>> placements(most + 1);
     vector<long long> dp(most + 1, LLONG_MAX);
-
     dp[0] = 0;
     for (int i = 0; i < n; i++) {
         auto [f, t] = components[i];
-        for (int j = 0; j < dp.size(); j++) 
+        for (int j = 0; j < dp.size(); j++)
             if (dp[j] != LLONG_MAX) placements[f + j - 2 * min(f, j)].emplace_back(f + j, dp[j] + (f - min(j, f)) * t);
-        
+
         for (int j = 0; j < 2; j++) {
             priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<>> pq;
             for (int k = j; k < dp.size(); k += 2) {
                 while (!pq.empty() && pq.top().second < k) pq.pop();
-                
+
                 for (auto &p : placements[k]) pq.emplace(p.second - k / 2 * t, p.first);
                 placements[k].clear();
                 dp[k] = !pq.empty() ? pq.top().first + k / 2 * t : LLONG_MAX;
