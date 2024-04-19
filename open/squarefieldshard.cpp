43a7cf34772850ccdb2aeca4d85697bfc02036e1
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-04-19 23:28:50 +0800

    Update squarefieldshard.cpp

--- open/squarefieldshard.cpp
+++ open/squarefieldshard.cpp
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void backtrack(int i, int squares, int curr, int &len, vector<pair<int, int>> &points, vector<pair<pair<int, int>, pair<int, int>>> &minmax) {
-    if (squares < minmax.size() && curr <= len) {
-        if (i == points.size()) len = min(len, curr);
+void backtrack(int i, int squares, int curr, int &y, vector<pair<int, int>> &points, vector<pair<pair<int, int>, pair<int, int>>> &minmax) {
+    if (squares < minmax.size() && curr <= y) {
+        if (i == points.size()) y = min(y, curr);
         else
             for (int j = 0; j <= squares; j++) {
                 auto original = minmax[j];
@@ -11,7 +11,7 @@ void backtrack(int i, int squares, int curr, int &len, vector<pair<int, int>> &p
                 p1 = {min(p1.first, points[i].first), min(p1.second, points[i].second)};
                 p2 = {max(p2.first, points[i].first), max(p2.second, points[i].second)};
 
-                backtrack(i + 1, max(squares, j + 1), max(curr, max(p2.first - p1.first, p2.second - p1.second)), len, points, minmax);
+                backtrack(i + 1, max(squares, j + 1), max(curr, max(p2.first - p1.first, p2.second - p1.second)), y, points, minmax);
                 minmax[j] = original;
             }
     }
@@ -29,12 +29,11 @@ int main() {
         cin >> n >> k;
 
         vector<pair<int, int>> points(n);
-        for (auto &p : points) cin >> p.first >> p.second;
+        for (auto &[X, Y] : points) cin >> X >> Y;
 
         vector<pair<pair<int, int>, pair<int, int>>> minmax(k + 1, {{INT_MAX, INT_MAX}, {INT_MIN, INT_MIN}});
-        int len = INT_MAX;
-        backtrack(0, 0, 0, len, points, minmax);
-
-        cout << "Case #" << x << ": " << len << "\n";
+        int y = INT_MAX;
+        backtrack(0, 0, 0, y, points, minmax);
+        cout << "Case #" << x << ": " << y << "\n";
     }
 }
