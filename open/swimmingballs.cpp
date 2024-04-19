ca4f8ff32b87fe2b6b7b9d69de5c2099672bc65f
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-04-19 23:22:59 +0800

    Update swimmingballs.cpp

--- open/swimmingballs.cpp
+++ open/swimmingballs.cpp
@@ -11,14 +11,15 @@ double height(double radius, double weight) {
         if (M_PI * m * m * (radius - m / 3.0) >= weight * 4 * M_PI * pow(radius, 3) / 3.0) r = m;
         else l = m;
     }
+
     return m;
 }
 
 double volume(double level, vector<pair<double, double>> &balls) {
     double total = 0;
-    for (auto &b : balls) {
-        double h = height(b.first, b.second);
-        total += M_PI * pow((h > level ? level : h), 2) * (b.first - (h > level ? level : h) / 3.0);
+    for (auto [r, w] : balls) {
+        auto h = height(r, w);
+        total += M_PI * pow(min(level, h), 2) * (r - min(level, h) / 3.0);
     }
     return total;
 }
@@ -36,13 +37,14 @@ int main() {
         cin >> n >> W >> L >> D >> V;
 
         vector<pair<double, double>> balls(n);
-        for (auto &b : balls) cin >> b.first >> b.second;
+        for (auto &[r, w] : balls) cin >> r >> w;
         double l = 0, r = D, m;
         while (l + EPSILON < r) {
             m = l + (r - l) / 2;
             if (W * L * m - volume(m, balls) > V) r = m;
             else l = m;
         }
+
         cout << fixed << setprecision(4) << m << "\n";
     }
 }
