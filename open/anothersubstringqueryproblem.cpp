9beef1a4f07bfd12c1f7282043c17092e16b3363
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-04-21 20:13:15 +0800

    Update anothersubstringqueryproblem.cpp

--- open/anothersubstringqueryproblem.cpp
+++ open/anothersubstringqueryproblem.cpp
@@ -1,88 +1,86 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> build(vector<int> &str, int ascii_range) {
+vector<int> sais(vector<int> &str, int range) {
     int n = str.size();
+    if (!n) return {};
+    if (n == 1) return {0};
+    if (n == 2) return str[0] < str[1] ? vector<int>{0, 1} : vector<int>{1, 0};
 
-    vector<int> sa(n), lms_i;
-    vector<bool> sl(n);
-    sl[n - 1] = false;
-    for (int i = n - 2; ~i; i--) {
-        sl[i] = (str[i] > str[i + 1] || (str[i] == str[i + 1] && sl[i + 1]));
-        if (sl[i] && !sl[i + 1]) lms_i.emplace_back(i + 1);
+    vector<int> sa(n, 0), sum_s(range + 1, 0), sum_l(range + 1, 0);
+    vector<bool> sl(n, false);
+    for (int i = n - 2; ~i; i--) sl[i] = (str[i] == str[i + 1]) ? sl[i + 1] : (str[i] < str[i + 1]);
+    for (int i = 0; i < n; i++) {
+        if (!sl[i]) sum_s[str[i]]++;
+        else sum_l[str[i] + 1]++;
     }
-    reverse(lms_i.begin(), lms_i.end());
 
-    auto induced_sort = [&](vector<int> lms) -> void {
-        vector<int> l(ascii_range, 0), r(ascii_range, 0);
-        for (int c : str) {
-            if (c + 1 < ascii_range) l[c + 1]++;
-            r[c]++;
-        }
-        partial_sum(l.begin(), l.end(), l.begin());
-        partial_sum(r.begin(), r.end(), r.begin());
+    for (int i = 0; i <= range; i++) {
+        sum_s[i] += sum_l[i];
+        if (i < range) sum_l[i + 1] += sum_s[i];
+    }
 
+    auto induced_sort = [&](vector<int> &lms) {
         fill(sa.begin(), sa.end(), -1);
-        for (int i = lms.size() - 1; ~i; i--) sa[--r[str[lms[i]]]] = lms[i];
-        for (int i : sa)
-            if (i && sl[i - 1]) sa[l[str[i - 1]]++] = i - 1;
+        vector<int> b(range + 1, 0);
+        copy(sum_s.begin(), sum_s.end(), b.begin());
+        for (auto i : lms)
+            if (i < n) sa[b[str[i]]++] = i;
 
-        fill(r.begin(), r.end(), 0);
-        for (int c : str) r[c]++;
-        partial_sum(r.begin(), r.end(), r.begin());
+        copy(sum_l.begin(), sum_l.end(), b.begin());
+        sa[b[str[n - 1]]++] = n - 1;
+        for (int j : sa)
+            if (j > 0 && !sl[j - 1]) sa[b[str[j - 1]]++] = j - 1;
 
-        for (int j = sa.size() - 1, i = sa[j]; ~j; i = sa[--j])
-            if (i && !sl[i - 1]) sa[--r[str[i - 1]]] = i - 1;
+        copy(sum_l.begin(), sum_l.end(), b.begin());
+        for (int i = n - 1, j = sa[i]; ~i; j = sa[--i])
+            if (j > 0 && sl[j - 1]) sa[--b[str[j - 1] + 1]] = j - 1;
     };
-    induced_sort(lms_i);
 
-    vector<int> lms_j(lms_i.size()), lms_str(lms_i.size());
-    for (int i = 0, j = 0; i < n; i++)
-        if (!sl[sa[i]] && sa[i] && sl[sa[i] - 1]) lms_j[j++] = sa[i];
-
-    int curr = 0;
-    sa[n - 1] = curr;
-    for (int k = 1; k < lms_j.size(); k++) {
-        int i = lms_j[k - 1], j = lms_j[k];
-        if (str[i] != str[j]) {
-            sa[j] = ++curr;
-            continue;
+    vector<int> lms_map(n + 1, -1), lms;
+    int m = 0;
+    for (int i = 1; i < n; i++)
+        if (!sl[i - 1] && sl[i]) {
+            lms_map[i] = m++;
+            lms.emplace_back(i);
         }
+    induced_sort(lms);
 
-        bool diff = false;
-        for (int a = i + 1, b = j + 1;; a++, b++) {
-            if (str[a] != str[b]) {
-                diff = true;
-                break;
+    if (m) {
+        vector<int> lms_sorted, str2(m);
+        for (int j : sa)
+            if (lms_map[j] != -1) lms_sorted.emplace_back(j);
+
+        int range2 = 0;
+        str2[lms_map[lms_sorted[0]]] = 0;
+        for (int i = 1; i < m; i++) {
+            int l = lms_sorted[i - 1], r = lms_sorted[i], l_end = (lms_map[l] + 1 < m) ? lms[lms_map[l] + 1] : n, r_end = (lms_map[r] + 1 < m) ? lms[lms_map[r] + 1] : n;
+            bool same = true;
+            if (l_end - l != r_end - r) same = false;
+            else {
+                while (l < l_end && str[l] == str[r]) {
+                    l++;
+                    r++;
+                }
+
+                if (l == n || str[l] != str[r]) same = false;
             }
 
-            if ((!sl[a] && sl[a - 1]) || (!sl[b] && sl[b - 1])) {
-                diff = !((!sl[a] && sl[a - 1]) && (!sl[b] && sl[b - 1]));
-                break;
-            }
+            if (!same) range2++;
+            str2[lms_map[lms_sorted[i]]] = range2;
         }
 
-        sa[j] = diff ? ++curr : curr;
+        auto sa2 = sais(str2, range2);
+        for (int i = 0; i < m; i++) lms_sorted[i] = lms[sa2[i]];
+        induced_sort(lms_sorted);
     }
-
-    for (int i = 0; i < lms_i.size(); i++) lms_str[i] = sa[lms_i[i]];
-    if (curr + 1 < lms_i.size()) {
-        auto lms_SA = build(lms_str, curr + 1);
-        for (int i = 0; i < lms_i.size(); i++) lms_j[i] = lms_i[lms_SA[i]];
-    }
-    induced_sort(lms_j);
 
     return sa;
 }
 
 vector<int> suffix_array(string &s, int ascii_range = 128) {
     vector<int> str(s.begin(), s.end());
-    str.emplace_back('$');
-
-    auto sa = build(str, ascii_range);
-    sa.erase(sa.begin());
-
-    return sa;
+    return sais(str, ascii_range);
 }
 
 struct Hash {
