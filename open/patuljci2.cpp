f39f74eed00e80b560868c61e48fbad06207d240
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-05-02 16:42:18 +0800

    Create patuljci2.cpp

---
+++ open/patuljci2.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, c;
+    cin >> n >> c;
+
+    vector<vector<int>> indices(c + 1);
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+
+        indices[x].emplace_back(i);
+    }
+
+    int m;
+    cin >> m;
+
+    while (m--) {
+        int a, b;
+        cin >> a >> b;
+
+        for (int x = 1; x <= c; x++)
+            if ((lower_bound(begin(indices[x]), end(indices[x]), b) - lower_bound(begin(indices[x]), end(indices[x]), a - 1)) > (b - a + 1) / 2) {
+                cout << "yes " << x << "\n";
+                goto next;
+            }
+
+        cout << "no\n";
+        next:;
+    }
+}
