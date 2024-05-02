d0ebc94b03297a5f3db33711c0f181971a5afd8f
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-05-02 17:26:55 +0800

    Create dugovi.cpp

---
+++ open/dugovi.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    vector<int> a(n + 1), b(n + 1), outdegree(n + 1, 0);
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i] >> b[i];
+
+        outdegree[a[i]]++;
+    }
+
+    vector<int> sum(n + 1, 0);
+    vector<bool> visited(n + 1, false);
+    int money = 0;
+    for (int i = 1; i <= n; i++)
+        if (!outdegree[i] && !visited[i])
+            for (int j = i, k = a[j]; !outdegree[j]; j = a[j], k = a[k]) {
+                money += max(0, b[j] - sum[j]);
+                sum[k] += b[j];
+                visited[j] = true;
+                outdegree[j] = 0;
+                outdegree[k]--;
+            }
+
+    for (int i = 1; i <= n; i++)
+        if (outdegree[i]) {
+            int m = INT_MAX;
+            for (int j = i, k = a[j]; outdegree[k]; j = k, k = a[k]) {
+                int diff1 = max(0, b[k] - b[j] - sum[k]), diff2 = max(0, b[k] - sum[k]);
+                money += diff1;
+                m = min(m, diff2 - diff1);
+                outdegree[k] = 0;
+            }
+            money += m;
+        }
+
+    cout << money;
+}
