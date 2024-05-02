70effd61a51340ff01f13fed8e13ed8d4c1ed9e4
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-05-02 21:08:25 +0800

    Create mafija.cpp

---
+++ nus/cs4234/mafija.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    vector<int> a(n), indegree(n, 0);
+    for (int &ai : a) {
+        cin >> ai;
+
+        indegree[--ai]++;
+    }
+
+    vector<bool> visited(n, false);
+    int count = 0;
+    auto dfs = [&](auto &&self, int v, bool mafia) -> void {
+        visited[v] = true;
+        indegree[a[v]]--;
+        if (mafia) count++;
+        if ((!indegree[a[v]] || mafia) && !visited[a[v]]) self(self, a[v], !mafia);
+    };
+
+    for (int i = 0; i < n; i++)
+        if (!indegree[i] && !visited[i]) dfs(dfs, i, true);
+
+    for (int i = 0; i < n; i++)
+        if (!visited[i]) dfs(dfs, i, false);
+
+    cout << count;
+}
