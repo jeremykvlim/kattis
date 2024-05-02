19013e635fa78bf721d0c23865b4629532e8914e
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-05-02 11:41:33 +0800

    Update allpairspath.java

--- nus/cs2040/allpairspath.java
+++ nus/cs2040/allpairspath.java
@@ -10,33 +10,33 @@ public class allpairspath {
             var line = input.split(" ");
             int n = Integer.parseInt(line[0]), m = Integer.parseInt(line[1]), q = Integer.parseInt(line[2]);
 
-            var adjMatrix = new int[n][n];
-            for (var row : adjMatrix) Arrays.fill(row, Integer.MAX_VALUE);
-            for (int i = 0; i < n; i++) adjMatrix[i][i] = 0;
+            var dist = new int[n][n];
+            for (var row : dist) Arrays.fill(row, Integer.MAX_VALUE);
+            for (int i = 0; i < n; i++) dist[i][i] = 0;
 
             while (m-- > 0) {
                 var edge = br.readLine().split(" ");
                 int u = Integer.parseInt(edge[0]), v = Integer.parseInt(edge[1]), w = Integer.parseInt(edge[2]);
-                adjMatrix[u][v] = Math.min(adjMatrix[u][v], w);
+                dist[u][v] = Math.min(dist[u][v], w);
             }
 
             for (int i = 0; i < n; i++)
                 for (int j = 0; j < n; j++)
                     for (int k = 0; k < n; k++)
-                        if (adjMatrix[j][i] != Integer.MAX_VALUE && adjMatrix[i][k] != Integer.MAX_VALUE)
-                            adjMatrix[j][k] = Math.min(adjMatrix[j][k], adjMatrix[j][i] + adjMatrix[i][k]);
+                        if (dist[j][i] != Integer.MAX_VALUE && dist[i][k] != Integer.MAX_VALUE)
+                            dist[j][k] = Math.min(dist[j][k], dist[j][i] + dist[i][k]);
 
             for (int i = 0; i < n; i++)
-                if (adjMatrix[i][i] < 0)
+                if (dist[i][i] < 0)
                     for (int j = 0; j < n; j++)
                         for (int k = 0; k < n; k++)
-                            if (adjMatrix[j][i] != Integer.MAX_VALUE && adjMatrix[i][k] != Integer.MAX_VALUE)
-                                adjMatrix[j][k] = -Integer.MAX_VALUE;
+                            if (dist[j][i] != Integer.MAX_VALUE && dist[i][k] != Integer.MAX_VALUE)
+                                dist[j][k] = -Integer.MAX_VALUE;
 
 
             while (q-- > 0) {
                 var query = br.readLine().split(" ");
-                int u = Integer.parseInt(query[0]), v = Integer.parseInt(query[1]), w = adjMatrix[u][v];
+                int u = Integer.parseInt(query[0]), v = Integer.parseInt(query[1]), w = dist[u][v];
                 pw.println(w == Integer.MAX_VALUE ? "Impossible" : (w == -Integer.MAX_VALUE ? "-Infinity" : w));
             }
             pw.println();
