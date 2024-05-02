6d5324eb1e83c7b3c78081dc657f744ac3ba26d0
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-05-02 15:55:12 +0800

    Update treasure.cpp

--- nus/cs3233/treasure.cpp
+++ nus/cs3233/treasure.cpp
@@ -9,47 +9,65 @@ int main() {
     cin >> n >> m >> k;
 
     vector<vector<char>> grid(n, vector<char>(m));
-    pair<int, int> s;
+    pair<int, int> s, e;
     for (int r = 0; r < n; r++)
         for (int c = 0; c < m; c++) {
             cin >> grid[r][c];
 
             if (grid[r][c] == 'S') s = {r, c};
+            if (grid[r][c] == 'G') e = {r, c};
         }
 
     vector<int> dr{0, 0, 1, -1}, dc{1, -1, 0, 0};
-    unordered_map<char, int> cost{{'.', 1}, {'F', 2}, {'M', 3}, {'G', 1}};
-    vector<vector<vector<int>>> dist(n, vector<vector<int>>(m, vector<int>(k + 1, INT_MAX)));
+    vector<vector<vector<int>>> dist(n, vector<vector<int>>(m, vector<int>(k + 1, -1)));
     dist[s.first][s.second][k] = 1;
-    priority_queue<tuple<int, pair<int, int>, int>, vector<tuple<int, pair<int, int>, int>>, greater<>> pq;
-    pq.emplace(1, s, k);
+    priority_queue<tuple<int, int, int, int>, vector<tuple<int, int, int, int>>, greater<>> pq;
+    pq.emplace(1, s.first, s.second, k);
     while (!pq.empty()) {
-        auto [d, p, stam] = pq.top();
+        auto [d, x, y, stam] = pq.top();
         pq.pop();
-        auto [x, y] = p;
-
-        if (d > dist[x][y][stam]) continue;
-
-        if (grid[x][y] == 'G') {
-            cout << d;
-            exit(0);
-        }
 
         for (int i = 0; i < 4; i++) {
             int r = x + dr[i], c = y + dc[i];
-            if (r < 0 || r >= n || c < 0 || c >= m || grid[r][c] == '#' || grid[r][c] == 'S' || cost[grid[r][c]] > stam) continue;
+            if (r < 0 || r >= n || c < 0 || c >= m) continue;
 
-            if (dist[r][c][stam - cost[grid[r][c]]] > dist[x][y][stam]) {
-                dist[r][c][stam - cost[grid[r][c]]] = dist[x][y][stam];
-                pq.emplace(dist[r][c][stam - cost[grid[r][c]]], make_pair(r, c), stam - cost[grid[r][c]]);
+            switch (grid[r][c]) {
+                case 'F':
+                    if (stam >= 2)
+                        if (dist[r][c][stam - 2] == -1) {
+                            dist[r][c][stam - 2] = dist[x][y][stam];
+                            pq.emplace(d, r, c, stam - 2);
+                        }
+                    break;
+                case 'M':
+                    if (stam >= 3)
+                        if (dist[r][c][stam - 3] == -1) {
+                            dist[r][c][stam - 3] = dist[x][y][stam];
+                            pq.emplace(d, r, c, stam - 3);
+                        }
+                    break;
+                case '#':
+                    if (dist[r][c][stam] == -1) dist[r][c][stam] = INT_MAX;
+                    break;
+                default:
+                    if (stam >= 1)
+                        if (dist[r][c][stam - 1] == -1) {
+                            dist[r][c][stam - 1] = dist[x][y][stam];
+                            pq.emplace(d, r, c, stam - 1);
+                        }
+                    break;
             }
         }
 
-        if (dist[x][y][k] > dist[x][y][stam] + 1) {
-            dist[x][y][k] = dist[x][y][stam] + 1;
-            pq.emplace(dist[x][y][k], p, k);
+        if (dist[x][y][k] == -1) {
+            dist[x][y][k] = d + 1;
+            pq.emplace(d + 1, x, y, k);
         }
     }
 
-    cout << -1;
+    int days = INT_MAX;
+    for (int d : dist[e.first][e.second])
+        if (d != -1) days = min(days, d);
+
+    cout << (days == INT_MAX ? -1 : days);
 }
