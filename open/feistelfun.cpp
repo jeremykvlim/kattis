b51b5afd302bf4af54e65c6a999a515da52a2340
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-05-07 13:44:31 +0800

    Update feistelfun.cpp

--- open/feistelfun.cpp
+++ open/feistelfun.cpp
@@ -8,7 +8,7 @@ int main() {
     int m;
     cin >> m;
 
-    int upper = m / 2 + 1;
+    int bound = m / 2 + 1;
     unordered_map<bitset<80>, long long> seen;
     bitset<80> x1(0), y1(1), x2(1), y2(0);
 
@@ -16,17 +16,17 @@ int main() {
         return ((y << shift) >> m) ^ (y << shift) & bitset<80>((1LL << m) - 1);
     };
 
-    for (auto i = 0LL; i < 1 << upper; i++) {
+    for (auto i = 0LL; i < 1 << bound; i++) {
         if (!seen.count((y1 << m) | x1)) seen[(y1 << m) | x1] = i;
         x1 ^= f(y1);
         swap(y1, x1);
     }
 
     auto rounds = LLONG_MAX;
-    for (auto i = 0LL; i < 1 << upper; i++) {
-        if (seen.count((y2 << m) | x2)) rounds = min(rounds, seen[(y2 << m) | x2] + (i << upper));
-        x2 ^= f(y1, (1 << upper) % m);
-        y2 ^= f(x1, (1 << upper) % m);
+    for (auto i = 0LL; i < 1 << bound; i++) {
+        if (seen.count((y2 << m) | x2)) rounds = min(rounds, seen[(y2 << m) | x2] + (i << bound));
+        x2 ^= f(y1, (1 << bound) % m);
+        y2 ^= f(x1, (1 << bound) % m);
         swap(y1, x2);
         swap(y2, x1);
     }
