b6ea2549008d6ad2a36dbbf5727b8bb830e8b140
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-05-20 14:28:24 +0800

    Update integerlists.java

--- nus/cs2040/integerlists.java
+++ nus/cs2040/integerlists.java
@@ -1,66 +1,48 @@
 import java.io.*;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class integerlists {
     public static void main(String[] args) throws IOException {
         var br = new BufferedReader(new InputStreamReader(System.in));
         var pw = new PrintWriter(System.out);
-        
+
         int t = Integer.parseInt(br.readLine());
+        next:
         while (t-- > 0) {
             var program = br.readLine();
             int elements = Integer.parseInt(br.readLine());
-            
+
             if (elements == 0) {
                 var empty = br.readLine();
-                var error = program.contains("D");
-                if (!error) pw.println(empty);
-                else pw.println("error");
+                pw.println(program.contains("D") ? "error" : empty);
                 continue;
             }
-            
-            var list = br.readLine().replaceAll("\\[","").replaceAll("]","").split(",");
-            var deque = new ArrayDeque<Integer>();
-            
-            for (var s : list) {
-                int element = Integer.parseInt(s);
-                deque.add(element);
-            }
-            
-            boolean reversed = false, empty = false;
-            
-            for (int i = 0; i < program.length(); i++) {
+
+            var dq = Arrays.stream(br.readLine().replaceAll("\\[","").replaceAll("]","").split(","))
+                     .map(Integer::parseInt).collect(Collectors.toCollection(ArrayDeque::new));
+
+            var reversed = false;
+            for (int i = 0; i < program.length(); i++)
                 if (program.charAt(i) == 'R') reversed = !reversed;
                 else {
-                    if (!deque.isEmpty()) {
-                        if (!reversed) deque.removeFirst();
-                        else deque.removeLast();
+                    if (!dq.isEmpty()) {
+                        if (!reversed) dq.removeFirst();
+                        else dq.removeLast();
                     }
                     else {
                         pw.println("error");
-                        empty = true;
-                        break;
+                        continue next;
                     }
                 }
-            }
-            
-            if (empty) continue;
-            int size = deque.size();
+
             pw.print("[");
-            
-            if (!reversed)
-                for (int i = 0; i < size; i++) {
-                    if (deque.size() != 1) pw.print(deque.removeFirst()+",");
-                    else pw.print(deque.removeFirst());
-                }
-            else
-                for (int i = 0; i < size; i++) {
-                    if (deque.size() != 1) pw.print(deque.removeLast() + ",");
-                    else pw.print(deque.removeLast());
-                }
-                
+            while (!dq.isEmpty())
+                if (dq.size() > 1) pw.print((!reversed ? dq.removeFirst() : dq.removeLast()) + ",");
+                else pw.print(!reversed ? dq.removeFirst() : dq.removeLast());
             pw.println("]");
         }
+        
         pw.flush();
     }
 }
