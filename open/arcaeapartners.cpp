7810c85c11cc51264f72e8869faed9b3935652ea
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-06-06 19:38:09 +0800

    Update arcaeapartners.cpp

--- open/arcaeapartners.cpp
+++ open/arcaeapartners.cpp
@@ -1,15 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void update(vector<vector<int>> &fenwick, int i, int j, int value) {
-    for (; i < fenwick[j].size(); i += i & -i) fenwick[j][i] = max(fenwick[j][i], value);
-}
+template <typename T>
+struct FenwickTree2D {
+    vector<vector<T>> BIT;
 
-int query(vector<vector<int>> &fenwick, int i, int j) {
-    int value = 0;
-    for (; i; i &= (i - 1)) value = max(value, fenwick[j][i]);
-    return value;
-}
+    void update(int i, int j, T value) {
+        for (; i < BIT[j].size(); i += i & -i) BIT[j][i] = max(BIT[j][i], value);
+    }
+
+    T rmq(int i, int j) {
+        T value = 0;
+        for (; i; i &= (i - 1)) value = max(value, BIT[j][i]);
+        return value;
+    }
+
+    FenwickTree2D(int n, int m) : BIT(n, vector<T>(m, 0)) {}
+};
 
 int main() {
     ios::sync_with_stdio(false);
@@ -32,19 +39,19 @@ int main() {
         }
     }
 
-    int index = 0;
-    for (auto &[p, i] : step) i = ++index;
+    int index = 1;
+    for (auto &[p, i] : step) i = index++;
     for (auto &[g, p, b] : values) p = step[p];
     sort(values.begin(), values.end(), [&](auto v1, auto v2) {return get<0>(v1) != get<0>(v2) ? get<0>(v1) > get<0>(v2) : get<1>(v1) < get<1>(v2);});
 
-    vector<vector<int>> fenwick(max(n, k) + 1, vector<int>(index + 1, 0));
+    FenwickTree2D<int> fw(max(n, k) + 1, index + 1);
     for (auto [g, p, b] : values)
         if (!b)
-            for (int i = 0; i <= k; i++) update(fenwick, p, i, query(fenwick, p, i) + 1);
+            for (int i = 0; i <= k; i++) fw.update(p, i, fw.rmq(p, i) + 1);
         else
-            for (int i = k; i; i--) update(fenwick, p, i, query(fenwick, p, i - 1) + 1);
+            for (int i = k; i; i--) fw.update(p, i, fw.rmq(p, i - 1) + 1);
 
     int d = 0;
-    for (int i = 1; i <= n; i++) d = max(d, query(fenwick, index, i));
+    for (int i = 1; i <= n; i++) d = max(d, fw.rmq(index, i));
     cout << d;
 }
