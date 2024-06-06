4d391efd309ce60e21374aa8ef7ca6faaad27bd1
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-06-06 19:02:53 +0800

    Update kindaokarray.cpp

--- nus/cs3233/kindaokarray.cpp
+++ nus/cs3233/kindaokarray.cpp
@@ -1,86 +1,109 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> sais(vector<int> &str, int range) {
-    int n = str.size();
-    if (!n) return {};
-    if (n == 1) return {0};
-    if (n == 2) return str[0] < str[1] ? vector<int>{0, 1} : vector<int>{1, 0};
+struct SuffixArray {
+    vector<int> SA, ascii, lcp;
 
-    vector<int> sa(n, 0), sum_s(range + 1, 0), sum_l(range + 1, 0);
-    vector<bool> sl(n, false);
-    for (int i = n - 2; ~i; i--) sl[i] = (str[i] == str[i + 1]) ? sl[i + 1] : (str[i] < str[i + 1]);
-    for (int i = 0; i < n; i++) {
-        if (!sl[i]) sum_s[str[i]]++;
-        else sum_l[str[i] + 1]++;
-    }
+    vector<int> sais(vector<int> &ascii1, int range) {
+        int n = ascii1.size();
+        if (!n) return {};
+        if (n == 1) return {0};
+        if (n == 2) ascii1[0] < ascii1[1] ? vector<int>{0, 1} : vector<int>{1, 0};
 
-    for (int i = 0; i <= range; i++) {
-        sum_s[i] += sum_l[i];
-        if (i < range) sum_l[i + 1] += sum_s[i];
-    }
-
-    auto induced_sort = [&](vector<int> &lms) {
-        fill(sa.begin(), sa.end(), -1);
-        vector<int> b(range + 1, 0);
-        copy(sum_s.begin(), sum_s.end(), b.begin());
-        for (int i : lms)
-            if (i < n) sa[b[str[i]]++] = i;
-
-        copy(sum_l.begin(), sum_l.end(), b.begin());
-        sa[b[str[n - 1]]++] = n - 1;
-        for (int j : sa)
-            if (j > 0 && !sl[j - 1]) sa[b[str[j - 1]]++] = j - 1;
-
-        copy(sum_l.begin(), sum_l.end(), b.begin());
-        for (int i = n - 1, j = sa[i]; ~i; j = sa[--i])
-            if (j > 0 && sl[j - 1]) sa[--b[str[j - 1] + 1]] = j - 1;
-    };
-
-    vector<int> lms_map(n + 1, -1), lms;
-    int m = 0;
-    for (int i = 1; i < n; i++)
-        if (!sl[i - 1] && sl[i]) {
-            lms_map[i] = m++;
-            lms.emplace_back(i);
+        vector<int> sa(n, 0), sum_s(range + 1, 0), sum_l(range + 1, 0);
+        vector<bool> sl(n, false);
+        for (int i = n - 2; ~i; i--) sl[i] = (ascii1[i] == ascii1[i + 1]) ? sl[i + 1] : (ascii1[i] < ascii1[i + 1]);
+        for (int i = 0; i < n; i++) {
+            if (!sl[i]) sum_s[ascii1[i]]++;
+            else sum_l[ascii1[i] + 1]++;
         }
-    induced_sort(lms);
 
-    if (m) {
-        vector<int> lms_sorted, str2(m);
-        for (int j : sa)
-            if (lms_map[j] != -1) lms_sorted.emplace_back(j);
+        for (int i = 0; i <= range; i++) {
+            sum_s[i] += sum_l[i];
+            if (i < range) sum_l[i + 1] += sum_s[i];
+        }
 
-        int range2 = 0;
-        str2[lms_map[lms_sorted[0]]] = 0;
-        for (int i = 1; i < m; i++) {
-            int l = lms_sorted[i - 1], r = lms_sorted[i], l_end = (lms_map[l] + 1 < m) ? lms[lms_map[l] + 1] : n, r_end = (lms_map[r] + 1 < m) ? lms[lms_map[r] + 1] : n;
-            bool same = true;
-            if (l_end - l != r_end - r) same = false;
-            else {
-                while (l < l_end && str[l] == str[r]) {
-                    l++;
-                    r++;
+        auto induced_sort = [&](vector<int> &lms) {
+            fill(sa.begin(), sa.end(), -1);
+            vector<int> b(range + 1, 0);
+            copy(sum_s.begin(), sum_s.end(), b.begin());
+            for (int i : lms)
+                if (i < n) sa[b[ascii1[i]]++] = i;
+
+            copy(sum_l.begin(), sum_l.end(), b.begin());
+            sa[b[ascii1[n - 1]]++] = n - 1;
+            for (int j : sa)
+                if (j > 0 && !sl[j - 1]) sa[b[ascii1[j - 1]]++] = j - 1;
+
+            copy(sum_l.begin(), sum_l.end(), b.begin());
+            for (int i = n - 1, j = sa[i]; ~i; j = sa[--i])
+                if (j > 0 && sl[j - 1]) sa[--b[ascii1[j - 1] + 1]] = j - 1;
+        };
+
+        vector<int> lms_map(n + 1, -1), lms;
+        int m = 0;
+        for (int i = 1; i < n; i++)
+            if (!sl[i - 1] && sl[i]) {
+                lms_map[i] = m++;
+                lms.emplace_back(i);
+            }
+        induced_sort(lms);
+
+        if (m) {
+            vector<int> lms_sorted, ascii2(m);
+            for (int j : sa)
+                if (lms_map[j] != -1) lms_sorted.emplace_back(j);
+
+            int range2 = 0;
+            ascii2[lms_map[lms_sorted[0]]] = 0;
+            for (int i = 1; i < m; i++) {
+                int l = lms_sorted[i - 1], r = lms_sorted[i], l_end = (lms_map[l] + 1 < m) ? lms[lms_map[l] + 1] : n, r_end = (lms_map[r] + 1 < m) ? lms[lms_map[r] + 1] : n;
+                bool same = true;
+                if (l_end - l != r_end - r) same = false;
+                else {
+                    while (l < l_end && ascii1[l] == ascii1[r]) {
+                        l++;
+                        r++;
+                    }
+
+                    if (l == n || ascii1[l] != ascii1[r]) same = false;
                 }
 
-                if (l == n || str[l] != str[r]) same = false;
+                if (!same) range2++;
+                ascii2[lms_map[lms_sorted[i]]] = range2;
             }
 
-            if (!same) range2++;
-            str2[lms_map[lms_sorted[i]]] = range2;
+            auto sa2 = sais(ascii2, range2);
+            for (int i = 0; i < m; i++) lms_sorted[i] = lms[sa2[i]];
+            induced_sort(lms_sorted);
         }
 
-        auto sa2 = sais(str2, range2);
-        for (int i = 0; i < m; i++) lms_sorted[i] = lms[sa2[i]];
-        induced_sort(lms_sorted);
+        return sa;
     }
 
-    return sa;
-}
+    void kasai() {
+        int n = ascii.size();
+        lcp.resize(n - 1);
+        vector<int> rank(n);
+        for (int i = 0; i < n; i++) rank[SA[i]] = i;
+        for (int i = 0, k = 0; i < n; i++) {
+            if (k) k--;
+            if (!rank[i]) continue;
 
-vector<int> suffix_array(vector<int> &str, int range = 128) {
-    return sais(str, range);
-}
+            int j = SA[rank[i] - 1];
+            while (i + k < n && j + k < n && ascii[i + k] == ascii[j + k]) k++;
+            lcp[rank[i] - 1] = k;
+        }
+    }
+
+    auto operator[](int i) {
+        return SA[i];
+    }
+
+    SuffixArray(vector<int> a, int r = 128) : ascii(a.begin(), a.end()) {
+        SA = sais(ascii, r);
+    };
+};
 
 vector<int> kasai(vector<int> s, vector<int> sa) {
     int n = s.size();
@@ -123,11 +146,12 @@ int main() {
         if (i && a[indices[i - 1]] != a[indices[i]]) range++;
         str[indices[i]] = range;
     }
-    auto sa = suffix_array(str, ++range), lcp = kasai(str, sa);
 
+    SuffixArray sa(str, ++range);
+    sa.kasai();
     auto even = 0LL;
     for (int i = 0; i < n; i++) {
-        int j = i ? sa[i] + lcp[i - 1] : sa[i], count = pref_sum[n] - pref_sum[j];
+        int j = i ? sa[i] + sa.lcp[i - 1] : sa[i], count = pref_sum[n] - pref_sum[j];
         even += (pref_xor[sa[i]]) ? count : n - j - count;
     }
 
