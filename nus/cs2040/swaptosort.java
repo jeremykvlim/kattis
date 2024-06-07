750a542427679ac4b8772720f8909a01a23b3331
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-06-08 03:00:26 +0800

    Update swaptosort.java

--- nus/cs2040/swaptosort.java
+++ nus/cs2040/swaptosort.java
@@ -3,21 +3,20 @@ import java.io.*;
 public class swaptosort {
     public static void main(String[] args) throws IOException {
         var br = new BufferedReader(new InputStreamReader(System.in));
-        
+
         var input = br.readLine().split(" ");
         int n = Integer.parseInt(input[0]), k = Integer.parseInt(input[1]);
 
-        var sets = new int[n + 1];
-        for (int i = 1; i <= n; i++) sets[i] = i;
+        var dsu = new DisjointSet(n + 1);
 
         while (k-- > 0) {
             var pair = br.readLine().split(" ");
-            int a = Integer.parseInt(pair[0]), b = Integer.parseInt(pair[1]), aSet = find(a, sets), bSet = find(b, sets);
-            if (aSet != bSet) sets[bSet] = aSet;
+            int a = Integer.parseInt(pair[0]), b = Integer.parseInt(pair[1]);
+            dsu.unite(a, b);
         }
 
         for (int i = 1, j = n; i <= n / 2; i++, j--)
-            if (find(i, sets) != find(j, sets)) {
+            if (dsu.find(i) != dsu.find(j)) {
                 System.out.println("No");
                 return;
             }
@@ -25,7 +24,30 @@ public class swaptosort {
         System.out.println("Yes");
     }
 
-    static int find(int p, int[] sets) {
-        return (sets[p] == p) ? p : (sets[p] = find(sets[p], sets));
+    static class DisjointSet {
+        int[] sets, size;
+
+        DisjointSet(int n) {
+            sets = new int[n];
+            size = new int[n];
+            for (int i = 0; i < n; i++) {
+                sets[i] = i;
+                size[i] = 1;
+            }
+        }
+
+        boolean unite(int p, int q) {
+            int p_set = find(p), q_set = find(q);
+            if (p_set != q_set) {
+                sets[q_set] = p_set;
+                size[p_set] += size[q_set];
+                return true;
+            }
+            return false;
+        }
+
+        int find(int p) {
+            return (sets[p] == p) ? p : (sets[p] = find(sets[p]));
+        }
     }
 }
