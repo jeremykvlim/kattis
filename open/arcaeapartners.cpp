b0911123e535b8c8e1026af3eb1c82c9ed7f03a1
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-06-08 02:16:00 +0800

    Update arcaeapartners.cpp

--- open/arcaeapartners.cpp
+++ open/arcaeapartners.cpp
@@ -1,21 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template <typename T>
+template <typename T, typename F>
 struct FenwickTree2D {
     vector<vector<T>> BIT;
+    F f;
 
     void update(int i, int j, T value) {
-        for (; i < BIT[j].size(); i += i & -i) BIT[j][i] = max(BIT[j][i], value);
+        for (; i < BIT[j].size(); i += i & -i) BIT[j][i] = f(BIT[j][i], value);
     }
 
-    T rmq(int i, int j) {
+    T range_query(int i, int j) {
         T value = 0;
-        for (; i; i &= (i - 1)) value = max(value, BIT[j][i]);
+        for (; i; i &= (i - 1)) value = f(value, BIT[j][i]);
         return value;
     }
 
-    FenwickTree2D(int n, int m) : BIT(n, vector<T>(m, 0)) {}
+    FenwickTree2D(int n, int m, F func) : BIT(n, vector<T>(m, 0)), f(func) {}
 };
 
 int main() {
@@ -44,14 +45,15 @@ int main() {
     for (auto &[g, p, b] : values) p = step[p];
     sort(values.begin(), values.end(), [&](auto v1, auto v2) {return get<0>(v1) != get<0>(v2) ? get<0>(v1) > get<0>(v2) : get<1>(v1) < get<1>(v2);});
 
-    FenwickTree2D<int> fw(max(n, k) + 1, index + 1);
+    auto _max = [](int x, int y) {return max(x, y);};
+    FenwickTree2D<int, decltype(_max)> fw(max(n, k) + 1, index + 1, _max);
     for (auto [g, p, b] : values)
         if (!b)
-            for (int i = 0; i <= k; i++) fw.update(p, i, fw.rmq(p, i) + 1);
+            for (int i = 0; i <= k; i++) fw.update(p, i, fw.range_query(p, i) + 1);
         else
-            for (int i = k; i; i--) fw.update(p, i, fw.rmq(p, i - 1) + 1);
+            for (int i = k; i; i--) fw.update(p, i, fw.range_query(p, i - 1) + 1);
 
     int d = 0;
-    for (int i = 1; i <= n; i++) d = max(d, fw.rmq(index, i));
+    for (int i = 1; i <= n; i++) d = max(d, fw.range_query(index, i));
     cout << d;
 }
