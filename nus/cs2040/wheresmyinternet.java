0bbfad68eee6c797ea7eb9bada802ae9dd2873a1
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-06-08 02:51:26 +0800

    Update wheresmyinternet.java

--- nus/cs2040/wheresmyinternet.java
+++ nus/cs2040/wheresmyinternet.java
@@ -4,22 +4,20 @@ public class wheresmyinternet {
     public static void main(String[] args) throws IOException {
         var br = new BufferedReader(new InputStreamReader(System.in));
         var pw = new PrintWriter(System.out);
-        
+
         var input = br.readLine().split(" ");
         int n = Integer.parseInt(input[0]), m = Integer.parseInt(input[1]);
 
-        var sets = new int[n + 1];
-        for (int i = 1; i <= n; i++) sets[i] = i;
-
+        var dsu = new DisjointSet(n + 1);
         while (m-- > 0) {
             var line = br.readLine().split(" ");
-            int a = Integer.parseInt(line[0]), b = Integer.parseInt(line[1]), aSet = find(a, sets), bSet = find(b, sets);
-            sets[bSet] = aSet;
+            int a = Integer.parseInt(line[0]), b = Integer.parseInt(line[1]);
+            dsu.unite(a, b);
         }
 
         var connected = true;
-        for (int i = 2, root = find(1, sets); i <= n; i++)
-            if (root != find(i, sets)) {
+        for (int i = 2, root = dsu.find(1); i <= n; i++)
+            if (root != dsu.find(i)) {
                 pw.println(i);
                 connected = false;
             }
@@ -28,7 +26,30 @@ public class wheresmyinternet {
         pw.flush();
     }
 
-    static int find(int p, int[] sets) {
-        return (sets[p] == p) ? p : (sets[p] = find(sets[p], sets));
+    static class DisjointSet {
+        int[] sets, size;
+
+        DisjointSet(int n) {
+            sets = new int[n];
+            size = new int[n];
+            for (int i = 0; i < n; i++) {
+                sets[i] = i;
+                size[i] = 1;
+            }
+        }
+
+        boolean unite(int p, int q) {
+            int p_set = find(p), q_set = find(q);
+            if (p_set != q_set) {
+                sets[q_set] = p_set;
+                size[p_set] += size[q_set];
+                return true;
+            }
+            return false;
+        }
+
+        int find(int p) {
+            return (sets[p] == p) ? p : (sets[p] = find(sets[p]));
+        }
     }
 }
