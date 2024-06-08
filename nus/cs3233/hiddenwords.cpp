7abe76eeff3e691fff11f1eee060640640dd522b
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-06-08 17:32:14 +0800

    Update hiddenwords.cpp

--- nus/cs3233/hiddenwords.cpp
+++ nus/cs3233/hiddenwords.cpp
@@ -1,32 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct TrieNode {
-    vector<int> next;
-    int count = 0;
+struct Trie {
+    enum ascii {
+        LOWER = 97,
+        UPPER = 65
+    };
 
-    TrieNode() {
-        next.resize(26, -1);
+    struct TrieNode {
+        vector<int> next;
+        int count = 0;
+
+        TrieNode(int range = 26) : next(range, -1) {}
+    };
+
+    vector<TrieNode> T;
+    ascii a;
+
+    Trie(int n = 1, ascii a = LOWER) : T(n), a(a) {}
+
+    void add(string &s) {
+        int node = 0;
+        for (char c : s) {
+            int pos = c - a;
+
+            if (T[node].next[pos] == -1) {
+                T[node].next[pos] = T.size();
+                T.emplace_back();
+            }
+            node = T[node].next[pos];
+        }
+        T[node].count++;
+    }
+
+    auto & operator[](int i) {
+        return T[i];
     }
 };
-
-int dfs(vector<vector<bool>> &visited, vector<string> &grid, int i, int j, vector<TrieNode> &trie, int node) {
-    visited[i][j] = true;
-    int words = trie[node].count;
-    trie[node].count = 0;
-
-    vector<int> dr{1, -1, 0, 0}, dc{0, 0, 1, -1};
-    for (int k = 0; k < 4; k++) {
-        int r = i + dr[k], c = j + dc[k];
-        if (r < 0 || r >= grid.size() || c < 0 || c >= grid[0].size() || visited[r][c]) continue;
-
-        int pos = grid[r][c] - 'A';
-        if (trie[node].next[pos] != -1) words += dfs(visited, grid, r, c, trie, trie[node].next[pos]);
-    }
-    visited[i][j] = false;
-
-    return words;
-}
 
 int main() {
     ios::sync_with_stdio(false);
@@ -41,30 +51,38 @@ int main() {
     int n;
     cin >> n;
 
-    vector<TrieNode> trie(1);
+    Trie trie(1, Trie::UPPER);
     while (n--) {
         string s;
         cin >> s;
 
-        int node = 0;
-        for (char c : s) {
-            int pos = c - 'A';
-
-            if (trie[node].next[pos] == -1) {
-                trie[node].next[pos] = trie.size();
-                trie.emplace_back();
-            }
-            node = trie[node].next[pos];
-        }
-        trie[node].count++;
+        trie.add(s);
     }
 
     int words = 0;
     vector<vector<bool>> visited(h, vector<bool>(w, false));
+    auto dfs = [&](auto &&self, int i, int j, int node) -> int {
+        visited[i][j] = true;
+        int words = trie[node].count;
+        trie[node].count = 0;
+
+        vector<int> dr{1, -1, 0, 0}, dc{0, 0, 1, -1};
+        for (int k = 0; k < 4; k++) {
+            int r = i + dr[k], c = j + dc[k];
+            if (0 <= r && r < grid.size() && 0 <= c && c < grid[0].size() && !visited[r][c]) {
+                int pos = grid[r][c] - 'A';
+                if (trie[node].next[pos] != -1) words += self(self, r, c, trie[node].next[pos]);
+            }
+        }
+        visited[i][j] = false;
+
+        return words;
+    };
+
     for (int i = 0; i < h; i++)
         for (int j = 0; j < w; j++) {
             int pos = grid[i][j] - 'A';
-            if (trie[0].next[pos] != -1) words += dfs(visited, grid, i, j, trie, trie[0].next[pos]);
+            if (trie[0].next[pos] != -1) words += dfs(dfs, i, j, trie[0].next[pos]);
         }
 
     cout << words;
