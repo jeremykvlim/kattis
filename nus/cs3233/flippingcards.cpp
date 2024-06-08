f9219c71edee8f1e7ed557549575b41384ae2b7e
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-06-08 11:47:26 +0800

    Update flippingcards.cpp

--- nus/cs3233/flippingcards.cpp
+++ nus/cs3233/flippingcards.cpp
@@ -1,9 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int find(int p, vector<int> &sets) {
-    return (sets[p] == p) ? p : (sets[p] = find(sets[p], sets));
-}
+struct DisjointSet {
+    vector<int> sets;
+
+    int find(int p) {
+        return (sets[p] == p) ? p : (sets[p] = find(sets[p]));
+    }
+
+    bool unite(int p, int q) {
+        int p_set = find(p), q_set = find(q);
+        if (p_set != q_set) {
+            sets[q_set] = p_set;
+            return true;
+        }
+        return false;
+    }
+
+    DisjointSet(int n) : sets(n) {
+        iota(sets.begin(), sets.end(), 0);
+    }
+};
 
 int main() {
     ios::sync_with_stdio(false);
@@ -16,23 +33,20 @@ int main() {
         int n;
         cin >> n;
 
+        DisjointSet dsu(2 * (n + 1));
         vector<bool> visited(2 * (n + 1), false);
-        vector<int> sets(2 * (n + 1));
-        iota(sets.begin(), sets.end(), 0);
 
         bool possible = true;
         while (n--) {
             int p, q;
             cin >> p >> q;
+
             if (!possible) continue;
 
-            int p_set = find(p, sets), q_set = find(q, sets);
+            int p_set = dsu.find(p), q_set = dsu.find(q);
             if (visited[p_set] && visited[q_set]) possible = false;
 
-            if (p_set != q_set) {
-                sets[q_set] = p_set;
-                visited[p_set] = visited[p_set] | visited[q_set];
-            } else visited[p_set] = true;
+            visited[p_set] = dsu.unite(p, q) ? visited[p_set] | visited[q_set] : true;
         }
 
         cout << (possible ? "possible\n" : "impossible\n");
