d82dc2c6f2b4105ac2ea8306100027e4f3134920
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-06-08 12:25:13 +0800

    Update ispmerger.cpp

--- open/ispmerger.cpp
+++ open/ispmerger.cpp
@@ -1,9 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int find(int p, vector<int> &sets) {
-    return (sets[p] == p) ? p : (sets[p] = find(sets[p], sets));
-}
+struct DisjointSet {
+    vector<int> sets, size;
+
+    int find(int p) {
+        return (sets[p] == p) ? p : (sets[p] = find(sets[p]));
+    }
+
+    bool unite(int p, int q) {
+        int p_set = find(p), q_set = find(q);
+        if (p_set != q_set) {
+            sets[q_set] = p_set;
+            size[p_set] += size[q_set];
+            return true;
+        }
+        return false;
+    }
+
+    DisjointSet(int n) : sets(n), size(n, 1) {
+        iota(sets.begin(), sets.end(), 0);
+    }
+};
 
 int main() {
     ios::sync_with_stdio(false);
@@ -12,7 +30,7 @@ int main() {
     int n, m, k;
     cin >> n >> m >> k;
 
-    vector<int> capacity(n), links(n, 0), sets(n), size(n, 1);
+    vector<int> capacity(n), links(n, 0);
     for (int &c : capacity) cin >> c;
 
     if (accumulate(capacity.begin(), capacity.end(), 0) < 2 * (n - 1)) {
@@ -20,18 +38,15 @@ int main() {
         exit(0);
     }
 
-    iota(sets.begin(), sets.end(), 0);
-
+    DisjointSet dsu(n);
     while (m--) {
         int u, v;
         cin >> u >> v;
 
-        int u_set = find(u, sets), v_set = find(v, sets);
-        if (u_set != v_set) {
-            sets[v_set] = u_set;
+        int u_set = dsu.find(u), v_set = dsu.find(v);
+        if (dsu.unite(u, v)) {
             capacity[u_set] += capacity[v_set];
             links[u_set] += links[v_set];
-            size[u_set] += size[v_set];
         }
 
         capacity[u_set] -= 2;
@@ -40,7 +55,7 @@ int main() {
 
     vector<int> roots;
     for (int i = 0; i < n; i++)
-        if (find(i, sets) == i) roots.emplace_back(i);
+        if (i == dsu.find(i)) roots.emplace_back(i);
 
     if (roots.size() == 1) {
         cout << "yes";
@@ -54,12 +69,12 @@ int main() {
         if (!capacity[s]) {
             k--;
 
-            if (links[s] == size[s] - 1) complete += 2;
+            if (links[s] == dsu.size[s] - 1) complete += 2;
             else isolated++;
 
             degree += 2;
         } else if (capacity[s] == 1) {
-            if (links[s] == size[s] - 1) complete++;
+            if (links[s] == dsu.size[s] - 1) complete++;
             else incomplete++;
         } else isolated++;
     }
