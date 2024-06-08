302bd96f6ceae4631e6e99d165a42825a1ea7ade
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-06-08 16:40:37 +0800

    Update decisionmaking.cpp

--- nus/cs3233/decisionmaking.cpp
+++ nus/cs3233/decisionmaking.cpp
@@ -3,12 +3,60 @@ using namespace std;
 
 constexpr int MODULO = 998244353;
 
-struct TrieNode {
-    vector<int> next;
-    int index = -1, link = -1;
+struct Trie {
+    struct TrieNode {
+        vector<int> next;
+        int index = -1, link = -1;
 
-    TrieNode() {
-        next.resize(2, -1);
+        TrieNode() {
+            next.resize(2, -1);
+        }
+    };
+
+    vector<TrieNode> T;
+
+    Trie(int n = 1) : T(n) {}
+
+    void add(string &s, int i) {
+        int node = 0;
+        for (char c : s) {
+            int pos = c == 'T';
+
+            if (T[node].next[pos] == -1) {
+                T[node].next[pos] = T.size();
+                T.emplace_back();
+            }
+            node = T[node].next[pos];
+        }
+
+        if (T[node].index == -1) T[node].index = i;
+    }
+
+    void build_links() {
+        queue<int> q;
+        q.emplace(0);
+        while (!q.empty()) {
+            int v = q.front();
+            q.pop();
+
+            int p = T[v].link;
+            for (int c = 0; c < 2; c++) {
+                int u = T[v].next[c];
+
+                if (u != -1) {
+                    T[u].link = (p == -1) ? 0 : T[p].next[c];
+                    q.emplace(u);
+                } else T[v].next[c] = (p == -1) ? 0 : T[p].next[c];
+            }
+        }
+    }
+
+    auto size() {
+        return T.size();
+    }
+
+    auto & operator[](int i) {
+        return T[i];
     }
 };
 
@@ -39,13 +87,13 @@ vector<vector<long long>> ref(vector<vector<long long>> &matrix) {
     auto matrix_inv = I(r);
 
     for (int i = 0; i < r; i++) {
-        int pivot = i;
-        while (pivot < r && !matrix[pivot][i]) pivot++;
+        int pivot = find_if(matrix.begin() + i, matrix.end(), [i](auto r) {return r[i];}) - matrix.begin();
 
         if (pivot == r) continue;
 
         swap(matrix[i], matrix[pivot]);
         swap(matrix_inv[i], matrix_inv[pivot]);
+
         auto temp = matrix[i][i];
         for (int j = 0; j < c; j++) {
             matrix[i][j] = mul(matrix[i][j], pow(temp, MODULO - 2, MODULO), MODULO);
@@ -75,47 +123,21 @@ int main() {
     int n;
     cin >> n;
 
-    vector<TrieNode> trie(1);
+    Trie trie;
     for (int i = 0; i < n; i++) {
         string s;
         cin >> s;
 
-        int node = 0;
-        for (char c : s) {
-            int pos = c == 'T';
-
-            if (trie[node].next[pos] == -1) {
-                trie[node].next[pos] = trie.size();
-                trie.emplace_back();
-            }
-            node = trie[node].next[pos];
-        }
-
-        if (trie[node].index == -1) trie[node].index = i;
+        trie.add(s, i);
     }
-
-    queue<int> q;
-    q.emplace(0);
-    while (!q.empty()) {
-        int v = q.front();
-        q.pop();
-
-        int p = trie[v].link;
-        for (int c = 0; c < 2; c++) {
-            int u = trie[v].next[c];
-
-            if (u != -1) {
-                trie[u].link = (p == -1) ? 0 : trie[p].next[c];
-                q.emplace(u);
-            } else trie[v].next[c] = (p == -1) ? 0 : trie[p].next[c];
-        }
-    }
+    trie.build_links();
 
     vector<vector<long long>> a(n, vector<long long>(n, 0)), p(trie.size(), vector<long long>(n));
     p[0][0] = 1;
     vector<bool> visited(trie.size(), false);
     visited[0] = true;
     int count = 1;
+    queue<int> q;
     q.emplace(0);
     while (!q.empty()) {
         int v = q.front();
@@ -124,7 +146,6 @@ int main() {
         if (trie[v].index != -1) a[trie[v].index] = p[v];
         else {
             int w = trie[v].next[0] ^ trie[v].next[1];
-
             for (int u : trie[v].next)
                 if (!visited[u]) {
                     if (!visited[u ^ w]) p[u][count++] = 1;
