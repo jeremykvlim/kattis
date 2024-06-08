c6e98b6955362c87300e016d51a79cb8575385ef
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-06-08 12:19:47 +0800

    Update clumsycardinals.cpp

--- open/clumsycardinals.cpp
+++ open/clumsycardinals.cpp
@@ -1,9 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int find(int p, vector<int> &sets) {
-    return (sets[p] == p) ? p : (sets[p] = find(sets[p], sets));
-}
+struct DisjointSet {
+    vector<int> sets;
+
+    int find(int p) {
+        return (sets[p] == p) ? p : (sets[p] = find(sets[p]));
+    }
+
+    bool unite(int p, int q) {
+        int p_set = find(p), q_set = find(q);
+        if (p_set != q_set) {
+            sets[q_set] = p_set;
+            return true;
+        }
+        return false;
+    }
+
+    DisjointSet(int n) : sets(n) {
+        iota(sets.begin(), sets.end(), 0);
+    }
+};
 
 int main() {
     ios::sync_with_stdio(false);
@@ -12,31 +29,23 @@ int main() {
     int n;
     cin >> n;
 
-    vector<int> sets(n);
-    iota(sets.begin(), sets.end(), 0);
-
-    unordered_map<int, int> nesw, nwse;
-    int C = n;
+    DisjointSet dsu(n);
+    unordered_map<int, int> NE_SW, NW_SE;
+    int count = n;
     for (int i = 0; i < n; i++) {
         int r, c;
         cin >> r >> c;
 
         for (int j = 0; j <= 2; j++) {
-            if (nesw.count(r + c + j - 1)) {
-                int i_set = find(i, sets), j_set = find(nesw[r + c + j - 1], sets);
-                if (i_set != j_set) C--;
-                sets[i_set] = j_set;
-            }
+            if (NE_SW.count(r + c + j - 1))
+                if (dsu.unite(i, NE_SW[r + c + j - 1])) count--;
 
-            if (nwse.count(r - c + j - 1)) {
-                int i_set = find(i, sets), j_set = find(nwse[r - c + j - 1], sets);
-                if (i_set != j_set) C--;
-                sets[i_set] = j_set;
-            }
+            if (NW_SE.count(r - c + j - 1))
+                if (dsu.unite(i, NW_SE[r - c + j - 1])) count--;
         }
 
-        nesw[r + c] = nwse[r - c] = i;
+        NE_SW[r + c] = NW_SE[r - c] = i;
     }
 
-    cout << C;
+    cout << count;
 }
