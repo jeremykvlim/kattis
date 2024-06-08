0d12e91a86df706242cf2dc885c954287321801e
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-06-08 18:34:47 +0800

    Update letsmeet.cpp

--- open/letsmeet.cpp
+++ open/letsmeet.cpp
@@ -2,16 +2,21 @@
 using namespace std;
 
 void rref(int n, vector<vector<double>> &matrix) {
-    for (int i = 0; i < n; i++) {
-        int d = max_element(matrix.begin() + i, matrix.end(), [&](auto r1, auto r2) {return fabs(r1[i]) < fabs(r2[i]);}) - matrix.begin();
-        swap(matrix[i], matrix[d]);
-        
-        for (int j = i + 1; j < n; j++) {
-            for (int k = i + 1; k <= n; k++) matrix[j][k] -= matrix[i][k] * matrix[j][i] / matrix[i][i];
+    int r = matrix.size(), c = matrix[0].size();
+
+    for (int i = 0; i < min(r, c); i++) {
+        int pivot = max_element(matrix.begin() + i, matrix.end(), [&](auto r1, auto r2) {return fabs(r1[i]) < fabs(r2[i]);}) - matrix.begin();
+
+        if (pivot == r) return;
+
+        swap(matrix[i], matrix[pivot]);
+
+        for (int j = i + 1; j < r; j++) {
+            for (int k = i + 1; k < c; k++) matrix[j][k] -= matrix[i][k] * matrix[j][i] / matrix[i][i];
             matrix[j][i] = 0;
         }
     }
-    
+
     for (int i = n - 1; ~i; i--) {
         for (int j = n - 1; j > i; j--) matrix[i][n] -= matrix[i][j] * matrix[j][n];
         matrix[i][n] /= matrix[i][i];
@@ -27,11 +32,10 @@ int main() {
 
     vector<vector<bool>> adj_matrix(n, vector<bool>(n, false)), meet(n, vector<bool>(n, false));
     vector<int> degree(n, 0);
-
     while (m--) {
         int u, v;
         cin >> u >> v;
-        
+
         adj_matrix[u][v] = adj_matrix[v][u] = true;
         degree[u]++;
         degree[v]++;
@@ -49,7 +53,7 @@ int main() {
     while (!q.empty()) {
         auto [u, v] = q.front();
         q.pop();
-        
+
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++)
                 if (adj_matrix[u][i] && adj_matrix[v][j] && !meet[i][j]) {
