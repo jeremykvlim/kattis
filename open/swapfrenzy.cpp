8e37e58b291eff62edd95217a87d9b9e8785321d
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-06-10 23:43:05 +0800

    Update swapfrenzy.cpp

--- open/swapfrenzy.cpp
+++ open/swapfrenzy.cpp
@@ -1,36 +1,6 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-long long largest(long long curr, int pos, int k, bool repeat, vector<int> &digits, vector<vector<unordered_set<long long>>> &visited) {
-    if (pos < 0) {
-        if (!repeat && (k & 1)) curr -= digits[0] + 10 * digits[1] - 10 * digits[0] - digits[1];
-        return curr;
-    }
-
-    if (!k) return curr;
-    if (visited[pos][k - 1].count(curr)) return -1;
-
-    visited[pos][k - 1].emplace(curr);
-    int p = pos;
-    vector<int> indices;
-    for (int i = pos - 1; ~i; i--)
-        if (digits[p] < digits[i]) {
-            p = i;
-            indices = {p};
-        } else if (digits[p] == digits[i]) indices.emplace_back(i);
-
-    auto l = (p == pos) ? largest(curr, pos - 1, k, repeat, digits, visited) : -1LL;
-    for (int i : indices) {
-        auto next = curr - llround(pow(10, pos)) * digits[pos] - llround(pow(10, i)) * digits[i] + llround(pow(10, pos)) * digits[i] + llround(pow(10, i)) * digits[pos];
-        swap(digits[pos], digits[i]);
-
-        l = max(l, largest(next, pos - 1, k - 1, repeat, digits, visited));
-        swap(digits[pos], digits[i]);
-    }
-
-    return l;
-}
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -39,17 +9,45 @@ int main() {
     int k;
     cin >> n >> k;
 
-    int rank = log10(n) + 1;
-    vector<int> digits(rank);
-    vector<vector<unordered_set<long long>>> visited(rank, vector<unordered_set<long long>>(k));
+    vector<int> digits;
     vector<bool> seen(10, false);
-    int i = 0;
     bool repeat = false;
     for (auto temp = n; temp; temp /= 10) {
-        digits[i++] = temp % 10;
+        digits.emplace_back(temp % 10);
         repeat |= seen[temp % 10];
         seen[temp % 10] = true;
     }
+    vector<vector<unordered_set<long long>>> visited(digits.size(), vector<unordered_set<long long>>(k));
 
-    cout << largest(n, rank - 1, k, repeat, digits, visited);
+    auto largest = [&](auto &&self, long long curr, int pos, int k) -> long long {
+        if (pos < 0) {
+            if (!repeat && (k & 1)) curr -= digits[0] + 10 * digits[1] - 10 * digits[0] - digits[1];
+            return curr;
+        }
+
+        if (!k) return curr;
+        if (visited[pos][k - 1].count(curr)) return -1;
+
+        visited[pos][k - 1].emplace(curr);
+        int p = pos;
+        vector<int> indices;
+        for (int i = pos - 1; ~i; i--)
+            if (digits[p] < digits[i]) {
+                p = i;
+                indices = {p};
+            } else if (digits[p] == digits[i]) indices.emplace_back(i);
+
+        auto l = p == pos ? self(self, curr, pos - 1, k) : -1LL;
+        for (int i : indices) {
+            auto next = curr - llround(pow(10, pos)) * digits[pos] - llround(pow(10, i)) * digits[i] + llround(pow(10, pos)) * digits[i] + llround(pow(10, i)) * digits[pos];
+            swap(digits[pos], digits[i]);
+
+            l = max(l, self(self, next, pos - 1, k - 1));
+            swap(digits[pos], digits[i]);
+        }
+
+        return l;
+    };
+
+    cout << largest(largest, n, digits.size() - 1, k);
 }
