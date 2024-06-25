2fc3a9a22f74415826dab6e3ea22dfdaf1bb5465
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-06-26 03:26:50 +0800

    Update battleofpokenom.cpp

--- open/battleofpokenom.cpp
+++ open/battleofpokenom.cpp
@@ -1,10 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool valid(vector<char> v) {
-    return v.empty() || (sort(v.begin(), v.end()), v.front() == v.back());
-}
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -15,27 +11,33 @@ int main() {
     while (t--) {
         int k;
         cin >> k;
-        
+
         vector<char> steps(2 * k);
         for (auto &s : steps) cin >> s;
-        
+
         int s = 0;
-        vector<int> points{0, 0}, remaining{k, k}, prev{-1, -1};
-        for (int i = 0; i < k * 2 && points[0] + remaining[0] >= points[1] && points[1] + remaining[1] >= points[0]; i++, s++) {
-            if (steps[i] == 'E') points[i & 1]++;
-            remaining[i & 1]--;
-            if (points[0] + remaining[0] < points[1] || points[1] + remaining[1] < points[0]) prev[i & 1] = i;
+        vector<int> points{0, 0}, rounds{k, k}, end{-1, -1};
+        for (; s < 2 * k; s++) {
+            if (steps[s] == 'E') points[s & 1]++;
+            rounds[s & 1]--;
+            if (points[0] + rounds[0] < points[1] || points[1] + rounds[1] < points[0]) {
+                end[s & 1] = s;
+                s++;
+                break;
+            }
         }
 
         for (int c = 0; c < s; c++) {
-            vector<char> a, b;
+            char a = '_', b = '_';
             for (int i = c; i < s; i++)
-                if (prev[i & 1] != i) (i & 1 ? a : b).emplace_back(steps[i]);
+                if (end[i & 1] != i) {
+                    if ((i & 1 ? a : b) != '_' && (i & 1 ? a : b) != steps[i]) goto next;
+                    (i & 1 ? a : b) = steps[i];
+                }
 
-            if (valid(a) && valid(b)) {
-                cout << s << ' ' << c << "\n";
-                break;
-            }
+            cout << s << " " << c << "\n";
+            break;
+            next:;
         }
     }
 }
