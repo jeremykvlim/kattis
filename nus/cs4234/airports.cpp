b8f438efb2ee55956c746faf8e11104f624d9755
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-06-30 02:57:37 +0800

    Update airports.cpp

--- nus/cs4234/airports.cpp
+++ nus/cs4234/airports.cpp
@@ -31,7 +31,7 @@ int main() {
         for (int i = 0; i < n; i++) row[i] += inspections[i];
     }
 
-    vector<tuple<int, int, int, int>> flights(m);
+    vector<array<int, 4>> flights(m);
     for (auto &fl : flights) {
         int s, f, t;
         cin >> s >> f >> t;
@@ -41,19 +41,19 @@ int main() {
 
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
-            for (int k = 0; k < n; k++)
-                time[j][k] = min(time[j][k], time[j][i] + time[i][k]);
+            for (int k = 0; k < n; k++) time[j][k] = min(time[j][k], time[j][i] + time[i][k]);
 
     vector<vector<int>> adj_list(m);
     for (int i = 0; i < m; i++)
         for (int j = 0; j < m; j++)
-            if (get<1>(flights[i]) == get<0>(flights[j]) && get<3>(flights[i]) <= get<2>(flights[j]) ||
-                get<3>(flights[i]) + time[get<1>(flights[i]) - 1][get<0>(flights[j]) - 1] <= get<2>(flights[j])) adj_list[i].emplace_back(j);
+            if (flights[i][1] == flights[j][0] && flights[i][3] <= flights[j][2] ||
+                flights[i][3] + time[flights[i][1] - 1][flights[j][0] - 1] <= flights[j][2]) adj_list[i].emplace_back(j);
 
     vector<int> match(m, -1);
     int count = 0;
+    vector<bool> visited(m);
     for (int i = 0; i < m; i++) {
-        vector<bool> visited(m, false);
+        fill(visited.begin(), visited.end(), false);
         if (dfs(i, match, visited, adj_list)) count++;
     }
 
