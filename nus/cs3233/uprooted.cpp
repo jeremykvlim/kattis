76d9eced73f3a933afbe2f7896b25b61aa20ab33
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-06-30 03:01:47 +0800

    Update uprooted.cpp

--- nus/cs3233/uprooted.cpp
+++ nus/cs3233/uprooted.cpp
@@ -8,17 +8,17 @@ int main() {
     int n, m, S;
     cin >> n >> m >> S;
 
-    vector<vector<tuple<int, int, int>>> adj_list(n + 1);
+    vector<vector<array<int, 3>>> adj_list(n + 1);
     for (int i = 1; i <= m; i++) {
         int a, b, c;
         cin >> a >> b >> c;
 
-        adj_list[a].emplace_back(b, c, i);
-        adj_list[b].emplace_back(a, c, i);
+        adj_list[a].push_back({b, c, i});
+        adj_list[b].push_back({a, c, i});
     }
 
     vector<bool> s(n + 1, false);
-    vector<vector<tuple<int, int, int>>> curr(n + 1);
+    vector<vector<array<int, 3>>> curr(n + 1);
     while (S--) {
         s[1] = true;
         for (int _ = 0; _ < n - 1; _++) {
@@ -27,7 +27,7 @@ int main() {
 
             s[p] = true;
             for (auto [q, r, i] : adj_list[p])
-                if (s[q]) curr[p].emplace_back(q, r, i);
+                if (s[q]) curr[p].push_back({q, r, i});
         }
 
         for (int i = 1; i <= n; i++) {
@@ -39,13 +39,12 @@ int main() {
 
     vector<int> branches;
     for (int p = 1; p <= n; p++) {
-        tuple<int, int, int> best{-1, -1, -1};
-        auto &[a, c, i] = best;
+        array<int, 3> best{-1, -1, -1};
 
-        for (auto [q, r, j] : adj_list[p])
-            if (c < r) best = {q, r, j};
+        for (auto [q, r, i] : adj_list[p])
+            if (best[1] < r) best = {q, r, i};
 
-        if (a != -1) branches.emplace_back(i);
+        if (best[0] != -1) branches.emplace_back(best[2]);
     }
 
     if (branches.size() != n - 1) cout << "IMPOSSIBLE";
