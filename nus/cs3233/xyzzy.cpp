d518ddf9b8e17744e4b692e088368c1fb73ac1e0
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-06-30 02:58:29 +0800

    Update xyzzy.cpp

--- nus/cs3233/xyzzy.cpp
+++ nus/cs3233/xyzzy.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool relax(vector<tuple<int, int, int>> &edges, vector<int> &dist, bool detect = false) {
+bool relax(vector<array<int, 3>> &edges, vector<int> &dist, bool detect = false) {
     bool change = false;
     for (auto &[u, v, e] : edges)
         if (-e <= dist[u] && dist[v] < dist[u] + e) {
@@ -18,7 +18,7 @@ int main() {
 
     int n;
     while (cin >> n && n != -1) {
-        vector<tuple<int, int, int>> edges;
+        vector<array<int, 3>> edges;
 
         for (int u = 1; u <= n; u++) {
             int e, doorways;
@@ -28,7 +28,7 @@ int main() {
                 int v;
                 cin >> v;
 
-                edges.emplace_back(u, v, e);
+                edges.push_back({u, v, e});
             }
         }
 
@@ -39,7 +39,7 @@ int main() {
             for (int i = 0; i < n; i++)
                 if (!relax(edges, dist)) break;
         };
-        
+
         bellman_ford();
         relax(edges, dist, true);
         bellman_ford();
