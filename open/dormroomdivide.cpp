bf28ad2890beabf43239c43aa5f51ae251dc7af6
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-06-30 02:41:22 +0800

    Update dormroomdivide.cpp

--- open/dormroomdivide.cpp
+++ open/dormroomdivide.cpp
@@ -2,8 +2,7 @@
 using namespace std;
 
 double area(pair<double, double> p1, pair<double, double> p2, pair<double, double> p3) {
-    return 0.5 * fabs(p1.first * p2.second + p2.first * p3.second + p3.first * p1.second
-                    - p2.first * p1.second - p3.first * p2.second - p1.first * p3.second);
+    return 0.5 * fabs(p1.first * p2.second + p2.first * p3.second + p3.first * p1.second - p2.first * p1.second - p3.first * p2.second - p1.first * p3.second);
 }
 
 int main() {
@@ -22,7 +21,7 @@ int main() {
         sum += area(coords[0], coords[i], coords[i + 1]);
         pref[i] = sum;
     }
-  
+
     int j;
     for (int i = 0; i < n - 1; i++)
         if (sum <= 2 * pref[i]) {
@@ -30,10 +29,10 @@ int main() {
             break;
         }
 
-    auto calc = [&](int i) -> tuple<double, double, double> {
-        return make_tuple(coords[i].first - coords[0].first,
-                          coords[0].second - coords[i].second,
-                          sum - 2 * pref[i - 1] + coords[i].first * coords[0].second - coords[0].first * coords[i].second);
+    auto calc = [&](int i) -> array<double, 3> {
+        return {coords[i].first - coords[0].first,
+                coords[0].second - coords[i].second,
+                sum - 2 * pref[i - 1] + coords[i].first * coords[0].second - coords[0].first * coords[i].second};
     };
 
     auto [x1, y1, a1] = calc(j);
