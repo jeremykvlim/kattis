670bfb72ec62e803a03fe1b42737056d9feba90a
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-06-30 14:55:03 +0800

    Update collidingtraffic.cpp

--- open/collidingtraffic.cpp
+++ open/collidingtraffic.cpp
@@ -1,8 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-constexpr double EPSILON = 1e-6;
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -12,41 +10,41 @@ int main() {
 
     while (c--) {
         int n;
-        long double r;
+        double r;
         cin >> n >> r;
 
-        vector<pair<long double, long double>> position(n), velocity(n);
+        vector<pair<double, double>> position(n), velocity(n);
         for (int i = 0; i < n; i++) {
-            long double d, s;
+            double d, s;
             cin >> position[i].first >> position[i].second >> d >> s;
 
             velocity[i] = {s * cos((90 - d) / 180 * M_PI), s * sin((90 - d) / 180 * M_PI)};
         }
 
-        auto time = LDBL_MAX;
+        auto time = DBL_MAX;
         for (int i = 0; i < n; i++)
             for (int j = 0; j < i; j++) {
-                long double t;
                 auto boat1 = make_pair(position[i].first - position[j].first, position[i].second - position[j].second),
-                         v = make_pair(velocity[i].first - velocity[j].first, velocity[i].second - velocity[j].second);
+                        v = make_pair(velocity[i].first - velocity[j].first, velocity[i].second - velocity[j].second);
 
-                if (hypotl(boat1.first, boat1.second) < r + EPSILON) t = 0;
-                else if (hypotl(v.first, v.second) < EPSILON) continue;
+                double t;
+                if (hypot(boat1.first, boat1.second) < r + 1e-6) t = 0;
+                else if (hypot(v.first, v.second) < 1e-6) continue;
                 else {
                     t = (boat1.first * -v.first + boat1.second * -v.second) / (v.first * v.first + v.second * v.second);
-                    if (t + EPSILON < 0) continue;
+                    if (t + 1e-6 < 0) continue;
                     else {
                         auto boat2 = make_pair(boat1.first + v.first * t, boat1.second + v.second * t);
 
-                        if (hypotl(boat2.first, boat2.second) > r) continue;
-                        else t -= sqrt(r * r - (boat2.first * boat2.first + boat2.second * boat2.second)) / hypotl(v.first, v.second);
+                        if (hypot(boat2.first, boat2.second) > r) continue;
+                        else t -= sqrt(r * r - (boat2.first * boat2.first + boat2.second * boat2.second)) / hypot(v.first, v.second);
                     }
                 }
 
                 time = min(time, t);
             }
 
-        if (time == LDBL_MAX) cout << "No collision.\n";
+        if (time == DBL_MAX) cout << "No collision.\n";
         else cout << fixed << setprecision(0) << time << "\n";
     }
 }
