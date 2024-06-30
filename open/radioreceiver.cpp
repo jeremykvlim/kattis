9ebd99c5c2a39a99a264660b2c65d531d36e6c34
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-06-30 19:25:06 +0800

    Update radioreceiver.cpp

--- open/radioreceiver.cpp
+++ open/radioreceiver.cpp
@@ -20,27 +20,27 @@ int main() {
             bound = max({bound, p, t});
         }
         sort(messages.begin(), messages.end(), [](auto p1, auto p2) {return p1.second < p2.second;});
-
-        auto possible = [&](double m) -> bool {
-            double l1 = messages[0].first - m, r1 = messages[0].first + m;
-            for (int i = 1; i < n; i++) {
-                int t = messages[i].second - messages[i - 1].second;
-                double l2 = messages[i].first - m, r2 = messages[i].first + m;
-
-                if (l1 - t > r2 || r1 + t < l2) return false;
-
-                l1 = max(l1 - t, l2);
-                r1 = min(r1 + t, r2);
-            }
-
-            return true;
-        };
-
+        
         double l = 0, r = bound, m;
-        while (l + 1e-9 < r && r / l > 1 + 1e-9) {
+        while (l + 1e-9 < r && l + l * 1e-9 < r) {
             m = l + (r - l) / 2;
 
-            if (possible(m)) r = m;
+            auto possible = [&]() -> bool {
+                double l1 = messages[0].first - m, r1 = messages[0].first + m;
+                for (int i = 1; i < n; i++) {
+                    int t = messages[i].second - messages[i - 1].second;
+                    double l2 = messages[i].first - m, r2 = messages[i].first + m;
+
+                    if (l1 - t > r2 || r1 + t < l2) return false;
+
+                    l1 = max(l1 - t, l2);
+                    r1 = min(r1 + t, r2);
+                }
+
+                return true;
+            };
+            
+            if (possible()) r = m;
             else l = m;
         }
 
