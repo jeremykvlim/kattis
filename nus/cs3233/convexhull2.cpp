684e134cd59e4cdbcaf008352ae58e367eb935bd
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-07-02 03:04:42 +0800

    Update convexhull2.cpp

--- nus/cs3233/convexhull2.cpp
+++ nus/cs3233/convexhull2.cpp
@@ -1,26 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-double cross(pair<long long, long long> i, pair<long long, long long> j, pair<long long, long long> k) {
-    return (double) (k.first - i.first) * (double) (j.second - i.second) - (double) (k.second - i.second) * (double) (j.first - i.first);
+template <typename T>
+struct Point {
+    T x, y;
+
+    Point() {}
+    Point(T x, T y) : x(x), y(y) {}
+
+    auto operator<(Point<T> &p) const {
+        return x != p.x ? x < p.x : y < p.y;
+    }
+
+    auto operator==(Point<T> &p) const {
+        return x == p.x && y == p.y;
+    }
+};
+
+template <typename T>
+double cross(Point<T> a, Point<T> b, Point<T> c) {
+    return (double) (c.x - a.x) * (double) (b.y - a.y) - (double) (c.y - a.y) * (double) (b.x - a.x);
 }
 
-void monotone(vector<pair<long long, long long>> &points, deque<pair<long long, long long>> &convex) {
+template <typename T>
+deque<Point<T>> monotone(vector<Point<T>> &points) {
     sort(points.begin(), points.end());
     points.erase(unique(points.begin(), points.end()), points.end());
 
+    if (points.size() < 3) return deque<Point<T>>(points.begin(), points.end());
+        
+    deque<Point<T>> convex_hull;
     for (auto p : points) {
-        while (convex.size() > 1 && cross(convex[1], convex[0], p) > 0) convex.pop_front();
-        convex.emplace_front(p);
+        while (convex_hull.size() > 1 && cross(convex_hull[1], convex_hull[0], p) > 0) convex_hull.pop_front();
+        convex_hull.emplace_front(p);
     }
 
-    int s = convex.size();
+    int s = convex_hull.size();
     points.pop_back();
     reverse(points.begin(), points.end());
     for (auto p : points) {
-        while (convex.size() > s && cross(convex[1], convex[0], p) > 0) convex.pop_front();
-        convex.emplace_front(p);
+        while (convex_hull.size() > s && cross(convex_hull[1], convex_hull[0], p) > 0) convex_hull.pop_front();
+        convex_hull.emplace_front(p);
     }
+
+    convex_hull.pop_front();
+    return convex_hull;
 }
 
 int main() {
@@ -30,20 +54,17 @@ int main() {
     int n;
     cin >> n;
 
-    vector<pair<long long, long long>> points;
+    vector<Point<int>> points;
     while (n--) {
-        long long x, y;
+        int x, y;
         char c;
         cin >> x >> y >> c;
 
         if (c == 'Y') points.emplace_back(x, y);
     }
 
-    deque<pair<long long, long long>> convex;
-    monotone(points, convex);
-    
-    convex.pop_front();
-    reverse(convex.begin(), convex.end());
-    cout << convex.size() << "\n";
-    for (auto [x, y] : convex) cout << x << " " << y << "\n";
+    auto convex_hull = monotone(points);
+    reverse(convex_hull.begin(), convex_hull.end());
+    cout << convex_hull.size() << "\n";
+    for (auto [x, y] : convex_hull) cout << x << " " << y << "\n";
 }
