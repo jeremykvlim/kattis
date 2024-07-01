dc68567d85192b3618ec3f9d7f1f35a228f8901d
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-07-02 03:12:00 +0800

    Update hardarray.cpp

--- nus/cs3233/hardarray.cpp
+++ nus/cs3233/hardarray.cpp
@@ -1,6 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+template <typename T>
+struct Point {
+    T x, y;
+
+    Point() {}
+    Point(T x, T y) : x(x), y(y) {}
+
+    auto operator<(Point<T> &p) const {
+        return x != p.x ? x < p.x : y < p.y;
+    }
+};
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -8,17 +20,17 @@ int main() {
     int n;
     cin >> n;
 
-    vector<pair<long long, long long>> points(n + 1, {0, 0});
+    vector<Point<long long>> points(n + 1);
     for (int i = 0; i < n; i++) {
-        cin >> points[i].first;
+        cin >> points[i].x;
 
-        if (i) points[i].first += points[i - 1].first;
+        if (i) points[i].x += points[i - 1].x;
     }
 
     for (int i = 0; i < n; i++) {
-        cin >> points[i].second;
+        cin >> points[i].y;
 
-        if (i) points[i].second += points[i - 1].second;
+        if (i) points[i].y += points[i - 1].y;
     }
     sort(points.begin(), points.end());
 
@@ -26,21 +38,21 @@ int main() {
 
     __int128 d = 1e27;
     auto update = [&](auto p1, auto p2) {
-        d = min(d, sq(p1.first - p2.first) + sq(p1.second - p2.second));
+        d = min(d, sq(p1.x - p2.x) + sq(p1.y - p2.y));
     };
 
-    auto cmp = [](auto p1, auto p2) {return p1.second < p2.second;};
-    multiset<pair<long long, long long>, decltype(cmp)> ms(cmp);
+    auto cmp = [](auto p1, auto p2) {return p1.y < p2.y;};
+    multiset<Point<long long>, decltype(cmp)> ms(cmp);
     vector<decltype(ms)::const_iterator> its(n + 1);
     for (int i = 0, j = 0; i <= n; i++) {
-        for (; j < i && sq(points[j].first - points[i].first) >= d; j++) ms.erase(its[j]);
+        for (; j < i && sq(points[j].x - points[i].x) >= d; j++) ms.erase(its[j]);
 
         auto it = ms.upper_bound(points[i]);
         if (it != ms.begin()) {
             update(*prev(it), points[i]);
-            for (auto p = prev(it); p != ms.begin() && sq(points[i].second - p->second) < d;) update(*--p, points[i]);
+            for (auto p = prev(it); p != ms.begin() && sq(points[i].y - p->y) < d;) update(*--p, points[i]);
         }
-        for (; it != ms.end() && sq(it->second - points[i].second) < d; it++) update(*it, points[i]);
+        for (; it != ms.end() && sq(it->y - points[i].y) < d; it++) update(*it, points[i]);
         its[i] = ms.emplace_hint(ms.upper_bound(points[i]), points[i]);
     }
 
