68f8342a548b352ac54f50447d0316e7203cb64d
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-07-02 01:29:43 +0800

    Update drivesafely.cpp

--- open/drivesafely.cpp
+++ open/drivesafely.cpp
@@ -1,6 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+template <typename T>
+struct Point {
+    T x, y;
+
+    Point() {}
+    Point(T x, T y) : x(x), y(y) {}
+};
+
+template <typename T>
+double dist(Point<T> a, Point<T> b) {
+    return sqrt((double) (a.x - b.x) * (a.x - b.x) + (double) (a.y - b.y) * (a.y - b.y));
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -8,16 +21,18 @@ int main() {
     int n, k;
     cin >> n >> k;
 
-    vector<double> x(n), y(n);
-    for (int i = 0; i < n; i++) cin >> x[i] >> y[i];
+    vector<Point<double>> points(n);
+    for (auto &[x, y] : points) cin >> x >> y;
 
     vector<double> len(n - 1, 0), angle(n, 0);
     angle[0] = 180;
     for (int i = 0; i < n - 1; i++) {
-        len[i] = hypot(x[i + 1] - x[i], y[i + 1] - y[i]);
+        len[i] = dist(points[i], points[i + 1]);
 
         if (i < n - 2) {
-            auto x1 = x[i + 1] - x[i], y1 = y[i + 1] - y[i], x2 = x[i + 2] - x[i + 1], y2 = y[i + 2] - y[i + 1];
+            auto x1 = points[i + 1].x - points[i].x, y1 = points[i + 1].y - points[i].y,
+                 x2 = points[i + 2].x - points[i + 1].x, y2 = points[i + 2].y - points[i + 1].y;
+
             angle[i + 1] = 180 - abs(atan2(x1 * y2 - x2 * y1, x1 * x2 + y1 * y2)) / M_PI * 180;
         }
     }
@@ -47,6 +62,5 @@ int main() {
 
     auto time = DBL_MAX;
     for (int i = 0; i <= k; i++) time = min(time, dp[n - 2][i][1]);
-
     cout << fixed << setprecision(6) << time;
 }
