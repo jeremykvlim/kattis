6755734be51b24a0e73967c979245f925a248988
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-07-01 22:24:21 +0800

    Update closestpair2.cpp

--- open/closestpair2.cpp
+++ open/closestpair2.cpp
@@ -8,41 +8,42 @@ int main() {
     int n;
     while (cin >> n && n) {
         vector<pair<double, double>> points(n);
-        vector<tuple<double, double, int>> sorted(n);
+        vector<pair<pair<double, double>, int>> sorted(n);
         for (int i = 0; i < n; i++) {
             cin >> points[i].first >> points[i].second;
 
-            sorted[i] = {points[i].first, points[i].second, i};
+            sorted[i] = {points[i], i};
         }
         sort(sorted.begin(), sorted.end());
 
         auto sq = [](double v) -> double {return v * v;};
 
         auto d = DBL_MAX;
-        int p1 = -1, p2 = -1;
-        auto update = [&](auto t1, auto t2) {
-            auto dist = sq(get<0>(t1) - get<0>(t2)) + sq(get<1>(t1) - get<1>(t2));
+        int a = -1, b = -1;
+        auto update = [&](auto p1, auto p2) {
+            auto dist = sq(p1.first.first - p2.first.first) + sq(p1.first.second - p2.first.second);
             if (d > dist) {
                 d = dist;
-                p1 = get<2>(t1);
-                p2 = get<2>(t2); }
+                a = p1.second;
+                b = p2.second; 
+            }
         };
 
-        auto cmp = [](auto t1, auto t2) {return get<1>(t1) < get<1>(t2);};
-        multiset<tuple<double, double, int>, decltype(cmp)> ms(cmp);
+        auto cmp = [](auto p1, auto p2) {return p1.first.second < p2.first.second;};
+        multiset<pair<pair<double, double>, int>, decltype(cmp)> ms(cmp);
         vector<decltype(ms)::const_iterator> its(n);
         for (int i = 0, j = 0; i < n; i++) {
-            for (; j < i && sq(get<0>(sorted[j]) - get<0>(sorted[i])) >= d; j++) ms.erase(its[j]);
+            for (; j < i && sq(sorted[j].first.first - sorted[i].first.first) >= d; j++) ms.erase(its[j]);
 
             auto it = ms.upper_bound(sorted[i]);
             if (it != ms.begin()) {
                 update(*prev(it), sorted[i]);
-                for (auto p = prev(it); p != ms.begin() && sq(get<1>(sorted[i]) - get<1>(*p)) < d;) update(*--p, sorted[i]);
+                for (auto p = prev(it); p != ms.begin() && sq(sorted[i].first.second - p->first.second) < d;) update(*--p, sorted[i]);
             }
-            for (; it != ms.end() && sq(get<1>(*it) - get<1>(sorted[i])) < d; it++) update(*it, sorted[i]);
+            for (; it != ms.end() && sq(it->first.second - sorted[i].first.second) < d; it++) update(*it, sorted[i]);
             its[i] = ms.emplace_hint(ms.upper_bound(sorted[i]), sorted[i]);
         }
 
-        cout << fixed << setprecision(2) << points[p1].first << " " << points[p1].second << " " << points[p2].first << " " << points[p2].second << "\n";
+        cout << fixed << setprecision(2) << points[a].first << " " << points[a].second << " " << points[b].first << " " << points[b].second << "\n";
     }
 }
