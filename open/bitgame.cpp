a2e1157cf0354ac1d8b6a939252c2e166c6796c4
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-07-04 02:05:23 +0800

    Update bitgame.cpp

--- open/bitgame.cpp
+++ open/bitgame.cpp
@@ -1,34 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool dfs(string &paper) {
-    if (paper.size() == 20) {
-        bool num;
-        cout << "? " << stoi(paper, nullptr, 2) << "\n" << flush;
-        cin >> num;
-
-        return num;
-    }
-
-    string bits{"01"};
-    if (rand() & 1) swap(bits[0], bits[1]);
-
-    for (char b : bits) {
-        paper += b;
-        bool win = !dfs(paper);
-        paper.pop_back();
-
-        if (win) return true;
-    }
-
-    return false;
-}
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    srand(time(nullptr));
-    string s;
-    cout << (dfs(s) ? "! alice" : "! bob");
+    mt19937 rng(random_device{}());
+    string paper;
+    auto dfs = [&](auto &&self) {
+        if (paper.size() == 20) {
+            bool num;
+            cout << "? " << stoi(paper, nullptr, 2) << "\n" << flush;
+            cin >> num;
+
+            return num;
+        }
+
+        string bits{"01"};
+        if (rng() & 1) swap(bits[0], bits[1]);
+
+        for (char b : bits) {
+            paper += b;
+            bool win = !self(self);
+            paper.pop_back();
+
+            if (win) return true;
+        }
+
+        return false;
+    };
+
+    cout << (dfs(dfs) ? "! alice" : "! bob");
 }
