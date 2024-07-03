fa8f1fdf1d6b6b92dbfd2ae307c0a6ce3b2cac39
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-07-04 03:21:52 +0800

    Update aijeopardy.py

--- open/aijeopardy.py
+++ open/aijeopardy.py
@@ -1,22 +1,39 @@
-x = int(input())
-(n, k) = (x, 1) if x > 1 else (0, 0)
+import math
 
-for i in range(2, min(x + 1, 200)):
-    l, r = i, x
-    while l + 1 < r:
-        m = l + (r - l) // 2
-        c = 1
-        for j in range(i):
-            c *= m - j
-            c //= j + 1
-            if c > x:
-                r = m
-                break
-        
-        if c == x:
-            (n, k) = min((n, k), (m, i))
-            break
-        elif c < x:
-            l = m
+def C(n, k):
+    if k == 0: return 1
+    return C(n - 1, k - 1) * n // k
+
+
+def search(x):
+    k = 1
+    while (2 * k + 1) * x > 4**k:
+        n, c = 1, math.factorial(k) * x
+        while n**k < c: n <<= 1
+
+        l, r = 1, n
+        while l + 1 < r:
+            m = l + (r - l) // 2
+            if m**k < c: l = m
+            else: r = m
+
+        l = max(r, 2 * k)
+        c = C(l, k)
+        for n in range(l, r + k + 1):
+            if c == x: yield n, min(k, n - k)
+            c *= n + 1
+            c //= n + 1 - k
+
+        k += 1
+
+
+x = int(input())
+if x == 1:
+    print(0, 0)
+    exit(0)
+
+n, k = x, 1
+for new_n, new_k in search(x):
+    if new_n < n: n, k = new_n, new_k
 
 print(n, k)
