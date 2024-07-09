fcf208a901960cb86973ce205cc9bab38755d426
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-07-09 22:35:09 +0800

    Update wipeyourwhiteboards.cpp

--- nus/cs3233/wipeyourwhiteboards.cpp
+++ nus/cs3233/wipeyourwhiteboards.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-pair<long long, long long> bezout(long long a, long long b) {
+template <typename T>
+pair<T, T> bezout(T a, T b) {
     if (!a) return {0, 1};
     auto [x, y] = bezout(b % a, a);
     return {y - (b / a) * x, x};
@@ -13,17 +14,20 @@ int main() {
 
     int t;
     cin >> t;
+    
     while (t--) {
         long long R, S, Q;
         cin >> R >> S >> Q;
-        
+
         auto [A, B] = bezout(R, -S);
         long long l = INT_MIN, r = INT_MAX, m;
         while (l + 1 < r) {
             m = l + (r - l) / 2;
+
             if (Q / __gcd(R, -S) * A + m * S / __gcd(R, -S) > 0 && Q / __gcd(R, -S) * -B - m * R / __gcd(R, -S) > 0) l = m;
             else r = m;
         }
+        
         cout << Q / __gcd(R, -S) * A + l * S / __gcd(R, -S) << " " << Q / __gcd(R, -S) * -B - l * R / __gcd(R, -S) << "\n";
     }
 }
