aaa159bed21f46be4c45c821ec11e5e7fcb55245
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-07-10 15:24:41 +0800

    Update knapsack101.cpp

--- nus/cs3233/knapsack101.cpp
+++ nus/cs3233/knapsack101.cpp
@@ -1,55 +1,299 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long mul(long long x, long long y, long long mod) {
-    auto product = x * y - mod * (long long) (1.L / mod * x * y);
-    return product + mod * (product < 0) - mod * (product >= mod);
-}
-
-long long pow(long long base, long long exponent, long long mod) {
-    auto value = 1LL;
-    for (; exponent; exponent >>= 1) {
-        if (exponent & 1) value = mul(value, base, mod);
-        base = mul(base, base, mod);
+template<typename T>
+T inverse(T a, T mod) {
+    T u = 0, v = 1;
+    while (a) {
+        T t = mod / a;
+        mod -= t * a;
+        swap(a, mod);
+        u -= t * v;
+        swap(u, v);
     }
 
+    return u;
+}
+
+template <typename T>
+T mul(T x, T y, T mod) {
+    long long px = x, py = y, pmod = mod;
+    auto product = px * py - pmod * (long long) (1.L / pmod * px * py);
+    return product + pmod * (product < 0) - pmod * (product >= pmod);
+}
+
+template <typename T>
+T pow(T base, T exponent, T mod) {
+    T value = 1;
+    while (exponent) {
+        if (exponent & 1) value = mul(value, base, mod);
+        base = mul(base, base, mod);
+        exponent >>= 1;
+    }
     return value;
 }
 
-void dfs(vector<int> &w, int p, int i, int j, int depth, int d, vector<int> &indices, unordered_map<long long, vector<int>> &half, long long z = 1) {
-    if (half.size() >= 50000) return;
+bool isprime(long long n) {
+    if (n < 2) return false;
+    if (n % 6 % 4 != 1) return (n | 1) == 3;
 
-    if (depth == d) {
-        half[z] = indices;
-        return;
+    vector<int> bases{2, 325, 9375, 28178, 450775, 9780504, 1795265022};
+    int s = __builtin_ctzll(n - 1);
+    auto d = n >> s;
+    for (int a : bases) {
+        auto p = pow(a % n, d, n);
+        int i = s;
+        while (1 < p && p < n - 1 && a % n && i--) p = mul(p, p, n);
+        if (p != n - 1 && i != s) return false;
     }
 
-    if (i > j) return;
-
-    dfs(w, p, i + 1, j, depth, d, indices, half, z);
-    indices.emplace_back(i);
-
-    auto zi = mul(z, w[i - 1], p);
-    dfs(w, p, i + 1, j, depth + 1, d, indices, half, zi);
-    indices.pop_back();
+    return true;
 }
+
+template <typename T>
+struct MODULO {
+    static T value;
+};
+
+template <typename T>
+T MODULO<T>::value;
+
+auto &m = MODULO<int>::value;
+bool PRIME_MOD;
+
+template<typename M>
+struct ModInt {
+    using T = typename decay<decltype(M::value)>::type;
+    T value;
+
+    constexpr ModInt() : value() {}
+
+    template<typename U>
+    ModInt(const U &x) {
+        value = normalize(x);
+    }
+
+    template<typename U>
+    static T normalize(const U &x) {
+        T v = x;
+        if (!(-mod() <= x && x < mod())) v = (T) (x % mod());
+        if (v < 0) v += mod();
+        return v;
+    }
+
+    const T & operator()() const {
+        return value;
+    }
+
+    template<typename U>
+    explicit operator U() const {
+        return (U) value;
+    }
+
+    constexpr static T mod() {
+        return M::value;
+    }
+
+    inline auto & operator+=(const ModInt &v) {
+        if ((value += v.value) >= mod()) value -= mod();
+        return *this;
+    }
+
+    inline auto & operator-=(const ModInt &v) {
+        if ((value -= v.value) < 0) value += mod();
+        return *this;
+    }
+
+    template<typename U>
+    inline auto & operator+=(const U &v) {
+        return *this += ModInt(v);
+    }
+
+    template<typename U>
+    inline auto & operator-=(const U &v) {
+        return *this -= ModInt(v);
+    }
+
+    auto & operator++() {
+        return *this += 1;
+    }
+
+    auto & operator--() {
+        return *this -= 1;
+    }
+
+    auto operator++(int) {
+        ModInt v(*this);
+        *this += 1;
+        return v;
+    }
+
+    auto operator--(int) {
+        ModInt v(*this);
+        *this -= 1;
+        return v;
+    }
+
+    auto operator-() const {
+        return ModInt(-value);
+    }
+
+    template<typename U = M>
+    typename enable_if<is_same<typename ModInt<U>::T, int>::value, ModInt>::type &operator*=(const ModInt &v) {
+        value = normalize((long long) value * (long long) v.value);
+        return *this;
+    }
+
+    template<typename U = M>
+    typename enable_if<is_same<typename ModInt<U>::T, long long>::value, ModInt>::type &operator*=(const ModInt &v) {
+        value = normalize(value * v.value - (long long) ((long double) value * v.value / mod()) * mod());
+        return *this;
+    }
+
+    template<typename U = M>
+    typename enable_if<!is_integral<typename ModInt<U>::T>::value, ModInt>::type &operator*=(const ModInt &v) {
+        value = normalize(value * v.value);
+        return *this;
+    }
+
+    auto & operator/=(const ModInt &v) {
+        if (PRIME_MOD) return *this *= ModInt(pow(v.value, mod() - 2, mod()));
+        return *this *= ModInt(inverse(v.value, mod()));
+    }
+};
+
+template<typename T>
+bool operator==(const ModInt<T> &lhs, const ModInt<T> &rhs) {
+    return lhs.value == rhs.value;
+}
+
+template<typename T, typename U>
+bool operator==(const ModInt<T> &lhs, U rhs) {
+    return lhs == ModInt<T>(rhs);
+}
+
+template<typename T, typename U>
+bool operator==(U lhs, const ModInt<T> &rhs) {
+    return ModInt<T>(lhs) == rhs;
+}
+
+template<typename T>
+bool operator!=(const ModInt<T> &lhs, const ModInt<T> &rhs) {
+    return !(lhs == rhs);
+}
+
+template<typename T, typename U>
+bool operator!=(const ModInt<T> &lhs, U rhs) {
+    return !(lhs == rhs);
+}
+
+template<typename T, typename U>
+bool operator!=(U lhs, const ModInt<T> &rhs) {
+    return !(lhs == rhs);
+}
+
+template<typename T>
+bool operator>(const ModInt<T> &lhs, const ModInt<T> &rhs) {
+    return lhs.value > rhs.value;
+}
+
+template<typename T>
+bool operator<(const ModInt<T> &lhs, const ModInt<T> &rhs) {
+    return lhs.value < rhs.value;
+}
+
+template<typename T>
+ModInt<T> operator+(const ModInt<T> &lhs, const ModInt<T> &rhs) {
+    return ModInt<T>(lhs) += rhs;
+}
+
+template<typename T, typename U>
+ModInt<T> operator+(const ModInt<T> &lhs, U rhs) {
+    return ModInt<T>(lhs) += rhs;
+}
+
+template<typename T, typename U>
+ModInt<T> operator+(U lhs, const ModInt<T> &rhs) {
+    return ModInt<T>(lhs) += rhs;
+}
+
+template<typename T>
+ModInt<T> operator-(const ModInt<T> &lhs, const ModInt<T> &rhs) {
+    return ModInt<T>(lhs) -= rhs;
+}
+
+template<typename T, typename U>
+ModInt<T> operator-(const ModInt<T> &lhs, U rhs) {
+    return ModInt<T>(lhs) -= rhs;
+}
+
+template<typename T, typename U>
+ModInt<T> operator-(U lhs, const ModInt<T> &rhs) {
+    return ModInt<T>(lhs) -= rhs;
+}
+
+template<typename T>
+ModInt<T> operator*(const ModInt<T> &lhs, const ModInt<T> &rhs) {
+    return ModInt<T>(lhs) *= rhs;
+}
+
+template<typename T, typename U>
+ModInt<T> operator*(const ModInt<T> &lhs, U rhs) {
+    return ModInt<T>(lhs) *= rhs;
+}
+
+template<typename T, typename U>
+ModInt<T> operator*(U lhs, const ModInt<T> &rhs) {
+    return ModInt<T>(lhs) *= rhs;
+}
+
+template<typename T>
+ModInt<T> operator/(const ModInt<T> &lhs, const ModInt<T> &rhs) {
+    return ModInt<T>(lhs) /= rhs;
+}
+
+template<typename T, typename U>
+ModInt<T> operator/(const ModInt<T> &lhs, U rhs) {
+    return ModInt<T>(lhs) /= rhs;
+}
+
+template<typename T, typename U>
+ModInt<T> operator/(U lhs, const ModInt<T> &rhs) {
+    return ModInt<T>(lhs) /= rhs;
+}
+
+template <typename U, typename T>
+U & operator<<(U &stream, const ModInt<T> &v) {
+    return stream << v();
+}
+
+template <typename U, typename T>
+U & operator>>(U &stream, ModInt<T> &v) {
+    typename common_type<typename ModInt<T>::T, long long>::type x;
+    stream >> x;
+    v.value = ModInt<T>::normalize(x);
+    return stream;
+}
+
+using modint = ModInt<MODULO<int>>;
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n, p;
-    cin >> n >> p;
+    int n;
+    cin >> n >> m;
+    PRIME_MOD = isprime(m);
 
     vector<int> w(2 * n);
     for (int &wi : w) cin >> wi;
 
-    long long z;
+    modint z;
     cin >> z;
 
-    auto pw = 1LL;
-    for (int i = 0; i < n; i++) pw = mul(pw, w[i], p);
-    z = mul(z, pow(pw, p - 2, p), p);
+    modint pw = 1;
+    for (int i = 0; i < n; i++) pw *= w[i];
+    z /= pw;
 
     if (z == 1) {
         for (int i = 0; i < n; i++) cout << 1;
@@ -58,20 +302,37 @@ int main() {
     }
 
     string s(2 * n, '0');
-    for (int i = 1; i <= n; i++) {
+    for (int d = 1; d <= n; d++) {
         vector<int> l, r;
         unordered_map<long long, vector<int>> left, right;
 
-        dfs(w, p, 1, n, 0, i, l, left);
-        dfs(w, p, n + 1, 2 * n, 0, i, r, right);
+        auto dfs = [&](auto &&self, int i, int j, vector<int> &indices, unordered_map<long long, vector<int>> &half, int depth = 0, modint z = 1) {
+            if (half.size() >= 5e4) return;
+
+            if (depth == d) {
+                half[z.value] = indices;
+                return;
+            }
+
+            if (i > j) return;
+
+            self(self, i + 1, j, indices, half, depth, z);
+            indices.emplace_back(i);
+
+            self(self, i + 1, j, indices, half, depth + 1, z * w[i - 1]);
+            indices.pop_back();
+        };
+
+        dfs(dfs, 1, n, l, left);
+        dfs(dfs, n + 1, 2 * n, r, right);
 
         for (auto [zl, indices] : left) {
-            auto zr = mul(z, zl, p);
+            auto zr = z * zl;
 
-            if (right.count(zr)) {
+            if (right.count(zr.value)) {
                 for (int j = 0; j < n; j++) s[j] = '1';
                 for (int k : indices) s[k - 1] = '0';
-                for (int k : right[zr]) s[k - 1] = '1';
+                for (int k : right[zr.value]) s[k - 1] = '1';
 
                 cout << s;
                 exit(0);
