191388af4bfc736fca58e1b024bc35423136c19a
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-07-10 15:36:57 +0800

    Update widgettree.cpp

--- nus/cs2040c/widgettree.cpp
+++ nus/cs2040c/widgettree.cpp
@@ -1,53 +1,289 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long maintain(int v, int mod, vector<unordered_map<int, int>> &build, vector<long long> &m) {
-    if (m[v] != -1) return m[v];
-
-    auto c = 1LL;
-    for (auto [u, count] : build[v]) c += (maintain(u, mod, build, m) * count) % mod;
-
-    return m[v] = c % mod;
-}
-
-long long rebuild(int v, int mod, unordered_set<int> &y, vector<unordered_map<int, int>> &build, vector<long long> &m, vector<long long> &r, vector<bool> &extra) {
-    if (r[v] != -1) return r[v];
-
-    if (y.count(v)) {
-        extra[v] = true;
-        return maintain(v, mod, build, m) % mod;
+template<typename T>
+T inverse(T a, T mod) {
+    T u = 0, v = 1;
+    while (a) {
+        T t = mod / a;
+        mod -= t * a;
+        swap(a, mod);
+        u -= t * v;
+        swap(u, v);
     }
 
-    auto c = 0LL;
-    bool e = false;
-    for (auto [u, count] : build[v]) {
-        c += (rebuild(u, mod, y, build, m, r, extra) * count) % mod;
-        e |= extra[u];
+    return u;
+}
+
+template <typename T>
+T mul(T x, T y, T mod) {
+    long long px = x, py = y, pmod = mod;
+    auto product = px * py - pmod * (long long) (1.L / pmod * px * py);
+    return product + pmod * (product < 0) - pmod * (product >= pmod);
+}
+
+template <typename T>
+T pow(T base, T exponent, T mod) {
+    T value = 1;
+    while (exponent) {
+        if (exponent & 1) value = mul(value, base, mod);
+        base = mul(base, base, mod);
+        exponent >>= 1;
+    }
+    return value;
+}
+
+bool isprime(long long n) {
+    if (n < 2) return false;
+    if (n % 6 % 4 != 1) return (n | 1) == 3;
+
+    vector<int> bases{2, 325, 9375, 28178, 450775, 9780504, 1795265022};
+    int s = __builtin_ctzll(n - 1);
+    auto d = n >> s;
+    for (int a : bases) {
+        auto p = pow(a % n, d, n);
+        int i = s;
+        while (1 < p && p < n - 1 && a % n && i--) p = mul(p, p, n);
+        if (p != n - 1 && i != s) return false;
     }
 
-    extra[v] = e;
-    return (r[v] = (c + e) % mod) % mod;
+    return true;
 }
 
-bool dfs(int v, vector<unordered_map<int, int>> &adj_list, vector<char> &state, vector<unordered_map<int, int>> &build) {
-    state[v] = 1;
-    build[v] = adj_list[v];
-    for (auto [u, count] : adj_list[v])
-        if (!state[u]) {
-            if (dfs(u, adj_list, state, build)) return true;
-        }
-        else if (state[u] == 1) return true;
-    state[v] = 2;
+template <typename T>
+struct MODULO {
+    static T value;
+};
 
-    return false;
+template <typename T>
+T MODULO<T>::value;
+
+auto &m = MODULO<int>::value;
+bool PRIME_MOD;
+
+template<typename M>
+struct ModInt {
+    using T = typename decay<decltype(M::value)>::type;
+    T value;
+
+    constexpr ModInt() : value() {}
+
+    template<typename U>
+    ModInt(const U &x) {
+        value = normalize(x);
+    }
+
+    template<typename U>
+    static T normalize(const U &x) {
+        T v = x;
+        if (!(-mod() <= x && x < mod())) v = (T) (x % mod());
+        if (v < 0) v += mod();
+        return v;
+    }
+
+    const T & operator()() const {
+        return value;
+    }
+
+    template<typename U>
+    explicit operator U() const {
+        return (U) value;
+    }
+
+    constexpr static T mod() {
+        return M::value;
+    }
+
+    inline auto & operator+=(const ModInt &v) {
+        if ((value += v.value) >= mod()) value -= mod();
+        return *this;
+    }
+
+    inline auto & operator-=(const ModInt &v) {
+        if ((value -= v.value) < 0) value += mod();
+        return *this;
+    }
+
+    template<typename U>
+    inline auto & operator+=(const U &v) {
+        return *this += ModInt(v);
+    }
+
+    template<typename U>
+    inline auto & operator-=(const U &v) {
+        return *this -= ModInt(v);
+    }
+
+    auto & operator++() {
+        return *this += 1;
+    }
+
+    auto & operator--() {
+        return *this -= 1;
+    }
+
+    auto operator++(int) {
+        ModInt v(*this);
+        *this += 1;
+        return v;
+    }
+
+    auto operator--(int) {
+        ModInt v(*this);
+        *this -= 1;
+        return v;
+    }
+
+    auto operator-() const {
+        return ModInt(-value);
+    }
+
+    template<typename U = M>
+    typename enable_if<is_same<typename ModInt<U>::T, int>::value, ModInt>::type &operator*=(const ModInt &v) {
+        value = normalize((long long) value * (long long) v.value);
+        return *this;
+    }
+
+    template<typename U = M>
+    typename enable_if<is_same<typename ModInt<U>::T, long long>::value, ModInt>::type &operator*=(const ModInt &v) {
+        value = normalize(value * v.value - (long long) ((long double) value * v.value / mod()) * mod());
+        return *this;
+    }
+
+    template<typename U = M>
+    typename enable_if<!is_integral<typename ModInt<U>::T>::value, ModInt>::type &operator*=(const ModInt &v) {
+        value = normalize(value * v.value);
+        return *this;
+    }
+
+    auto & operator/=(const ModInt &v) {
+        if (PRIME_MOD) return *this *= ModInt(pow(v.value, mod() - 2, mod()));
+        return *this *= ModInt(inverse(v.value, mod()));
+    }
+};
+
+template<typename T>
+bool operator==(const ModInt<T> &lhs, const ModInt<T> &rhs) {
+    return lhs.value == rhs.value;
 }
+
+template<typename T, typename U>
+bool operator==(const ModInt<T> &lhs, U rhs) {
+    return lhs == ModInt<T>(rhs);
+}
+
+template<typename T, typename U>
+bool operator==(U lhs, const ModInt<T> &rhs) {
+    return ModInt<T>(lhs) == rhs;
+}
+
+template<typename T>
+bool operator!=(const ModInt<T> &lhs, const ModInt<T> &rhs) {
+    return !(lhs == rhs);
+}
+
+template<typename T, typename U>
+bool operator!=(const ModInt<T> &lhs, U rhs) {
+    return !(lhs == rhs);
+}
+
+template<typename T, typename U>
+bool operator!=(U lhs, const ModInt<T> &rhs) {
+    return !(lhs == rhs);
+}
+
+template<typename T>
+bool operator>(const ModInt<T> &lhs, const ModInt<T> &rhs) {
+    return lhs.value > rhs.value;
+}
+
+template<typename T>
+bool operator<(const ModInt<T> &lhs, const ModInt<T> &rhs) {
+    return lhs.value < rhs.value;
+}
+
+template<typename T>
+ModInt<T> operator+(const ModInt<T> &lhs, const ModInt<T> &rhs) {
+    return ModInt<T>(lhs) += rhs;
+}
+
+template<typename T, typename U>
+ModInt<T> operator+(const ModInt<T> &lhs, U rhs) {
+    return ModInt<T>(lhs) += rhs;
+}
+
+template<typename T, typename U>
+ModInt<T> operator+(U lhs, const ModInt<T> &rhs) {
+    return ModInt<T>(lhs) += rhs;
+}
+
+template<typename T>
+ModInt<T> operator-(const ModInt<T> &lhs, const ModInt<T> &rhs) {
+    return ModInt<T>(lhs) -= rhs;
+}
+
+template<typename T, typename U>
+ModInt<T> operator-(const ModInt<T> &lhs, U rhs) {
+    return ModInt<T>(lhs) -= rhs;
+}
+
+template<typename T, typename U>
+ModInt<T> operator-(U lhs, const ModInt<T> &rhs) {
+    return ModInt<T>(lhs) -= rhs;
+}
+
+template<typename T>
+ModInt<T> operator*(const ModInt<T> &lhs, const ModInt<T> &rhs) {
+    return ModInt<T>(lhs) *= rhs;
+}
+
+template<typename T, typename U>
+ModInt<T> operator*(const ModInt<T> &lhs, U rhs) {
+    return ModInt<T>(lhs) *= rhs;
+}
+
+template<typename T, typename U>
+ModInt<T> operator*(U lhs, const ModInt<T> &rhs) {
+    return ModInt<T>(lhs) *= rhs;
+}
+
+template<typename T>
+ModInt<T> operator/(const ModInt<T> &lhs, const ModInt<T> &rhs) {
+    return ModInt<T>(lhs) /= rhs;
+}
+
+template<typename T, typename U>
+ModInt<T> operator/(const ModInt<T> &lhs, U rhs) {
+    return ModInt<T>(lhs) /= rhs;
+}
+
+template<typename T, typename U>
+ModInt<T> operator/(U lhs, const ModInt<T> &rhs) {
+    return ModInt<T>(lhs) /= rhs;
+}
+
+template <typename U, typename T>
+U & operator<<(U &stream, const ModInt<T> &v) {
+    return stream << v();
+}
+
+template <typename U, typename T>
+U & operator>>(U &stream, ModInt<T> &v) {
+    typename common_type<typename ModInt<T>::T, long long>::type x;
+    stream >> x;
+    v.value = ModInt<T>::normalize(x);
+    return stream;
+}
+
+using modint = ModInt<MODULO<int>>;
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n, mod;
-    cin >> n >> mod;
+    int n;
+    cin >> n >> m;
+    PRIME_MOD = isprime(m);
 
     vector<unordered_map<int, int>> adj_list(n);
     for (int i = 0; i < n; i++) {
@@ -61,9 +297,23 @@ int main() {
             adj_list[i][d]++;
         }
     }
-    vector<char> state(n, 0);
+
+    vector<int> state(n, 0);
     vector<unordered_map<int, int>> build(n);
-    if (dfs(0, adj_list, state, build) || build[0].empty()) {
+    auto dfs = [&](auto &&self, int v = 0) -> bool {
+        state[v] = 1;
+        build[v] = adj_list[v];
+        for (auto [u, count] : adj_list[v])
+            if (!state[u]) {
+                if (self(self, u)) return true;
+            }
+            else if (state[u] == 1) return true;
+
+        state[v] = 2;
+        return false;
+    };
+
+    if (dfs(dfs) || build[0].empty()) {
         cout << "Invalid";
         exit(0);
     }
@@ -76,8 +326,16 @@ int main() {
         exit(0);
     }
 
-    vector<long long> m(n, -1);
-    cout << maintain(0, mod, build, m) << "\n";
+    vector<long long> cost(n, -1);
+    auto maintain = [&](auto &&self, int v = 0) {
+        if (cost[v] != -1) return cost[v];
+
+        modint c = 1;
+        for (auto [u, count] : build[v]) c += self(self, u) * count;
+        return cost[v] = c.value;
+    };
+
+    cout << maintain(maintain) << "\n";
 
     while (q--) {
         int x;
@@ -93,6 +351,25 @@ int main() {
 
         vector<long long> r(n, -1);
         vector<bool> extra(n, false);
-        cout << rebuild(0, mod, y, build, m, r, extra) << "\n";
+        auto rebuild = [&](auto &&self, int v = 0) {
+            if (r[v] != -1) return r[v];
+
+            if (y.count(v)) {
+                extra[v] = true;
+                return maintain(maintain, v);
+            }
+
+            modint c = 0;
+            bool e = false;
+            for (auto [u, count] : build[v]) {
+                c += self(self, u) * count;
+                e |= extra[u];
+            }
+
+            extra[v] = e;
+            return r[v] = (c + e).value;
+        };
+
+        cout << rebuild(rebuild) << "\n";
     }
 }
