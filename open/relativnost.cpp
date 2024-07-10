956b40591b38011db787b7be8fefea9779a36875
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-07-10 16:44:53 +0800

    Update relativnost.cpp

--- open/relativnost.cpp
+++ open/relativnost.cpp
@@ -269,21 +269,6 @@ U & operator>>(U &stream, ModInt<T> &v) {
 
 using modint = ModInt<integral_constant<decay<decltype(MODULO)>::type, MODULO>>;
 
-long long mul(long long x, long long y, long long mod) {
-    auto product = x * y - mod * (long long) (1.L / mod * x * y);
-    return product + mod * (product < 0) - mod * (product >= mod);
-}
-
-long long pow(long long base, long long exponent, long long mod) {
-    auto value = 1LL;
-    for (; exponent; exponent >>= 1) {
-        if (exponent & 1) value = mul(value, base, mod);
-        base = mul(base, base, mod);
-    }
-
-    return value;
-}
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
