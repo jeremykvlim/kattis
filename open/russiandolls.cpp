4936eeb9bc4664e0017a90860d248cc490a739a5
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-07-18 01:15:09 +0800

    Update russiandolls.cpp

--- open/russiandolls.cpp
+++ open/russiandolls.cpp
@@ -1,51 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Doll {
-    int h, d, w;
-
-    bool operator<(const Doll &doll) const {
-        int weight = -doll.w;
-        return tie(h, d, w) < tie(doll.h, doll.d, weight);
-    }
-};
-
-bool arrange(int i, int j, int count, vector<Doll> &dolls, vector<vector<vector<bool>>> &dp, bool set) {
-    int k = max(i, j);
-    if (2 * count > dolls.size() - 1 || 2 * (k - count) > dolls.size() - 1 || dp[i][j][count]) return false;
-    if (k++ == dolls.size() - 1) return true;
-
-    if ((dolls[k].h - 2 * dolls[k].w >= dolls[i].h && dolls[k].d - 2 * dolls[k].w >= dolls[i].d)
-        && arrange(k, j, count + 1, dolls, dp, set)) {
-        if (set) cout << dolls[k].h << " " << dolls[k].d << " " << dolls[k].w << "\n";
-        return true;
-    }
-
-    if ((dolls[k].h - 2 * dolls[k].w >= dolls[j].h && dolls[k].d - 2 * dolls[k].w >= dolls[j].d)
-        && arrange(i, k, count, dolls, dp, set)) {
-        if (!set) cout << dolls[k].h << " " << dolls[k].d << " " << dolls[k].w << "\n";
-        return true;
-    }
-
-    dp[i][j][count] = true;
-    return false;
-}
-
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int n;
     while (cin >> n && n) {
-        vector<Doll> dolls(2 * n + 1);
-        for (int i = 1; i <= 2 * n; i++) cin >> dolls[i].h >> dolls[i].d >> dolls[i].w;
-        sort(dolls.begin(), dolls.end());
+        vector<array<int, 3>> d(2 * n + 1, {0, 0, 0});
+        for (int i = 1; i <= 2 * n; i++) cin >> d[i][0] >> d[i][1] >> d[i][2];
+        sort(d.begin(), d.end(), [](auto a1, auto a2) {return a1[0] != a2[0] ? a1[0] < a2[0] : a1[1] != a2[1] ? a1[1] < a2[1] : a1[2] > a2[2];});
 
         vector<vector<vector<bool>>> dp(2 * n + 1, vector<vector<bool>>(2 * n + 1, vector<bool>(2 * n + 1, false)));
-        arrange(0, 0, 0, dolls, dp, false);
+        auto arrange = [&](auto &&self, bool first, int count = 0, int i = 0, int j = 0) -> bool {
+            int k = max(i, j);
+            if (2 * count >= d.size() || 2 * (k - count) >= d.size() || dp[i][j][count]) return false;
+            if (k++ == d.size() - 1) return true;
+
+            auto valid = [&](int l) {
+                return d[k][0] - 2 * d[k][2] >= d[l][0] && d[k][1] - 2 * d[k][2] >= d[l][1];
+            };
+
+            if (valid(j) && self(self, first, count, i, k)) {
+                if (first) cout << d[k][0] << " " << d[k][1] << " " << d[k][2] << "\n";
+                return true;
+            }
+
+            if (valid(i) && self(self, first, count + 1, k, j)) {
+                if (!first) cout << d[k][0] << " " << d[k][1] << " " << d[k][2] << "\n";
+                return true;
+            }
+
+            dp[i][j][count] = true;
+            return false;
+        };
+
+        arrange(arrange, true);
         cout << "-\n";
-        arrange(0, 0, 0, dolls, dp, true);
+        arrange(arrange, false);
         cout << "\n";
     }
 }
