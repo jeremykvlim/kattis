62db549650221d739452a6ee364817130a26810b
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-07-17 22:27:51 +0800

    Update golfbot.cpp

--- nus/cs3233/golfbot.cpp
+++ nus/cs3233/golfbot.cpp
@@ -299,11 +299,13 @@ constexpr unsigned long long MODULO = 2524775926340780033;
 using modint = MontgomeryModInt<integral_constant<decay<decltype(MODULO)>::type, MODULO>>;
 
 template <typename T>
-vector<modint> convolve(vector<T> a, vector<T> b) {
+vector<T> convolve(vector<T> a, vector<T> b) {
     int n = 1;
     while (n < a.size() + b.size() - 1) n <<= 1;
-    a.resize(n);
-    b.resize(n);
+
+    vector<modint> ntt_a(n), ntt_b(n);
+    for (int i = 0; i < a.size(); i++) ntt_a[i] = a[i];
+    for (int i = 0; i < b.size(); i++) ntt_b[i] = b[i];
 
     modint n_mod = n;
     auto w = pow(3ULL, (MODULO - 1) / n, MODULO), w_inv = pow(w, MODULO - 2, MODULO), n_inv = pow(n_mod.value, MODULO - 2, MODULO);
@@ -328,13 +330,19 @@ vector<modint> convolve(vector<T> a, vector<T> b) {
                     v[j] += t;
                 }
     };
-    ntt(a, w);
-    ntt(b, w);
+    ntt(ntt_a, w);
+    ntt(ntt_b, w);
 
-    vector<modint> c(n);
-    for (int i = 0; i < n; i++) c[i] = a[i] * b[i];
-    ntt(c, w_inv);
-    for (auto &ci : c) ci *= n_inv;
+    vector<modint> ntt_c(n);
+    for (int i = 0; i < n; i++) ntt_c[i] = ntt_a[i] * ntt_b[i];
+    ntt(ntt_c, w_inv);
+
+    vector<T> c(n);
+    for (int i = 0; i < n; i++) {
+        ntt_c[i] *= n_inv;
+
+        c[i] = ntt_c[i].value;
+    }
 
     c.resize(a.size() + b.size() - 1);
     return c;
@@ -349,7 +357,7 @@ int main() {
     int n;
     cin >> n;
 
-    vector<modint> count_k(2e5 + 1, 0);
+    vector<int> count_k(2e5 + 1, 0);
     while (n--) {
         int k;
         cin >> k;
@@ -372,7 +380,7 @@ int main() {
 
     int holes = 0;
     for (int i = 1; i <= 2e5; i++)
-        if ((count_k[i] || c[i].value) && count_d[i]) holes++;
+        if ((count_k[i] || c[i]) && count_d[i]) holes++;
 
     cout << holes;
 }
