2fc7ecc4cea71bd67baba9636ebfc8554414d25a
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-07-18 00:07:45 +0800

    Update rebeldie.cpp

--- open/rebeldie.cpp
+++ open/rebeldie.cpp
@@ -1,31 +1,13 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
 using namespace std;
-using namespace __gnu_pbds;
-
-struct Die {
-    int up, down, left, right, front, back, height, width;
-
-    bool operator==(const Die &d) const {
-        return tie(up, down, left, right, front, back, height, width) ==
-               tie(d.up, d.down, d.left, d.right, d.front, d.back, d.height, d.width);
-    }
-};
 
 struct Hash {
-    static uint64_t encode(Die d) {
+    static uint64_t encode(array<int, 8> a) {
         auto encoded = 0ULL;
-        encoded = (encoded << 8) | d.up;
-        encoded = (encoded << 8) | d.down;
-        encoded = (encoded << 8) | d.left;
-        encoded = (encoded << 8) | d.right;
-        encoded = (encoded << 8) | d.front;
-        encoded = (encoded << 8) | d.back;
-        encoded = (encoded << 8) | d.height;
-        encoded = (encoded << 8) | d.width;
+        for (int e : a) encoded = (encoded << 8) | e;
         return encoded;
     }
-    
+
     static uint64_t h(uint64_t key) {
         auto hash = key + 0x9e3779b97f4a7c15;
         hash = (hash ^ (hash >> 30)) * 0xbf58476d1ce4e5b9;
@@ -34,50 +16,56 @@ struct Hash {
         return hash;
     }
 
-    size_t operator()(Die d) const {
+    size_t operator()(array<int, 8> a) const {
         static uint64_t SEED = chrono::steady_clock::now().time_since_epoch().count();
-        return h(encode(d) + SEED);
+        return h(encode(a) + SEED);
     }
 };
 
-void rotate(Die &s, char direction) {
-    switch (direction) {
+void rotate(array<int, 8> &state, char dir) {
+    switch (dir) {
         case 'N':
-            swap(s.down, s.back), swap(s.back, s.up), swap(s.up, s.front);
+            swap(state[1], state[5]);
+            swap(state[5], state[0]);
+            swap(state[0], state[4]);
             break;
         case 'S':
-            swap(s.front, s.up), swap(s.up, s.back), swap(s.back, s.down);
+            swap(state[4], state[0]);
+            swap(state[0], state[5]);
+            swap(state[5], state[1]);
             break;
         case 'E':
-            swap(s.down, s.right), swap(s.right, s.up), swap(s.up, s.left);
+            swap(state[1], state[3]);
+            swap(state[3], state[0]);
+            swap(state[0], state[2]);
             break;
         case 'W':
-            swap(s.left, s.up), swap(s.up, s.right), swap(s.right, s.down);
+            swap(state[2], state[0]);
+            swap(state[0], state[3]);
+            swap(state[3], state[1]);
             break;
     }
 }
 
-void dfs(Die curr, vector<vector<int>> &board, gp_hash_table<Die, null_type, Hash> &visited,
-         vector<pair<pair<int, int>, char>> &directions) {
-
-    if (curr.height + 1 == board.size() && curr.width + 1 == board[0].size()) {
+void dfs(array<int, 8> curr, vector<vector<int>> &board, unordered_set<array<int, 8>, Hash> &visited, vector<pair<pair<int, int>, char>> &directions) {
+    if (curr[6] + 1 == board.size() && curr[7] + 1 == board[0].size()) {
         cout << "YES";
         exit(0);
     }
 
     for (auto [d, c] : directions) {
-        Die next = curr;
-        next.height += d.first;
-        next.width += d.second;
-        if (!(0 <= next.height && next.height < board.size() && 0 <= next.width && next.width < board[0].size())) continue;
-        
+        auto next = curr;
+        next[6] += d.first;
+        next[7] += d.second;
+        if (!(0 <= next[6] && next[6] < board.size() && 0 <= next[7] && next[7] < board[0].size())) continue;
+
         rotate(next, c);
 
-        if (next.down == -1) next.down = board[next.height][next.width];
-        if (next.down != board[next.height][next.width]) continue;
-        
-        if (visited.find(next) == visited.end()) {
-            visited.insert(next);
+        if (next[1] == -1) next[1] = board[next[6]][next[7]];
+        if (next[1] != board[next[6]][next[7]]) continue;
+
+        if (!visited.count(next)) {
+            visited.emplace(next);
             dfs(next, board, visited, directions);
         }
     }
@@ -99,9 +87,9 @@ int main() {
                                                   {{0,  1}, 'E'},
                                                   {{0,  -1}, 'W'}};
 
-    gp_hash_table<Die, null_type, Hash> visited;
-    Die start{-1, board[0][0], -1, -1, -1, -1, 0, 0};
-    visited.insert(start);
+    unordered_set<array<int, 8>, Hash> visited;
+    array<int, 8> start{-1, board[0][0], -1, -1, -1, -1, 0, 0};
+    visited.emplace(start);
     dfs(start, board, visited, directions);
 
     cout << "NO";
