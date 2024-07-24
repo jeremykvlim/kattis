d29455ac3deccd7102e98255f0b1f6ca4016abce
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-07-24 18:13:57 +0800

    Update donations.cpp

--- open/donations.cpp
+++ open/donations.cpp
@@ -12,9 +12,9 @@ int main() {
     vector<vector<double>> dp(2, vector<double>(n + 1, 0));
     dp[0][0] = x;
     for (int i = 0; i < n; i++) {
-        dp[i & 1 ^ 1] = vector<double>(n + 1, 0);
+        dp[(i & 1) ^ 1] = vector<double>(n + 1, 0);
         for (int j = i; j <= n; j++)
-            for (int k = j; k <= n; k++) dp[i & 1 ^ 1][k] = max(dp[i & 1 ^ 1][k], (dp[i & 1][j] - (k - j) * y + max(0.0, (k - j) * y - z) * c / 100.0) * (1.0 + r / 100.0));
+            for (int k = j; k <= n; k++) dp[(i & 1) ^ 1][k] = max(dp[(i & 1) ^ 1][k], (dp[i & 1][j] - (k - j) * y + max(0.0, (k - j) * y - z) * c / 100.0) * (1.0 + r / 100.0));
     }
 
     cout << fixed << setprecision(6) << dp[n & 1][n];
