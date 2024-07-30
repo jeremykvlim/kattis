a1a2cf85625a6ef190a76084b2a2bfe4733d984c
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-07-31 03:26:49 +0800

    Create organfreeman.cpp

---
+++ open/organfreeman.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    vector<int> fact(10, 1);
+    for (int i = 2; i < 10; i++) fact[i] = i * fact[i - 1];
+
+    int n;
+    cin >> n;
+
+    string s;
+    for (int i = fact.size() - 1, x; ~i; --i) {
+        x = n / fact[i];
+        n %= fact[i];
+        if (x) s += string(x, '0' + i - 1 + (i > 1));
+    }
+    reverse(s.begin(), s.end());
+    if (s.size() > 1 && s[0] == '0') s[0]++;
+
+    cout << s;
+}
