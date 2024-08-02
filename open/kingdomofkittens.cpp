a8a932e8f4238d094bc9eeee3de939c73374a2b6
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-08-02 16:30:48 +0800

    Update kingdomofkittens.cpp

--- open/kingdomofkittens.cpp
+++ open/kingdomofkittens.cpp
@@ -8,36 +8,36 @@ struct Point {
     Point() {}
     Point(T x, T y) : x(x), y(y) {}
 
-    auto operator<(Point<T> &p) const {
+    auto operator<(Point &p) const {
         return x != p.x ? x < p.x : y < p.y;
     }
 
-    auto operator==(Point<T> &p) const {
+    auto operator==(Point &p) const {
         return x == p.x && y == p.y;
     }
 
-    auto operator!=(Point<T> &p) const {
+    auto operator!=(Point &p) const {
         return x != p.x || y != p.y;
     }
 
-    Point operator-(Point<T> &p) const {
+    Point operator-(Point &p) const {
         return {x - p.x, y - p.y};
     }
 };
 
 template <typename T>
-double dot(Point<T> a, Point<T> b) {
-    return (double) (a.x * b.x) + (a.y * b.y);
+T dot(Point<T> a, Point<T> b) {
+    return (a.x * b.x) + (a.y * b.y);
 }
 
 template <typename T>
-double cross(Point<T> a, Point<T> b) {
-    return (double) (a.x * b.y) - (a.y * b.x);
+T cross(Point<T> a, Point<T> b) {
+    return (a.x * b.y) - (a.y * b.x);
 }
 
 template <typename T>
-double cross(Point<T> a, Point<T> b, Point<T> c) {
-    return (double) (c.x - a.x) * (double) (b.y - a.y) - (double) (c.y - a.y) * (double) (b.x - a.x);
+T cross(Point<T> a, Point<T> b, Point<T> c) {
+    return (c.x - a.x) * (b.y - a.y) - (c.y - a.y) * (b.x - a.x);
 }
 
 template <typename T>
