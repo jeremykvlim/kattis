7458d967ab87a52d863f6c23118d6d4ac3d0aa27
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-08-02 17:46:18 +0800

    Create flatlandolympics.cpp

---
+++ open/flatlandolympics.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+struct FenwickTree {
+    vector<T> BIT;
+
+    void update(int i, T value) {
+        for (; i < BIT.size(); i += i & -i) BIT[i] += value;
+    }
+
+    T pref_sum(int i) {
+        T sum = 0;
+        for (; i; i &= (i - 1)) sum += BIT[i];
+        return sum;
+    }
+
+    FenwickTree(int n) : BIT(n, 0) {}
+};
+
+template <typename T>
+struct Point {
+    T x, y;
+
+    Point() {}
+    Point(T x, T y) : x(x), y(y) {}
+
+    auto operator<(Point &p) const {
+        return x != p.x ? x < p.x : y < p.y;
+    }
+
+    auto operator==(Point &p) const {
+        return x == p.x && y == p.y;
+    }
+
+    auto operator!=(Point &p) const {
+        return x != p.x || y != p.y;
+    }
+
+    Point operator-() {
+        return {-x, -y};
+    }
+
+    Point operator+(Point &p) const {
+        return {x + p.x, y + p.y};
+    }
+
+    Point operator-(Point &p) const {
+        return {x - p.x, y - p.y};
+    }
+
+    auto & operator-=(Point &p) {
+        x -= p.x;
+        y -= p.y;
+        return *this;
+    }
+};
+
+template <typename T>
+T dot(Point<T> a, Point<T> b) {
+    return (a.x * b.x) + (a.y * b.y);
+}
+
+template <typename T>
+T cross(Point<T> a, Point<T> b) {
+    return (a.x * b.y) - (a.y * b.x);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    Point<long long> s, e;
+    int n;
+    cin >> s.x >> s.y >> e.x >> e.y >> n;
+
+    vector<Point<long long>> up, down;
+    int left = 0, right = 0;
+    for (int i = 0; i < n; i++) {
+        Point<long long> p;
+        cin >> p.x >> p.y;
+
+        if (cross(e - s, p - s) > 0) up.emplace_back(p);
+        else if (cross(e - s, p - s) < 0) down.emplace_back(p);
+        else if (dot(e - s, p - s) < 0) left++;
+        else right++;
+    }
+
+    long long count = left * (left - 1) / 2 + right * (right - 1) / 2;
+
+    auto inversions = [&](auto points) {
+        int n = points.size();
+
+        vector<int> indices(n);
+        iota(indices.begin(), indices.end(), 0);
+
+        vector<int> pref_s(n);
+        sort(indices.begin(), indices.end(), [&](int i, int j) {return cross(points[i] - s, points[j] - s) > 0;});
+        for (int i = 1; i < n; i++) pref_s[indices[i]] = pref_s[indices[i - 1]] + (cross(points[indices[i - 1]] - s, points[indices[i]] - s) > 0);
+
+        vector<int> pref_e(n);
+        sort(indices.begin(), indices.end(), [&](int i, int j) {return cross(points[i] - e, points[j] - e) < 0;});
+        for (int i = 1; i < n; i++) pref_e[indices[i]] = pref_e[indices[i - 1]] + (cross(points[indices[i - 1]] - e, points[indices[i]] - e) < 0);
+
+        sort(indices.begin(), indices.end(), [&](int i, int j) {return pref_s[i] < pref_s[j] || (pref_s[i] == pref_s[j] && pref_e[i] < pref_e[j]);});
+        FenwickTree<int> fw(n + 1);
+        auto inversions = 0LL;
+        for (int i = 0; i < n; i++) {
+            inversions += fw.pref_sum(pref_e[indices[i]] + 1);
+            fw.update(pref_e[indices[i]] + 1, 1);
+        }
+        return inversions;
+    };
+
+    count += inversions(up);
+    swap(s, e);
+    count += inversions(down);
+
+    cout << count;
+}
