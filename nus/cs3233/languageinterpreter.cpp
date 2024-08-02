a3c52a035ea04736c2a2e16a0cdb917ed463e9bc
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-08-02 16:10:23 +0800

    Update languageinterpreter.cpp

--- nus/cs3233/languageinterpreter.cpp
+++ nus/cs3233/languageinterpreter.cpp
@@ -20,10 +20,6 @@ struct Matrix {
         return C;
     }
 
-    friend auto operator*=(Matrix<T> &A, Matrix<T> &B) {
-        return A = A * B;
-    }
-
     auto & operator[](int i) {
         return mat[i];
     }
