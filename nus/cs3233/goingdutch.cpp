ced170f9451a3c3a6399550105b50b245c9a5844
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-08-03 17:35:27 +0800

    Create goingdutch.cpp

---
+++ nus/cs3233/goingdutch.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int m, n;
+    cin >> m >> n;
+
+    vector<int> balance(m);
+    for (int i = 0; i < n; i++) {
+        int a, b, p;
+        cin >> a >> b >> p;
+
+        balance[a] -= p;
+        balance[b] += p;
+    }
+    
+    vector<int> dp(1 << m, 0);
+    for (int i = 1; i < 1 << m; ++i) {
+        int sum = 0;
+        for (int j = 0; j < m; ++j)
+            if (i & (1 << j)) {
+                sum += balance[j];
+                dp[i] = max(dp[i], dp[i ^ (1 << j)]);
+            }
+        if (!sum) dp[i]++;
+    }
+
+    cout << m - dp.back();
+}
