98b93540428470c9ddc1d7733b9626ccd12a9d64
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-08-10 01:51:23 +0800

    Create barktree.cpp

---
+++ open/barktree.cpp
@@ -0,0 +1,169 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+struct Point {
+    T x, y;
+
+    Point() {}
+    Point(T x, T y) : x(x), y(y) {}
+
+    auto operator<(Point &p) const {
+        return x != p.x ? x < p.x : y < p.y;
+    }
+
+    bool operator!=(Point p) const {
+        return x != p.x || y != p.y;
+    }
+
+    Point operator+(Point p) const {
+        return {x + p.x, y + p.y};
+    }
+
+    Point operator-(Point p) const {
+        return {x - p.x, y - p.y};
+    }
+
+    Point operator*(T c) {
+        return {c * x, c * y};
+    }
+};
+
+template <typename T>
+double angle(Point<T> p) {
+    return atan2(p.y, p.x);
+}
+
+template <typename T>
+T dot(Point<T> a, Point<T> b) {
+    return (a.x * b.x) + (a.y * b.y);
+}
+
+template <typename T>
+T cross(Point<T> a, Point<T> b) {
+    return (a.x * b.y) - (a.y * b.x);
+}
+
+template <typename T>
+double dist(Point<T> p) {
+    return sqrt(p.x * p.x + p.y * p.y);
+}
+
+template <typename T>
+double dist(Point<T> a, Point<T> b) {
+    return sqrt((double) (a.x - b.x) * (a.x - b.x) + (double) (a.y - b.y) * (a.y - b.y));
+}
+
+template <typename T>
+int signum(T v) {
+    return fabs(v) < 1e-8 ? 0 : (v > 0) - (v < 0);
+}
+
+template <typename T>
+bool point_in_polygon(vector<Point<T>> polygon, Point<T> p) {
+    bool inside = false;
+    for (int i = 0; i < polygon.size(); i++) {
+        auto a = polygon[i] - p, b = polygon[(i + 1) % polygon.size()] - p;
+        if (a.y > b.y) swap(a, b);
+        if (signum(a.y) <= 0 && 0 < signum(b.y) && signum(cross(a, b)) < 0) inside = !inside;
+        if (!signum(cross(a, b)) && signum(dot(a, b)) <= 0) return false;
+    }
+    return inside;
+}
+
+template <typename T>
+struct Line {
+    Point<T> a, b;
+
+    Line() {}
+    Line(Point<T> a, Point<T> b) : a(a), b(b) {}
+    Line(T &a, T &b, T &c) {
+        if (a == 0 && b != 0) {
+            a = {0, c / b};
+            b = {1, c / b};
+        } else if (b == 0) {
+            a = {c / a, 0};
+            b = {c / a, 1};
+        } else if (c == 0) {
+            a = {0, c / b};
+            b = {1, (c - a) / b};
+        } else {
+            a = {0, c / b};
+            b = {c / a, 0};
+        }
+    }
+};
+
+template <typename T>
+T area_of_parallelogram(const Line<T> l1, const Line<T> l2) {
+    return cross(l1.b - l1.a, l2.b - l2.a);
+}
+
+template <typename T>
+Point<T> line_intersection_point(Line<T> l1, Line<T> l2) {
+    auto area = area_of_parallelogram(l1, l2);
+    auto pos = cross(l1.b - l1.a, l1.b - l2.a);
+
+    if (!signum(area) && !signum(pos)) return l2.a;
+    if (!signum(area)) return {numeric_limits<T>::epsilon(), numeric_limits<T>::epsilon()};
+
+    return l2.a + (l2.b - l2.a) * (pos / area);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<Point<double>> toys(n), trees(m);
+    for (auto &[x, y] : toys) cin >> x >> y;
+    for (auto &[x, y] : trees) cin >> x >> y;
+
+    vector<Point<double>> wrapped{Point(0., 0.)};
+    vector<double> angles{0};
+    Point<double> spot(0, 0);
+    double len = 0;
+    for (auto toy : toys)
+        while (spot != toy) {
+            auto prev = wrapped.back(), a = spot - prev;
+
+            Point<double> tree(0, 0);
+            auto smallest = 2 * M_PI;
+            for (auto t : trees)
+                if (point_in_polygon({prev, spot, toy}, t)) {
+                    auto b = t - prev;
+
+                    auto theta = acos(dot(a, b) / (dist(a) * dist(b)));
+                    if (smallest > theta) {
+                        smallest = theta;
+                        tree = t;
+                    }
+                }
+            auto curr = tree != Point(0., 0.) ? line_intersection_point(Line(prev, tree), Line(spot, toy)) : toy, b = curr - prev;
+
+            auto theta = fmod(angle(a) - angle(b) + 3 * M_PI, 2 * M_PI) - M_PI;
+            if (wrapped.size() > 1 && ((angles.back() < 0) ^ (angles.back() < -theta))) {
+                curr = line_intersection_point(Line(wrapped[wrapped.size() - 2], prev), Line(spot, toy));
+                wrapped.pop_back();
+                angles.pop_back();
+            } else {
+                angles.back() += theta;
+
+                if (tree != Point(0., 0.)) {
+                    wrapped.emplace_back(tree);
+                    angles.emplace_back(signum(theta) * 1e-8);
+                }
+            }
+
+            double l = 0;
+            for (int i = 0; i < wrapped.size() - 1; i++) l += dist(wrapped[i], wrapped[i + 1]);
+            l += dist(wrapped.back(), curr);
+            len = max(len, l);
+
+            spot = curr;
+        }
+
+    cout << fixed << setprecision(2) << len;
+}
