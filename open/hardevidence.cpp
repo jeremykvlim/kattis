008a412b0b1d71c34611edadada66b98a3175e8d
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-08-09 21:26:53 +0800

    Update hardevidence.cpp

--- open/hardevidence.cpp
+++ open/hardevidence.cpp
@@ -14,21 +14,20 @@ int main() {
     int sectors = 9;
     auto theta = 2 * M_PI / sectors, a = 0.;
     for (int i = 0; i < sectors; i++) {
-        auto angle = [&](auto a) {
-            auto xp = R * cos(a), yp = R * sin(a);
+        auto angle = [&](auto theta) {
+            auto xp = R * cos(theta), yp = R * sin(theta);
 
             vector<double> angles;
             for (int i = 0; i < n; i++) angles.emplace_back(atan2(y[i] - yp, x[i] - xp));
             sort(angles.begin(), angles.end());
 
-            auto diff = [](auto a, auto b) {
-                while (a - b < -1e-11) a += 2 * M_PI;
-                return a - b;
+            auto diff = [](auto a1, auto a2) {
+                while (a2 - a1 < -1e-11) a2 += 2 * M_PI;
+                return a2 - a1;
             };
 
             double gap = 0;
-            for (int i = 1; i < angles.size(); i++) gap = max(gap, diff(angles[i], angles[i - 1]));
-            gap = max(gap, diff(angles[0], angles.back()));
+            for (int i = 0; i < angles.size(); i++) gap = max(gap, diff(angles[i], angles[(i + 1) % angles.size()]));
 
             return 2 * M_PI - gap;
         };
