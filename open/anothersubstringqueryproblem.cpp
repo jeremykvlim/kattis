a49671dea0ec6ca16ade15cf70704b4439225256
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-08-20 23:07:51 +0800

    Update anothersubstringqueryproblem.cpp

--- open/anothersubstringqueryproblem.cpp
+++ open/anothersubstringqueryproblem.cpp
@@ -1,7 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+template <typename T>
+struct SparseTable {
+    vector<vector<T>> ST;
+    function<T(T, T)> f;
+
+    SparseTable(vector<T> v, function<T(T, T)> func) : f(move(func)) {
+        if (v.empty()) return;
+        int n = __lg(v.size()) + 1;
+        ST.resize(n);
+        ST.front() = v;
+        for (int i = 1; i < n; i++) {
+            ST[i].resize(v.size() - (1 << i) + 1);
+            for (int j = 0; j <= v.size() - (1 << i); j++)
+                ST[i][j] = f(ST[i - 1][j], ST[i - 1][j + (1 << (i - 1))]);
+        }
+    }
+
+    T range_query(int l, int r) {
+        int i = __lg(r - l + 1);
+        return f(ST[i][l], ST[i][r - (1 << i) + 1]);
+    }
+};
+
 struct SuffixArray {
+    string s;
     vector<int> SA, ascii;
 
     vector<int> sais(vector<int> &ascii1, int range) {
@@ -81,6 +105,22 @@ struct SuffixArray {
         return sa;
     }
 
+    vector<int> kasai() {
+        int n = ascii.size();
+        vector<int> lcp(n), rank(n);
+        for (int i = 0; i < n; i++) rank[SA[i]] = i;
+        for (int i = 0, k = 0; i < n; i++) {
+            if (k) k--;
+            if (!rank[i]) continue;
+
+            int j = SA[rank[i] - 1];
+            while (i + k < n && j + k < n && ascii[i + k] == ascii[j + k]) k++;
+            lcp[rank[i] - 1] = k;
+        }
+        lcp.back() = n;
+        return lcp;
+    }
+
     auto begin() {
         return SA.begin();
     }
@@ -93,7 +133,68 @@ struct SuffixArray {
         return SA[i];
     }
 
-    SuffixArray(string &s, int r = 128) : ascii(s.begin(), s.end()) {
+    pair<int, int> search(string &t, SparseTable<int> &lcp_st) {
+        int match_l = mismatch(t.begin(), t.end(), s.begin() + SA.front(), s.end()).first - t.begin(),
+            match_r = mismatch(t.begin(), t.end(), s.begin() + SA.back(), s.end()).first - t.begin(),
+            match_m, l = 0, r = s.size(), m;
+
+        auto update_match = [&]() {
+            if (SA[m] + match_m < s.size() && s[SA[m] + match_m] > t[match_m]) {
+                r = m;
+                match_r = match_m;
+            } else {
+                l = m;
+                match_l = match_m;
+            }
+        };
+
+        auto match_lcp = [&](int m) -> pair<int, int> {
+            auto search_half = [&](bool left) {
+                int l = left ? -1 : m, r = left ? m : s.size();
+                while (l + 1 < r) {
+                    int mid = l + (r - l) / 2;
+
+                    if (lcp_st.range_query(left ? mid : l, left ? r - 1 : mid - 1) < t.size()) (left ? l : r) = mid;
+                    else (left ? r : l) = mid;
+                }
+                return r;
+            };
+
+            return {search_half(true), search_half(false)};
+        };
+
+        while (l + 1 < r) {
+            m = l + (r - l) / 2;
+
+            if (match_l < match_r) {
+                int lcp = lcp_st.range_query(m, r - 1);
+
+                if (match_r < lcp) r = m;
+                else if (match_r > lcp) l = m;
+                else {
+                    match_m = mismatch(t.begin() + match_r, t.end(), s.begin() + SA[m] + match_r, s.end()).first - t.begin();
+
+                    if (match_m != t.size()) update_match();
+                    else return match_lcp(m);
+                }
+            } else {
+                int lcp = lcp_st.range_query(l, m - 1);
+
+                if (match_l < lcp) l = m;
+                else if (match_l > lcp) r = m;
+                else {
+                    match_m = mismatch(t.begin() + match_l, t.end(), s.begin() + SA[m] + match_l, s.end()).first - t.begin();
+
+                    if (match_m != t.size()) update_match();
+                    else return match_lcp(m);
+                }
+            }
+        }
+
+        return {l, r};
+    }
+
+    SuffixArray(string &s, int r = 128) : s(s), ascii(s.begin(), s.end()) {
         SA = sais(ascii, r);
     };
 };
@@ -101,31 +202,31 @@ struct SuffixArray {
 template <typename T>
 struct WaveletTree {
     vector<T> WT, temp;
-    vector<int> pref1, pref2;
+    vector<int> pref_even, pref_odd;
     int b;
 
-    void range_select(vector<array<int, 3>> &queries) {
+    void select(vector<array<int, 3>> &queries) {
         for (int bit = b; ~bit; bit--) {
             for (int i = 0; i < WT.size(); i++) {
-                pref1[i + 1] = pref1[i] + !((WT[i] >> bit) & 1);
-                pref2[i + 1] = pref2[i] + ((WT[i] >> bit) & 1);
+                pref_even[i + 1] = pref_even[i] + !((WT[i] >> bit) & 1);
+                pref_odd[i + 1] = pref_odd[i] + ((WT[i] >> bit) & 1);
             }
 
-            int it1 = 0, it2 = pref1.back();
-            for (int e : WT) temp[!((e >> bit) & 1) ? it1++ : it2++] = e;
+            int even = 0, odd = pref_even.back();
+            for (int e : WT) temp[!((e >> bit) & 1) ? even++ : odd++] = e;
             swap(WT, temp);
 
             for (auto &[l, r, k] : queries) {
                 if (r - l < k || !k) continue;
 
-                int K = pref1[r] - pref1[l];
+                int K = pref_even[r] - pref_even[l];
                 if (k <= K) {
-                    l = pref1[l];
-                    r = pref1[r];
+                    l = pref_even[l];
+                    r = pref_even[r];
                 } else {
                     k -= K;
-                    l = pref1.back() + pref2[l];
-                    r = pref1.back() + pref2[r];
+                    l = pref_even.back() + pref_odd[l];
+                    r = pref_even.back() + pref_odd[r];
                 }
             }
         }
@@ -135,12 +236,10 @@ struct WaveletTree {
         return WT[i];
     }
 
-    WaveletTree(vector<T> a, vector<array<int, 3>> &order_statistics) : WT(a.begin(), a.end()),
-                                                                        temp(a.size()),
-                                                                        pref1(a.size() + 1),
-                                                                        pref2(a.size() + 1) {
+    WaveletTree(vector<T> a, vector<array<int, 3>> &k_order_statistics) : WT(a.begin(), a.end()), temp(a.size()),
+                                                                          pref_even(a.size() + 1), pref_odd(a.size() + 1) {
         b = __lg(*max_element(a.begin(), a.end()));
-        range_select(order_statistics);
+        select(k_order_statistics);
     }
 };
 
@@ -153,8 +252,10 @@ int main() {
     cin >> s >> q;
 
     SuffixArray sa(s);
-    vector<array<int, 3>> order_statistics(q);
-    for (auto &[l, r, k] : order_statistics) {
+    SparseTable<int> st(sa.kasai(), [](int x, int y) {return min(x, y);});
+
+    vector<array<int, 3>> k_order_statistics(q);
+    for (auto &[l, r, k] : k_order_statistics) {
         string t;
         cin >> t >> k;
 
@@ -163,12 +264,10 @@ int main() {
             continue;
         }
 
-        l = lower_bound(sa.begin(), sa.end(), 0, [&](int i, int) -> bool {return lexicographical_compare(s.begin() + i, s.end(), t.begin(), t.end());}) - sa.begin();
-        r = lower_bound(sa.begin() + l, sa.end(), 0, [&](int i, int) -> bool {return mismatch(t.begin(), t.end(), s.begin() + i, s.end()).first == t.end();}) - sa.begin();
-
+        tie(l, r) = sa.search(t, st);
         if (r - l < k || s[sa[r - 1] + t.size() - 1] != t.back()) k = 0;
     }
 
-    WaveletTree<int> wt(sa.SA, order_statistics);
-    for (auto [l, r, k] : order_statistics) cout << (k ? wt[r - 1] + 1 : -1) << "\n";
+    WaveletTree<int> wt(sa.SA, k_order_statistics);
+    for (auto [l, r, k] : k_order_statistics) cout << (k ? wt[r - 1] + 1 : -1) << "\n";
 }
