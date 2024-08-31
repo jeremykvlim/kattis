ee0266c578d52970f661ccaefca41efd4fc902cc
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-09-01 06:26:14 +0800

    Create bugs.cpp

---
+++ open/bugs.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> prefix_function(string s) {
+    vector<int> pi(s.size());
+    for (int i = 1; i < s.size(); i++) {
+        int j = pi[i - 1];
+        while (j && s[i] != s[j]) j = pi[j - 1];
+        if (s[i] == s[j]) j++;
+        pi[i] = j;
+    }
+
+    return pi;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    string b;
+    while (cin >> t >> b) {
+        cin.ignore();
+
+        auto pi = prefix_function(b);
+        while (t--) {
+            string s;
+            getline(cin, s);
+
+            int k = 0;
+            auto temp = s;
+            vector<int> match(s.size());
+            for (int i = 0, j = 0; i < s.size(); i++) {
+                while (j && s[i] != b[j]) j = pi[j - 1];
+                if (s[i] == b[j]) j++;
+
+                temp[k] = s[i];
+                match[k++] = j;
+
+                if (j == b.size()) {
+                    k -= b.size();
+                    j = !k ? 0 : match[k - 1];
+                }
+            }
+
+            temp.resize(k);
+            cout << temp << "\n";
+        }
+    }
+}
