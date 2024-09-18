b6275ee25edef0020c94d69160e3542b2f0dbad1
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-09-19 04:59:10 +0800

    Update probedroids.cpp

--- open/probedroids.cpp
+++ open/probedroids.cpp
@@ -1,6 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+template <typename T>
+struct Point {
+    T x, y;
+
+    Point() {}
+    Point(T x, T y) : x(x), y(y) {}
+
+    auto operator==(const Point &p) const {
+        return x == p.x && y == p.y;
+    }
+
+    auto operator+=(const T &v) {
+        x += v;
+        y += v;
+    }
+
+    Point operator+(const Point &p) const {
+        return {x + p.x, y + p.y};
+    }
+
+    Point operator-(const Point &p) const {
+        return {x - p.x, y - p.y};
+    }
+};
+
+template <typename T>
+T cross(Point<T> a, Point<T> b) {
+    return (a.x * b.y) - (a.y * b.x);
+}
+
 long long destroyed(long long n, long long m, long long mid) {
     if (mid < n) return (__gcd(m - 1, mid - 1) + m * mid - 1) / 2;
     else if (mid < n + m) return (__gcd(n - 1, n + m - mid - 1) + mid * n - (n - m) * n - 1) / 2;
@@ -15,7 +45,7 @@ int main() {
     int q;
     cin >> n >> m >> q;
 
-    vector<pair<long long, long long>> drdc(max(n, m));
+    vector<Point<long long>> drdc(max(n, m));
     while (q--) {
         long long i;
         cin >> i;
@@ -26,31 +56,39 @@ int main() {
             long long l = 0, r = INT_MAX, mid;
             while (l + 1 < r) {
                 mid = l + (r - l) / 2;
+                
                 if (destroyed(n, m, mid) >= i) r = mid;
                 else l = mid;
             }
             i -= destroyed(n, m, l);
 
-            auto curr = make_pair(0LL, 1LL), remaining = (l < n) ? make_pair(l - 1, m - 1) : make_pair(n - 1, m - 1 + n - l);
-            while (curr.first + 1 < n && curr.second < m && (curr.first + 1) * remaining.second <= curr.second * remaining.first) curr.first++;
+            Point<long long> curr = {0, 1}, remaining = {l - 1, m - 1};
+            if (l >= n) remaining += n - l;
 
+            auto valid = [&](Point<long long> p) {
+                return p.x < n && p.y < m;
+            };
+
+            for (Point<long long> next = {curr.x + 1, curr.y}; valid(next) && cross(next, remaining) <= 0; next = {++curr.x + 1, curr.y});
             drdc[0] = {1, 0};
-            int pos = 1;
-            for (;;) {
-                auto next = make_pair(curr.first + drdc[pos - 1].first, curr.second + drdc[pos - 1].second);
-                if (next.first >= n || next.second >= m) {
-                    if (curr.first * remaining.second > curr.second * remaining.first) {
-                        auto destroy = min((n - 1) / curr.first, (m - 1) / curr.second);
-                        if (destroy > i - 1) {
-                            cout << curr.first * i + 1 << " " << curr.second * i + 1 << "\n";
+            for (int k = 1;;) {
+                auto next = curr + drdc[k - 1];
+
+                if (valid(next)) (cross(next, remaining) > 0 ? drdc[k++] : curr) = next;
+                else {
+                    if (cross(curr, remaining) > 0) {
+                        auto j = min((n - 1) / curr.x, (m - 1) / curr.y);
+                        
+                        if (i <= j) {
+                            cout << curr.x * i + 1 << " " << curr.y * i + 1 << "\n";
                             break;
                         }
 
-                        i -= destroy;
+                        i -= j;
                     }
-                    curr = drdc[--pos];
-                } else if (next.first * remaining.second > next.second * remaining.first) drdc[pos++] = next;
-                else curr = next;
+
+                    curr = drdc[--k];
+                }
             }
         }
     }
