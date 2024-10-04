fa0d26de121b068c85c5fe0758e1542dfff7ad15
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-10-04 11:43:21 +0800

    Update domino.cpp

--- open/domino.cpp
+++ open/domino.cpp
@@ -1,31 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void dijkstra(vector<vector<array<int, 6> *>> adj_list, vector<int> &potential) {
-    vector<int> dist(potential.size(), INT_MAX);
-    dist[0] = 0;
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
-    pq.emplace(0, 0);
-    while (!pq.empty()) {
-        auto [d, v] = pq.top();
-        pq.pop();
-
-        for (auto e : adj_list[v]) {
-            auto [u, v, w, capacity, flow, index] = *e;
-
-            if (capacity == flow) continue;
-
-            if (dist[v] > d + w + potential[u] - potential[v]) {
-                dist[v] = d + w + potential[u] - potential[v];
-                pq.emplace(dist[v], v);
-            }
-        }
-    }
-
-    for (int i = 0; i < dist.size(); i++)
-        if (dist[i] != INT_MAX) potential[i] += dist[i];
-}
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -56,49 +31,55 @@ int main() {
         dominos -= count[i];
     }
 
-    vector<array<int, 6>> edges;
-    int index = 0;
-    auto add = [&](int u, int v, int w) {
-        edges.push_back({v, u, w, 1, 0, index++});
-        edges.push_back({u, v, -w, 0, 0, index++});
-    };
-
     int id = 2;
     vector<vector<int>> coverable(n, vector<int>(n, -1));
+    vector<array<int, 3>> process;
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++) {
             if (i + 1 < n && count[table[i][j] + table[i + 1][j]]-- > 0) {
                 if (coverable[i][j] == -1) coverable[i][j] = id++;
                 if (coverable[i + 1][j] == -1) coverable[i + 1][j] = id++;
 
-                if ((i + j) & 1) add(coverable[i + 1][j], coverable[i][j], 0);
-                else add(coverable[i][j], coverable[i + 1][j], 0);
+                if ((i + j) & 1) process.push_back({coverable[i + 1][j], coverable[i][j], 0});
+                else process.push_back({coverable[i][j], coverable[i + 1][j], 0});
             }
 
             if (j + 1 < n && count[table[i][j] + table[i][j + 1]]-- > 0) {
                 if (coverable[i][j] == -1) coverable[i][j] = id++;
                 if (coverable[i][j + 1] == -1) coverable[i][j + 1] = id++;
 
-                if ((i + j) & 1) add(coverable[i][j + 1], coverable[i][j], 0);
-                else add(coverable[i][j], coverable[i][j + 1], 0);
+                if ((i + j) & 1) process.push_back({coverable[i][j + 1], coverable[i][j], 0});
+                else process.push_back({coverable[i][j], coverable[i][j + 1], 0});
             }
 
             if (coverable[i][j] == -1) continue;
 
-            if ((i + j) & 1) add(coverable[i][j], 1, -table[i][j]);
-            else add(0, coverable[i][j], -table[i][j]);
+            if ((i + j) & 1) process.push_back({coverable[i][j], 1, -table[i][j]});
+            else process.push_back({0, coverable[i][j], -table[i][j]});
         }
 
-    vector<vector<array<int, 6> *>> adj_list(id);
-    for (auto &e : edges) adj_list[e[1]].emplace_back(&e);
+    vector<array<int, 3>> edges(2 * process.size());
+    vector<int> capacity(2 * process.size(), 0), flow(2 * process.size(), 0);
+    vector<vector<array<int, 3>>> adj_list(id);
+    for (int i = 0; i < process.size(); i++) {
+        auto [u, v, w] = process[i];
+
+        edges[2 * i] = {u, v, w};
+        edges[2 * i + 1] = {v, u, -w};
+        capacity[2 * i] = 1;
+        adj_list[u].push_back({v, w, 2 * i});
+        adj_list[v].push_back({u, -w, 2 * i + 1});
+    }
 
     vector<int> potential(id, INT_MAX);
     potential[0] = 0;
 
     auto bellman_ford = [&]() {
-        for (int i = 0; i < id; i++)
-            for (auto [u, v, w, capacity, flow, index] : edges)
-                if (capacity != flow) potential[v] = min(potential[v], potential[u] + w);
+        for (int _ = 0; _ < id; _++)
+            for (int i = 0; i < edges.size(); i++) {
+                auto [u, v, w] = edges[i];
+                if (capacity[i] != flow[i]) potential[v] = min(potential[v], potential[u] + w);
+            }
     };
     bellman_ford();
 
@@ -108,13 +89,12 @@ int main() {
 
         if (v == 1) return true;
 
-        for (auto e : adj_list[v]) {
-            auto &[u, v, w, capacity, flow, index] = *e;
-            if (capacity == flow || potential[v] != potential[u] + w) continue;
+        for (auto [u, w, i] : adj_list[v]) {
+            if (capacity[i] == flow[i] || potential[u] != potential[v] + w) continue;
 
-            if (!visited[v] && self(self, v)) {
-                flow++;
-                edges[index ^ 1][4]--;
+            if (!visited[u] && self(self, u)) {
+                flow[i]++;
+                flow[i ^ 1]--;
 
                 sum += w;
                 return true;
@@ -125,7 +105,31 @@ int main() {
     };
 
     while (k--) {
-        dijkstra(adj_list, potential);
+        auto dijkstra = [&]() {
+            vector<int> dist(id, INT_MAX);
+            dist[0] = 0;
+
+            priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+            pq.emplace(0, 0);
+            while (!pq.empty()) {
+                auto [d, v] = pq.top();
+                pq.pop();
+
+                for (auto [u, w, i] : adj_list[v]) {
+                    if (capacity[i] == flow[i]) continue;
+
+                    if (dist[u] > d + potential[v] + w - potential[u]) {
+                        dist[u] = d + potential[v] + w - potential[u];
+                        pq.emplace(dist[u], u);
+                    }
+                }
+            }
+
+            for (int i = 0; i < id; i++)
+                if (dist[i] != INT_MAX) potential[i] += dist[i];
+        };
+
+        dijkstra();
 
         fill(visited.begin(), visited.end(), false);
         if (!dfs(dfs)) break;
