cb7851a5c0d22092a4eae418276dab9bde9557b8
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-10-04 09:48:20 +0800

    Update plankton.cpp

--- open/plankton.cpp
+++ open/plankton.cpp
@@ -39,7 +39,7 @@ int main() {
                 if (!visited[u]) self(self, u);
         };
         for (auto [u, v, w] : edges)
-            if (dist[u] != 1e20 && w + dist[u] < dist[v]) dfs(dfs, u);
+            if (dist[u] != 1e20 && dist[v] > dist[u] + w) dfs(dfs, u);
 
         cout << (visited[fn] ? "TRUE\n" : "FALSE\n");
     }
