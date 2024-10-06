d8968020c754adaea2fa3247715ef51627e54609
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-10-06 21:39:32 +0800

    Update committeeassignment.cpp

--- nus/cs3233/committeeassignment.cpp
+++ nus/cs3233/committeeassignment.cpp
@@ -1,24 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int dfs(int person, int committees, int &curr, vector<int> &hostile, vector<int> &members) {
-    if (curr <= committees) return curr;
-    if (person == hostile.size()) {
-        curr = min(curr, committees);
-        return committees;
-    }
-
-    int least = hostile.size();
-    for (int i = 0; i <= committees; i++)
-        if (!(hostile[person] & members[i])) {
-            members[i] |= 1 << person;
-            least = min(least, dfs(person + 1, committees + (i == committees), curr, hostile, members));
-            members[i] ^= 1 << person;
-        }
-
-    return least;
-}
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -26,17 +8,35 @@ int main() {
     int n, m;
     while (cin >> n >> m && n || m) {
         unordered_map<string, int> compress;
-        vector<int> hostile(n), members(n);
+        vector<int> hostile(n, 0);
         while (m--) {
             string a, b;
             cin >> a >> b;
-            
+
             int p1 = compress.count(a) ? compress[a] : compress[a] = compress.size(), p2 = compress.count(b) ? compress[b] : compress[b] = compress.size();
             hostile[p1] |= 1 << p2;
             hostile[p2] |= 1 << p1;
         }
 
-        int curr = n;
-        cout << dfs(0, 0, curr, hostile, members) << "\n";
+        vector<int> members(n, 0);
+        auto dfs = [&](auto &&self, int p = 0, int committees = 0) -> int {
+            if (n <= committees) return n;
+
+            if (p == hostile.size()) {
+                n = min(n, committees);
+                return committees;
+            }
+
+            int least = hostile.size();
+            for (int i = 0; i <= committees; i++)
+                if (!(hostile[p] & members[i])) {
+                    members[i] |= 1 << p;
+                    least = min(least, self(self, p + 1, committees + (i == committees)));
+                    members[i] ^= 1 << p;
+                }
+
+            return least;
+        };
+        cout << dfs(dfs) << "\n";
     }
 }
