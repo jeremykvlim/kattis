0f533142cee01bc4743807d361abe71d69f3d74e
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-10-06 22:14:51 +0800

    Update boxarrowdiagram.cpp

--- open/boxarrowdiagram.cpp
+++ open/boxarrowdiagram.cpp
@@ -1,32 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void bfs(int u, int v, vector<vector<int>> &adj_list, vector<bool> &visited, vector<int> &count) {
-    adj_list[u].emplace_back(v);
-    if (visited[u]) {
-        count[v]++;
-
-        if (!visited[v]) {
-            visited[v] = true;
-            queue<int> q;
-            q.emplace(v);
-            while (!q.empty()) {
-                v = q.front();
-                q.pop();
-
-                for (int w : adj_list[v]) {
-                    count[w]++;
-
-                    if (!visited[w]) {
-                        visited[w] = true;
-                        q.emplace(w);
-                    }
-                }
-            }
-        }
-    }
-}
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -49,21 +23,47 @@ int main() {
     }
 
     vector<vector<int>> adj_list(n);
+    vector<int> count(n, 0);
     vector<bool> visited(n, false);
-    vector<int> count(n);
     visited[0] = true;
+
+    auto bfs = [&](int v, int u) {
+        adj_list[v].emplace_back(u);
+        if (visited[v]) {
+            count[u]++;
+            if (visited[u]) return;
+
+            visited[u] = true;
+            queue<int> q;
+            q.emplace(u);
+            while (!q.empty()) {
+                u = q.front();
+                q.pop();
+
+                for (int t : adj_list[u]) {
+                    count[t]++;
+
+                    if (!visited[t]) {
+                        visited[t] = true;
+                        q.emplace(t);
+                    }
+                }
+            }
+        }
+    };
+
     for (int i = 0; i < m; i++)
-        if (!removed[i]) bfs(edges[i].first - 1, edges[i].second - 1, adj_list, visited, count);
+        if (!removed[i]) bfs(edges[i].first - 1, edges[i].second - 1);
 
     reverse(queries.begin(), queries.end());
-    stack<int> references;
+    stack<int> counts;
     for (auto &[c, x] : queries) {
-        if (c == 1) bfs(edges[x - 1].first - 1, edges[x - 1].second - 1, adj_list, visited, count);
-        else references.emplace(count[x - 1]);
+        if (c == 1) bfs(edges[x - 1].first - 1, edges[x - 1].second - 1);
+        else counts.emplace(count[x - 1]);
     }
 
-    while (!references.empty()) {
-        cout << references.top() << "\n";
-        references.pop();
+    while (!counts.empty()) {
+        cout << counts.top() << "\n";
+        counts.pop();
     }
 }
