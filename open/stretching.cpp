9d7d35ac61665a4562e46961c37e22107f193644
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-10-08 15:51:16 +0800

    Update stretching.cpp

--- open/stretching.cpp
+++ open/stretching.cpp
@@ -18,20 +18,20 @@ int main() {
             for (int j = 0; j <= n - i; j++, t++) {
                 auto sub = s.substr(j, i);
 
-                auto valid = [&](auto &&self, string sub, int k, int l, int r) -> bool {
+                auto valid = [&](auto &&self, string sub, int l, int r, int k = 0) -> bool {
                     if (l > r) return !k;
                     if (r - l + 1 - i + k < 0 || (r - l + 1 - i + k) % i || s[l] != sub[k]) return false;
                     if (visited[l][r][k] == t) return dp[l][r][k];
 
                     visited[l][r][k] = t;
-                    if (k == i - 1) return dp[l][r][k] = self(self, sub, 0, l + 1, r);
+                    if (k == i - 1) return dp[l][r][k] = self(self, sub, l + 1, r);
                     for (int j = l + 1; j <= r; j++)
                         if (s[j] == sub[k + 1])
-                            if (self(self, sub, 0, l + 1, j - 1) && self(self, sub, k + 1, j, r)) return dp[l][r][k] = true;
+                            if (self(self, sub, l + 1, j - 1) && self(self, sub, j, r, k + 1)) return dp[l][r][k] = true;
 
                     return dp[l][r][k] = false;
                 };
-                if (valid(valid, sub, 0, 0, n - 1)) p = !p[0] ? sub : min(p, sub);
+                if (valid(valid, sub, 0, n - 1)) p = !p[0] ? sub : min(p, sub);
             }
 
             if (p[0]) {
