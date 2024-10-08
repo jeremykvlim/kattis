1c51287554a8f7b8cac26b05919c15b06abb2cc0
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-10-08 15:30:53 +0800

    Update stack.cpp

--- nus/cs3233/stack.cpp
+++ nus/cs3233/stack.cpp
@@ -1,19 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int ops(vector<vector<int>> &dp, string &s, int i, int j) {
-    if (i > j) return 0;
-    if (dp[i][j] == -1) {
-        dp[i][j] = ops(dp, s, i, j - 1) + 1;
-
-        for (int k = i; k < j; k++)
-            if (s[k] == s[j])
-                dp[i][j] = min(dp[i][j], ops(dp, s, i, k) + ops(dp, s, k + 1, j - 1));
-    }
-
-    return dp[i][j];
-}
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -27,6 +14,18 @@ int main() {
         getline(cin, s);
 
         vector<vector<int>> dp(s.size(), vector<int>(s.size(), -1));
-        cout << s.size() + 2 * ops(dp, s, 0, s.size() - 1) << "\n";
+        auto ops = [&](auto &&self, int l, int r) -> int {
+            if (l > r) return 0;
+
+            if (dp[l][r] == -1) {
+                dp[l][r] = self(self, l, r - 1) + 1;
+
+                for (int m = l; m < r; m++)
+                    if (s[m] == s[r]) dp[l][r] = min(dp[l][r], self(self, l, m) + self(self, m + 1, r - 1));
+            }
+
+            return dp[l][r];
+        };
+        cout << s.size() + 2 * ops(ops, 0, s.size() - 1) << "\n";
     }
 }
