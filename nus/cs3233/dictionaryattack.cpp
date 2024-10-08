34640efc06cc3b7bf8d35c2b2c33672d55025851
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-10-08 16:39:38 +0800

    Update dictionaryattack.cpp

--- nus/cs3233/dictionaryattack.cpp
+++ nus/cs3233/dictionaryattack.cpp
@@ -1,21 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool acceptable(string &w, string &p, int diff, int digits, int changes) {
-    if (diff == digits) return digits > changes;
-    if (!changes || diff - digits > 2 * (changes - digits)) return true;
-
-    for (int i = 0; i < p.size() - 1; i++) {
-        int transpose = (w[i + 1] == p[i]) + (w[i] == p[i + 1]) - (w[i] == p[i]) - (w[i + 1] == p[i + 1]);
-
-        swap(p[i], p[i + 1]);
-        if (!acceptable(w, p, diff - transpose, digits, changes - 1)) return false;
-        else swap(p[i], p[i + 1]);
-    }
-
-    return true;
-}
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -37,7 +22,22 @@ int main() {
                 if (isdigit(p[i])) digits++;
             }
 
-            if (!acceptable(w, p, diff, digits, 3)) goto next;
+            auto acceptable = [&](auto &&self, int diff, int changes = 3) -> bool {
+                if (diff == digits) return digits > changes;
+                if (!changes || diff - digits > 2 * (changes - digits)) return true;
+
+                for (int i = 0; i < p.size() - 1; i++) {
+                    int transpose = (w[i + 1] == p[i]) + (w[i] == p[i + 1]) - (w[i] == p[i]) - (w[i + 1] == p[i + 1]);
+
+                    swap(p[i], p[i + 1]);
+                    if (!self(self, diff - transpose, changes - 1)) return false;
+                    swap(p[i], p[i + 1]);
+                }
+
+                return true;
+            };
+
+            if (!acceptable(acceptable, diff, 3)) goto next;
         }
 
         cout << p << "\n";
