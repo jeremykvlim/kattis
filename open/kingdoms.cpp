d64312fff9d1fcf919b11ad391b4f3943f6a0c20
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-10-08 16:18:08 +0800

    Update kingdoms.cpp

--- open/kingdoms.cpp
+++ open/kingdoms.cpp
@@ -1,19 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void dfs(int v, vector<bool> &visited, vector<long long> &sum, vector<vector<long long>> &gold) {
-    visited[v] = true;
-    for (int i = 0; i < sum.size(); i++)
-        if ((v & (1 << i)) && sum[i] < 0 && !visited[v - (1 << i)]) {
-            for (int j = 0; j < sum.size(); j++)
-                if (v & (1 << j)) sum[j] -= gold[i][j];
-
-            dfs(v - (1 << i), visited, sum, gold);
-            for (int j = 0; j < sum.size(); j++)
-                if (v & (1 << j)) sum[j] += gold[i][j];
-        }
-}
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -33,14 +20,32 @@ int main() {
         vector<long long> sum(n, 0);
         for (auto &row : gold)
             for (int i = 0; i < n; i++) sum[i] += row[i];
-        dfs((1 << n) - 1, visited, sum, gold);
 
-        bool survivors = false;
+        auto dfs = [&](auto &&self, int v) -> void {
+            visited[v] = true;
+            for (int i = 0; i < n; i++)
+                if ((v & (1 << i)) && sum[i] < 0 && !visited[v - (1 << i)]) {
+                    for (int j = 0; j < n; j++)
+                        if (v & (1 << j)) sum[j] -= gold[i][j];
+
+                    self(self, v - (1 << i));
+                    for (int j = 0; j < n; j++)
+                        if (v & (1 << j)) sum[j] += gold[i][j];
+                }
+        };
+
+        dfs(dfs, (1 << n) - 1);
+
+        vector<int> survivors;
         for (int i = 0; i < n; i++)
-            if (visited[1 << i]) {
-                cout << i + 1 << " ";
-                survivors = true;
-            }
-        cout << (survivors ? "\n" : "0\n");
+            if (visited[1 << i]) survivors.emplace_back(i + 1);
+
+        if (survivors.empty()) {
+            cout << "0\n";
+            continue;
+        }
+
+        for (int i : survivors) cout << i << " ";
+        cout << "\n";
     }
 }
