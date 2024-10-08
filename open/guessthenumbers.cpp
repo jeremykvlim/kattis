6280963673504f749a14aa21bbcc645ff58c7026
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-10-08 16:08:34 +0800

    Update guessthenumbers.cpp

--- open/guessthenumbers.cpp
+++ open/guessthenumbers.cpp
@@ -1,39 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int backtrack(int &i, int j, string expression, vector<int> values) {
-    if (expression[j] != '(') {
-        i = j + 1;
-        return values[expression[j] - '0'];
-    }
-    int e1 = backtrack(i, j + 1, expression, values);
-    char op = expression[i];
-    int e2 = backtrack(i, i + 1, expression, values);
-    i++;
-    return op == '+' ? e1 + e2 : op == '-' ? e1 - e2 : e1 * e2;
-}
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int n;
     while (cin >> n && n) {
-        int m;
         vector<int> values(n);
-        string expression;
-
         for (int &v : values) cin >> v;
+        sort(values.begin(), values.end());
+
+        int m;
+        string expression;
         cin >> m >> expression;
 
         unordered_map<char, int> unknowns;
         for (char &c : expression)
             if (isalpha(c)) c = unknowns.count(c) ? '0' + unknowns[c] : '0' + (unknowns[c] = unknowns.size());
-        sort(values.begin(), values.end());
 
         do {
             int i;
-            if (backtrack(i, 0, expression, values) == m) {
+            auto dfs = [&](auto &&self, int j = 0) -> int {
+                if (expression[j] != '(') {
+                    i = j + 1;
+                    return values[expression[j] - '0'];
+                }
+                int e1 = self(self, j + 1);
+                char op = expression[i];
+                int e2 = self(self, i + 1);
+                i++;
+                return op == '+' ? e1 + e2 : op == '-' ? e1 - e2 : e1 * e2;
+            };
+
+            if (dfs(dfs) == m) {
                 cout << "YES\n";
                 goto next;
             }
