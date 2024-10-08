0d6c08415028e32e557684d56245721ac38cea1c
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-10-08 15:24:39 +0800

    Update cubiccycle.cpp

--- open/cubiccycle.cpp
+++ open/cubiccycle.cpp
@@ -1,23 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int dfs(vector<vector<int>> &adj_list, vector<bool> &visited, int u) {
-    if (count(visited.begin(), visited.end(), true) + 1 == visited.size()) return !adj_list[u][0] || !adj_list[u][1] || !adj_list[u][2];
-
-    visited[u] = true;
-    int degree = 0, cycles = 0;
-    vector<int> neighbours(3, 0);
-    for (int v : adj_list[u])
-        if (!visited[v]) neighbours[degree++] = v;
-    for (int i = 0; i < degree; i++) {
-        if (degree == 2 && count_if(adj_list[neighbours[1 - i]].begin(), adj_list[neighbours[1 - i]].end(), [&](int u) {return u && visited[u];}) == 2) continue;
-        cycles += dfs(adj_list, visited, neighbours[i]);
-    }
-    visited[u] = false;
-
-    return cycles;
-}
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -36,5 +19,22 @@ int main() {
     }
 
     vector<bool> visited(n, false);
-    cout << dfs(adj_list, visited, 0) / 2;
+    auto dfs = [&](auto &&self, int v = 0) -> int {
+        if (count(visited.begin(), visited.end(), true) + 1 == visited.size()) return !adj_list[v][0] || !adj_list[v][1] || !adj_list[v][2];
+
+        visited[v] = true;
+        int degree = 0, cycles = 0;
+        vector<int> neighbours(3, 0);
+        for (int u : adj_list[v])
+            if (!visited[u]) neighbours[degree++] = u;
+
+        for (int i = 0; i < degree; i++) {
+            if (degree == 2 && count_if(adj_list[neighbours[1 - i]].begin(), adj_list[neighbours[1 - i]].end(), [&](int u) {return u && visited[u];}) == 2) continue;
+            cycles += self(self, neighbours[i]);
+        }
+        visited[v] = false;
+
+        return cycles;
+    };
+    cout << dfs(dfs) / 2;
 }
