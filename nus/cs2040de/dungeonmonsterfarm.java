bfe872d2f98a542bc33ca79778ba64df6e49a6bb
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-10-18 21:27:00 +0800

    Update dungeonmonsterfarm.java

--- nus/cs2040de/dungeonmonsterfarm.java
+++ nus/cs2040de/dungeonmonsterfarm.java
@@ -16,81 +16,81 @@ public class dungeonmonsterfarm {
             }
         }
     }
-}
 
-class Monster {
-    int type, daysRaised, weight, dailyWeightGain;
+    static class Monster {
+        int type, daysRaised, weight, dailyWeightGain;
 
-    Monster(int t, int w, int dwg) {
-        type = t;
-        daysRaised = 0;
-        weight = w;
-        dailyWeightGain = dwg;
-    }
-}
-
-class Farm implements AutoCloseable {
-    int coins, capacity;
-    ArrayList<Monster> monsters;
-
-    final int[] buyingPrice = {5, 20, 84}, initialWeight = {1, 2, 4}, sellingPrice = {1, 4, 6};
-
-    Farm(int X, int N) {
-        coins = X;
-        capacity = N;
-        monsters = new ArrayList<>();
-    }
-
-    void print() {
-        System.out.println(coins + " " + capacity);
-    }
-
-    int buyMonster(int type, int count, int dailyWeightGain) {
-        int cost = count * buyingPrice[type];
-        if (coins < cost || capacity < count) return monsters.size();
-
-        coins -= cost;
-        capacity -= count;
-        while (count-- > 0) monsters.add(new Monster(type, initialWeight[type], dailyWeightGain));
-        print();
-        return monsters.size();
-    }
-
-    int sellMonster(int type, int count) {
-        var list = new ArrayList<Monster>();
-        for (var m : monsters)
-            if (m.type == type) list.add(m);
-
-        if (list.size() < count) return coins;
-
-        list.sort((a, b) -> b.weight - a.weight);
-        int profit = 0, sold = 0;
-        for (var m : list) {
-            profit += m.weight * sellingPrice[m.type];
-            monsters.remove(m);
-            if (++sold == count) break;
+        Monster(int t, int w, int dwg) {
+            type = t;
+            daysRaised = 0;
+            weight = w;
+            dailyWeightGain = dwg;
         }
-        capacity += sold;
-        coins += profit;
-        print();
-        return coins;
     }
 
-    void raiseMonster(int days) {
-        for (var m : monsters) {
-            m.weight += m.dailyWeightGain * days;
-            m.daysRaised += days;
+    static class Farm implements AutoCloseable {
+        int coins, capacity;
+        ArrayList<Monster> monsters;
+
+        final int[] buyingPrice = {5, 20, 84}, initialWeight = {1, 2, 4}, sellingPrice = {1, 4, 6};
+
+        Farm(int X, int N) {
+            coins = X;
+            capacity = N;
+            monsters = new ArrayList<>();
         }
-        print();
-    }
 
-    @Override
-    public void close() {
-        int profit = 0;
-        for (var m : monsters) profit += m.weight * sellingPrice[m.type];
-        capacity += monsters.size();
-        monsters.clear();
-        coins += profit;
-        print();
+        void print() {
+            System.out.println(coins + " " + capacity);
+        }
+
+        int buyMonster(int type, int count, int dailyWeightGain) {
+            int cost = count * buyingPrice[type];
+            if (coins < cost || capacity < count) return monsters.size();
+
+            coins -= cost;
+            capacity -= count;
+            while (count-- > 0) monsters.add(new Monster(type, initialWeight[type], dailyWeightGain));
+            print();
+            return monsters.size();
+        }
+
+        int sellMonster(int type, int count) {
+            var list = new ArrayList<Monster>();
+            for (var m : monsters)
+                if (m.type == type) list.add(m);
+
+            if (list.size() < count) return coins;
+
+            list.sort((a, b) -> b.weight - a.weight);
+            int profit = 0, sold = 0;
+            for (var m : list) {
+                profit += m.weight * sellingPrice[m.type];
+                monsters.remove(m);
+                if (++sold == count) break;
+            }
+            capacity += sold;
+            coins += profit;
+            print();
+            return coins;
+        }
+
+        void raiseMonster(int days) {
+            for (var m : monsters) {
+                m.weight += m.dailyWeightGain * days;
+                m.daysRaised += days;
+            }
+            print();
+        }
+
+        @Override
+        public void close() {
+            int profit = 0;
+            for (var m : monsters) profit += m.weight * sellingPrice[m.type];
+            capacity += monsters.size();
+            monsters.clear();
+            coins += profit;
+            print();
+        }
     }
 }
