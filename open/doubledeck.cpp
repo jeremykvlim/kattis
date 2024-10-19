a33ef0812507cfce431823348c5fbc13feb0477f
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-10-20 06:37:56 +0800

    Create doubledeck.cpp

---
+++ open/doubledeck.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename T>
+struct FenwickTree {
+    vector<T> BIT;
+    function<T(T, T)> f;
+
+    void update(int i, T value) {
+        for (; i < BIT.size(); i += i & -i) BIT[i] = f(BIT[i], value);
+    }
+
+    T range_query(int i) {
+        T value = 0;
+        for (; i; i &= (i - 1)) value = f(value, BIT[i]);
+        return value;
+    }
+
+    FenwickTree(int n, function<T(T, T)> func) : BIT(n, 0), f(move(func)) {}
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, k;
+    cin >> n >> k;
+
+    vector<deque<int>> indices(n + 1);
+    for (int i = 0; i < n * k; i++) {
+        int xi;
+        cin >> xi;
+
+        indices[xi].emplace_front(i);
+    }
+
+    FenwickTree<int> fw(n * k + 1, [](int x, int y) {return max(x, y);});
+    for (int i = 0; i < n * k; i++) {
+        int xi;
+        cin >> xi;
+
+        for (int j : indices[xi]) fw.update(j + 1, fw.range_query(j) + 1);
+    }
+
+    cout << fw.range_query(n * k);
+}
