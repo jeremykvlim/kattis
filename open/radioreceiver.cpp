be2bdc4b05536f8a41489891eb23464c064fbf7d
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-10-29 18:16:31 +0800

    Update radioreceiver.cpp

--- open/radioreceiver.cpp
+++ open/radioreceiver.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
+    
     int c;
     cin >> c;
 
@@ -12,38 +12,13 @@ int main() {
         int n;
         cin >> n;
 
-        int bound = -1;
-        vector<pair<int, int>> messages(n);
-        for (auto &[p, t] : messages) {
-            cin >> p >> t;
+        vector<int> p(n), t(n);
+        for (int i = 0; i < n; i++) cin >> p[i] >> t[i];
 
-            bound = max({bound, p, t});
-        }
-        sort(messages.begin(), messages.end(), [](auto p1, auto p2) {return p1.second < p2.second;});
+        int d = 0;
+        for (int i = 0; i < n; i++) 
+            for (int j = 0; j < i; j++) d = max(d, abs(p[i] - p[j]) - abs(t[i] - t[j]));
         
-        double l = 0, r = bound, m;
-        while (l + 1e-9 < r && l + l * 1e-9 < r) {
-            m = l + (r - l) / 2;
-
-            auto possible = [&]() -> bool {
-                double l1 = messages[0].first - m, r1 = messages[0].first + m;
-                for (int i = 1; i < n; i++) {
-                    int t = messages[i].second - messages[i - 1].second;
-                    double l2 = messages[i].first - m, r2 = messages[i].first + m;
-
-                    if (l1 - t > r2 || r1 + t < l2) return false;
-
-                    l1 = max(l1 - t, l2);
-                    r1 = min(r1 + t, r2);
-                }
-
-                return true;
-            };
-            
-            if (possible()) r = m;
-            else l = m;
-        }
-
-        cout << "Case #" << x << ": " << fixed << setprecision(9) << m << "\n";
+        cout << "Case #" << x << ": " << fixed << setprecision(9) << d / 2.0 << "\n";
     }
 }
