3dfc810ef85c2561e8d1be955ec39870e6b6d0fc
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-11-04 20:20:51 +0800

    Update pieceofcake.cpp

--- open/pieceofcake.cpp
+++ open/pieceofcake.cpp
@@ -25,17 +25,15 @@ int main() {
     for (auto &[x, y] : points) cin >> x >> y;
 
     vector<vector<double>> ways(n, vector<double>(n, -1));
-    double lg_nck = 0, lg_kci = 0;
-    for (int i = 0; i < k; i++) lg_nck += log10(n - i) - log10(i + 1);
-    for (int i = 0; i < n - k + 1; i++) {
-        for (int j = 0; j < n; j++) ways[j][(n - k + 1 - i + j) % n] = lg_kci;
-        lg_kci += log10(k + i - 1) - log10(i + 1);
+    for (int i = 0; i <= n - k; i++) {
+        auto lg_kci = lgamma(k + i - 1) - lgamma(k - 1) - lgamma(i + 1);
+        for (int j = 0; j < n; j++) ways[j][(j - k + 1 - i + n) % n] = lg_kci;
     }
 
-    double area = 0;
+    double area = 0, lg_nck = lgamma(n + 1) - lgamma(k + 1) - lgamma(n - k + 1);
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
-            if (ways[i][j] != -1) area += pow(10, ways[i][j] - lg_nck) * cross(points[i], points[j]);
+            if (ways[i][j] != -1) area += exp(ways[i][j] - lg_nck) * cross(points[i], points[j]);
 
     cout << fixed << setprecision(6) << abs(area) / 2;
 }
