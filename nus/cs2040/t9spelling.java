9e5bb61a513cb3630199bccb9f561ff86810fbf0
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-11-06 15:05:00 +0800

    Update t9spelling.java

--- nus/cs2040/t9spelling.java
+++ nus/cs2040/t9spelling.java
@@ -1,46 +1,21 @@
-import java.io.*;
+import java.util.*;
 
-public class t9spelling {
-    static int prev = -1;
-    
-    public static void main(String[] args) throws IOException {
-        var br = new BufferedReader(new InputStreamReader(System.in));
+public class Main {
+    public static void main(String[] args) {
+        var sc = new Scanner(System.in);
 
-        int n = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(sc.nextLine());
+        String[] keypad = {"+ ", "", " abc", " def", " ghi", " jkl", " mno", " pqrs", " tuv", " wxyz"};
         for (int x = 1; x <= n; x++) {
-            int num, repetitions;
-            System.out.print("Case #" + x + ": ");
-            var line = br.readLine().toCharArray();
-            for (char c : line) {
-                if (c == ' ') {
-                    repetitions = 1;
-                    num = 0;
-                    press(repetitions, num);
-                } else if (c == 's') {
-                    repetitions = 4;
-                    num = 7;
-                    press(repetitions, num);
-                } else if (c == 'z') {
-                    repetitions = 4;
-                    num = 9;
-                    press(repetitions, num);
-                } else if (c < 's') {
-                    repetitions = (c - 'a') % 3 + 1;
-                    num = (c - 'a') / 3 + 2;
-                    press(repetitions, num);
-                } else if (c < 'z') {
-                    repetitions = (c - 'a' - 1) % 3 + 1;
-                    num = (c - 'a' - 1) / 3 + 2;
-                    press(repetitions, num);
-                }
-            }
-            System.out.println();
+            var s = new StringBuilder("Case #" + x + ": ");
+            for (char c : sc.nextLine().toCharArray())
+                for (int i = 0; i < keypad.length; i++)
+                    if (keypad[i].indexOf(c) != -1) {
+                        if (s.length() > 0 && Character.getNumericValue(s.charAt(s.length() - 1)) == i) s.append(' ');
+                        s.append(String.valueOf(i).repeat(keypad[i].indexOf(c)));
+                        break;
+                    }
+            System.out.println(s);
         }
-    }
-
-    static void press(int repetitions, int num) {
-        if (prev == num) System.out.print(" ");
-        while (repetitions-- > 0) System.out.print(num);
-        prev = num;
     }
 }
