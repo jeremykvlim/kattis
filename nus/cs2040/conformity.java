bbd4c78f0f5b23d81509a9208aca784766718a39
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-11-06 16:10:41 +0800

    Update conformity.java

--- nus/cs2040/conformity.java
+++ nus/cs2040/conformity.java
@@ -7,14 +7,13 @@ public class conformity {
         var br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        var map = new HashMap<String, Integer>();
-
+        var count = new HashMap<String, Integer>();
         while (n-- > 0) {
             var s = Arrays.stream(br.readLine().split(" ")).sorted().collect(Collectors.joining(""));
-            map.put(s, map.getOrDefault(s, 0) + 1);
+            count.put(s, count.getOrDefault(s, 0) + 1);
         }
 
-        int most = map.values().stream().max(Integer::compare).orElse(0);
-        System.out.println(map.values().stream().filter(integer -> integer == most).mapToInt(i -> i).sum());
+        int most = count.values().stream().max(Integer::compare).orElse(0);
+        System.out.println(count.values().stream().filter(integer -> integer == most).mapToInt(i -> i).sum());
     }
 }
