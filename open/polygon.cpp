07c0eb5306430a285ca690911d2fea32833a03b2
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-11-07 21:24:21 +0800

    Update polygon.cpp

--- open/polygon.cpp
+++ open/polygon.cpp
@@ -36,15 +36,15 @@ int main() {
 
     int n;
     while (cin >> n && n) {
-        vector<Point<int>> points(2 * n + 1);
+        vector<Point<int>> points(2 * n);
         for (int i = 0; i < n; i++) cin >> points[i].x >> points[i].y;
         points[n] = points[n - 1];
-        for (int i = n; ~i; i--) {
+        for (int i = n - 1; ~i; i--) {
             if (i) points[i + n] = points[i - 1];
             if (points[i + n] < points[i]) swap(points[i + n], points[i]);
         }
 
-        vector<int> indices(2 * n + 1);
+        vector<int> indices(2 * n);
         iota(indices.begin(), indices.end(), 0);
         sort(indices.begin(), indices.end(), [&](int i, int j) {return points[i] < points[j] || points[i] == points[j] && i > j;});
 
@@ -53,8 +53,8 @@ int main() {
             return cross_product < 0 || !cross_product && (cross(points[i] - points[i + n], points[j] - points[j + n])) < 0;
         };
         multiset<int, decltype(cmp)> ms(cmp);
-        vector<decltype(ms)::const_iterator> its(n + 1);
-        vector<bool> valid(n + 1, false);
+        vector<decltype(ms)::const_iterator> its(n);
+        vector<bool> valid(n, false);
         auto check = [&](int i, int j) {
             int c1 = cross(points[i] - points[i + n], points[i] - points[j + n]);
             if (c1 < 0) return false;
@@ -62,8 +62,8 @@ int main() {
             int c2 = cross(points[j] - points[j + n], points[j] - points[i + n]);
             if (c2 > 0) return false;
 
-            if (c1 || c2) return points[i] != points[j];
-            else return !(cross(points[i] - points[i + n], points[j] - points[j + n]));
+            if (c1 || c2 ? points[i] != points[j] : !(cross(points[i] - points[i + n], points[j] - points[j + n]))) return true;
+            return false;
         };
         for (int i : indices)
             if (i < n) {
