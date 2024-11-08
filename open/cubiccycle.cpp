8ce05780dfd53ea4f58fc2d23994aeecbcb6f272
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-11-08 16:00:43 +0800

    Update cubiccycle.cpp

--- open/cubiccycle.cpp
+++ open/cubiccycle.cpp
@@ -24,13 +24,13 @@ int main() {
 
         visited[v] = true;
         int degree = 0, cycles = 0;
-        vector<int> neighbours(3, 0);
+        vector<int> neighbors(3, 0);
         for (int u : adj_list[v])
-            if (!visited[u]) neighbours[degree++] = u;
+            if (!visited[u]) neighbors[degree++] = u;
 
         for (int i = 0; i < degree; i++) {
-            if (degree == 2 && count_if(adj_list[neighbours[1 - i]].begin(), adj_list[neighbours[1 - i]].end(), [&](int u) {return u && visited[u];}) == 2) continue;
-            cycles += self(self, neighbours[i]);
+            if (degree == 2 && count_if(adj_list[neighbors[1 - i]].begin(), adj_list[neighbors[1 - i]].end(), [&](int u) {return u && visited[u];}) == 2) continue;
+            cycles += self(self, neighbors[i]);
         }
         visited[v] = false;
 
