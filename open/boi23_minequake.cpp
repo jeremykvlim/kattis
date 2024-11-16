b070b0723ff8b0aecff086a0630b01f149230977
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-11-17 05:41:12 +0800

    Update boi23_minequake.cpp

--- open/boi23_minequake.cpp
+++ open/boi23_minequake.cpp
@@ -34,17 +34,16 @@ int main() {
         spilled = min(spilled, dp[v]);
         for (int u : adj_list[v])
             if (u != prev) {
-                int su = subtree_size[u], sv = subtree_size[v];
-                auto du = dp[u], dv = dp[v];
+                int temp1 = subtree_size[u], temp2 = subtree_size[v];
+                auto temp3 = dp[u], temp4 = dp[v];
+                
                 subtree_size[v] -= subtree_size[u];
                 dp[v] -= dp[u] + subtree_size[u] + 2LL * (subtree_size[v] - 1) * subtree_size[u];
                 dp[u] += dp[v] + subtree_size[v] + 2LL * (subtree_size[u] - 1) * subtree_size[v];
                 subtree_size[u] += subtree_size[v];
                 self(self, u, v);
-                subtree_size[u] = su;
-                subtree_size[v] = sv;
-                dp[u] = du;
-                dp[v] = dv;
+                
+                tie(subtree_size[u], subtree_size[v], dp[u], dp[v]) = tie(temp1, temp2, temp3, temp4);
             }
     };
     dfs2(dfs2);
