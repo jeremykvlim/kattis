b38f818dc78d6c6f4260bf1e37d6acd00b9ea125
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-11-17 19:10:36 +0800

    Create histogrami.cpp

---
+++ open/histogrami.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+struct Point {
+    T x, y;
+
+    Point() {}
+    Point(T x, T y) : x(x), y(y) {}
+};
+
+template <typename T>
+struct FenwickTree {
+    vector<T> BIT;
+
+    void update(int i, T v) {
+        for (; i < BIT.size(); i += i & -i) BIT[i] += v;
+    }
+
+    T pref_sum(int i) {
+        T sum = 0;
+        for (; i; i &= (i - 1)) sum += BIT[i];
+        return sum;
+    }
+
+    FenwickTree(int n) : BIT(n, 0) {}
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m, g;
+    cin >> n >> m >> g;
+
+    int max_x_h = -1, max_y_h = -1, max_x_s = -1, max_y_s = -1;
+    vector<Point<int>> h(n), s(m);
+    for (auto &[x, y] : h) {
+        cin >> x >> y;
+
+        max_x_h = max(max_x_h, x);
+        max_y_h = max(max_y_h, y);
+    }
+    for (auto &[x, y] : s) {
+        cin >> x >> y;
+
+        max_x_s = max(max_x_s, x);
+        max_y_s = max(max_y_s, y);
+    }
+
+    vector<int> y_h(max_x_h + 1, 0);
+    for (int i = 1; i < n; i += 2)
+        for (int x = h[i - 1].x + 1; x <= h[i].x; x++) y_h[x] = h[i].y;
+
+    vector<vector<int>> y_s(max_x_s + 1);
+    for (int i = 0; i < m; i++) y_s[s[i].x].emplace_back(s[i].y);
+
+    int w = h[n - 1].x;
+    FenwickTree<int> fw_count(max(max_y_h, max_y_s) + 1);
+    FenwickTree<long long> fw_sum(max(max_y_h, max_y_s) + 1);
+    vector<Point<int>> p(w + 1);
+    vector<int> indices(max_y_s + 1, -1), lower_count(max_y_s + 1, 0), upper_count(max_y_s + 1, 0);
+    vector<long long> d(w + 1, 1e18), lower_sum(max_y_s + 1, 0), upper_sum(max_y_s + 1, 0);
+    d[0] = 0;
+    for (int y : y_s[0]) indices[y] = 0;
+    for (int x = 1; x <= w; x++) {
+        fw_count.update(y_h[x], 1);
+        fw_sum.update(y_h[x], y_h[x]);
+
+        for (int y : y_s[x]) {
+            int X = indices[y];
+
+            int lc = fw_count.pref_sum(y - 1), uc = fw_count.pref_sum(max_y_h) - fw_count.pref_sum(y);
+            auto ls = fw_sum.pref_sum(y - 1), us = fw_sum.pref_sum(max_y_h) - fw_sum.pref_sum(y);
+
+            if (~X) {
+                long long delta = g & 1 ? lc - lower_count[y] + uc - upper_count[y] 
+                                        : us - upper_sum[y] - ls + lower_sum[y] + (long long) y * (lc - lower_count[y]) - (long long) y * (uc - upper_count[y]);
+
+                if (d[x] > d[X] + delta) {
+                    d[x] = d[X] + delta;
+                    p[x] = {X, y};
+                }
+            }
+            indices[y] = x;
+            lower_count[y] = lc;
+            upper_count[y] = uc;
+            lower_sum[y] = ls;
+            upper_sum[y] = us;
+        }
+    }
+
+    cout << d[w] << "\n";
+    vector<Point<int>> h_;
+    for (Point<int> curr{w, 0}, prev; curr.x; curr = prev) {
+        prev = p[curr.x];
+
+        if (curr.y == prev.y) h_.back().x = prev.x;
+        else {
+            h_.emplace_back(curr.x, prev.y);
+            h_.emplace_back(prev);
+        }
+    }
+    reverse(h_.begin(), h_.end());
+    cout << h_.size() << "\n";
+    for (auto [x, y] : h_) cout << x << " " << y << "\n";
+}
