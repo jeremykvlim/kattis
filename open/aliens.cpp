b8938578a76e1092e80651b04a1a55a4651f97e1
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-11-28 00:07:32 +0800

    Update aliens.cpp

--- open/aliens.cpp
+++ open/aliens.cpp
@@ -1,12 +1,111 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template <typename T>
-pair<T, T> bezout(T a, T b) {
-    if (!a) return {0, 1};
-    auto [x, y] = bezout(b % a, a);
-    return {y - (b / a) * x, x};
-}
+struct SuffixArray {
+    vector<int> SA, ascii, SA_inv;
+
+    vector<int> sais(vector<int> &ascii1, int range) {
+        int n = ascii1.size();
+        if (!n) return {};
+        if (n == 1) return {0};
+        if (n == 2) ascii1[0] < ascii1[1] ? vector<int>{0, 1} : vector<int>{1, 0};
+
+        vector<int> sa(n, 0), sum_s(range + 1, 0), sum_l(range + 1, 0);
+        vector<bool> sl(n, false);
+        for (int i = n - 2; ~i; i--) sl[i] = (ascii1[i] == ascii1[i + 1]) ? sl[i + 1] : (ascii1[i] < ascii1[i + 1]);
+        for (int i = 0; i < n; i++) {
+            if (!sl[i]) sum_s[ascii1[i]]++;
+            else sum_l[ascii1[i] + 1]++;
+        }
+
+        for (int i = 0; i <= range; i++) {
+            sum_s[i] += sum_l[i];
+            if (i < range) sum_l[i + 1] += sum_s[i];
+        }
+
+        auto induced_sort = [&](vector<int> &lms) {
+            fill(sa.begin(), sa.end(), -1);
+            vector<int> b(range + 1, 0);
+            copy(sum_s.begin(), sum_s.end(), b.begin());
+            for (int i : lms)
+                if (i < n) sa[b[ascii1[i]]++] = i;
+
+            copy(sum_l.begin(), sum_l.end(), b.begin());
+            sa[b[ascii1[n - 1]]++] = n - 1;
+            for (int j : sa)
+                if (j > 0 && !sl[j - 1]) sa[b[ascii1[j - 1]]++] = j - 1;
+
+            copy(sum_l.begin(), sum_l.end(), b.begin());
+            for (int i = n - 1, j = sa[i]; ~i; j = sa[--i])
+                if (j > 0 && sl[j - 1]) sa[--b[ascii1[j - 1] + 1]] = j - 1;
+        };
+
+        vector<int> lms_map(n + 1, -1), lms;
+        int m = 0;
+        for (int i = 1; i < n; i++)
+            if (!sl[i - 1] && sl[i]) {
+                lms_map[i] = m++;
+                lms.emplace_back(i);
+            }
+        induced_sort(lms);
+
+        if (m) {
+            vector<int> lms_sorted, ascii2(m);
+            for (int j : sa)
+                if (lms_map[j] != -1) lms_sorted.emplace_back(j);
+
+            int range2 = 0;
+            ascii2[lms_map[lms_sorted[0]]] = 0;
+            for (int i = 1; i < m; i++) {
+                int l = lms_sorted[i - 1], r = lms_sorted[i], l_end = (lms_map[l] + 1 < m) ? lms[lms_map[l] + 1] : n, r_end = (lms_map[r] + 1 < m) ? lms[lms_map[r] + 1] : n;
+                bool same = true;
+                if (l_end - l != r_end - r) same = false;
+                else {
+                    while (l < l_end && ascii1[l] == ascii1[r]) {
+                        l++;
+                        r++;
+                    }
+
+                    if (l == n || ascii1[l] != ascii1[r]) same = false;
+                }
+
+                if (!same) range2++;
+                ascii2[lms_map[lms_sorted[i]]] = range2;
+            }
+
+            auto sa2 = sais(ascii2, range2);
+            for (int i = 0; i < m; i++) lms_sorted[i] = lms[sa2[i]];
+            induced_sort(lms_sorted);
+        }
+
+        return sa;
+    }
+
+    vector<int> kasai() {
+        int n = ascii.size();
+        vector<int> lcp(n);
+        SA_inv.resize(n);
+        for (int i = 0; i < n; i++) SA_inv[SA[i]] = i;
+        for (int i = 0, k = 0; i < n; i++) {
+            if (k) k--;
+            if (!SA_inv[i]) continue;
+
+            int j = SA[SA_inv[i] - 1];
+            while (i + k < n && j + k < n && ascii[i + k] == ascii[j + k]) k++;
+            lcp[SA_inv[i] - 1] = k;
+        }
+        lcp.back() = n;
+        return lcp;
+    }
+
+    int & operator[](int i) {
+        return SA[i];
+    }
+
+    SuffixArray(string &s, int r = 128) : ascii(s.begin(), s.end()) {
+        SA = sais(ascii, r);
+    };
+};
 
 int main() {
     ios::sync_with_stdio(false);
@@ -17,48 +116,38 @@ int main() {
         string s;
         cin >> s;
 
-        vector<__int128> pref(s.size() + 1, 0), inv(s.size() + 1, 1);
-        __int128 base = 1, b = (long long) 1e16 + 61, mod = (1ULL << 63) + 27;
-        for (int i = 0; i < s.size(); i++) {
-            pref[i + 1] = (pref[i] + base * (s[i] - 'a' + 1)) % mod;
-            base = (base * b) % mod;
-
-            auto [x, y] = bezout(base, mod);
-            if (x < 0) x += mod;
-            inv[i + 1] = x;
-        }
-
-        auto pos = [&](int len) -> int {
-            vector<vector<pair<long long, int>>> table(4e4);
-            auto appearances = [&](long long k) -> int {
-                int pos = k % table.size();
-                for (auto &[key, count] : table[pos])
-                    if (key == k) return ++count;
-
-                table[pos].emplace_back(k, 1);
-                return 1;
-            };
-
-            int pos = -1;
-            for (int i = 0, j = i + len - 1; i < s.size() && j < s.size(); i++, j++)
-                if (appearances(((pref[j + 1] - pref[i] + mod) % mod) * inv[i] % mod) >= m) pos = i;
-
-            return pos;
-        };
-
-        if (pos(1) == -1) {
-            cout << "none\n";
+        int n = s.size();
+        if (m == 1) {
+            cout << n << " 0\n";
             continue;
         }
 
-        int l = 0, r = s.size() + 1, mid;
-        while (l + 1 < r) {
-            mid = l + (r - l) / 2;
+        SuffixArray sa(s);
+        auto lcp = sa.kasai();
+        deque<int> mono_lcp, mono_sa;
+        int len = 0, pos = -1;
+        for (int i = 0; i < n; i++) {
+            if (i) {
+                while (!mono_lcp.empty() && lcp[i - 1] <= lcp[mono_lcp.back()]) mono_lcp.pop_back();
+                mono_lcp.emplace_back(i - 1);
+            }
+            while (!mono_sa.empty() && sa[i] >= sa[mono_sa.back()]) mono_sa.pop_back();
+            mono_sa.emplace_back(i);
 
-            if (pos(mid) == -1) r = mid;
-            else l = mid;
+            if (i >= m - 1) {
+                while (!mono_lcp.empty() && mono_lcp.front() <= i - m) mono_lcp.pop_front();
+                while (!mono_sa.empty() && mono_sa.front() <= i - m) mono_sa.pop_front();
+
+                if (!mono_lcp.empty()) {
+                    if (len < lcp[mono_lcp.front()]) {
+                        len = lcp[mono_lcp.front()];
+                        pos = sa[mono_sa.front()];
+                    } else if (len == lcp[mono_lcp.front()]) pos = max(pos, sa[mono_sa.front()]);
+                }
+            }
         }
-
-        cout << l << " " << pos(l) << "\n";
+        
+        if (!len) cout << "none\n";
+        else cout << len << " " << pos << "\n";
     }
 }
