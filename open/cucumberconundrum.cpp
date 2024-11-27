3e2a481f1fee8eec97cf22cd5c55fac213ee74f9
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-11-28 02:49:25 +0800

    Create cucumberconundrum.cpp

---
+++ open/cucumberconundrum.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    double s, r;
+    int n, z;
+    cin >> s >> r >> n >> z;
+
+    int count = 0, bbb;
+    while (s >= 2 * r) {
+        count += (int) (M_PI / asin(r / (s - r)));
+        s -= 2 * r;
+    }
+    if (s >= r) count++;
+    cout << min({n, count, (int) (s * s * z / (100 * r * r))});
+}
