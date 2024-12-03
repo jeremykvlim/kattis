13d82a07c33d5bf5002aa563c62ae7a848e43750
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-12-04 04:03:16 +0800

    Update onewayroads.cpp

--- open/onewayroads.cpp
+++ open/onewayroads.cpp
@@ -1,6 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<pair<int, int>> tarjan(int n, vector<vector<int>> &adj_list) {
+    int count = 0;
+    vector<int> order(n + 1), low(n + 1);
+    vector<pair<int, int>> non_bridges;
+    auto dfs = [&](auto &&self, int v = 1, int prev = -1) -> void {
+        order[v] = low[v] = ++count;
+        for (int u : adj_list[v])
+            if (u != prev) {
+                if (!order[u]) {
+                    self(self, u, v);
+                    low[v] = min(low[v], low[u]);
+
+                    if (low[u] > order[v]) {
+                        cout << "NO\n";
+                        exit(0);
+                    }
+                } else {
+                    low[v] = min(low[v], order[u]);
+                    if (order[u] > order[v]) continue;
+                }
+                non_bridges.emplace_back(v, u);
+            }
+    };
+    dfs(dfs);
+    
+    return non_bridges;
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -17,28 +45,7 @@ int main() {
         adj_list[y].emplace_back(x);
     }
 
-    int count = 0;
-    vector<int> order(n + 1), low(n + 1);
-    vector<pair<int, int>> roads;
-    auto dfs = [&](auto &&self, int v = 1, int prev = -1) -> void {
-        order[v] = low[v] = ++count;
-        for (int u : adj_list[v])
-            if (u != prev) {
-                if (!order[u]) {
-                    self(self, u, v);
-                    low[v] = min(low[v], low[u]);
-                    if (low[u] > order[v]) {
-                        cout << "NO\n";
-                        exit(0);
-                    }
-                } else {
-                    low[v] = min(low[v], order[u]);
-                    if (order[u] > order[v]) continue;
-                }
-                roads.emplace_back(v, u);
-            }
-    };
-    dfs(dfs);
+    auto roads = tarjan(n, adj_list);
 
     cout << "YES\n";
     for (auto [x, y] : roads) cout << x << " " << y << "\n";
