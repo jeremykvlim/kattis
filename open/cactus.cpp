7283d4aa7c34d736eaaf892706ab084a1454ed83
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-12-03 18:30:47 +0800

    Update cactus.cpp

--- open/cactus.cpp
+++ open/cactus.cpp
@@ -1,37 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void tarjan(int v, vector<int> &order, vector<int> &low, vector<bool> &stacked, int &count, int &sccs, bool &valid, vector<vector<int>> &adj_list, stack<int> &s) {
-    if (!valid) return;
+int tarjan(int n, vector<vector<int>> &adj_list) {
+    vector<int> order(n + 1, 0), low(n + 1, 0);
+    vector<bool> stacked(n + 1, false);
+    stack<int> st;
+    int count = 0, sccs = 0;
 
-    order[v] = low[v] = ++count;
-    s.emplace(v);
-    stacked[v] = true;
+    auto dfs = [&](auto &&self, int v) -> void {
+        order[v] = low[v] = ++count;
+        st.emplace(v);
+        stacked[v] = true;
 
-    int simple = 0;
-    for (int &u : adj_list[v])
-        if (!order[u]) {
-            tarjan(u, order, low, stacked, count, sccs, valid, adj_list, s);
-            low[v] = min(low[v], low[u]);
-            if (low[u] != order[v]) simple++;
-        } else if (stacked[u]) {
-            low[v] = min(low[v], order[u]);
-            simple++;
+        int simple = 0;
+        for (int u : adj_list[v])
+            if (!order[u]) {
+                self(self, u);
+                low[v] = min(low[v], low[u]);
+                if (low[u] != order[v]) simple++;
+            } else if (stacked[u]) {
+                low[v] = min(low[v], order[u]);
+                simple++;
+            }
+
+        if (simple > 1) {
+            cout << "NO";
+            exit(0);
         }
-    if (simple > 1) {
-        valid = false;
-        return;
-    }
 
-    if (order[v] == low[v]) {
-        sccs++;
-        int u;
-        do {
-            u = s.top();
-            s.pop();
-            stacked[u] = false;
-        } while (u != v);
-    }
+        if (order[v] == low[v]) {
+            sccs++;
+            int u;
+            do {
+                u = st.top();
+                st.pop();
+                stacked[u] = false;
+            } while (u != v);
+        }
+    };
+
+    for (int v = 1; v <= n; v++)
+        if (!order[v]) dfs(dfs, v);
+
+    return sccs;
 }
 
 int main() {
@@ -41,21 +52,13 @@ int main() {
     int n, m;
     cin >> n >> m;
 
-    vector<vector<int>> adj_list(n);
+    vector<vector<int>> adj_list(n + 1);
     while (m--) {
         int u, v;
         cin >> u >> v;
 
-        adj_list[u].emplace_back(v);
+        adj_list[u + 1].emplace_back(v + 1);
     }
-
-    vector<int> order(n, 0), low(n, 0);
-    vector<bool> stacked(n, false);
-    stack<int> s;
-    int count = 0, sccs = 0;
-    bool valid = true;
-    for (int i = 0; i < n && valid; i++)
-        if (!order[i]) tarjan(i, order, low, stacked, count, sccs, valid, adj_list, s);
-
-    cout << (sccs == 1 && valid ? "YES" : "NO");
+    
+    cout << (tarjan(n, adj_list) == 1 ? "YES" : "NO");
 }
