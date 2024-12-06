f19da88fd454eccc14d196b5e7d30aa9cf85a44b
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-12-06 19:48:27 +0800

    Create primitiveroots.cpp

---
+++ open/primitiveroots.cpp
@@ -0,0 +1,123 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+T mul(T x, T y, T mod) {
+    long long px = x, py = y, pmod = mod;
+    auto product = px * py - pmod * (long long) (1.L / pmod * px * py);
+    return product + pmod * (product < 0) - pmod * (product >= pmod);
+}
+
+template <typename T>
+T pow(T base, T exponent, T mod) {
+    T value = 1;
+    while (exponent) {
+        if (exponent & 1) value = mul(value, base, mod);
+        base = mul(base, base, mod);
+        exponent >>= 1;
+    }
+    return value;
+}
+
+bool isprime(long long n) {
+    if (n < 2) return false;
+    if (n % 6 % 4 != 1) return (n | 1) == 3;
+
+    vector<int> bases{2, 325, 9375, 28178, 450775, 9780504, 1795265022};
+    int s = __builtin_ctzll(n - 1);
+    auto d = n >> s;
+    for (int a : bases) {
+        auto p = pow(a % n, d, n);
+        int i = s;
+        while (1 < p && p < n - 1 && a % n && i--) p = mul(p, p, n);
+        if (p != n - 1 && i != s) return false;
+    }
+
+    return true;
+}
+
+long long brent(long long n) {
+    if (!(n & 1)) return 2;
+
+    mt19937_64 rng(random_device{}());
+    for (;;) {
+        auto x = 2LL, y = 2LL, g = 1LL, q = 1LL, xs = 1LL, c = (long long) rng() % (n - 1) + 1;
+        for (int i = 1; g == 1; i <<= 1, y = x) {
+            for (int j = 1; j < i; j++) x = mul(x, x, n) + c;
+            for (int j = 0; j < i && g == 1; j += 128) {
+                xs = x;
+                for (int k = 0; k < min(128, i - j); k++) {
+                    x = mul(x, x, n) + c;
+                    q = mul(q, abs(x - y), n);
+                }
+                g = __gcd(q, n);
+            }
+        }
+
+        if (g == n) g = 1;
+        while (g == 1) {
+            xs = mul(xs, xs, n) + c;
+            g = __gcd(abs(xs - y), n);
+        }
+        if (g != n) return isprime(g) ? g : brent(g);
+    }
+}
+
+vector<long long> factorize(long long n) {
+    vector<long long> pfs;
+
+    auto dnc = [&](auto &&self, long long m) -> void {
+        if (m < 2) return;
+        if (isprime(m)) {
+            pfs.emplace_back(m);
+            return;
+        }
+
+        auto d = brent(m);
+        self(self, d);
+        self(self, m / d);
+    };
+    dnc(dnc, n);
+
+    return pfs;
+}
+
+long long primitive_root(long long m) {
+    if (m == 1) return 0;
+    if (m == 2) return 1;
+    if (m == 4) return 3;
+    if (!(m % 4)) return -1;
+
+    auto p = m & 1 ? m : m / 2;
+    auto pfs = factorize(p);
+    sort(pfs.begin(), pfs.end());
+    bool composite = pfs.size() > 1;
+    if (composite && unique(pfs.begin(), pfs.end()) == pfs.end()) return -1;
+
+    auto q = pfs[0];
+    auto phi = p / q * (q - 1);
+    pfs = factorize(q - 1);
+
+    if (composite) pfs.emplace_back(q);
+    sort(pfs.begin(), pfs.end());
+    pfs.erase(unique(pfs.begin(), pfs.end()), pfs.end());
+    for (auto g = 2LL; g < m; g++) {
+        if (gcd(g, m) != 1) continue;
+
+        for (auto pf : pfs)
+            if (pow(g, phi / pf, m) == 1) goto next;
+
+        return g;
+        next:;
+    }
+    return -1;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long long m;
+    cin >> m;
+    cout << primitive_root(m);
+}
