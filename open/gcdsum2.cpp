f9dc2f9756f72bedaac5a538628ee4d43ff5a8ec
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-12-08 19:53:40 +0800

    Update gcdsum2.cpp

--- open/gcdsum2.cpp
+++ open/gcdsum2.cpp
@@ -1,29 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<long long> phi(int n) {
-    vector<long long> phi(n + 1, 0);
+vector<int> totients(int n) {
     vector<bool> prime(n + 1, true);
-    vector<int> primes;
+    vector<int> phis(n + 1, 0), primes;
     for (int i = 2; i <= n; i++) {
         if (prime[i]) {
             primes.emplace_back(i);
-            phi[i] = i - 1;
+            phis[i] = i - 1;
         }
 
         for (int j = 0; primes[j] <= n / i; j++) {
             int k = i * primes[j];
             prime[k] = false;
-            phi[k] = phi[i] * phi[primes[j]];
 
             if (!(i % primes[j])) {
-                phi[k] = phi[i] * primes[j];
+                phis[k] = phis[i] * primes[j];
                 break;
             }
+            phis[k] = phis[i] * phis[primes[j]];
         }
     }
 
-    return phi;
+    return phis;
 }
 
 int main() {
@@ -33,13 +32,15 @@ int main() {
     int n;
     cin >> n;
 
-    auto totient = phi(n);
-    for (int i = 1; i <= n; i++) totient[i] += totient[i - 1];
-    
+    auto phis = totients(n);
+
+    vector<long long> pref{phis.begin(), phis.end()};
+    for (int i = 1; i <= n; i++) pref[i] += pref[i - 1];
+
     auto sum = 0LL;
     for (int l = 1, r; l <= n; l = r + 1) {
         r = n / (n / l);
-        sum += totient[n / l] * (l + r) * (r - l + 1) / 2;
+        sum += pref[n / l] * (l + r) * (r - l + 1) / 2;
     }
 
     cout << sum;
