6b746ae70dda28f2d39f9c62519ef589d555c0cc
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2024-12-23 14:50:34 +0800

    Update bst.java

--- nus/cs2040/bst.java
+++ nus/cs2040/bst.java
@@ -2,23 +2,33 @@ import java.io.*;
 import java.util.*;
 
 public class bst {
-    public static void main(String[] args) throws IOException {
+    public static void main(String args[]) throws IOException {
         var br = new BufferedReader(new InputStreamReader(System.in));
         var pw = new PrintWriter(System.out);
         
         int n = Integer.parseInt(br.readLine());
-        var tree = new TreeSet<Integer, Integer>();
-        long total = 0;
-        while (n-- > 0) {
-            int i = Integer.parseInt(br.readLine()), c = 0;
-            Integer floor = tree.floorKey(i), ceiling = tree.ceilingKey(i);
+        int[] seq = new int[n], l = new int[n + 2], r = new int[n + 2], pref = new int[n + 2];
+              
+        for (int i = 0; i < n; i++) seq[i] = Integer.parseInt(br.readLine());
+        for (int i = n - 1; i >= 0; i--) {
+            int x = seq[i];
+            l[x] = r[x] = x;
             
-            if (floor == null && ceiling != null) c = tree.get(ceiling) + 1;
-            else if (ceiling == null && floor != null) c = tree.get(floor) + 1;
-            else if (ceiling != null) c = Math.max(tree.get(ceiling), tree.get(floor)) + 1;
-
-            tree.put(i, c);
-            total += c;
+            if (x > 1 && l[x - 1] != 0) l[x] = l[x - 1];
+            if (x + 1 <= n && r[x + 1] != 0) r[x] = r[x + 1];
+            
+            r[l[x]] = r[x];
+            l[r[x]] = l[x];
+            
+            pref[l[x]]++;
+            pref[r[x] + 1]--;
+        }
+        
+        for (int i = 2; i <= n; i++) pref[i] += pref[i - 1];
+        
+        long total = 0;
+        for (int x : seq) {
+            total += pref[x] - 1;
             pw.println(total);
         }
         pw.flush();
