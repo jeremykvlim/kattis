6fd1dc1a7529002294ea85a2b0143af7b23bf9ee
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-01-05 05:45:48 +0800

    Update debellatio.cpp

--- nus/cs3233/debellatio.cpp
+++ nus/cs3233/debellatio.cpp
@@ -1,8 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct DisjointSets {
-    vector<int> sets, parent;
+struct PersistentDisjointSets {
+    vector<int> sets;
+    deque<pair<int, int>> history;
 
     int find(int p) {
         return (sets[p] == p) ? p : (sets[p] = find(sets[p]));
@@ -12,214 +13,273 @@ struct DisjointSets {
         int p_set = find(p), q_set = find(q);
         if (p_set != q_set) {
             sets[q_set] = p_set;
-            parent[q] = p;
+            history.emplace_back(q_set, q_set);
             return true;
         }
         return false;
     }
 
-    DisjointSets(int n) : sets(n), parent(n, -1) {
+    int record() {
+        return history.size();
+    }
+
+    void undo(int version = 0) {
+        while (record() > version) {
+            sets[history.back().first] = history.back().second;
+            history.pop_back();
+        }
+    }
+
+    void delete_history(int version = 0) {
+        history.resize(version);
+    }
+
+    void reset() {
+        iota(sets.begin(), sets.end(), 0);
+    }
+
+    PersistentDisjointSets(int n) : sets(n) {
         iota(sets.begin(), sets.end(), 0);
     }
 };
 
-pair<int, vector<int>> micali_vazirani(int n, vector<pair<int, int>> edges) {
-    vector<int> match(n, -1);
+struct ForwardStar {
+    vector<int> head, next;
 
-    vector<vector<array<int, 3>>> adj_list(n);
-    for (auto [u, v] : edges) {
-        adj_list[u].push_back({v, (int) adj_list[v].size(), 0});
-        adj_list[v].push_back({u, (int) adj_list[u].size() - 1, 0});
+    ForwardStar() {}
+    ForwardStar(int n, int m) : head(n, -1), next(m) {}
+    void add_edge(int u, int edge_id) {
+        next[edge_id] = head[u];
+        head[u] = edge_id;
     }
+};
 
-    bool change;
-    do {
-        change = false;
-        for (auto &al : adj_list)
-            for (auto &[u, index, label] : al) label = 0;
+pair<int, vector<int>> gabow(int n, vector<pair<int, int>> edges) {
+    vector<pair<int, int>> adj_list(2 * edges.size());
+    vector<int> degree(n + 2, 0);
+    for (auto [u, v] : edges) {
+        degree[u + 1]++;
+        degree[v + 1]++;
+    }
+    for (int i = 1; i < n + 2; i++) degree[i] += degree[i - 1];
+    for (auto [u, v] : edges) {
+        adj_list[degree[u]++] = {u, v};
+        adj_list[degree[v]++] = {v, u};
+    }
+    for (int i = n + 1; i; i--) degree[i] = degree[i - 1];
+    degree[0] = 0;
 
-        vector<int> even(n, 1e9), odd(n, 1e9);
-        vector<vector<int>> vertices(n);
-        auto set_level = [&](int v, int level) {
-            if (level & 1) odd[v] = level;
-            else even[v] = level;
+    int h = n / 2;
+    vector<int> match(n + 1, 0), label(n + 1, 0), potential(n + 1, 1);
+    vector<pair<int, int>> link(n + 1, {0, 0});
+    ForwardStar list(h + 1, adj_list.size()), blossom(n + 1, n + 1);
+    PersistentDisjointSets pdsu(n + 1);
+    int matches = 0;
+    while (2 * matches < n - 1) {
+        int outer = 1, p_augment = INT_MAX, p_curr = 0, count = 0, version = 0;
+        queue<int> q;
+        for (int u = 1; u <= n; u++) {
+            if (!match[u]) {
+                q.emplace(u);
+                label[u] = u;
+            } else label[u] = 0;
+        }
 
-            vertices[level].emplace_back(v);
+        auto make_outer = [&](int x, int y, int z, int p) {
+            link[z] = {x, y};
+            label[z] = label[x];
+            potential[z] = p_curr + 1 + p;
+            q.emplace(z);
         };
 
-        for (int u = 0; u < n; u++)
-            if (match[u] == -1) set_level(u, 0);
-
-        auto min_level = [&](int v) {
-            return min(even[v], odd[v]);
+        auto grow = [&](int x, int y, int z) {
+            label[y] = -1;
+            potential[y] = p_curr;
+            make_outer(x, y, z, 0);
         };
 
-        auto tenacity = [&](int u, int v) {
-            if (match[u] == v) return odd[u] + odd[v] + 1;
-            return even[u] + even[v] + 1;
-        };
+        auto contract = [&](int x, int y) {
+            int base_x = pdsu.find(x), base_y = pdsu.find(y), l = -(++count + 1);
+            label[match[base_x]] = label[match[base_y]] = l;
 
-        DisjointSets dsu(n);
-        int mark_count = 1;
-        vector<int> count(n, 0), mark(n, 0), indices(n, 0);
-        vector<bool> visited(n, false);
-        vector<vector<int>> predecessors(n);
-        vector<vector<pair<int, int>>> bridges(2 * (n + 1)), children(n);
-        vector<pair<pair<int, int>, pair<int, int>>> support_bridge(n);
-        for (int i = 0; i < n && !change; i++) {
-            for (int v : vertices[i])
-                for (auto &[u, index, label] : adj_list[v])
-                    if (!label && !((odd[v] == i) ^ (match[v] == u))) {
-                        if (min_level(u) >= i + 1) {
-                            label = 1;
-                            adj_list[u][index][2] = 1;
+            int lca;
+            for (;;) {
+                if (match[base_y]) swap(base_x, base_y);
+                base_x = lca = pdsu.find(link[base_x].first);
+                if (label[match[base_x]] == l) break;
+                label[match[base_x]] = l;
+            }
 
-                            if (min_level(u) > i + 1) set_level(u, i + 1);
-                            predecessors[u].emplace_back(v);
-                        } else {
-                            label = 2;
-                            adj_list[u][index][2] = 2;
+            base_x = pdsu.find(x), base_y = pdsu.find(y);
+            for (int base : {base_x, base_y})
+                for (; base != lca; base = pdsu.sets[link[base].first]) {
+                    int z = match[base];
+                    make_outer(x, y, z, p_curr - potential[z]);
 
-                            int j = tenacity(u, v);
-                            if (j < 2 * (n + 1)) bridges[j].emplace_back(v, u);
-                        }
-                    }
-
-            for (auto [s, t] : bridges[2 * i + 1]) {
-                int vl = dsu.find(s), vr = dsu.find(t);
-                if (visited[vl] || visited[vr] || vl == vr) continue;
-
-                vector<int> support{vl, vr};
-                auto double_dfs = [&]() -> pair<int, int> {
-                    vector<int> tl{vl}, tr{vr};
-
-                    mark[vl] = ++mark_count;
-                    mark[vr] = ++mark_count;
-                    for (;;) {
-                        if (!min_level(tl.back()) && !min_level(tr.back())) return {tl.back(), tr.back()};
-
-                        auto dfs = [&](bool left) {
-                            auto &t1 = left ? tl : tr, &t2 = left ? tr : tl;
-                            int m = left ? mark[vl] : mark[vr], v = t1.back();
-                            for (int &k = indices[v]; k < predecessors[v].size(); k++) {
-                                int u = predecessors[v][k];
-                                if (visited[u]) continue;
-
-                                int u_set = dsu.find(u);
-                                if (!mark[u_set]) {
-                                    mark[u_set] = m;
-                                    t1.emplace_back(u_set);
-                                    support.emplace_back(u_set);
-                                    children[v].emplace_back(u, u_set);
-                                    return -1;
-                                } else if (u_set == t2.back()) children[v].emplace_back(u, u_set);
-                            }
-                            t1.pop_back();
-
-                            if (t1.empty()) {
-                                if (t2.size() == 1) {
-                                    mark[t2[0]] = 0;
-                                    return t2[0];
-                                }
-
-                                mark[t2.back()] = m;
-                                t1.emplace_back(t2.back());
-                                t2.pop_back();
-                            }
-                            return -1;
-                        };
-
-                        int bottleneck = min_level(tl.back()) >= min_level(tr.back()) ? dfs(true) : dfs(false);
-                        if (bottleneck != -1) return {bottleneck, bottleneck};
-                    }
-                };
-                auto [x, y] = double_dfs();
-
-                for (int v : support) {
-                    if (v == y) continue;
-
-                    support_bridge[v] = {{s, t}, {vl, vr}};
-                    dsu.unite(y, v);
-                    if (x != y) continue;
-
-                    set_level(v, 2 * i + 1 - min_level(v));
-                    for (auto [u, index, label] : adj_list[v])
-                        if (even[v] > odd[v] && label == 2 && match[v] != u) {
-                            int j = tenacity(u, v);
-                            if (j < 2 * (n + 1)) bridges[j].emplace_back(v, u);
-                        }
+                    pdsu.sets[base] = pdsu.sets[z] = lca;
+                    pdsu.history.emplace_back(base, lca);
+                    pdsu.history.emplace_back(z, lca);
                 }
-                if (x == y) continue;
+        };
 
-                queue<int> q;
-                auto augment = [&](int u, int v) {
-                    if (visited[u] || visited[v] || match[u] == v || match[v] == u) return;
+        for (;;) {
+            while (!q.empty()) {
+                int x = q.front(), l_x = label[x], p_x = potential[x], base_x = pdsu.find(x);
+                q.pop();
 
-                    visited[u] = visited[v] = true;
-                    q.emplace(u);
-                    q.emplace(v);
-                    match[u] = v;
-                    match[v] = u;
-                };
+                for (int i = degree[x]; i < degree[x + 1]; i++) {
+                    int y = adj_list[i].second, l_y = label[y], p_y = potential[y], base_y = pdsu.find(y);
 
-                bool first = true;
-                auto find_path = [&](auto &&self1, int x, int y) {
-                    if (x == y) return;
-
-                    if (!first && odd[x] > even[x]) {
-                        int p = predecessors[x][0], k = 0;
-                        while (dsu.find(predecessors[p][k]) != dsu.find(p)) k++;
-                        x = predecessors[p][k];
-                        augment(p, x);
-                    } else {
-                        first = false;
-
-                        auto [a, b] = support_bridge[x].first;
-                        auto [a_set, b_set] = support_bridge[x].second;
-                        if (mark[x] == (mark[a_set] ^ 1) || mark[x] == mark[b_set]) {
-                            swap(a, b);
-                            swap(a_set, b_set);
+                    if (l_y > 0) {
+                        int p_mid = (p_x + p_y) / 2;
+                        if (l_x != l_y) {
+                            if (p_mid == p_curr) goto done;
+                            p_augment = min(p_augment, p_mid);
+                        } else {
+                            if (base_x == base_y) continue;
+                            if (p_mid == p_curr) {
+                                contract(x, y);
+                                base_x = pdsu.find(x);
+                            } else if (p_mid <= h) list.add_edge(p_mid, i);
                         }
-                        augment(a, b);
-
-                        auto dfs = [&](auto &&self2, int v, int v_set) {
-                            if (v_set == x) {
-                                self1(self1, v, v_set);
-                                return true;
-                            }
-
-                            for (auto [u, u_set] : children[v_set])
-                                if ((u_set == x || mark[u_set] == mark[v_set]) && self2(self2, u, u_set)) {
-                                    self1(self1, v, v_set);
-                                    augment(v_set, u);
-                                    return true;
-                                }
-                            return false;
-                        };
-                        dfs(dfs, a, a_set);
-                        x = dsu.parent[x];
-                        dfs(dfs, b, b_set);
+                    } else if (!l_y) {
+                        int p_next = p_x + 1;
+                        if (p_next == p_curr) grow(x, y, match[y]);
+                        else if (p_next <= h) list.add_edge(p_next, i);
                     }
-                    self1(self1, x, y);
-                };
-                find_path(find_path, x, y);
-                change = true;
-                
-                while (!q.empty()) {
-                    int v = q.front();
-                    q.pop();
-
-                    for (auto [u, index, label] : adj_list[v])
-                        if (label == 1 && min_level(u) > min_level(v) && !visited[u] && ++count[u] == predecessors[u].size()) {
-                            visited[u] = true;
-                            q.emplace(u);
-                        }
                 }
             }
-        }
-    } while (change);
 
-    int matches = (n - count(match.begin(), match.end(), -1)) / 2;
+            int limit = min(h + 1, p_augment);
+            for (p_curr++; p_curr <= limit; p_curr++) {
+                version = pdsu.record();
+                if (p_curr == limit) break;
+
+                bool updated = false;
+                for (int i = list.head[p_curr]; ~i; i = list.next[i]) {
+                    auto [x, y] = adj_list[i];
+                    int l_x = label[x], p_x = potential[x], base_x = pdsu.find(x),
+                            l_y = label[y], p_y = potential[y], base_y = pdsu.find(y);
+
+                    if (l_y > 0) {
+                        if (p_curr != (p_x + p_y) / 2 || base_x == base_y) continue;
+                        if (l_x != l_y) {
+                            p_augment = p_curr;
+                            goto next;
+                        } else {
+                            contract(x, y);
+                            updated = true;
+                        }
+                    } else if (!l_y) {
+                        int z = match[y];
+                        grow(x, y, z);
+                        updated = true;
+                    }
+                }
+                list.head[p_curr] = -1;
+                if (updated) goto next;
+            }
+            if (p_curr > h) return {matches, match};
+            next:;
+            if (p_curr == p_augment) break;
+        }
+        done:;
+
+        for (int u = 1; u <= n; u++) {
+            if (label[u] > 0) potential[u] -= p_curr;
+            else if (label[u] < 0) potential[u] = p_curr + 1 - potential[u];
+        }
+        pdsu.reset();
+        pdsu.delete_history(version);
+        pdsu.undo();
+
+        for (int u = 1; u <= n; u++) {
+            label[u] = 0;
+            blossom.add_edge(pdsu.find(u), u);
+        }
+
+        stack<int> s;
+        auto dfs = [&](auto &&self1, int x, int base_x) -> bool {
+            int l_base_x = label[base_x], p_x = potential[x];
+            for (int i = degree[x]; i < degree[x + 1]; i++) {
+                int y = adj_list[i].second, p_y = potential[y];
+                if (p_x != -p_y) continue;
+
+                int base_y = pdsu.find(y), l_base_y = label[base_y];
+                if (l_base_y > 0) {
+                    if (l_base_x >= l_base_y) continue;
+                    int size = s.size();
+                    for (int base = base_y; base != base_x; base = pdsu.find(link[base].first)) {
+                        int z = match[base], base_z = pdsu.find(z);
+                        link[base_z] = {x, y};
+                        pdsu.sets[base] = pdsu.sets[base_z] = base_x;
+                        s.emplace(base_z);
+                    }
+
+                    while (s.size() > size) {
+                        int base_v = s.top();
+                        s.pop();
+
+                        for (int v = blossom.head[base_v]; ~v; v = blossom.next[v]) {
+                            if (!self1(self1, v, base_x)) continue;
+                            while (s.size() > size) s.pop();
+                            return true;
+                        }
+                    }
+                } else if (!l_base_y) {
+                    label[base_y] = -1;
+                    int z = match[base_y];
+
+                    auto rematch = [&](auto &&self2, int u, int v) {
+                        int t = match[u];
+                        match[u] = v;
+                        if (match[t] != u) return;
+
+                        auto [a, b] = link[u];
+                        if (pdsu.find(b) == b) {
+                            match[t] = a;
+                            self2(self2, match[t], t);
+                        } else {
+                            self2(self2, a, b);
+                            self2(self2, b, a);
+                        }
+                    };
+
+                    if (!z) {
+                        rematch(rematch, x, y);
+                        rematch(rematch, y, x);
+                        return true;
+                    }
+
+                    int base_z = pdsu.find(z);
+                    link[base_z] = {x, y};
+                    label[base_z] = outer++;
+                    for (int v = blossom.head[base_z]; ~v; v = blossom.next[v])
+                        if (self1(self1, v, base_z)) return true;
+                }
+            }
+            return false;
+        };
+
+        for (int u = 1; u <= n; u++)
+            if (!match[u]) {
+                int base_u = pdsu.find(u);
+                if (label[base_u]) continue;
+
+                label[base_u] = outer++;
+                for (int v = blossom.head[base_u]; ~v; v = blossom.next[v]) {
+                    if (!dfs(dfs, v, base_u)) continue;
+                    matches++;
+                    break;
+                }
+            }
+
+        pdsu.reset();
+        pdsu.delete_history();
+        fill(potential.begin(), potential.end(), 1);
+        fill(list.head.begin(), list.head.end(), -1);
+        fill(blossom.head.begin(), blossom.head.end(), -1);
+    }
     return {matches, match};
 }
 
@@ -230,33 +290,20 @@ int main() {
     int n, m;
     cin >> n >> m;
 
-    vector<pair<int, int>> edges;
-    while (m--) {
-        int a, b;
-        cin >> a >> b;
-        a--;
-        b--;
+    vector<pair<int, int>> edges(m);
+    for (auto &[a, b] : edges) cin >> a >> b;
 
-        edges.emplace_back(a, b);
-    }
-
-    auto [matches, match] = micali_vazirani(n, edges);
+    auto [matches, match] = gabow(n, edges);
     if (matches * 2 == n) cout << "2\n" << flush;
     else {
         cout << "1\n" << flush;
-        for (int a = 0; a < n; a++)
-            if (match[a] == -1) {
-                cout << a + 1 << "\n" << flush;
+        for (int a = 1; a <= n; a++)
+            if (match[a] == 0) {
+                cout << a << "\n" << flush;
                 break;
             }
     }
 
-    for (;;) {
-        int a;
-        cin >> a;
-
-        if (!a) exit(0);
-
-        cout << match[a - 1] + 1 << "\n" << flush;
-    }
+    int a;
+    while (cin >> a && a) cout << match[a] << "\n" << flush;
 }
