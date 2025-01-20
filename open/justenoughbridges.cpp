479f5f3860cfb27735690156fd2d3a7562ec5073
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-01-21 01:48:06 +0800

    Create justenoughbridges.cpp

---
+++ open/justenoughbridges.cpp
@@ -0,0 +1,126 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Hash {
+    size_t operator()(pair<int, int> p) const {
+        auto h = hash<int>()(p.first);
+        h ^= hash<int>()(p.second) + 0x9e3779b9 + (h << 6) + (h >> 2);
+        return h;
+    }
+};
+
+tuple<vector<int>, vector<int>, int> hopcroft_karp(int n, int m, vector<pair<int, int>> edges) {
+    vector<int> adj_list(edges.size()), l(n, -1), r(m, -1), degree(n + 1, 0);
+    for (auto [u, v] : edges) degree[u]++;
+    for (int i = 1; i <= n; i++) degree[i] += degree[i - 1];
+    for (auto [u, v] : edges) adj_list[--degree[u]] = v;
+
+    int matches = 0;
+    vector<int> src(n), prev(n);
+    queue<int> q;
+    for (;;) {
+        fill(src.begin(), src.end(), -1);
+        fill(prev.begin(), prev.end(), -1);
+
+        for (int i = 0; i < n; i++)
+            if (!~l[i]) q.emplace(src[i] = prev[i] = i);
+
+        int temp = matches;
+        while (!q.empty()) {
+            int v = q.front();
+            q.pop();
+
+            if (~l[src[v]]) continue;
+
+            for (int j = degree[v]; j < degree[v + 1]; j++) {
+                int u = adj_list[j];
+
+                if (!~r[u]) {
+                    while (~u) {
+                        r[u] = v;
+                        swap(l[v], u);
+                        v = prev[v];
+                    }
+
+                    matches++;
+                    break;
+                }
+
+                if (!~prev[r[u]]) {
+                    q.emplace(u = r[u]);
+                    prev[u] = v;
+                    src[u] = src[v];
+                }
+            }
+        }
+
+        if (temp == matches) return {l, r, matches};
+    }
+}
+
+pair<int, vector<int>> tarjan(int n, vector<vector<int>> &adj_list) {
+    vector<int> order(n + 1, 0), low(n + 1, 0), component(n + 1, 0);
+    vector<bool> stacked(n + 1, false);
+    stack<int> st;
+    int count = 0, sccs = 0;
+
+    auto dfs = [&](auto &&self, int v) -> void {
+        order[v] = low[v] = ++count;
+        st.emplace(v);
+        stacked[v] = true;
+
+        for (int u : adj_list[v])
+            if (!order[u]) {
+                self(self, u);
+                low[v] = min(low[v], low[u]);
+            } else if (stacked[u]) low[v] = min(low[v], order[u]);
+
+        if (order[v] == low[v]) {
+            sccs++;
+            int u;
+            do {
+                u = st.top();
+                st.pop();
+                stacked[u] = false;
+                component[u] = sccs;
+            } while (u != v);
+        }
+    };
+
+    for (int v = 1; v <= n; v++)
+        if (!order[v]) dfs(dfs, v);
+
+    return {sccs, component};
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<pair<int, int>> edges(m);
+    for (auto &[u, v] : edges) cin >> u >> v;
+
+    auto [l, r, matches] = hopcroft_karp(n + 1, n + 1, edges);
+    if (matches != n) {
+        cout << 0;
+        exit(0);
+    }
+
+    vector<vector<int>> adj_list(2 * n + 1);
+    unordered_set<pair<int, int>, Hash> seen;
+    for (auto [u, v] : edges) {
+        if (l[u] == v && !seen.count({u, v})) {
+            adj_list[v + n].emplace_back(u);
+            seen.emplace(u, v);
+        } else adj_list[u].emplace_back(v + n);
+    }
+    auto [sccs, component] = tarjan(2 * n, adj_list);
+    int bridges = m - n;
+    for (int i = 1; i <= n; i++)
+        if (component[i] == component[l[i] + n]) bridges++;
+
+    cout << bridges;
+}
