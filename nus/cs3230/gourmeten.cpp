e56a6b5798bc3816a984213b0300b8eec5d6397e
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-02-03 18:20:30 +0800

    Update gourmeten.cpp

--- nus/cs3230/gourmeten.cpp
+++ nus/cs3230/gourmeten.cpp
@@ -8,10 +8,11 @@ int main() {
     int m, n;
     cin >> m >> n;
 
-    vector<int> dp(m + 1, 0), t(n);
-    dp[0] = 1;
+    vector<int> t(n);
     for (int &ti : t) cin >> ti;
 
+    vector<int> dp(m + 1, 0);
+    dp[0] = 1;
     for (int i = 1; i <= m; i++)
         for (int ti : t)
             if (ti <= i) dp[i] += dp[i - ti];
