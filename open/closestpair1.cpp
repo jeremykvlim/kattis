b603f05d50683736b9bf8b72203d02aa2b12e46b
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-02-07 17:47:35 +0800

    Update closestpair1.cpp

--- open/closestpair1.cpp
+++ open/closestpair1.cpp
@@ -116,6 +116,54 @@ struct Point {
     };
 };
 
+template <typename T>
+double dist(const Point<T> &a, const Point<T> &b) {
+    return sqrt((double) (a.x - b.x) * (a.x - b.x) + (double) (a.y - b.y) * (a.y - b.y));
+}
+
+template <typename T>
+double squared_dist(const Point<T> &a, const Point<T> &b) {
+    return (double) (a.x - b.x) * (a.x - b.x) + (double) (a.y - b.y) * (a.y - b.y);
+}
+
+template <typename T>
+pair<pair<int, int>, T> closest_pair(const vector<Point<T>> &points) {
+    int n = points.size();
+
+    vector<pair<Point<T>, int>> sorted(n);
+    for (int i = 0; i < n; i++) sorted[i] = {points[i], i};
+    sort(sorted.begin(), sorted.end(), [](auto p1, auto p2) {return p1.first == p2.first ? p1.second < p2.second : p1.first < p2.first;});
+
+    auto d = DBL_MAX;
+    int a = -1, b = -1;
+    auto update = [&](auto p1, auto p2) {
+        auto dist = squared_dist(p1.first, p2.first);
+        if (d > dist) {
+            d = dist;
+            a = p1.second;
+            b = p2.second;
+        }
+    };
+
+    auto sq = [](double v) -> double {return v * v;};
+    auto cmp = [](auto p1, auto p2) {return p1.first.y < p2.first.y;};
+    multiset<pair<Point<T>, int>, decltype(cmp)> ms(cmp);
+    vector<typename decltype(ms)::const_iterator> its(n);
+    for (int i = 0, j = 0; i < n; i++) {
+        for (; j < i && sq(sorted[j].first.x - sorted[i].first.x) >= d; j++) ms.erase(its[j]);
+
+        auto it = ms.upper_bound(sorted[i]);
+        if (it != ms.begin()) {
+            update(*prev(it), sorted[i]);
+            for (auto p = prev(it); p != ms.begin() && sq(sorted[i].first.y - p->first.y) < d;) update(*(--p), sorted[i]);
+        }
+        for (; it != ms.end() && sq(it->first.y - sorted[i].first.y) < d; it++) update(*it, sorted[i]);
+        its[i] = ms.emplace_hint(ms.upper_bound(sorted[i]), sorted[i]);
+    }
+
+    return {{a, b}, dist(points[a], points[b])};
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -123,42 +171,9 @@ int main() {
     int n;
     while (cin >> n && n) {
         vector<Point<double>> points(n);
-        vector<pair<Point<double>, int>> sorted(n);
-        for (int i = 0; i < n; i++) {
-            cin >> points[i].x >> points[i].y;
-
-            sorted[i] = {points[i], i};
-        }
-        sort(sorted.begin(), sorted.end(), [](auto p1, auto p2) {return p1.first == p2.first ? p1.second < p2.second : p1.first < p2.first;});
-
-        auto sq = [](double v) -> double {return v * v;};
-
-        auto d = DBL_MAX;
-        int a = -1, b = -1;
-        auto update = [&](auto p1, auto p2) {
-            auto dist = sq(p1.first.x - p2.first.x) + sq(p1.first.y - p2.first.y);
-            if (d > dist) {
-                d = dist;
-                a = p1.second;
-                b = p2.second;
-            }
-        };
-
-        auto cmp = [](auto p1, auto p2) {return p1.first.y < p2.first.y;};
-        multiset<pair<Point<double>, int>, decltype(cmp)> ms(cmp);
-        vector<decltype(ms)::const_iterator> its(n);
-        for (int i = 0, j = 0; i < n; i++) {
-            for (; j < i && sq(sorted[j].first.x - sorted[i].first.x) >= d; j++) ms.erase(its[j]);
-
-            auto it = ms.upper_bound(sorted[i]);
-            if (it != ms.begin()) {
-                update(*prev(it), sorted[i]);
-                for (auto p = prev(it); p != ms.begin() && sq(sorted[i].first.y - p->first.y) < d;) update(*(--p), sorted[i]);
-            }
-            for (; it != ms.end() && sq(it->first.y - sorted[i].first.y) < d; it++) update(*it, sorted[i]);
-            its[i] = ms.emplace_hint(ms.upper_bound(sorted[i]), sorted[i]);
-        }
-
+        for (auto &[x, y] : points) cin >> x >> y;
+        auto [pair, d] = closest_pair(points);
+        auto [a, b] = pair;
         cout << fixed << setprecision(2) << points[a].x << " " << points[a].y << " " << points[b].x << " " << points[b].y << "\n";
     }
 }
