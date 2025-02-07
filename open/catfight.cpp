bc1db56faf22175d5d036647a0e99616a197ab2e
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-02-08 01:56:33 +0800

    Create catfight.cpp

---
+++ open/catfight.cpp
@@ -0,0 +1,253 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+bool approximately_equal(const T &v1, const T &v2) {
+    return fabs(v1 - v2) <= 1e-8;
+}
+
+template <typename T>
+struct Point {
+    T x, y;
+
+    Point() {}
+    Point(T x, T y) : x(x), y(y) {}
+
+    template <typename U>
+    Point(const Point<U> &p) : x((T) p.x), y((T) p.y) {}
+
+    Point operator-() const {
+        return {-x, -y};
+    }
+
+    bool operator<(const Point &p) const {
+        return !approximately_equal(x, p.x) ? x < p.x : y < p.y;
+    }
+
+    bool operator>(const Point &p) const {
+        return !approximately_equal(x, p.x) ? x > p.x : y > p.y;
+    }
+
+    bool operator==(const Point &p) const {
+        if constexpr (is_floating_point_v<T>) return approximately_equal(x, p.x) && approximately_equal(y, p.y);
+        return x == p.x && y == p.y;
+    }
+
+    bool operator!=(const Point &p) const {
+        if constexpr (is_floating_point_v<T>) return !approximately_equal(x, p.x) || !approximately_equal(y, p.y);
+        return x != p.x || y != p.y;
+    }
+
+    bool operator<=(const Point &p) const {
+        return *this < p || *this == p;
+    }
+
+    bool operator>=(const Point &p) const {
+        return *this > p || *this == p;
+    }
+
+    Point operator+(const Point &p) const {
+        return {x + p.x, y + p.y};
+    }
+
+    Point operator+(const T &v) const {
+        return {x + v, y + v};
+    }
+
+    Point & operator+=(const Point &p) {
+        x += p.x;
+        y += p.y;
+        return *this;
+    }
+
+    Point & operator+=(const T &v) {
+        x += v;
+        y += v;
+        return *this;
+    }
+
+    Point operator-(const Point &p) const {
+        return {x - p.x, y - p.y};
+    }
+
+    Point operator-(const T &v) const {
+        return {x - v, y - v};
+    }
+
+    Point & operator-=(const Point &p) {
+        x -= p.x;
+        y -= p.y;
+        return *this;
+    }
+
+    Point & operator-=(const T &v) {
+        x -= v;
+        y -= v;
+        return *this;
+    }
+
+    Point operator*(const T &v) const {
+        return {x * v, y * v};
+    }
+
+    Point & operator*=(const T &v) {
+        x *= v;
+        y *= v;
+        return *this;
+    }
+
+    Point operator/(const T &v) const {
+        return {x / v, y / v};
+    }
+
+    Point & operator/=(const T &v) {
+        x /= v;
+        y /= v;
+        return *this;
+    }
+
+    struct Hash {
+        size_t operator()(Point<T> p) const {
+            auto h = 0ULL;
+            h ^= hash<T>()(p.x) + 0x9e3779b9 + (h << 6) + (h >> 2);
+            h ^= hash<T>()(p.y) + 0x9e3779b9 + (h << 6) + (h >> 2);
+            return h;
+        }
+    };
+};
+
+template <typename T>
+double squared_dist(const Point<T> &a, const Point<T> &b) {
+    return (double) (a.x - b.x) * (a.x - b.x) + (double) (a.y - b.y) * (a.y - b.y);
+}
+
+template <typename T>
+pair<pair<int, int>, double> closest_pair(const vector<Point<T>> &points) {
+    int n = points.size();
+
+    vector<pair<Point<T>, int>> sorted(n);
+    for (int i = 0; i < n; i++) sorted[i] = {points[i], i};
+    sort(sorted.begin(), sorted.end(), [](auto p1, auto p2) {return p1.first == p2.first ? p1.second < p2.second : p1.first < p2.first;});
+
+    auto d = DBL_MAX;
+    int a = -1, b = -1;
+    auto update = [&](auto p1, auto p2) {
+        auto dist = squared_dist(p1.first, p2.first);
+        if (d > dist) {
+            d = dist;
+            a = p1.second;
+            b = p2.second;
+        }
+    };
+
+    auto sq = [](double v) -> double {return v * v;};
+    auto cmp = [](auto p1, auto p2) {return p1.first.y < p2.first.y;};
+    multiset<pair<Point<T>, int>, decltype(cmp)> ms(cmp);
+    vector<typename decltype(ms)::const_iterator> its(n);
+    for (int i = 0, j = 0; i < n; i++) {
+        for (; j < i && sq(sorted[j].first.x - sorted[i].first.x) >= d; j++) ms.erase(its[j]);
+
+        auto it = ms.upper_bound(sorted[i]);
+        if (it != ms.begin()) {
+            update(*prev(it), sorted[i]);
+            for (auto p = prev(it); p != ms.begin() && sq(sorted[i].first.y - p->first.y) < d;) update(*(--p), sorted[i]);
+        }
+        for (; it != ms.end() && sq(it->first.y - sorted[i].first.y) < d; it++) update(*it, sorted[i]);
+        its[i] = ms.emplace_hint(ms.upper_bound(sorted[i]), sorted[i]);
+    }
+
+    return {{a, b}, d};
+}
+
+double circle_intersection_area(double d, double r1, double r2) {
+    if (d >= r1 + r2) return 0;
+
+    auto rmin = min(r1, r2), rmax = max(r1, r2);
+    if (d <= rmax - rmin) return M_PI * rmin * rmin;
+
+    auto r1sq = r1 * r1, r2sq = r2 * r2, alpha = acos((d * d + r1sq - r2sq) / (2 * d * r1)) * 2, beta = acos((d * d + r2sq - r1sq) / (2 * d * r2)) * 2;
+    return 0.5 * (r1sq * (alpha - sin(alpha)) + r2sq * (beta - sin(beta)));
+}
+
+template <typename T>
+struct KDTree {
+    struct KDNode {
+        Point<T> p;
+        bool dim;
+
+        KDNode() {}
+        KDNode(const Point<T> &p, bool dim = false) : p(p), dim(dim) {}
+    };
+
+    vector<Point<T>> points;
+    vector<KDNode> KDT;
+    vector<pair<int, int>> children;
+
+    KDTree(int n, const vector<Point<T>> &p) : points(p), KDT(n), children(n, {-1, -1}) {
+        int i = 0;
+        build(i, 0, points.size());
+    }
+
+    int build(int &i, int l, int r, bool dim = false) {
+        if (l >= r) return -1;
+
+        int m = l + (r - l) / 2;
+        nth_element(points.begin() + l, points.begin() + m, points.begin() + r, [dim](const auto &a, const auto &b) {return !dim ? a.x < b.x : a.y < b.y;});
+
+        int j = i++;
+        KDT[j] = {points[m], dim};
+        auto &[cl, cr] = children[j];
+        cl = build(i, l, m, !dim);
+        cr = build(i, m + 1, r, !dim);
+        return j;
+    }
+
+    double query(const Point<T> &p) {
+        return query(0, p);
+    }
+
+    double query(int i, const Point<T> &p) {
+        if (i == -1) return 1e30;
+
+        auto dist = squared_dist(p, KDT[i].p);
+        auto diff = !KDT[i].dim ? (p - KDT[i].p).x : (p - KDT[i].p).y;
+
+        auto [cl, cr] = children[i];
+        dist = min(dist, query(diff <= 0 ? cl : cr, p));
+        if (diff * diff < dist) dist = min(dist, query(diff <= 0 ? cr : cl, p));
+        return dist;
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    double rf, rm;
+    cin >> n >> rf >> rm;
+
+    vector<Point<double>> male, female;
+    while (n--) {
+        char g;
+        double x, y;
+        cin >> g >> x >> y;
+
+        if (g == 'M') male.emplace_back(x, y);
+        else female.emplace_back(x, y);
+    }
+
+    double overlap = 0;
+    if (male.size() >= 2) overlap = max(overlap, circle_intersection_area(sqrt(closest_pair(male).second), rm, rm));
+    if (female.size() >= 2) overlap = max(overlap, circle_intersection_area(sqrt(closest_pair(female).second), rf, rf));
+    if (!female.empty() && !male.empty()) {
+        auto g1 = male, g2 = female;
+        if (male.size() >= female.size()) swap(g1, g2);
+
+        KDTree<double> kdt(g1.size(), g1);
+        auto d = 1e30;
+        for (auto p : g2) d = min(d, kdt.query(p));
+        overlap = max(overlap, circle_intersection_area(sqrt(d), rf, rm));
+    }
+    cout << fixed << setprecision(5) << overlap;
+}
