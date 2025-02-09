00d723c2ab9cb401490cd5fa4b980188db39087d
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-02-10 01:14:55 +0800

    Update factorfree.cpp

--- nus/cs3233/factorfree.cpp
+++ nus/cs3233/factorfree.cpp
@@ -1,18 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool dfs(int l, int r, vector<int> &parent, vector<int> &left, vector<int> &right, int p = -1) {
-    if (l + 1 > r) return true;
-    for (int i = l, j = r - 1; i <= j; i++, j--) {
-        parent[i] = p;
-        if (left[i] < l && right[i] >= r) return dfs(l, i, parent, left, right, i) && dfs(i + 1, r, parent, left, right, i);
-        parent[j] = p;
-        if (left[j] < l && right[j] >= r) return dfs(l, j, parent, left, right, j) && dfs(j + 1, r, parent, left, right, j);
-    }
-
-    return false;
-}
-
 vector<int> sieve(int n) {
     vector<int> lpf(n + 1, 0);
     for (int i = 2; i <= n; i++)
@@ -29,21 +17,38 @@ int main() {
     int n;
     cin >> n;
 
-    vector<int> elements(n);
-    for (int &a : elements) cin >> a;
 
-    int biggest = *max_element(elements.begin(), elements.end());
+    vector<int> a(n);
+    int biggest = 0;
+    for (int &ai : a) {
+        cin >> ai;
+
+        biggest = max(biggest, ai);
+    }
+
     vector<int> lpf = sieve(biggest), prev(biggest + 1, -1), left(n, -1), right(n, n), parent(n, 0);
-    for (int i = 0; i < n; i++) 
-        for (int e = elements[i]; e > 1; e /= lpf[e]) {
-            if (prev[lpf[e]] < i) {
-                left[i] = max(left[i], prev[lpf[e]]);
-                if (prev[lpf[e]] != -1) right[prev[lpf[e]]] = min(right[prev[lpf[e]]], i);
+    for (int i = 0; i < n; i++)
+        for (int ai = a[i]; ai > 1; ai /= lpf[ai]) {
+            if (prev[lpf[ai]] < i) {
+                left[i] = max(left[i], prev[lpf[ai]]);
+                if (prev[lpf[ai]] != -1) right[prev[lpf[ai]]] = min(right[prev[lpf[ai]]], i);
             }
-            prev[lpf[e]] = i;
+            prev[lpf[ai]] = i;
         }
 
-    if (dfs(0, n, parent, left, right))
+    auto dfs = [&](auto &&self, int l, int r, int p = -1) {
+        if (l + 1 > r) return true;
+        for (int i = l, j = r - 1; i <= j; i++, j--) {
+            parent[i] = p;
+            if (left[i] < l && right[i] >= r) return self(self, l, i, i) && self(self, i + 1, r, i);
+            parent[j] = p;
+            if (left[j] < l && right[j] >= r) return self(self, l, j, j) && self(self, j + 1, r, j);
+        }
+
+        return false;
+    };
+
+    if (dfs(dfs, 0, n))
         for (int p : parent) cout << p + 1 << " ";
     else cout << "impossible";
 }
