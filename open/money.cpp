c0dec423f66ace07d95da4acadc3f5d242b8daab
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-02-13 11:09:23 +0800

    Update money.cpp

--- open/money.cpp
+++ open/money.cpp
@@ -1,25 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long dnc(int l, int r, int start, int end, vector<pair<int, int>> &producer, vector<pair<int, int>> &consumer) {
-    if (l > r) return 0;
-    
-    int m = l + (r - l) / 2, best = start;
-    auto profit = LLONG_MIN;
-    for (int i = start; i <= end; i++) {
-        int duration = consumer[i].second - producer[m].second, price = consumer[i].first - producer[m].first;
-        auto cost = !duration || price <= 0 ? (long long) -abs(duration) * abs(price) : (long long) duration * price;
-        if (cost >= profit) {
-            profit = cost;
-            best = i;
-        }
-    }
-
-    if (l < r) profit = max({profit, dnc(l, m - 1, start, best, producer, consumer), dnc(m + 1, r, best, end, producer, consumer)});
-
-    return profit;
-}
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -32,21 +13,40 @@ int main() {
     for (auto &[q, e] : consumer) cin >> q >> e;
 
     sort(producer.begin(), producer.end());
-    vector<pair<int, int>> selection;
+    vector<pair<int, int>> choose;
     for (int i = 0, j; i < m; i = j) {
-        selection.emplace_back(producer[i]);
+        choose.emplace_back(producer[i]);
         for (j = i; j < m && producer[j].second >= producer[i].second; j++);
     }
-    producer.swap(selection);
-    selection.clear();
+    producer = choose;
+    choose.clear();
 
     sort(consumer.rbegin(), consumer.rend());
     for (int i = 0, j; i < n; i = j) {
-        selection.emplace_back(consumer[i]);
+        choose.emplace_back(consumer[i]);
         for (j = i; j < n && consumer[j].second <= consumer[i].second; j++);
     }
-    consumer.swap(selection);
+    consumer = choose;
     reverse(consumer.begin(), consumer.end());
 
-    cout << max(0LL, dnc(0, producer.size() - 1, 0, consumer.size() - 1, producer, consumer));
+    auto dnc = [&](auto &&self, int pl, int pr, int cl, int cr) -> long long {
+        if (pl >= pr) return 0;
+
+        auto profit = LLONG_MIN;
+        int pm = pl + (pr - pl) / 2, cm = cl;
+        for (int i = cl; i <= cr; i++) {
+            auto [p, d] = producer[pm];
+            auto [q, e] = consumer[i];
+            int days = e - d, price = q - p;
+            auto cost = !days || price <= 0 ? (long long) -abs(days) * abs(price) : (long long) days * price;
+            if (cost >= profit) {
+                profit = cost;
+                cm = i;
+            }
+        }
+
+        if (pl + 1 < pr) profit = max({profit, self(self, pl, pm, cl, cm), self(self, pm, pr, cm, cr)});
+        return profit;
+    };
+    cout << max(0LL, dnc(dnc, 0, producer.size(), 0, consumer.size()));
 }
