39f9882dc470ba6fad5cd2c832520426bd6d6a4f
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-02-24 01:35:12 +0800

    Create 3sideddice.cpp

---
+++ open/3sideddice.cpp
@@ -0,0 +1,152 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+template <typename T>
+struct Point {
+    T x, y;
+
+    Point() {}
+    Point(T x, T y) : x(x), y(y) {}
+
+    template <typename U>
+    Point(const Point<U> &p) : x((T) p.x), y((T) p.y) {}
+
+    Point operator-() const {
+        return {-x, -y};
+    }
+
+    bool operator<(const Point &p) const {
+        return !approximately_equal(x, p.x) ? x < p.x : y < p.y;
+    }
+
+    bool operator>(const Point &p) const {
+        return !approximately_equal(x, p.x) ? x > p.x : y > p.y;
+    }
+
+    bool operator==(const Point &p) const {
+        if constexpr (is_floating_point_v<T>) return approximately_equal(x, p.x) && approximately_equal(y, p.y);
+        return x == p.x && y == p.y;
+    }
+
+    bool operator!=(const Point &p) const {
+        if constexpr (is_floating_point_v<T>) return !approximately_equal(x, p.x) || !approximately_equal(y, p.y);
+        return x != p.x || y != p.y;
+    }
+
+    bool operator<=(const Point &p) const {
+        return *this < p || *this == p;
+    }
+
+    bool operator>=(const Point &p) const {
+        return *this > p || *this == p;
+    }
+
+    Point operator+(const Point &p) const {
+        return {x + p.x, y + p.y};
+    }
+
+    Point operator+(const T &v) const {
+        return {x + v, y + v};
+    }
+
+    Point & operator+=(const Point &p) {
+        x += p.x;
+        y += p.y;
+        return *this;
+    }
+
+    Point & operator+=(const T &v) {
+        x += v;
+        y += v;
+        return *this;
+    }
+
+    Point operator-(const Point &p) const {
+        return {x - p.x, y - p.y};
+    }
+
+    Point operator-(const T &v) const {
+        return {x - v, y - v};
+    }
+
+    Point & operator-=(const Point &p) {
+        x -= p.x;
+        y -= p.y;
+        return *this;
+    }
+
+    Point & operator-=(const T &v) {
+        x -= v;
+        y -= v;
+        return *this;
+    }
+
+    Point operator*(const T &v) const {
+        return {x * v, y * v};
+    }
+
+    Point & operator*=(const T &v) {
+        x *= v;
+        y *= v;
+        return *this;
+    }
+
+    Point operator/(const T &v) const {
+        return {x / v, y / v};
+    }
+
+    Point & operator/=(const T &v) {
+        x /= v;
+        y /= v;
+        return *this;
+    }
+
+    struct Hash {
+        size_t operator()(Point<T> p) const {
+            auto h = 0ULL;
+            h ^= hash<T>()(p.x) + 0x9e3779b9 + (h << 6) + (h >> 2);
+            h ^= hash<T>()(p.y) + 0x9e3779b9 + (h << 6) + (h >> 2);
+            return h;
+        }
+    };
+};
+
+template <typename T>
+T cross(Point<T> a, Point<T> b) {
+    return (a.x * b.y) - (a.y * b.x);
+}
+
+template <typename T>
+T area_of_parallelogram(Point<T> a, Point<T> b, Point<T> c) {
+    Point<T> u = b - a, v = c - a;
+    return abs(cross(u, v));
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    for (;;) {
+        array<Point<int>, 4> p;
+        for (auto &[x, y] : p) {
+            int z;
+            cin >> x >> y >> z;
+
+            if (!x && !y && !z) exit(0);
+        }
+        auto a1 = area_of_parallelogram(p[0], p[1], p[2]),
+             a2 = area_of_parallelogram(p[0], p[1], p[3]),
+             a3 = area_of_parallelogram(p[0], p[2], p[3]),
+             a4 = area_of_parallelogram(p[1], p[2], p[3]);
+
+        if (a1 != a2 + a3 + a4) {
+            cout << "NO\n";
+            continue;
+        }
+
+        if (!a1) cout << ((min({p[0].x, p[1].x, p[2].x}) < p[3].x && p[3].x < max({p[0].x, p[1].x, p[2].x})) ||
+                          (min({p[0].y, p[1].y, p[2].y}) < p[3].y && p[3].y < max({p[0].y, p[1].y, p[2].y})) ? "YES\n" : "NO\n");
+        else cout << (a2 && a3 && a4 ? "YES\n" : "NO\n");
+    }
+}
