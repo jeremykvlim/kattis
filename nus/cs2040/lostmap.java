bb032dde4c7c17d2a4e6af501354f899efb5ae92
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-02-27 12:05:14 +0800

    Update lostmap.java

--- nus/cs2040/lostmap.java
+++ nus/cs2040/lostmap.java
@@ -10,16 +10,17 @@ public class lostmap {
         var adjMatrix = new int[n][n];
         for (int i = 0; i < n; i++) adjMatrix[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
-        for (var e : prim_dense(n, adjMatrix)) pw.println(e.v + " " + e.u);
+        for (var e : prim_dense(n, adjMatrix)) pw.println(e.first + " " + e.second);
         pw.flush();
     }
 
-    static Edge[] prim_dense(int n, int[][] adjMatrix) {
+    static ArrayList<Pair<Integer, Integer>> prim_dense(int n, int[][] adjMatrix) {
         var dist = new int[n];
         Arrays.fill(dist, Integer.MAX_VALUE);
         dist[0] = 0;
         var visited = new boolean[n];
-        var mst = new Edge[n - 1];
+        var mst = new ArrayList<Pair<Integer, Integer>>(n - 1);
+        for (int i = 0; i < n - 1; i++) mst.add(new Pair<>(-1, -1));
         for (int i = 0; i < n; i++) {
             int d = Integer.MAX_VALUE, j = 0;
             for (int k = 0; k < n; k++)
@@ -32,19 +33,18 @@ public class lostmap {
             for (int k = 0; k < n; k++)
                 if (!visited[k] && dist[k] > adjMatrix[j][k]) {
                     dist[k] = adjMatrix[j][k];
-                    mst[k - 1] = new Edge(Math.min(j, k) + 1, Math.max(j, k) + 1);
+                    mst.set(k - 1, new Pair<>(Math.min(j, k) + 1, Math.max(j, k) + 1));
                 }
         }
 
         return mst;
     }
 
-    static class Edge {
-        int v, u;
-
-        Edge(int f, int s) {
-            v = f;
-            u = s;
+    record Pair<T extends Comparable<T>, U extends Comparable<U>>(T first, U second) implements Comparable<Pair<T, U>> {
+        @Override
+        public int compareTo(Pair<T, U> p) {
+            int cmp = first.compareTo(p.first);
+            return (cmp == 0) ? second.compareTo(p.second) : cmp;
         }
     }
 }
