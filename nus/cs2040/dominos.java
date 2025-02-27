3940ec97db5c5e281db0095d9912a19274cdf7d8
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-02-27 21:28:03 +0800

    Update dominos.java

--- nus/cs2040/dominos.java
+++ nus/cs2040/dominos.java
@@ -4,9 +4,10 @@ import java.util.*;
 public class dominos {
     public static void main(String[] args) throws IOException {
         var br = new BufferedReader(new InputStreamReader(System.in));
+        var pw = new PrintWriter(System.out);
 
-        int t = Integer.parseInt(br.readLine());
-        while (t-- > 0) {
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
             var input = br.readLine().split(" ");
             int n = Integer.parseInt(input[0]), m = Integer.parseInt(input[1]);
 
@@ -18,31 +19,55 @@ public class dominos {
                 adjList[Integer.parseInt(input[0])].add(Integer.parseInt(input[1]));
             }
 
-            var visited = new boolean[n + 1];
-            var s = new Stack<Integer>();
-            for (int i = 1; i <= n; i++)
-                if (!visited[i]) dfs(i, visited, adjList, s, true);
+            var t = tarjan(n, adjList);
+            var sccs = (int) t.first;
+            var component = (int[]) t.second;
 
-            visited = new boolean[n + 1];
+            var degree = new int[sccs + 1];
+            for (int v = 1; v <= n; v++)
+                for (var u : adjList[v])
+                    if (component[(int) u] != component[v]) degree[component[(int) u]]++;
+
             int count = 0;
-            while (!s.isEmpty()) {
-                int v = s.pop();
-
-                if (!visited[v]) {
-                    dfs(v, visited, adjList, s, false);
-                    count++;
-                }
-            }
-
-            System.out.println(count);
+            for (int i = 1; i <= sccs; i++)
+                if (degree[i] == 0) count++;
+            pw.println(count);
         }
+        pw.flush();
     }
 
-    static void dfs(int v, boolean[] visited, ArrayList<Integer>[] adjList, Stack<Integer> s, boolean stack) {
-        visited[v] = true;
-        for (var u : adjList[v])
-            if (!visited[u]) dfs(u, visited, adjList, s, stack);
-      
-        if (stack) s.push(v);
+    record Tuple<T, U>(T first, U second) {}
+
+    static Tuple<Integer, int[]> tarjan(int n, ArrayList<Integer>[] adjList) {
+        int[] order = new int[n + 1], low = new int[n + 1], component = new int[n + 1], counts = new int[2];
+        var stacked = new boolean[n + 1];
+        var st = new Stack<Integer>();
+
+        for (int v = 1; v <= n; v++)
+            if (order[v] == 0) dfs(v, adjList, order, low, component, stacked, st, counts);
+
+        return new Tuple<>(counts[1], component);
+    }
+
+    static void dfs(int v, ArrayList<Integer>[] adjList, int[] order, int[] low, int[] component, boolean[] stacked, Stack<Integer> st, int[] counts) {
+        order[v] = low[v] = ++counts[0];
+        st.add(v);
+        stacked[v] = true;
+
+        for (int u : adjList[v])
+            if (order[u] == 0) {
+                dfs(u, adjList, order, low, component, stacked, st, counts);
+                low[v] = Math.min(low[v], low[u]);
+            } else if (stacked[u]) low[v] = Math.min(low[v], order[u]);
+
+        if (order[v] == low[v]) {
+            counts[1]++;
+            int u;
+            do {
+                u = st.pop();
+                stacked[u] = false;
+                component[u] = counts[1];
+            } while (u != v);
+        }
     }
 }
