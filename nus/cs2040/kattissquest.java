f1b2bb5ad7cbdd62b8d639f20d07dfc8ac479c54
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-02-27 12:50:39 +0800

    Update kattissquest.java

--- nus/cs2040/kattissquest.java
+++ nus/cs2040/kattissquest.java
@@ -5,20 +5,21 @@ public class kattissquest {
     public static void main(String[] args) throws IOException {
         var br = new BufferedReader(new InputStreamReader(System.in));
         var pw = new PrintWriter(System.out);
-        
+
         int n = Integer.parseInt(br.readLine());
-        
-        var set = new TreeSet<Quest>();
+
+        var quests = new TreeSet<Triple<Long, Long, Integer>>();
         while (n-- > 0) {
             var command = br.readLine().split(" ");
-            if (command[0].equals("add")) set.add(new Quest(Long.parseLong(command[1]), Long.parseLong(command[2]), n));
+
+            if (command[0].equals("add")) quests.add(new Triple<>(Long.parseLong(command[1]), Long.parseLong(command[2]), n));
             else {
                 long energy = Long.parseLong(command[1]), gold = 0;
-                while (set.floor(new Quest(energy, Integer.MAX_VALUE, Integer.MAX_VALUE)) != null) {
-                    var bestQuest = set.floor(new Quest(energy, Integer.MAX_VALUE, Integer.MAX_VALUE));
-                    energy -= bestQuest.consumption;
-                    gold += bestQuest.reward;
-                    set.remove(bestQuest);
+                while (quests.floor(new Triple<>(energy, Long.MAX_VALUE, Integer.MAX_VALUE)) != null) {
+                    var q = quests.floor(new Triple<>(energy, Long.MAX_VALUE, Integer.MAX_VALUE));
+                    energy -= q.first;
+                    gold += q.second;
+                    quests.remove(q);
                 }
                 pw.println(gold);
             }
@@ -26,21 +27,13 @@ public class kattissquest {
         pw.flush();
     }
 
-    static class Quest implements Comparable<Quest> {
-        long consumption, reward;
-        int index;
-
-        Quest(long c, long r, int i) {
-            consumption = c;
-            reward = r;
-            index = i;
-        }
-
+    record Triple<T extends Comparable<T>, U extends Comparable<U>, V extends Comparable<V>>(T first, U second, V third) implements Comparable<Triple<T, U, V>> {
         @Override
-        public int compareTo(Quest q) {
-            if (consumption != q.consumption) return Long.compare(consumption, q.consumption);
-            else if (reward != q.reward) return Long.compare(reward, q.reward);
-            else return index - q.index;
+        public int compareTo(Triple<T, U, V> t) {
+            int cmp = first.compareTo(t.first);
+            if (cmp != 0) return cmp;
+            cmp = second.compareTo(t.second);
+            return (cmp == 0) ? third.compareTo(t.third) : cmp;
         }
     }
 }
