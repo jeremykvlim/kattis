9233258aa2060eb413e3e80d9c16fd65893dc011
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-02-27 20:22:26 +0800

    Update bots.java

--- nus/cs2040/bots.java
+++ nus/cs2040/bots.java
@@ -9,74 +9,71 @@ public class bots {
         var input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         int n = input[0], m = input[1];
 
-        var adjListRegular = new ArrayList[n];
-        var adjListTranspose = new ArrayList[n];
-        for (int i = 0; i < n; i++) {
-            adjListRegular[i] = new ArrayList<>();
-            adjListTranspose[i] = new ArrayList<>();
-        }
+        var adjList = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) adjList[i] = new ArrayList<>();
 
         while (m-- > 0) {
             input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
             int p = input[0], q = input[1];
-
-            adjListRegular[p].add(q);
-            adjListTranspose[q].add(p);
+            adjList[p + 1].add(q + 1);
         }
 
-        var visited = new boolean[n];
-        var s = new Stack<Integer>();
-        for (int i = 0; i < n; i++)
-            if (!visited[i]) dfs1(i, visited, adjListRegular, s);
+        var t = tarjan(n, adjList);
+        var sccs = (int) t.first;
+        var component = (int[]) t.second;
+        var members = (ArrayList<ArrayList>) t.third;
 
-        var component = new int[n];
-        Arrays.fill(component, -1);
-        int sccs = 0;
-        while (!s.isEmpty()) {
-            int v = s.pop();
-
-            if (component[v] == -1) {
-                dfs2(v, adjListTranspose, component, sccs);
-                sccs++;
-            }
-        }
-
-        var size = new int[sccs];
-        for (int i = 0; i < n; i++) size[component[i]]++;
-
-        var neighbor = new ArrayList<HashSet<Integer>>(sccs);
-        for (int i = 0; i < sccs; i++) neighbor.add(new HashSet<>());
-
-        var degree = new int[sccs];
-        for (int v = 0; v < n; v++)
-            for (var u : adjListRegular[v])
-                if (component[v] != component[(int) u])
-                    if (!neighbor.get(component[v]).contains(component[(int) u])) {
-                        neighbor.get(component[v]).add(component[(int) u]);
-                        degree[component[(int) u]]++;
-                    }
+        var degree = new int[sccs + 1];
+        for (int v = 1; v <= n; v++)
+            for (var u : adjList[v])
+                if (component[(int) u] != component[v]) degree[component[(int) u]]++;
 
         int solo = 0, net = 0;
-        for (int i = 0; i < sccs; i++)
+        for (int i = 1; i <= sccs; i++)
             if (degree[i] == 0) {
-                if (size[i] == 1) solo++;
+                if (members.get(i).size() == 1) solo++;
                 else net++;
             }
         pw.println(solo + " " + net);
         pw.flush();
     }
 
-    static void dfs1(int v, boolean[] visited, ArrayList<Integer>[] adjListRegular, Stack<Integer> s) {
-        visited[v] = true;
-        for (var u : adjListRegular[v])
-            if (!visited[u]) dfs1(u, visited, adjListRegular, s);
+    record Tuple<T, U, V>(T first, U second, V third) {}
 
-        s.push(v);
+    static Tuple<Integer, int[], ArrayList<ArrayList<Integer>>> tarjan(int n, ArrayList<Integer>[] adjList) {
+        int[] order = new int[n + 1], low = new int[n + 1], component = new int[n + 1], counts = new int[2];
+        var members = new ArrayList<ArrayList<Integer>>();
+        members.add(new ArrayList<>());
+        var stacked = new boolean[n + 1];
+        var st = new Stack<Integer>();
+
+        for (int v = 1; v <= n; v++)
+            if (order[v] == 0) dfs(v, adjList, order, low, component, members, stacked, st, counts);
+
+        return new Tuple<>(counts[1], component, members);
     }
 
-    static void dfs2(int v, ArrayList<Integer>[] adjListTranspose, int[] component, int sccs) {
-        component[v] = sccs;
-        for (var u : adjListTranspose[v])
-            if (component[u] == -1) dfs2(u, adjListTranspose, component, sccs);
+    static void dfs(int v, ArrayList<Integer>[] adjList, int[] order, int[] low, int[] component, ArrayList<ArrayList<Integer>> members, boolean[] stacked, Stack<Integer> st, int[] counts) {
+        order[v] = low[v] = ++counts[0];
+        st.add(v);
+        stacked[v] = true;
+
+        for (int u : adjList[v])
+            if (order[u] == 0) {
+                dfs(u, adjList, order, low, component, members, stacked, st, counts);
+                low[v] = Math.min(low[v], low[u]);
+            } else if (stacked[u]) low[v] = Math.min(low[v], order[u]);
+
+        if (order[v] == low[v]) {
+            counts[1]++;
+            members.add(new ArrayList<>());
+            int u;
+            do {
+                u = st.pop();
+                stacked[u] = false;
+                component[u] = counts[1];
+                members.get(counts[1]).add(u);
+            } while (u != v);
+        }
     }
 }
