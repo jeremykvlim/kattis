3623d5b577e4686f900e1f0177a1f97490ccb543
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-02-27 12:32:24 +0800

    Update communicationssatellite.java

--- nus/cs2040/communicationssatellite.java
+++ nus/cs2040/communicationssatellite.java
@@ -4,60 +4,47 @@ import java.util.*;
 public class communicationssatellite {
     public static void main(String[] args) throws IOException {
         var br = new BufferedReader(new InputStreamReader(System.in));
+        var pw = new PrintWriter(System.out);
 
         int n = Integer.parseInt(br.readLine());
-        var satellites = new Triplet[n];
+        var satellites = new ArrayList<Triple<Integer, Integer, Integer>>(n);
         for (int i = 0; i < n; i++) {
             var line = br.readLine().split(" ");
-            satellites[i] = new Triplet(Integer.parseInt(line[0]), Integer.parseInt(line[1]), Integer.parseInt(line[2]));
+            satellites.add(new Triple<>(Integer.parseInt(line[0]), Integer.parseInt(line[1]), Integer.parseInt(line[2])));
         }
 
-        var pq = new PriorityQueue<Edge>();
+        var pq = new PriorityQueue<Triple<Double, Integer, Integer>>();
         for (int i = 0; i < n; i++)
             for (int j = i + 1; j < n; j++)
-                pq.add(new Edge(i, j, Math.hypot(satellites[i].first - satellites[j].first, satellites[i].second - satellites[j].second) - satellites[i].third - satellites[j].third));
+                pq.add(new Triple<>(Math.hypot(satellites.get(i).first - satellites.get(j).first, satellites.get(i).second - satellites.get(j).second) - satellites.get(i).third - satellites.get(j).third, i, j));
 
         var sum = 0.0;
         var dsu = new DisjointSets(n);
         while (n > 1 && !pq.isEmpty()) {
             var e = pq.poll();
-            
-            if (dsu.find(e.source) != dsu.find(e.destination)) {
+            var w = e.first;
+            int u = e.second, v = e.third;
+
+            if (dsu.find(u) != dsu.find(v)) {
+                dsu.unite(u, v);
+                sum += w;
                 n--;
-                dsu.unite(e.source, e.destination);
-                sum += e.weight;
             }
         }
-
-        System.out.printf("%.6f", sum);
+        pw.format("%.6f", sum);
+        pw.flush();
     }
 
-    static class Triplet {
-        int first, second, third;
-
-        Triplet(int f, int s, int t) {
-            first = f;
-            second = s;
-            third = t;
-        }
-    }
-
-    static class Edge implements Comparable<Edge> {
-        int source, destination;
-        double weight;
-
-        Edge(int src, int dest, double w) {
-            source = src;
-            destination = dest;
-            weight = w;
-        }
-
+    record Triple<T extends Comparable<T>, U extends Comparable<U>, V extends Comparable<V>>(T first, U second, V third) implements Comparable<Triple<T, U, V>> {
         @Override
-        public int compareTo(Edge e) {
-            return Double.compare(weight, e.weight);
+        public int compareTo(Triple<T, U, V> t) {
+            int cmp = first.compareTo(t.first);
+            if (cmp != 0) return cmp;
+            cmp = second.compareTo(t.second);
+            return (cmp == 0) ? third.compareTo(t.third) : cmp;
         }
     }
-    
+
     static class DisjointSets {
         int[] sets;
 
