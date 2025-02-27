c1ca84efedb3f0a737911393c66f8d49cfc8745b
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-02-27 14:34:12 +0800

    Update classy.java

--- nus/cs2040/classy.java
+++ nus/cs2040/classy.java
@@ -4,44 +4,39 @@ import java.util.*;
 public class classy {
     public static void main(String[] args) throws IOException {
         var br = new BufferedReader(new InputStreamReader(System.in));
-        
+        var pw = new PrintWriter(System.out);
+
         int t = Integer.parseInt(br.readLine());
         while (t-- > 0) {
             int n = Integer.parseInt(br.readLine());
-            var people = new Person[n];
 
-            for (int i = 0; n > 0; n--, i++) {
-                var middle = new String[11];
-                Arrays.fill(middle, "middle");
+            var people = new ArrayList<Pair<String, String>>(n);
+            for (int i = 0; i < n; i++) {
                 var input = br.readLine().split(" ");
-                var name = input[0].replace(":", "");
                 var classes = input[1].split("-");
-                for (int j = classes.length - 1, k = 10; j >= 0; j--, k--) middle[k] = classes[j];
-                people[i] = new Person(name, middle);
+                var levels = new char[10];
+                Arrays.fill(levels, 'b');
+
+                for (int j = classes.length - 1, k = 0; j >= 0; j--, k++) {
+                    if (classes[j].charAt(0) == 'u') levels[k] = 'a';
+                    else if (classes[j].charAt(0) == 'l') levels[k] = 'c';
+                }
+                people.add(new Pair<>(new String(levels), input[0].substring(0, input[0].length() - 1)));
             }
-            
-            Arrays.sort(people);
-            for (var p : people) System.out.println(p.name);
-            for (int i = 0; i < 30; i++) System.out.print("=");
-            System.out.println();
+            Collections.sort(people);
+
+            for (var p : people) pw.println(p.second);
+            for (int i = 0; i < 30; i++) pw.print('=');
+            pw.println();
         }
+        pw.flush();
     }
-    
-    static class Person implements Comparable<Person> {
-        String name;
-        String[] classes;
 
-        Person(String n, String[] c) {
-            name = n;
-            classes = c;
-        }
-
+    record Pair<T extends Comparable<T>, U extends Comparable<U>>(T first, U second) implements Comparable<Pair<T, U>> {
         @Override
-        public int compareTo(Person p) {
-            for (int i = 10; i >= 0; i--) {
-                if (!p.classes[i].equals(classes[i]))
-                    return p.classes[i].charAt(0) - classes[i].charAt(0);
-            } return name.compareTo(p.name); 
+        public int compareTo(Pair<T, U> p) {
+            int cmp = first.compareTo(p.first);
+            return (cmp == 0) ? second.compareTo(p.second) : cmp;
         }
     }
 }
