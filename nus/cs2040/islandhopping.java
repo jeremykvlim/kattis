2516490e5d9c5e13ef6b31e77611ad4aa3a3162a
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-02-27 11:58:27 +0800

    Update islandhopping.java

--- nus/cs2040/islandhopping.java
+++ nus/cs2040/islandhopping.java
@@ -5,7 +5,7 @@ public class islandhopping {
     public static void main(String[] args) throws IOException {
         var br = new BufferedReader(new InputStreamReader(System.in));
         var pw = new PrintWriter(System.out);
-        
+
         int n = Integer.parseInt(br.readLine());
         while (n-- > 0) {
             int m = Integer.parseInt(br.readLine());
@@ -16,49 +16,44 @@ public class islandhopping {
                 distances[i][1] = Double.parseDouble(line[1]);
             }
 
-            var min = new double[m];
-            Arrays.fill(min, Double.POSITIVE_INFINITY);
-            min[0] = 0;
+            var dist = new double[m];
+            Arrays.fill(dist, Double.POSITIVE_INFINITY);
+            dist[0] = 0;
 
-            var pq = new PriorityQueue<Pair>();
-            pq.add(new Pair(0, 0));
+            var pq = new PriorityQueue<Pair<Double, Integer>>();
+            pq.add(new Pair<>(0., 0));
 
             var visited = new boolean[m];
             var total = 0.0;
 
             while (!pq.isEmpty()) {
-                var curr = pq.poll();
-                if (visited[curr.first]) continue;
-                visited[curr.first] = true;
-                total += curr.second;
+                var p = pq.poll();
+                int v = p.second;
 
-                for (int i = 0; i < m; i++) {
-                    if (visited[i]) continue;
-                    var dist = Math.hypot(distances[curr.first][0] - distances[i][0], distances[curr.first][1] - distances[i][1]);
-                    if (dist < min[i]) {
-                        min[i] = dist;
-                        pq.add(new Pair(i, dist));
+                if (visited[v]) continue;
+                visited[v] = true;
+                total += p.first;
+
+                for (int u = 0; u < m; u++) {
+                    if (visited[u]) continue;
+                    var d = Math.hypot(distances[v][0] - distances[u][0], distances[v][1] - distances[u][1]);
+                    if (dist[u] > d) {
+                        dist[u] = d;
+                        pq.add(new Pair<>(d, u));
                     }
                 }
             }
 
             pw.format("%.3f\n", total);
-            pw.flush();
         }
+        pw.flush();
     }
 
-    static class Pair implements Comparable<Pair> {
-        int first;
-        double second;
-
-        public Pair(int f, double s) {
-            first = f;
-            second = s;
-        }
-
+    record Pair<T extends Comparable<T>, U extends Comparable<U>>(T first, U second) implements Comparable<Pair<T, U>> {
         @Override
-        public int compareTo(Pair p) {
-            return Double.compare(this.second, p.second);
+        public int compareTo(Pair<T, U> p) {
+            int cmp = first.compareTo(p.first);
+            return (cmp == 0) ? second.compareTo(p.second) : cmp;
         }
     }
 }
