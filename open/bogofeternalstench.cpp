9128f80cdbfecd9ca7b37e79ca510aec481f8ccb
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-02-28 14:56:27 +0800

    Update bogofeternalstench.cpp

--- open/bogofeternalstench.cpp
+++ open/bogofeternalstench.cpp
@@ -8,32 +8,44 @@ int main() {
     int n, m;
     cin >> n >> m;
 
-    vector<vector<pair<int, int>>> adj_list(n + 1);
-    while (m--) {
-        int u, v, w;
-        cin >> u >> v >> w;
+    vector<vector<int>> adj_list(n + 1);
+    vector<array<int, 3>> edges(m);
+    for (auto &[u, v, s] : edges) {
+        cin >> u >> v >> s;
 
-        adj_list[u].emplace_back(v, w);
+        adj_list[u].emplace_back(v);
     }
 
-    vector<long long> dist(n + 1, 1e18);
-    dist[1] = 0;
-    vector<int> visited(n + 1, 0);
-    priority_queue<pair<long long, long long>, vector<pair<long long, long long>>, greater<>> pq;
-    pq.emplace(0, 1);
-    while (!pq.empty()) {
-        auto [d, v] = pq.top();
-        pq.pop();
+    vector<long long> dp1(n + 1, 1e18), dp2(n + 1, 1e18);
+    dp1[1] = 0;
+    queue<int> q;
+    q.emplace(1);
+    while (!q.empty()) {
+        int v = q.front();
+        q.pop();
 
-        if (visited[v]++ > n) continue;
-
-        for (auto [u, w] : adj_list[v])
-            if (dist[u] > max(0LL, dist[v] + w)) {
-                if (visited[u] > 1) dist[u] = (w < 0 ? 0 : max(0LL, dist[v] + w));
-                else dist[u] = max(0LL, dist[v] + w);
-                pq.emplace(dist[u], u);
+        for (int u : adj_list[v])
+            if (dp1[u]) {
+                q.emplace(u);
+                dp1[u] = 0;
             }
     }
 
-    cout << dist[n];
+    for (int i = 0; i < n; i++) {
+        vector<long long> temp(n + 1, 1e18);
+        temp[1] = 0;
+        for (auto [u, v, s] : edges) {
+            temp[v] = min(temp[v], max(0LL, dp1[u] + s));
+            dp2[v] = min(dp2[v], max(0LL, dp2[u] + s));
+        }
+        dp1 = temp;
+    }
+
+    for (auto [u, v, s] : edges)
+        if (dp2[v] > max(0LL, dp2[u] + s)) dp2[v] = min(dp2[v], dp1[v]);
+
+    dp2[1] = 0;
+    for (int i = 0; i < n; i++)
+        for (auto [u, v, s] : edges) dp2[v] = min(dp2[v], max(0LL, dp2[u] + s));
+    cout << dp2[n];
 }
