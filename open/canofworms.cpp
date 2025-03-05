1f1c6d55101ad924df73acab424ae5219c6f7030
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-03-05 15:28:26 +0800

    Create canofworms.cpp

---
+++ open/canofworms.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    vector<array<int, 3>> fences(n);
+    vector<int> l(n), r(n);
+    for (int i = 0; i < n; i++) {
+        cin >> fences[i][0] >> fences[i][1];
+
+        fences[i][2] = i;
+        l[i] = r[i] = i;
+    }
+    sort(fences.begin(), fences.end());
+
+    bool change;
+    do {
+        change = false;
+        auto update = [&](bool rev = false) {
+            stack<int> s;
+            for (int i = rev ? n - 1 : 0; (rev ? ~i : i < n); (rev ? i-- : i++)) {
+                auto valid = [&](int j) {
+                    return abs(fences[i][0] - fences[j][0]) <= fences[i][1];
+                };
+
+                while (!s.empty() && valid(s.top())) {
+                    int j = s.top();
+                    s.pop();
+
+                    if (l[i] > l[j]) {
+                        l[i] = l[j];
+                        change = true;
+                    }
+
+                    if (r[i] < r[j]) {
+                        r[i] = r[j];
+                        change = true;
+                    }
+                }
+                s.emplace(i);
+            }
+        };
+        update(false);
+        update(true);
+    } while (change);
+
+    vector<int> cans(n);
+    for (int i = 0; i < n; i++) cans[fences[i][2]] = r[i] - l[i] + 1;
+    for (int i = 0; i < n; i++) cout << cans[i] << " ";
+}
