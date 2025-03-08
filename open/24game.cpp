df7577c843a68491a91fbdf8dcb2372f4910cec6
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-03-09 03:48:21 +0800

    Update 24game.cpp

--- open/24game.cpp
+++ open/24game.cpp
@@ -1,42 +1,80 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template <typename T>
-struct Fraction {
-    T numer, denom;
+struct Hash {
+    template <typename T>
+    static inline void combine(size_t &h, const T &v) {
+        h ^= Hash{}(v) + 0x9e3779b9 + (h << 6) + (h >> 2);
+    }
 
-    Fraction() {}
-    Fraction(T n, T d) : numer(n), denom(d) {
+    template <typename T>
+    size_t operator()(const T &v) const {
+        if constexpr (requires {tuple_size<T>::value;})
+            return apply([](const auto &...e) {
+                size_t h = 0;
+                (combine(h, e), ...);
+                return h;
+            }, v);
+        else if constexpr (requires {declval<T>().begin(); declval<T>().end();} && !is_same_v<T, string>) {
+            size_t h = 0;
+            for (const auto &e : v) combine(h, e);
+            return h;
+        } else return hash<T>{}(v);
+    }
+};
+
+template <typename T>
+struct Fraction : array<T, 2> {
+    using F = array<T, 2>;
+
+    Fraction() = default;
+    Fraction(T n, T d) : F{n, d} {
         reduce();
     }
 
+    T & numer() {
+        return (*this)[0];
+    }
+
+    T & denom() {
+        return (*this)[1];
+    }
+
+    const T & numer() const {
+        return (*this)[0];
+    }
+
+    const T & denom() const {
+        return (*this)[1];
+    }
+
     void reduce() {
-        if (denom < 0) {
-            numer *= -1;
-            denom *= -1;
+        if (denom() < 0) {
+            numer() *= -1;
+            denom() *= -1;
         }
 
-        T g = __gcd(abs(numer), denom);
+        T g = __gcd(abs(numer()), denom());
         if (g) {
-            numer /= g;
-            denom /= g;
+            numer() /= g;
+            denom() /= g;
         }
     }
 
     bool operator<(const Fraction &f) const {
-        return numer * f.denom < f.numer * denom;
+        return numer() * f.denom() < f.numer() * denom();
     }
 
     bool operator>(const Fraction &f) const {
-        return numer * f.denom > f.numer * denom;
+        return numer() * f.denom() > f.numer() * denom();
     }
 
     bool operator==(const Fraction &f) const {
-        return numer == f.numer && denom == f.denom;
+        return numer() == f.numer() && denom() == f.denom();
     }
 
     bool operator!=(const Fraction &f) const {
-        return numer != f.numer || denom != f.denom;
+        return numer() != f.numer() || denom() != f.denom();
     }
 
     bool operator<=(const Fraction &f) const {
@@ -48,97 +86,88 @@ struct Fraction {
     }
 
     Fraction operator+(const Fraction &f) const {
-        return {numer * f.denom + f.numer * denom, denom * f.denom};
+        return {numer() * f.denom() + f.numer() * denom(), denom() * f.denom()};
     }
 
     Fraction operator+(const T &v) const {
-        return {numer + v * denom, denom};
+        return {numer() + v * denom(), denom()};
     }
 
     Fraction & operator+=(const Fraction &f) {
-        numer = numer * f.denom + f.numer * denom;
-        denom *= f.denom;
+        numer() = numer() * f.denom() + f.numer() * denom();
+        denom() *= f.denom();
         reduce();
         return *this;
     }
 
     Fraction & operator+=(const T &v) {
-        numer += v * denom;
+        numer() += v * denom();
         reduce();
         return *this;
     }
 
     Fraction operator-(const Fraction &f) const {
-        return {numer * f.denom - f.numer * denom, denom * f.denom};
+        return {numer() * f.denom() - f.numer() * denom(), denom() * f.denom()};
     }
 
     Fraction operator-(const T &v) const {
-        return {numer - v * denom, denom};
+        return {numer() - v * denom(), denom()};
     }
 
     Fraction & operator-=(const Fraction &f) {
-        numer = numer * f.denom - f.numer * denom;
-        denom *= f.denom;
+        numer() = numer() * f.denom() - f.numer() * denom();
+        denom() *= f.denom();
         reduce();
         return *this;
     }
 
     Fraction & operator-=(const T &v) {
-        numer -= v * denom;
+        numer() -= v * denom();
         reduce();
         return *this;
     }
 
     Fraction operator*(const Fraction &f) const {
-        return {numer * f.numer, denom * f.denom};
+        return {numer() * f.numer(), denom() * f.denom()};
     }
 
     Fraction operator*(const T &v) const {
-        return {numer * v, denom};
+        return {numer() * v, denom()};
     }
 
     Fraction & operator*=(const Fraction &f) {
-        numer *= f.numer;
-        denom *= f.denom;
+        numer() *= f.numer();
+        denom() *= f.denom();
         reduce();
         return *this;
     }
 
     Fraction & operator*=(const T &v) {
-        numer *= v;
+        numer() *= v;
         reduce();
         return *this;
     }
 
     Fraction operator/(const Fraction &f) const {
-        return {numer * f.denom, denom * f.numer};
+        return {numer() * f.denom(), denom() * f.numer()};
     }
 
     Fraction operator/(const T &v) const {
-        return {numer, denom * v};
+        return {numer(), denom() * v};
     }
 
     Fraction & operator/=(const Fraction &f) {
-        numer *= f.denom;
-        denom *= f.numer;
+        numer() *= f.denom();
+        denom() *= f.numer();
         reduce();
         return *this;
     }
 
     Fraction & operator/=(const T &v) {
-        denom *= v;
+        denom() *= v;
         reduce();
         return *this;
     }
-
-    struct FractionHash {
-        size_t operator()(Fraction<T> f) const {
-            auto h = 0ULL;
-            h ^= hash<T>()(f.numer) + 0x9e3779b9 + (h << 6) + (h >> 2);
-            h ^= hash<T>()(f.denom) + 0x9e3779b9 + (h << 6) + (h >> 2);
-            return h;
-        }
-    };
 };
 
 int main() {
@@ -152,11 +181,11 @@ int main() {
     for (int &ci : c) cin >> ci;
     sort(c.begin(), c.end());
 
-    vector<unordered_map<Fraction<int>, string, Fraction<int>::FractionHash>> memo(1 << C);
+    vector<unordered_map<Fraction<int>, string, Hash>> memo(1 << C);
     auto dp = [&](auto &&self, int m1) {
         if (!memo[m1].empty()) return memo[m1];
 
-        unordered_map<Fraction<int>, string, Fraction<int>::FractionHash> solutions;
+        unordered_map<Fraction<int>, string, Hash> solutions;
         if (__builtin_popcount(m1) == 1) {
             for (int i = 0; i < C; i++)
                 if ((m1 >> i) & 1) {
@@ -174,8 +203,8 @@ int main() {
                         if (!solutions.count({f1 - f2})) solutions[{f1 - f2}] = "(" + s1 + "-" + s2 + ")";
                         if (!solutions.count({f2 - f1})) solutions[{f2 - f1}] = "(" + s2 + "-" + s1 + ")";
                         if (!solutions.count({f1 * f2})) solutions[{f1 * f2}] = "(" + s1 + "*" + s2 + ")";
-                        if (f2.numer && !solutions.count({f1 / f2})) solutions[{f1 / f2}] = "(" + s1 + "/" + s2 + ")";
-                        if (f1.numer && !solutions.count({f2 / f1})) solutions[{f2 / f1}] = "(" + s2 + "/" + s1 + ")";
+                        if (f2.numer() && !solutions.count({f1 / f2})) solutions[{f1 / f2}] = "(" + s1 + "/" + s2 + ")";
+                        if (f1.numer() && !solutions.count({f2 / f1})) solutions[{f2 / f1}] = "(" + s2 + "/" + s1 + ")";
                     }
             }
         }
