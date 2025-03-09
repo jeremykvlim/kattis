2a505a73d0da39282cbb175772de221c880d0862
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-03-10 00:06:56 +0800

    Update almostpythagorean.cpp

--- open/almostpythagorean.cpp
+++ open/almostpythagorean.cpp
@@ -9,10 +9,10 @@ int main() {
     cin >> n;
 
     vector<bool> valid(n + 2, false);
-    int bound = ceil(sqrt(n)) + 1;
     vector<long long> squares(n + 2);
     for (auto i = 0LL; i <= n + 1; i++) squares[i] = i * i;
 
+    int bound = ceil(sqrt(n)) + 1;
     for (int a = 0; a <= bound; a++)
         for (int b = a; b <= bound; b++) {
             int sum = squares[a] + squares[b];
