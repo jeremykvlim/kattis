8fb670810c8828ca152de8dd10103095f94294d5
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-03-11 15:20:46 +0800

    Update bribe.cpp

--- nus/cs3233/bribe.cpp
+++ nus/cs3233/bribe.cpp
@@ -1,21 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-double convert(int mask, int need, int money, vector<int> &b, vector<double> &p, vector<vector<double>> &dp) {
-    if (dp[mask][need] > -1) return dp[mask][need];
-    if (!need) return dp[mask][need] = 1;
-    if (need > __builtin_popcount(mask)) return dp[mask][need] = 0;
-
-    double prob = 0;
-    for (int i = 0; i < b.size(); i++) {
-        if (!(mask & 1 << i) || b[i] > money) continue;
-        auto success = p[i] * convert(mask ^ 1 << i, need - 1, money - b[i], b, p, dp), failure = (1 - p[i]) * convert(mask ^ 1 << i, need, money - b[i], b, p, dp);
-        prob = max(prob, success + failure);
-    }
-
-    return dp[mask][need] = prob;
-}
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -31,11 +16,25 @@ int main() {
         vector<double> p(n);
         for (int i = 0; i < n; i++) {
             cin >> b[i] >> p[i];
-            
+
             p[i] /= 100;
         }
 
         vector<vector<double>> dp(1 << n, vector<double>(c + 1, -2));
-        cout << fixed << setprecision(6) << convert((1 << n) - 1, c, m, b, p, dp) << "\n";
+        auto convert = [&](auto &&self, int mask, int c, int m) {
+            if (dp[mask][c] > -1) return dp[mask][c];
+            if (!c) return dp[mask][c] = 1;
+            if (c > popcount((unsigned) mask)) return dp[mask][c] = 0;
+
+            double prob = 0;
+            for (int i = 0; i < b.size(); i++) {
+                if (!((mask >> i) & 1) || b[i] > m) continue;
+                auto success = p[i] * self(self, mask ^ (1 << i), c - 1, m - b[i]), failure = (1 - p[i]) * self(self, mask ^ (1 << i), c, m - b[i]);
+                prob = max(prob, success + failure);
+            }
+
+            return dp[mask][c] = prob;
+        };
+        cout << fixed << setprecision(6) << convert(convert, (1 << n) - 1, c, m) << "\n";
     }
 }
