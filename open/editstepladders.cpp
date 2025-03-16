a97fb56c7791ec9a46a06e4e0615428f02af634e
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-03-17 03:02:15 +0800

    Update editstepladders.cpp

--- open/editstepladders.cpp
+++ open/editstepladders.cpp
@@ -11,13 +11,12 @@ int main() {
     while (cin >> w) {
         int c = 1;
 
-        if (dp0.count(w)) c = max(c, dp0[w] + 1);
+        c = max(c, dp0[w] + 1);
 
         for (int i = 0; i < w.size(); i++) {
             auto s1 = w.substr(0, i) + w.substr(i + 1), s2 = w;
             s2[i] = '#';
-            if (dp1.count(s1)) c = max(c, dp1[s1] + 1);
-            if (dp2.count(s2)) c = max(c, dp2[s2] + 1);
+            c = max({c, dp1[s1] + 1, dp2[s2] + 1});
         }
 
         dp1[w] = c;
@@ -26,10 +25,8 @@ int main() {
         for (int i = 0; i < w.size(); i++) {
             auto s1 = w.substr(0, i) + w.substr(i + 1), s2 = w;
             s2[i] = '#';
-            if (dp0.count(s1)) dp0[s1] = max(dp0[s1], c);
-            else dp0[s1] = c;
-            if (dp2.count(s2)) dp2[s2] = max(dp2[s2], c);
-            else dp2[s2] = c;
+            dp0[s1] = max(dp0[s1], c);
+            dp2[s2] = max(dp2[s2], c);
         }
     }
     cout << count;
