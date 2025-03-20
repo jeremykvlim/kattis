5fcfcea7e4c250e55c0ee3021b17cf955511592c
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-03-20 11:26:14 +0800

    Update t9spelling.java

--- nus/cs2040/t9spelling.java
+++ nus/cs2040/t9spelling.java
@@ -1,21 +1,23 @@
-import java.util.*;
+import java.io.*;
 
 public class t9spelling {
-    public static void main(String[] args) {
-        var sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        var br = new BufferedReader(new InputStreamReader(System.in));
+        var pw = new PrintWriter(System.out);
 
-        int n = Integer.parseInt(sc.nextLine());
+        int n = Integer.parseInt(br.readLine());
         String[] keypad = {"+ ", "", " abc", " def", " ghi", " jkl", " mno", " pqrs", " tuv", " wxyz"};
         for (int x = 1; x <= n; x++) {
             var s = new StringBuilder("Case #" + x + ": ");
-            for (char c : sc.nextLine().toCharArray())
+            for (char c : br.readLine().toCharArray())
                 for (int i = 0; i < keypad.length; i++)
                     if (keypad[i].indexOf(c) != -1) {
                         if (s.length() > 0 && s.charAt(s.length() - 1) - '0' == i) s.append(' ');
                         s.append(String.valueOf(i).repeat(keypad[i].indexOf(c)));
                         break;
                     }
-            System.out.println(s);
+            pw.println(s);
         }
+        pw.flush();
     }
 }
