5178192f0941af4c31188c582890fb75878110df
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-03-21 00:50:20 +0800

    Create powersigns.cpp

---
+++ open/powersigns.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string s;
+    cin >> s;
+
+    s = '0' + s;
+    for (int i = s.size() - 1; ~i; i--) {
+        while (s[i] > '1') {
+            s[i - 1]++;
+            s[i] -= 2;
+        }
+
+        if (1 < i && i < s.size() - 1 && s[i] == '1' && s[i + 1] == '1') {
+            s[i + 1] = '-';
+            s[i] = '0';
+            s[i - 1]++;
+        }
+    }
+    if (s[0] == '0') s.erase(s.begin());
+    for (char &c : s)
+        if (c == '1') c = '+';
+    cout << s;
+}
