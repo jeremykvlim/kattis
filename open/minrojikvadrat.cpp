131316b56e9ae7f436947099688f51c795ba7fbc
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-03-21 07:14:32 +0800

    Update minrojikvadrat.cpp

--- open/minrojikvadrat.cpp
+++ open/minrojikvadrat.cpp
@@ -13,9 +13,8 @@ int main() {
         exit(0);
     }
 
-    int m = 4 * ((n - 2) / 3 + !!(n % 3)) + 8;
+    int i = (n - 2) / 3 + !!(n % 3), m = 4 * i + 8;
     vector<int> gaps(m, 3);
-    int i = (n - 2) / 3 + !!(n % 3);
     gaps[0] = 0;
     gaps[1] = gaps[i + 2] = gaps[i + 3] = gaps[2 * i + 4] = gaps[2 * i + 5] = gaps[3 * i + 6] = gaps[3 * i + 7] = n % 3 ? 1 : 2;
 
