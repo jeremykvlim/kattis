5b8ba75dc91afeb47fa0fc0e4a7415f341cd7188
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-03-21 18:33:01 +0800

    Update towers.cpp

--- open/towers.cpp
+++ open/towers.cpp
@@ -1,21 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-double cmp(vector<int> &t1, int t1_end, vector<int> &t2, int t2_end) {
-    double exponent = 1, scale = 1;
-    for (int i = t2_end - 1; ~i; i--) {
-        double a = i < t2_end ? t2[i] : 1, b = i < t1_end ? t1[i] : 1, ratio = scale * log(b) / log(a) - 1;
-        scale = abs(ratio) < 1e-9 ? 1 : pow(a, exponent * ratio);
-
-        if (scale > 50) return 2;
-        else if (scale < 0.05) return 0;
-
-        exponent = pow(a, exponent);
-    }
-
-    return scale;
-}
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -30,8 +15,24 @@ int main() {
         cin >> tower;
 
         istringstream iss(tower);
-        while (getline(ss, base, '^')) t.emplace_back(stoi(base));
+        while (getline(iss, base, '^')) t.emplace_back(stoi(base));
     }
+
+    auto cmp = [&](vector<int> &t1, int t1_end, vector<int> &t2, int t2_end) -> double {
+        double exponent = 1, scale = 1;
+        for (int i = t2_end - 1; ~i; i--) {
+            double a = i < t2_end ? t2[i] : 1, b = i < t1_end ? t1[i] : 1, ratio = scale * log(b) / log(a) - 1;
+            scale = abs(ratio) < 1e-9 ? 1 : pow(a, ratio * exponent);
+
+            if (scale > 50) return 2;
+            else if (scale < 0.05) return 0;
+
+            exponent = pow(a, exponent);
+        }
+
+        return scale;
+    };
+
     stable_sort(towers.begin(), towers.end(), [&](auto t1, auto t2) {
         int t1_end = find(t1.begin(), t1.end(), 1) - t1.begin(), t2_end = find(t2.begin(), t2.end(), 1) - t2.begin();
         return t1_end < t2_end ? cmp(t1, t1_end, t2, t2_end) < 1 : cmp(t2, t2_end, t1, t1_end) > 1;
