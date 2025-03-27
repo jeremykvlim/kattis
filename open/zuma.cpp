5f28cd9b585acb0581240dabb3c3522a586b73cd
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-03-28 04:03:51 +0800

    Update zuma.cpp

--- open/zuma.cpp
+++ open/zuma.cpp
@@ -1,22 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int marbles(int l, int r, int count, vector<vector<vector<int>>> &dp, vector<int> &colors, int k) {
-    if (l > r) return 0;
-    if (l == r) return k - 1 - count;
-    if (dp[l][r][count] != -1) return dp[l][r][count];
-
-    dp[l][r][count] = INT_MAX;
-    if (count < k - 1) dp[l][r][count] = marbles(l, r, count + 1, dp, colors, k) + 1;
-    if (count == k - 1) dp[l][r][count] = marbles(l + 1, r, 0, dp, colors, k);
-
-    for (int i = l + 1; i <= r; i++)
-        if (colors[i] == colors[l]) 
-            dp[l][r][count] = min(dp[l][r][count], marbles(l + 1, i - 1, 0, dp, colors, k) + marbles(i, r, min(k - 1, count + 1), dp, colors, k));
-
-    return dp[l][r][count];
-}
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -27,6 +11,20 @@ int main() {
     vector<int> colors(n);
     for (int &c : colors) cin >> c;
 
-    vector<vector<vector<int>>> dp(n, vector<vector<int>>(n, vector<int>(k, -1)));
-    cout << marbles(0, n - 1, 0, dp, colors, k);
+    vector<vector<vector<int>>> memo(n, vector<vector<int>>(n, vector<int>(k, -1)));
+    auto dp = [&](auto &&self, int l, int r, int count = 0) {
+        if (l > r) return 0;
+        if (l == r) return k - count - 1;
+        if (memo[l][r][count] != -1) return memo[l][r][count];
+
+        memo[l][r][count] = INT_MAX;
+        if (count < k - 1) memo[l][r][count] = self(self, l, r, count + 1) + 1;
+        if (count == k - 1) memo[l][r][count] = self(self, l + 1, r);
+
+        for (int m = l + 1; m <= r; m++)
+            if (colors[l] == colors[m]) memo[l][r][count] = min(memo[l][r][count], self(self, l + 1, m - 1) + self(self, m, r, min(k - 1, count + 1)));
+
+        return memo[l][r][count];
+    };
+    cout << dp(dp, 0, n - 1);
 }
