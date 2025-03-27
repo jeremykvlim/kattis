a4dd0474cfbde71f632503352cc716ab228c668f
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-03-28 03:58:53 +0800

    Update reset.cpp

--- open/reset.cpp
+++ open/reset.cpp
@@ -1,22 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool complete(vector<pair<int, int>> tasks, long long total, long long &resets, long long attempts, int c) {
-    auto remaining = attempts * c;
-    for (auto [t, d] : tasks) {
-        auto spent = min((long long) t / d + (t % d != 0), attempts);
-        if (remaining < spent) return false;
-
-        remaining -= spent;
-        total -= min((long long) t, attempts * d);
-        c -= spent == attempts;
-    }
-
-    if (total > min(remaining, (long long) c)) return false;
-    resets = attempts;
-    return true;
-}
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -28,11 +12,27 @@ int main() {
     for (auto &[t, d] : tasks) cin >> t >> d;
 
     long long l = 1, r = 1, m, total = accumulate(tasks.begin(), tasks.end(), 0LL, [](auto t, auto p) {return t + p.first;}), resets = 0;
-    while (!complete(tasks, total, resets, r, c)) r <<= 1;
+    auto complete = [&](auto attempts) {
+        auto remaining = attempts * c, required = 0LL;
+        int count = 0;
+        for (auto [t, d] : tasks) {
+            auto spent = min((long long) t / d + (t % d != 0), attempts);
+            if (remaining < spent) return false;
+
+            remaining -= spent;
+            required += min((long long) t, attempts * d);
+            count += spent == attempts;
+        }
+
+        if (total > required + min(remaining, (long long) c - count)) return false;
+        resets = attempts;
+        return true;
+    };
+    while (!complete(r)) r <<= 1;
     while (l + 1 < r) {
         m = l + (r - l) / 2;
 
-        if (complete(tasks, total, resets, m, c)) r = m;
+        if (complete(m)) r = m;
         else l = m;
     }
 
