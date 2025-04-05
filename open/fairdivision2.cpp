01b2e0631dd4dfce339574d17f6ea1d77bd6fb5e
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-04-06 07:44:31 +0800

    Create fairdivision2.cpp

---
+++ open/fairdivision2.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    long long m;
+    cin >> n >> m;
+
+    for (auto q = 2LL;; q++) {
+        auto q_pow = 1LL;
+        for (int _ = 0; _ < n - 1; _++) {
+            if (q_pow > m / q) {
+                cout << "impossible";
+                exit(0);
+            }
+            q_pow *= q;
+        }
+
+        for (auto t = q - 1; t; t--) {
+            auto s = 0LL, a = q_pow;
+            
+            for (int _ = 0; _ < n; _++) {
+                s += a;
+                a /= q;
+                a *= t;
+                if (s > m) goto next;
+            }
+
+
+            if (!(m % s)) {
+                auto p = q - t;
+                cout << p << " " << q;
+                exit(0);
+            }
+            next:;
+        }
+    }
+}
