6c3e89d2ce4919e58e29f99ac50be8e702577b14
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-04-08 21:41:12 +0800

    Create simplification.cpp

---
+++ nus/cs3233/simplification.cpp
@@ -0,0 +1,159 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+bool approximately_equal(const T &v1, const T &v2, double epsilon = 1e-5) {
+    return fabs(v1 - v2) <= epsilon;
+}
+
+template <typename T>
+struct Point {
+    T x, y;
+
+    Point() {}
+    Point(T x, T y) : x(x), y(y) {}
+
+    template <typename U>
+    Point(U x, U y) : x(x), y(y) {}
+
+    template <typename U>
+    Point(const Point<U> &p) : x((T) p.x), y((T) p.y) {}
+
+    const auto begin() const {
+        return &x;
+    }
+
+    const auto end() const {
+        return &y + 1;
+    }
+
+    Point operator-() const {
+        return {-x, -y};
+    }
+
+    bool operator<(const Point &p) const {
+        return !approximately_equal(x, p.x) ? x < p.x : y < p.y;
+    }
+
+    bool operator>(const Point &p) const {
+        return !approximately_equal(x, p.x) ? x > p.x : y > p.y;
+    }
+
+    bool operator==(const Point &p) const {
+        if constexpr (is_floating_point_v<T>) return approximately_equal(x, p.x) && approximately_equal(y, p.y);
+        return x == p.x && y == p.y;
+    }
+
+    bool operator!=(const Point &p) const {
+        if constexpr (is_floating_point_v<T>) return !approximately_equal(x, p.x) || !approximately_equal(y, p.y);
+        return x != p.x || y != p.y;
+    }
+
+    bool operator<=(const Point &p) const {
+        return *this < p || *this == p;
+    }
+
+    bool operator>=(const Point &p) const {
+        return *this > p || *this == p;
+    }
+
+    Point operator+(const Point &p) const {
+        return {x + p.x, y + p.y};
+    }
+
+    Point operator+(const T &v) const {
+        return {x + v, y + v};
+    }
+
+    Point & operator+=(const Point &p) {
+        x += p.x;
+        y += p.y;
+        return *this;
+    }
+
+    Point & operator+=(const T &v) {
+        x += v;
+        y += v;
+        return *this;
+    }
+
+    Point operator-(const Point &p) const {
+        return {x - p.x, y - p.y};
+    }
+
+    Point operator-(const T &v) const {
+        return {x - v, y - v};
+    }
+
+    Point & operator-=(const Point &p) {
+        x -= p.x;
+        y -= p.y;
+        return *this;
+    }
+
+    Point & operator-=(const T &v) {
+        x -= v;
+        y -= v;
+        return *this;
+    }
+
+    Point operator*(const T &v) const {
+        return {x * v, y * v};
+    }
+
+    Point & operator*=(const T &v) {
+        x *= v;
+        y *= v;
+        return *this;
+    }
+
+    Point operator/(const T &v) const {
+        return {x / v, y / v};
+    }
+
+    Point & operator/=(const T &v) {
+        x /= v;
+        y /= v;
+        return *this;
+    }
+};
+
+template <typename T>
+T cross(const Point<T> &a, const Point<T> &b, const Point<T> &c) {
+    return (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n, m;
+    cin >> n >> m;
+
+    vector<Point<int>> points(n + 1);
+    for (auto &[x, y] : points) cin >> x >> y;
+
+    vector<int> l(n + 1), r(n + 1);
+    iota(l.begin(), l.end(), -1);
+    iota(r.begin(), r.end(), 1);
+
+    priority_queue<array<int, 4>, vector<array<int, 4>>, greater<>> pq;
+    for (int i = 1; i < n; i++) pq.push({abs(cross(points[i], points[i - 1], points[i + 1])), i, i - 1, i + 1});
+    vector<bool> removed(n + 1, false);
+    int steps = n - m;
+    while (!pq.empty() && steps) {
+        auto [a, i, li, ri] = pq.top();
+        pq.pop();
+
+        if (removed[i] || li != l[i] || ri != r[i]) continue;
+
+        cout << i << "\n";
+        removed[i] = true;
+        steps--;
+        r[li] = ri;
+        l[ri] = li;
+
+        if (l[li] >= 0) pq.push({abs(cross(points[li], points[l[li]], points[ri])), li, l[li], ri});
+        if (r[ri] <= n) pq.push({abs(cross(points[ri], points[li], points[r[ri]])), ri, li, r[ri]});
+    }
+}
