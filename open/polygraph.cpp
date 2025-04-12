6713df4bb153d0fc32ae515154d70270305f7972
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-04-12 16:04:09 +0800

    Update polygraph.cpp

--- open/polygraph.cpp
+++ open/polygraph.cpp
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
+#include <tr2/dynamic_bitset>
 using namespace std;
+using namespace tr2;
 
 int main() {
     ios::sync_with_stdio(false);
@@ -17,28 +19,28 @@ int main() {
         for (auto &st : statements) {
             string s;
             getline(cin, s);
-            
+
             istringstream iss(s);
             st = {istream_iterator<string>(iss), istream_iterator<string>()};
         }
 
-        vector<bitset<501>> bitmasks(m);
+        vector<dynamic_bitset<>> bitmasks(m, dynamic_bitset<>(n + 1));
         for (int i = 0; i < m; i++) {
             bitmasks[i].flip(stoi(statements[i][0]));
             for (int j = 2; j < statements[i].size(); j++) bitmasks[i].flip(stoi(statements[i][j]));
 
-            if (statements[i][1] == "S" || statements[i][1] == "L") bitmasks[i] ^= 1;
+            if (statements[i][1] == "S" || statements[i][1] == "L") bitmasks[i].flip(0);
         }
 
         for (int i = 1; i <= n; i++) {
-            for (auto mask : bitmasks)
-                if (mask[i]) {
-                    bitset<501> temp;
+            for (auto m1 : bitmasks)
+                if (m1[i]) {
+                    dynamic_bitset<> temp(n + 1);
                     for (int j = 1; j < i; j++) temp[j] = true;
 
-                    if ((mask & temp).none()) {
-                        for (auto &bm : bitmasks)
-                            if (bm[i] && bm != mask) bm ^= mask;
+                    if ((m1 & temp).none()) {
+                        for (auto &m2 : bitmasks)
+                            if (m2[i] && m2 != m1) m2 ^= m1;
                         break;
                     }
                 }
@@ -46,11 +48,11 @@ int main() {
 
         sort(bitmasks.begin(), bitmasks.end(), [](auto b1, auto b2) {return b1.to_string() < b2.to_string();});
         string cities(n, '-');
-        for (auto b : bitmasks) {
-            int msb = b._Find_first();
-            while (b._Find_next(msb) != 501) msb = b._Find_next(msb);
+        for (auto mask : bitmasks) {
+            int msb = mask.find_first();
+            while (mask.find_next(msb) <= n) msb = mask.find_next(msb);
 
-            if (msb && (bitset<501>(1) << (msb - 1)) == b >> 1) cities[msb - 1] = b[0] ? 'T' : 'L';
+            if (msb && dynamic_bitset<>(n + 1, 1) << (msb - 1) == mask >> 1) cities[msb - 1] = mask[0] ? 'T' : 'L';
         }
 
         cout << "Case #" << x + 1 << ": ";
