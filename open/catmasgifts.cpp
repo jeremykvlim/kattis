5c23ac56020bd06d7c29fb56d24782aafb6700ec
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-04-15 20:37:38 +0800

    Update catmasgifts.cpp

--- open/catmasgifts.cpp
+++ open/catmasgifts.cpp
@@ -12,10 +12,10 @@ int main() {
     dp1[1][0][0] = dp1[1][0][1] = dp2[1][0][0] = 1;
     for (int i = 2; i <= n; i++) {
         auto P = [&](int j) -> pair<double, double> {
-            if (!j) return {0, 0};
+            if (!~j) return {0, 0};
             auto p = (double) j / (i - 1), q = 1 - p;
-            return {p * dp1[i - 1][j - 1][1] + q * dp1[i - 1][j][0],
-                    p * dp2[i - 1][j - 1][1] + q * dp2[i - 1][j][0]};
+            return {p * (j ? dp1[i - 1][j - 1][1] : 0) + q * dp1[i - 1][j][0],
+                    p * (j ? dp2[i - 1][j - 1][1] : 0) + q * dp2[i - 1][j][0]};
         };
 
         for (int j = 0; j <= i - 1; j++)
