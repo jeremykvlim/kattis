679845e5af5628e94e06dee7966f8d2db935acce
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-04-22 13:20:57 +0800

    Create breadfirstsearch.cpp

---
+++ open/breadfirstsearch.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> adj_list(n + 1);
+    while (m--) {
+        int v1, v2;
+        cin >> v1 >> v2;
+
+        adj_list[v1].emplace_back(v2);
+        adj_list[v2].emplace_back(v1);
+    }
+
+    vector<int> a(n), indices(n + 1);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+
+        indices[a[i]] = i;
+    }
+
+    for (int v1 = 1; v1 <= n; v1++) sort(adj_list[v1].begin(), adj_list[v1].end(), [&](int a1, int a2) {return indices[a1] < indices[a2];});
+
+    int count = 0;
+    vector<bool> bread(n + 1, false);
+    bool change;
+    do {
+        change = false;
+
+        vector<bool> visited(n + 1, false);
+        deque<int> dq{1};
+        for (int i = 0; !dq.empty() && i < n;) {
+            int v = dq.front();
+            dq.pop_front();
+
+            if (visited[v]) continue;
+            visited[v] = true;
+
+            if (v != a[i]) {
+                if (!bread[a[i]]) {
+                    bread[a[i]] = true;
+                    count++;
+                }
+                change = true;
+                break;
+            }
+
+            for (int j = adj_list[v].size() - 1; ~j; j--) {
+                int u = adj_list[v][j];
+                if (!visited[u] && bread[u]) dq.emplace_front(u);
+            }
+
+            for (int u : adj_list[v])
+                if (!visited[u] && !bread[u]) dq.emplace_back(u);
+            i++;
+        }
+    } while (change);
+    cout << count;
+}
