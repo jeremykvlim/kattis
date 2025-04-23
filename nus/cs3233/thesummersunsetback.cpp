5c2ad6fc13c55c5aa59d7f071b18f4504881be29
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-04-23 19:39:32 +0800

    Create thesummersunsetback.cpp

---
+++ nus/cs3233/thesummersunsetback.cpp
@@ -0,0 +1,215 @@
+#include <bits/stdc++.h>
+#include <tr2/dynamic_bitset>
+using namespace std;
+using namespace tr2;
+
+struct LaminarTree {
+    int n;
+    vector<vector<int>> LT;
+
+    LaminarTree(int n, const vector<array<int, 3>> &edges) : n(n), LT(2 * n - 1) {
+        build(edges);
+    }
+
+    void build(const vector<array<int, 3>> &edges) {
+        vector<vector<pair<int, int>>> adj_list(2 * n - 1);
+        for (auto [u, v, w] : edges) {
+            adj_list[u].emplace_back(v, w);
+            adj_list[v].emplace_back(u, w);
+        }
+
+        vector<bool> add(2 * n - 1, true);
+        for (int i = 0; i < n - 1; i++) {
+            vector<bool> visited(n + i, false);
+            vector<int> order(n - i, -1), dist(n + i, 0);
+            priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+            for (int v = 0; v < n + i; v++)
+                if (add[v]) {
+                    for (auto [u, w] : adj_list[v]) dist[v] += w;
+                    pq.emplace(dist[v], v);
+                }
+
+            for (int j = 0; j < n - i; j++)
+                while (!~order[j] && !pq.empty()) {
+                    auto [d, v] = pq.top();
+                    pq.pop();
+
+                    if (visited[v]) continue;
+                    visited[v] = true;
+
+                    order[j] = v;
+                    for (auto [u, w] : adj_list[v])
+                        if (!visited[u]) {
+                            dist[u] -= w;
+                            pq.emplace(dist[u], u);
+                        }
+                }
+
+            int u = order[n - i - 1], v = order[n - i - 2];
+            add[u] = add[v] = false;
+            adj_list[u].clear();
+            adj_list[v].clear();
+            LT[n + i].emplace_back(u);
+            LT[n + i].emplace_back(v);
+
+            for (int s = 0; s < n + i; s++)
+                if (add[s])
+                    for (auto &[t, w] : adj_list[s]) {
+                        if (t == u || t == v) {
+                            t = n + i;
+                            adj_list[n + i].emplace_back(s, w);
+                        }
+                    }
+        }
+    }
+
+    vector<vector<bool>> subtree_masks() {
+        vector<vector<bool>> masks(n - 1, vector<bool>(2 * n - 1, false));
+        for (int i = 0; i < n - 1; i++) {
+            masks[i][LT[i + n][0]] = true;
+            stack<int> st;
+            st.emplace(LT[i + n][0]);
+            while (!st.empty()) {
+                int v = st.top();
+                st.pop();
+
+                if (v >= n)
+                    for (int u : LT[v])
+                        if (!masks[i][u]) {
+                            masks[i][u] = true;
+                            st.emplace(u);
+                        }
+
+            }
+        }
+        return masks;
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, s;
+    cin >> n >> s;
+
+    dynamic_bitset<> p_assigned(s), p_unassigned(s);
+    vector<array<int, 3>> unassigned;
+    for (int i = 1; i <= n; i++) {
+        char ch;
+        cin >> ch;
+
+        if (ch == 'A') {
+            int c;
+            cin >> c;
+            c--;
+
+            p_assigned.flip(c);
+        } else {
+            int ai, bi;
+            cin >> ai >> bi;
+            ai--;
+            bi--;
+
+            unassigned.push_back({ai, bi, i});
+            p_unassigned.flip(ai);
+        }
+    }
+    int U = unassigned.size();
+
+    vector<vector<int>> adj_list(s);
+    for(int i = 0; i < U; i++) {
+        adj_list[unassigned[i][0]].emplace_back(unassigned[i][1]);
+        adj_list[unassigned[i][1]].emplace_back(unassigned[i][0]);
+    }
+
+    vector<vector<int>> components(s);
+    vector<bool> visited(s, false);
+    for (int i = 0; i < s; i++) {
+        if (!visited[i]) {
+            visited[i] = true;
+            components[i].emplace_back(i);
+            queue<int> q;
+            q.emplace(i);
+            while (!q.empty()) {
+                int v = q.front();
+                q.pop();
+
+                for (int u : adj_list[v]) {
+                    if (!visited[u]) {
+                        visited[u] = true;
+                        q.emplace(u);
+                        components[i].emplace_back(u);
+                    }
+                }
+            }
+
+            int xor_sum = 0;
+            for (int v : components[i]) xor_sum ^= (p_assigned[v] + p_unassigned[v] + 1) & 1;
+
+            if (xor_sum) {
+                cout << 0;
+                exit(0);
+            }
+        }
+    }
+
+    int m = 1e9;
+    dynamic_bitset<> events(s);
+    for (int i = 0; i < s; i++) {
+        int k = components[i].size();
+        if (k <= 1) continue;
+
+        vector<int> indices(s, -1);
+        for (int j = 0; j < k; j++) indices[components[i][j]] = j;
+
+        vector<array<int, 3>> edges;
+        for (int j = 0; j < U; j++) {
+            int u = indices[unassigned[j][0]], v = indices[unassigned[j][1]];
+            if (~u && ~v) edges.push_back({u, v, 1});
+        }
+        if (edges.empty()) continue;
+
+        LaminarTree lt(k, edges);
+        for (auto mask : lt.subtree_masks()) {
+            int count = 0;
+            for (int j = 0; j < U; j++) {
+                int u = indices[unassigned[j][0]], v = indices[unassigned[j][1]];
+                if ((~u && mask[u]) ^ (~v && mask[v])) count++;
+            }
+
+            if (count && count < m) {
+                m = count;
+                dynamic_bitset<> temp(s);
+                for (int j = 0; j < k; j++)
+                    if (mask[j]) temp.set(components[i][j]);
+                events = temp;
+            }
+        }
+    }
+
+    if (m == 1e9) {
+        cout << "IMPOSSIBLE";
+        exit(0);
+    }
+
+    dynamic_bitset<> dbs_a(U), dbs_b(U);
+    vector<int> cross;
+    for (int i = 0; i < U; i++) {
+        dbs_a[i] = events[unassigned[i][0]];
+        dbs_b[i] = events[unassigned[i][1]];
+        if (dbs_a[i] ^ dbs_b[i]) cross.emplace_back(i);
+    }
+
+    vector<pair<int, int>> deceive;
+    if (!((events.count() ^ (p_assigned & events).count() ^ (dbs_a & dbs_b).count()) & 1) && !cross.empty()) {
+        int i = cross[0];
+        deceive.emplace_back(unassigned[i][2], (events[unassigned[i][0]] ? unassigned[i][0] : unassigned[i][1]) + 1);
+        for (int j : cross)
+            if (i != j) deceive.emplace_back(unassigned[j][2], (events[unassigned[j][0]] ? unassigned[j][1] : unassigned[j][0]) + 1);
+    } else
+        for (int i : cross) deceive.emplace_back(unassigned[i][2], (events[unassigned[i][0]] ? unassigned[i][1] : unassigned[i][0]) + 1);
+
+    cout << m << "\n";
+    for (auto [vi, si] : deceive) cout << vi << " " << si << "\n";
+}
