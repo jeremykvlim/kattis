d5454eebe07de5314d22174daf676933df3e4a6f
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-04-27 01:20:31 +0800

    Update lostmap.java

--- nus/cs2040/lostmap.java
+++ nus/cs2040/lostmap.java
@@ -10,35 +10,37 @@ public class lostmap {
         var adjMatrix = new int[n][n];
         for (int i = 0; i < n; i++) adjMatrix[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
-        for (var e : prim_dense(n, adjMatrix)) pw.println(e.first + " " + e.second);
+        for (var e : prim_dense(n, adjMatrix).first) pw.println(e.first + " " + e.second);
         pw.flush();
     }
 
-    static ArrayList<Pair<Integer, Integer>> prim_dense(int n, int[][] adjMatrix) {
+    static Tuple<ArrayList<Pair<Integer, Integer>>, Integer> prim_dense(int n, int[][] adjMatrix) {
         var dist = new int[n];
         Arrays.fill(dist, Integer.MAX_VALUE);
         dist[0] = 0;
         var visited = new boolean[n];
         var mst = new ArrayList<Pair<Integer, Integer>>(n - 1);
         for (int i = 0; i < n - 1; i++) mst.add(new Pair<>(-1, -1));
-        for (int i = 0; i < n; i++) {
-            int d = Integer.MAX_VALUE, j = 0;
-            for (int k = 0; k < n; k++)
-                if (!visited[k] && d > dist[k]) {
-                    d = dist[k];
-                    j = k;
-                }
 
-            visited[j] = true;
+        int weight = 0;
+        for (int __ = 0; __ < n; __++) {
+            int u = -1;
+            for (int v = 0; v < n; v++)
+                if (!visited[v] && (u < 0 || dist[v] < dist[u])) u = v;
+
+            visited[u] = true;
+            weight += dist[u];
             for (int k = 0; k < n; k++)
-                if (!visited[k] && dist[k] > adjMatrix[j][k]) {
-                    dist[k] = adjMatrix[j][k];
-                    mst.set(k - 1, new Pair<>(Math.min(j, k) + 1, Math.max(j, k) + 1));
+                if (!visited[k] && dist[k] > adjMatrix[u][k]) {
+                    dist[k] = adjMatrix[u][k];
+                    mst.set(k - 1, new Pair<>(Math.min(u, k) + 1, Math.max(u, k) + 1));
                 }
         }
 
-        return mst;
+        return new Tuple<>(mst, weight);
     }
+
+    record Tuple<T, U>(T first, U second) {}
 
     record Pair<T extends Comparable<T>, U extends Comparable<U>>(T first, U second) implements Comparable<Pair<T, U>> {
         @Override
