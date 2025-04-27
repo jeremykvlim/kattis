4c4b12f816f1ec24a4d30b997162ded709a3d32f
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-04-27 23:12:43 +0800

    Update fikaoptimization.cpp

--- open/fikaoptimization.cpp
+++ open/fikaoptimization.cpp
@@ -1,6 +1,84 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct CentroidDecomposition {
+    vector<vector<int>> adj_list;
+    vector<bool> removed;
+    vector<int> subtree_size, depth;
+    vector<long long> sum;
+
+    CentroidDecomposition(int n) : adj_list(n), removed(n, false), subtree_size(n, 0), depth(n, -1), sum(n, 0) {}
+
+    void add_edge(int u, int v) {
+        adj_list[u].emplace_back(v);
+        adj_list[v].emplace_back(u);
+    }
+
+    void decompose(const vector<int> &b, const vector<vector<int>> &indices, int root = 1) {
+        auto dfs1 = [&](auto &&self, int v, int prev = -1) -> void {
+            subtree_size[v] = 1;
+            for (int u : adj_list[v])
+                if (u != prev && !removed[u]) {
+                    self(self, u, v);
+                    subtree_size[v] += subtree_size[u];
+                }
+        };
+        dfs1(dfs1, root);
+
+        auto dfs2 = [&](auto &&self, int size, int v, int prev = -1) -> int {
+            for (int u : adj_list[v])
+                if (u != prev && !removed[u])
+                    if (2 * subtree_size[u] > size) return self(self, size, u, v);
+            return v;
+        };
+        int c = dfs2(dfs2, subtree_size[root], root);
+
+        queue<int> q;
+        auto process = [&](int s, bool subtree = false) {
+            vector<int> path{s};
+            depth[s] = subtree;
+            q.emplace(s);
+            while (!q.empty()) {
+                int v = q.front();
+                q.pop();
+
+                for (int u : adj_list[v])
+                    if (!removed[u] && depth[u] == -1) {
+                        q.emplace(u);
+                        path.emplace_back(u);
+                        depth[u] = depth[v] + 1;
+                    }
+            }
+
+            vector<int> diff;
+            for (int v : path)
+                for (int i : indices[v])
+                    if (b[i] >= depth[v]) diff.emplace_back(b[i] - depth[v]);
+            sort(diff.begin(), diff.end());
+
+            int k = diff.size();
+            vector<long long> pref(k + 1, 0);
+            for (int i = 0; i < k; i++) pref[i + 1] = pref[i] + diff[i];
+
+            for (int v : path) {
+                int i = lower_bound(diff.begin(), diff.end(), depth[v]) - diff.begin();
+                if (i < k) sum[v] += (!subtree ? 1 : -1) * (pref[k] - pref[i] - (long long) depth[v] * (k - i));
+                depth[v] = -1;
+            }
+        };
+        process(c);
+
+        removed[c] = true;
+
+        for (int v : adj_list[c])
+            if (!removed[v]) process(v, true);
+
+        for (int v : adj_list[c])
+            if (!removed[v]) decompose(b, indices, v);
+    }
+};
+
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -8,13 +86,12 @@ int main() {
     int n, m;
     cin >> n >> m;
 
-    vector<vector<int>> adj_list(n + 1);
+    CentroidDecomposition cd(n + 1);
     for (int _ = 0; _ < n - 1; _++) {
         int u, v;
         cin >> u >> v;
 
-        adj_list[u].emplace_back(v);
-        adj_list[v].emplace_back(u);
+        cd.add_edge(u, v);
     }
 
     auto total = 0LL;
@@ -28,74 +105,6 @@ int main() {
         indices[r].emplace_back(i);
     }
 
-    vector<long long> sum(n + 1, 0);
-    vector<int> subtree_size(n + 1), dist(n + 1, -1);
-    vector<bool> removed(n + 1, false);
-    queue<int> q;
-    auto decompose = [&](auto &&self, int root = 1) -> void {
-        auto centroid = [&]() {
-            auto dfs1 = [&](auto &&self, int v, int prev = -1) -> void {
-                subtree_size[v] = 1;
-                for (int u : adj_list[v])
-                    if (u != prev && !removed[u]) {
-                        self(self, u, v);
-                        subtree_size[v] += subtree_size[u];
-                    }
-            };
-            dfs1(dfs1, root);
-
-            auto dfs2 = [&](auto &&self, int size, int v, int prev = -1) -> int {
-                for (int u : adj_list[v])
-                    if (u != prev && !removed[u])
-                        if (2 * subtree_size[u] > size) return self(self, size, u, v);
-                return v;
-            };
-            return dfs2(dfs2, subtree_size[root], root);
-        };
-        int c = centroid();
-        removed[c] = true;
-
-        auto process = [&](int s, bool subtree = false) {
-            vector<int> path{s};
-            dist[s] = subtree;
-            q.emplace(s);
-            while (!q.empty()) {
-                int v = q.front();
-                q.pop();
-
-                for (int u : adj_list[v])
-                    if (!removed[u] && dist[u] == -1) {
-                        q.emplace(u);
-                        path.emplace_back(u);
-                        dist[u] = dist[v] + 1;
-                    }
-            }
-
-            vector<int> diff;
-            for (int v : path)
-                for (int i : indices[v])
-                    if (b[i] >= dist[v]) diff.emplace_back(b[i] - dist[v]);
-            sort(diff.begin(), diff.end());
-
-            int k = diff.size();
-            vector<long long> pref(k + 1, 0);
-            for (int i = 0; i < k; i++) pref[i + 1] = pref[i] + diff[i];
-
-            for (int v : path) {
-                int i = lower_bound(diff.begin(), diff.end(), dist[v]) - diff.begin();
-                if (i < k) sum[v] += (!subtree ? 1 : -1) * (pref[k] - pref[i] - (long long) dist[v] * (k - i));
-                dist[v] = -1;
-            }
-        };
-        process(c);
-
-        for (int v : adj_list[c])
-            if (!removed[v]) process(v, true);
-
-        for (int v : adj_list[c])
-            if (!removed[v]) self(self, v);
-    };
-    decompose(decompose);
-
-    cout << total + *max_element(sum.begin(), sum.end());
+    cd.decompose(b, indices);
+    cout << total + *max_element(cd.sum.begin(), cd.sum.end());
 }
