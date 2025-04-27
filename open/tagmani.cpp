8484b6b943f5685716c1e7011a7dacbe5b110164
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-04-27 23:19:13 +0800

    Update tagmani.cpp

--- open/tagmani.cpp
+++ open/tagmani.cpp
@@ -20,13 +20,12 @@ struct FenwickTree {
 };
 
 struct CentroidDecomposition {
-    int n;
     vector<vector<pair<int, int>>> adj_list, centroids;
     vector<bool> removed;
     vector<int> subtree_size;
     vector<vector<long long>> dist;
 
-    CentroidDecomposition(int n) : n(n), adj_list(n), centroids(n), removed(n, false), subtree_size(n, 0) {}
+    CentroidDecomposition(int n) : adj_list(n), centroids(n), removed(n, false), subtree_size(n, 0) {}
 
     void add_edge(int u, int v, int w) {
         adj_list[u].emplace_back(v, w);
@@ -51,8 +50,9 @@ struct CentroidDecomposition {
             return v;
         };
         int c = dfs2(dfs2, subtree_size[root], root);
+        removed[c] = true;
 
-        if (dist.size() <= layer) dist.emplace_back(vector<long long>(n, 0));
+        if (dist.size() <= layer) dist.emplace_back(vector<long long>(adj_list.size(), 0));
         auto dfs3 = [&](auto &&self, int v, int prev = -1, long long d = 0) -> void {
             dist[layer][v] = d;
             centroids[v].emplace_back(c, layer);
@@ -61,10 +61,12 @@ struct CentroidDecomposition {
         };
         dfs3(dfs3, c);
 
-        removed[c] = true;
-
         for (auto [v, w] : adj_list[c])
             if (!removed[v]) decompose(v, layer + 1);
+    }
+
+    auto & operator[](int i) {
+        return centroids[i];
     }
 };
 
@@ -90,45 +92,43 @@ int main() {
 
     vector<vector<long long>> keys(n + 1);
     for (auto [u, v, t, d] : trains)
-        for (auto [c, lvl] : cd.centroids[u]) keys[c].emplace_back(t - cd.dist[lvl][u]);
+        for (auto [c, lvl] : cd[u]) keys[c].emplace_back(t - cd.dist[lvl][u]);
 
-    int s = 0;
     for (int c = 1; c <= n; c++) {
         sort(keys[c].begin(), keys[c].end());
         keys[c].erase(unique(keys[c].begin(), keys[c].end()), keys[c].end());
-        s = max(s, (int) keys[c].size());
     }
 
-    vector<int> dp(k, 0);
     vector<FenwickTree<int>> fws;
     for (int c = 0; c <= n; c++) fws.emplace_back(keys[c].size() + 1, [&](int x, int y) { return max(x, y); });
     for (int i = 0; i < k; i++) {
         auto [u, v, t, d] = trains[i];
 
-        for (auto [c, lvl] : cd.centroids[v]) {
+        int dp = 0;
+        for (auto [c, layer] : cd[v]) {
             if (keys[c].empty()) continue;
 
-            int j = lower_bound(keys[c].begin(), keys[c].end(), cd.dist[lvl][v] + t + d) - keys[c].begin();
-            if (j < keys[c].size()) dp[i] = max(dp[i], fws[c].range_query(keys[c].size() - j));
+            int j = lower_bound(keys[c].begin(), keys[c].end(), cd.dist[layer][v] + t + d) - keys[c].begin();
+            if (j < keys[c].size()) dp = max(dp, fws[c].range_query(keys[c].size() - j));
         }
-        dp[i]++;
 
-        for (auto [c, lvl] : cd.centroids[u]) {
+        for (auto [c, layer] : cd[u]) {
             if (keys[c].empty()) continue;
 
-            int j = lower_bound(keys[c].begin(), keys[c].end(), t - cd.dist[lvl][u]) - keys[c].begin();
-            fws[c].update(keys[c].size() - j, dp[i]);
+            int j = lower_bound(keys[c].begin(), keys[c].end(), t - cd.dist[layer][u]) - keys[c].begin();
+            fws[c].update(keys[c].size() - j, dp + 1);
         }
     }
 
-    vector<int> x(n + 1, 0);
-    for (int i = 1; i <= n; i++)
-        for (auto [c, lvl] : cd.centroids[i]) {
+    for (int v = 1; v <= n; v++) {
+        int x = 0;
+
+        for (auto [c, layer] : cd[v]) {
             if (keys[c].empty()) continue;
 
-            int j = lower_bound(keys[c].begin(), keys[c].end(), cd.dist[lvl][i]) - keys[c].begin();
-            if (j < keys[c].size()) x[i] = max(x[i], fws[c].range_query(keys[c].size() - j));
+            int j = lower_bound(keys[c].begin(), keys[c].end(), cd.dist[layer][v]) - keys[c].begin();
+            if (j < keys[c].size()) x = max(x, fws[c].range_query(keys[c].size() - j));
         }
-
-    for (int i = 1; i <= n; i++) cout << x[i] << " ";
+        cout << x << " ";
+    }
 }
