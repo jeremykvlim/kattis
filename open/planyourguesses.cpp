4ed4f56b9e63edb9f47d9e234fd77e0e960c93aa
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-05-06 13:03:14 +0800

    Create planyourguesses.cpp

---
+++ open/planyourguesses.cpp
@@ -0,0 +1,192 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+struct Fraction : array<T, 2> {
+    using F = array<T, 2>;
+
+    Fraction() = default;
+    Fraction(T n, T d) : F{n, d} {
+        reduce();
+    }
+
+    T & numer() {
+        return (*this)[0];
+    }
+
+    T & denom() {
+        return (*this)[1];
+    }
+
+    const T & numer() const {
+        return (*this)[0];
+    }
+
+    const T & denom() const {
+        return (*this)[1];
+    }
+
+    void reduce() {
+        if (denom() < 0) {
+            numer() *= -1;
+            denom() *= -1;
+        }
+
+        T g = __gcd(abs(numer()), denom());
+        if (g) {
+            numer() /= g;
+            denom() /= g;
+        }
+    }
+
+    bool operator<(const Fraction &f) const {
+        return numer() * f.denom() < f.numer() * denom();
+    }
+
+    bool operator>(const Fraction &f) const {
+        return numer() * f.denom() > f.numer() * denom();
+    }
+
+    bool operator==(const Fraction &f) const {
+        return numer() == f.numer() && denom() == f.denom();
+    }
+
+    bool operator!=(const Fraction &f) const {
+        return numer() != f.numer() || denom() != f.denom();
+    }
+
+    bool operator<=(const Fraction &f) const {
+        return *this < f || *this == f;
+    }
+
+    bool operator>=(const Fraction &f) const {
+        return *this > f || *this == f;
+    }
+
+    Fraction operator+(const Fraction &f) const {
+        return {numer() * f.denom() + f.numer() * denom(), denom() * f.denom()};
+    }
+
+    Fraction operator+(const T &v) const {
+        return {numer() + v * denom(), denom()};
+    }
+
+    Fraction & operator+=(const Fraction &f) {
+        numer() = numer() * f.denom() + f.numer() * denom();
+        denom() *= f.denom();
+        reduce();
+        return *this;
+    }
+
+    Fraction & operator+=(const T &v) {
+        numer() += v * denom();
+        reduce();
+        return *this;
+    }
+
+    Fraction operator-(const Fraction &f) const {
+        return {numer() * f.denom() - f.numer() * denom(), denom() * f.denom()};
+    }
+
+    Fraction operator-(const T &v) const {
+        return {numer() - v * denom(), denom()};
+    }
+
+    Fraction & operator-=(const Fraction &f) {
+        numer() = numer() * f.denom() - f.numer() * denom();
+        denom() *= f.denom();
+        reduce();
+        return *this;
+    }
+
+    Fraction & operator-=(const T &v) {
+        numer() -= v * denom();
+        reduce();
+        return *this;
+    }
+
+    Fraction operator*(const Fraction &f) const {
+        return {numer() * f.numer(), denom() * f.denom()};
+    }
+
+    Fraction operator*(const T &v) const {
+        return {numer() * v, denom()};
+    }
+
+    Fraction & operator*=(const Fraction &f) {
+        numer() *= f.numer();
+        denom() *= f.denom();
+        reduce();
+        return *this;
+    }
+
+    Fraction & operator*=(const T &v) {
+        numer() *= v;
+        reduce();
+        return *this;
+    }
+
+    Fraction operator/(const Fraction &f) const {
+        return {numer() * f.denom(), denom() * f.numer()};
+    }
+
+    Fraction operator/(const T &v) const {
+        return {numer(), denom() * v};
+    }
+
+    Fraction & operator/=(const Fraction &f) {
+        numer() *= f.denom();
+        denom() *= f.numer();
+        reduce();
+        return *this;
+    }
+
+    Fraction & operator/=(const T &v) {
+        denom() *= v;
+        reduce();
+        return *this;
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long long n;
+    int m, l;
+    cin >> n >> m >> l;
+    if (!l) l = m;
+
+    vector<long long> p2(m + 1, 1);
+    for (int i = 1; i <= m; i++) p2[i] = p2[i - 1] * 2;
+
+    auto greedy = [&](auto size, auto guesses, bool extra = true) -> long long {
+        if (!size) return 0;
+
+        auto count = 0LL;
+        if (extra) {
+            count++;
+            if (size <= guesses) return count;
+            size -= guesses;
+        }
+
+        for (int i = guesses; i; i--) {
+            count += p2[i - 1] * (size / (p2[i] - 1));
+            size %= p2[i] - 1;
+            if (size > p2[i] - 1 - i) {
+                size = p2[i] - 1 - i;
+                count++;
+            }
+        }
+        return count;
+    };
+
+    auto count = n > p2[l] - 2 ? greedy(n - p2[l] + 2, m) + greedy(p2[l] - 2, m - 1, false) - (l > 1) : greedy(n, l - 1) - 1;
+    if (!count) {
+        cout << "0 1";
+        exit(0);
+    }
+
+    Fraction<long long> p(count, n);
+    cout << p.numer() << " " << p.denom();
+}
