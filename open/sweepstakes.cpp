289c763554cd0c0ed71425f3487975a15aba447e
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-05-09 15:51:29 +0800

    Update sweepstakes.cpp

--- open/sweepstakes.cpp
+++ open/sweepstakes.cpp
@@ -1,130 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Hash {
-    template <typename T>
-    static inline void combine(size_t &h, const T &v) {
-        h ^= Hash{}(v) + 0x9e3779b9 + (h << 6) + (h >> 2);
+void cooley_tukey(int n, vector<complex<double>> &v) {
+    static vector<int> rev;
+    static vector<complex<double>> twiddles;
+
+    if (rev.empty() && twiddles.empty()) {
+        rev.resize(n);
+        twiddles.resize(n, 1);
+
+        for (int i = 0; i < n; i++) rev[i] = (rev[i >> 1] | (i & 1) << __lg(n)) >> 1;
+
+        for (int k = 2; k < n; k <<= 1) {
+            auto w = polar(1., M_PI / k);
+            for (int i = k; i < k << 1; i++) twiddles[i] = i & 1 ? twiddles[i >> 1] * w : twiddles[i >> 1];
+        }
     }
 
-    template <typename T>
-    size_t operator()(const T &v) const {
-        if constexpr (requires { tuple_size<T>::value; })
-            return apply([](const auto &...e) {
-                size_t h = 0;
-                (combine(h, e), ...);
-                return h;
-            }, v);
-        else if constexpr (requires { declval<T>().begin(); declval<T>().end(); } && !is_same_v<T, string>) {
-            size_t h = 0;
-            for (const auto &e : v) combine(h, e);
-            return h;
-        } else return hash<T>{}(v);
-    }
-};
+    for (int i = 0; i < n; i++)
+        if (i < rev[i]) swap(v[i], v[rev[i]]);
 
-void update(pair<vector<double>, int> &distribution, double p) {
-    auto &[poly, offset] = distribution;
-    poly.emplace_back(0);
-    auto q = 1 - p;
-    for (int i = poly.size() - 1; ~i; i--) {
-        poly[i] *= q;
-        if (i) poly[i] += poly[i - 1] * p;
-    }
-
-    int l = find_if(poly.begin(), poly.end(), [](auto value) { return value > 1e-14; }) - poly.begin(),
-        r = find_if(poly.rbegin(), poly.rend(), [](auto value) { return value > 1e-14; }) - poly.rbegin();
-
-    poly = {poly.begin() + l, poly.end() - r};
-    offset += l;
+    for (int k = 1; k < n; k <<= 1)
+        for (int i = 0; i < n; i += k << 1)
+            for (int j = 0; j < k; j++) {
+                auto t = v[i + j + k] * twiddles[j + k];
+                v[i + j + k] = v[i + j] - t;
+                v[i + j] += t;
+            }
 }
 
-struct SegmentTree {
-    struct Segment {
-        int queries;
-        unordered_set<pair<int, int>, Hash> squares;
-        Segment() : queries(0) {}
+vector<complex<double>> fft(int n, const vector<complex<double>> &f) {
+    auto F = f;
+    for (auto &v : F) v = conj(v);
+    cooley_tukey(n, F);
+    for (auto &v : F) v = conj(v);
+    return F;
+}
 
-        auto & operator=(const vector<pair<int, int>> &v) {
-            queries = v.size();
-            for (auto [i, j] : v) squares.emplace(i, j);
-            return *this;
-        }
-
-        friend auto operator+(const Segment &sl, const Segment &sr) {
-            Segment seg;
-            seg.queries = sl.queries + sr.queries;
-            seg.squares.insert(sl.squares.begin(), sl.squares.end());
-            seg.squares.insert(sr.squares.begin(), sr.squares.end());
-            return seg;
-        }
-    };
-
-    int n, t;
-    vector<double> p, q;
-    vector<Segment> ST;
-
-    void pull(int i) {
-        ST[i] = ST[i << 1] + ST[i << 1 | 1];
-    }
-
-    void build() {
-        for (int i = n - 1; i; i--) pull(i);
-    }
-
-    int midpoint(int l, int r) {
-        int i = 1 << __lg(r - l);
-        return min(l + i, r - (i >> 1));
-    }
-
-    void query(const pair<vector<double>, int> &distribution) {
-        return query(distribution, 1, 0, n);
-    }
-
-    void query(const pair<vector<double>, int> &distribution1, int i, int l, int r) {
-        if (l + 1 == r) {
-            auto distribution2 = make_pair(vector<double>{1}, 0);
-            for (auto [I, J] : ST[i].squares) update(distribution2, p[I] + q[J]);
-
-            auto [poly1, offset1] = distribution1;
-            auto [poly2, offset2] = distribution2;
-            auto valid = [&](int j) {
-                return 0 <= j - offset2 && j - offset2 < poly2.size() && 0 <= t - j - offset1 && t - j - offset1 < poly1.size();
-            };
-
-            double sum = 0;
-            for (int j = 0; j <= ST[i].queries; j++)
-                if (valid(j)) sum += poly2[j - offset2] * poly1[t - j - offset1];
-
-            for (int j = 0; j <= ST[i].queries; j++) {
-                if (valid(j)) cout << fixed << setprecision(6) << poly2[j - offset2] * poly1[t - j - offset1] / sum << " ";
-                else cout << "0 ";
-            }
-            cout << "\n";
-            return;
-        }
-
-        int m = midpoint(l, r);
-        auto range_query = [&](int c, int l, int r) {
-            auto temp = distribution1;
-            for (auto [I, J] : ST[i].squares)
-                if (!ST[c].squares.count({I, J})) update(temp, p[I] + q[J]);
-            query(temp, c, l, r);
-        };
-        range_query(i << 1, l, m);
-        range_query(i << 1 | 1, m, r);
-    }
-
-    auto & operator[](int i) {
-        return ST[i];
-    }
-
-    SegmentTree(int n, const vector<vector<pair<int, int>>> &a, int t, const vector<double> &p, const vector<double> &q) : n(n), t(t), p(p), q(q), ST(2 * n) {
-        int m = bit_ceil(a.size());
-        for (int i = 0; i < a.size(); i++) ST[(i + m) % n + n] = a[i];
-        build();
-    }
-};
+vector<complex<double>> ifft(int n, const vector<complex<double>> &F) {
+    auto f = F;
+    cooley_tukey(n, f);
+    for (auto &v : f) v /= n;
+    return f;
+}
 
 int main() {
     ios::sync_with_stdio(false);
@@ -135,27 +53,40 @@ int main() {
 
     if (!Q) exit(0);
 
-    vector<double> p(m + 1), q(n + 1);
-    for (int i = 1; i <= m; i++) cin >> p[i];
-    for (int i = 1; i <= n; i++) cin >> q[i];
+    vector<double> p(m), q(n);
+    for (auto &pi : p) cin >> pi;
+    for (auto &qj : q) cin >> qj;
 
-    vector<vector<pair<int, int>>> queries(Q);
-    for (auto &sq : queries) {
+    const int size = 1 << 11;
+    vector<complex<double>> roots(size), G_t(size, 1), G_row(size);
+    for (int i = 0; i < size; i++) roots[i] = polar(1., -2 * M_PI * i / size);
+    for (auto pi : p) {
+        fill(G_row.begin(), G_row.end(), 1);
+        for (auto qj : q) {
+            auto p_mine = pi + qj, p_no_mine = 1 - p_mine;
+            for (int i = 0; i < size; i++) G_row[i] *= p_mine * roots[i] + complex<double>(p_no_mine, 0);
+        }
+        for (int i = 0; i < size; i++) G_t[i] *= G_row[i];
+    }
+    auto f_t = ifft(size, G_t);
+
+    vector<complex<double>> G_s(size), G_t_not_s(size);
+    while (Q--) {
         int s;
         cin >> s;
 
-        while (s--) {
-            int i, j;
-            cin >> i >> j;
+        fill(G_s.begin(), G_s.end(), 1);
+        for (int _ = 0; _ < s; _++) {
+            int r, c;
+            cin >> r >> c;
 
-            sq.emplace_back(i, j);
+            auto p_mine = p[r - 1] + q[c - 1], p_no_mine = 1 - p_mine;
+            for (int i = 0; i < size; i++) G_s[i] *= p_mine * roots[i] + complex<double>(p_no_mine, 0);
         }
-    }
-    SegmentTree st(Q, queries, t, p, q);
+        for (int i = 0; i < size; i++) G_t_not_s[i] = G_t[i] / G_s[i];
 
-    auto distribution = make_pair(vector<double>{1}, 0);
-    for (int i = 1; i <= m; i++)
-        for (int j = 1; j <= n; j++)
-            if (!st[1].squares.count({i, j})) update(distribution, p[i] + q[j]);
-    st.query(distribution);
+        auto f_s = ifft(size, G_s), f_t_not_s = ifft(size, G_t_not_s);
+        for (int i = 0; i <= s; i++) cout << fixed << setprecision(6) << (!(0 <= t - i && t - i <= m * n - s) ? 0 : f_s[i % size].real() * f_t_not_s[(t - i) % size].real() / f_t[t % size].real()) << " ";
+        cout << "\n";
+    }
 }
