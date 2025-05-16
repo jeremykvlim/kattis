94cbf22fb3e496791e79abcd5a700a78fbfd1e14
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-05-17 03:12:55 +0800

    Update kaldorianknights.cpp

--- open/kaldorianknights.cpp
+++ open/kaldorianknights.cpp
@@ -374,7 +374,7 @@ int main() {
     }
 
     vector<modint> fact(n + 1, 1);
-    for (int i = 1; i <= n; i++) fact[i] = fact[i - 1] * i;
+    for (int i = 2; i <= n; i++) fact[i] = fact[i - 1] * i;
 
     auto ranks = fact[n];
     vector<modint> dp(h + 1);
