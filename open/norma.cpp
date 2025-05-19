3ee6569edef4bb2bbd8818c31feb83fd97907314
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-05-19 13:35:44 +0800

    Create norma.cpp

---
+++ open/norma.cpp
@@ -0,0 +1,405 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T, typename U, typename V>
+T mul(U x, V y, T mod) {
+    return (unsigned __int128) x * y % mod;
+}
+
+template <typename T, typename U>
+T pow(T base, U exponent, T mod) {
+    T value = 1;
+    while (exponent) {
+        if (exponent & 1) value = mul(base, value, mod);
+        base = mul(base, base, mod);
+        exponent >>= 1;
+    }
+    return value;
+}
+
+bool isprime(unsigned long long n) {
+    if (n < 2) return false;
+    if (n == 2 || n == 5 || n == 11) return true;
+    if (n % 6 % 4 != 1) return (n | 1) == 3;
+
+    auto miller_rabin = [&](int a) {
+        int s = countr_zero(n - 1);
+        auto d = n >> s, x = pow(a % n, d, n);
+        if (x == 1 || x == n - 1) return true;
+
+        while (s--) {
+            x = mul(x, x, n);
+            if (x == n - 1) return true;
+        }
+        return false;
+    };
+    if (!miller_rabin(2) || !miller_rabin(3)) return false;
+
+    auto lucas_pseudoprime = [&]() {
+        auto normalize = [&](__int128 &x) {
+            if (x < 0) x += ((-x / n) + 1) * n;
+        };
+
+        __int128 D = -3;
+        for (;;) {
+            D += D > 0 ? 2 : -2;
+            D *= -1;
+
+            int jacobi = 1;
+            auto jacobi_symbol = [&](__int128 n) {
+                auto a = D;
+                normalize(a);
+
+                while (a) {
+                    while (!(a & 1)) {
+                        a >>= 1;
+                        if ((n & 7) == 3 || (n & 7) == 5) jacobi = -jacobi;
+                    }
+                    if ((a & 3) == 3 && (n & 3) == 3) jacobi = -jacobi;
+
+                    swap(a, n);
+                    a %= n;
+                }
+                return n == 1;
+            };
+            if (!jacobi_symbol(n)) return false;
+            if (jacobi == -1) break;
+        }
+
+        string bits;
+        auto temp = n + 1;
+        while (temp) {
+            bits += (temp & 1) ? '1' : '0';
+            temp >>= 1;
+        }
+        bits.pop_back();
+        reverse(bits.begin(), bits.end());
+
+        auto div2mod = [&](__int128 x) -> unsigned long long {
+            if (x & 1) x += n;
+            normalize(x >>= 1);
+
+            return x % n;
+        };
+
+        __int128 U = 1, V = 1;
+        for (char b : bits) {
+            auto U_2k = mul(U, V, n), V_2k = div2mod(mul(V, V, n) + D * mul(U, U, n));
+
+            if (b == '0') {
+                U = U_2k;
+                V = V_2k;
+            } else {
+                U = div2mod(U_2k + V_2k);
+                V = div2mod(D * U_2k + V_2k);
+            }
+        }
+
+        return !U;
+    };
+    return lucas_pseudoprime();
+}
+
+template <typename M>
+struct BarrettModInt {
+    using G = int;
+    using H = long long;
+    using I = unsigned int;
+    using J = unsigned long long;
+    using K = unsigned __int128;
+
+    I value;
+    static inline bool prime_mod;
+    static inline J inv_mod;
+    static constexpr int bit_length = sizeof(J) * 8;
+
+    static void init() {
+        prime_mod = mod() == 998244353 || mod() == 1e9 + 7 || mod() == 1e9 + 9 || mod() == 1e6 + 69 || isprime(mod());
+        inv_mod = (J) -1 / mod();
+    }
+
+    constexpr BarrettModInt() : value() {}
+
+    template <typename V>
+    BarrettModInt(const V &x) {
+        value = normalize(x);
+    }
+
+    template <typename V>
+    static I normalize(const V &x) {
+        V v = x;
+        if (!(-mod() <= x && x < mod())) v = x % mod();
+        return v < 0 ? v + mod() : v;
+    }
+
+    static I reduce(J v) {
+        H r = (H) (v - (((K) v * inv_mod) >> bit_length) * mod());
+        return r >= mod() ? r - mod() : r;
+    }
+
+    const I & operator()() const {
+        return value;
+    }
+
+    template <typename V>
+    explicit operator V() const {
+        return (V) value;
+    }
+
+    constexpr static H mod() {
+        return M::value;
+    }
+
+    inline auto & operator+=(const BarrettModInt &v) {
+        H t = (H) value + v.value;
+        value = t >= mod() ? t - mod() : t;
+        return *this;
+    }
+
+    inline auto & operator-=(const BarrettModInt &v) {
+        H t = (H) value - v.value;
+        value = t < 0 ? t + mod() : t;
+        return *this;
+    }
+
+    template <typename V>
+    inline auto & operator+=(const V &v) {
+        return *this += BarrettModInt(v);
+    }
+
+    template <typename V>
+    inline auto & operator-=(const V &v) {
+        return *this -= BarrettModInt(v);
+    }
+
+    auto & operator++() {
+        return *this += 1;
+    }
+
+    auto & operator--() {
+        return *this -= 1;
+    }
+
+    auto operator++(int) {
+        return *this += 1;
+    }
+
+    auto operator--(int) {
+        return *this -= 1;
+    }
+
+    auto operator-() const {
+        return (BarrettModInt) 0 - *this;
+    }
+
+    template <typename V = M>
+    typename enable_if<is_same<typename BarrettModInt<V>::I, unsigned int>::value, BarrettModInt>::type & operator*=(const BarrettModInt &v) {
+        value = reduce((J) value * v.value);
+        return *this;
+    }
+
+    auto & operator/=(const BarrettModInt &v) {
+        return *this *= inv(v);
+    }
+
+    static BarrettModInt pow(BarrettModInt base, J exponent) {
+        BarrettModInt v = 1;
+        while (exponent) {
+            if (exponent & 1) v *= base;
+            base *= base;
+            exponent >>= 1;
+        }
+        return v;
+    }
+
+    static BarrettModInt inv(const BarrettModInt &v) {
+        if (prime_mod) return pow(v, mod() - 2);
+
+        H x = 0, y = 1, a = v.value, m = mod();
+        while (a) {
+            H t = m / a;
+            m -= t * a;
+            swap(a, m);
+            x -= t * y;
+            swap(x, y);
+        }
+
+        return (BarrettModInt) x;
+    }
+};
+
+template <typename T>
+bool operator==(const BarrettModInt<T> &lhs, const BarrettModInt<T> &rhs) {
+    return lhs.value == rhs.value;
+}
+
+template <typename T, typename U>
+bool operator==(const BarrettModInt<T> &lhs, U rhs) {
+    return lhs == BarrettModInt<T>(rhs);
+}
+
+template <typename T, typename U>
+bool operator==(U lhs, const BarrettModInt<T> &rhs) {
+    return BarrettModInt<T>(lhs) == rhs;
+}
+
+template <typename T>
+bool operator!=(const BarrettModInt<T> &lhs, const BarrettModInt<T> &rhs) {
+    return !(lhs == rhs);
+}
+
+template <typename T, typename U>
+bool operator!=(const BarrettModInt<T> &lhs, U rhs) {
+    return !(lhs == rhs);
+}
+
+template <typename T, typename U>
+bool operator!=(U lhs, const BarrettModInt<T> &rhs) {
+    return !(lhs == rhs);
+}
+
+template <typename T>
+bool operator>(const BarrettModInt<T> &lhs, const BarrettModInt<T> &rhs) {
+    return lhs.value > rhs.value;
+}
+
+template <typename T>
+bool operator<(const BarrettModInt<T> &lhs, const BarrettModInt<T> &rhs) {
+    return lhs.value < rhs.value;
+}
+
+template <typename T>
+BarrettModInt<T> operator+(const BarrettModInt<T> &lhs, const BarrettModInt<T> &rhs) {
+    return BarrettModInt<T>(lhs) += rhs;
+}
+
+template <typename T, typename U>
+BarrettModInt<T> operator+(const BarrettModInt<T> &lhs, U rhs) {
+    return BarrettModInt<T>(lhs) += rhs;
+}
+
+template <typename T, typename U>
+BarrettModInt<T> operator+(U lhs, const BarrettModInt<T> &rhs) {
+    return BarrettModInt<T>(lhs) += rhs;
+}
+
+template <typename T>
+BarrettModInt<T> operator-(const BarrettModInt<T> &lhs, const BarrettModInt<T> &rhs) {
+    return BarrettModInt<T>(lhs) -= rhs;
+}
+
+template <typename T, typename U>
+BarrettModInt<T> operator-(const BarrettModInt<T> &lhs, U rhs) {
+    return BarrettModInt<T>(lhs) -= rhs;
+}
+
+template <typename T, typename U>
+BarrettModInt<T> operator-(U lhs, const BarrettModInt<T> &rhs) {
+    return BarrettModInt<T>(lhs) -= rhs;
+}
+
+template <typename T>
+BarrettModInt<T> operator*(const BarrettModInt<T> &lhs, const BarrettModInt<T> &rhs) {
+    return BarrettModInt<T>(lhs) *= rhs;
+}
+
+template <typename T, typename U>
+BarrettModInt<T> operator*(const BarrettModInt<T> &lhs, U rhs) {
+    return BarrettModInt<T>(lhs) *= rhs;
+}
+
+template <typename T, typename U>
+BarrettModInt<T> operator*(U lhs, const BarrettModInt<T> &rhs) {
+    return BarrettModInt<T>(lhs) *= rhs;
+}
+
+template <typename T>
+BarrettModInt<T> operator/(const BarrettModInt<T> &lhs, const BarrettModInt<T> &rhs) {
+    return BarrettModInt<T>(lhs) /= rhs;
+}
+
+template <typename T, typename U>
+BarrettModInt<T> operator/(const BarrettModInt<T> &lhs, U rhs) {
+    return BarrettModInt<T>(lhs) /= rhs;
+}
+
+template <typename T, typename U>
+BarrettModInt<T> operator/(U lhs, const BarrettModInt<T> &rhs) {
+    return BarrettModInt<T>(lhs) /= rhs;
+}
+
+template <typename T, typename U>
+U & operator<<(U &stream, const BarrettModInt<T> &v) {
+    return stream << v();
+}
+
+template <typename T, typename U>
+U & operator>>(U &stream, BarrettModInt<T> &v) {
+    typename common_type<typename BarrettModInt<T>::I, long long>::type x;
+    stream >> x;
+    v = BarrettModInt<T>(x);
+    return stream;
+}
+
+constexpr unsigned int MODULO = 1e9;
+using modint = BarrettModInt<integral_constant<decay<decltype(MODULO)>::type, MODULO>>;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    modint::init();
+
+    int n;
+    cin >> n;
+
+    vector<long long> a(n + 1);
+    for (int i = 1; i <= n; i++) cin >> a[i];
+
+    auto total = [&](int l, int r) -> modint {
+        return (long long) r * (r + 1) / 2 - (long long) l * (l + 1) / 2;
+    };
+
+    modint sum = 0, sum_min = 0, sum_max = 0;
+    vector<int> mono_dec{0}, mono_inc{0};
+    vector<modint> pref_min(n + 1), pref_min_indices(n + 1), pref_max(n + 1), pref_max_indices(n + 1);
+    for (int i = 1; i <= n; i++) {
+        auto process = [&](auto &st1, auto &st2, auto &pref1, auto &pref2, auto &pref_indices1, auto &pref_indices2, auto cmp) {
+            while (!st1.empty() && st1.back() && cmp(st1.back())) {
+                int j = st1.back();
+                st1.pop_back();
+                int k = st1.back();
+
+                auto it1 = lower_bound(st2.begin(), st2.end(), k), it2 = lower_bound(st2.begin(), st2.end(), j);
+                if (it1 < it2) {
+                    int l = *it1, m = *prev(it2), r = *it2;
+                    sum_max += (a[i] - a[j]) * (pref1[m] - pref1[l] + a[l] * (l - k) + a[r] * (j - m));
+                    sum_min += (a[i] - a[j]) * (pref_indices1[m] - pref_indices1[l] + total(k, l) * a[l] + total(m, j) * a[r]);
+                } else {
+                    sum_max += (a[i] - a[j]) * (j - k) * a[*it1];
+                    sum_min += (a[i] - a[j]) * total(k, j) * a[*it1];
+                }
+
+                pref2[j] = 0;
+                pref_indices2[j] = 0;
+            }
+        };
+        process(mono_dec, mono_inc, pref_min, pref_max, pref_min_indices, pref_max_indices, [&](int j) { return a[j] <= a[i]; });
+        process(mono_inc, mono_dec, pref_max, pref_min, pref_max_indices, pref_min_indices, [&](int j) { return a[j] >= a[i]; });
+
+        sum_max += a[i] * a[i];
+        sum_min += (__int128) a[i] * a[i] * i;
+
+        auto update = [&](auto &st, auto &pref, auto &pref_indices) {
+            int j = st.back();
+            st.emplace_back(i);
+            pref[i] = pref[j] + a[i] * (i - j);
+            pref_indices[i] = pref_indices[j] + a[i] * total(j, i);
+        };
+        update(mono_dec, pref_max, pref_max_indices);
+        update(mono_inc, pref_min, pref_min_indices);
+
+        sum += sum_max * (i + 1) - sum_min;
+    }
+    cout << sum;
+}
