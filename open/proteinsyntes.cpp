73e18c76adf0a302e3ae0e10d2ef4a4845ba3b10
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-05-19 20:07:59 +0800

    Update proteinsyntes.cpp

--- open/proteinsyntes.cpp
+++ open/proteinsyntes.cpp
@@ -44,8 +44,8 @@ struct Trie {
 
     void add(string &s) {
         int node = 0;
-        for (char ch : s) {
-            int pos = ch - a;
+        for (char c : s) {
+            int pos = c - a;
 
             if (T[node].next[pos] == -1) {
                 T[node].next[pos] = T.size();
