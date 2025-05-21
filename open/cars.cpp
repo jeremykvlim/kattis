8cf3dafe3ac1a807336eaafcdb652b361f3e7516
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-05-21 19:43:06 +0800

    Update cars.cpp

--- open/cars.cpp
+++ open/cars.cpp
@@ -1,41 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-pair<double, double> interval(double position, double speed, double dimension) {
-    if (!speed) return abs(position) < dimension ? make_pair(INT_MIN, INT_MAX) : make_pair(0, 0);
-    double t1 = (-dimension - position) / speed, t2 = (dimension - position) / speed;
-
-    return {min(t1, t2), max(t1, t2)};
-}
-
-bool crash(double t, vector<double> car1, vector<double> car2) {
-    vector<pair<double, double>> times {{0, t},
-                                        interval(car1[0] - car2[0], car1[2] - car2[2], car1[4] + car2[4]),
-                                        interval(car1[1] - car2[1], car1[3] - car2[3], car1[5] + car2[5])};
-
-    return max(times[0].first, max(times[1].first, times[2].first)) < min(times[0].second, min(times[1].second, times[2].second));
-}
-
-vector<double> read() {
-    char d;
-    double x, y, s, w, l;
-    cin >> d >> x >> y >> s >> w >> l;
-    w /= 2.0, l /= 2.0;
-
-    unordered_map<char, pair<int, int>> directions{{'N', {0, 1}}, {'S', {0, -1}}, {'W', {-1, 0}}, {'E', {1, 0}}};
-    auto dir = directions[d];
-    auto dx = dir.first * s, dy = dir.second * s;
-    if (dx) swap(w, l);
-    return vector<double>{x, y, dx, dy, w, l};
-}
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int t;
     cin >> t;
+    
+    auto read = [&]() -> array<double, 6> {
+        char d;
+        double x, y, s, w, l;
+        cin >> d >> x >> y >> s >> w >> l;
+        w /= 2.0, l /= 2.0;
 
-    vector<double> car1 = read(), car2 = read();
-    cout << (crash(t, car1, car2) ? "crash" : "safe");
+        unordered_map<char, array<int, 2>> directions{{'N', {0, 1}}, {'S', {0, -1}}, {'W', {-1, 0}}, {'E', {1, 0}}};
+        auto dx = directions[d][0] * s, dy = directions[d][1] * s;
+        if (dx) swap(w, l);
+        return {x, y, dx, dy, w, l};
+    };
+
+    auto car1 = read(), car2 = read();
+    
+    auto interval = [&](double pos, double speed, double dimension) -> pair<double, double> {
+        if (!speed) return abs(pos) < dimension ? make_pair(INT_MIN, INT_MAX) : make_pair(0, 0);
+        auto t1 = - (dimension + pos) / speed, t2 = (dimension - pos) / speed;
+
+        return minmax(t1, t2);
+    };
+    
+    vector<pair<double, double>> times{{0, t},
+                                       interval(car1[0] - car2[0], car1[2] - car2[2], car1[4] + car2[4]),
+                                       interval(car1[1] - car2[1], car1[3] - car2[3], car1[5] + car2[5])};
+    cout << (max({times[0].first, times[1].first, times[2].first}) < min({times[0].second, times[1].second, times[2].second}) ? "crash" : "safe");
 }
