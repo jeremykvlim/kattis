88b6bb402389ba1e6c043836b962a4764bc22023
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-05-21 18:00:40 +0800

    Update aladin.cpp

--- open/aladin.cpp
+++ open/aladin.cpp
@@ -26,177 +26,138 @@ __int128 linear_mod_sum(int a, int n, int mod) {
     return (__int128) a * n * (n - 1) / 2 - (__int128) mod * floor_division_sum(n, a, 0, mod);
 }
 
-struct SizeBalancedTree {
-    struct SBTNode {
-        int l, r, size;
+struct WeightBalancedTree {
+    struct WBTNode {
+        int l, r, size, virtual_size, a, b, rem;
+        long long sum;
 
-        int a, b, rem, base_count, aggregate_count;
-        long long base_sum, aggregate_sum;
-
-        SBTNode() : l(0), r(0), size(1), a(-1), b(-1), rem(-1), base_count(-1), aggregate_count(-1), base_sum(-1), aggregate_sum(-1) {}
+        WBTNode(int l = 0, int r = 0) : l(l), r(r), size(1), virtual_size(-1), a(-1), b(-1), rem(-1), sum(-1) {}
 
         auto & operator=(const array<int, 4> &arr) {
             l = r = 0;
             size = 1;
-            aggregate_count = base_count = arr[0];
+            virtual_size = arr[0];
             a = arr[1];
             b = arr[2];
             rem = arr[3];
             int c = (a + rem) % b;
-            aggregate_sum = base_sum = linear_mod_sum(a, arr[0], b) + (long long) arr[0] * c - (floor_division_sum(arr[0], a, c, b) - floor_division_sum(arr[0], a, 0, b)) * b;
+            sum = linear_mod_sum(a, arr[0], b) + (long long) arr[0] * c - (floor_division_sum(arr[0], a, c, b) - floor_division_sum(arr[0], a, 0, b)) * b;
             return *this;
         }
     };
 
     int root, nodes;
-    vector<SBTNode> SBT;
+    vector<WBTNode> WBT;
     vector<int> recycled;
 
-    SizeBalancedTree(int n, int m) : root(0), nodes(1), SBT(n + 1) {
-        root = node(m);
+    WeightBalancedTree(int n, int m) : root(0), nodes(1), WBT(n + 1) {
+        root = node(m, 0, 1);
     }
 
-    int node(int count, int a = 0, int b = 1, int rem = 0) {
+    int size(int i) const {
+        return !i ? 0 : WBT[i].size;
+    }
+
+    int virtual_size(int i) const {
+        return !i ? 0 : WBT[i].virtual_size;
+    }
+
+    long long sum(int i) const {
+        return !i ? 0 : WBT[i].sum;
+    }
+
+    void pull(int i) {
+        int l = WBT[i].l, r = WBT[i].r;
+
+        WBT[i].size = size(l) + size(r) + 1;
+        WBT[i].virtual_size = virtual_size(l) + virtual_size(r);
+        WBT[i].sum = sum(l) + sum(r);
+    }
+
+    int node(int l, int r) {
         int i;
         if (!recycled.empty()) {
             i = recycled.back();
             recycled.pop_back();
         } else i = nodes++;
 
-        SBT[i] = {count, a, b, rem};
+        WBT[i] = WBTNode(l, r);
         return i;
     }
 
-    int size(int i) const {
-        return !i ? 0 : SBT[i].size;
-    }
+    int node(int virtual_size, int a, int b, int rem = 0) {
+        int i;
+        if (!recycled.empty()) {
+            i = recycled.back();
+            recycled.pop_back();
+        } else i = nodes++;
 
-    int count(int i) const {
-        return !i ? 0 : SBT[i].aggregate_count;
-    }
-
-    long long sum(int i) const {
-        return !i ? 0 : SBT[i].aggregate_sum;
-    }
-
-    void pull(int i) {
-        int l = SBT[i].l, r = SBT[i].r;
-
-        SBT[i].size = size(l) + size(r) + 1;
-        SBT[i].aggregate_count = count(l) + count(r) + SBT[i].base_count;
-        SBT[i].aggregate_sum = sum(l) + sum(r) + SBT[i].base_sum;
-    }
-
-    int rotate_left(int i) {
-        int j = SBT[i].r;
-        SBT[i].r = SBT[j].l;
-        SBT[j].l = i;
-        pull(i);
-        pull(j);
-        return j;
-    }
-
-    int rotate_right(int i) {
-        int j = SBT[i].l;
-        SBT[i].l = SBT[j].r;
-        SBT[j].r = i;
-        pull(i);
-        pull(j);
-        return j;
-    }
-
-    int balance_left(int i) {
-        if (!i) return 0;
-
-        int &l = SBT[i].l, &r = SBT[i].r;
-        if (!l) return i;
-
-        int ll = SBT[l].l, lr = SBT[l].r;
-        if (ll && size(ll) > size(r)) i = rotate_right(i);
-        else if (lr && size(lr) > size(r)) {
-            l = rotate_left(l);
-            i = rotate_right(i);
-        } else return i;
-
-        l = balance_left(l);
-        r = balance_right(r);
-        i = balance_left(i);
-        i = balance_right(i);
+        WBT[i] = {virtual_size, a, b, rem};
         return i;
     }
 
-    int balance_right(int i) {
-        if (!i) return 0;
-
-        int &l = SBT[i].l, &r = SBT[i].r;
-        if (!r) return i;
-
-        int rr = SBT[r].r, rl = SBT[r].l;
-        if (rr && size(rr) > size(l)) i = rotate_left(i);
-        else if (rl && size(rl) > size(l)) {
-            r = rotate_right(r);
-            i = rotate_left(i);
-        } else return i;
-
-        l = balance_left(l);
-        r = balance_right(r);
-        i = balance_left(i);
-        i = balance_right(i);
-        return i;
-    }
-
-    void destroy(int i) {
-        if (!i) return;
-
-        destroy(SBT[i].l);
-        destroy(SBT[i].r);
+    pair<int, int> excise(int i) {
         recycled.emplace_back(i);
+        return pair(WBT[i].l, WBT[i].r);
+    }
+
+    bool heavy(int s1, int s2) {
+        return s1 > s2 * 3;
     }
 
     int meld(int i, int j) {
         if (!i || !j) return i ^ j;
 
-        if (size(i) > size(j)) {
-            SBT[i].r = meld(SBT[i].r, j);
-            pull(i);
-            return balance_left(i);
-        } else {
-            SBT[j].l = meld(i, SBT[j].l);
-            pull(j);
-            return balance_right(j);
+        if (heavy(size(i), size(j))) {
+            auto [l, r] = excise(i);
+            if (heavy(size(r) + size(j), size(l))) {
+                auto [rl, rr] = excise(r);
+                return meld(meld(l, rl), meld(rr, j));
+            }
+            return meld(l, meld(r, j));
         }
+
+        if (heavy(size(j), size(i))) {
+            auto [l, r] = excise(j);
+            if (heavy(size(l) + size(i), size(r))) {
+                auto [ll, lr] = excise(l);
+                return meld(meld(i, ll), meld(lr, r));
+            }
+            return meld(meld(i, l), r);
+        }
+
+        int k = node(i, j);
+        pull(k);
+        return k;
     }
 
     pair<int, int> split(int i, int k) {
         if (!i) return {0, 0};
+        if (!k) return {0, i};
+        if (k == virtual_size(i)) return {i, 0};
 
-        int diff = k - count(SBT[i].l);
-        if (diff < 0) {
-            auto [ll, lr] = split(SBT[i].l, k);
-            SBT[i].l = lr;
-            pull(i);
-            return {ll, i};
-        } else if (diff > SBT[i].base_count) {
-            auto [rl, rr] = split(SBT[i].r, diff - SBT[i].base_count);
-            SBT[i].r = rl;
-            pull(i);
-            return {i, rr};
-        } else if (diff == SBT[i].base_count) {
-            int r = SBT[i].r;
-            SBT[i].r = 0;
-            pull(i);
-            return {i, r};
-        } else {
-            int ml = node(diff, SBT[i].a, SBT[i].b, SBT[i].rem),
-                mr = node(SBT[i].base_count - diff, SBT[i].a, SBT[i].b, ((long long) SBT[i].a * diff + SBT[i].rem) % SBT[i].b);
-
-            SBT[ml].l = SBT[i].l;
-            SBT[mr].r = SBT[i].r;
-            pull(ml);
-            pull(mr);
-            recycled.emplace_back(i);
-            return {ml, mr};
+        if (size(i) == 1) {
+            int j = node(WBT[i].virtual_size - k, WBT[i].a, WBT[i].b, ((long long) WBT[i].a * k + WBT[i].rem) % WBT[i].b);
+            WBT[i].virtual_size = k;
+            WBT[i].sum -= WBT[j].sum;
+            return {i, j};
         }
+
+        auto [a, b] = excise(i);
+        if (k <= virtual_size(a)) {
+            auto [ll, rr] = split(a, k);
+            return {ll, meld(rr, b)};
+        } else {
+            auto [ll, rr] = split(b, k - virtual_size(a));
+            return {meld(a, ll), rr};
+        }
+    }
+
+    void destroy(int i) {
+        if (!i) return;
+        destroy(WBT[i].l);
+        destroy(WBT[i].r);
+        recycled.emplace_back(i);
     }
 
     void modify(int l, int r, int a, int b) {
@@ -222,7 +183,7 @@ int main() {
     int n, Q;
     cin >> n >> Q;
 
-    SizeBalancedTree sbt(3 * Q, n);
+    WeightBalancedTree wbt(4 * Q + 1, n);
     while (Q--) {
         int q;
         cin >> q;
@@ -231,11 +192,11 @@ int main() {
             int l, r, a, b;
             cin >> l >> r >> a >> b;
 
-            sbt.modify(l, r, a, b);
-        } else {
+            wbt.modify(l, r, a, b);
+        } else if (q == 2) {
             int l, r;
             cin >> l >> r;
-            cout << sbt.range_query(l, r) << "\n";
+            cout << wbt.range_query(l, r) << "\n";
         }
     }
 }
