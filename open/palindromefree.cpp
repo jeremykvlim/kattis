e55ef0df2a78382cd0fc37cbe8695f99a45b11c2
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-05-22 22:22:25 +0800

    Update palindromefree.cpp

--- open/palindromefree.cpp
+++ open/palindromefree.cpp
@@ -1,18 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long count(vector<vector<vector<vector<long long>>>> &dp, string s, int pos = 0, int x = 10, int y = 10, bool less = true) {
-    if (pos == s.size()) return 1;
-    if (dp[pos][x][y][less] != -1) return dp[pos][x][y][less];
-
-    dp[pos][x][y][less] = 0;
-    for (int i = 0; i <= (less ? s[pos] - '0' : 9); i++)
-        if (i != x && i != y)
-            dp[pos][x][y][less] += (!i && x == 10 && y == 10) ? count(dp, s, pos + 1, x, y, false) : count(dp, s, pos + 1, i, x, less && i == s[pos] - '0');
-
-    return dp[pos][x][y][less];
-}
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -20,9 +8,25 @@ int main() {
     long long a, b;
     cin >> a >> b;
 
-    vector<vector<vector<vector<long long>>>> dp1(18, vector<vector<vector<long long>>>(11, vector<vector<long long>>(11, vector<long long>(2, -1)))),
-                                              dp2(18, vector<vector<vector<long long>>>(11, vector<vector<long long>>(11, vector<long long>(2, -1))));
-    if (a) a = count(dp1, to_string(a - 1));
-    b = count(dp2, to_string(b));
+    int size = 18 * 11 * 11 * 2;
+    vector<long long> memo1(size, -1), memo2(size, -1);
+    auto index = [&](int p, int x, int y, int less) {
+        return ((p * 18 + x) * 11 + y) * 11 + less;
+    };
+    auto dp = [&](auto &&self, auto &memo, const string &s, int p = 0, int x = 10, int y = 10, bool less = true) -> long long {
+        if (p == s.size()) return 1;
+        int i = index(p, x, y, less);
+        if (memo[i] != -1) return memo[i];
+
+        memo[i] = 0;
+        for (int d = 0; d <= (less ? s[p] - '0' : 9); d++)
+            if (d != x && d != y)
+                memo[i] += (!d && x == 10 && y == 10) ? self(self, memo, s, p + 1, x, y, false) : self(self, memo, s, p + 1, d, x, less && d == s[p] - '0');
+
+        return memo[i];
+    };
+
+    if (a) a = dp(dp, memo1, to_string(a - 1));
+    b = dp(dp, memo2, to_string(b));
     cout << b - a;
 }
