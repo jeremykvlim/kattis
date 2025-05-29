4117d0cdd6439274067df9fb22f0d2e19ae6c37c
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-05-29 14:10:59 +0800

    Create drzava.cpp

---
+++ open/drzava.cpp
@@ -0,0 +1,199 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+bool approximately_equal(const T &v1, const T &v2, double epsilon = 1e-5) {
+    return fabs(v1 - v2) <= epsilon;
+}
+
+template <typename T>
+int sgn(const T &v) {
+    return approximately_equal(v, (T) 0) ? 0 : (v > 0) - (v < 0);
+}
+
+template <typename T>
+struct Point {
+    T x, y;
+
+    Point() {}
+    Point(T x, T y) : x(x), y(y) {}
+
+    template <typename U>
+    Point(U x, U y) : x(x), y(y) {}
+
+    template <typename U>
+    Point(const Point<U> &p) : x((T) p.x), y((T) p.y) {}
+
+    const auto begin() const {
+        return &x;
+    }
+
+    const auto end() const {
+        return &y + 1;
+    }
+
+    Point operator-() const {
+        return {-x, -y};
+    }
+
+    bool operator<(const Point &p) const {
+        return sgn(x - p.x) ? x < p.x : y < p.y;
+    }
+
+    bool operator>(const Point &p) const {
+        return sgn(x, p.x) ? x > p.x : y > p.y;
+    }
+
+    bool operator==(const Point &p) const {
+        if constexpr (is_floating_point_v<T>) return !sgn(x - p.x) && !sgn(y - p.y);
+        return x == p.x && y == p.y;
+    }
+
+    bool operator!=(const Point &p) const {
+        if constexpr (is_floating_point_v<T>) return sgn(x - p.x) || sgn(y - p.y);
+        return x != p.x || y != p.y;
+    }
+
+    bool operator<=(const Point &p) const {
+        return *this < p || *this == p;
+    }
+
+    bool operator>=(const Point &p) const {
+        return *this > p || *this == p;
+    }
+
+    Point operator+(const Point &p) const {
+        return {x + p.x, y + p.y};
+    }
+
+    Point operator+(const T &v) const {
+        return {x + v, y + v};
+    }
+
+    Point & operator+=(const Point &p) {
+        x += p.x;
+        y += p.y;
+        return *this;
+    }
+
+    Point & operator+=(const T &v) {
+        x += v;
+        y += v;
+        return *this;
+    }
+
+    Point operator-(const Point &p) const {
+        return {x - p.x, y - p.y};
+    }
+
+    Point operator-(const T &v) const {
+        return {x - v, y - v};
+    }
+
+    Point & operator-=(const Point &p) {
+        x -= p.x;
+        y -= p.y;
+        return *this;
+    }
+
+    Point & operator-=(const T &v) {
+        x -= v;
+        y -= v;
+        return *this;
+    }
+
+    Point operator*(const T &v) const {
+        return {x * v, y * v};
+    }
+
+    Point & operator*=(const T &v) {
+        x *= v;
+        y *= v;
+        return *this;
+    }
+
+    Point operator/(const T &v) const {
+        return {x / v, y / v};
+    }
+
+    Point & operator/=(const T &v) {
+        x /= v;
+        y /= v;
+        return *this;
+    }
+};
+
+template <typename T>
+double euclidean_dist(const Point<T> &a, const Point<T> &b = {0, 0}) {
+    return sqrt((double) (a.x - b.x) * (a.x - b.x) + (double) (a.y - b.y) * (a.y - b.y));
+}
+
+struct DisjointSets {
+    vector<int> sets;
+
+    int find(int v) {
+        return sets[v] == v ? v : (sets[v] = find(sets[v]));
+    }
+
+    bool unite(int u, int v) {
+        int u_set = find(u), v_set = find(v);
+        if (u_set != v_set) {
+            sets[v_set] = u_set;
+            return true;
+        }
+        return false;
+    }
+
+    DisjointSets(int n) : sets(n) {
+        iota(sets.begin(), sets.end(), 0);
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, k;
+    cin >> n >> k;
+
+    vector<pair<Point<double>, int>> cities(n);
+    for (int i = 0; i < n; i++) {
+        cin >> cities[i].first.x >> cities[i].first.y >> cities[i].second;
+        cities[i].second %= k;
+    }
+    sort(cities.begin(), cities.end());
+
+    double l = 0, r = 1e9, m;
+    while (l + 1e-12 < r && l + l * 1e-12 < r) {
+        m = l + (r - l) / 2;
+
+        auto vaild = [&](auto d) {
+            DisjointSets dsu(n);
+            vector<vector<bool>> dp(n, vector<bool>(k + 1, false));
+            for (int i = 0; i < n; i++) dp[i][cities[i].second] = true;
+
+            for (int i = 0; i < n; i++)
+                for (int j = i + 1; j < n; j++) {
+                    if (cities[j].first.x - cities[i].first.x > d) break;
+                    if (euclidean_dist(cities[i].first, cities[j].first) > d) continue;
+
+                    int i_set = dsu.find(i), j_set = dsu.find(j);
+                    if (dsu.unite(i, j)) {
+                        vector<bool> temp(2 * k + 1, false);
+                        for (int k1 = 0; k1 <= k; k1++)
+                            if (dp[i_set][k1])
+                                for (int k2 = 0; k2 <= k; k2++)
+                                    if (dp[j_set][k2]) temp[k1 + k2] = true;
+                        for (int k1 = 0; k1 <= 2 * k; k1++)
+                            if (temp[k1] || dp[j_set][k1 % k]) dp[i_set][k1 % k] = true;
+                        if (dp[i_set][0]) return true;
+                    }
+                }
+            return false;
+        };
+
+        if (vaild(m)) r = m;
+        else l = m;
+    }
+    cout << fixed << setprecision(3) << l;
+}
