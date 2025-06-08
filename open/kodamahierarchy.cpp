f635920f494fec30461157bd15bd45ec2e071efc
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-06-09 05:43:14 +0800

    Update kodamahierarchy.cpp

--- open/kodamahierarchy.cpp
+++ open/kodamahierarchy.cpp
@@ -10,94 +10,91 @@ int main() {
 
     vector<pair<int, int>> kodamas(n);
     for (auto &[a, c] : kodamas) cin >> a >> c;
-    sort(kodamas.begin(), kodamas.end(), [&](auto p1, auto p2) { return p1.second != p2.second ? p1.second > p2.second : p1.first < p2.first; });
-    kodamas.erase(unique(kodamas.begin(), kodamas.end()), kodamas.end());
+    sort(kodamas.begin(), kodamas.end(), [&](auto p1, auto p2) { return p1.first != p2.first ? p1.first < p2.first : p1.second > p2.second; });
 
-    n = kodamas.size();
-    vector<int> compress(n);
-    for (int i = 0; i < n; i++) compress[i] = kodamas[i].first;
-    sort(compress.begin(), compress.end());
-    compress.erase(unique(compress.begin(), compress.end()), compress.end());
-    for (auto &[a, c] : kodamas) a = lower_bound(compress.begin(), compress.end(), a) - compress.begin();
-
-    vector<vector<int>> adj_list(n);
+    vector<array<int, 3>> keys(n);
     for (int i = 0; i < n; i++) {
         auto [a, c] = kodamas[i];
-        adj_list[a].emplace_back(i);
+        keys[i] = {c, -a, -i};
     }
+    auto temp = keys;
+    sort(keys.begin(), keys.end());
 
-    int size = ceil(sqrt(n));
-    vector<bool> valid(n, false);
-    vector<int> dp(n, 0), seen(size, -1);
-    vector<vector<int>> pref_max_a(size, vector<int>(size, -1)), pref_min_c(size, vector<int>(size, INT_MAX)), suff_min_dp(size, vector<int>(size, INT_MAX));
-    auto query = [&](int c = INT_MAX) {
-        auto it = find_if(pref_min_c.begin(), pref_min_c.end(), [c](const auto &block) { return block.back() < c; });
-        if (it == pref_min_c.end()) return INT_MAX;
+    vector<int> rank(n + 1), indices(n + 1);
+    for (int i = 0; i < n; i++) rank[i] = lower_bound(keys.begin(), keys.end(), temp[i]) - keys.begin();
+    rank[n] = n;
+    iota(indices.begin(), indices.end(), 0);
+    sort(indices.begin(), indices.end(), [&](int i, int j) { return rank[i] < rank[j]; });
 
-        int l = -1;
-        for (int i = (it - pref_min_c.begin()) * size; i < n; i++)
-            if (valid[i] && kodamas[i].second < c) {
-                l = i;
-                break;
-            }
-        if (l == -1) return INT_MAX;
-
-        int v = dp[l] + 1, b = l / size;
-        for (int i = l + 1; i < min(n, (b + 1) * size); i++)
-            if (valid[i] && kodamas[l] < kodamas[i]) {
-                v = min(v, dp[i] + 1);
-                l = i;
-            }
-
-        for (int i = b + 1; i < size; i++) {
-            int j = lower_bound(pref_max_a[i].begin(), pref_max_a[i].end(), kodamas[l].first) - pref_max_a[i].begin();
-            if (j < size) v = min(v, suff_min_dp[i][j]);
-            if (seen[i] != -1 && kodamas[l] <= kodamas[i * size + seen[i]]) l = i * size + seen[i];
+    vector<int> dp(n + 1, n + 1);
+    for (int j = n + 1; int i : indices)
+        if (j > i) {
+            j = i;
+            dp[i] = i < n;
         }
-        return v;
-    };
 
-    for (auto indices : adj_list) {
-        if (indices.empty()) continue;
+    vector<int> head(n + 1), tail(n + 1), next(n + 1), prev(n + 1);
+    auto dnc = [&](auto &&self, int l, int r, const vector<int> &indices) {
+        if (l + 1 == r) return;
 
-        queue<pair<int, int>> q;
-        sort(indices.begin(), indices.end());
+        int m = l + (r - l) / 2;
+        vector<int> left, right;
+        for (int i : indices)
+            if (i < m) left.emplace_back(i);
+            else right.emplace_back(i);
+        self(self, l, m, left);
+
+        deque<int> mono_dec, mono_inc;
         for (int i : indices) {
-            int v = query(kodamas[i].second);
-            q.emplace(i, v == INT_MAX ? 1 : v);
-        }
+            if (i < m) {
+                while (!mono_dec.empty() && mono_dec.back() < i) mono_dec.pop_back();
+                mono_dec.emplace_back(i);
+            } else {
+                reverse(mono_dec.begin(), mono_dec.end());
+                head[i] = prev[i] = -1;
+                if (!mono_dec.empty()) {
+                    head[i] = prev[i] = mono_dec.back();
+                    tail[prev[i]] = next[prev[i]] = -1;
+                    mono_dec.pop_back();
 
-        while (!q.empty()) {
-            auto [l, v] = q.front();
-            q.pop();
+                    while (!mono_dec.empty()) {
+                        int j = mono_dec.back();
+                        mono_dec.pop_back();
 
-            dp[l] = v;
-            valid[l] = true;
-
-            int b = l / size, j = b * size, a = -1, c = INT_MAX;
-            pair<int, int> k{-1, INT_MAX};
-            vector<bool> visited(size, false);
-            for (int i = 0; i < size; i++) {
-                if (i + j < n && valid[i + j]) {
-                    a = max(a, kodamas[i + j].first);
-                    c = min(c, kodamas[i + j].second);
-
-                    if (k < kodamas[i + j]) {
-                        k = kodamas[i + j];
-                        seen[b] = i;
-                        visited[i] = true;
+                        if (dp[j] < dp[head[i]]) {
+                            next[j] = head[i];
+                            head[i] = j;
+                            tail[next[j]] = j;
+                            tail[j] = -1;
+                        }
                     }
                 }
-                pref_max_a[b][i] = a;
-                pref_min_c[b][i] = c;
-            }
 
-            v = INT_MAX;
-            for (int i = size - 1; ~i; i--) {
-                if (i + j < n && visited[i]) v = min(v, dp[i + j] + 1);
-                suff_min_dp[b][i] = v;
+                while (!mono_inc.empty() && mono_inc.back() > i) {
+                    int j = mono_inc.back();
+                    mono_inc.pop_back();
+
+                    int v = head[j];
+                    while (~v && v < prev[i]) v = next[v];
+                    if (~v) {
+                        while (~prev[i] && dp[v] <= dp[prev[i]]) prev[i] = tail[prev[i]];
+                        if (~prev[i]) {
+                            tail[v] = prev[i];
+                            next[prev[i]] = v;
+                        } else {
+                            head[i] = v;
+                            tail[v] = -1;
+                        }
+                        prev[i] = prev[j];
+                    }
+                }
+
+                if (~head[i]) dp[i] = min(dp[i], dp[head[i]] + (i < n));
+                mono_inc.emplace_back(i);
             }
         }
-    }
-    cout << query() - 1;
+        self(self, m, r, right);
+    };
+    dnc(dnc, 0, n + 1, indices);
+    cout << dp[n];
 }
