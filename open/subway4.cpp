29946b5ac76cdb1f7a24955b62a429e053206073
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-07-06 01:18:10 +0800

    Update subway4.cpp

--- open/subway4.cpp
+++ open/subway4.cpp
@@ -23,24 +23,91 @@ struct Hash {
     }
 };
 
-struct DisjointSets {
-    vector<int> sets;
+struct WeightedDisjointSets {
+    vector<int> sets, prio, weight;
 
-    int find(int v) {
-        return sets[v] == v ? v : (sets[v] = find(sets[v]));
-    }
-
-    bool unite(int u, int v) {
-        int u_set = find(u), v_set = find(v);
-        if (u_set != v_set) {
-            sets[v_set] = u_set;
-            return true;
-        }
-        return false;
-    }
-
-    DisjointSets(int n) : sets(n) {
+    WeightedDisjointSets(int n) : sets(n), prio(n), weight(n, INT_MAX) {
         iota(sets.begin(), sets.end(), 0);
+        iota(prio.begin(), prio.end(), 0);
+        shuffle(prio.begin(), prio.end(), mt19937_64(random_device{}()));
+    }
+
+    int &compress(int v) {
+        if (sets[v] == v) return sets[v];
+        while (weight[sets[v]] <= weight[v]) sets[v] = sets[sets[v]];
+        return sets[v];
+    }
+
+    int find(int v, int w = INT_MAX - 1) {
+        while (weight[v] <= w) v = compress(v);
+        return v;
+    }
+
+    void detach(int v) {
+        if (sets[v] == v) return;
+        detach(sets[v]);
+    }
+
+    int attach(int v, int w = INT_MAX - 1) {
+        while (weight[v] <= w) v = sets[v];
+        return v;
+    }
+
+    void link(int u, int v, int w) {
+        detach(u);
+        detach(v);
+        while (u != v) {
+            u = attach(u, w);
+            v = attach(v, w);
+            if (prio[u] < prio[v]) swap(u, v);
+            swap(sets[v], u);
+            swap(weight[v], w);
+        }
+        attach(u);
+    }
+
+    void cut(int v, int w) {
+        while (sets[v] != v) {
+            if (weight[v] == w) {
+                int u = v;
+                while (sets[u] != u) u = sets[u];
+                sets[v] = v;
+                weight[v] = INT_MAX;
+                return;
+            }
+            v = compress(v);
+        }
+    }
+
+    void cut(int u, int v, int w) {
+        cut(u, w);
+        cut(v, w);
+    }
+
+    int path_max(int u, int v) {
+        if (find(u) != find(v)) return -1;
+
+        for (;;) {
+            if (weight[u] > weight[v]) swap(u, v);
+            if (sets[u] == v) return u;
+            u = sets[u];
+        }
+    }
+
+    int unite(int u, int v, int w) {
+        if (u != v) {
+            int t = path_max(u, v);
+            if (t == -1) {
+                link(u, v, w);
+                return -1;
+            } else if (weight[t] > w) {
+                int x = weight[t];
+                cut(t, weight[t]);
+                link(u, v, w);
+                return x;
+            }
+        }
+        return w;
     }
 };
 
@@ -51,56 +118,32 @@ int main() {
     int n;
     cin >> n;
 
-    vector<vector<int>> adj_list(n);
-    for (int i = 0; i < n - 1; i++) {
-        int a, b;
-        cin >> a >> b;
+    unordered_set<pair<int, int>, Hash> t1, t2;
+    auto read = [&](auto &t) {
+        for (int _ = 0; _ < n - 1; _++) {
+            int u, v;
+            cin >> u >> v;
 
-        adj_list[a].emplace_back(b);
-        adj_list[b].emplace_back(a);
-    }
-
-    unordered_set<pair<int, int>, Hash> plan;
-    for (int i = 0; i < n - 1; i++) {
-        int a, b;
-        cin >> a >> b;
-
-        if (a > b) swap(a, b);
-        plan.emplace(a, b);
-    }
-
-    vector<int> depth(n), prev(n);
-    auto dfs = [&](auto &&self, int v = 0, int p = -1) -> void {
-        depth[v] = depth[p] + 1;
-        prev[v] = p;
-        for (int u : adj_list[v])
-            if (u != p) self(self, u, v);
+            t.emplace(minmax(u, v));
+        }
     };
-    dfs(dfs);
+    read(t1);
+    read(t2);
 
-    DisjointSets dsu(n);
-    for (int a = 0; a < n; a++)
-        for (int b : adj_list[a]) {
-            if (a > b) continue;
-
-            if (plan.count({a, b})) {
-                plan.erase({a, b});
-
-                if (prev[a] == b) dsu.unite(b, a);
-                else dsu.unite(a, b);
-            }
+    WeightedDisjointSets wdsu(n);
+    vector<pair<int, int>> edges;
+    for (auto [u, v] : t1)
+        if (t2.count({u, v})) wdsu.unite(u, v, -1);
+        else {
+            edges.emplace_back(u, v);
+            wdsu.unite(u, v, edges.size() - 1);
         }
 
-    cout << plan.size() << "\n";
-    for (auto [a2, b2] : plan) {
-        int a1 = dsu.find(a2), b1 = dsu.find(b2);
-        if (depth[a1] < depth[b1]) {
-            swap(a1, b1);
-            swap(a2, b2);
+    cout << edges.size() << "\n";
+    for (auto [c, d] : t2)
+        if (!t1.count({c, d})) {
+            int i = wdsu.unite(c, d, -1);
+            auto [a, b] = edges[i];
+            cout << a << " " << b << " " << c << " " << d << "\n";
         }
-        b1 = prev[a1];
-
-        cout << a1 << " " << b1 << " " << a2 << " " << b2 << "\n";
-        dsu.unite(b2, a2);
-    }
 }
