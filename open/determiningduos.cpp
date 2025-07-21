0f4c6e39fa6de64de3ad96deebfa1b4a901f1243
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-07-21 16:39:49 +0800

    Create determiningduos.cpp

---
+++ open/determiningduos.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, r;
+    cin >> n >> r;
+
+    auto mask = (((__int128) 1) << r) - 1;
+    vector<__int128> masks(2 * n);
+    for (int i = 0; i < r; i++)
+        for (int j = 0; j < 2 * n; j++) {
+            int x;
+            cin >> x;
+
+            if (x > n) masks[j] |= ((__int128) 1) << i;
+        }
+    sort(masks.begin(), masks.end());
+
+    for (int i = 0; i < n; i++)
+        if ((masks[i] ^ masks[2 * n - i - 1]) != mask) {
+            cout << "impossible";
+            exit(0);
+        }
+    cout << "possible";
+}
