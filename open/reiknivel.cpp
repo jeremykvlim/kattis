a80c68817f8f097997841a1819a9aa572030f02f
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-08-10 21:47:08 +0800

    Update reiknivel.cpp

--- open/reiknivel.cpp
+++ open/reiknivel.cpp
@@ -13,31 +13,50 @@ int main() {
         exit(0);
     }
 
-    vector<tuple<char, int, int>> ops;
+    int c_max = 0;
+    vector<vector<int>> charge(4, vector<int>(10, 1e9));
+    unordered_map<char, int> ctoi{{'+', 0}, {'-', 1}, {'*', 2}, {'/', 3}};
+    unordered_map<int, char> itoc{{0, '+'}, {1, '-'}, {2, '*'}, {3, '/'}};
     while (a--) {
         char t;
         int y, c;
         cin >> t >> y >> c;
 
-        if (!(((t == '+' || t == '-') && !y) || ((t == '*' || t == '/') && y == 1))) ops.emplace_back(t, y, c);
+        if (!(((t == '+' || t == '-') && !y) || ((t == '*' || t == '/') && y == 1))) charge[ctoi[t]][y] = min(charge[ctoi[t]][y], c);
     }
-    sort(ops.begin(), ops.end());
-    ops.erase(unique(ops.begin(), ops.end(), [&](auto op1, auto op2) { return get<0>(op1) == get<0>(op2) &&
-                                                                              get<1>(op1) == get<1>(op2); }), ops.end());
+
+    vector<tuple<char, int, int>> ops;
+    for (int y = 0; y <= 9; y++)
+        for (int i = 0; i <= 3; i++)
+            if (charge[i][y] < 1e9) {
+                ops.emplace_back(itoc[i], y, charge[i][y]);
+                c_max = max(c_max, charge[i][y]);
+            }
+
+    if (ops.empty()) {
+        cout << "Engin leid!";
+        exit(0);
+    }
 
     vector<int> dist(1e8, 1e9);
     dist[0] = 0;
-    vector<queue<int>> buckets(4);
-    buckets[0].emplace(0);
-    for (int queued = 1, b = 0, dv = 0; queued;) {
-        while (buckets[b].empty()) {
-            b = (b + 1) % 4;
+    array<deque<int>, 4> buckets;
+    buckets[0].emplace_back(0);
+    for (int dv = 0;;) {
+        while (buckets[0].empty()) {
+            if (all_of(buckets.begin(), buckets.end(), [&](const auto &b) { return b.empty(); })) {
+                cout << "Engin leid!";
+                exit(0);
+            }
             dv++;
+            buckets[0].swap(buckets[1]);
+            buckets[1].swap(buckets[2]);
+            buckets[2].swap(buckets[3]);
+            buckets[3].clear();
         }
 
-        int v = buckets[b].front();
-        buckets[b].pop();
-        queued--;
+        int v = buckets[0].front();
+        buckets[0].pop_front();
 
         if (dv != dist[v]) continue;
 
@@ -47,21 +66,21 @@ int main() {
         }
 
         for (auto [t, y, c] : ops) {
-            long long u;
+            int u;
             if (t == '+') u = v + y;
             else if (t == '-') u = v - y;
-            else if (t == '*') u = (long long) v * y;
-            else u = v / y;
+            else if (t == '/') u = v / y;
+            else {
+                if (__builtin_mul_overflow(v, y, &u)) u = -1;
+                else u = v * y;
+            }
 
             if (!(0 <= u && u < 1e8)) continue;
 
             if (dist[u] > dv + c) {
                 dist[u] = dv + c;
-                buckets[(b + c) % 4].emplace(u);
-                queued++;
+                buckets[c].emplace_back(u);
             }
         }
     }
-
-    cout << "Engin leid!";
 }
