bab3536abfb2051596f4ddd8fec82de70f827178
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-08-10 21:25:33 +0800

    Update chemtechcontagion.cpp
    I don't like HLD

--- open/chemtechcontagion.cpp
+++ open/chemtechcontagion.cpp
@@ -43,54 +43,43 @@ int main() {
         degree[v]++;
     }
 
-    vector<int> parent(n + 1), depth(n + 1, 0), subtree_size(n + 1), heavy(n + 1, -1);
+    vector<vector<int>> lift(__lg(n) + 1, vector<int>(n + 1, 1));
+    vector<int> depth(n + 1, 0), in(n + 1), out(n + 1);
     vector<long long> degree_sum(n + 1, 0);
+    int count = 0;
     auto dfs1 = [&](auto &&self, int v = 1, int prev = -1) -> void {
-        subtree_size[v] = 1;
+        in[v] = count++;
+        if (~prev) {
+            depth[v] = depth[prev] + 1;
+            lift[0][v] = prev;
+            for (int i = 1; i <= __lg(n); i++) lift[i][v] = lift[i - 1][lift[i - 1][v]];
+        }
+
         for (int u : adj_list[v])
             if (u != prev) {
-                parent[u] = v;
                 depth[u] = depth[v] + 1;
                 degree_sum[u] = degree_sum[v] + degree[u];
                 self(self, u, v);
-                subtree_size[v] += subtree_size[u];
-                if (subtree_size[u] > subtree_size[heavy[v]]) heavy[v] = u;
             }
+        out[v] = count;
     };
     dfs1(dfs1);
 
-    int count = 0;
-    vector<int> order(n + 1), head(n + 1);
-    auto dfs2 = [&](auto &&self, int v = 1, int h = 1) -> void {
-        head[v] = h;
-        order[v] = ++count;
-        if (heavy[v] != -1) self(self, heavy[v], h);
-
-        for (int u : adj_list[v])
-            if (u != parent[v] && u != heavy[v]) self(self, u, u);
-    };
-    dfs2(dfs2);
-
-    auto lca = [&](int u, int v) {
-        while (head[u] != head[v])
-            if (depth[head[u]] > depth[head[v]]) u = parent[head[u]];
-            else v = parent[head[v]];
-
-        return depth[u] < depth[v] ? u : v;
+    auto ancestor = [&](int v, int u) {
+        return in[v] <= in[u] && in[u] < out[v];
     };
 
-    vector<long long> pipes(q);
-    vector<pair<int, int>> queries(q);
-    vector<vector<int>> indices(n + 1);
-    for (int i = 0; i < q; i++) {
-        auto &[a, b] = queries[i];
-        cin >> a >> b;
+    auto lca = [&](int u, int v) -> int {
+        if (ancestor(u, v)) return u;
+        if (ancestor(v, u)) return v;
 
-        int anc = lca(a, b);
-        pipes[i] = degree_sum[a] + degree_sum[b] - 2 * degree_sum[anc] + degree[anc];
-        indices[a].emplace_back(i);
-        indices[b].emplace_back(i);
-    }
+        if (depth[u] < depth[v]) swap(u, v);
+
+        for (int i = __lg(n); ~i; i--)
+            if (!ancestor(lift[i][u], v)) u = lift[i][u];
+
+        return lift[0][u];
+    };
 
     vector<vector<int>> sweep(n + 1);
     for (auto [u, v] : edges) {
@@ -98,33 +87,44 @@ int main() {
         sweep[v].emplace_back(u);
     }
 
+    vector<pair<int, int>> queries(q);
+    vector<int> lcas(q);
+    vector<long long> pipes(q);
+    vector<vector<int>> indices(n + 1);
+    for (int i = 0; i < q; i++) {
+        auto &[a, b] = queries[i];
+        cin >> a >> b;
+
+        lcas[i] = lca(a, b);
+        pipes[i] = degree_sum[a] + degree_sum[b] - 2 * degree_sum[lcas[i]] + degree[lcas[i]];
+        indices[a].emplace_back(i);
+        indices[b].emplace_back(i);
+    }
+
     FenwickTree<long long> fw(n + 1);
-    auto dfs3 = [&](auto &&self, int v = 1, int prev = -1) -> void {
-        for (int u : sweep[v]) fw.update(order[u], 1);
+    vector<int> visits(n + 1, 0);
+    auto dfs2 = [&](auto &&self, int v = 1, int prev = -1) -> void {
+        for (int u : sweep[v]) {
+            fw.update(in[u] + 1, 1);
+            fw.update(out[u] + 1, -1);
+            visits[u]++;
+        }
 
         for (int i : indices[v]) {
             auto [a, b] = queries[i];
-
-            auto p = 0LL;
-            while (head[a] != head[b])
-                if (depth[head[a]] > depth[head[b]]) {
-                    p += fw.pref_sum(order[a]) - fw.pref_sum(order[head[a]] - 1);
-                    a = parent[head[a]];
-                } else {
-                    p += fw.pref_sum(order[b]) - fw.pref_sum(order[head[b]] - 1);
-                    b = parent[head[b]];
-                }
-
-            auto [l, r] = minmax(order[a], order[b]);
-            pipes[i] -= 2 * (p + fw.pref_sum(r) - fw.pref_sum(l - 1));
+            pipes[i] -= 2 * (fw.pref_sum(in[a] + 1) + fw.pref_sum(in[b] + 1) - 2 * fw.pref_sum(in[lcas[i]] + 1) + visits[lcas[i]]);
         }
 
         for (int u : adj_list[v])
             if (u != prev) self(self, u, v);
 
-        for (int u : sweep[v]) fw.update(order[u], -1);
+        for (int u : sweep[v]) {
+            fw.update(in[u] + 1, -1);
+            fw.update(out[u] + 1, 1);
+            visits[u]--;
+        }
     };
-    dfs3(dfs3);
+    dfs2(dfs2);
 
     for (auto p : pipes) cout << p << "\n";
 }
