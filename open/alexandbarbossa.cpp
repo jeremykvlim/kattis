7ac72d9bd9134e77dbb5ffb6c5d9cc3dfda59cee
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-08-14 01:16:26 +0800

    Create alexandbarbossa.cpp

---
+++ open/alexandbarbossa.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    unsigned int k, m, n;
+    cin >> k >> m >> n;
+    
+    auto r = k % (m + n);
+    cout << (r >= n || (r / m) & 1 ? "Alex" : "Barbossa");
+}
