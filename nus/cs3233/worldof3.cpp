6cffc353456b0d682ab046328bfeb6ecd646af9e
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-08-14 20:14:02 +0800

    Update worldof3.cpp

--- nus/cs3233/worldof3.cpp
+++ nus/cs3233/worldof3.cpp
@@ -43,6 +43,19 @@ tuple<vector<int>, T, vector<vector<T>>> held_karp(int n, const vector<vector<T>
     return {tour, len, dp};
 }
 
+template <typename T, typename F>
+void fast_subset_transform(int n, vector<T> &v, F &&f) {
+    for (int k = 1; k < n; k <<= 1)
+        for (int i = 0; i < n; i += k << 1)
+            for (int j = 0; j < k; j++) v[i + j + k] = f(v[i + j + k], v[i + j]);
+}
+
+template <typename T>
+T gospers_hack(T mask) {
+    T lsb_mask = mask | (mask - 1);
+    return (lsb_mask + 1) | (((~lsb_mask & -~lsb_mask) - 1) >> (countr_zero(mask) + 1));
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -98,72 +111,73 @@ int main() {
     int spell_mask = 0;
     for (int c : spells) spell_mask |= 1 << indices[c];
 
-    vector<vector<long long>> cost1(k + 1, vector<long long>(1 << k, 1e18));
-    for (int src = 0; src <= k; src++) {
-        auto [tour, len, dp] = held_karp(k, d, src);
-
-        for (auto m1 = 1U; m1 < 1 << k; m1++)
-            for (auto m2 = m1 & spell_mask; m2; m2 &= m2 - 1) {
-                int i = countr_zero(m2);
-                cost1[src][m1] = min(cost1[src][m1], dp[m1][i]);
-            }
-    }
-
     vector<int> popcount(1 << k, 0);
-    for (int mask = 1; mask < 1 << k; mask++) popcount[mask] = popcount[mask >> 1] + (mask & 1);
+    vector<vector<unsigned>> masks_by_pop(k + 1);
+    masks_by_pop[0] = {0};
+    for (auto mask = 1U; mask < 1 << k; mask++) masks_by_pop[popcount[mask] = popcount[mask & (mask - 1)] + 1].emplace_back(mask);
 
-    vector<vector<long long>> cost2(k + 1, vector<long long>(1 << k, 1e18));
-    vector<vector<vector<long long>>> cost3(k + 1, vector<vector<long long>>(1 << k, vector<long long>(k, 1e18)));
-    for (int src = 0; src <= k; src++)
-        for (auto m1 = 0U; m1 < 1 << k; m1++) {
-            if (src && ((1 << (src - 1)) & spell_mask)) {
-                int a = popcount[m1];
-                if (a <= a_max) {
-                    if (a < a_max) cost2[src][m1] = min(cost2[src][m1], (long long) t);
-                    for (auto m2 = m1; m2; m2 &= m2 - 1) {
-                        int i = countr_zero(m2);
-                        cost3[src][m1][i] = min(cost3[src][m1][i], (long long) t);
-                    }
-                }
+    vector<long long> cost(1 << k), temp(1 << k);
+    vector<vector<long long>> dp1(k + 1, vector<long long>(1 << k, 1e18)), dp2(k + 1, vector<long long>(1 << k));
+    dp1[0][0] = 0;
+    for (int src = 0; src <= k; src++) {
+        for (int p = 0; p <= k; p++)
+            for (auto mask = 0U; mask < 1 << k; mask++) cost[mask] = dp2[p][mask] = 1e18;
+
+        auto [tour, len, dp_tsp] = held_karp(k, d, src);
+        for (auto m1 = 1U; m1 < 1 << k; m1++) {
+            auto m2 = m1 & spell_mask;
+            if (m2) {
+                for (; m2; m2 &= m2 - 1) cost[m1] = min(cost[m1], dp_tsp[m1][countr_zero(m2)]);
+                cost[m1] = max(cost[m1], (long long) t);
             }
-
-            for (auto m2 = m1; m2; --m2 &= m1)
-                if (m2 & spell_mask)
-                    if (cost1[src][m2] != 1e18) {
-                        cost1[src][m2] = max(cost1[src][m2], (long long) t);
-                        int a = popcount[m1] - popcount[m2];
-                        if (a <= a_max) {
-                            if (a < a_max) cost2[src][m1] = min(cost2[src][m1], cost1[src][m2]);
-                            for (auto m3 = m1 & ~m2; m3; m3 &= m3 - 1) {
-                                int i = countr_zero(m3);
-                                cost3[src][m1][i] = min(cost3[src][m1][i], cost1[src][m2]);
-                            }
-                        }
-                    }
         }
 
-    vector<vector<long long>> dp(1 << k, vector<long long>(k + 1, 1e18));
-    dp[0][0] = 0;
-    for (auto m1 = 0U; m1 < 1 << k; m1++) {
-        auto m2 = m1 ^ ((1 << k) - 1);
-        for (int src = 0; src <= k; src++)
-            if (dp[m1][src] != 1e18) {
-                for (int i = 0; i < k; i++)
-                    if ((m2 >> i) & 1)
-                        if (d[src][i + 1] != 1e18) dp[m1 | (1 << i)][i + 1] = min(dp[m1 | (1 << i)][i + 1], dp[m1][src] + d[src][i + 1]);
+        for (int p = 1; p <= k; p++) {
+            fill(temp.begin(), temp.end(), 1e18);
+            for (auto mask : masks_by_pop[p]) temp[mask] = cost[mask];
+            fast_subset_transform(1 << k, temp, [](auto x, auto y) { return min(x, y); });
+            for (int q = 1; q <= p; q++)
+                for (auto mask = 0U; mask < 1 << k; mask++) dp2[q][mask] = min(dp2[q][mask], temp[mask]);
+        }
 
-                if (m1 == (1 << k) - 1)
-                    if (d[src][0] != 1e18) dp[m1][0] = min(dp[m1][0], dp[m1][src] + d[src][0]);
+        bool src_spell = src && (spell_mask >> (src - 1)) & 1;
+        for (int p = 0; p <= k; p++)
+            for (auto m1 : masks_by_pop[p])
+                if (dp1[src][m1] != 1e18) {
+                    auto m2 = m1 ^ ((1 << k) - 1);
 
-                for (auto m3 = m2; m3; --m3 &= m2) {
-                    if (cost2[src][m3] != 1e18) dp[m1 | m3][0] = min(dp[m1 | m3][0], dp[m1][src] + cost2[src][m3]);
-
-                    for (auto m4 = m3; m4; m4 &= m4 - 1) {
-                        int i = countr_zero(m4);
-                        if (cost3[src][m3][i] != 1e18) dp[m1 | m3][i + 1] = min(dp[m1 | m3][i + 1], dp[m1][src] + cost3[src][m3][i]);
+                    for (auto m3 = m2; m3; m3 &= m3 - 1) {
+                        int i = countr_zero(m3);
+                        if (d[src][i + 1] != 1e18) dp1[i + 1][m1 | (1 << i)] = min(dp1[i + 1][m1 | (1 << i)], dp1[src][m1] + d[src][i + 1]);
                     }
+
+                    if (m1 == (1 << k) - 1)
+                        if (d[src][0] < 1e18) dp1[0][m1] = min(dp1[0][m1], dp1[src][m1] + d[src][0]);
+
+                    if (p == k) continue;
+
+                    vector<int> pos;
+                    for (auto m3 = m2; m3; m3 &= m3 - 1) pos.emplace_back(countr_zero(m3));
+                    int m = pos.size();
+
+                    for (int a = 1; a <= m; a++)
+                        for (auto m3 = (1U << a) - 1; m3 && m3 < 1 << m; m3 = gospers_hack(m3)) {
+                            auto m4 = 0U;
+                            for (auto m5 = m3; m5; m5 &= m5 - 1) m4 |= (1 << pos[countr_zero(m5)]);
+
+                            int c1 = max(1, a - a_max + 1);
+                            if (src_spell && a < a_max) dp2[c1][m4] = min(dp2[c1][m4], (long long) t);
+                            if (dp2[c1][m4] != 1e18) dp1[0][m1 | m4] = min(dp1[0][m1 | m4], dp1[src][m1] + dp2[c1][m4]);
+
+                            int c2 = max(1, a - a_max);
+                            if (c2 <= k)
+                                for (auto m5 = m4; m5; m5 &= m5 - 1) {
+                                    int i = countr_zero(m5), m6 = m4 ^ (1 << i);
+                                    if (src_spell && a <= a_max) dp2[c2][m6] = min(dp2[c2][m6], (long long) t);
+                                    if (dp2[c2][m6] != 1e18) dp1[i + 1][m1 | m4] = min(dp1[i + 1][m1 | m4], dp1[src][m1] + dp2[c2][m6]);
+                                }
+                        }
                 }
-            }
     }
-    cout << dp.back()[0];
+    cout << dp1[0].back();
 }
