84dd94e70c976573c6205c04658b3a741a61b050
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-08-16 00:24:31 +0800

    Create conmaster.cpp

---
+++ open/conmaster.cpp
@@ -0,0 +1,179 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+struct Fraction : array<T, 2> {
+    using F = array<T, 2>;
+
+    Fraction() = default;
+    Fraction(T n, T d) : F{n, d} {
+        reduce();
+    }
+
+    T & numer() {
+        return (*this)[0];
+    }
+
+    T & denom() {
+        return (*this)[1];
+    }
+
+    const T & numer() const {
+        return (*this)[0];
+    }
+
+    const T & denom() const {
+        return (*this)[1];
+    }
+
+    void reduce() {
+        if (denom() < 0) {
+            numer() *= -1;
+            denom() *= -1;
+        }
+
+        T g = __gcd(abs(numer()), denom());
+        if (g) {
+            numer() /= g;
+            denom() /= g;
+        }
+    }
+
+    bool operator<(const Fraction &f) const {
+        return numer() * f.denom() < f.numer() * denom();
+    }
+
+    bool operator>(const Fraction &f) const {
+        return numer() * f.denom() > f.numer() * denom();
+    }
+
+    bool operator==(const Fraction &f) const {
+        return numer() == f.numer() && denom() == f.denom();
+    }
+
+    bool operator!=(const Fraction &f) const {
+        return numer() != f.numer() || denom() != f.denom();
+    }
+
+    bool operator<=(const Fraction &f) const {
+        return *this < f || *this == f;
+    }
+
+    bool operator>=(const Fraction &f) const {
+        return *this > f || *this == f;
+    }
+
+    Fraction operator+(const Fraction &f) const {
+        return {numer() * f.denom() + f.numer() * denom(), denom() * f.denom()};
+    }
+
+    Fraction operator+(const T &v) const {
+        return {numer() + v * denom(), denom()};
+    }
+
+    Fraction & operator+=(const Fraction &f) {
+        numer() = numer() * f.denom() + f.numer() * denom();
+        denom() *= f.denom();
+        reduce();
+        return *this;
+    }
+
+    Fraction & operator+=(const T &v) {
+        numer() += v * denom();
+        reduce();
+        return *this;
+    }
+
+    Fraction operator-(const Fraction &f) const {
+        return {numer() * f.denom() - f.numer() * denom(), denom() * f.denom()};
+    }
+
+    Fraction operator-(const T &v) const {
+        return {numer() - v * denom(), denom()};
+    }
+
+    Fraction & operator-=(const Fraction &f) {
+        numer() = numer() * f.denom() - f.numer() * denom();
+        denom() *= f.denom();
+        reduce();
+        return *this;
+    }
+
+    Fraction & operator-=(const T &v) {
+        numer() -= v * denom();
+        reduce();
+        return *this;
+    }
+
+    Fraction operator*(const Fraction &f) const {
+        return {numer() * f.numer(), denom() * f.denom()};
+    }
+
+    Fraction operator*(const T &v) const {
+        return {numer() * v, denom()};
+    }
+
+    Fraction & operator*=(const Fraction &f) {
+        numer() *= f.numer();
+        denom() *= f.denom();
+        reduce();
+        return *this;
+    }
+
+    Fraction & operator*=(const T &v) {
+        numer() *= v;
+        reduce();
+        return *this;
+    }
+
+    Fraction operator/(const Fraction &f) const {
+        return {numer() * f.denom(), denom() * f.numer()};
+    }
+
+    Fraction operator/(const T &v) const {
+        return {numer(), denom() * v};
+    }
+
+    Fraction & operator/=(const Fraction &f) {
+        numer() *= f.denom();
+        denom() *= f.numer();
+        reduce();
+        return *this;
+    }
+
+    Fraction & operator/=(const T &v) {
+        denom() *= v;
+        reduce();
+        return *this;
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long long k, a, b, c;
+    cin >> k >> a >> b >> c;
+
+    vector<long long> v(k + 1, 0);
+    auto calc = [&](long long n, long long m) -> long long {
+        return v[m] - a * m + v[n - 1 - m] - b * (n - 1 - m);
+    };
+
+    for (int n = 1, m = 0; n <= k; n++) {
+        m = min(m, n - 1);
+
+        auto v1 = calc(n, m);
+        while (m + 1 <= n - 1) {
+            auto v2 = calc(n, m + 1);
+            if (v1 <= v2) {
+                v1 = v2;
+                m++;
+            } else break;
+        }
+        v[n] = v1 + c;
+    }
+
+    Fraction<long long> f{v[k], k};
+    cout << f.numer() << "/" << f.denom();
+}
