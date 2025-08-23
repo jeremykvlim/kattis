c6e3552e15515a444b607a4f5cd2b6e787ee858a
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-08-23 22:16:55 +0800

    Create tacotuesday.cpp

---
+++ open/tacotuesday.cpp
@@ -0,0 +1,408 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T, typename U, typename V>
+T mul(U x, V y, T mod) {
+    return (unsigned __int128) x * y % mod;
+}
+
+template <typename T, typename U>
+T pow(T base, U exponent, T mod) {
+    T value = 1;
+    while (exponent) {
+        if (exponent & 1) value = mul(base, value, mod);
+        base = mul(base, base, mod);
+        exponent >>= 1;
+    }
+    return value;
+}
+
+bool isprime(unsigned long long n) {
+    if (n < 2) return false;
+    if (n == 2 || n == 5 || n == 11) return true;
+    if (n % 6 % 4 != 1) return (n | 1) == 3;
+
+    auto miller_rabin = [&](int a) {
+        int s = countr_zero(n - 1);
+        auto d = n >> s, x = pow(a % n, d, n);
+        if (x == 1 || x == n - 1) return true;
+
+        while (s--) {
+            x = mul(x, x, n);
+            if (x == n - 1) return true;
+        }
+        return false;
+    };
+    if (!miller_rabin(2) || !miller_rabin(3)) return false;
+
+    auto lucas_pseudoprime = [&]() {
+        auto normalize = [&](__int128 &x) {
+            if (x < 0) x += ((-x / n) + 1) * n;
+        };
+
+        __int128 D = -3;
+        for (;;) {
+            D += D > 0 ? 2 : -2;
+            D *= -1;
+
+            int jacobi = 1;
+            auto jacobi_symbol = [&](__int128 n) {
+                auto a = D;
+                normalize(a);
+
+                while (a) {
+                    while (!(a & 1)) {
+                        a >>= 1;
+                        if ((n & 7) == 3 || (n & 7) == 5) jacobi = -jacobi;
+                    }
+                    if ((a & 3) == 3 && (n & 3) == 3) jacobi = -jacobi;
+
+                    swap(a, n);
+                    a %= n;
+                }
+                return n == 1;
+            };
+            if (!jacobi_symbol(n)) return false;
+            if (jacobi == -1) break;
+        }
+
+        string bits;
+        auto temp = n + 1;
+        while (temp) {
+            bits += (temp & 1) ? '1' : '0';
+            temp >>= 1;
+        }
+        bits.pop_back();
+        reverse(bits.begin(), bits.end());
+
+        auto div2mod = [&](__int128 x) -> unsigned long long {
+            if (x & 1) x += n;
+            normalize(x >>= 1);
+
+            return x % n;
+        };
+
+        __int128 U = 1, V = 1;
+        for (char b : bits) {
+            auto U_2k = mul(U, V, n), V_2k = div2mod(mul(V, V, n) + D * mul(U, U, n));
+
+            if (b == '0') {
+                U = U_2k;
+                V = V_2k;
+            } else {
+                U = div2mod(U_2k + V_2k);
+                V = div2mod(D * U_2k + V_2k);
+            }
+        }
+
+        return !U;
+    };
+    return lucas_pseudoprime();
+}
+
+template <typename M>
+struct MontgomeryModInt {
+    using T = typename decay<decltype(M::value)>::type;
+    using U = typename conditional<is_same<T, unsigned int>::value, unsigned long long, typename conditional<is_same<T, unsigned long long>::value, unsigned __int128, void>::type>::type;
+    using I = typename conditional<is_same<T, unsigned int>::value, int, typename conditional<is_same<T, unsigned long long>::value, long long, void>::type>::type;
+    using J = typename conditional<is_same<T, unsigned int>::value, long long, typename conditional<is_same<T, unsigned long long>::value, __int128, void>::type>::type;
+
+    T value;
+    static inline pair<T, U> r;
+    static inline bool prime_mod;
+    static constexpr int bit_length = sizeof(T) * 8;
+
+    static void init() {
+        prime_mod = mod() == 998244353 || mod() == (unsigned long long) 1e9 + 7 || mod() == (unsigned long long) 1e9 + 9 || mod() == (unsigned long long) 1e6 + 69 || mod() == 2524775926340780033 || isprime(mod());
+        r = {mod(), - (U) mod() % mod()};
+        while (mod() * r.first != 1) r.first *= (T) 2 - mod() * r.first;
+    }
+
+    constexpr MontgomeryModInt() : value() {}
+
+    MontgomeryModInt(const J &x) {
+        value = reduce((U) x * r.second);
+    }
+
+    static T reduce(const U &x) {
+        T q = (U) x * r.first, v = (x >> bit_length) + mod() - (((U) q * mod()) >> bit_length);
+        return v >= mod() ? v - mod() : v;
+    }
+
+    T operator()() const {
+        return reduce((U) value);
+    }
+
+    template <typename V>
+    explicit operator V() const {
+        return (V) value;
+    }
+
+    constexpr static T mod() {
+        return M::value;
+    }
+
+    inline auto & operator+=(const MontgomeryModInt &v) {
+        if ((I) (value += v.value) >= mod()) value -= mod();
+        return *this;
+    }
+
+    inline auto & operator-=(const MontgomeryModInt &v) {
+        if ((I) (value -= v.value) < 0) value += mod();
+        return *this;
+    }
+
+    template <typename U>
+    inline auto & operator+=(const U &v) {
+        return *this += (MontgomeryModInt) v;
+    }
+
+    template <typename U>
+    inline auto & operator-=(const U &v) {
+        return *this -= (MontgomeryModInt) v;
+    }
+
+    auto & operator++() {
+        return *this += 1;
+    }
+
+    auto & operator--() {
+        return *this -= 1;
+    }
+
+    auto operator++(int) {
+        return *this += 1;
+    }
+
+    auto operator--(int) {
+        return *this -= 1;
+    }
+
+    auto operator-() const {
+        return (MontgomeryModInt) 0 - *this;
+    }
+
+    template <typename V = M>
+    typename enable_if<is_same<typename MontgomeryModInt<V>::T, unsigned int>::value, MontgomeryModInt>::type & operator*=(const MontgomeryModInt &v) {
+        value = reduce((unsigned long long) value * v.value);
+        return *this;
+    }
+
+    template <typename V = M>
+    typename enable_if<is_same<typename MontgomeryModInt<V>::T, unsigned long long>::value, MontgomeryModInt>::type & operator*=(const MontgomeryModInt &v) {
+        value = reduce((unsigned __int128) value * v.value);
+        return *this;
+    }
+
+    template <typename V = M>
+    typename enable_if<!is_integral<typename MontgomeryModInt<V>::T>::value, MontgomeryModInt>::type & operator*=(const MontgomeryModInt &v) {
+        value = reduce(value * v.value);
+        return *this;
+    }
+
+    auto & operator/=(const MontgomeryModInt &v) {
+        return *this *= inv(v);
+    }
+
+    static MontgomeryModInt pow(MontgomeryModInt base, T exponent) {
+        MontgomeryModInt v = 1;
+        while (exponent) {
+            if (exponent & 1) v *= base;
+            base *= base;
+            exponent >>= 1;
+        }
+        return v;
+    }
+
+    static MontgomeryModInt inv(const MontgomeryModInt &v) {
+        if (prime_mod) return pow(v, mod() - 2);
+
+        T x = 0, y = 1, a = v.value, m = mod();
+        while (a) {
+            T t = m / a;
+            m -= t * a;
+            swap(a, m);
+            x -= t * y;
+            swap(x, y);
+        }
+
+        return (MontgomeryModInt) x;
+    }
+};
+
+template <typename T>
+bool operator==(const MontgomeryModInt<T> &lhs, const MontgomeryModInt<T> &rhs) {
+    return lhs.value == rhs.value;
+}
+
+template <typename T, typename U>
+bool operator==(const MontgomeryModInt<T> &lhs, U rhs) {
+    return lhs == MontgomeryModInt<T>(rhs);
+}
+
+template <typename T, typename U>
+bool operator==(U lhs, const MontgomeryModInt<T> &rhs) {
+    return MontgomeryModInt<T>(lhs) == rhs;
+}
+
+template <typename T>
+bool operator!=(const MontgomeryModInt<T> &lhs, const MontgomeryModInt<T> &rhs) {
+    return !(lhs == rhs);
+}
+
+template <typename T, typename U>
+bool operator!=(const MontgomeryModInt<T> &lhs, U rhs) {
+    return !(lhs == rhs);
+}
+
+template <typename T, typename U>
+bool operator!=(U lhs, const MontgomeryModInt<T> &rhs) {
+    return !(lhs == rhs);
+}
+
+template <typename T>
+bool operator>(const MontgomeryModInt<T> &lhs, const MontgomeryModInt<T> &rhs) {
+    return lhs() > rhs();
+}
+
+template <typename T>
+bool operator<(const MontgomeryModInt<T> &lhs, const MontgomeryModInt<T> &rhs) {
+    return lhs() < rhs();
+}
+
+template <typename T>
+bool operator>=(const MontgomeryModInt<T> &lhs, const MontgomeryModInt<T> &rhs) {
+    return lhs > rhs || lhs == rhs;
+}
+
+template <typename T>
+bool operator<=(const MontgomeryModInt<T> &lhs, const MontgomeryModInt<T> &rhs) {
+    return lhs < rhs || lhs == rhs;
+}
+
+template <typename T>
+MontgomeryModInt<T> operator+(const MontgomeryModInt<T> &lhs, const MontgomeryModInt<T> &rhs) {
+    return MontgomeryModInt<T>(lhs) += rhs;
+}
+
+template <typename T, typename U>
+MontgomeryModInt<T> operator+(const MontgomeryModInt<T> &lhs, U rhs) {
+    return MontgomeryModInt<T>(lhs) += rhs;
+}
+
+template <typename T, typename U>
+MontgomeryModInt<T> operator+(U lhs, const MontgomeryModInt<T> &rhs) {
+    return MontgomeryModInt<T>(lhs) += rhs;
+}
+
+template <typename T>
+MontgomeryModInt<T> operator-(const MontgomeryModInt<T> &lhs, const MontgomeryModInt<T> &rhs) {
+    return MontgomeryModInt<T>(lhs) -= rhs;
+}
+
+template <typename T, typename U>
+MontgomeryModInt<T> operator-(const MontgomeryModInt<T> &lhs, U rhs) {
+    return MontgomeryModInt<T>(lhs) -= rhs;
+}
+
+template <typename T, typename U>
+MontgomeryModInt<T> operator-(U lhs, const MontgomeryModInt<T> &rhs) {
+    return MontgomeryModInt<T>(lhs) -= rhs;
+}
+
+template <typename T>
+MontgomeryModInt<T> operator*(const MontgomeryModInt<T> &lhs, const MontgomeryModInt<T> &rhs) {
+    return MontgomeryModInt<T>(lhs) *= rhs;
+}
+
+template <typename T, typename U>
+MontgomeryModInt<T> operator*(const MontgomeryModInt<T> &lhs, U rhs) {
+    return MontgomeryModInt<T>(lhs) *= rhs;
+}
+
+template <typename T, typename U>
+MontgomeryModInt<T> operator*(U lhs, const MontgomeryModInt<T> &rhs) {
+    return MontgomeryModInt<T>(lhs) *= rhs;
+}
+
+template <typename T>
+MontgomeryModInt<T> operator/(const MontgomeryModInt<T> &lhs, const MontgomeryModInt<T> &rhs) {
+    return MontgomeryModInt<T>(lhs) /= rhs;
+}
+
+template <typename T, typename U>
+MontgomeryModInt<T> operator/(const MontgomeryModInt<T> &lhs, U rhs) {
+    return MontgomeryModInt<T>(lhs) /= rhs;
+}
+
+template <typename T, typename U>
+MontgomeryModInt<T> operator/(U lhs, const MontgomeryModInt<T> &rhs) {
+    return MontgomeryModInt<T>(lhs) /= rhs;
+}
+
+template <typename T, typename U>
+U & operator<<(U &stream, const MontgomeryModInt<T> &v) {
+    return stream << v();
+}
+
+template <typename T, typename U>
+U & operator>>(U &stream, MontgomeryModInt<T> &v) {
+    typename common_type<typename MontgomeryModInt<T>::T, long long>::type x;
+    stream >> x;
+    v = MontgomeryModInt<T>(x);
+    return stream;
+}
+
+constexpr unsigned long long MODULO = 1e9 + 7;
+using modint = MontgomeryModInt<integral_constant<decay<decltype(MODULO)>::type, MODULO>>;
+
+template <typename T, typename F>
+void fast_subset_transform(int n, vector<T> &v, F &&f) {
+    for (int k = 1; k < n; k <<= 1)
+        for (int i = 0; i < n; i += k << 1)
+            for (int j = 0; j < k; j++) v[i + j + k] = f(v[i + j + k], v[i + j]);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    modint::init();
+
+    int n;
+    cin >> n;
+
+    vector<modint> dp1(1 << 10, 0), dp2(1 << 10, 0);
+    for (int x = 1; x <= n; x++) {
+        string I;
+        cin >> I;
+
+        int len = I.size();
+        vector<int> digit(len);
+        for (int i = 0; i < len; i++) digit[i] = I[i] - '0';
+
+        for (int mask = 0; mask < 1 << 10; mask++) {
+            vector<vector<array<int, 2>>> memo(len, vector<array<int, 2>>(2, {-1, -1}));
+            auto dp = [&](auto &&self, int pos = 0, int bound = 1, int nlz = 0) {
+                if (pos == len) return nlz;
+                if (~memo[pos][bound][nlz]) return memo[pos][bound][nlz];
+                memo[pos][bound][nlz] = 0;
+                for (int d = 0; d <= (bound ? digit[pos] : 9); d++)
+                    if (!d && !nlz) memo[pos][bound][nlz] += self(self, pos + 1, bound && d == digit[pos], 0);
+                    else if ((mask >> d) & 1) memo[pos][bound][nlz] += self(self, pos + 1, bound && d == digit[pos], 1);
+                return memo[pos][bound][nlz];
+            };
+            dp1[mask] = dp(dp);
+        }
+        fast_subset_transform(1 << 10, dp1, [](auto x, auto y) { return x - y; });
+
+        fill(dp2.begin(), dp2.end(), 0);
+        dp2[0] = 1;
+        for (int m1 = 1; m1 < 1 << 10; m1++)
+            if (dp1[m1])
+                for (int m2 = 0; m2 < 1 << 10; m2++)
+                    if (!(m1 & m2) && dp2[m2]) dp2[m1 | m2] += dp1[m1] * dp2[m2];
+
+        cout << "Case " << x << ": " << accumulate(dp2.begin(), dp2.end(), (modint) 0) - 1 << "\n";
+    }
+}
