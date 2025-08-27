d3dfbdd202e4f321ba42b76d45ebee449c8ff938
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-08-27 22:45:05 +0800

    Update feistelfun.cpp

--- open/feistelfun.cpp
+++ open/feistelfun.cpp
@@ -8,28 +8,44 @@ int main() {
     int m;
     cin >> m;
 
-    int bound = m / 2 + 1;
-    unordered_map<bitset<80>, long long> seen;
-    bitset<80> x1(0), y1(1), x2(1), y2(0);
-
-    auto f = [&](bitset<80> y, int shift = 1) -> bitset<80> {
-        return ((y << shift) >> m) ^ (y << shift) & bitset<80>((1LL << m) - 1);
+    auto wrap = [&](__int128 mask, int s) {
+        return ((mask >> (m - s)) ^ (mask << s)) & (((__int128) 1 << m) - 1);
     };
 
-    for (auto i = 0LL; i < 1 << bound; i++) {
-        if (!seen.count((y1 << m) | x1)) seen[(y1 << m) | x1] = i;
-        x1 ^= f(y1);
-        swap(y1, x1);
+    int n = 1 << 19;
+    long long x1 = 0, y1 = 1, x2 = 1, y2 = 0;
+    vector<pair<long long, int>> baby(n), giant(n);
+    for (int i = 0; i < n; i++) {
+        auto mask = (y1 << m) | x1;
+        baby[i] = {mask, i};
+        x1 ^= wrap(y1, 1);
+        swap(x1, y1);
     }
+
+    for (int i = 0, k = n % m; i < n; i++) {
+        auto mask = (y2 << m) | x2;
+        giant[i] = {mask, i};
+        x2 ^= wrap(y1, k);
+        y2 ^= wrap(x1, k);
+        swap(y1, x2);
+        swap(x1, y2);
+    }
+
+    auto dedupe = [&](auto &v) {
+        sort(v.begin(), v.end());
+        v.erase(unique(v.begin(), v.end(), [&](auto p1, auto p2) { return p1.first == p2.first; }), v.end());
+    };
+    dedupe(baby);
+    dedupe(giant);
 
     auto rounds = LLONG_MAX;
-    for (auto i = 0LL; i < 1 << bound; i++) {
-        if (seen.count((y2 << m) | x2)) rounds = min(rounds, seen[(y2 << m) | x2] + (i << bound));
-        x2 ^= f(y1, (1 << bound) % m);
-        y2 ^= f(x1, (1 << bound) % m);
-        swap(y1, x2);
-        swap(y2, x1);
+    for (int i = 0, j = 0; i < baby.size() && j < giant.size();) {
+        if (baby[i].first == giant[j].first) {
+            rounds = min(rounds, ((long long) giant[j].second << 19) + baby[i].second);
+            i++;
+            j++;
+        } else if (baby[i].first < giant[j].first) i++;
+        else j++;
     }
-
     cout << rounds + 1;
 }
