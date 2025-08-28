f064d48962bdfb4bbedfee5e6f20aeb925aba16a
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-08-29 05:31:56 +0800

    Update efficientbusrouting.cpp

--- open/efficientbusrouting.cpp
+++ open/efficientbusrouting.cpp
@@ -1,19 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> dfs(vector<vector<int>> &adj_list, int v, int prev = -1) {
-    vector<int> leaves;
-    for (int u : adj_list[v]) {
-        if (u == prev) continue;
-        
-        auto children = dfs(adj_list, u, v);
-        leaves.insert(leaves.end(), children.begin(), children.end());
-    }
-
-    if (leaves.empty()) leaves.emplace_back(v + 1);
-    return leaves;
-}
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -22,16 +9,27 @@ int main() {
     cin >> n;
 
     vector<vector<int>> adj_list(n);
-    for (int i = 0; i < n - 1; i++) {
+    for (int _ = 0; _ < n - 1; _++) {
         int u, v;
         cin >> u >> v;
-        
+
         adj_list[u - 1].emplace_back(v - 1);
         adj_list[v - 1].emplace_back(u - 1);
     }
 
     int root = find_if(adj_list.begin(), adj_list.end(), [&](auto neighbors) { return neighbors.size() > 1; }) - adj_list.begin();
-    auto leaves = dfs(adj_list, root);
+    auto dfs = [&](auto &&self, int v, int prev = -1) -> vector<int> {
+        vector<int> leaves;
+        for (int u : adj_list[v]) {
+            if (u != prev) {
+                auto children = self(self, u, v);
+                leaves.insert(leaves.end(), children.begin(), children.end());
+            }
+        }
+        if (leaves.empty()) leaves.emplace_back(v + 1);
+        return leaves;
+    };
+    auto leaves = dfs(dfs, root);
 
     int size = leaves.size();
     cout << (size + 1) / 2 << "\n";
