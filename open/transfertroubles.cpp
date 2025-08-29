982b36d680d17ca78cc6d6499190ce2f30d7345f
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-08-29 16:37:09 +0800

    Create transfertroubles.cpp

---
+++ open/transfertroubles.cpp
@@ -0,0 +1,131 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+struct FlowNetwork {
+    struct Arc {
+        int u, rev;
+        T cap, initial_cap;
+        Arc(int u, int rev, T cap) : u(u), rev(rev), cap(cap), initial_cap(cap) {}
+    };
+
+    int n;
+    vector<vector<Arc>> network;
+    vector<int> dist;
+    vector<typename vector<Arc>::iterator> it;
+
+    FlowNetwork(int n) : n(n), network(n), dist(n), it(n) {}
+
+    void add_arc(int u, int v, T cap_uv, T cap_vu = 0) {
+        if (u == v) return;
+
+        network[u].emplace_back(v, network[v].size(), cap_uv);
+        network[v].emplace_back(u, network[u].size() - 1, cap_vu);
+    }
+
+    bool bfs(int s, int t) {
+        fill(dist.begin(), dist.end(), -1);
+        dist[s] = 0;
+        queue<int> q;
+        q.emplace(s);
+        while (!q.empty()) {
+            int v = q.front();
+            q.pop();
+
+            for (auto [u, _, cap, __] : network[v])
+                if (cap > 0 && !~dist[u]) {
+                    dist[u] = dist[v] + 1;
+                    q.emplace(u);
+                }
+        }
+        return ~dist[t];
+    }
+
+    T dfs(int v, int t, T f) {
+        if (v == t) return f;
+
+        for (; it[v] != network[v].end(); it[v]++) {
+            auto &[u, rev, cap, _] = *it[v];
+            if (cap > 0 && dist[u] == dist[v] + 1) {
+                T aug = dfs(u, t, min(f, cap));
+                if (aug > 0) {
+                    cap -= aug;
+                    network[u][rev].cap += aug;
+                    return aug;
+                }
+            }
+        }
+        return 0;
+    }
+
+    T max_flow(int s, int t) {
+        T flow = 0, aug;
+        while (bfs(s, t)) {
+            for (int v = 0; v < n; v++) it[v] = network[v].begin();
+            while ((aug = dfs(s, t, numeric_limits<T>::max())) > 0) flow += aug;
+        }
+        return flow;
+    }
+
+    vector<tuple<int, int, T>> flow_decomposition() {
+        vector<tuple<int, int, T>> path;
+        for (int v = 0; v < n; v++)
+            for (auto [u, _, cap, initial_cap] : network[v])
+                if (cap > 0 && cap > initial_cap) path.emplace_back(u, v, cap - initial_cap);
+
+        return path;
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n, r, p;
+        cin >> n >> r >> p;
+
+        unordered_map<string, int> indices;
+        vector<long long> b(n, 0);
+        for (int i = 0; i < n; i++) {
+            string bank;
+            cin >> bank >> b[i];
+
+            indices[bank] = i;
+        }
+
+        vector<array<int, 3>> edges;
+        while (r--) {
+            string bank1, bank2;
+            int l;
+            cin >> bank1 >> bank2 >> l;
+
+            edges.push_back({indices[bank1], indices[bank2], l});
+        }
+
+        vector<int> cost(n, 0);
+        while (p--) {
+            string bank;
+            int a;
+            cin >> bank >> a;
+
+            cost[indices[bank]] += a;
+        }
+
+        FlowNetwork<int> fn(n + 2);
+        int total = 0;
+        for (int i = 0; i < n; i++) {
+            int c = b[i] - cost[i];
+            if (c > 0) fn.add_arc(n, i, c);
+            else if (c < 0) {
+                fn.add_arc(i, n + 1, -c);
+                total += -c;
+            }
+        }
+        for (auto [u, v, c] : edges) fn.add_arc(u, v, c);
+        cout << (fn.max_flow(n, n + 1) == total ? "yes\n" : "no\n");
+    }
+}
