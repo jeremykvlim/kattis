d04eda013be1d00454c7e18b5907eb061ea22d00
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-09-01 03:42:13 +0800

    Update biggest.cpp

--- nus/cs3233/biggest.cpp
+++ nus/cs3233/biggest.cpp
@@ -1,8 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-constexpr int LIMIT = 360 * 60 * 60;
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -10,25 +8,26 @@ int main() {
     int t;
     cin >> t;
 
+    const int limit = 360 * 60 * 60;
     while (t--) {
         int r, n, a, m, s;
         cin >> r >> n >> a >> m >> s;
 
-        vector<bool> sliced(LIMIT + 1);
+        vector<bool> sliced(limit + 1);
         for (int i = 0, pos = 0; i < n && !sliced[pos]; i++) {
             sliced[pos] = true;
             pos += a * 60 * 60 + m * 60 + s;
-            if (pos >= LIMIT) pos -= LIMIT;
+            if (pos >= limit) pos -= limit;
         }
 
-        sliced[LIMIT] = true;
+        sliced[limit] = true;
         int dist = 0;
-        for (int curr = 0, prev = 0; curr <= LIMIT; curr++)
+        for (int curr = 0, prev = 0; curr <= limit; curr++)
             if (sliced[curr]) {
                 dist = max(dist, curr - prev);
                 prev = curr;
             }
 
-        cout << fixed << setprecision(4) << M_PI * r * r * dist / LIMIT << "\n";
+        cout << fixed << setprecision(4) << M_PI * r * r * dist / limit << "\n";
     }
 }
