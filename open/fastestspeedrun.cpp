e970a057d3b5fbcdcb6c72c37cea83c3ef1ded9d
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-08-31 16:39:09 +0800

    Update fastestspeedrun.cpp

--- open/fastestspeedrun.cpp
+++ open/fastestspeedrun.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 struct DisjointSets {
-    vector<int> sets;
+    vector<int> sets, parent;
 
     int find(int v) {
         return sets[v] == v ? v : (sets[v] = find(sets[v]));
@@ -11,71 +11,131 @@ struct DisjointSets {
     bool unite(int u, int v) {
         int u_set = find(u), v_set = find(v);
         if (u_set != v_set) {
-            sets[v_set] = u_set;
+            parent[v_set] = sets[v_set] = u_set;
             return true;
         }
         return false;
     }
 
-    DisjointSets(int n) : sets(n) {
+    DisjointSets(int n) : sets(n), parent(n, -1) {
         iota(sets.begin(), sets.end(), 0);
     }
 };
 
-pair<vector<int>, vector<long long>> edmonds(const vector<vector<pair<int, int>>> &adj_list_transpose) {
-    int n = adj_list_transpose.size();
-
-    vector<vector<long long>> dist(n, vector<long long>(n, 1e18));
-    for (int v = 0; v < n; v++)
-        for (auto [u, w] : adj_list_transpose[v]) dist[v][u] = min(dist[v][u], (long long) w);
+template<typename T>
+pair<T, vector<int>> edmonds_dense(int n, vector<vector<pair<int, T>>> adj_list_transpose, int root = 0) {
+    for (auto &neighbors : adj_list_transpose) sort(neighbors.begin(), neighbors.end(), [&](const auto &p1, const auto &p2) { return p1.second > p2.second; });
 
     DisjointSets dsu(n);
-    stack<int> path;
-    vector<int> state(n, 0), prev(n);
-    state[0] = 2;
-    vector<long long> weights(n, 0);
-    for (int i = 0; i < n; i++) {
-        if (state[dsu.find(i)] == 2) continue;
-
-        int v = dsu.find(i);
-        while (state[v] != 2)
-            if (!state[v]) {
-                for (int u = 0; u < n; u++)
-                    if (dsu.find(u) == v) dist[v][u] = 1e18;
-
-                int u = 0;
-                auto w = dist[v][0];
-                for (int t = 0; t < n; t++)
-                    if (w > dist[v][t]) {
-                        w = dist[v][t];
-                        u = t;
-                    }
-
-                path.emplace(v);
-                prev[v] = u;
-                weights[v] = w;
-                state[v] = 1;
-                v = dsu.find(u);
-            } else {
-                while (path.top() != v) {
-                    int u = path.top();
-                    path.pop();
-
-                    if (dsu.unite(v, u))
-                        for (int t = 0; t < n; t++) dist[v][t] = min(dist[v][t], dist[u][t] + weights[v] - weights[u]);
-                }
-
-                v = dsu.find(v);
-                state[v] = 0;
+    vector<T> cost(n, 0), loss(n, 0);
+    vector<pair<int, int>> chosen_edge(n, {-1, -1});
+    vector<int> visited(n, 0);
+    vector<bool> cycle(n, false);
+    stack<pair<vector<int>, vector<pair<int, int>>>> history;
+    for (T len = 0;;) {
+        int root_set = dsu.find(root);
+        for (int v = 0; v < n; v++)
+            if (v == dsu.find(v)) {
+                cost[v] = v != root_set ? numeric_limits<T>::max() : 0;
+                chosen_edge[v] = {-1, -1};
             }
 
-        while (!path.empty()) {
-            state[path.top()] = 2;
-            path.pop();
+        for (int v = 0; v < n; v++) {
+            while (!adj_list_transpose[v].empty() && dsu.find(v) == dsu.find(adj_list_transpose[v].back().first)) adj_list_transpose[v].pop_back();
+            if (!adj_list_transpose[v].empty()) {
+                auto [u, w] = adj_list_transpose[v].back();
+                T c = w - loss[v];
+                int v_set = dsu.find(v);
+                if (cost[v_set] > c) {
+                    cost[v_set] = c;
+                    chosen_edge[v_set] = {u, v};
+                }
+            }
         }
-    }
 
-    return {prev, weights};
+        for (int v = 0; v < n; v++)
+            if (v == dsu.find(v) && cost[v] == numeric_limits<T>::max()) return {-1, {}};
+
+        int count = 1;
+        vector<vector<int>> cycles;
+        fill(cycle.begin(), cycle.end(), false);
+        fill(visited.begin(), visited.end(), 0);
+        visited[root_set] = count++;
+        for (int v = 0; v < n; v++)
+            if (v == dsu.find(v) && !visited[v]) {
+                int u = v;
+                stack<int> s;
+                while (!visited[u]) {
+                    visited[u] = count;
+                    s.emplace(u);
+                    u = dsu.find(chosen_edge[u].first);
+                }
+
+                if (visited[u] == count) {
+                    cycle[u] = true;
+                    cycles.emplace_back(vector<int>{u});
+                    while (s.top() != u) {
+                        cycle[s.top()] = true;
+                        cycles.back().emplace_back(s.top());
+                        s.pop();
+                    }
+                }
+                count++;
+            }
+
+        if (!cycles.empty()) {
+            for (int v = 0; v < n; v++) {
+                int v_set = dsu.find(v);
+                if (cycle[v_set]) {
+                    if (v == v_set) len += cost[v_set];
+                    loss[v] += cost[v_set];
+                }
+            }
+
+            for (auto &c : cycles) {
+                vector<pair<int, int>> edges;
+                for (int ci : c) {
+                    if (ci != c[0]) dsu.unite(c[0], ci);
+                    edges.emplace_back(chosen_edge[ci]);
+                }
+                history.emplace(c, edges);
+            }
+            continue;
+        }
+
+        for (int v = 0; v < n; v++)
+            if (v == dsu.find(v)) len += cost[v];
+
+        vector<int> mst(n, -1);
+        for (int v = 0; v < n; v++)
+            if (v == dsu.find(v)) {
+                if (v == dsu.find(root)) chosen_edge[v] = {-1, root};
+                else mst[chosen_edge[v].second] = chosen_edge[v].first;
+            }
+
+        while (!history.empty()) {
+            auto [c, edges] = history.top();
+            history.pop();
+
+            auto [u, v] = chosen_edge[c[0]];
+            for (int ci : c) visited[ci] = count;
+            int t = v;
+            while (t != -1 && visited[t] != count) t = dsu.parent[t];
+            count++;
+
+            for (int i = 0; i < c.size(); i++) 
+                if (c[i] == t) {
+                    mst[v] = u;
+                    chosen_edge[c[i]] = {u, v};
+                } else {
+                    auto [x, y] = edges[i];
+                    mst[y] = x;
+                    chosen_edge[c[i]] = {x, y};
+                }
+        }
+        mst[root] = root;
+        return {len, mst};
+    }
 }
 
 int main() {
@@ -85,19 +145,20 @@ int main() {
     int n;
     cin >> n;
 
-    vector<vector<pair<int, int>>> adj_list_transpose(n + 1);
+    vector<vector<pair<int, long long>>> adj_list_transpose(n + 1);
     for (int i = 1; i <= n; i++) {
-        int x, s;
+        int x;
+        long long s;
         cin >> x >> s;
 
         for (int j = 0; j <= n; j++) {
-            int a;
+            long long a;
             cin >> a;
 
-            if (i != j) adj_list_transpose[i].emplace_back(j, j == x ? s : a);
+            if (i != j) adj_list_transpose[i].emplace_back(j, j == x ? min(a, s) : a);
         }
     }
 
-    auto [prev, weights] = edmonds(adj_list_transpose);
-    cout << accumulate(weights.begin(), weights.end(), 0LL);
+    auto [len, mst] = edmonds_dense(n + 1, adj_list_transpose);
+    cout << len;
 }
