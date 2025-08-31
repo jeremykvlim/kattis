e2bfad73aed74d8c311014bc8527094786c03865
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-09-01 04:49:26 +0800

    Update similarspacing.cpp

--- open/similarspacing.cpp
+++ open/similarspacing.cpp
@@ -1,25 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int build(vector<long long> pref, long long a, long long b) {
-    vector<int> dp(pref.size(), 0);
-    priority_queue<pair<int, int>> pq;
-    int sites = 1;
-    for (int i = 0, j = 0; i < pref.size(); i++) {
-        while (i > j && pref[i] - pref[j] >= b) {
-            pq.emplace(dp[j], j);
-            j++;
-        }
-
-        while (!pq.empty() && pref[i] - pref[pq.top().second] > a) pq.pop();
-
-        dp[i] = pq.empty() ? 1 : pq.top().first + 1;
-        sites = max(sites, dp[i]);
-    }
-
-    return sites;
-}
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -27,30 +8,46 @@ int main() {
     int n, k;
     cin >> n >> k;
 
-    vector<int> dist(n - 1);
     vector<long long> pref(n, 0);
     for (int i = 0; i < n - 1; i++) {
-        cin >> dist[i];
+        int d;
+        cin >> d;
 
-        pref[i + 1] = pref[i] + dist[i];
+        pref[i + 1] = pref[i] + d;
     }
 
-    auto diff = 1e18;
+    vector<int> dp(n);
+    long long diff = 1e18;
     for (int i = 0; i < n; i++)
         for (int j = 0; j < i; j++) {
             auto b = pref[i] - pref[j], l = b - 1, r = min(diff + b, pref[n - 1]);
 
-            if (build(pref, r, b) < k) continue;
+            auto build = [&](vector<long long> pref, auto a) {
+                fill(dp.begin(), dp.end(), 0);
+                int sites = 1;
+                priority_queue<pair<int, int>> pq;
+                for (int i = 0, j = 0; i < pref.size(); i++) {
+                    while (i > j && pref[i] - pref[j] >= b) {
+                        pq.emplace(dp[j], j);
+                        j++;
+                    }
+
+                    while (!pq.empty() && pref[i] - pref[pq.top().second] > a) pq.pop();
+                    sites = max(sites, dp[i] = pq.empty() ? 1 : pq.top().first + 1);
+                }
+
+                return sites;
+            };
+
+            if (build(pref, r) < k) continue;
 
             while (l + 1 < r) {
                 auto m = l + (r - l) / 2;
 
-                if (build(pref, m, b) >= k) r = m;
+                if (build(pref, m) >= k) r = m;
                 else l = m;
             }
-
             diff = min(diff, r - b);
         }
-
     cout << diff;
 }
