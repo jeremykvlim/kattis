39c4d9d64aa2e4a101232b9f5cd65e0ddcbff0de
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-09-07 20:47:35 +0800

    Update imoharderproblem.cpp

--- open/imoharderproblem.cpp
+++ open/imoharderproblem.cpp
@@ -8,9 +8,8 @@ int main() {
     string s;
     cin >> s;
 
-    auto k = (double) count(s.begin(), s.end(), '?') / 2, l = 0.0;
+    auto k = 0.5 * count(s.begin(), s.end(), '?'), l = 0.;
     for (int i = 0; i < s.size(); i++)
         if (s[i] != 'T') l += (s[i] == 'H' ? 2 : 1) * (i + 1);
-
-    cout << fixed << setprecision(6) << l - pow(count(s.begin(), s.end(), 'H') + k, 2) - k / 2;
+    cout << fixed << setprecision(6) << l - pow(count(s.begin(), s.end(), 'H') + k, 2) - 0.5 * k;
 }
