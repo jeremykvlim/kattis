e4ea773709e4093d6cbd70ec2062f26d567268da
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-09-07 20:45:15 +0800

    Update fixingbugs.cpp

--- open/fixingbugs.cpp
+++ open/fixingbugs.cpp
@@ -13,14 +13,14 @@ int main() {
     for (int i = 0; i < b; i++) cin >> p[i] >> s[i];
 
     vector<int> fails(b, 0);
-    vector<vector<vector<double>>> dp(1 << b, vector<vector<double>>(t, vector<double>(t, 0)));
+    vector<vector<vector<double>>> memo(1 << b, vector<vector<double>>(t, vector<double>(t, 0)));
     vector<vector<vector<bool>>> visited(1 << b, vector<vector<bool>>(t, vector<bool>(t, false)));
-    auto fix = [&](auto &&self, int mask = 0, int time = 0, int count = 0) -> double {
+    auto dp = [&](auto &&self, int mask = 0, int time = 0, int count = 0) -> double {
         if (mask == (1 << b) - 1 || time == t) return 0.;
-        if (visited[mask][time][count]) return dp[mask][time][count];
+        if (visited[mask][time][count]) return memo[mask][time][count];
 
         visited[mask][time][count] = true;
-        double prob = -1.0;
+        double prob = -1;
         int j;
         for (int i = 0; i < b; i++)
             if (!((mask >> i) & 1) && p[i] * s[i] > prob) {
@@ -32,14 +32,13 @@ int main() {
 
         p[j] *= f;
         fails[j]++;
-        dp[mask][time][count] += (1. - temp) * self(self, mask, time + 1, count + 1);
+        memo[mask][time][count] += (1. - temp) * self(self, mask, time + 1, count + 1);
 
         p[j] = temp;
         fails[j]--;
-        dp[mask][time][count] += p[j] * (s[j] + self(self, mask | (1 << j), time + 1, count - fails[j]));
+        memo[mask][time][count] += p[j] * (s[j] + self(self, mask | (1 << j), time + 1, count - fails[j]));
 
-        return dp[mask][time][count];
+        return memo[mask][time][count];
     };
-
-    cout << fixed << setprecision(6) << fix(fix);
+    cout << fixed << setprecision(6) << dp(dp);
 }
