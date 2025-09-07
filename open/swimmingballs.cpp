62279f684a8e3271a6c6cd483ac2554326fcf144
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-09-07 20:50:36 +0800

    Update swimmingballs.cpp

--- open/swimmingballs.cpp
+++ open/swimmingballs.cpp
@@ -1,20 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-double height(double R, double w) {
-    if (w >= 1) return 2 * R;
-
-    double l = 0, r = 2 * R, m;
-    while (l + 1e-4 < r && l + l * 1e-4 < r) {
-        m = l + (r - l) / 2;
-
-        if (M_PI * m * m * (R - m / 3.0) >= w * 4 * M_PI * pow(R, 3) / 3.0) r = m;
-        else l = m;
-    }
-
-    return m;
-}
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -37,8 +23,18 @@ int main() {
             auto volume = [&]() {
                 double total = 0;
                 for (auto [R, w] : balls) {
-                    auto h = height(R, w);
-                    total += M_PI * pow(min(m, h), 2) * (R - min(m, h) / 3.0);
+                    auto h = 2 * R;
+                    if (w < 1) {
+                        double l = 0, r = 2 * R, mid;
+                        while (l + 1e-4 < r && l + l * 1e-4 < r) {
+                            mid = l + (r - l) / 2;
+
+                            if (M_PI * mid * mid * (R - mid / 3) >= w * 4 * M_PI * pow(R, 3) / 3) r = mid;
+                            else l = mid;
+                        }
+                        h = mid;
+                    }
+                    total += M_PI * pow(min(m, h), 2) * (R - min(m, h) / 3);
                 }
 
                 return total;
@@ -47,7 +43,6 @@ int main() {
             if (W * L * m - volume() > V) r = m;
             else l = m;
         }
-
         cout << fixed << setprecision(4) << m << "\n";
     }
 }
