4b28692cec15ffd766874fc99574231148bdda61
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-09-07 20:46:09 +0800

    Update radioreceiver.cpp

--- open/radioreceiver.cpp
+++ open/radioreceiver.cpp
@@ -16,9 +16,9 @@ int main() {
         for (int i = 0; i < n; i++) cin >> p[i] >> t[i];
 
         int d = 0;
-        for (int i = 0; i < n; i++) 
+        for (int i = 0; i < n; i++)
             for (int j = 0; j < i; j++) d = max(d, abs(p[i] - p[j]) - abs(t[i] - t[j]));
-        
-        cout << "Case #" << x << ": " << fixed << setprecision(9) << d / 2.0 << "\n";
+
+        cout << "Case #" << x << ": " << fixed << setprecision(9) << d * 0.5 << "\n";
     }
 }
