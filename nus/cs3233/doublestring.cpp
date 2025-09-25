8b47bad6144f266ec27a1385075584462aa9718d
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-09-25 17:32:28 +0800

    Update

--- nus/cs3233/doublestring.cpp
+++ nus/cs3233/doublestring.cpp
@@ -2,10 +2,11 @@
 using namespace std;
 
 vector<int> z_function(const string &s) {
-    vector<int> z(s.size());
-    for (int i = 1, l = 0, r = 0; i < s.size(); i++) {
+    int n = s.size();
+    vector<int> z(n, 0);
+    for (int i = 1, l = 0, r = 0; i < n; i++) {
         if (i < r) z[i] = min(r - i, z[i - l]);
-        while (i + z[i] < s.size() && s[z[i]] == s[i + z[i]]) z[i]++;
+        while (i + z[i] < n && s[z[i]] == s[i + z[i]]) z[i]++;
         if (i + z[i] > r) {
             l = i;
             r = i + z[i];
