8db543df35a16b1e73be66018f0e5c3b07fda6f5
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-10-16 06:48:09 +0800

    Update maxclique.cpp

--- nus/cs3233/maxclique.cpp
+++ nus/cs3233/maxclique.cpp
@@ -1,34 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int bron_kerbosch(int n, vector<unsigned __int128> adj_list) {
+pair<int, unsigned __int128> bron_kerbosch(int n, vector<unsigned __int128> adj_list) {
     int max_clique_size = 0;
+    unsigned __int128 clique_mask = 0;
 
-    auto dfs = [&](auto &&self, unsigned __int128 p, unsigned __int128 x = 0, unsigned __int128 r = 0) {
-        if (!p && !x) {
+    auto dfs = [&](auto &&self, unsigned __int128 p, unsigned __int128 r = 0) {
+        if (!p) {
             int size = popcount(r);
-            max_clique_size = max(max_clique_size, size);
+            if (max_clique_size < size) {
+                max_clique_size = size;
+                clique_mask = r;
+            }
             return;
         }
+
+        if (popcount(r) + popcount(p) <= max_clique_size) return;
 
         auto lsb = [&](unsigned __int128 v) {
             return v ? ((v & ULLONG_MAX) ? countr_zero(v) : 64 + countr_zero(v >> 64)) : 0;
         };
-        int pivot = lsb(p | x);
 
-        auto candidates = p & ~adj_list[pivot];
-        while (candidates) {
-            int v = lsb(candidates);
+        auto color_order = [&](unsigned __int128 p) -> vector<pair<int, int>> {
+            int color = 0;
+            vector<pair<int, int>> order;
+            while (p) {
+                color++;
+                for (auto q = p; q;) {
+                    int v = lsb(q);
+                    auto mask = (unsigned __int128) 1 << v;
+                    q &= q - 1;
+                    order.emplace_back(v, color);
+                    p &= ~mask;
+                    q &= ~adj_list[v];
+                }
+            }
+            reverse(order.begin(), order.end());
+            return order;
+        };
+
+        for (auto [v, color] : color_order(p)) {
+            if (popcount(r) + color <= max_clique_size) return;
             auto mask = (unsigned __int128) 1 << v;
-            self(self, p & adj_list[v], x & adj_list[v], r | mask);
+            self(self, p & adj_list[v], r | mask);
             p &= ~mask;
-            x |= mask;
-            candidates &= ~mask;
         }
     };
     dfs(dfs, ((unsigned __int128) 1 << n) - 1);
 
-    return max_clique_size;
+    return {max_clique_size, clique_mask};
 }
 
 int main() {
@@ -46,5 +66,5 @@ int main() {
         adj_list[u] |= (unsigned __int128) 1 << v;
         adj_list[v] |= (unsigned __int128) 1 << u;
     }
-    cout << bron_kerbosch(V, adj_list);
+    cout << bron_kerbosch(V, adj_list).first;
 }
