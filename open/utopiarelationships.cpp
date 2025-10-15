d3096f1fbc097eb255e715de7edd809d9b514ebc
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-10-15 12:22:02 +0800

    Create utopiarelationships.cpp

---
+++ open/utopiarelationships.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+tuple<vector<int>, vector<int>, int> hopcroft_karp(int n, int m, const vector<pair<int, int>> &edges) {
+    vector<int> adj_list(edges.size()), l(n, -1), r(m, -1), degree(n + 1, 0);
+    for (auto [u, v] : edges) degree[u]++;
+    for (int i = 1; i <= n; i++) degree[i] += degree[i - 1];
+    for (auto [u, v] : edges) adj_list[--degree[u]] = v;
+
+    int matches = 0;
+    vector<int> src(n), prev(n);
+    queue<int> q;
+    for (;;) {
+        fill(src.begin(), src.end(), -1);
+        fill(prev.begin(), prev.end(), -1);
+
+        for (int i = 0; i < n; i++)
+            if (!~l[i]) q.emplace(src[i] = prev[i] = i);
+
+        int temp = matches;
+        while (!q.empty()) {
+            int v = q.front();
+            q.pop();
+
+            if (~l[src[v]]) continue;
+
+            for (int j = degree[v]; j < degree[v + 1]; j++) {
+                int u = adj_list[j];
+
+                if (!~r[u]) {
+                    while (~u) {
+                        r[u] = v;
+                        swap(l[v], u);
+                        v = prev[v];
+                    }
+
+                    matches++;
+                    break;
+                }
+
+                if (!~prev[r[u]]) {
+                    q.emplace(u = r[u]);
+                    prev[u] = v;
+                    src[u] = src[v];
+                }
+            }
+        }
+
+        if (temp == matches) return {l, r, matches};
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<pair<int, int>> edges(2 * m);
+    for (int i = 0; i < m; i++) {
+        int a, b;
+        cin >> a >> b;
+        a--;
+        b--;
+
+        edges[2 * i] = {a, b};
+        edges[2 * i + 1] = {b, a};
+    }
+
+    auto [l, r, matches] = hopcroft_karp(n, n, edges);
+    if (matches != n) {
+        cout << -1;
+        exit(0);
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) cout << ((l[i] == j) + (r[j] == i)) * 5000 << " ";
+        cout << "\n";
+    }
+}
