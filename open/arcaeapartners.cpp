bc4a27ce3188e977535ff0fcd300f387fd9bf580
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-10-17 01:45:20 +0800

    Update

--- open/arcaeapartners.cpp
+++ open/arcaeapartners.cpp
@@ -2,21 +2,21 @@
 using namespace std;
 
 template <typename T>
-struct FenwickTree2D {
-    vector<vector<T>> BIT;
+struct FenwickTree {
+    vector<T> BIT;
     function<T(T, T)> f;
 
-    FenwickTree2D(int n, int m, function<T(T, T)> func) : BIT(n, vector<T>(m, 0)), f(move(func)) {}
-
-    void update(int i, int j, T v) {
-        for (; i && i < BIT[j].size(); i += i & -i) BIT[j][i] = f(BIT[j][i], v);
+    void update(int i, T v) {
+        for (; i && i < BIT.size(); i += i & -i) BIT[i] = f(BIT[i], v);
     }
 
-    T range_query(int i, int j) {
+    T range_query(int i) {
         T v = 0;
-        for (; i; i &= i - 1) v = f(v, BIT[j][i]);
+        for (; i; i &= i - 1) v = f(v, BIT[i]);
         return v;
     }
+
+    FenwickTree(int n, function<T(T, T)> func) : BIT(n, 0), f(move(func)) {}
 };
 
 int main() {
@@ -26,33 +26,33 @@ int main() {
     int n, k;
     cin >> n >> k;
 
+    vector<int> p(n);
     vector<array<int, 3>> values;
-    map<int, int> indices;
     for (int i = 0; i < n; i++) {
-        int g, p, ga, pa;
-        cin >> g >> p >> ga >> pa;
+        int g, ga, pa;
+        cin >> g >> p[i] >> ga >> pa;
 
-        indices[p];
-        values.push_back({g, p, 0});
+        values.push_back({g, p[i], 0});
         if (ga && pa) {
-            indices[pa];
+            p.emplace_back(pa);
             values.push_back({ga, pa, 1});
         }
     }
+    sort(p.begin(), p.end());
+    p.erase(unique(p.begin(), p.end()), p.end());
+    int count = p.size();
 
-    int index = 1;
-    for (auto &[p, i] : indices) i = index++;
-    for (auto &[g, p, b] : values) p = indices[p];
+    for (auto &v : values) v[1] = lower_bound(p.begin(), p.end(), v[1]) - p.begin() + 1;
     sort(values.begin(), values.end(), [&](auto v1, auto v2) { return v1[0] != v2[0] ? v1[0] > v2[0] : v1[1] < v2[1]; });
 
-    FenwickTree2D<int> fw(max(n, k) + 1, index + 1, [](int x, int y) { return max(x, y); });
-    for (auto [g, p, b] : values)
+    vector<FenwickTree<int>> fws(max(n, k) + 1, FenwickTree<int>(count + 1, [](int x, int y) { return max(x, y); }));
+    for (auto [g, j, b] : values)
         if (!b)
-            for (int i = 0; i <= k; i++) fw.update(p, i, fw.range_query(p, i) + 1);
+            for (int i = 0; i <= k; i++) fws[i].update(j, fws[i].range_query(j) + 1);
         else
-            for (int i = k; i; i--) fw.update(p, i, fw.range_query(p, i - 1) + 1);
+            for (int i = k; i; i--) fws[i].update(j, fws[i - 1].range_query(j) + 1);
 
     int d = 0;
-    for (int i = 1; i <= n; i++) d = max(d, fw.range_query(index, i));
+    for (int i = 1; i <= n; i++) d = max(d, fws[i].range_query(count));
     cout << d;
 }
