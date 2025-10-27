afbbe977804623f2656bb52882b33a4c55015dbf
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-10-27 17:52:25 +0800

    Update trafikverketsplan.cpp

--- open/trafikverketsplan.cpp
+++ open/trafikverketsplan.cpp
@@ -17,21 +17,18 @@ int main() {
         adj_list[b].emplace_back(a, false);
     }
 
-    int lg = __lg(n) + 1;
-    vector<vector<int>> lift(lg, vector<int>(n + 1, 0));
-    vector<int> up(n + 1, 0), down(n + 1, 0), depth(n + 1, 0);
-    vector<bool> visited(n + 1, false);
-    visited[1] = true;
-    queue<int> q;
-    q.emplace(1);
-    while (!q.empty()) {
-        int v = q.front();
-        q.pop();
+    auto lsb = [&](int x) {
+        return x & -x;
+    };
+
+    vector<pair<int, int>> tour;
+    vector<int> index(n + 1), depth(n + 1, 0), up(n + 1, 0), down(n + 1, 0), anc_mask(n + 1, 0), head(n + 2);
+    auto dfs = [&](auto &&self, int v = 1, int prev = 1) -> void {
+        tour.emplace_back(v, prev);
+        index[v] = tour.size();
 
         for (auto [u, d] : adj_list[v]) {
-            if (!visited[u]) {
-                visited[u] = true;
-                lift[0][u] = v;
+            if (u != prev) {
                 depth[u] = depth[v] + 1;
                 if (d) {
                     down[u] = down[v] + 1;
@@ -40,30 +37,29 @@ int main() {
                     up[u] = up[v] + 1;
                     down[u] = down[v];
                 }
-                q.emplace(u);
+                self(self, u, v);
+                head[index[u]] = v;
+                if (lsb(index[v]) < lsb(index[u])) index[v] = index[u];
             }
         }
-    }
+    };
+    dfs(dfs);
+    for (auto [v, p] : tour) anc_mask[v] = anc_mask[p] | lsb(index[v]);
 
-    for (int i = 1; i < lg; i++)
-        for (int v = 1; v <= n; v++) lift[i][v] = lift[i - 1][lift[i - 1][v]];
-
-    auto lca = [&](int u, int v) {
-        if (depth[u] < depth[v]) swap(u, v);
-
-        int diff = depth[u] - depth[v];
-        for (int i = 0; i < lg; i++)
-            if ((diff >> i) & 1) u = lift[i][u];
-
-        if (u == v) return u;
-
-        for (int i = lg - 1; ~i; i--)
-            if (lift[i][u] != lift[i][v]) {
-                u = lift[i][u];
-                v = lift[i][v];
+    auto lca = [&](int u, int v) -> int {
+        if (unsigned above = index[u] ^ index[v]; above) {
+            above = (anc_mask[u] & anc_mask[v]) & -bit_floor(above);
+            if (unsigned below = anc_mask[u] ^ above; below) {
+                below = bit_floor(below);
+                u = head[(index[u] & -below) | below];
             }
+            if (unsigned below = anc_mask[v] ^ above; below) {
+                below = bit_floor(below);
+                v = head[(index[v] & -below) | below];
+            }
+        }
 
-        return lift[0][u];
+        return depth[u] < depth[v] ? u : v;
     };
 
     int Q;
@@ -72,7 +68,7 @@ int main() {
     while (Q--) {
         int h, w;
         cin >> h >> w;
-        
+
         int a = lca(h, w);
         if (up[h] - up[a] != depth[h] - depth[a] || down[w] - down[a] != depth[w] - depth[a]) {
             cout << "nej\n";
