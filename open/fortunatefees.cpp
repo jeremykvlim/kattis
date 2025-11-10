f69421ea6fcba0999063bba3ac4ad4ef9e1afd1e
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-11-10 16:09:43 +0800

    Update fortunatefees.cpp

--- open/fortunatefees.cpp
+++ open/fortunatefees.cpp
@@ -3,10 +3,11 @@ using namespace std;
 
 struct BlockCutTree {
     int n;
-    vector<vector<int>> BCT, bccs, lift;
-    vector<int> node_id, depth, in, out, sizes, cuts;
+    vector<vector<int>> BCT, bccs;
+    vector<int> node_id, index, depth, parent, anc_mask, head, sizes, cuts;
     vector<long long> pref_sizes, pref_cuts;
     vector<bool> cutpoint;
+    vector<pair<int, int>> tour;
 
     BlockCutTree(int n, vector<vector<int>> &adj_list) : n(n), node_id(n + 1, -1), cutpoint(n + 1, false) {
         tarjan(adj_list);
@@ -50,10 +51,11 @@ struct BlockCutTree {
 
         int m = node;
         BCT.resize(m);
-        lift.assign(__lg(m) + 1, vector<int>(m));
+        index.resize(m);
         depth.resize(m);
-        in.resize(m);
-        out.resize(m);
+        parent.resize(m);
+        anc_mask.resize(m);
+        head.resize(m + 1);
         node = 0;
         for (auto &comp : bccs) {
             for (int v : comp)
@@ -75,39 +77,43 @@ struct BlockCutTree {
         pref_sizes[0] = sizes[0];
         pref_cuts[0] = cuts[0];
 
-        int count = 0;
-        auto dfs = [&](auto &&self, int v = 0, int prev = -1) -> void {
-            in[v] = count++;
-            if (~prev) {
-                pref_sizes[v] = pref_sizes[prev] + sizes[v];
-                pref_cuts[v] = pref_cuts[prev] + cuts[v];
-                depth[v] = depth[prev] + 1;
-                lift[0][v] = prev;
-                for (int i = 1; i <= __lg(m); i++) lift[i][v] = lift[i - 1][lift[i - 1][v]];
-            }
+        auto lsb = [&](int x) {
+            return x & -x;
+        };
+
+        auto dfs = [&](auto &&self, int v = 0) -> void {
+            tour.emplace_back(v, parent[v]);
+            index[v] = tour.size();
 
             for (int u : BCT[v])
-                if (u != prev) self(self, u, v);
-
-            out[v] = count;
+                if (u != parent[v]) {
+                    parent[u] = v;
+                    depth[u] = depth[v] + 1;
+                    pref_sizes[u] = pref_sizes[v] + sizes[u];
+                    pref_cuts[u] = pref_cuts[v] + cuts[u];
+                    self(self, u);
+                    head[index[u]] = v;
+                    if (lsb(index[v]) < lsb(index[u])) index[v] = index[u];
+                }
         };
         dfs(dfs);
-    }
-
-    bool ancestor(int v, int u) {
-        return in[v] <= in[u] && in[u] < out[v];
+        for (auto [v, p] : tour) anc_mask[v] = anc_mask[p] | lsb(index[v]);
     }
 
     int lca(int u, int v) {
-        if (ancestor(u, v)) return u;
-        if (ancestor(v, u)) return v;
+        if (unsigned above = index[u] ^ index[v]; above) {
+            above = (anc_mask[u] & anc_mask[v]) & -bit_floor(above);
+            if (unsigned below = anc_mask[u] ^ above; below) {
+                below = bit_floor(below);
+                u = head[(index[u] & -below) | below];
+            }
+            if (unsigned below = anc_mask[v] ^ above; below) {
+                below = bit_floor(below);
+                v = head[(index[v] & -below) | below];
+            }
+        }
 
-        if (depth[u] < depth[v]) swap(u, v);
-
-        for (int i = __lg(BCT.size()); ~i; i--)
-            if (!ancestor(lift[i][u], v)) u = lift[i][u];
-
-        return lift[0][u];
+        return depth[u] < depth[v] ? u : v;
     }
 
     bool iscutpoint(int node) {
@@ -141,10 +147,8 @@ int main() {
             int s, e;
             char _;
             cin >> _ >> s >> _ >> e >> _;
-            s++;
-            e++;
 
-            for (int j = s; j <= e; j++) {
+            for (int j = s + 1; j <= e + 1; j++) {
                 adj_list[i].emplace_back(j);
                 adj_list[j].emplace_back(i);
             }
