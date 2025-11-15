d058f043ff670980d5a03cf800d5f0dde99789fe
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-11-15 08:15:30 +0800

    Update seattleberries.cpp

--- open/seattleberries.cpp
+++ open/seattleberries.cpp
@@ -5,29 +5,28 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n;
-    cin >> n;
+    int n, m;
+    cin >> n >> m;
 
-    vector<int> aunt(n);
-    for (int &i : aunt) cin >> i;
-    cin.ignore();
+    vector<int> indices(n + 1);
+    for (int i = 0; i < n; i++) {
+        int p;
+        cin >> p;
 
-    string s;
-    while (getline(cin, s)) {
-        istringstream iss(s);
-        vector<int> p{istream_iterator<int>{iss}, istream_iterator<int>{}};
+        indices[p] = i;
+    }
 
-        vector<int> indices(n);
-        for (int i = 0; i < n; i++) indices[p[i] - 1] = i;
+    while (m-- > 1) {
+        vector<int> lcs;
+        for (int _ = 0; _ < n; _++) {
+            int pi;
+            cin >> pi;
 
-        vector<int> lis;
-        for (int i : indices) {
-            int k = aunt[i] - 1;
-
-            auto it = lower_bound(lis.begin(), lis.end(), k);
-            if (it == lis.end()) lis.emplace_back(k);
-            else *it = k;
+            pi = indices[pi];
+            auto it = lower_bound(lcs.begin(), lcs.end(), pi);
+            if (it == lcs.end()) lcs.emplace_back(pi);
+            else *it = pi;
         }
-        cout << lis.size() << "\n";
+        cout << lcs.size() << "\n";
     }
 }
