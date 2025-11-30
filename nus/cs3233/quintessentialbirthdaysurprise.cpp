1bb9eb9e2646b0c1a7d6e810ace41adfc8726dc8
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-12-01 03:43:31 +0800

    Update quintessentialbirthdaysurprise.cpp

--- nus/cs3233/quintessentialbirthdaysurprise.cpp
+++ nus/cs3233/quintessentialbirthdaysurprise.cpp
@@ -1,26 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+template <typename T>
+struct AffineFunction {
+    T m, c;
+    int i;
+
+    AffineFunction(T m = 0, T c = -1e10, int i = -1) : m(m), c(c), i(i) {}
+
+    T operator()(T x) {
+        return m * x + c;
+    }
+};
+
+template <typename T>
 struct LiChaoSegmentTree {
-    struct Function {
-        long long m, c;
-        int i;
-
-        Function(long long m = 0, long long c = -1e10, int i = -1) : m(m), c(c), i(i) {}
-
-        long long operator()(long long x) {
-            return m * x + c;
-        }
-    };
     int n;
-    vector<long long> xs;
-    vector<Function> ST;
+    vector<T> xs;
+    vector<AffineFunction<T>> ST;
 
-    void insert(Function f) {
+    void insert(AffineFunction<T> f) {
         insert(1, 0, n, f);
     }
 
-    void insert(int i, int l, int r, Function f) {
+    void insert(int i, int l, int r, AffineFunction<T> f) {
         bool left = f(xs[l]) > ST[i](xs[l]);
         if (l + 1 == r) {
             if (left) ST[i] = f;
@@ -35,11 +38,11 @@ struct LiChaoSegmentTree {
         else insert(i << 1 | 1, m, r, f);
     }
 
-    pair<long long, int> query(long long x) {
+    pair<T, int> query(T x) {
         return query(1, lower_bound(xs.begin(), xs.end(), x) - xs.begin(), 0, n);
     }
 
-    pair<long long, int> query(int i, int pos, int l, int r) {
+    pair<T, int> query(int i, int pos, int l, int r) {
         if (l + 1 == r) return {ST[i](xs[pos]), ST[i].i};
 
         int m = midpoint(l, r);
@@ -52,7 +55,7 @@ struct LiChaoSegmentTree {
         return min(l + i, r - (i >> 1));
     }
 
-    LiChaoSegmentTree(int n, const vector<long long> &xs) : n(n), xs(xs), ST(2 * n) {}
+    LiChaoSegmentTree(int n, const vector<T> &xs) : n(n), xs(xs), ST(2 * n) {}
 };
 
 int main() {
@@ -84,7 +87,7 @@ int main() {
         adj_matrix[a][b] = adj_matrix[b][a] = false;
     }
 
-    vector<LiChaoSegmentTree> lcsts(6, LiChaoSegmentTree(V.size(), V));
+    vector<LiChaoSegmentTree<long long>> lcsts(6, LiChaoSegmentTree<long long>(V.size(), V));
     vector<int> prev(n, -1);
     int j = -1;
     long long m = -1e10;
@@ -102,7 +105,7 @@ int main() {
                 }
             }
 
-        LiChaoSegmentTree::Function f(v, 0, i);
+        AffineFunction<long long> f(v, 0, i);
         if (satisfaction < v) satisfaction = v;
         else {
             f.c = satisfaction;
