f3cbc4559c741bef2de8cb0b64d7dcad06cc2536
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-12-04 22:34:37 +0800

    Update skysthelimit.cpp

--- open/skysthelimit.cpp
+++ open/skysthelimit.cpp
@@ -120,36 +120,19 @@ T cross(const Point<T> &a, const Point<T> &b, const Point<T> &c) {
     return (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
 }
 
-template <typename T>
-vector<pair<Point<T>, int>> monotone_chain(vector<pair<Point<T>, int>> points, bool collinear = false) {
-    sort(points.begin(), points.end());
-    points.erase(unique(points.begin(), points.end()), points.end());
-
-    if (points.size() < 3) return points;
-
-    vector<pair<Point<T>, int>> convex_hull;
-
-    auto clockwise = [&](auto p) {
-        T cross_product = cross(convex_hull[convex_hull.size() - 2].first, convex_hull.back().first, p.first);
-        return collinear ? cross_product <= 0 : cross_product < 0;
-    };
-
-    for (auto p : points) {
-        while (convex_hull.size() > 1 && clockwise(p)) convex_hull.pop_back();
-        convex_hull.emplace_back(p);
+template <typename T, int sign = -1, bool collinear = false>
+struct MonotonicHull : deque<Point<T>> {
+    bool violates(const auto &a, const auto &b, const auto &c) {
+        auto cp = cross(a, b, c);
+        if constexpr (sign < 0) cp = -cp;
+        return collinear ? cp >= 0 : cp > 0;
     }
 
-    int s = convex_hull.size();
-    points.pop_back();
-    reverse(points.begin(), points.end());
-    for (auto p : points) {
-        while (convex_hull.size() > s && clockwise(p)) convex_hull.pop_back();
-        convex_hull.emplace_back(p);
+    void add(const auto &p) {
+        while (this->size() > 1 && violates((*this)[1], (*this)[0], p)) this->pop_front();
+        this->emplace_front(p);
     }
-
-    convex_hull.pop_back();
-    return convex_hull;
-}
+};
 
 int main() {
     ios::sync_with_stdio(false);
@@ -159,26 +142,24 @@ int main() {
     double k;
     cin >> n >> k;
 
-    vector<double> h(n + 1);
-    vector<pair<Point<double>, int>> points;
-    points.emplace_back(Point<double>(0, 0), 0);
+    vector<double> h(n + 2, 0);
+    vector<Point<double>> points(n + 2);
+    points[0] = {0, 0};
+    points[n + 1] = {n + 1, 0};
     for (int i = 1; i <= n; i++) {
         cin >> h[i];
 
         h[i] += k * i * (i - n - 1);
-        if (h[i] >= 0) points.emplace_back(Point<double>(i, h[i]), points.size());
+        points[i] = {(double) i, -h[i]};
     }
-    points.emplace_back(Point<double>(n + 1, 0), points.size());
 
-    auto convex_hull = monotone_chain(points);
-    sort(convex_hull.begin(), convex_hull.end(), [&](auto p1, auto p2) { return p1.second < p2.second; });
+    MonotonicHull<double> hull;
+    for (auto p : points) hull.add(p);
 
-    for (int i = 1, j = 0; i <= n; i++) {
-        while (j < convex_hull.size() && points[convex_hull[j].second].first.x < i) j++;
-        if (points[convex_hull[j].second].first.x == i) continue;
-
-        int l = points[convex_hull[j - 1].second].first.x, r = points[convex_hull[j].second].first.x;
-        h[i] = h[l] + (h[r] - h[l]) / (r - l) * (i - l);
+    int m = hull.size();
+    for (int i = m - 1; i; i--) {
+        int l = hull[i].x, r = hull[i - 1].x;
+        for (int j = l; j < r; j++) h[j] = (h[l] * (r - j) + h[r] * (j - l)) / (r - l);
     }
 
     double height = 0;
