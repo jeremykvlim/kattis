1b7d7ed7b371fbcbe52d164df58508d115cddb8f
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-12-06 07:16:22 +0800

    Update hackenbushsimplicity.py

--- open/hackenbushsimplicity.py
+++ open/hackenbushsimplicity.py
@@ -8,20 +8,20 @@ def parse(s):
         return int(P), int(Q)
     return int(s), 0
 
-x_numer, x_denom_exp = parse(sys.stdin.readline())
-y_numer, y_denom_exp = parse(sys.stdin.readline())
+px, qx = parse(sys.stdin.readline())
+py, qy = parse(sys.stdin.readline())
 
 def scale(p, q, Q):
     if q >= Q: return p >> q - Q
     return p << Q - q
 
-l, r = -1, max(x_denom_exp, y_denom_exp) + 1
+l, r = -1, max(qx, qy) + 1
 while l + 1 < r:
     m = l + (r - l) // 2
-    if scale(x_numer, x_denom_exp, m) + 1 < -scale(-y_numer, y_denom_exp, m): r = m
+    if scale(px, qx, m) + 1 < -scale(-py, qy, m): r = m
     else: l = m
 
-P, Q = scale(x_numer, x_denom_exp, r) + 1, r
+P, Q = scale(px, qx, r) + 1, r
 while Q and not P & 1:
     P >>= 1
     Q -= 1
