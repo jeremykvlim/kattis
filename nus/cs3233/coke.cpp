277ac635f9268a53b8f4ae7cee4d1ff915d5d516
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-12-13 07:02:39 +0800

    Update coke.cpp

--- nus/cs3233/coke.cpp
+++ nus/cs3233/coke.cpp
@@ -1,38 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int buy(int cokes, int ones, int fives, vector<vector<vector<int>>> &dp) {
-    if (dp[cokes][ones][fives] != -1) return dp[cokes][ones][fives];
-    if (!cokes) return dp[cokes][ones][fives] = 0;
-
-    int coins = INT_MAX;
-    if (ones >= 8) coins = min(coins, 8 + buy(cokes - 1, ones - 8, fives, dp));
-    if (ones >= 3 && fives >= 1) coins = min(coins, 4 + buy(cokes - 1, ones - 3, fives - 1, dp));
-    if (fives >= 2) coins = min(coins, 2 + buy(cokes - 1, ones + 2, fives - 2, dp));
-    return dp[cokes][ones][fives] = coins;
-}
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int t;
     cin >> t;
-  
-    vector<vector<vector<int>>> dp;
+
     while (t--) {
         int c, n1, n5, n10;
         cin >> c >> n1 >> n5 >> n10;
 
-        int coins = INT_MAX, tens = min(c, n10);
+        int total = n1 + 5 * n5 + 10 * n10;
+        n5 += n10;
 
-        dp.resize(c + 1);
-        for (auto &i : dp) {
-            i.resize(n1 + 2 * tens + n5 + tens + 1);
-            for (auto &j : i) j.resize(n5 + tens + 1, -1);
+        vector<int> dp((n5 + 1) * (n10 + 1), 1e9), temp((n5 + 1) * (n10 + 1), 1e9);
+        auto index = [&](int f, int t) {
+            return f * (n10 + 1) + t;
+        };
+        dp[index(n5 - n10, n10)] = 0;
+        for (int cokes = 0; cokes < c; cokes++) {
+            fill(temp.begin(), temp.end(), 1e9);
+            int remaining = total - 8 * cokes;
+
+            for (int fives = 0; fives <= n5; fives++) {
+                for (int tens = 0; tens <= n10; tens++) {
+                    int coins = dp[index(fives, tens)];
+                    if (coins == 1e9) continue;
+
+                    int ones = remaining - 5 * fives - 10 * tens;
+                    if (ones < 0) continue;
+
+                    auto update = [&](int fives, int tens, int coins) {
+                        int i = index(fives, tens);
+                        temp[i] = min(temp[i], coins);
+                    };
+
+                    if (tens) update(fives, tens - 1, coins + 1);
+                    if (fives > 1) update(fives - 2, tens, coins + 2);
+                    if (fives && ones > 2) update(fives - 1, tens, coins + 4);
+                    if (ones >= 8) update(fives, tens, coins + 8);
+                    if (tens && ones > 2 && fives + 1 <= n5) update(fives + 1, tens - 1, coins + 4);
+                }
+            }
+            dp = temp;
         }
-
-        for (int i = 0; i <= tens && n1 >= 3 * i; i++) coins = min(coins, tens - i + 4 * i + buy(c - tens, n1 - 3 * i + 2 * (tens - i), n5 + i, dp));
-        cout << coins << "\n";
+        cout << *min_element(dp.begin(), dp.end()) << "\n";
     }
 }
