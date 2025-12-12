915fb88620511cabd372e4896230e3b31dcd96d1
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-12-13 06:43:42 +0800

    Update 1dfroggerhard.cpp

--- open/1dfroggerhard.cpp
+++ open/1dfroggerhard.cpp
@@ -1,17 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long dfs(vector<vector<int>> &adj_list, vector<unordered_set<int>> &magic, vector<int> &board, int root, int v) {
-    bool inserted = magic[root].emplace(board[v]).second;
-
-    long long wins = magic[root].size();
-    for (int u : adj_list[v])
-        wins += dfs(adj_list, magic, board, root, u);
-
-    if (inserted) magic[root].erase(board[v]);
-    return wins;
-}
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -19,47 +8,45 @@ int main() {
     int n;
     cin >> n;
 
-    vector<int> board(n + 1), root(n + 1);
+    vector<int> k(n + 1), root(n + 1);
     vector<unordered_set<int>> magic(n + 1);
     for (int i = 1; i <= n; i++) {
-        cin >> board[i];
+        cin >> k[i];
 
-        if (i + board[i] < 1 || i + board[i] > n) {
-            root[i] = i;
-            magic[i].emplace(board[i]);
-        }
+        if (!(1 <= i + k[i] && i + k[i] <= n)) magic[root[i] = i].emplace(k[i]);
     }
 
     vector<bool> visited(n + 1);
     for (int i = 1; i <= n; i++) {
         if (visited[i]) continue;
 
-        int v = i;
+        int j = i;
         vector<int> hops;
-        while (1 <= v && v <= n && !visited[v]) {
-            visited[v] = true;
-            hops.emplace_back(v);
-            v += board[v];
+        for (; 1 <= j && j <= n && !visited[j]; j += k[j]) {
+            visited[j] = true;
+            hops.emplace_back(j);
         }
 
-        if (1 <= v && v <= n) {
+        if (1 <= j && j <= n) {
             reverse(hops.begin(), hops.end());
-            int len = find(hops.begin(), hops.end(), v) - hops.begin();
+            int len = find(hops.begin(), hops.end(), j) - hops.begin();
             if (len < hops.size())
-                for (int j = 0; j <= len; j++) {
-                    root[hops[j]] = hops.front();
-                    magic[hops.front()].emplace(board[hops[j]]);
-                }
+                for (int l = 0; l <= len; l++) magic[root[hops[l]] = hops.front()].emplace(k[hops[l]]);
         }
     }
 
     vector<vector<int>> adj_list(n + 1);
     for (int v = 1; v <= n; v++)
-        if (!root[v]) adj_list[v + board[v]].emplace_back(v);
+        if (!root[v]) adj_list[v + k[v]].emplace_back(v);
 
     auto wins = 0LL;
+    auto dfs = [&](auto &&self, int v, int r) -> void {
+        bool inserted = magic[r].emplace(k[v]).second;
+        wins += magic[r].size();
+        for (int u : adj_list[v]) self(self, u, r);
+        if (inserted) magic[r].erase(k[v]);
+    };
     for (int v = 1; v <= n; v++)
-        if (root[v]) wins += dfs(adj_list, magic, board, root[v], v);
-
+        if (root[v]) dfs(dfs, v, root[v]);
     cout << wins;
 }
