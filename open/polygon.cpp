9abc716ae2c940271bba966f2d87e804c281d03e
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-12-14 20:19:18 +0800

    Update polygon.cpp

--- open/polygon.cpp
+++ open/polygon.cpp
@@ -2,6 +2,17 @@
 using namespace std;
 
 template <typename T>
+bool approximately_equal(const T &v1, const T &v2, double epsilon = 1e-5) {
+    return fabs(v1 - v2) <= epsilon;
+}
+
+template <typename T>
+int sgn(const T &v) {
+    if (!is_floating_point_v<T>) return (v > 0) - (v < 0);
+    return approximately_equal(v, (T) 0) ? 0 : (v > 0) - (v < 0);
+}
+
+template <typename T>
 struct Point {
     T x, y;
 
@@ -120,58 +131,60 @@ T cross(const Point<T> &a, const Point<T> &b) {
     return (a.x * b.y) - (a.y * b.x);
 }
 
+template <typename T>
+bool simple(const vector<Point<T>> &polygon) {
+    int n = polygon.size();
+
+    auto points = polygon;
+    points.resize(2 * n);
+    points[n] = polygon[n - 1];
+    for (int i = n - 1; ~i; i--) {
+        if (i) points[i + n] = points[i - 1];
+        if (points[i + n] < points[i]) swap(points[i + n], points[i]);
+    }
+
+    vector<int> indices(2 * n);
+    iota(indices.begin(), indices.end(), 0);
+    sort(indices.begin(), indices.end(), [&](int i, int j) { return points[i] != points[j] ? points[i] < points[j] : i > j; });
+
+    auto cmp = [&](int i, int j) {
+        T cp = cross(points[j] - points[i], points[j] - points[j + n]);
+        return cp < 0 || !sgn(cp) && cross(points[i] - points[i + n], points[j] - points[j + n]) < 0;
+    };
+    multiset<int, decltype(cmp)> ms(cmp);
+    vector<typename decltype(ms)::const_iterator> its(n);
+    vector<bool> valid(n, false);
+    auto check = [&](int i, int j) {
+        T c1 = cross(points[i] - points[i + n], points[i] - points[j + n]);
+        if (c1 < 0) return false;
+
+        T c2 = cross(points[j] - points[j + n], points[j] - points[i + n]);
+        if (c2 > 0) return false;
+
+        if (c1 || (c2 ? points[i] != points[j] : !sgn(cross(points[i] - points[i + n], points[j] - points[j + n])))) return true;
+        return false;
+    };
+    for (int i : indices)
+        if (i < n) {
+            auto it = its[i] = ms.emplace(i);
+            valid[i] = true;
+            if (it != ms.begin() && check(*prev(it), i) || ++it != ms.end() && check(i, *it)) return false;
+        } else if (valid[i - n]) {
+            auto it = ms.erase(its[i - n]);
+            valid[i - n] = false;
+            if (it != ms.begin() && it != ms.end() && check(*prev(it), *it)) return false;
+        }
+    return true;
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int n;
     while (cin >> n && n) {
-        vector<Point<int>> points(2 * n);
-        for (int i = 0; i < n; i++) cin >> points[i].x >> points[i].y;
-        points[n] = points[n - 1];
-        for (int i = n - 1; ~i; i--) {
-            if (i) points[i + n] = points[i - 1];
-            if (points[i + n] < points[i]) swap(points[i + n], points[i]);
-        }
-
-        vector<int> indices(2 * n);
-        iota(indices.begin(), indices.end(), 0);
-        sort(indices.begin(), indices.end(), [&](int i, int j) { return points[i] < points[j] || points[i] == points[j] && i > j; });
-
-        auto cmp = [&](int i, int j) {
-            int cross_product = cross(points[j] - points[i], points[j] - points[j + n]);
-            return cross_product < 0 || !cross_product && (cross(points[i] - points[i + n], points[j] - points[j + n])) < 0;
-        };
-        multiset<int, decltype(cmp)> ms(cmp);
-        vector<decltype(ms)::const_iterator> its(n);
-        vector<bool> valid(n, false);
-        auto check = [&](int i, int j) {
-            int c1 = cross(points[i] - points[i + n], points[i] - points[j + n]);
-            if (c1 < 0) return false;
-
-            int c2 = cross(points[j] - points[j + n], points[j] - points[i + n]);
-            if (c2 > 0) return false;
-
-            if (c1 || c2 ? points[i] != points[j] : !(cross(points[i] - points[i + n], points[j] - points[j + n]))) return true;
-            return false;
-        };
-        for (int i : indices)
-            if (i < n) {
-                auto it = its[i] = ms.emplace(i);
-                valid[i] = true;
-                if (it != ms.begin() && check(*prev(it), i) || ++it != ms.end() && check(i, *it)) {
-                    cout << "NO\n";
-                    goto next;
-                }
-            } else if (valid[i - n]) {
-                auto it = ms.erase(its[i - n]);
-                valid[i - n] = false;
-                if (it != ms.begin() && it != ms.end() && check(*prev(it), *it)) {
-                    cout << "NO\n";
-                    goto next;
-                }
-            }
-        cout << "YES\n";
-        next:;
+        vector<Point<int>> polygon(n);
+        for (auto &[x, y] : polygon) cin >> x >> y;
+        cout << (simple(polygon) ? "YES\n" : "NO\n");
     }
 }
