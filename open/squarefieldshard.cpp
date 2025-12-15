453a3db46c951f37681fec1e64d9cbe9d35eef5e
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-12-15 23:10:27 +0800

    Update squarefieldshard.cpp

--- open/squarefieldshard.cpp
+++ open/squarefieldshard.cpp
@@ -1,22 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void backtrack(int i, int squares, int curr, int &y, vector<pair<int, int>> &points, vector<pair<pair<int, int>, pair<int, int>>> &minmax) {
-    if (squares < minmax.size() && curr <= y) {
-        if (i == points.size()) y = min(y, curr);
-        else
-            for (int j = 0; j <= squares; j++) {
-                auto original = minmax[j];
-                auto &[p1, p2] = minmax[j];
-                p1 = {min(p1.first, points[i].first), min(p1.second, points[i].second)};
-                p2 = {max(p2.first, points[i].first), max(p2.second, points[i].second)};
-
-                backtrack(i + 1, max(squares, j + 1), max(curr, max(p2.first - p1.first, p2.second - p1.second)), y, points, minmax);
-                minmax[j] = original;
-            }
-    }
-}
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -24,16 +8,30 @@ int main() {
     int t;
     cin >> t;
 
-    for (int x = 1; x <= t; x++) {
+    for (int X = 1; X <= t; X++) {
         int n, k;
         cin >> n >> k;
 
         vector<pair<int, int>> points(n);
-        for (auto &[X, Y] : points) cin >> X >> Y;
+        for (auto &[x, y] : points) cin >> x >> y;
 
         vector<pair<pair<int, int>, pair<int, int>>> minmax(k + 1, {{INT_MAX, INT_MAX}, {INT_MIN, INT_MIN}});
-        int y = INT_MAX;
-        backtrack(0, 0, 0, y, points, minmax);
-        cout << "Case #" << x << ": " << y << "\n";
+        int Y = INT_MAX;
+        auto dfs = [&](auto &&self, int i = 0, int squares = 0, int len = 0) -> void {
+            if (squares < minmax.size() && len <= Y) {
+                if (i == points.size()) Y = min(Y, len);
+                else
+                    for (int j = 0; j <= squares; j++) {
+                        auto temp = minmax[j];
+                        auto &[p1, p2] = minmax[j];
+                        p1 = {min(p1.first, points[i].first), min(p1.second, points[i].second)};
+                        p2 = {max(p2.first, points[i].first), max(p2.second, points[i].second)};
+                        self(self, i + 1, max(squares, j + 1), max(len, max(p2.first - p1.first, p2.second - p1.second)));
+                        minmax[j] = temp;
+                    }
+            }
+        };
+        dfs(dfs);
+        cout << "Case #" << X << ": " << Y << "\n";
     }
 }
