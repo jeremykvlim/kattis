b594f3860a0e4e9b73efcf8ac3909905595b0a8b
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-12-18 16:35:44 +0800

    Update xorisland.cpp

--- open/xorisland.cpp
+++ open/xorisland.cpp
@@ -10,16 +10,48 @@ int main() {
 
     vector<int> hats(n);
     for (int &a : hats) cin >> a;
+    sort(hats.begin(), hats.end());
 
-    vector<bool> dp(1 << n, false);
-    for (int i = 0; i < n; i++)
-        for (int j = i + 1; j < n; j++)
-            for (int k = j + 1; k < n; k++) dp[(1 << i) | (1 << j) | (1 << k)] = !(hats[i] ^ hats[j] ^ hats[k]);
+    vector<array<int, 3>> triples;
+    for (int a = 0; a < n; a++)
+        for (int b = a + 1; b < n; b++)
+            for (int c = b + 1; c < n; c++)
+                if ((hats[a] ^ hats[b]) == hats[c]) triples.push_back({a, b, c});
 
-    int meetings = n;
-    for (int mask = 0; mask < 1 << n; mask++)
-        if (!dp[mask]) meetings = min(meetings, n - popcount((unsigned) mask));
-        else
-            for (int i = 0; i < n; i++) dp[mask | (1 << i)] = true;
+    vector<bool> visited(n, false);
+    int meetings = n, count = 0;
+    auto dfs = [&](auto &&self, int i = 0) -> void {
+        if (i == triples.size()) {
+            meetings = min(meetings, count);
+            return;
+        }
+
+        auto [a, b, c] = triples[i];
+        if (visited[a] || visited[b] || visited[c]) {
+            self(self, i + 1);
+            return;
+        }
+
+        if (meetings == count) return;
+
+        visited[a] = true;
+        count++;
+        self(self, i + 1);
+        count--;
+        visited[a] = false;
+
+        visited[b] = true;
+        count++;
+        self(self, i + 1);
+        count--;
+        visited[b] = false;
+
+        visited[c] = true;
+        count++;
+        self(self, i + 1);
+        count--;
+        visited[c] = false;
+    };
+    dfs(dfs);
     cout << meetings;
 }
