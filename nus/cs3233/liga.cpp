60e98c7b0e010292c541ca7e304e3401edaf3a43
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-12-22 06:44:04 +0800

    Update liga.cpp

--- nus/cs3233/liga.cpp
+++ nus/cs3233/liga.cpp
@@ -1,13 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int read() {
-    string s;
-    cin >> s;
-    
-    return s == "?" ? -1 : stoi(s);
-}
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -16,6 +9,12 @@ int main() {
     cin >> n;
 
     while (n--) {
+        auto read = [&]() {
+            string s;
+            cin >> s;
+            
+            return s == "?" ? -1 : stoi(s);
+        };
         int t = read(), w = read(), d = read(), l = read(), p = read();
         if (t == -1 && l == -1) l = 0;
 
@@ -23,8 +22,9 @@ int main() {
             for (int drew = d == -1 ? 0 : d; drew <= (d == -1 ? 100 : d); drew++) {
                 int total = t == -1 ? won + drew + l : t, lost = l == -1 ? t - won - drew : l, points = p == -1 ? 3 * won + drew : p;
 
-                if (total == won + drew + lost && 0 <= total && total <= 100 && 0 <= lost && lost <= 100 && points == 3 * won + drew) 
+                if (total == won + drew + lost && total >= 0 && total <= 100 && lost >= 0 && lost <= 100 && points == 3 * won + drew) {
                     cout << total << " " << won << " " << drew << " " << lost << " " << points << "\n";
+                }
             }
     }
 }
