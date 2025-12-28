ccd8b00bbf0e1737e4c22681a64442565fb95764
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-12-28 12:36:48 +0800

    Update polygraph.cpp

--- open/polygraph.cpp
+++ open/polygraph.cpp
@@ -1,7 +1,41 @@
 #include <bits/stdc++.h>
-#include <tr2/dynamic_bitset>
 using namespace std;
-using namespace tr2;
+
+template <int S>
+struct GaloisField2Matrix {
+    int r, c;
+    vector<bitset<S>> mat;
+
+    GaloisField2Matrix(int rows, bool v = false) : r(rows), c(S), mat(rows, v ? ~bitset<S>() : bitset<S>()) {}
+    GaloisField2Matrix(const vector<bitset<S>> &mat) : r(mat.size()), c(S), mat(mat) {}
+
+    auto & operator[](int i) {
+        return mat[i];
+    }
+};
+
+template <int S>
+vector<int> rref(GaloisField2Matrix<S> &matrix) {
+    int n = matrix.r, m = matrix.c;
+
+    int rank = 0;
+    vector<int> match(m, -1);
+    for (int c = 0; c < m && rank < n; c++) {
+        int pivot = rank;
+        for (; pivot < n && !matrix[pivot][c]; pivot++);
+
+        if (pivot == n) continue;
+        swap(matrix[pivot], matrix[rank]);
+        match[c] = rank;
+
+        for (int i = 0; i < n; i++)
+            if (i != rank && matrix[i][c]) matrix[i] ^= matrix[rank];
+
+        rank++;
+    }
+
+    return match;
+}
 
 int main() {
     ios::sync_with_stdio(false);
@@ -13,46 +47,45 @@ int main() {
     for (int x = 0; x < t; x++) {
         int n, m;
         cin >> n >> m;
-        cin.ignore();
 
-        vector<vector<string>> statements(m);
-        for (auto &st : statements) {
-            string s;
-            getline(cin, s);
+        vector<bitset<501>> rows(m);
+        for (auto &row : rows) {
+            int i;
+            char c;
+            cin >> i >> c;
+            i--;
 
-            istringstream iss(s);
-            st = {istream_iterator<string>(iss), istream_iterator<string>()};
+            if (c == 'T' || c == 'L') {
+                int j;
+                cin >> j;
+                j--;
+
+                row.flip(i);
+                row.flip(j);
+                if (c == 'L') row.flip(500);
+            } else {
+                int j, k;
+                cin >> j >> k;
+                j--;
+                k--;
+
+                row.flip(i);
+                row.flip(j);
+                row.flip(k);
+                if (c == 'S') row.flip(500);
+            }
         }
 
-        vector<dynamic_bitset<>> bitmasks(m, dynamic_bitset<>(n + 1));
-        for (int i = 0; i < m; i++) {
-            bitmasks[i].flip(stoi(statements[i][0]));
-            for (int j = 2; j < statements[i].size(); j++) bitmasks[i].flip(stoi(statements[i][j]));
+        GaloisField2Matrix<501> matrix(rows);
+        auto match = rref(matrix);
 
-            if (statements[i][1] == "S" || statements[i][1] == "L") bitmasks[i].flip(0);
-        }
+        bitset<501> mask;
+        for (int c = 0; c < n; c++) mask[c] = !~match[c];
 
-        for (int i = 1; i <= n; i++) {
-            for (auto m1 : bitmasks)
-                if (m1[i]) {
-                    dynamic_bitset<> temp(n + 1);
-                    for (int j = 1; j < i; j++) temp[j] = true;
-
-                    if ((m1 & temp).none()) {
-                        for (auto &m2 : bitmasks)
-                            if (m2[i] && m2 != m1) m2 ^= m1;
-                        break;
-                    }
-                }
-        }
-
-        sort(bitmasks.begin(), bitmasks.end(), [](auto b1, auto b2) { return b1.to_string() < b2.to_string(); });
         string cities(n, '-');
-        for (auto mask : bitmasks) {
-            int msb = mask.find_first();
-            while (mask.find_next(msb) <= n) msb = mask.find_next(msb);
-
-            if (msb && dynamic_bitset<>(n + 1, 1) << (msb - 1) == mask >> 1) cities[msb - 1] = mask[0] ? 'T' : 'L';
+        for (int i = 0; i < n; i++) {
+            if (!~match[i] || (matrix[match[i]] & mask).any()) continue;
+            cities[i] = matrix[match[i]][500] ? 'T' : 'L';
         }
 
         cout << "Case #" << x + 1 << ": ";
