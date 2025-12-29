8aac2e13a5c51087aa12cfaca8a6cd894d93308c
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2025-12-29 17:34:33 +0800

    Update podnizovi.cpp

--- open/podnizovi.cpp
+++ open/podnizovi.cpp
@@ -2,25 +2,86 @@
 using namespace std;
 
 template <typename T>
-struct SparseTable {
-    vector<vector<T>> ST;
-    function<T(T, T)> f;
+struct WaveletMatrix {
+    int lg;
+    vector<int> mid;
+    vector<vector<int>> pref;
+    vector<pair<T, int>> order;
 
-    SparseTable() {}
-    SparseTable(vector<T> v, function<T(T, T)> func) : f(move(func)) {
-        if (v.empty()) return;
-        int n = __lg(v.size()) + 1;
-        ST.resize(n);
-        ST.front() = v;
-        for (int i = 1; i < n; i++) {
-            ST[i].resize(v.size() - (1 << i) + 1);
-            for (int j = 0; j <= v.size() - (1 << i); j++)
-                ST[i][j] = f(ST[i - 1][j], ST[i - 1][j + (1 << (i - 1))]);
+    WaveletMatrix(int n, vector<T> a) : lg(__lg(*max_element(a.begin(), a.end())) + 1), order(n) {
+        mid.resize(lg);
+        pref.assign(lg, vector<int>(n + 1, 0));
+
+        vector<pair<T, int>> temp(n);
+        for (int i = 0; i < n; i++) order[i] = {a[i], i};
+        for (int b = lg - 1; ~b; b--) {
+            for (int i = 0; i < n; i++) pref[b][i + 1] = pref[b][i] + ((order[i].first >> b) & 1);
+
+            int zeroes = 0, ones = mid[b] = n - pref[b][n];
+            for (int i = 0; i < n; i++) temp[((order[i].first >> b) & 1) ? ones++ : zeroes++] = order[i];
+            order.swap(temp);
         }
     }
 
-    auto size() {
-        return ST.size();
+    tuple<T, int, int, int> quantile(int l, int r, int k) {
+        T v = 0;
+        for (int b = lg - 1; ~b; b--) {
+            int zeroes = (r - pref[b][r]) - (l - pref[b][l]);
+            if (k >= zeroes) {
+                k -= zeroes;
+                v |= ((T) 1) << b;
+                l = pref[b][l] + mid[b];
+                r = pref[b][r] + mid[b];
+            } else {
+                l -= pref[b][l];
+                r -= pref[b][r];
+            }
+        }
+        return {v, k, r - l, order[l + k].second};
+    }
+
+    int freq_less(int l, int r, T x) {
+        int f = 0;
+        for (int b = lg - 1; ~b; b--) {
+            if ((x >> b) & 1) {
+                f += (r - pref[b][r]) - (l - pref[b][l]);
+                l = pref[b][l] + mid[b];
+                r = pref[b][r] + mid[b];
+            } else {
+                l -= pref[b][l];
+                r -= pref[b][r];
+            }
+        }
+        return f;
+    }
+
+    int freq(int l, int r, T x) {
+        if ((((T) 1) << lg) <= x) return 0;
+        for (int b = lg - 1; ~b; b--) {
+            if ((x >> b) & 1) {
+                l = pref[b][l] + mid[b];
+                r = pref[b][r] + mid[b];
+            } else {
+                l -= pref[b][l];
+                r -= pref[b][r];
+            }
+        }
+        return r - l;
+    }
+
+    T lower_bound(int l, int r, T x) {
+        int f = freq_less(l, r, x);
+        return f == r - l ? -1 : get<0>(quantile(l, r, f));
+    }
+
+    T predecessor(int l, int r, T x) {
+        int f = freq_less(l, r, x);
+        return !f ? -1 : get<0>(quantile(l, r, f - 1));
+    }
+
+    T successor(int l, int r, T x) {
+        int f = freq_less(l, r, x) + freq(l, r, x);
+        return f == r - l ? -1 : get<0>(quantile(l, r, f));
     }
 };
 
@@ -31,45 +92,51 @@ int main() {
     int n, k, b, m;
     cin >> n >> k >> b >> m;
 
-    vector<int> a(n + 1);
-    int biggest = 0;
-    for (int i = 1; i <= n; i++) {
-        cin >> a[i];
+    vector<int> a(n);
+    for (int &ai : a) cin >> ai;
 
-        biggest = max(biggest, a[i]);
-    }
+    auto s = a;
+    sort(s.begin(), s.end());
+    s.erase(unique(s.begin(), s.end()), s.end());
 
-    vector<int> last(biggest + 1);
-    vector<vector<int>> indices(biggest + 1);
-    for (int i = 1; i <= n; i++) {
-        last[a[i]] = i;
-        indices[a[i]].emplace_back(i);
-    }
+    vector<vector<int>> pos(s.size() + 1);
+    for (int i = 1; i <= n; i++) pos[a[i - 1] = lower_bound(s.begin(), s.end(), a[i - 1]) - s.begin() + 1].emplace_back(i);
+    WaveletMatrix<int> wm(n, a);
 
-    SparseTable<int> st(last, [](int x, int y) { return max(x, y); });
-    auto dfs = [&](auto &&self, vector<int> curr = {-1}, int v = 0) {
-        for (int j = 0; j <= biggest; j++) {
-            for (int i = st.size() - 1; ~i; i--)
-                if (j < st.ST[i].size() && st.ST[i][j] && st.ST[i][j] <= curr[0]) {
-                    j += (1 << i);
-                    if (j > biggest) return;
+    auto dfs = [&](auto &&self, int l = 0, int h = 0, int ways = 0, const vector<int> &dp1 = {}, const vector<int> &dp2 = {}) -> void {
+        if (ways) {
+            k -= ways;
+            while (ways--) cout << h << "\n";
+            if (!k) exit(0);
+        }
+        if (l >= n) return;
+
+        for (int ai = 1; ai <= s.size(); ai++) {
+            ai = wm.lower_bound(l, n, ai);
+            if (ai == -1) return;
+
+            int total = 0;
+            vector<int> temp1, temp2;
+            if (dp1.empty() && dp2.empty()) {
+                total = min(k, (int) pos[ai].size());
+                for (int i = 0; i < total; i++) {
+                    temp1.emplace_back(pos[ai][i]);
+                    temp2.emplace_back(1);
                 }
-
-            int h = ((long long) v * b % m + j) % m;
-            vector<int> next;
-            for (int i = upper_bound(indices[j].begin(), indices[j].end(), curr[0]) - indices[j].begin(); i < indices[j].size(); i++)
-                for (int l : curr) {
-                    if (l >= indices[j][i]) break;
-
-                    cout << h << "\n";
-                    if (!--k) exit(0);
-
-                    next.emplace_back(indices[j][i]);
+            } else {
+                int t = 0;
+                for (int i = upper_bound(pos[ai].begin(), pos[ai].end(), l) - pos[ai].begin(), j = 0; i < pos[ai].size(); i++) {
+                    for (; j < dp1.size() && dp1[j] < pos[ai][i]; j++) t = min(k, t + dp2[j]);
+                    if (t) {
+                        temp1.emplace_back(pos[ai][i]);
+                        temp2.emplace_back(t);
+                        total += t;
+                        if (total >= k) break;
+                    }
                 }
-
-            if (!next.empty()) self(self, next, h);
+            }
+            if (total) self(self, *lower_bound(pos[ai].begin(), pos[ai].end(), l + 1), ((long long) h * b + s[ai - 1]) % m, min(total, k), temp1, temp2);
         }
     };
-
     dfs(dfs);
 }
