9558adebef08472ea33a19f531cd87171e4aebe9
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2026-01-10 18:33:19 +0800

    Update fargaharet.cpp

--- open/fargaharet.cpp
+++ open/fargaharet.cpp
@@ -42,48 +42,40 @@ Matrix<T> I(int r, int c) {
 }
 
 struct PURQSegmentTree {
-    static inline Matrix<unsigned long long> A;
     static inline vector<int> a;
     static inline int size;
 
     struct Segment {
         Matrix<unsigned long long> M;
 
-        Segment() {}
-        Segment(const Matrix<unsigned long long> &M) : M(M) {}
+        Segment() : M(I<unsigned long long>(11, 11)) {}
 
-        auto & operator=(const int &b) {
-            int il = b * size + 1, ir = min((int) a.size() - 1, (b + 1) * size);
-
-            auto update = [&](int c = 0, unsigned long long s = 0) {
-                vector<unsigned long long> count(10, 0);
-                auto sum = count[c] = s;
-                for (int i = il; i <= ir; i++) {
-                    auto temp = count[a[i]];
-                    count[a[i]] = sum + 1;
-                    sum += count[a[i]] - temp;
+        auto & operator=(int b) {
+            *this = Segment();
+            int b_l = b * size + 1, b_r = min((int) a.size() - 1, (b + 1) * size);
+            vector<unsigned long long> sum(11, 1);
+            sum[10] = 0;
+            for (int i = b_l; i <= b_r; i++) {
+                auto &row = M.mat[a[i]];
+                for (int c = 0; c < 10; c++) {
+                    auto temp = row[c];
+                    row[c] = sum[c];
+                    sum[c] = 2 * sum[c] - temp;
                 }
-                return count;
-            };
-            auto temp = update();
-
-            for (int c = 0; c < 10; c++) A[c] = update(c, 1);
-
-            M = Matrix<unsigned long long>(11);
-            for (int r = 0; r < 10; r++) {
-                M[r][10] = temp[r];
-                for (int c = 0; c < 10; c++) M[r][c] = A[c][r] - temp[r];
+                auto temp = row[10];
+                row[10] = sum[10] + 1;
+                sum[10] = 2 * sum[10] - temp + 1;
             }
-            for (int c = 0; c < 10; c++) M[10][c] = 0;
-            M[10][10] = 1;
-
             return *this;
         }
 
-        friend auto operator+(const Segment &sl, const Segment &sr) {
-            Segment seg;
-            seg.M = sr.M * sl.M;
-            return seg;
+        auto & operator+=(const Segment &seg) {
+            M = seg.M * M;
+            return *this;
+        }
+
+        friend auto operator+(Segment sl, const Segment &sr) {
+            return sl += sr;
         }
     };
 
@@ -92,6 +84,10 @@ struct PURQSegmentTree {
 
     void pull(int i) {
         ST[i] = ST[i << 1] + ST[i << 1 | 1];
+    }
+
+    void build() {
+        for (int i = n - 1; i; i--) pull(i);
     }
 
     void point_update(int i, const int &v) {
@@ -112,11 +108,11 @@ struct PURQSegmentTree {
         return ST[i];
     }
 
-    PURQSegmentTree(int n, int s, const vector<int> &arr) : n(n), ST(2 * n, I<unsigned long long>(11, 11)) {
-        size = s;
+    PURQSegmentTree(int n, const vector<int> &arr, int s) : n(n), ST(2 * n) {
         a = arr;
-        A = Matrix<unsigned long long>(10);
-        for (int i = 0; i < n; i++) point_update(i, i);
+        size = s;
+        for (int i = 0; i < n; i++) ST[i + n] = i;
+        build();
     }
 };
 
@@ -130,8 +126,8 @@ int main() {
     vector<int> a(n + 1);
     for (int i = 1; i <= n; i++) cin >> a[i];
 
-    int size = 1 << 7;
-    PURQSegmentTree st(bit_ceil((unsigned) n / size), size, a);
+    int size = ceil(sqrt(n)), blocks = (n + size - 1) / size;
+    PURQSegmentTree st(bit_ceil((unsigned) blocks), a, size);
 
     auto plans = [&]() {
         auto sum = 1ULL;
