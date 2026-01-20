6b516270c29a8d1a7aca6e7e92f9c69e53316c03
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2026-01-21 03:03:19 +0800

    Create circlemerge.cpp

---
+++ open/circlemerge.cpp
@@ -0,0 +1,237 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T, typename U, typename V>
+T mul(U x, V y, T mod) {
+    return (unsigned __int128) x * y % mod;
+}
+
+template <typename T, typename U>
+T pow(T base, U exponent, T mod) {
+    T value = 1;
+    while (exponent) {
+        if (exponent & 1) value = mul(base, value, mod);
+        base = mul(base, base, mod);
+        exponent >>= 1;
+    }
+    return value;
+}
+
+bool isprime(unsigned long long n) {
+    if (n < 2) return false;
+    if (n == 2 || n == 5 || n == 11) return true;
+    if (n % 6 % 4 != 1) return (n | 1) == 3;
+
+    auto miller_rabin = [&](int a) {
+        int s = countr_zero(n - 1);
+        auto d = n >> s, x = pow(a % n, d, n);
+        if (x == 1 || x == n - 1) return true;
+
+        while (s--) {
+            x = mul(x, x, n);
+            if (x == n - 1) return true;
+        }
+        return false;
+    };
+    if (!miller_rabin(2) || !miller_rabin(3)) return false;
+
+    auto lucas_pseudoprime = [&]() {
+        auto normalize = [&](__int128 &x) {
+            if (x < 0) x += ((-x / n) + 1) * n;
+        };
+
+        __int128 D = -3;
+        for (;;) {
+            D += D > 0 ? 2 : -2;
+            D *= -1;
+
+            int jacobi = 1;
+            auto jacobi_symbol = [&](__int128 n) {
+                auto a = D;
+                normalize(a);
+
+                while (a) {
+                    while (!(a & 1)) {
+                        a >>= 1;
+                        if ((n & 7) == 3 || (n & 7) == 5) jacobi = -jacobi;
+                    }
+                    if ((a & 3) == 3 && (n & 3) == 3) jacobi = -jacobi;
+
+                    swap(a, n);
+                    a %= n;
+                }
+                return n == 1;
+            };
+            if (!jacobi_symbol(n)) return false;
+            if (jacobi == -1) break;
+        }
+
+        string bits;
+        auto temp = n + 1;
+        while (temp) {
+            bits += (temp & 1) ? '1' : '0';
+            temp >>= 1;
+        }
+        bits.pop_back();
+        reverse(bits.begin(), bits.end());
+
+        auto div2mod = [&](__int128 x) -> unsigned long long {
+            if (x & 1) x += n;
+            normalize(x >>= 1);
+
+            return x % n;
+        };
+
+        __int128 U = 1, V = 1;
+        for (char b : bits) {
+            auto U_2k = mul(U, V, n), V_2k = div2mod(mul(V, V, n) + D * mul(U, U, n));
+
+            if (b == '0') {
+                U = U_2k;
+                V = V_2k;
+            } else {
+                U = div2mod(U_2k + V_2k);
+                V = div2mod(D * U_2k + V_2k);
+            }
+        }
+
+        return !U;
+    };
+    return lucas_pseudoprime();
+}
+
+template <typename T>
+T brent(T n) {
+    if (!(n & 1)) return 2;
+
+    static mt19937_64 rng(random_device{}());
+    for (;;) {
+        T x = 2, y = 2, g = 1, q = 1, xs = 1, c = rng() % (n - 1) + 1;
+        for (int i = 1; g == 1; i <<= 1, y = x) {
+            for (int j = 1; j < i; j++) x = mul(x, x, n) + c;
+            for (int j = 0; j < i && g == 1; j += 128) {
+                xs = x;
+                for (int k = 0; k < min(128, i - j); k++) {
+                    x = mul(x, x, n) + c;
+                    q = mul(q, max(x, y) - min(x, y), n);
+                }
+                g = __gcd(q, n);
+            }
+        }
+
+        if (g == n) g = 1;
+        while (g == 1) {
+            xs = mul(xs, xs, n) + c;
+            g = __gcd(max(xs, y) - min(xs, y), n);
+        }
+        if (g != n) return isprime(g) ? g : brent(g);
+    }
+}
+
+template <typename T>
+vector<pair<T, int>> factorize(T n) {
+    unordered_map<T, int> pfs;
+
+    auto dfs = [&](auto &&self, T m) -> void {
+        if (m < 2) return;
+        if (isprime(m)) {
+            pfs[m]++;
+            return;
+        }
+
+        T pf = brent(m);
+        pfs[pf]++;
+        self(self, m / pf);
+    };
+    dfs(dfs, n);
+
+    return {pfs.begin(), pfs.end()};
+}
+
+template <typename T>
+vector<T> divisors(T n) {
+    auto pfs = factorize(n);
+    vector<T> divs{1};
+
+    auto dfs = [&](auto &&self, T d = 1, int i = 0) {
+        if (i == pfs.size()) return;
+
+        self(self, d, i + 1);
+        auto [pf, pow] = pfs[i];
+        while (pow--) {
+            d *= pf;
+            divs.emplace_back(d);
+            self(self, d, i + 1);
+        }
+    };
+    dfs(dfs);
+
+    return divs;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    vector<long long> pref;
+    unordered_set<long long> seen1;
+    auto a_max = 0LL, total = 0LL;
+    for (int i = 0; i < n; i++) {
+        long long ai;
+        cin >> ai;
+
+        pref.emplace_back(total);
+        seen1.emplace(total);
+        a_max = max(a_max, ai);
+        total += ai;
+    }
+    auto m = min((long long) n, total / a_max);
+
+    auto divs = divisors(total);
+    sort(divs.begin(), divs.end());
+    divs.erase(unique(divs.begin(), divs.end()), divs.end());
+    divs.erase(divs.begin() + (upper_bound(divs.begin(), divs.end(), m) - divs.begin()), divs.end());
+
+    int bound = 2e3;
+    vector<int> candidates;
+    unordered_set<long long> seen2;
+    for (int d : divs)
+        if (d <= bound) {
+            candidates.emplace_back(d);
+            seen2.emplace(d);
+        }
+
+    for (int i = max(0, (int) divs.size() - bound); i < divs.size(); i++) {
+        int d = divs[i];
+        if (d > bound) {
+            candidates.emplace_back(d);
+            seen2.emplace(d);
+        }
+    }
+
+    mt19937_64 rng(random_device{}());
+    for (int _ = 0; _ < n; _++) {
+        int i = rng() % n, j = rng() % n;
+        if (i == j) continue;
+        auto d = total / gcd(total, abs(pref[i] - pref[j]));
+        if (bound < d && d <= m && seen2.emplace(d).second) candidates.emplace_back(d);
+    }
+
+    sort(candidates.rbegin(), candidates.rend());
+    for (int d : candidates) {
+        auto q = total / d;
+        for (int i = 0; i < upper_bound(pref.begin(), pref.end(), q - 1) - pref.begin(); i++) {
+            auto p = pref[i];
+            for (int _ = 0; _ < d - 1; _++) {
+                p += q;
+                if (!seen1.count(p)) goto next;
+            }
+            cout << n - d;
+            exit(0);
+            next:;
+        }
+    }
+}
