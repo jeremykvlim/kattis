fb9fe68d45ef6da2d0ee71f49b21f77502b97d59
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2026-01-27 03:21:30 +0800

    Update collection.cpp

--- open/collection.cpp
+++ open/collection.cpp
@@ -1,239 +1,123 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct SplayTree {
-    struct SplayNode {
-        array<int, 3> family;
-        int size, key, path_min, path_max;
+struct Treap {
+    static inline mt19937_64 rng{random_device{}()};
 
-        SplayNode(int key = 0) : family{0, 0, 0}, size(1), key(key), path_min(key), path_max(key) {}
+    struct TreapNode {
+        int l, r, size;
+        unsigned long long prio;
+        int key;
+
+        TreapNode() : l(0), r(0), size(1), prio(0), key(0) {}
+
+        auto & operator=(const int &k) {
+            prio = rng();
+            key = k;
+            return *this;
+        }
     };
 
-    vector<SplayNode> ST;
+    vector<TreapNode> T;
 
-    SplayTree(int n) : ST(n + 1) {
-        iota(ST.begin(), ST.end(), 0);
+    Treap(int n) : T(n + 1) {
+        iota(T.begin(), T.end(), 0);
     }
 
-    auto & operator[](int i) {
-        return ST[i];
+    int size(int i) const {
+        return !i ? 0 : T[i].size;
     }
 
-    int size(int i) {
-        return !i ? 0 : ST[i].size;
-    }
-
-    int key(int i) {
-        return !i ? 0 : ST[i].key;
-    }
-
-    int path_min(int i) {
-        return !i ? INT_MAX : ST[i].path_min;
-    }
-
-    int path_max(int i) {
-        return !i ? INT_MIN : ST[i].path_max;
-    }
-
-    void pull(int i) {
-        if (!i) return;
-        auto [l, r, p] = ST[i].family;
-        ST[i].size = size(l) + size(r) + 1;
-        ST[i].path_min = min({path_min(l), path_min(r), key(i)});
-        ST[i].path_max = max({path_max(l), path_max(r), key(i)});
-    }
-
-    void push(int i) {
-        if (!i) return;
-    }
-
-    void splay(int i) {
-        auto root = [&](int i) {
-            auto [l, r, p] = ST[ST[i].family[2]].family;
-            return !i || l != i && r != i;
-        };
-
-        auto child = [&](int i, int parent) { return ST[parent].family[1] == i; };
-
-        auto rotate = [&](int i) {
-            int j = ST[i].family[2], k = ST[j].family[2];
-            if (!root(j)) ST[k].family[child(j, k)] = i;
-
-            int c = child(i, j), s = ST[j].family[c] = ST[i].family[c ^ 1];
-            if (s) ST[s].family[2] = j;
-
-            ST[i].family[c ^ 1] = j;
-            ST[i].family[2] = k;
-            ST[j].family[2] = i;
-            pull(j);
-        };
-
-        auto propagate = [&](auto &&self, int i) -> void {
-            if (!root(i)) self(self, ST[i].family[2]);
-            push(i);
-        };
-
-        propagate(propagate, i);
-        while (!root(i)) {
-            int j = ST[i].family[2], k = ST[j].family[2];
-            if (!root(j)) rotate(child(i, j) != child(j, k) ? i : j);
-            rotate(i);
-        }
-        pull(i);
-    }
-
-    pair<int, int> split(int i, int k) {
-        if (!i) return {0, 0};
-        for (;;) {
-            push(i);
-            auto [l, r, p] = ST[i].family;
-            int sl = size(l);
-            if (k <= sl) {
-                if (!l) {
-                    splay(i);
-                    return {0, i};
-                }
-                i = l;
-            } else if (k == sl + 1) {
-                splay(i);
-                r = ST[i].family[1];
-                ST[i].family[1] = 0;
-                pull(i);
-                if (r) ST[r].family[2] = 0;
-                return {i, r};
-            } else {
-                k -= sl + 1;
-                if (!r) {
-                    splay(i);
-                    return {i, 0};
-                }
-                i = r;
-            }
-        }
-    }
-
-    int subtree_max(int i) {
-        while (ST[i].family[1]) {
-            push(i);
-            i = ST[i].family[1];
-        }
-        push(i);
-        return i;
-    }
-
-    int meld(int i, int j) {
-        if (!i || !j) return i ^ j;
-        i = subtree_max(i);
-        splay(i);
-        ST[i].family[1] = j;
-        ST[j].family[2] = i;
-        pull(i);
-        return i;
-    }
-
-    int kth(int i, int k) {
+    int pull(int i) {
         if (!i) return 0;
-        for (;;) {
-            push(i);
-            auto [l, r, p] = ST[i].family;
-            int sl = size(l);
-            if (k <= sl) i = l;
-            else if (k == sl + 1) {
-                splay(i);
-                return i;
-            } else {
-                k -= sl + 1;
-                i = r;
-            }
+        T[i].size = size(T[i].l) + size(T[i].r) + 1;
+        return i;
+    }
+
+    pair<int, int> split(int i, const int &key) {
+        if (!i) return {0, 0};
+
+        if (T[i].key <= key) {
+            auto [l, r] = split(T[i].r, key);
+            T[i].r = l;
+            return {pull(i), r};
+        } else {
+            auto [l, r] = split(T[i].l, key);
+            T[i].l = r;
+            return {l, pull(i)};
         }
     }
 
-    pair<int, int> split_by_key(int i, int key) {
+    pair<int, int> split_by_order(int i, int k) {
         if (!i) return {0, 0};
-        int j = i, p = 0;
-        while (j) {
-            push(j);
-            if (key <= ST[j].key) {
-                p = j;
-                j = ST[j].family[0];
-            } else j = ST[j].family[1];
-        }
 
-        if (p) {
-            splay(p);
-            int l = ST[p].family[0];
-            if (l) ST[l].family[2] = 0;
-            ST[p].family[0] = 0;
-            pull(p);
-            return {l, p};
+        int sl = size(T[i].l);
+        if (k <= sl) {
+            auto [l, r] = split_by_order(T[i].l, k);
+            T[i].l = r;
+            return {l, pull(i)};
         } else {
-            i = subtree_max(i);
-            splay(i);
-            int r = ST[i].family[1];
-            if (r) ST[r].family[2] = 0;
-            ST[i].family[1] = 0;
-            pull(i);
-            return {i, 0};
+            auto [l, r] = split_by_order(T[i].r, k - sl - 1);
+            T[i].r = l;
+            return {pull(i), r};
         }
     }
 
     int meld_by_key(int i, int j) {
         if (!i || !j) return i ^ j;
-        if (path_max(i) < path_min(j)) return meld(i, j);
-        if (path_max(j) < path_min(i)) return meld(j, i);
 
-        push(i);
-        auto [l, r, p] = ST[i].family;
-        if (l) ST[l].family[2] = 0;
-        if (r) ST[r].family[2] = 0;
+        if (T[i].prio > T[j].prio) {
+            auto [jl, jr] = split(j, T[i].key);
+            T[i].l = meld_by_key(T[i].l, jl);
+            T[i].r = meld_by_key(T[i].r, jr);
+            return pull(i);
+        } else {
+            auto [il, ir] = split(i, T[j].key);
+            T[j].l = meld_by_key(T[j].l, il);
+            T[j].r = meld_by_key(T[j].r, ir);
+            return pull(j);
+        }
+    }
 
-        auto [ll, rr] = split_by_key(j, ST[i].key);
+    int find_by_order(int i, int k) const {
+        for (; i;) {
+            int sl = size(T[i].l);
+            if (k < sl) i = T[i].l;
+            else if (k == sl) return i;
+            else {
+                k -= sl + 1;
+                i = T[i].r;
+            }
+        }
+        return 0;
+    }
 
-        if (!l || !ll) ll ^= l;
-        else if (path_max(l) < path_min(ll)) ll = meld(l, ll);
-        else if (path_max(ll) < path_min(l)) ll = meld(ll, l);
-        else ll = meld_by_key(l, ll);
-
-        if (ll) ST[ll].family[2] = i;
-        ST[i].family[0] = ll;
-
-        if (!r || !rr) rr ^= r;
-        else if (path_max(r) < path_min(rr)) rr = meld(r, rr);
-        else if (path_max(rr) < path_min(r)) rr = meld(rr, r);
-        else rr = meld_by_key(r, rr);
-
-        if (rr) ST[rr].family[2] = i;
-        ST[i].family[1] = rr;
-
-        pull(i);
-        return i;
+    auto & operator[](int i) {
+        return T[i];
     }
 };
 
 int main() {
     ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    cin.tie(0);
 
     int n, q;
     cin >> n >> q;
 
     stack<int> s;
-    SplayTree st(n);
+    Treap treap(n);
     vector<int> root(n + 1);
     iota(root.begin(), root.end(), 0);
     while (q--) {
         int t, i, j;
         cin >> t >> i >> j;
 
-        if (t == 1) {
-            int k = st.kth(root[i], j);
-            cout << st[k].key << "\n";
-            root[i] = k;
-        } else if (t == 2) {
-            root[i] = st.meld_by_key(root[i], root[j]);
+        if (t == 1) cout << treap[treap.find_by_order(root[i], j - 1)].key << "\n";
+        else if (t == 2) {
+            root[i] = treap.meld_by_key(root[i], root[j]);
             s.emplace(j);
         } else {
-            auto [l, r] = st.split(root[i], j);
+            auto [l, r] = treap.split_by_order(root[i], j);
             root[i] = l;
             root[s.top()] = r;
             s.pop();
