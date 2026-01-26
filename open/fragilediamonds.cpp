d1f812f49f9bb2de8434945886dc71fcdf7267f0
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2026-01-27 02:48:53 +0800

    Update fragilediamonds.cpp

--- open/fragilediamonds.cpp
+++ open/fragilediamonds.cpp
@@ -1,198 +1,144 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct SizeBalancedTree {
-    static inline vector<long long> y;
+struct Treap {
+    static inline mt19937_64 rng{random_device{}()};
 
-    struct SBTNode {
-        int l, r, size;
+    struct TreapNode {
+        int l, r;
+        unsigned long long prio;
         pair<long long, int> key;
+        long long x_plus, x_minus, x_max_plus, x_max_minus;
 
-        long long xm, xp, xl, xr;
+        TreapNode() : l(0), r(0), prio(0), key{0, 0}, x_plus(0), x_minus(0), x_max_plus(LLONG_MIN), x_max_minus(LLONG_MIN) {}
 
-        SBTNode() : l(0), r(0), size(1), key{0, 0}, xm(LLONG_MIN), xp(LLONG_MIN), xl(LLONG_MAX), xr(LLONG_MIN) {}
-
-        auto & operator=(const pair<long long, int> &k) {
-            l = r = 0;
-            size = 1;
-            key = k;
-            auto [xi, i] = k;
-            xm = -xi + 2 * y[i];
-            xp = xi + 2 * y[i];
-            xl = xr = xi;
+        auto & operator=(const pair<pair<long long, int>, long long> &k) {
+            prio = rng();
+            key = k.first;
+            x_plus = x_max_plus = k.first.first + 2 * k.second;
+            x_minus = x_max_minus = -k.first.first + 2 * k.second;
             return *this;
         }
     };
 
     int root, nodes;
-    vector<SBTNode> SBT;
-    stack<int> recycled;
+    vector<TreapNode> T;
 
-    SizeBalancedTree(int n) : root(0), nodes(1), SBT(n + 1) {
-        y.resize(n);
+    Treap(int n) : root(0), nodes(1), T(n + 1) {}
+
+    long long max_plus(int i) const {
+        return !i ? LLONG_MIN : T[i].x_max_plus;
     }
 
-    int size(int i) const {
-        return !i ? 0 : SBT[i].size;
+    long long max_minus(int i) const {
+        return !i ? LLONG_MIN : T[i].x_max_minus;
     }
 
-    long long x_minus(int i) const {
-        return !i ? LLONG_MIN : SBT[i].xm;
-    }
-
-    long long x_plus(int i) const {
-        return !i ? LLONG_MIN : SBT[i].xp;
-    }
-
-    long long x_min(int i) const {
-        return !i ? LLONG_MAX : SBT[i].xl;
-    }
-
-    long long x_max(int i) const {
-        return !i ? LLONG_MIN : SBT[i].xr;
-    }
-
-    int node(const pair<int, int> &key) {
-        int i;
-        if (!recycled.empty()) {
-            i = recycled.top();
-            recycled.pop();
-        } else i = nodes++;
-
-        SBT[i] = key;
-        return i;
-    }
-
-    void pull(int i) {
-        int l = SBT[i].l, r = SBT[i].r;
-        SBT[i].size = size(l) + size(r) + 1;
-
-        auto [xj, j] = SBT[i].key;
-        SBT[i].xm = max({-xj + 2 * y[j], x_minus(l), x_minus(r)});
-        SBT[i].xp = max({xj + 2 * y[j], x_plus(l), x_plus(r)});
-        SBT[i].xl = min(xj, min(x_min(l), x_min(r)));
-        SBT[i].xr = max(xj, max(x_max(l), x_max(r)));
-    }
-
-    int rotate_left(int i) {
-        int j = SBT[i].r;
-        SBT[i].r = SBT[j].l;
-        SBT[j].l = i;
-        pull(i);
-        pull(j);
-        return j;
-    }
-
-    int rotate_right(int i) {
-        int j = SBT[i].l;
-        SBT[i].l = SBT[j].r;
-        SBT[j].r = i;
-        pull(i);
-        pull(j);
-        return j;
-    }
-
-    int balance_left(int i) {
+    int pull(int i) {
         if (!i) return 0;
-        int &l = SBT[i].l, &r = SBT[i].r;
-        if (!l) return i;
-
-        int ll = SBT[l].l, lr = SBT[l].r;
-        if (ll && size(ll) > size(r)) i = rotate_right(i);
-        else if (lr && size(lr) > size(r)) {
-            l = rotate_left(l);
-            i = rotate_right(i);
-        } else return i;
-
-        l = balance_left(l);
-        r = balance_right(r);
-        i = balance_left(i);
-        i = balance_right(i);
+        int l = T[i].l, r = T[i].r;
+        T[i].x_max_plus = max({T[i].x_plus, max_plus(l), max_plus(r)});
+        T[i].x_max_minus = max({T[i].x_minus, max_minus(l), max_minus(r)});
         return i;
     }
 
-    int balance_right(int i) {
-        if (!i) return 0;
-        int &l = SBT[i].l, &r = SBT[i].r;
-        if (!r) return i;
+    pair<int, int> split(int i, const pair<long long, int> &key) {
+        if (!i) return {0, 0};
 
-        int rr = SBT[r].r, rl = SBT[r].l;
-        if (rr && size(rr) > size(l)) i = rotate_left(i);
-        else if (rl && size(rl) > size(l)) {
-            r = rotate_right(r);
-            i = rotate_left(i);
-        } else return i;
-
-        l = balance_left(l);
-        r = balance_right(r);
-        i = balance_left(i);
-        i = balance_right(i);
-        return i;
-    }
-
-    int insert(const pair<long long, int> &key) {
-        return root = insert(root, key);
-    }
-
-    int insert(int i, const pair<long long, int> &key) {
-        if (!i) return node(key);
-        if (key < SBT[i].key) {
-            SBT[i].l = insert(SBT[i].l, key);
-            pull(i);
-            return balance_left(i);
+        if (T[i].key <= key) {
+            auto [l, r] = split(T[i].r, key);
+            T[i].r = l;
+            return {pull(i), r};
         } else {
-            SBT[i].r = insert(SBT[i].r, key);
-            pull(i);
-            return balance_right(i);
+            auto [l, r] = split(T[i].l, key);
+            T[i].l = r;
+            return {l, pull(i)};
         }
     }
 
-    int erase(const pair<long long, int> &key) {
-        return root = erase(root, key);
+    int meld(int i, int j) {
+        if (!i || !j) return i ^ j;
+
+        if (T[i].prio > T[j].prio) {
+            T[i].r = meld(T[i].r, j);
+            return pull(i);
+        } else {
+            T[j].l = meld(i, T[j].l);
+            return pull(j);
+        }
+    }
+
+    int insert(const pair<long long, int> &key, long long y) {
+        int i = nodes++;
+        T[i] = {key, y};
+
+        auto [l, r] = split(root, key);
+        root = meld(meld(l, i), r);
+        return i;
     }
 
     int erase(int i, const pair<long long, int> &key) {
         if (!i) return 0;
-        if (SBT[i].key == key) {
-            if (!SBT[i].l || !SBT[i].r) {
-                recycled.emplace(i);
-                return SBT[i].l ^ SBT[i].r;
-            } else {
-                int c = SBT[i].r;
-                while (SBT[c].l) c = SBT[c].l;
-                SBT[i].key = SBT[c].key;
-                SBT[i].r = erase(SBT[i].r, SBT[c].key);
-            }
-        } else if (key < SBT[i].key) SBT[i].l = erase(SBT[i].l, key);
-        else SBT[i].r = erase(SBT[i].r, key);
+        if (T[i].key == key) return meld(T[i].l, T[i].r);
 
-        pull(i);
-        return key < SBT[i].key ? balance_left(i) : balance_right(i);
+        if (T[i].key > key) T[i].l = erase(T[i].l, key);
+        else T[i].r = erase(T[i].r, key);
+
+        return pull(i);
     }
 
-    pair<int, int> query(int i, long long xl, long long xr, long long x, int sgn) {
-        if (!i || (sgn == 1 ? x_plus(i) : x_minus(i)) <= sgn * x || x_max(i) < xl || x_min(i) > xr) return {0, -1};
+    pair<int, int> erase_by_x_plus(int i, long long x) {
+        if (!i || max_plus(i) <= x) return {i, 0};
 
-        auto [xa, a] = query(SBT[i].l, xl, xr, x, sgn);
-        if (~a) return {xa, a};
-
-        auto [xj, j] = SBT[i].key;
-        if (xl <= xj && xj <= xr)
-            if (sgn * xj + 2 * y[j] > sgn * x) return {xj, j};
-
-        auto [xb, b] = query(SBT[i].r, xl, xr, x, sgn);
-        if (~b) return {xb, b};
-        return {0, -1};
+        int l = T[i].l, r = T[i].r;
+        if (l && max_plus(l) > x) {
+            auto [ll, j] = erase_by_x_plus(l, x);
+            T[i].l = ll;
+            return {pull(i), j};
+        } else if (T[i].x_plus <= x) {
+            auto [rr, j] = erase_by_x_plus(r, x);
+            T[i].r = rr;
+            return {pull(i), j};
+        }
+        return {erase(i, T[i].key), i};
     }
 
-    pair<int, int> query(long long xl, long long xr, long long x, int sgn) {
-        auto [xj, j] = query(root, xl, xr, x, sgn);
-        if (~j) erase({xj, j});
-        return {xj, j};
+    pair<int, int> erase_by_x_minus(int i, long long x) {
+        if (!i || max_minus(i) <= x) return {i, 0};
+
+        int l = T[i].l, r = T[i].r;
+        if (l && max_minus(l) > x) {
+            auto [ll, j] = erase_by_x_minus(l, x);
+            T[i].l = ll;
+            return {pull(i), j};
+        } else if (T[i].x_minus <= x) {
+            auto [rr, j] = erase_by_x_minus(r, x);
+            T[i].r = rr;
+            return {pull(i), j};
+        }
+        return {erase(i, T[i].key), i};
     }
 
-    auto & operator[](int i) {
-        return SBT[i];
+    vector<int> query(long long x, long long y, int i) {
+        vector<int> v;
+        auto [l, r] = split(root, {x - 2 * y, INT_MAX});
+        auto [rl, rr] = split(r, {x + 2 * y - 1, INT_MAX});
+        auto [rll, rlr] = split(rl, {x, INT_MAX});
+
+        while (rll && max_plus(rll) > x) {
+            auto [ll, j] = erase_by_x_plus(rll, x);
+            rll = ll;
+            v.emplace_back(T[j].key.second);
+        }
+        while (rlr && max_minus(rlr) > -x) {
+            auto [lr, j] = erase_by_x_minus(rlr, -x);
+            rlr = lr;
+            v.emplace_back(T[j].key.second);
+        }
+
+        root = meld(l, meld(rll, meld(rlr, rr)));
+        insert({x, i}, y);
+        return v;
     }
 };
 
@@ -203,25 +149,12 @@ int main() {
     int n;
     cin >> n;
 
-    SizeBalancedTree sbt(n + 1);
+    Treap treap(n);
     for (int i = 1; i <= n; i++) {
         long long x, y;
         cin >> x >> y;
 
-        sbt.y[i] = y;
-        vector<int> broken;
-        for (;;) {
-            auto [xj, j] = sbt.query(x - 2 * y + 1, x, x, 1);
-            if (!~j) break;
-            broken.emplace_back(j);
-        }
-        for (;;) {
-            auto [xj, j] = sbt.query(x + 1, x + 2 * y - 1, x, -1);
-            if (!~j) break;
-            broken.emplace_back(j);
-        }
-
-        sbt.insert({x, i});
+        auto broken = treap.query(x, y, i);
         sort(broken.begin(), broken.end());
         cout << broken.size() << " ";
         for (int j : broken) cout << j << " ";
