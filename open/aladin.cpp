de80926316e19836fa16a3edbf6d16eb038be675
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2026-01-29 01:59:20 +0800

    Update aladin.cpp

--- open/aladin.cpp
+++ open/aladin.cpp
@@ -22,161 +22,187 @@ long long floor_division_sum(long long n, int a, int c, int d) {
     return sum;
 }
 
-__int128 linear_mod_sum(int a, int n, int mod) {
-    return (__int128) a * n * (n - 1) / 2 - (__int128) mod * floor_division_sum(n, a, 0, mod);
-}
+struct ImplicitTreap {
+    static inline mt19937_64 rng{random_device{}()};
 
-struct WeightBalancedTree {
-    struct WBTNode {
-        int l, r, size, virtual_size, a, b, rem;
-        long long sum;
+    static long long progression_sum(int n, int a, int mod, int rem) {
+        int c = (a + rem) % mod;
+        return (__int128) a * n * (n - 1) / 2 + (long long) n * c - (__int128) mod * floor_division_sum(n, a, c, mod);
+    }
 
-        WBTNode(int l = 0, int r = 0) : l(l), r(r), size(1), virtual_size(-1), a(-1), b(-1), rem(-1), sum(-1) {}
+    struct TreapNode {
+        array<int, 3> family;
+        unsigned long long prio;
+        int base, len, a, mod, rem;
+        long long base_sum, sum;
 
-        auto & operator=(const array<int, 4> &arr) {
-            l = r = 0;
-            size = 1;
-            virtual_size = arr[0];
-            a = arr[1];
-            b = arr[2];
-            rem = arr[3];
-            int c = (a + rem) % b;
-            sum = linear_mod_sum(a, arr[0], b) + (long long) arr[0] * c - (floor_division_sum(arr[0], a, c, b) - floor_division_sum(arr[0], a, 0, b)) * b;
-            return *this;
-        }
+        TreapNode(int base = 0, int a = 0, int mod = 1, int rem = 0, long long base_sum = 0) : family{0, 0, 0}, prio(rng()),
+                                                                                               base(base), len(base), a(a), mod(mod), rem(rem), base_sum(base_sum), sum(base_sum) {}
     };
 
-    int root, nodes;
-    vector<WBTNode> WBT;
+    vector<TreapNode> T;
     stack<int> recycled;
+    int root;
 
-    WeightBalancedTree(int n, int m) : root(0), nodes(1), WBT(n + 1) {
-        root = node(m, 0, 1);
+    ImplicitTreap(int n) : T(1), root(0) {
+        root = node(n, 0, 1, 0, progression_sum(n, 0, 1, 0));
     }
 
-    int size(int i) const {
-        return !i ? 0 : WBT[i].size;
+    int base(int i) const {
+        return !i ? 0 : T[i].base;
     }
 
-    int virtual_size(int i) const {
-        return !i ? 0 : WBT[i].virtual_size;
+    int len(int i) const {
+        return !i ? 0 : T[i].len;
+    }
+
+    long long base_sum(int i) const {
+        return !i ? 0 : T[i].base_sum;
     }
 
     long long sum(int i) const {
-        return !i ? 0 : WBT[i].sum;
+        return !i ? 0 : T[i].sum;
     }
 
     void pull(int i) {
-        int l = WBT[i].l, r = WBT[i].r;
-
-        WBT[i].size = size(l) + size(r) + 1;
-        WBT[i].virtual_size = virtual_size(l) + virtual_size(r);
-        WBT[i].sum = sum(l) + sum(r);
+        if (!i) return;
+        auto [l, r, p] = T[i].family;
+        T[i].len = len(l) + len(r) + T[i].base;
+        T[i].sum = sum(l) + sum(r) + T[i].base_sum;
     }
 
-    int node(int l, int r) {
+    int node(int base, int a, int mod, int rem, long long base_sum) {
         int i;
         if (!recycled.empty()) {
             i = recycled.top();
             recycled.pop();
-        } else i = nodes++;
-
-        WBT[i] = WBTNode(l, r);
+        } else {
+            T.emplace_back();
+            i = T.size() - 1;
+        }
+        T[i] = {base, a, mod, rem, base_sum};
         return i;
     }
 
-    int node(int virtual_size, int a, int b, int rem = 0) {
-        int i;
-        if (!recycled.empty()) {
-            i = recycled.top();
-            recycled.pop();
-        } else i = nodes++;
-
-        WBT[i] = {virtual_size, a, b, rem};
-        return i;
-    }
-
-    int splice(int l, int r) {
-        int i = node(l, r);
+    void attach(int i, int c, int j) {
+        T[i].family[c] = j;
+        if (j) T[j].family[2] = i;
         pull(i);
-        return i;
-    }
-
-    pair<int, int> excise(int i) {
-        recycled.emplace(i);
-        return {WBT[i].l, WBT[i].r};
-    }
-
-    bool heavy(int s1, int s2) {
-        return s1 > s2 * 3;
     }
 
     int meld(int i, int j) {
-        if (!i || !j) return i ^ j;
-
-        if (heavy(size(i), size(j))) {
-            auto [l, r] = excise(i);
-            if (heavy(size(r) + size(j), size(l))) {
-                auto [rl, rr] = excise(r);
-                return meld(meld(l, rl), meld(rr, j));
-            }
-            return meld(l, meld(r, j));
+        if (!i || !j) {
+            int k = i ^ j;
+            if (k) T[k].family[2] = 0;
+            return k;
         }
-
-        if (heavy(size(j), size(i))) {
-            auto [l, r] = excise(j);
-            if (heavy(size(l) + size(i), size(r))) {
-                auto [ll, lr] = excise(l);
-                return meld(meld(i, ll), meld(lr, r));
-            }
-            return meld(meld(i, l), r);
+        if (T[i].prio < T[j].prio) {
+            attach(i, 1, meld(T[i].family[1], j));
+            T[i].family[2] = 0;
+            return i;
+        } else {
+            attach(j, 0, meld(i, T[j].family[0]));
+            T[j].family[2] = 0;
+            return j;
         }
-
-        return splice(i, j);
     }
 
-    pair<int, int> virtual_split(int i, int k) {
+    pair<int, int> split(int i, int k) {
         if (!i) return {0, 0};
-        if (!k) return {0, i};
-        if (k == virtual_size(i)) return {i, 0};
-
-        if (size(i) == 1) {
-            int j = node(WBT[i].virtual_size - k, WBT[i].a, WBT[i].b, ((long long) WBT[i].a * k + WBT[i].rem) % WBT[i].b);
-            WBT[i].virtual_size = k;
-            WBT[i].sum -= WBT[j].sum;
-            return {i, j};
+        auto [l, r, p] = T[i].family;
+        int sl = len(l);
+        if (k < sl) {
+            auto [ll, lr] = split(l, k);
+            attach(i, 0, lr);
+            if (ll) T[ll].family[2] = 0;
+            T[i].family[2] = 0;
+            return {ll, i};
+        } else if (k > sl + base(i)) {
+            auto [rl, rr] = split(r, k - sl - base(i));
+            attach(i, 1, rl);
+            if (rr) T[rr].family[2] = 0;
+            T[i].family[2] = 0;
+            return {i, rr};
+        } else if (k == sl) {
+            attach(i, 0, 0);
+            if (l) T[l].family[2] = 0;
+            T[i].family[2] = 0;
+            return {l, i};
+        } else if (k == sl + base(i)) {
+            attach(i, 1, 0);
+            if (r) T[r].family[2] = 0;
+            T[i].family[2] = 0;
+            return {i, r};
         }
 
-        auto [a, b] = excise(i);
-        if (k <= virtual_size(a)) {
-            auto [ll, rr] = virtual_split(a, k);
-            return {ll, meld(rr, b)};
+        int a = T[i].a, mod = T[i].mod, rem = T[i].rem, base_l = k - sl, base_r = T[i].base - base_l;
+        if (base_l <= base_r) {
+            auto sum = progression_sum(base_l, a, mod, rem);
+            T[i].base = base_r;
+            T[i].rem = ((long long) a * base_l + rem) % mod;
+            T[i].base_sum -= sum;
+            T[i].family[0] = 0;
+            pull(i);
+            return {insert(l, sl, node(base_l, a, mod, rem, sum)), i};
         } else {
-            auto [ll, rr] = virtual_split(b, k - virtual_size(a));
-            return {meld(a, ll), rr};
+            auto sum = progression_sum(base_r, a, mod, ((long long) a * base_l + rem) % mod);
+            T[i].base = base_l;
+            T[i].base_sum -= sum;
+            T[i].family[1] = 0;
+            pull(i);
+            return {i, insert(r, 0, node(base_r, a, mod, ((long long) a * base_l + rem) % mod, sum))};
         }
+    }
+
+    int insert(int i, int pos, int m) {
+        auto [l, r] = split(i, pos);
+        return meld(meld(l, m), r);
+    }
+
+    void erase(int l, int r) {
+        auto [rl, rr] = split(root, r);
+        auto [ll, lr] = split(rl, l - 1);
+        destroy(lr);
+        root = meld(ll, rr);
     }
 
     void destroy(int i) {
         if (!i) return;
-        destroy(WBT[i].l);
-        destroy(WBT[i].r);
+        auto [l, r, p] = T[i].family;
+        destroy(l);
+        destroy(r);
+        T[i] = {};
         recycled.emplace(i);
     }
 
-    void range_update(int l, int r, int a, int b) {
-        auto [rl, rr] = virtual_split(root, r);
-        auto [ll, lr] = virtual_split(rl, l - 1);
-        destroy(lr);
-        root = meld(meld(ll, node(r - l + 1, a, b)), rr);
+    void range_update(int l, int r, int a, int mod) {
+        erase(l, r);
+        root = insert(root, l - 1, node(r - l + 1, a, mod, 0, progression_sum(r - l + 1, a, mod, 0)));
+    }
+
+    long long pref_sum(int k) {
+        auto s = 0LL;
+        for (int i = root; i && k;) {
+            auto [l, r, p] = T[i].family;
+
+            int sl = len(l);
+            if (k <= sl) {
+                i = l;
+                continue;
+            }
+            k -= sl;
+            s += sum(l);
+
+            if (k <= base(i)) return s + progression_sum(k, T[i].a, T[i].mod, T[i].rem);
+            k -= base(i);
+            s += base_sum(i);
+            i = r;
+        }
+        return s;
     }
 
     long long range_query(int l, int r) {
-        auto [rl, rr] = virtual_split(root, r);
-        auto [ll, lr] = virtual_split(rl, l - 1);
-        auto s = sum(lr);
-        root = meld(meld(ll, lr), rr);
-        return s;
+        return pref_sum(r) - pref_sum(l - 1);
     }
 };
 
@@ -187,7 +213,7 @@ int main() {
     int n, Q;
     cin >> n >> Q;
 
-    WeightBalancedTree wbt(4 * Q + 1, n);
+    ImplicitTreap treap(n);
     while (Q--) {
         int q;
         cin >> q;
@@ -196,11 +222,11 @@ int main() {
             int l, r, a, b;
             cin >> l >> r >> a >> b;
 
-            wbt.range_update(l, r, a, b);
-        } else if (q == 2) {
+            treap.range_update(l, r, a, b);
+        } else {
             int l, r;
             cin >> l >> r;
-            cout << wbt.range_query(l, r) << "\n";
+            cout << treap.range_query(l, r) << "\n";
         }
     }
 }
