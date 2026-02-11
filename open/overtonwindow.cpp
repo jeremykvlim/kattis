c28e8a54a8729360d3a6f5912222999b71ed2553
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2026-02-11 19:52:39 +0800

    Create overtonwindow.cpp

---
+++ open/overtonwindow.cpp
@@ -0,0 +1,194 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+struct Point {
+    T x, y;
+
+    Point() {}
+    Point(T x, T y) : x(x), y(y) {}
+
+    template <typename U>
+    Point(U x, U y) : x(x), y(y) {}
+
+    template <typename U>
+    Point(const Point<U> &p) : x((T) p.x), y((T) p.y) {}
+
+    const auto begin() const {
+        return &x;
+    }
+
+    const auto end() const {
+        return &y + 1;
+    }
+
+    Point operator-() const {
+        return {-x, -y};
+    }
+
+    Point operator~() const {
+        return {-y, x};
+    }
+
+    bool operator<(const Point &p) const {
+        return x != p.x ? x < p.x : y < p.y;
+    }
+
+    bool operator>(const Point &p) const {
+        return x != p.x ? x > p.x : y > p.y;
+    }
+
+    bool operator==(const Point &p) const {
+        return x == p.x && y == p.y;
+    }
+
+    bool operator!=(const Point &p) const {
+        return x != p.x || y != p.y;
+    }
+
+    bool operator<=(const Point &p) const {
+        return *this < p || *this == p;
+    }
+
+    bool operator>=(const Point &p) const {
+        return *this > p || *this == p;
+    }
+
+    Point operator+(const Point &p) const {
+        return {x + p.x, y + p.y};
+    }
+
+    Point operator+(const T &v) const {
+        return {x + v, y + v};
+    }
+
+    Point & operator+=(const Point &p) {
+        x += p.x;
+        y += p.y;
+        return *this;
+    }
+
+    Point & operator+=(const T &v) {
+        x += v;
+        y += v;
+        return *this;
+    }
+
+    Point operator-(const Point &p) const {
+        return {x - p.x, y - p.y};
+    }
+
+    Point operator-(const T &v) const {
+        return {x - v, y - v};
+    }
+
+    Point & operator-=(const Point &p) {
+        x -= p.x;
+        y -= p.y;
+        return *this;
+    }
+
+    Point & operator-=(const T &v) {
+        x -= v;
+        y -= v;
+        return *this;
+    }
+
+    Point operator*(const T &v) const {
+        return {x * v, y * v};
+    }
+
+    Point & operator*=(const T &v) {
+        x *= v;
+        y *= v;
+        return *this;
+    }
+
+    Point operator/(const T &v) const {
+        return {x / v, y / v};
+    }
+
+    Point & operator/=(const T &v) {
+        x /= v;
+        y /= v;
+        return *this;
+    }
+};
+
+template <typename T>
+T cross(const Point<T> &a, const Point<T> &b, const Point<T> &c) {
+    return (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
+}
+
+template <typename T>
+vector<pair<Point<T>, int>> monotone_chain(vector<pair<Point<T>, int>> points, bool collinear = false) {
+    sort(points.begin(), points.end());
+    points.erase(unique(points.begin(), points.end(), [&](auto &p1, auto &p2) { return p1.first == p2.first; }), points.end());
+
+    if (points.size() < 3) return points;
+
+    vector<pair<Point<T>, int>> convex_hull;
+
+    auto clockwise = [&](auto p) {
+        T cross_product = cross(convex_hull[convex_hull.size() - 2].first, convex_hull.back().first, p.first);
+        return collinear ? cross_product <= 0 : cross_product < 0;
+    };
+
+    for (auto p : points) {
+        while (convex_hull.size() > 1 && clockwise(p)) convex_hull.pop_back();
+        convex_hull.emplace_back(p);
+    }
+
+    int s = convex_hull.size();
+    points.pop_back();
+    reverse(points.begin(), points.end());
+    for (auto p : points) {
+        while (convex_hull.size() > s && clockwise(p)) convex_hull.pop_back();
+        convex_hull.emplace_back(p);
+    }
+
+    convex_hull.pop_back();
+    return convex_hull;
+}
+
+int duval(const vector<int> &s) {
+    int n = s.size();
+
+    auto ss = s;
+    ss.insert(ss.end(), s.begin(), s.end());
+    int min_rotation_index = 0;
+    for (int i = 0; i < n;) {
+        int j = i + 1, k = i;
+        min_rotation_index = i;
+        for (; j < 2 * n && ss[j] >= ss[k]; j++) {
+            if (ss[j] > ss[k]) k = i;
+            else k++;
+        }
+        for (; i <= k && i < n; i += j - k);
+    }
+    return min_rotation_index;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    vector<pair<Point<long long>, int>> points(n);
+    for (int i = 0; i < n; i++) {
+        auto &[x, y] = points[i].first;
+        cin >> x >> y;
+
+        points[i].second = i;
+    }
+
+    auto convex_hull = monotone_chain(points, true);
+    int k = convex_hull.size();
+    cout << k << "\n";
+
+    vector<int> indices(k);
+    for (int i = 0; i < k; i++) indices[i] = convex_hull[i].second;
+    for (int i = 0, l = duval(indices); i < k; i++) cout << indices[(l + i) % k] + 1 << " ";
+}
