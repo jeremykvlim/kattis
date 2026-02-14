1392c8ef0def6bece1edfdd715406eed07286040
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2026-02-14 17:13:53 +0800

    Update asimplegame.cpp

--- open/asimplegame.cpp
+++ open/asimplegame.cpp
@@ -1,6 +1,83 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+template <typename T>
+pair<T, vector<T>> linear_program_solution(const vector<vector<T>> &A, const vector<T> &b, const vector<T> &c) {
+    int m = b.size(), n = c.size();
+
+    vector<vector<T>> tableau(m + 2, vector<T>(n + 2));
+    vector<int> basic(m), non_basic(n + 1);
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) tableau[i][j] = A[i][j];
+        basic[i] = n + i;
+        tableau[i][n] = -1;
+        tableau[i][n + 1] = b[i];
+    }
+
+    iota(non_basic.begin(), non_basic.end(), 0);
+    for (int j = 0; j < n; j++) tableau[m][j] = -c[j];
+    non_basic[n] = -1;
+    tableau[m + 1][n] = 1;
+
+    auto pivot = [&](int row, int col) {
+        T inv = 1 / tableau[row][col];
+        for (int i = 0; i < m + 2; i++)
+            if (i != row && fabs(tableau[i][col]) > 1e-8) {
+                T temp = tableau[i][col] * inv;
+                for (int j = 0; j < n + 2; j++) tableau[i][j] -= tableau[row][j] * temp;
+                tableau[i][col] = tableau[row][col] * temp;
+            }
+        for (int i = 0; i < m + 2; i++)
+            if (i != row) tableau[i][col] *= -inv;
+        for (int j = 0; j < n + 2; j++)
+            if (j != col) tableau[row][j] *= inv;
+        tableau[row][col] = inv;
+        swap(basic[row], non_basic[col]);
+    };
+
+    auto simplex = [&](int phase) -> bool {
+        for (int r = m + (phase == 1);;) {
+            int col = -1;
+            for (int j = 0; j <= n; j++) {
+                if (phase == 2 && non_basic[j] == -1) continue;
+                if (col == -1 || make_pair(tableau[r][j], non_basic[j]) < make_pair(tableau[r][col], non_basic[col])) col = j;
+            }
+            if (tableau[r][col] >= -1e-8) return true;
+
+            int row = -1;
+            for (int i = 0; i < m; i++) {
+                if (tableau[i][col] <= 1e-8) continue;
+                if (row == -1 || make_pair(tableau[i][n + 1] / tableau[i][col], basic[i]) < make_pair(tableau[row][n + 1] / tableau[row][col], basic[row])) row = i;
+            }
+            if (row == -1) return false;
+
+            pivot(row, col);
+        }
+    };
+
+    int row = 0;
+    for (int i = 1; i < m; i++)
+        if (tableau[i].back() < tableau[row].back()) row = i;
+
+    if (tableau[row][n + 1] <= -1e-8) {
+        pivot(row, n);
+        if (!simplex(1) || tableau[m + 1][n + 1] < -1e-8) return {-numeric_limits<T>::infinity(), {}};
+        for (int i = 0; i < m; i++)
+            if (basic[i] == -1) {
+                int col = -1;
+                for (int j = 0; j <= n; j++)
+                    if (col == -1 || make_pair(tableau[i][j], non_basic[j]) < make_pair(tableau[i][col], non_basic[col])) col = j;
+                pivot(i, col);
+            }
+    }
+    if (!simplex(2)) return {numeric_limits<T>::infinity(), {}};
+
+    vector<T> solution(n, 0);
+    for (int i = 0; i < m; i++)
+        if (basic[i] < n) solution[basic[i]] = tableau[i][n + 1];
+    return {tableau[m][n + 1], solution};
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -8,63 +85,43 @@ int main() {
     int T, M, g;
     cin >> T >> M >> g;
 
-    vector<vector<int>> A(T, vector<int>(M));
-    for (auto &row : A)
-        for (int &Aij : row) cin >> Aij;
+    vector<vector<int>> a(T, vector<int>(M));
+    for (auto &row : a)
+        for (int &aij : row) cin >> aij;
 
-    vector<int> tyko(T, 0), maj(M, 0);
-    tyko[0] = maj[0] = 1;
-    
-    auto fictitious_play = [&]() {
-        for (int i = 1, t = 0, m = 0; i < 250000; i++) {
-            vector<double> p_tyko(T), p_maj(M);
-            for (int r = 0; r < T; r++) p_tyko[r] = (double) tyko[r] / i;
-            for (int c = 0; c < M; c++) p_maj[c] = (double) maj[c] / i;
-
-            double s = -1e9;
-            for (int r = 0; r < T; r++) {
-                double sum = 0;
-                for (int c = 0; c < M; c++) sum += A[r][c] * p_maj[c];
-
-                if (s < sum) {
-                    s = sum;
-                    t = r;
-                }
-            }
-
-            s = 1e9;
-            for (int c = 0; c < M; c++) {
-                double sum = 0;
-                for (int r = 0; r < T; r++) sum += A[r][c] * p_tyko[r];
-
-                if (s > sum) {
-                    s = sum;
-                    m = c;
-                }
-            }
-
-            tyko[t]++;
-            maj[m]++;
-        }
-
-        vector<double> equilibrium(T);
-        for (int r = 0; r < T; r++) equilibrium[r] = (double) tyko[r] / 250000;
-        return equilibrium;
-    };
-    auto equilibrium = fictitious_play();
-
-    int remaining = 1e5;
-    vector<int> count(T, 0);
-    vector<pair<double, int>> exact(T);
-    for (int r = 0; r < T; r++) {
-        count[r] = equilibrium[r] * 1e5;
-        remaining -= count[r];
-        exact[r] = {equilibrium[r] * 1e5, r};
+    vector<vector<double>> A(M + 2, vector<double>(T + 1));
+    vector<double> b(M + 2), c(T + 1);
+    for (int j = 0; j < M; j++) {
+        for (int i = 0; i < T; i++) A[j][i] = -a[i][j];
+        A[j][T] = 1;
+        b[j] = 100;
     }
-    sort(exact.rbegin(), exact.rend());
-    exact.resize(remaining);
-    for (auto [d, r] : exact) count[r]++;
+    for (int i = 0; i < T; i++) {
+        A[M][i] = 1;
+        A[M + 1][i] = -1;
+    }
+    b[M] = c[T] = 1;
+    b[M + 1] = -1;
 
-    for (int r = 0; r < T; r++)
-        while (count[r]--) cout << r + 1 << " ";
+    auto solution = linear_program_solution(A, b, c).second;
+    solution.resize(T);
+    auto sum = accumulate(solution.begin(), solution.end(), 0.);
+
+    int n = 1e5, total = 0;
+    vector<int> e(T);
+    vector<pair<double, int>> diffs;
+    for (int i = 0; i < T; i++) {
+        auto E = (solution[i] / sum) * n;
+        total += e[i] = floor(E);
+        diffs.emplace_back(e[i] - E, i);
+    }
+    sort(diffs.begin(), diffs.end());
+    for (int i = 0; i < n - total; i++) e[diffs[i].second]++;
+
+    vector<int> moves;
+    for (int i = 0; i < T; i++)
+        for (int j = 0; j < e[i]; j++) moves.emplace_back(i);
+
+    shuffle(moves.begin(), moves.end(), mt19937(random_device{}()));
+    for (int t : moves) cout << t + 1 << " ";
 }
