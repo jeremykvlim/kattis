715caafeb4e01bb550cf1b5b844f848cb2fb3b36
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2026-02-22 07:20:09 +0800

    Update thelastlaugh.cpp

--- nus/cs3233/thelastlaugh.cpp
+++ nus/cs3233/thelastlaugh.cpp
@@ -8,9 +8,13 @@ int main() {
     array<int, 4> X;
     for (int &Xi : X) cin >> Xi;
 
+    auto lsb = [&](int x) {
+        return x & -x;
+    };
+
     auto losing = [&]() {
-        int b = (X[1] - X[0]) & (-(X[1] - X[0]));
-        return ((X[2] - X[1] - 1) % (b * 2) != b - 1 || (X[3] - X[2] - 1) % (b * 2) != b - 1);
+        int mask = lsb(X[1] - X[0]);
+        return ((X[2] - X[1] - 1) % (mask << 1) != mask - 1 || (X[3] - X[2] - 1) % (mask << 1) != mask - 1);
     };
 
     auto read = [&]() {
@@ -36,30 +40,26 @@ int main() {
 
     for (;;) {
         auto move = [&](int y, int i, int j) {
-            auto winning = [&](int y, int j) {
-                auto temp = X;
-                X[j] = y;
-                sort(X.begin(), X.end());
-                bool b = losing();
-                X = temp;
-                return !b;
-            };
+            if (X[i] <= y) return false;
 
-            if (y < X[i] && winning(y, j)) {
-                cout << "M " << X[j] << " " << y << "\n" << flush;
+            auto temp = X;
+            X[j] = y;
+            sort(X.begin(), X.end());
+            bool b = losing();
+            X = temp;
+            if (b) return false;
 
-                X[j] = y;
-                sort(X.begin(), X.end());
-                read();
-                return true;
-            }
-            return false;
+            cout << "M " << X[j] << " " << y << "\n" << flush;
+            X[j] = y;
+            sort(X.begin(), X.end());
+            read();
+            return true;
         };
 
-        if (!move(X[1] + ((X[3] - X[2]) & -(X[3] - X[2])), 2, 0) &&
-            !move(X[2] + ((X[2] - X[1]) & -(X[2] - X[1])), 3, 0) &&
-            !move(X[0] + ((X[2] - X[1]) & -(X[2] - X[1])), 1, 3) &&
-            !move(X[1] + ((X[2] - X[1]) & -(X[2] - X[1])), 2, 3)) {
+        if (!move(X[1] + lsb(X[3] - X[2]), 2, 0) &&
+            !move(X[2] + lsb(X[2] - X[1]), 3, 0) &&
+            !move(X[0] + lsb(X[2] - X[1]), 1, 3) &&
+            !move(X[1] + lsb(X[2] - X[1]), 2, 3)) {
             cout << "D\n" << flush;
             exit(0);
         }
