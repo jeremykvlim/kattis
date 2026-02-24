03a155fc2052a1b8f86ca4032b741d9b3cded400
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2026-02-24 21:59:03 +0800

    Update trezor.cpp

--- open/trezor.cpp
+++ open/trezor.cpp
@@ -1,27 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> sieve(int n) {
+vector<int> sieve(int n) {
     vector<int> spf(n + 1, 0), primes;
-    vector<vector<int>> pfs(n + 1);
     for (int i = 2; i <= n; i++) {
         if (!spf[i]) {
             spf[i] = i;
             primes.emplace_back(i);
-            pfs[i].emplace_back(i);
         }
 
         for (int p : primes) {
             auto j = (long long) i * p;
             if (j > n) break;
-
             spf[j] = p;
-            pfs[j] = pfs[i];
-            if (pfs[j].back() != p) pfs[j].emplace_back(p);
             if (p == spf[i]) break;
         }
     }
+    return spf;
+}
 
+vector<vector<int>> generate_prime_factors(int n, const vector<int> &spf) {
+    vector<vector<int>> pfs(n + 1);
+    for (int i = 2; i <= n; i++)
+        for (int j = i; j > 1;) {
+            int p = spf[j];
+            for (; !(j % p); j /= p);
+            pfs[i].emplace_back(p);
+        }
     return pfs;
 }
 
@@ -31,26 +36,28 @@ int main() {
 
     int a, b, l;
     cin >> a >> b >> l;
+    int n = a + b;
 
-    auto pfs = sieve(a + b);
+    auto pfs = generate_prime_factors(n, sieve(n));
 
     auto coprimes = [l](const vector<int> &factors) {
-        vector<array<int, 2>> p{{1, 1}};
-        for (int pf : factors) {
-            int s = p.size();
-            for (int j = 0; j < s; j++) p.push_back({p[j][0] * pf, -p[j][1]});
-        }
-
         int count = 0;
-        for (auto [d, sgn] : p) count += sgn * l / d;
+        for (int mask = 0; mask < 1 << (factors.size()); mask++) {
+            int bits = 0, d = 1;
+            for (int i = 0; i < factors.size(); i++)
+                if ((mask >> i) & 1) {
+                    bits++;
+                    d *= factors[i];
+                    if (l < d) break;
+                }
+            if (l >= d) count += (bits & 1 ? -1 : 1) * (l / d);
+        }
         return count;
     };
 
-    long long secure = 2 * coprimes(pfs[a + b]) - 2, insecure = 2 * l - secure - 2, supersecure = 2;
-    for (int x = 1, y = a + b - 1; x < a + b && y; x++, y--) {
+    long long secure = 2 * coprimes(pfs[n]) - 2, insecure = 2 * l - secure - 2, supersecure = 2;
+    for (int x = 1, y = n - 1; x < n && y; x++, y--) {
         auto fx = pfs[x], fy = pfs[y];
-        sort(fx.begin(), fx.end());
-        sort(fy.begin(), fy.end());
         int c1 = coprimes(fx), c2 = coprimes(fy);
 
         vector<int> fxy;
