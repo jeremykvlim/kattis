ecc8af0d5ba7538b27edaec09a215b81fef59b3a
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2026-02-27 00:56:27 +0800

    Update 3sideddice.cpp

--- open/3sideddice.cpp
+++ open/3sideddice.cpp
@@ -2,132 +2,80 @@
 using namespace std;
 
 template <typename T>
-struct Point {
-    T x, y;
+pair<T, vector<T>> linear_program_solution(const vector<vector<T>> &A, const vector<T> &b, const vector<T> &c) {
+    int m = b.size(), n = c.size();
 
-    Point() {}
-    Point(T x, T y) : x(x), y(y) {}
-
-    template <typename U>
-    Point(U x, U y) : x(x), y(y) {}
-
-    template <typename U>
-    Point(const Point<U> &p) : x((T) p.x), y((T) p.y) {}
-
-    const auto begin() const {
-        return &x;
+    vector<vector<T>> tableau(m + 2, vector<T>(n + 2));
+    vector<int> basic(m), non_basic(n + 1);
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) tableau[i][j] = A[i][j];
+        basic[i] = n + i;
+        tableau[i][n] = -1;
+        tableau[i][n + 1] = b[i];
     }
 
-    const auto end() const {
-        return &y + 1;
+    iota(non_basic.begin(), non_basic.end(), 0);
+    for (int j = 0; j < n; j++) tableau[m][j] = -c[j];
+    non_basic[n] = -1;
+    tableau[m + 1][n] = 1;
+
+    auto pivot = [&](int row, int col) {
+        T inv = 1 / tableau[row][col];
+        for (int i = 0; i < m + 2; i++)
+            if (i != row && fabs(tableau[i][col]) > 1e-8) {
+                T temp = tableau[i][col] * inv;
+                for (int j = 0; j < n + 2; j++) tableau[i][j] -= tableau[row][j] * temp;
+                tableau[i][col] = tableau[row][col] * temp;
+            }
+        for (int i = 0; i < m + 2; i++)
+            if (i != row) tableau[i][col] *= -inv;
+        for (int j = 0; j < n + 2; j++)
+            if (j != col) tableau[row][j] *= inv;
+        tableau[row][col] = inv;
+        swap(basic[row], non_basic[col]);
+    };
+
+    auto simplex = [&](int phase) -> bool {
+        for (int r = m + (phase == 1);;) {
+            int col = -1;
+            for (int j = 0; j <= n; j++) {
+                if (phase == 2 && non_basic[j] == -1) continue;
+                if (col == -1 || make_pair(tableau[r][j], non_basic[j]) < make_pair(tableau[r][col], non_basic[col])) col = j;
+            }
+            if (tableau[r][col] >= -1e-8) return true;
+
+            int row = -1;
+            for (int i = 0; i < m; i++) {
+                if (tableau[i][col] <= 1e-8) continue;
+                if (row == -1 || make_pair(tableau[i][n + 1] / tableau[i][col], basic[i]) < make_pair(tableau[row][n + 1] / tableau[row][col], basic[row])) row = i;
+            }
+            if (row == -1) return false;
+
+            pivot(row, col);
+        }
+    };
+
+    int row = 0;
+    for (int i = 1; i < m; i++)
+        if (tableau[i].back() < tableau[row].back()) row = i;
+
+    if (tableau[row][n + 1] <= -1e-8) {
+        pivot(row, n);
+        if (!simplex(1) || tableau[m + 1][n + 1] < -1e-8) return {-numeric_limits<T>::infinity(), {}};
+        for (int i = 0; i < m; i++)
+            if (basic[i] == -1) {
+                int col = -1;
+                for (int j = 0; j <= n; j++)
+                    if (col == -1 || make_pair(tableau[i][j], non_basic[j]) < make_pair(tableau[i][col], non_basic[col])) col = j;
+                pivot(i, col);
+            }
     }
+    if (!simplex(2)) return {numeric_limits<T>::infinity(), {}};
 
-    Point operator-() const {
-        return {-x, -y};
-    }
-
-    Point operator!() const {
-        return {y, x};
-    }
-
-    Point operator~() const {
-        return {-y, x};
-    }
-
-    bool operator<(const Point &p) const {
-        return x != p.x ? x < p.x : y < p.y;
-    }
-
-    bool operator>(const Point &p) const {
-        return x != p.x ? x > p.x : y > p.y;
-    }
-
-    bool operator==(const Point &p) const {
-        return x == p.x && y == p.y;
-    }
-
-    bool operator!=(const Point &p) const {
-        return x != p.x || y != p.y;
-    }
-
-    bool operator<=(const Point &p) const {
-        return *this < p || *this == p;
-    }
-
-    bool operator>=(const Point &p) const {
-        return *this > p || *this == p;
-    }
-
-    Point operator+(const Point &p) const {
-        return {x + p.x, y + p.y};
-    }
-
-    Point operator+(const T &v) const {
-        return {x + v, y + v};
-    }
-
-    Point & operator+=(const Point &p) {
-        x += p.x;
-        y += p.y;
-        return *this;
-    }
-
-    Point & operator+=(const T &v) {
-        x += v;
-        y += v;
-        return *this;
-    }
-
-    Point operator-(const Point &p) const {
-        return {x - p.x, y - p.y};
-    }
-
-    Point operator-(const T &v) const {
-        return {x - v, y - v};
-    }
-
-    Point & operator-=(const Point &p) {
-        x -= p.x;
-        y -= p.y;
-        return *this;
-    }
-
-    Point & operator-=(const T &v) {
-        x -= v;
-        y -= v;
-        return *this;
-    }
-
-    Point operator*(const T &v) const {
-        return {x * v, y * v};
-    }
-
-    Point & operator*=(const T &v) {
-        x *= v;
-        y *= v;
-        return *this;
-    }
-
-    Point operator/(const T &v) const {
-        return {x / v, y / v};
-    }
-
-    Point & operator/=(const T &v) {
-        x /= v;
-        y /= v;
-        return *this;
-    }
-};
-
-template <typename T>
-T cross(Point<T> a, Point<T> b) {
-    return (a.x * b.y) - (a.y * b.x);
-}
-
-template <typename T>
-T area_of_parallelogram(Point<T> a, Point<T> b, Point<T> c) {
-    Point<T> u = b - a, v = c - a;
-    return abs(cross(u, v));
+    vector<T> solution(n, 0);
+    for (int i = 0; i < m; i++)
+        if (basic[i] < n) solution[basic[i]] = tableau[i][n + 1];
+    return {tableau[m][n + 1], solution};
 }
 
 int main() {
@@ -135,25 +83,25 @@ int main() {
     cin.tie(nullptr);
 
     for (;;) {
-        array<Point<int>, 4> p;
+        array<array<double, 2>, 4> p;
         for (auto &[x, y] : p) {
             int z;
             cin >> x >> y >> z;
 
             if (!x && !y && !z) exit(0);
         }
-        auto a1 = area_of_parallelogram(p[0], p[1], p[2]),
-             a2 = area_of_parallelogram(p[0], p[1], p[3]),
-             a3 = area_of_parallelogram(p[0], p[2], p[3]),
-             a4 = area_of_parallelogram(p[1], p[2], p[3]);
 
-        if (a1 != a2 + a3 + a4) {
-            cout << "NO\n";
-            continue;
-        }
-
-        if (!a1) cout << ((min({p[0].x, p[1].x, p[2].x}) < p[3].x && p[3].x < max({p[0].x, p[1].x, p[2].x})) ||
-                          (min({p[0].y, p[1].y, p[2].y}) < p[3].y && p[3].y < max({p[0].y, p[1].y, p[2].y})) ? "YES\n" : "NO\n");
-        else cout << (a2 && a3 && a4 ? "YES\n" : "NO\n");
+        auto scale = 1e4;
+        vector<vector<double>> A{{scale, scale, scale, 0},
+                                 {-scale, -scale, -scale, 0},
+                                 {-scale, 0, 0, 1},
+                                 {0, -scale, 0, 1},
+                                 {0, 0, -scale, 1},
+                                 {p[0][0], p[1][0], p[2][0], 0},
+                                 {-p[0][0], -p[1][0], -p[2][0], 0},
+                                 {p[0][1], p[1][1], p[2][1], 0},
+                                 {-p[0][1], -p[1][1], -p[2][1], 0}};
+        auto value = linear_program_solution(A, vector<double>{scale, -scale, 0, 0, 0, p[3][0], -p[3][0], p[3][1], -p[3][1]}, vector<double>{0, 0, 0, 1}).first;
+        cout << (isfinite(value) && value > 1e-7 ? "YES\n" : "NO\n");
     }
 }
