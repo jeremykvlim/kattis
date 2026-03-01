e085eb0f7a2808e4796ef586dc0ebe299eea739f
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2026-03-01 16:42:51 +0800

    Update thepointofnoreturn.cpp

--- nus/cs3233/thepointofnoreturn.cpp
+++ nus/cs3233/thepointofnoreturn.cpp
@@ -1,7 +1,5 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
 using namespace std;
-using namespace __gnu_pbds;
 
 template <typename T>
 struct FenwickTree {
@@ -20,6 +18,16 @@ struct FenwickTree {
     T range_sum_query(int l, int r) {
         if (l >= r) return 0;
         return pref_sum(r) - pref_sum(l);
+    }
+
+    int lower_bound(T k) {
+        int i = 0;
+        for (int m = bit_ceil(BIT.size()); m; m >>= 1)
+            if (i + m < BIT.size() && BIT[i + m] < k) {
+                i += m;
+                k -= BIT[i];
+            }
+        return i + 1;
     }
 
     FenwickTree(int n) : BIT(n, 0) {}
@@ -45,20 +53,19 @@ int main() {
 
     vector<int> rank(n);
     for (int i = 0; i < n; i++) rank[i] = lower_bound(temp.begin(), temp.end(), titles[i]) - temp.begin() + 1;
-    
-    vector<int> l(n);
-    FenwickTree<int> fw(n + 1);
+
+    vector<int> r(n);
+    FenwickTree<int> fw1(n + 1);
     for (int i = n - 1; ~i; i--) {
-        l[i] = rank[i] - fw.pref_sum(rank[i]);
-        fw.update(rank[i], 1);
+        r[i] = rank[i] - fw1.pref_sum(rank[i]);
+        fw1.update(rank[i], 1);
     }
 
     vector<long long> diff(n + 1, 0), count(n + 1, 0), pref(n + 1, 0);
     for (int i = 0; i < n; i++) {
-        diff[l[i]]++;
+        diff[r[i]]++;
         diff[i + 1]--;
     }
-    
     for (int i = 1; i <= n; i++) {
         count[i] = count[i - 1] + diff[i];
         pref[i] = pref[i - 1] + count[i];
@@ -70,30 +77,25 @@ int main() {
         long long f;
         cin >> f;
 
-        if (f > pref[n]) answers[q] = -1;
-        else {
+        if (f <= pref[n]) {
             int i = lower_bound(pref.begin() + 1, pref.end(), f) - pref.begin();
             fatigues[i].emplace_back(f - pref[i - 1], q);
         }
     }
 
-    vector<pair<int, int>> times(n);
-    for (int i = 1; i <= n; i++) times[i - 1] = {l[i - 1], i};
-    sort(times.begin(), times.end());
+    vector<vector<int>> indices(n + 1);
+    for (int i = 1; i <= n; i++) indices[r[i - 1]].emplace_back(i);
 
     auto total = 0LL;
-    tree<int, null_type, less_equal<>, rb_tree_tag, tree_order_statistics_node_update> ost;
-    for (int i = 1, j = 0; i <= n; i++) {
-        while (j < times.size() && times[j].first <= i) {
-            ost.insert(times[j].second);
-            j++;
-        }
-
-        for (auto [f, q] : fatigues[i]) answers[q] = total + *ost.find_by_order(ost.order_of_key(i + 1) + f - 1) - i;
+    FenwickTree<int> pos(n + 1);
+    for (auto i : indices[0]) pos.update(i, 1);
+    for (int i = 1; i <= n; i++) {
+        for (auto j : indices[i]) pos.update(j, 1);
+        for (auto [f, q] : fatigues[i]) answers[q] = total + pos.lower_bound(pos.pref_sum(i) + f) - i;
         total += n - i;
     }
 
     for (auto a : answers)
-        if (a < 0) cout << "IMPOSSIBLE\n";
+        if (!~a) cout << "IMPOSSIBLE\n";
         else cout << a << "\n";
 }
