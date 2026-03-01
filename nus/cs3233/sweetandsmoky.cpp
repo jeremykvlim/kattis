d67e88f1f7619ae60595bee4c73614cb458a5385
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2026-03-02 02:11:19 +0800

    Update sweetandsmoky.cpp

--- nus/cs3233/sweetandsmoky.cpp
+++ nus/cs3233/sweetandsmoky.cpp
@@ -1,21 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string word(int n, int s) {
-    if (!n || (n == 1 && s != 4) || (n == 2 && s != 29)) return "IMPOSSIBLE";
-    if (n <= 2) return string(n, 'a');
-
-    int j = -1;
-    for (int i = 0; i <= n && j == -1; i++)
-        if (4 * n + 16 * i == s && !(n - i < i + 1 || n - i > 2 * i + 1)) j = i;
-    if (j == -1) return "IMPOSSIBLE";
-    
-    string word = "ab";
-    int count = n - 1 - 2 * j--;
-    while (j--) word += count-- > 0 ? "aab" : "ab";
-    return word + (count > 0 ? "aa" : "a");
-}
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -23,6 +8,32 @@ int main() {
     int n, s;
     cin >> n >> s;
 
-    auto w = word(n, s);
-    cout << (w != "IMPOSSIBLE" ? w : (w = word(n - 1, s - 25)) == "IMPOSSIBLE" ? w : "a" + w);
+    auto build = [&](int extra, int len_start, bool vowel_start) {
+        int r = s - 4 * n - extra;
+        if (r < 0 || (r & 15)) return;
+
+        int consonants = r >> 4, vowels = n - consonants,
+            blocks = consonants + vowel_start, pairs = vowels - blocks - len_start + 1;
+        if (0 <= pairs && pairs < blocks) {
+            string word(!vowel_start, 'b');
+            for (int i = 0; i < blocks; i++) {
+                int len = 1;
+                if (!i) len = len_start;
+                else if (pairs) {
+                    len = 2;
+                    pairs--;
+                }
+
+                word += string(len, 'a');
+                if (i < blocks - 1) word += 'b';
+            }
+            cout << word;
+            exit(0);
+        }
+    };
+    build(0, 1, true);
+    build(0, 2, false);
+    build(5, 1, false);
+    build(21, 2, true);
+    cout << "IMPOSSIBLE";
 }
