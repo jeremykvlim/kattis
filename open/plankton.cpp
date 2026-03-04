5fc4757af2084c13207173041354b5664e5b0a62
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2026-03-05 01:53:19 +0800

    Update plankton.cpp

--- open/plankton.cpp
+++ open/plankton.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define EPS 1e-7
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -12,25 +14,28 @@ int main() {
         int t, f, fu, fn;
         cin >> t >> f >> fu >> fn;
 
-        vector<tuple<int, int, double>> edges(t);
+        vector<tuple<int, int, double>> edges;
         vector<vector<int>> adj_list(f + 1);
-        for (int i = 0; i < t; i++) {
+        while (t--) {
             int fr, fg;
             double u;
             cin >> fr >> fg >> u;
 
-            edges[i] = {fr, fg, -u};
+            edges.emplace_back(fr, fg, -u);
             adj_list[fr].emplace_back(fg);
         }
 
         vector<double> dist(f + 1, 1e20);
         dist[fu] = 0;
-
-        auto bellman_ford = [&]() {
-            for (int i = 0; i < f; i++)
-                for (auto [u, v, w] : edges) dist[v] = min(dist[v], dist[u] + w);
-        };
-        bellman_ford();
+        for (int _ = 0; _ < f; _++) {
+            bool changed = false;
+            for (auto [u, v, w] : edges)
+                if (dist[v] > dist[u] + w) {
+                    dist[v] = dist[u] + w;
+                    changed = true;
+                }
+            if (!changed) break;
+        }
 
         vector<bool> visited(f + 1, false);
         auto dfs = [&](auto &&self, int v) -> void {
