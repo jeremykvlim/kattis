c3c488b4268cd18ec7a2873b15de8bc874bf6a8e
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2026-03-04 09:17:35 +0800

    Update greatestpair.cpp

--- open/greatestpair.cpp
+++ open/greatestpair.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> sieve(int n) {
+vector<int> sieve(int n) {
     vector<int> spf(n + 1, 0), primes;
     for (int i = 2; i <= n; i++) {
         if (!spf[i]) {
@@ -16,50 +16,33 @@ vector<vector<int>> sieve(int n) {
             if (p == spf[i]) break;
         }
     }
-
-    vector<int> exponent(n + 1, 1), base(n + 1, 1);
-    vector<vector<int>> divisors(n + 1);
-    divisors[1] = {1};
-    for (int i = 2; i <= n; i++) {
-        int p = spf[i], m = i / p;
-
-        if (spf[m] == p) {
-            exponent[i] = exponent[m] + 1;
-            base[i] = base[m];
-        } else base[i] = m;
-
-        vector<int> prime_powers(exponent[i] + 1, 1);
-        for (int k = 1; k <= exponent[i]; k++) prime_powers[k] = prime_powers[k - 1] * p;
-
-        for (int d : divisors[base[i]])
-            for (int pp : prime_powers) divisors[i].emplace_back(d * pp);
-    }
-
-    return divisors;
+    return spf;
 }
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    auto divisors = sieve(5e5);
-
-    vector<int> label(1e5 + 1), heavy(1e5 + 1);
+    vector<int> label(1e5 + 1), heavy(1e5 + 1), spf = sieve(5e5), indices(5e5 + 1);
     vector<long long> depth(1e5 + 1), dist(5e5 + 1, -1);
     vector<vector<pair<int, int>>> adj_list(1e5 + 1);
-    vector<vector<int>> bag(1e5 + 1);
-    vector<vector<long long>> bag_dist(1e5 + 1);
+    vector<vector<int>> active(1e5 + 1);
+    vector<vector<long long>> active_dists(1e5 + 1);
 
     int n;
     while (cin >> n && n) {
+        vector<int> labels;
         for (int i = 1; i <= n; i++) {
             cin >> label[i];
 
+            labels.emplace_back(label[i]);
             heavy[i] = depth[i] = 0;
             adj_list[i].clear();
-            bag[i].clear();
-            bag_dist[i].clear();
+            active[i].clear();
+            active_dists[i].clear();
         }
+        sort(labels.begin(), labels.end());
+        labels.erase(unique(labels.begin(), labels.end()), labels.end());
 
         for (int _ = 0; _ < n - 1; _++) {
             int u, v, w;
@@ -67,6 +50,26 @@ int main() {
 
             adj_list[u].emplace_back(v, w);
             adj_list[v].emplace_back(u, w);
+        }
+
+        vector<vector<int>> divs(labels.size());
+        for (int i = 0; i < labels.size(); i++) {
+            indices[labels[i]] = i;
+            vector<pair<int, int>> pfs;
+            for (int temp = labels[i]; temp > 1;) {
+                int pf = spf[temp], pow = 0;
+                for (; !(temp % pf); temp /= pf, pow++);
+                pfs.emplace_back(pf, pow);
+            }
+
+            divs[i].emplace_back(1);
+            for (auto [pf, pow] : pfs) {
+                int pp = pf, d = divs[i].size();
+                while (pow--) {
+                    for (int j = 0; j < d; j++) divs[i].emplace_back(divs[i][j] * pp);
+                    pp *= pf;
+                }
+            }
         }
 
         auto hld = [&](auto &&self, int v = 1, int prev = -1) -> int {
@@ -91,42 +94,36 @@ int main() {
                 if (u != prev && u != heavy[v]) {
                     self(self, u, v);
 
-                    for (int d : bag[u]) {
-                        bag_dist[u].emplace_back(dist[d]);
+                    for (int d : active[u]) {
+                        active_dists[u].emplace_back(dist[d]);
                         dist[d] = -1;
                     }
                 }
 
             if (heavy[v]) {
                 self(self, heavy[v], v);
-                swap(bag[v], bag[heavy[v]]);
+                swap(active[v], active[heavy[v]]);
             }
 
-            for (int d : divisors[label[v]])
-                if (dist[d] != -1) g = max(g, (dist[d] - depth[v]) * d);
-
-            for (int d : divisors[label[v]]) {
-                if (dist[d] == -1) bag[v].emplace_back(d);
+            for (int d : divs[indices[label[v]]]) {
+                if (~dist[d]) g = max(g, (dist[d] - depth[v]) * d);
+                else active[v].emplace_back(d);
                 dist[d] = max(dist[d], depth[v]);
             }
 
             for (auto [u, w] : adj_list[v])
-                if (u != prev && u != heavy[v]) {
-                    for (int i = 0; i < bag[u].size(); i++) {
-                        int d = bag[u][i];
-                        if (dist[d] != -1) g = max(g, (dist[d] + bag_dist[u][i] - 2 * depth[v]) * d);
+                if (u != prev && u != heavy[v])
+                    for (int i = 0; i < active[u].size(); i++) {
+                        int d = active[u][i];
+                        if (~dist[d]) g = max(g, (dist[d] + active_dists[u][i] - 2 * depth[v]) * d);
+                        else active[v].emplace_back(d);
+                        dist[d] = max(dist[d], active_dists[u][i]);
                     }
-
-                    for (int i = 0; i < bag[u].size(); i++) {
-                        int d = bag[u][i];
-                        if (dist[d] == -1) bag[v].emplace_back(d);
-                        dist[d] = max(dist[d], bag_dist[u][i]);
-                    }
-                }
         };
+
         dfs(dfs);
 
         cout << g << "\n";
-        for (int d : bag[1]) dist[d] = -1;
+        for (int d : active[1]) dist[d] = -1;
     }
 }
