4cae601a088fde102d95d552057ab8b1ad0dbd2c
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2026-04-08 01:56:41 +0800

    Update beetle.cpp

--- open/beetle.cpp
+++ open/beetle.cpp
@@ -13,21 +13,15 @@ int main() {
     sort(x.begin() + 1, x.end());
 
     int water = 0;
-    vector<vector<pair<int, int>>> dp(n + 1, vector<pair<int, int>>(n + 1));
+    vector<vector<array<int, 2>>> dp(n + 1, vector<array<int, 2>>(n + 1));
     for (int r = 1; r <= n; r++) {
-        for (auto &row : dp) fill(row.begin(), row.end(), make_pair(0, 0));
+        for (auto &row : dp) fill(row.begin(), row.end(), array{0, 0});
 
-        for (int i = 1; i <= n; i++) {
-            dp[i][i].first = dp[i][i].second = m - abs(x[i]) * r;
-            water = max(water, dp[i][i].first);
-        }
-
+        for (int i = 1; i <= n; i++) water = max(water, dp[i][i][0] = dp[i][i][1] = m - abs(x[i]) * r);
         for (int l = 2; l <= r; l++)
-            for (int i = 1, j = l; j <= n; i++, j++) {
-                dp[i][j].first = m + max(dp[i + 1][j].first - abs(x[i + 1] - x[i]) * (r - l + 1), dp[i + 1][j].second - abs(x[j] - x[i]) * (r - l + 1));
-                dp[i][j].second = m + max(dp[i][j - 1].second - abs(x[j] - x[j - 1]) * (r - l + 1), dp[i][j - 1].first - abs(x[j] - x[i]) * (r - l + 1));
-                water = max(water, max(dp[i][j].first, dp[i][j].second));
-            }
+            for (int i = 1, j = l; j <= n; i++, j++) water = max({water,
+                                                                  dp[i][j][0] = m + max(dp[i + 1][j][0] - abs(x[i + 1] - x[i]) * (r - l + 1), dp[i + 1][j][1] - abs(x[j] - x[i]) * (r - l + 1)),
+                                                                  dp[i][j][1] = m + max(dp[i][j - 1][1] - abs(x[j] - x[j - 1]) * (r - l + 1), dp[i][j - 1][0] - abs(x[j] - x[i]) * (r - l + 1))});
     }
     cout << water;
 }
