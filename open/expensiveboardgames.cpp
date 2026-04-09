5587632a23ae6b6fb70e610992b67393c2ff56c1
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2026-04-10 03:07:16 +0800

    Create expensiveboardgames.cpp

---
+++ open/expensiveboardgames.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+pair<T, vector<int>> jonker_volgenant(const vector<vector<T>> &C) {
+    int n = C.size(), m = C[0].size();
+
+    vector<T> dist(m), potential(m);
+    vector<int> row_match(n, -1), col_match(m, -1), cols(m), prev(m);
+    iota(cols.begin(), cols.end(), 0);
+    T d = 0;
+    for (int i = 0, c1 = -1, temp = 0; i < n; i++) {
+        for (int c = 0; c < m; c++) {
+            dist[c] = C[i][c] - potential[c];
+            prev[c] = i;
+        }
+
+        int s = 0, t = 0;
+        for (;;) {
+            if (s == t) {
+                temp = s;
+                d = dist[cols[t++]];
+                for (int j = t; j < m; j++) {
+                    c1 = cols[j];
+                    if (d < dist[c1]) continue;
+                    if (d > dist[c1]) {
+                        d = dist[c1];
+                        t = s;
+                    }
+                    cols[j] = exchange(cols[t++], c1);
+                }
+
+                for (int j = s; j < t; j++)
+                    if (!~col_match[c1 = cols[j]]) goto done;
+            }
+
+            int c2 = cols[s++], r = col_match[c2];
+            for (int j = t; j < m; j++) {
+                c1 = cols[j];
+                if (dist[c1] > C[r][c1] - C[r][c2] + potential[c2] - potential[c1] + d) {
+                    dist[c1] = C[r][c1] - C[r][c2] + potential[c2] - potential[c1] + d;
+                    prev[c1] = r;
+
+                    if (dist[c1] == d) {
+                        if (!~col_match[c1]) goto done;
+                        cols[j] = exchange(cols[t++], c1);
+                    }
+                }
+            }
+        }
+        done:;
+
+        for (int j = 0; j < temp; j++) potential[cols[j]] += dist[cols[j]] - d;
+        for (int r = -1; r != i;) {
+            r = col_match[c1] = prev[c1];
+            swap(c1, row_match[r]);
+        }
+    }
+
+    T cost = 0;
+    for (int i = 0; i < n; i++) cost += C[i][row_match[i]];
+    return {cost, row_match};
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    vector<vector<long long>> C(n, vector<long long>(n));
+    for (int i = 0; i < n; i++) {
+        int d, p;
+        cin >> d >> p;
+
+        for (int j = 0; j < n; j++) C[i][j] = p * max(0, j - d + 1);
+    }
+    cout << jonker_volgenant(C).first;
+}
