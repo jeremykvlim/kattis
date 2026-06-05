5aeac44451703d9663e405814516623e57d13166
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2026-06-06 06:08:28 +0800

    Update hopper.cpp

--- open/hopper.cpp
+++ open/hopper.cpp
@@ -11,58 +11,58 @@ int main() {
     vector<int> a(n);
     for (int &ai : a) cin >> ai;
 
-    auto update = [&](pair<int, array<int, 7>> &s, int b, int degree) {
+    auto update = [&](pair<int, array<int, 7>> &s, int pos, int degree) {
         if (!degree) {
-            s.first &= ~(1 << b);
-            s.second[b] = 0;
+            s.first &= ~(1 << pos);
+            s.second[pos] = 0;
         } else if (degree == 1) {
-            s.first &= ~(1 << b);
-            if (!s.second[b]) s.second[b] = 1;
+            s.first &= ~(1 << pos);
+            if (!s.second[pos]) s.second[pos] = 1;
         } else {
-            s.first |= 1 << b;
-            s.second[b] = 0;
+            s.first |= 1 << pos;
+            s.second[pos] = 0;
         }
     };
 
     auto encode = [&](const pair<int, array<int, 7>> &s) -> int {
         int e = 0;
-        for (int b = 0; b < D; b++) e |= (s.second[b] & 7) << (3 * b);
+        for (int pos = 0; pos < D; pos++) e |= s.second[pos] << (3 * pos);
         return e | (s.first << (3 * D));
     };
 
     int label = 1;
-    pair<int, array<int, 7>> s;
+    pair<int, array<int, 7>> s{};
     vector<int> encoded, ends(8, 0);
     vector<pair<int, array<int, 7>>> states;
-    auto dfs = [&](auto &&self, int b = 0) -> void {
-        if (b == D) {
+    auto dfs = [&](auto &&self, int pos = 0) -> void {
+        if (pos == D) {
             states.emplace_back(s);
             encoded.emplace_back(encode(s));
             return;
         }
 
-        update(s, b, 0);
-        self(self, b + 1);
-        update(s, b, 2);
-        self(self, b + 1);
-        update(s, b, 1);
+        update(s, pos, 0);
+        self(self, pos + 1);
+        update(s, pos, 2);
+        self(self, pos + 1);
+        update(s, pos, 1);
         for (int k = 1; k < label; k++)
             if (ends[k] == 1) {
-                s.second[b] = k;
+                s.second[pos] = k;
                 ends[k] = 2;
-                self(self, b + 1);
+                self(self, pos + 1);
                 ends[k] = 1;
             }
         
         if (label <= 7) {
-            s.second[b] = label;
+            s.second[pos] = label;
             ends[label] = 1;
             label++;
-            self(self, b + 1);
+            self(self, pos + 1);
             label--;
             ends[label] = 0;
         }
-        s.second[b] = 0;
+        s.second[pos] = 0;
     };
     dfs(dfs);
 
@@ -72,120 +72,146 @@ int main() {
     sort(order.begin(), order.end(), [&](int i, int j) { return encoded[i] < encoded[j]; });
     sort(encoded.begin(), encoded.end());
 
-    auto degree = [&](const pair<int, array<int, 7>> &s, int b) -> int {
-        return (s.first >> b) & 1 ? 2 : !!s.second[b];
-    };
-
-    vector<vector<array<int, 4>>> next(size);
+    int nodes = 0;
+    vector<int> single(size, 0), node_id(size, -1);
+    vector<bool> labeled(size, false);
     for (int i = 0; i < size; i++) {
-        auto add = [&](int mask = 0) {
-            int gain = 0;
-            array<int, 2> lengths;
-            for (int b = 0; b < D; b++)
-                if ((mask >> b) & 1) lengths[gain++] = b;
-
-            auto s1 = states[i];
-            if (gain == 2) {
-                auto [b1, b2] = lengths;
-                if (degree(s1, b1) == 1 && degree(s1, b2) == 1 && s1.second[b1] == s1.second[b2]) return;
+        vector<int> count(8, 0);
+        for (int pos = 0; pos < D; pos++)
+            if (!((states[i].first >> pos) & 1) && states[i].second[pos]) {
+                count[states[i].second[pos]]++;
+                labeled[i] = true;
             }
 
-            vector<int> degrees(D);
-            for (int b = 0; b < D; b++) degrees[b] = degree(s1, b);
+        for (int k = 1; k < 8; k++)
+            if (count[k] == 1) single[i]++;
 
+        if (single[i] <= 2 && (labeled[i] || !states[i].first)) node_id[i] = nodes++;
+    }
+
+    auto degree = [&](const pair<int, array<int, 7>> &s, int pos) -> int {
+        return (s.first >> pos) & 1 ? 2 : !!s.second[pos];
+    };
+
+    vector<int> degrees(7, 0);
+    vector<vector<array<int, 3>>> next(1 << D);
+    vector<vector<array<int, 2>>> last(1 << D);
+    for (int i = 0; i < size; i++) {
+        if (node_id[i] == -1) continue;
+
+        fill(degrees.begin(), degrees.end(), 0);
+        auto add = [&](int mask = 0) {
+            int gain = 0;
+            array<int, 2> offsets;
+            for (int pos = 0; pos < D; pos++)
+                if ((mask >> pos) & 1) offsets[gain++] = pos;
+            
+            auto s1 = states[i];
+            if (gain == 2) {
+                auto [l, r] = offsets;
+                if (degree(s1, l) == 1 && degree(s1, r) == 1 && s1.second[l] == s1.second[r]) return;
+            }
+            
             auto s2 = s1;
             if (gain == 1) {
-                int b1 = lengths[0];
-                if (!degrees[b1]) {
-                    update(s2, b1, 1);
+                int l = offsets[0];
+                if (!degrees[l]) {
+                    update(s2, l, 1);
                     vector<bool> used(8, false);
-                    for (int b = 0; b < D; b++)
-                        if (!((s2.first >> b) & 1) && s2.second[b]) used[s2.second[b]] = true;
-                    s2.second[b1] = find_if(used.begin() + 1, used.end(), [&](bool u) { return !u; }) - begin(used);
-                } else update(s2, b1, 2);
+                    for (int pos = 0; pos < D; pos++)
+                        if (!((s2.first >> pos) & 1) && s2.second[pos]) used[s2.second[pos]] = true;
+                    s2.second[l] = find_if(used.begin() + 1, used.end(), [&](bool u) { return !u; }) - used.begin();
+                } else update(s2, l, 2);
             } else if (gain == 2) {
-                auto [b1, b2] = lengths;
-                if (!degrees[b1] && !degrees[b2]) {
-                    update(s2, b1, 1);
-                    update(s2, b2, 1);
+                auto [l, r] = offsets;
+                if (!degrees[l] && !degrees[r]) {
+                    update(s2, l, 1);
+                    update(s2, r, 1);
                     vector<bool> used(8, false);
-                    for (int b = 0; b < D; b++)
-                        if (!((s2.first >> b) & 1) && s2.second[b]) used[s2.second[b]] = true;
-                    s2.second[b1] = s2.second[b2] = find_if(used.begin() + 1, used.end(), [&](bool u) { return !u; }) - begin(used);
-                } else if (degrees[b1] == 1 && !degrees[b2]) {
-                    update(s2, b1, 2);
-                    update(s2, b2, 1);
-                    s2.second[b2] = s1.second[b1];
-                } else if (!degrees[b1] && degrees[b2] == 1) {
-                    update(s2, b2, 2);
-                    update(s2, b1, 1);
-                    s2.second[b1] = s1.second[b2];
+                    for (int pos = 0; pos < D; pos++)
+                        if (!((s2.first >> pos) & 1) && s2.second[pos]) used[s2.second[pos]] = true;
+                    s2.second[l] = s2.second[r] = find_if(used.begin() + 1, used.end(), [&](bool u) { return !u; }) - used.begin();
+                } else if (degrees[l] == 1 && !degrees[r]) {
+                    update(s2, l, 2);
+                    update(s2, r, 1);
+                    s2.second[r] = s1.second[l];
+                } else if (!degrees[l] && degrees[r] == 1) {
+                    update(s2, r, 2);
+                    update(s2, l, 1);
+                    s2.second[l] = s1.second[r];
                 } else {
-                    update(s2, b1, 2);
-                    update(s2, b2, 2);
-                    if (s1.second[b1] != s1.second[b2] && s1.second[b2])
-                        for (int b = 0; b < D; b++)
-                            if (!((s2.first >> b) & 1) && s2.second[b] == s1.second[b2]) s2.second[b] = s1.second[b1];
+                    update(s2, l, 2);
+                    update(s2, r, 2);
+                    if (s1.second[l] != s1.second[r] && s1.second[r])
+                        for (int pos = 0; pos < D; pos++)
+                            if (!((s2.first >> pos) & 1) && s2.second[pos] == s1.second[r]) s2.second[pos] = s1.second[l];
                 }
             }
 
             pair<int, array<int, 7>> s3;
-            for (int b = D - 1; b; b--) {
-                int d = degree(s2, b - 1);
-                update(s3, b, d);
-                if (d == 1) s3.second[b] = s2.second[b - 1];
+            for (int pos = D - 1; pos; pos--) {
+                int d = degree(s2, pos - 1);
+                update(s3, pos, d);
+                if (d == 1) s3.second[pos] = s2.second[pos - 1];
             }
 
             update(s3, 0, gain);
-            if (gain == 1) s3.second[0] = !degrees[lengths[0]] ? s2.second[lengths[0]] : s1.second[lengths[0]];
+            if (gain == 1) s3.second[0] = !degrees[offsets[0]] ? s2.second[offsets[0]] : s1.second[offsets[0]];
 
             vector<int> seen(8, 0);
-            for (int b = 0, count = 1; b < D; b++)
-                if (!((s3.first >> b) & 1) && s3.second[b]) {
-                    int l = s3.second[b];
+            for (int pos = 0, count = 1; pos < D; pos++)
+                if (!((s3.first >> pos) & 1) && s3.second[pos]) {
+                    int l = s3.second[pos];
                     if (!seen[l]) seen[l] = count++;
-                    s3.second[b] = seen[l];
-                } else s3.second[b] = 0;
+                    s3.second[pos] = seen[l];
+                } else s3.second[pos] = 0;
 
-            next[i].push_back({mask, degree(s2, D - 1) == 1, order[lower_bound(encoded.begin(), encoded.end(), encode(s3)) - encoded.begin()], gain});
+            bool end = degree(s2, D - 1) == 1;
+            int j = order[lower_bound(encoded.begin(), encoded.end(), encode(s3)) - encoded.begin()];
+            if (single[i] + end == single[j] && node_id[j] != -1) next[mask].push_back({node_id[i], node_id[j], gain});
+            else if (single[i] + end == 2 && !labeled[j]) last[mask].push_back({node_id[i], gain});
         };
 
         int mask = 0;
-        for (int b = 0; b < D; b++)
-            if (degree(states[i], b) < 2) mask |= 1 << b;
-
+        for (int pos = 0; pos < D; pos++) {
+            degrees[pos] = degree(states[i], pos);
+            if (degrees[pos] < 2) mask |= 1 << pos;
+        }
         add();
 
-        for (int b1 = 0; b1 < D; b1++)
-            if ((mask >> b1) & 1) {
-                add(1 << b1);
-                for (int b2 = b1 + 1; b2 < D; b2++)
-                    if ((mask >> b2) & 1) add((1 << b1) | (1 << b2));
+        for (int l = 0; l < D; l++)
+            if ((mask >> l) & 1) {
+                add(1 << l);
+                for (int r = l + 1; r < D; r++)
+                    if ((mask >> r) & 1) add((1 << l) | (1 << r));
             }
     }
 
-    array<int, 3> unreachable{-1, -1, -1};
-    int start = order[lower_bound(encoded.begin(), encoded.end(), encode({})) - encoded.begin()];
-    vector<array<int, 3>> memo(size, array<int, 3>{-1, -1, -1}), temp(size);
-    memo[start][0] = 0;
-    auto dp = [&](int m1 = 0) {
-        for (int i = 0; i < size; i++) temp[i] = unreachable;
+    vector<vector<int>> valid(1 << D);
+    for (auto m1 = 0U; m1 < 1 << D; m1++) {
+        valid[m1].emplace_back(0);
+        for (auto m2 = m1; m2; m2 = (m2 - 1) & m1)
+            if (popcount(m2) <= 2) valid[m1].emplace_back(m2);
+    }
 
-        for (int i = 0; i < size; i++)
-            if (memo[i] != unreachable)
-                for (auto [m2, end, k, gain] : next[i])
-                    if (!(m2 & ~m1))
-                        for (int j = 0; j + end < 3; j++)
-                            if (~memo[i][j]) temp[k][j + end] = max(temp[k][j + end], memo[i][j] + gain);
+    int len = 1;
+    vector<int> memo(nodes, -3e4), temp(nodes);
+    memo[node_id[order[lower_bound(encoded.begin(), encoded.end(), encode({})) - encoded.begin()]]] = 0;
+    auto dp = [&](int m1 = 0) {
+        fill(temp.begin(), temp.end(), -3e4);
+
+        for (int m2 : valid[m1]) {
+            for (auto [u, v, gain] : next[m2]) temp[v] = max(temp[v], memo[u] + gain);
+            for (auto [v, gain] : last[m2]) len = max(len, memo[v] + gain + 1);
+        }
         memo = temp;
     };
-
     for (int i = 0; i < n; i++) {
         int mask = 0;
-        for (int b = 1; b <= min(D, i); b++)
-            if (abs(a[i] - a[i - b]) <= m) mask |= 1 << (b - 1);
+        for (int pos = 1; pos <= min(D, i); pos++)
+            if (abs(a[i] - a[i - pos]) <= m) mask |= 1 << (pos - 1);
         dp(mask);
     }
     while (D--) dp();
-    cout << memo[start][2] + 1;
+    cout << len;
 }
