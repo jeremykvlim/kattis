da472a2cb035f8258138a86df91a5bd4c9de6d23
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2026-06-07 19:11:47 +0800

    Update publiksport.cpp

--- open/publiksport.cpp
+++ open/publiksport.cpp
@@ -25,71 +25,45 @@ int main() {
         exit(0);
     }
 
-    vector<vector<int>> children(n + 1);
-    vector<int> depth(n + 1, 0), pref(n + 1, 0), heavy(n + 1, 0);
-    pref[0] = degree[0];
-    auto hld = [&](auto &&self, int v = 1, int prev = -1) -> int {
-        int subtree_size = 1, largest = 0;
+    int sum = -1e9;
+    vector<int> depth(n + 1, 0), pref(n + 1, 0);
+    pref[1] = degree[1];
+    auto dfs = [&](auto &&self, int v = 1, int prev = 0) -> unordered_map<int, int> {
+        vector<unordered_map<int, int>> maps;
         for (int u : adj_list[v])
             if (u != prev) {
-                children[v].emplace_back(u);
                 depth[u] = depth[v] + 1;
                 pref[u] = pref[v] + degree[u];
-                int size = self(self, u, v);
-                subtree_size += size;
-                if (largest < size) {
-                    largest = size;
-                    heavy[v] = u;
+                maps.emplace_back(self(self, u, v));
+            }
+        if (maps.empty()) return {{depth[v], pref[v]}};
+
+        int j = 0;
+        for (int i = 1; i < maps.size(); i++)
+            if (maps[j].size() < maps[i].size()) j = i;
+
+        auto m = move(maps[j]);
+        auto it = m.find(depth[v] + k - 1);
+        if (it != m.end()) sum = max(sum, it->second - pref[v] + degree[v]);
+
+        auto [it1, inserted1] = m.emplace(depth[v], pref[v]);
+        if (!inserted1) it1->second = max(it1->second, pref[v]);
+
+        for (int i = 0; i < maps.size(); i++)
+            if (i != j) {
+                for (auto [d, p] : maps[i]) {
+                    it = m.find(2 * depth[v] + k - 1 - d);
+                    if (it != m.end()) sum = max(sum, p + it->second - 2 * pref[v] + degree[v]);
+                }
+
+                for (auto [d, p] : maps[i]) {
+                    auto [it2, inserted2] = m.emplace(d, p);
+                    if (!inserted2) it2->second = max(it2->second, p);
                 }
             }
-        return subtree_size;
+        return m;
     };
-    hld(hld);
-
-    vector<int> tour, in(n + 1), out(n + 1);
-    int count = 0;
-    auto dfs1 = [&](auto &&self, int v = 1) -> void {
-        tour.emplace_back(v);
-        in[v] = count++;
-        for (int u : children[v]) self(self, u);
-        out[v] = count;
-    };
-    dfs1(dfs1);
-
-    int sum = -1e9;
-    queue<int> q;
-    vector<int> dp(*max_element(depth.begin(), depth.end()) + k, -1e9);
-    auto dfs2 = [&](auto &&self, int v = 1, bool undo = false) -> void {
-        for (int u : children[v])
-            if (u != heavy[v]) self(self, u, true);
-        if (heavy[v]) self(self, heavy[v], false);
-
-        dp[depth[v]] = max(dp[depth[v]], pref[v]);
-
-        int d = depth[v] + k - 1;
-        if (dp[d] != -1e9) sum = max(sum, dp[d] + degree[v] - pref[v]);
-
-        for (int u : children[v])
-            if (u != heavy[v]) {
-                int base = 2 * depth[v] + k - 1;
-                for (int i = in[u]; i < out[u]; i++) {
-                    int t = tour[i];
-                    if (base >= depth[t] && dp[base - depth[t]] != -1e9) sum = max(sum, dp[base - depth[t]] + degree[v] - 2 * pref[v] + pref[t]);
-                    q.emplace(t);
-                }
-
-                while (!q.empty()) {
-                    int t = q.front();
-                    q.pop();
-
-                    dp[depth[t]] = max(dp[depth[t]], pref[t]);
-                }
-            }
-
-        if (undo)
-            for (int i = in[v]; i < out[v]; i++) dp[depth[tour[i]]] = -1e9;
-    };
-    dfs2(dfs2);
+    dfs(dfs);
 
     if (sum == -1e9) cout << "-";
     else cout << sum - 2 * (k - 1);
