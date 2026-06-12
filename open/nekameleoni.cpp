fa528aee6bd43f1e9bf1bc552f2c5d2aacc81d6b
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2026-06-12 20:19:47 +0800

    Update nekameleoni.cpp

--- open/nekameleoni.cpp
+++ open/nekameleoni.cpp
@@ -1,6 +1,95 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct PURQSegmentTree {
+    static inline int k;
+
+    struct Monoid {
+        int len, subarray_len;
+        long long mask;
+        vector<pair<long long, int>> pref, suff;
+
+        Monoid() : len(0), subarray_len(1e9), mask(0) {}
+
+        auto & operator=(const int &v) {
+            mask = 1LL << (v - 1);
+            len = 1;
+            subarray_len = k == 1 ? 1 : 1e9;
+            pref = suff = {{mask, 1}};
+            return *this;
+        }
+
+        auto & operator+=(const Monoid &monoid) {
+            if (!len) return *this = monoid;
+            if (!monoid.len) return *this;
+
+            Monoid m;
+            m.len = len + monoid.len;
+            m.mask = mask | monoid.mask;
+            m.subarray_len = min(subarray_len, monoid.subarray_len);
+            m.pref = pref;
+            m.suff = monoid.suff;
+
+            for (auto [p_mask, p_len] : monoid.pref) {
+                auto merged = mask | p_mask;
+                if (merged != m.pref.back().first) m.pref.emplace_back(merged, len + p_len);
+            }
+
+            int i = monoid.pref.size() - 1;
+            for (auto [s_mask, s_len] : suff) {
+                auto merged = monoid.mask | s_mask;
+                if (merged != m.suff.back().first) m.suff.emplace_back(merged, monoid.len + s_len);
+                if (merged == (1LL << k) - 1) {
+                    for (; i && (s_mask | monoid.pref[i - 1].first) == (1LL << k) - 1; i--);
+                    m.subarray_len = min(m.subarray_len, s_len + monoid.pref[i].second);
+                }
+            }
+
+            return *this = m;
+        }
+
+        friend Monoid operator+(Monoid ml, const Monoid &mr) {
+            ml += mr;
+            return ml;
+        }
+    };
+
+    int n;
+    vector<Monoid> ST;
+
+    void pull(int i) {
+        ST[i] = ST[i << 1] + ST[i << 1 | 1];
+    }
+
+    void build() {
+        for (int i = n - 1; i; i--) pull(i);
+    }
+
+    void point_update(int i, const int &v) {
+        for (ST[i += n] = v; i > 1; i >>= 1) pull(i >> 1);
+    }
+
+    Monoid range_query(int l, int r) {
+        Monoid ml, mr;
+        for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
+            if (l & 1) ml = ml + ST[l++];
+            if (r & 1) mr = ST[--r] + mr;
+        }
+
+        return ml + mr;
+    }
+
+    auto & operator[](int i) {
+        return ST[i];
+    }
+
+    PURQSegmentTree(int n, int K, const vector<int> &a) : n(n), ST(2 * n) {
+        k = K;
+        for (int i = 0; i < a.size(); i++) ST[i + n] = a[i];
+        build();
+    }
+};
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -8,74 +97,10 @@ int main() {
     int n, k, m;
     cin >> n >> k >> m;
 
-    vector<int> arr(n + k + 1);
-    vector<set<int>> indices(k + 1);
-    for (int i = 1; i <= n + k; i++) {
-        if (i <= n) cin >> arr[i];
-        else arr[i] = i - n;
+    vector<int> a(n);
+    for (int &ai : a) cin >> ai;
 
-        indices[arr[i]].emplace(i);
-    }
-
-    set<array<int, 3>> intervals;
-    multiset<int> lengths;
-    auto add = [&](int l, int r, int i) {
-        intervals.insert({l, r, i});
-        if (i <= n) lengths.emplace(i - r);
-    };
-
-    auto remove = [&](int r, int i, auto it) {
-        intervals.erase(it);
-        if (i <= n) lengths.erase(lengths.lower_bound(i - r));
-    };
-
-    auto update = [&](int l1, int r1, int i) {
-        if (l1 > r1) return;
-
-        int temp = l1;
-        while (l1 <= r1) {
-            auto it = intervals.lower_bound({l1 + 1, (int) -1e9, (int) -1e9});
-            auto [l2, r2, j] = *(--it);
-            remove(r2, j, it);
-            if (l2 < l1) add(l2, l1 - 1, j);
-            if (r1 < r2) add(r1 + 1, r2, j);
-            l1 = r2 + 1;
-        }
-        l1 = temp;
-
-        auto it1 = intervals.lower_bound({l1, (int) 1e9, (int) 1e9});
-        if (it1 != intervals.begin()) {
-            auto [l2, r2, j] = *(--it1);
-            if (i == j) {
-                l1 = l2;
-                remove(r2, j, it1);
-            }
-        }
-        auto it2 = intervals.lower_bound({r1 + 1, (int) -1e9, (int) -1e9});
-        if (it2 != intervals.end()) {
-            auto [l2, r2, j] = *it2;
-            if (i == j) {
-                r1 = r2;
-                remove(r2, j, it2);
-            }
-        }
-        add(l1, r1, i);
-    };
-
-    add(1, n + k, 1e9);
-    vector<int> count(k + 1, 0);
-    for (int i = 1, r = 1, unique = 0; i <= n + k; i++) {
-        if (++count[arr[i]] == 1) unique++;
-
-        int l = r;
-        while (r <= n + k) {
-            if (unique != k) break;
-            if (!--count[arr[r]]) unique--;
-            r++;
-        }
-        if (l < r) update(l, r - 1, i);
-    }
-
+    PURQSegmentTree st(bit_ceil((unsigned) n), k, a);
     while (m--) {
         int q;
         cin >> q;
@@ -84,65 +109,7 @@ int main() {
             int p, v;
             cin >> p >> v;
 
-            if (arr[p] == v) continue;
-
-            int l1 = 1, r1 = p;
-            auto it = indices[arr[p]].lower_bound(p);
-            if (it != indices[arr[p]].begin()) l1 = *prev(it) + 1;
-            indices[arr[p]].erase(p);
-
-            int j = 1e9;
-            it = indices[arr[p]].lower_bound(p + 1);
-            if (it != indices[arr[p]].end()) j = *it;
-
-            auto pos = [&](int p) {
-                auto [l, r, i] = *prev(intervals.lower_bound({p + 1, (int) -1e9, (int) -1e9}));
-                return i;
-            };
-
-            int l = l1 - 1, r = r1 + 1, mid;
-            while (l + 1 < r) {
-                mid = l + (r - l) / 2;
-
-                if (pos(mid) <= j) l = mid;
-                else r = mid;
-            }
-            update(l1, l, j);
-
-            arr[p] = v;
-            indices[v].emplace(p);
-
-            j = 1e9;
-            it = indices[v].lower_bound(p + 1);
-            if (it != indices[v].end()) j = *it;
-
-            l = 0, r = p + 1;
-            while (l + 1 < r) {
-                mid = l + (r - l) / 2;
-
-                if (pos(mid) >= j) r = mid;
-                else l = mid;
-            }
-
-            if (r == j) continue;
-            auto [l2, r2, i] = *intervals.lower_bound({r, (int) -1e9, (int) -1e9});
-            if (i != j) continue;
-
-            l1 = l2;
-            r1 = min(p, r2);
-            vector<pair<int, int>> valid;
-            for (int i = 1, temp = l1; i <= k; i++, l1 = temp) {
-                it = indices[i].lower_bound(p);
-                if (it != indices[i].end() && *it < j) {
-                    if (it != indices[i].begin()) l1 = max(l1, *prev(it) + 1);
-                    if (l1 <= r1) valid.emplace_back(*it, l1);
-                }
-            }
-            sort(valid.begin(), valid.end());
-            for (auto [i, l1] : valid) update(l1, r1, i);
-        } else {
-            if (lengths.empty()) cout << "-1\n";
-            else cout << *lengths.begin() + 1 << "\n";
-        }
+            if (a[p - 1] != v) st.point_update(p - 1, a[p - 1] = v);
+        } else cout << (st[1].subarray_len == 1e9 ? -1 : st[1].subarray_len) << "\n";
     }
 }
