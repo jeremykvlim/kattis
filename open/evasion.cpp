f70058bf64aef484b9ff579818e64128183c67df
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2026-06-15 00:44:19 +0800

    Update evasion.cpp

--- open/evasion.cpp
+++ open/evasion.cpp
@@ -41,43 +41,19 @@ int main() {
     auto dist_s = dijkstra(s), dist_t = dijkstra(t), dist_p = dijkstra(p);
 
     auto p_to_s = dist_p[s], s_to_t = dist_s[t], p_to_t = (p_to_s == 1e18 || s_to_t == 1e18) ? (long long) 1e18 : p_to_s + s_to_t;
-    vector<bool> on_p_to_s_path(n + 1, false), on_s_to_t_path(n + 1, false);
-    for (int v = 1; v <= n; v++) {
-        on_p_to_s_path[v] = dist_p[v] < 1e18 && dist_s[v] < 1e18 && dist_p[v] + dist_s[v] == p_to_s;
-        on_s_to_t_path[v] = dist_s[v] < 1e18 && dist_t[v] < 1e18 && dist_s[v] + dist_t[v] == s_to_t;
-    }
+    auto vertex_on_path = [&](int v, auto &dist1, auto &dist2, long long total) {
+        return dist1[v] < 1e18 && dist2[v] < 1e18 && dist1[v] + dist2[v] == total;
+    };
 
-    vector<int> p_to_s_dir(m, 0), s_to_t_dir(m, 0);
-    vector<pair<long long, long long>> p_to_s_by_u(m, {-1, -1}), p_to_s_by_v(m, {-1, -1}),
-                                       s_to_t_by_u(m, {-1, -1}), s_to_t_by_v(m, {-1, -1});
-
-    for (int i = 0; i < m; i++) {
-        auto [u, v, w] = edges[i];
-
-        if (dist_p[u] < 1e18 && dist_s[v] < 1e18 && dist_p[u] + w + dist_s[v] == p_to_s) {
-            p_to_s_dir[i] |= 1;
-            p_to_s_by_u[i] = {dist_p[u], dist_p[u] + w};
-        }
-        if (dist_p[v] < 1e18 && dist_s[u] < 1e18 && dist_p[v] + w + dist_s[u] == p_to_s) {
-            p_to_s_dir[i] |= 2;
-            p_to_s_by_v[i] = {dist_p[v], dist_p[v] + w};
-        }
-
-        if (dist_s[u] < 1e18 && dist_t[v] < 1e18 && dist_s[u] + w + dist_t[v] == s_to_t) {
-            s_to_t_dir[i] |= 1;
-            s_to_t_by_u[i] = {dist_s[u] + p_to_s, dist_s[u] + p_to_s + w};
-        }
-        if (dist_s[v] < 1e18 && dist_t[u] < 1e18 && dist_s[v] + w + dist_t[u] == s_to_t) {
-            s_to_t_dir[i] |= 2;
-            s_to_t_by_v[i] = {dist_s[v] + p_to_s, dist_s[v] + p_to_s + w};
-        }
-    }
+    auto edge_on_path = [&](int u, int v, long long w, const vector<long long> &dist1, const vector<long long> &dist2, long long total) {
+        return dist1[u] < 1e18 && dist2[v] < 1e18 && dist1[u] + w + dist2[v] == total;
+    };
 
     vector<bool> valid(n + 1, false);
     for (int v = 1; v <= n; v++)
-        if (on_p_to_s_path[v])
+        if (vertex_on_path(v, dist_p, dist_s, p_to_s))
             for (auto [u, w, i] : adj_list[v])
-                if (!p_to_s_dir[i]) {
+                if (!edge_on_path(u, v, w, dist_p, dist_s, p_to_s) && !edge_on_path(v, u, w, dist_p, dist_s, p_to_s)) {
                     valid[v] = true;
                     break;
                 }
@@ -85,7 +61,7 @@ int main() {
     vector<array<long long, 2>> dist(n + 1, {(long long) 1e18, (long long) 1e18});
     dist[s][0] = 0;
     priority_queue<tuple<long long, int, bool>, vector<tuple<long long, int, bool>>, greater<>> pq;
-    pq.emplace(0LL, s, 0);
+    pq.emplace(0, s, false);
     auto time = -1LL;
     while (!pq.empty()) {
         auto [d, v, hidden] = pq.top();
@@ -104,68 +80,22 @@ int main() {
                 pq.emplace(max(d, dist_p[v]), v, 1);
             }
 
-        for (auto [u, w, i] : adj_list[v]) {
-            int dir = (u == edges[i][0] && v == edges[i][1]) ? 2 : 1;
-            if (!hidden) {
-                if (p_to_s_dir[i]) {
-                    bool caught = false;
-                    if (p_to_s_dir[i] & 1) {
-                        auto [l, r] = p_to_s_by_u[i];
-                        if (dir == 1) {
-                            if (d <= l && l <= d + w) caught = true;
-                        } else {
-                            if (l - 2 * w < d && d < r) caught = true;
-                        }
-                    }
-                    if (caught) continue;
+        auto caught = [&](int u, long long w) {
+            auto &dist1 = hidden ? dist_s : dist_p, &dist2 = hidden ? dist_t : dist_s;
+            auto total = hidden ? s_to_t : p_to_s, offset = hidden ? p_to_s : 0;
+            if (total == 1e18 || offset == 1e18) return false;
 
-                    if (p_to_s_dir[i] & 2) {
-                        auto [l, r] = p_to_s_by_v[i];
-                        if (dir == 2) {
-                            if (d <= l && l <= d + w) caught = true;
-                        } else {
-                            if (l - 2 * w < d && d < r) caught = true;
-                        }
-                    }
-                    if (caught) continue;
-                }
+            if (edge_on_path(v, u, w, dist1, dist2, total) && d <= offset + dist1[v] && offset + dist1[v] <= d + w) return true;
+            if (edge_on_path(u, v, w, dist1, dist2, total) && offset + dist1[u] - 2 * w < d && d < offset + dist1[u] + w) return true;
+            return vertex_on_path(u, dist1, dist2, total) && d + 2 * w == offset + dist1[u];
+        };
 
-                if (on_p_to_s_path[u] && d + 2 * w == dist_p[u]) continue;
-                if (dist[u][0] > d + 2 * w) {
-                    dist[u][0] = d + 2 * w;
-                    pq.emplace(d + 2 * w, u, 0);
+        for (auto [u, w, i] : adj_list[v])
+            if (!caught(u, w))
+                if (dist[u][hidden] > d + 2 * w) {
+                    dist[u][hidden] = d + 2 * w;
+                    pq.emplace(d + 2 * w, u, hidden);
                 }
-            } else {
-                if (s_to_t_dir[i]) {
-                    bool caught = false;
-                    if (s_to_t_dir[i] & 1) {
-                        auto [l, r] = s_to_t_by_u[i];
-                        if (dir == 1) {
-                            if (d <= l && l <= d + w) caught = true;
-                        } else {
-                            if (l - 2 * w < d && d < r) caught = true;
-                        }
-                    }
-                    if (caught) continue;
-
-                    if (s_to_t_dir[i] & 2) {
-                        auto [l, r] = s_to_t_by_v[i];
-                        if (dir == 2) {
-                            if (d <= l && l <= d + w) caught = true;
-                        } else {
-                            if (l - 2 * w < d && d < r) caught = true;
-                        }
-                    }
-                    if (caught) continue;
-                }
-
-                if (on_s_to_t_path[u] && d + 2 * w == dist_s[u] + p_to_s) continue;
-                if (dist[u][1] > d + 2 * w) {
-                    dist[u][1] = d + 2 * w;
-                    pq.emplace(d + 2 * w, u, 1);
-                }
-            }
-        }
     }
 
     if (!~time) {
