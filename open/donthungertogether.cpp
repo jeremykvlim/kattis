ada04cab2c86c886e4207eb5063ec98a4d145cd7
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2026-06-24 17:50:28 +0800

    Update donthungertogether.cpp

--- open/donthungertogether.cpp
+++ open/donthungertogether.cpp
@@ -1,31 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool survive(vector<pair<int, double>> food, double total) {
-    priority_queue<pair<int, double>, vector<pair<int, double>>, greater<>> pq;
-    for (int i = 0; i < food.size(); i++) {
-        pq.emplace(food[i]);
-        auto need = total;
-        while (!pq.empty()) {
-            auto [f, q] = pq.top();
-            pq.pop();
-
-            if (f < i + 1) continue;
-
-            auto use = min(need, q);
-            need -= use;
-            q -= use;
-
-            if (q > 0) pq.emplace(f, q);
-            if (need <= 0) break;
-        }
-
-        if (need > 0) return false;
-    }
-
-    return true;
-}
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -40,7 +15,32 @@ int main() {
     while (l + 1e-9 < r && l + l * 1e-9 < r) {
         m = l + (r - l) / 2;
 
-        if (!survive(food, m)) r = m;
+        auto survive = [&]() {
+            priority_queue<pair<int, double>, vector<pair<int, double>>, greater<>> pq;
+            for (int i = 0; i < n; i++) {
+                pq.emplace(food[i]);
+                auto need = m;
+                while (!pq.empty()) {
+                    auto [f, q] = pq.top();
+                    pq.pop();
+
+                    if (f < i + 1) continue;
+
+                    auto use = min(need, q);
+                    need -= use;
+                    q -= use;
+
+                    if (q > 0) pq.emplace(f, q);
+                    if (need <= 0) break;
+                }
+
+                if (need > 0) return false;
+            }
+
+            return true;
+        };
+
+        if (!survive()) r = m;
         else l = m;
     }
 
