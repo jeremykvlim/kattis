76b18708ec252b66d9717062249d74eec2187f10
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2026-08-22 23:08:58 +0800

    Update fargaharet.cpp

--- open/fargaharet.cpp
+++ open/fargaharet.cpp
@@ -21,8 +21,14 @@ struct Matrix {
         return C;
     }
 
-    friend auto operator*=(Matrix<T> &A, const Matrix<T> &B) {
-        return A = A * B;
+    friend auto operator*(const Matrix<T> &A, const vector<T> &v) {
+        int n = A.r, m = v.size();
+
+        vector<T> u(n, 0);
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++) u[i] += A[i][j] * v[j];
+
+        return u;
     }
 
     auto & operator[](int i) {
@@ -31,6 +37,15 @@ struct Matrix {
 
     auto & operator[](int i) const {
         return mat[i];
+    }
+
+    Matrix<T> transpose() const {
+        Matrix<T> mat_T(c, r);
+
+        for (int i = 0; i < r; i++)
+            for (int j = 0; j < c; j++) mat_T[j][i] = mat[i][j];
+
+        return mat_T;
     }
 };
 
@@ -43,43 +58,99 @@ Matrix<T> I(int r, int c) {
 
 struct PURQSegmentTree {
     static inline vector<int> a;
-    static inline int size;
+    static inline int n, size;
 
     struct Monoid {
+        static inline int p;
+        static inline array<vector<unsigned long long>, 2> U, V;
+
         Matrix<unsigned long long> M;
-
         Monoid() : M(I<unsigned long long>(11, 11)) {}
+        Monoid(Monoid &&) = default;
 
-        auto & operator=(int b) {
-            *this = Monoid();
-            int b_l = b * size + 1, b_r = min((int) a.size() - 1, (b + 1) * size);
-            vector<unsigned long long> sum(11, 1);
-            sum[10] = 0;
-            for (int i = b_l; i <= b_r; i++) {
-                auto &row = M.mat[a[i]];
-                for (int c = 0; c < 10; c++) {
-                    auto temp = row[c];
-                    row[c] = sum[c];
-                    sum[c] = 2 * sum[c] - temp;
-                }
-                auto temp = row[10];
-                row[10] = sum[10] + 1;
-                sum[10] = 2 * sum[10] - temp + 1;
+        Monoid(const Monoid &monoid) {
+            if (p <= 1) M = monoid.M;
+            else if (p & 1) {
+                auto M_T = monoid.M.transpose();
+                V[0] = M_T * V[0];
+                V[1] = M_T * V[1];
             }
+        }
+
+        auto & operator=(const Monoid &monoid) {
+            if (p > 1) {
+                p >>= 1;
+                for (int i = 0; i <= 10; i++)
+                    for (int j = 0; j <= 10; j++) M[i][j] += U[0][i] * V[0][j] - U[1][i] * V[1][j];
+            } else M = monoid.M;
+
+            return *this;
+        }
+
+        auto & operator=(int v) {
+            if (p < 0) {
+                for (int i = v * size + 1; i <= min((int) a.size() - 1, (v + 1) * size); i++) {
+                    int r = a[i];
+                    for (int c = 0; c <= 10; c++) {
+                        auto temp = exchange(M[r][c], M[10][c]);
+                        M[10][c] = 2 * M[10][c] - temp;
+                    }
+                }
+
+                return *this;
+            }
+
+            int k = v / 10, b = (k - 1) / size;
+            for (int i = 0; i < 2; i++) {
+                fill(U[i].begin(), U[i].end(), 0);
+                fill(V[i].begin(), V[i].end(), 0);
+            }
+            U[0][v % 10] = U[0][10] = U[1][a[k]] = U[1][10] = V[0][10] = V[1][10] = 1;
+            V[0][v % 10] = V[1][a[k]] = -1;
+
+            for (int i = k + 1; i <= min((int) a.size() - 1, (b + 1) * size); i++) {
+                int c = a[i];
+                for (int r = 0; r < 2; r++) {
+                    auto temp = exchange(U[r][c], U[r][10]);
+                    U[r][10] = 2 * U[r][10] - temp;
+                }
+            }
+
+            for (int i = k - 1; i > b * size; i--) {
+                int c = a[i];
+                for (int r = 0; r < 2; r++) {
+                    auto temp = V[r][c];
+                    V[r][c] = -exchange(V[r][10], temp + 2 * V[r][10]);
+                }
+            }
+
+            for (int i = 0; i <= 10; i++)
+                for (int j = 0; j <= 10; j++) M[i][j] += U[0][i] * V[0][j] - U[1][i] * V[1][j];
+
+            a[k] = v % 10;
+            p = b + n;
             return *this;
         }
 
         auto & operator+=(const Monoid &monoid) {
+            if (p > 1) {
+                if (!(p & 1)) {
+                    U[0] = monoid.M * U[0];
+                    U[1] = monoid.M * U[1];
+                }
+                return *this;
+            }
+
             M = monoid.M * M;
             return *this;
         }
 
         friend auto operator+(Monoid ml, const Monoid &mr) {
-            return ml += mr;
+            ml += mr;
+            return ml;
         }
     };
 
-    int n;
     vector<Monoid> ST;
 
     void pull(int i) {
@@ -108,10 +179,18 @@ struct PURQSegmentTree {
         return ST[i];
     }
 
-    PURQSegmentTree(int n, const vector<int> &arr, int s) : n(n), ST(2 * n) {
+    PURQSegmentTree(int m, const vector<int> &arr, int s) : ST(2 * m) {
         a = arr;
+        n = m;
         size = s;
-        for (int i = 0; i < n; i++) ST[i + n] = i;
+
+        for (int k = 0; k < 2; k++) {
+            Monoid::U[k].resize(11);
+            Monoid::V[k].resize(11);
+        }
+        Monoid::p = -1;
+        for (int i = 0; i < m; i++) ST[i + m] = i;
+        Monoid::p = 0;
         build();
     }
 };
@@ -128,22 +207,12 @@ int main() {
 
     int size = ceil(sqrt(n)), blocks = (n + size - 1) / size;
     PURQSegmentTree st(bit_ceil((unsigned) blocks), a, size);
-
-    auto plans = [&]() {
-        auto sum = 1ULL;
-        for (int r = 0; r < 10; r++) sum += st[1].M[r][10];
-        return sum;
-    };
-    cout << plans() << "\n";
+    cout << st[1].M[10][10] << "\n";
     while (q--) {
         int i, v;
         cin >> i >> v;
 
-        if (st.a[i] != v) {
-            st.a[i] = v;
-            int b = (i - 1) / size;
-            st.point_update(b, b);
-        }
-        cout << plans() << "\n";
+        if (st.a[i] != v) st.point_update((i - 1) / size, 10 * i + v);
+        cout << st[1].M[10][10] << "\n";
     }
 }
