0266c51b1849cb715c0c6be515f7466384170ac9
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2026-08-26 00:09:07 +0800

    Update bridgebuilding.cpp

--- open/bridgebuilding.cpp
+++ open/bridgebuilding.cpp
@@ -23,33 +23,6 @@ struct FenwickTree {
     FenwickTree(int n) : BIT(n, 0) {}
 };
 
-template <typename T>
-struct FenwickTree2D {
-    vector<vector<T>> BIT;
-    vector<vector<int>> js;
-
-    FenwickTree2D(int n, const vector<vector<int>> &js) : BIT(n), js(js) {
-        for (int i = 0; i < n; i++) BIT[i].assign(js[i].size() + 1, 0);
-    }
-
-    void update(int i, int j, T v) {
-        for (int temp = j; i < BIT.size(); i += i & -i, j = temp)
-            for (j = upper_bound(js[i].begin(), js[i].end(), j) - js[i].begin(); j < BIT[i].size(); j += j & -j) BIT[i][j] += v;
-    }
-
-    T pref_sum(int i, int j) {
-        T sum = 0;
-        for (int temp = j; i; i -= i & -i, j = temp)
-            for (j = upper_bound(js[i].begin(), js[i].end(), j) - js[i].begin(); j; j -= j & -j) sum += BIT[i][j];
-        return sum;
-    }
-
-    T rectangle_sum(int xl, int xr, int yl, int yr) {
-        if (xl >= xr || yl >= yr) return 0;
-        return pref_sum(xr, yr) - pref_sum(xl, yr) - pref_sum(xr, yl) + pref_sum(xl, yl);
-    }
-};
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -65,30 +38,16 @@ int main() {
 
     auto base = 0LL;
     vector<pair<int, int>> pairs(m);
-    vector<vector<int>> as(n + 1), bs(n + 1), js(n + 1);
-    for (int i = 0; i < m; i++) {
-        int a, b;
+    vector<vector<int>> as(n + 1), bs(n + 1);
+    for (auto &[a, b] : pairs) {
         cin >> a >> b;
 
         if (a > b) swap(a, b);
-        pairs[i] = {a, b};
         as[b].emplace_back(a);
         bs[a].emplace_back(b);
         base += pref[b] - pref[a];
-        for (; a <= n; a += a & -a) js[a].emplace_back(b);
     }
 
-    auto calc = [&](auto &p) {
-        return 2 * (pref[p.second] - pref[p.first]);
-    };
-    sort(pairs.begin(), pairs.end(), [&](const auto &p1, const auto &p2) { return calc(p1) > calc(p2); });
-
-    for (int i = 1; i <= n; i++) {
-        sort(js[i].begin(), js[i].end());
-        js[i].erase(unique(js[i].begin(), js[i].end()), js[i].end());
-    }
-
-    int count = 0;
     vector<int> left, right;
     vector<vector<int>> indices_l(n + 1), indices_r(n + 1);
     stack<int> mono;
@@ -100,12 +59,12 @@ int main() {
             right.emplace_back(r);
             indices_l[l].emplace_back(left.size() - 1);
             indices_r[r].emplace_back(right.size() - 1);
-            count++;
         }
         mono.emplace(i);
     }
 
-    vector<long long> total(count, 0);
+    int k = left.size();
+    vector<long long> total(k);
     FenwickTree<int> count1(n + 1), count2(n + 1);
     FenwickTree<long long> sum1(n + 1), sum2(n + 1);
     for (int i = 1; i <= n; i++) {
@@ -140,29 +99,50 @@ int main() {
         }
     }
 
-    FenwickTree2D<long long> count3(n + 1, js), sum3(n + 1, js);
-    vector<long long> target(count, 0);
-    for (int id = 0; id < count; id++) {
+    vector<long long> target(k);
+    vector<array<long long, 6>> sweep;
+    for (auto [a, b] : pairs) sweep.push_back({2 * (pref[b] - pref[a]), a, b, 0, -1, 0});
+    for (int id = 0; id < k; id++) {
         int i = left[id], j = right[id];
         target[id] = pref[j] - pref[i] + j - i;
-    }
-
-    vector<int> order(count);
-    iota(order.begin(), order.end(), 0);
-    sort(order.begin(), order.end(), [&](int i, int j) { return target[i] > target[j]; });
-    for (int k = 0; int id : order) {
-        int i = left[id], j = right[id];
-        for (; k < pairs.size() && calc(pairs[k]) > target[id]; k++) {
-            auto [a, b] = pairs[k];
-            count3.update(a, b, 1);
-            sum3.update(a, b, calc(pairs[k]));
-        }
 
         if (i + 1 < j) {
-            int l = i + 1, r = j - 1;
-            total[id] += sum3.rectangle_sum(l - 1, r, l - 1, r) - target[id] * count3.rectangle_sum(l - 1, r, l - 1, r);
+            sweep.push_back({target[id], n, j - 1, 1, id, 1});
+            sweep.push_back({target[id], i, j - 1, 1, id, -1});
         }
-        most = max(most, total[id]);
     }
+    sort(sweep.begin(), sweep.end(), [&](const auto &a1, const auto &a2) { return a1[0] != a2[0] ? a1[0] > a2[0] : a1[3] > a2[3]; });
+
+    FenwickTree<int> count(n + 1);
+    FenwickTree<long long> sum(n + 1);
+    auto dnc = [&](auto &&self, int l, int r) -> void {
+        if (l + 1 >= r) return;
+
+        int m = l + (r - l) / 2;
+        self(self, l, m);
+        self(self, m, r);
+
+        int j = l;
+        for (int i = m; i < r; i++) {
+            for (; j < m && sweep[i][1] >= sweep[j][1]; j++)
+                if (!sweep[j][3]) {
+                    count.update(sweep[j][2], 1);
+                    sum.update(sweep[j][2], sweep[j][0]);
+                }
+
+            if (sweep[i][3]) total[sweep[i][4]] += sweep[i][5] * (sum.pref_sum(sweep[i][2]) - target[sweep[i][4]] * count.pref_sum(sweep[i][2]));
+        }
+
+        for (int i = l; i < j; i++)
+            if (!sweep[i][3]) {
+                count.update(sweep[i][2], -1);
+                sum.update(sweep[i][2], -sweep[i][0]);
+            }
+
+        inplace_merge(sweep.begin() + l, sweep.begin() + m, sweep.begin() + r, [&](const auto &a1, const auto &a2) { return a1[1] < a2[1]; });
+    };
+    dnc(dnc, 0, sweep.size());
+
+    for (auto s : total) most = max(most, s);
     cout << base - most;
 }
