69fbdb637858cc8b168bdc034592d0aaa30b4d8b
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2026-08-25 21:03:12 +0800

    Update theline.cpp

--- open/theline.cpp
+++ open/theline.cpp
@@ -21,61 +21,67 @@ int main() {
         rs[i] = r;
         total += r - l;
     }
+    sort(pairs.begin(), pairs.end());
     sort(ls.begin(), ls.end());
     ls.erase(unique(ls.begin(), ls.end()), ls.end());
     sort(rs.begin(), rs.end());
     rs.erase(unique(rs.begin(), rs.end()), rs.end());
 
+    int s = rs.size();
     vector<array<int, 4>> pos(k);
     for (int i = 0; i < k; i++) {
         auto [l, r] = pairs[i];
         pos[i] = {
-                    (int) (upper_bound(rs.begin(), rs.end(), r) - rs.begin()),
-                    (int) (upper_bound(rs.begin(), rs.end(), 2 * r - ls[0] - 2) - rs.begin()),
-                    (int) (upper_bound(rs.begin(), rs.end(), 2 * l - ls[0] + 1) - rs.begin()),
-                    (int) (upper_bound(rs.begin(), rs.end(), 2 * (r - l) + ls[0] - 2) - rs.begin())
+                      (int) (upper_bound(rs.begin(), rs.end(), r) - rs.begin()),
+                      (int) (upper_bound(rs.begin(), rs.end(), 2 * r - ls[0] - 2) - rs.begin()),
+                      (int) (upper_bound(rs.begin(), rs.end(), 2 * l - ls[0] + 1) - rs.begin()),
+                      (int) (upper_bound(rs.begin(), rs.end(), 2 * (r - l) + ls[0] - 2) - rs.begin())
                  };
     }
 
     auto saved = 0LL;
-    vector<int> slope(rs.size() + 1, 0);
-    vector<long long> intercept(rs.size() + 1, 0);
-    auto add = [&](int l, int r, int d1, long long d2) {
+    vector<int> slope(s + 1, 0);
+    vector<long long> intercept(s + 1, 0);
+    int mid = 0, start = 0;
+    auto add = [&](int l, int r, int m, long long c) {
         if (l >= r) return;
-        slope[l] += d1;
-        slope[r] -= d1;
-        intercept[l] += d2;
-        intercept[r] -= d2;
+        slope[l] += m;
+        slope[r] -= m;
+        intercept[l] += c;
+        intercept[r] -= c;
     };
-    
+
     for (int l1 : ls) {
-        if (l1 != ls[0])
-            for (int i = 0; i < k; i++) {
-                auto [l2, r] = pairs[i];
-                for (; pos[i][1] && rs[pos[i][1] - 1] > 2 * r - l1 - 2; pos[i][1]--);
-                for (; pos[i][2] && rs[pos[i][2] - 1] > 2 * l2 - l1 + 1; pos[i][2]--);
-                for (; pos[i][3] < rs.size() && rs[pos[i][3]] < 2 * (r - l2) + l1 - 1; pos[i][3]++);
-            }
+        for (; mid < k && pairs[mid].first <= l1; mid++);
+        for (; start < s && rs[start] <= l1; start++);
 
         fill(slope.begin(), slope.end(), 0);
         fill(intercept.begin(), intercept.end(), 0);
-        for (int i = 0; i < k; i++) {
+
+        for (int i = 0; i < mid; i++) {
             auto [l2, r] = pairs[i];
-            if (l1 >= l2) {
-                add(0, pos[i][0], 1, 2LL * 0 - l1 - 1);
-                add(pos[i][0], pos[i][1], -1, 2LL * r - l1 - 1);
-            } else {
-                add(pos[i][2], pos[i][0], 1, 2LL * (0 - l2) + l1 - 1);
-                add(pos[i][0], pos[i][3], -1, 2LL * (r - l2) + l1 - 1);
-            }
+            if (r <= l1 + 1) continue;
+
+            for (; pos[i][1] && rs[pos[i][1] - 1] > 2 * r - l1 - 2; pos[i][1]--);
+            add(start, pos[i][0], 1, -l1 - 1);
+            add(pos[i][0], pos[i][1], -1, 2 * r - l1 - 1);
         }
 
-        int m = 0;
-        auto c = 0LL;
-        for (int i = 0; i < rs.size(); i++) {
+        for (int i = mid; i < k; i++) {
+            auto [l2, r] = pairs[i];
+            if (r + l1 <= 2 * l2 + 1) continue;
+
+            for (; pos[i][2] && rs[pos[i][2] - 1] > 2 * l2 - l1 + 1; pos[i][2]--);
+            for (; pos[i][3] < s && rs[pos[i][3]] < 2 * (r - l2) + l1 - 1; pos[i][3]++);
+            add(pos[i][2], pos[i][0], 1, l1 - 2 * l2 - 1);
+            add(pos[i][0], pos[i][3], -1, 2 * (r - l2) + l1 - 1);
+        }
+
+        long long m = 0, c = 0;
+        for (int i = start; i < s; i++) {
             m += slope[i];
             c += intercept[i];
-            if (l1 < rs[i]) saved = max(saved, (long long) m * rs[i] + c);
+            saved = max(saved, m * rs[i] + c);
         }
     }
     cout << total - saved;
