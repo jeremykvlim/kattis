6d5276686c2c790b8f85844b585807c499c33055
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2026-08-27 03:44:58 +0800

    Create oneway.cpp

---
+++ open/oneway.cpp
@@ -0,0 +1,213 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T, typename U>
+struct FlowNetwork {
+    struct Arc {
+        int u, rev;
+        T cap;
+        U cost;
+        Arc(int u, int rev, T cap, U cost) : u(u), rev(rev), cap(cap), cost(cost) {}
+    };
+
+    int n;
+    vector<vector<Arc>> network;
+    vector<int> dist;
+    vector<typename vector<Arc>::iterator> it;
+    U inf;
+    FlowNetwork(int n) : n(n), network(n), inf(numeric_limits<U>::max()), dist(n), it(n) {}
+
+    void add_arc(int u, int v, T cap_uv, U cost, T cap_vu = 0) {
+        if (u == v) return;
+
+        network[u].emplace_back(v, network[v].size(), cap_uv, cost);
+        network[v].emplace_back(u, network[u].size() - 1, cap_vu, -cost);
+    }
+
+    bool bfs(int s, int t) {
+        fill(dist.begin(), dist.end(), -1);
+        dist[s] = 0;
+        queue<int> q;
+        q.emplace(s);
+        while (!q.empty()) {
+            int v = q.front();
+            q.pop();
+
+            for (auto [u, _, cap, __] : network[v])
+                if (cap > 0 && !~dist[u]) {
+                    dist[u] = dist[v] + 1;
+                    q.emplace(u);
+                }
+        }
+        return ~dist[t];
+    }
+
+    T dfs(int v, int t, T flow) {
+        if (v == t) return flow;
+
+        for (; it[v] != network[v].end(); it[v]++) {
+            auto &[u, rev, cap, _] = *it[v];
+            if (cap > 0 && dist[u] == dist[v] + 1) {
+                T f = dfs(u, t, min(flow, cap));
+                if (f > 0) {
+                    cap -= f;
+                    network[u][rev].cap += f;
+                    return f;
+                }
+            }
+        }
+        return (T) 0;
+    }
+
+    T max_flow(int s, int t) {
+        T flow = 0, f;
+        while (bfs(s, t)) {
+            for (int v = 0; v < n; v++) it[v] = network[v].begin();
+            while ((f = dfs(s, t, numeric_limits<T>::max())) > 0) flow += f;
+        }
+        return flow;
+    }
+
+    pair<T, U> min_cost_max_flow(int s, int t) {
+        U cost = 0, epsilon = 0;
+        int scale = bit_ceil(2U * n);
+        for (int v = 0; v < n; v++)
+            for (auto &&a : network[v]) {
+                cost += a.cost * a.cap;
+                a.cost *= scale;
+                epsilon = max(epsilon, abs(a.cost));
+            }
+
+        T flow = max_flow(s, t);
+
+        vector<U> potential(n, 0), excess(n, 0);
+        vector<int> count(n, 0);
+        deque<int> active_stack;
+
+        auto push = [&](int v, Arc &a, U delta, bool active) {
+            if (delta > a.cap) delta = a.cap;
+            int u = a.u;
+            a.cap -= delta;
+            network[u][a.rev].cap += delta;
+            excess[v] -= delta;
+            excess[u] += delta;
+
+            if (active && 0 < excess[u] && excess[u] <= delta) active_stack.emplace_front(u);
+        };
+
+        auto relabel = [&](int v, U delta) {
+            if (delta < inf) potential[v] -= delta + epsilon;
+            else {
+                potential[v] -= epsilon;
+                count[v]--;
+            }
+        };
+
+        auto reduced_cost = [&](int v, const Arc &a) {
+            int diff = count[v] - count[a.u];
+            if (diff > 0) return inf;
+            if (diff < 0) return -inf;
+            return a.cost + potential[v] - potential[a.u];
+        };
+
+        auto check = [&](int v) {
+            if (excess[v]) return false;
+
+            U delta = inf;
+            for (auto &&a : network[v]) {
+                if (a.cap <= 0) continue;
+
+                U c = reduced_cost(v, a);
+                if (c < 0) return false;
+                delta = min(delta, c);
+            }
+
+            relabel(v, delta);
+            return true;
+        };
+
+        auto discharge = [&](int v) {
+            U delta = inf;
+
+            for (auto a = network[v].begin(); a != network[v].end(); a++) {
+                if (a->cap <= 0) continue;
+
+                if (reduced_cost(v, *a) < 0) {
+                    if (check(a->u)) {
+                        a--;
+                        continue;
+                    }
+
+                    push(v, *a, excess[v], true);
+                    if (!excess[v]) return;
+                } else delta = min(delta, reduced_cost(v, *a));
+            }
+
+            relabel(v, delta);
+            active_stack.emplace_front(v);
+        };
+
+        while (epsilon > 1) {
+            epsilon >>= 1;
+            active_stack.clear();
+
+            for (int v = 0; v < n; v++)
+                for (auto &&a : network[v])
+                    if (reduced_cost(v, a) < 0 && a.cap > 0) push(v, a, a.cap, false);
+
+            for (int v = 0; v < n; v++)
+                if (excess[v] > 0) active_stack.emplace_front(v);
+
+            while (!active_stack.empty()) {
+                int v = active_stack.front();
+                active_stack.pop_front();
+
+                discharge(v);
+            }
+        }
+
+        for (int v = 0; v < n; v++)
+            for (auto &&a : network[v]) {
+                a.cost /= scale;
+                cost -= a.cost * a.cap;
+            }
+
+        return {flow, cost / 2};
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> degree(n, 0);
+    FlowNetwork<int, long long> fn(n + 2);
+    while (m--) {
+        int u, v;
+        cin >> u >> v;
+        u--;
+        v--;
+
+        degree[v]++;
+        fn.add_arc(v, u, 1, 0);
+    }
+
+    for (int v = 0; v < n; v++) {
+        fn.add_arc(n, v, degree[v], 0);
+        int s = fn.network[v].size();
+        for (int c = 0; c < s; c++) fn.add_arc(v, n + 1, 1, c);
+    }
+    fn.min_cost_max_flow(n, n + 1);
+
+    int d = 0;
+    for (int v = 0; v < n; v++) {
+        int sum = 0;
+        for (auto &a : fn.network[n + 1])
+            if (a.u == v) sum += a.cap;
+        d = max(d, sum);
+    }
+    cout << d;
+}
