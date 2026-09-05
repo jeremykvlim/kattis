e67ee1de7397ced09973b5d817d9103e71e3e1a1
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2026-09-06 04:15:11 +0800

    Update rivers2.cpp

--- open/rivers2.cpp
+++ open/rivers2.cpp
@@ -1,11 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct SegmentTree {
+struct RURQSegmentTree {
     struct Monoid {
         array<long long, 7> sums;
+        long long count, sum_i, sum_sq, first;
 
-        Monoid() : sums{} {}
+        Monoid() : sums{}, count(0), sum_i(0), sum_sq(0), first(0) {}
 
         auto & operator+=(const Monoid &monoid) {
             sums[0] += monoid.sums[0];
@@ -13,6 +14,16 @@ struct SegmentTree {
             sums[2] += monoid.sums[2];
             sums[3] += monoid.sums[3];
             sums[6] += monoid.sums[6];
+
+            if (!count && monoid.count) {
+                sums[4] = monoid.sums[4];
+                sums[5] = monoid.sums[5];
+                first = monoid.first;
+            }
+
+            count += monoid.count;
+            sum_i += monoid.sum_i;
+            sum_sq += monoid.sum_sq;
             return *this;
         }
 
@@ -22,125 +33,138 @@ struct SegmentTree {
         }
     };
 
-    int n;
+    int n, h;
     vector<Monoid> ST;
     vector<array<long long, 4>> lazy;
-
-    SegmentTree(int n) : n(n), ST(2 * n), lazy(n, {0, 0, 0, 0}) {}
-
-    int midpoint(int l, int r) {
-        int i = 1 << __lg(r - l);
-        return min(l + i, r - (i >> 1));
-    }
 
     void pull(int i) {
         ST[i] = ST[i << 1] + ST[i << 1 | 1];
     }
 
-    void apply(int i, int l, int r, long long c0, long long c1, int b) {
-        auto sum_of_sq = [&](long long n) {
-            return n * (n + 1) * (2 * n + 1) / 6;
-        };
+    void build() {
+        for (int i = n - 1; i; i--) pull(i);
+    }
 
-        auto sum_i = ((long long) l + r + 1) * (r - l) / 2;
+    void apply(int i, long long c0, long long c1, int b) {
         ST[i].sums[6] += c0 * ST[i].sums[b ^ 1] + c1 * ST[i].sums[(b ^ 1) + 2];
-        ST[i].sums[b] += c0 * (r - l) + c1 * sum_i;
-        ST[i].sums[b + 2] += c0 * sum_i + c1 * (sum_of_sq(r) - sum_of_sq(l));
-        ST[i].sums[b + 4] += c0 + c1 * (l + 1);
+        ST[i].sums[b] += c0 * ST[i].count + c1 * ST[i].sum_i;
+        ST[i].sums[b + 2] += c0 * ST[i].sum_i + c1 * ST[i].sum_sq;
+        ST[i].sums[b + 4] += c0 + c1 * ST[i].first;
         if (i < n) {
             lazy[i][b] += c0;
             lazy[i][b + 2] += c1;
         }
     }
 
-    void push(int i, int l, int r) {
+    void push(int i) {
         auto [v0, h0, v1, h1] = lazy[i];
-        if (!v0 && !v1 && !h0 && !h1) return;
+        if (!v0 && !h0 && !v1 && !h1) return;
 
-        int m = midpoint(l, r);
         if (v0 || v1) {
-            apply(i << 1, l, m, v0, v1, 0);
-            apply(i << 1 | 1, m, r, v0, v1, 0);
+            apply(i << 1, v0, v1, 0);
+            apply(i << 1 | 1, v0, v1, 0);
         }
         if (h0 || h1) {
-            apply(i << 1, l, m, h0, h1, 1);
-            apply(i << 1 | 1, m, r, h0, h1, 1);
+            apply(i << 1, h0, h1, 1);
+            apply(i << 1 | 1, h0, h1, 1);
         }
         lazy[i] = {0, 0, 0, 0};
+    }
+
+    void push_down(int l, int r) {
+        for (int b = h; b; b--) {
+            if (((l >> b) << b) != l) push(l >> b);
+            if (((r >> b) << b) != r) push((r - 1) >> b);
+        }
+    }
+
+    void pull_up(int l, int r) {
+        for (int b = 1; b <= h; b++) {
+            if (((l >> b) << b) != l) pull(l >> b);
+            if (((r >> b) << b) != r) pull((r - 1) >> b);
+        }
     }
 
     void point_update(int p, long long c0, long long c1, int b) {
         range_update(p - 1, p, c0, c1, b);
     }
 
-    void range_update(int ql, int qr, long long c0, long long c1, int b) {
-        range_update(1, ql, qr, c0, c1, b, 0, n);
-    }
+    void range_update(int l, int r, long long c0, long long c1, int b) {
+        l += n;
+        r += n;
+        push_down(l, r);
 
-    void range_update(int i, int ql, int qr, long long c0, long long c1, int b, int l, int r) {
-        if (qr <= l || r <= ql) return;
-        if (ql <= l && r <= qr) {
-            apply(i, l, r, c0, c1, b);
-            return;
+        int temp_l = l, temp_r = r;
+        for (; l < r; l >>= 1, r >>= 1) {
+            if (l & 1) apply(l++, c0, c1, b);
+            if (r & 1) apply(--r, c0, c1, b);
         }
 
-        push(i, l, r);
-
-        int m = midpoint(l, r);
-        range_update(i << 1, ql, qr, c0, c1, b, l, m);
-        range_update(i << 1 | 1, ql, qr, c0, c1, b, m, r);
-
-        pull(i);
+        pull_up(temp_l, temp_r);
     }
 
-    array<long long, 2> point_query(int pos) {
-        return point_query(1, pos, 0, n);
+    Monoid point_query(int p) {
+        return range_query(p, p + 1);
     }
 
-    array<long long, 2> point_query(int i, int pos, int l, int r) {
-        if (l + 1 == r) return {ST[i].sums[0], ST[i].sums[1]};
+    Monoid range_query(int l, int r) {
+        l += n;
+        r += n;
+        push_down(l, r);
 
-        push(i, l, r);
-        int m = midpoint(l, r);
-        if (pos < m) return point_query(i << 1, pos, l, m);
-        return point_query(i << 1 | 1, pos, m, r);
+        Monoid ml, mr;
+        for (; l < r; l >>= 1, r >>= 1) {
+            if (l & 1) ml = ml + ST[l++];
+            if (r & 1) mr = ST[--r] + mr;
+        }
+
+        return ml + mr;
     }
 
     array<long long, 4> prefix_query(long long c, long long extra_v, long long extra_h) {
         array<long long, 4> a{0, 0, ST[1].sums[4], ST[1].sums[5]};
-        long long sum_v = 0, sum_h = 0, sum_both = 0, base = extra_v * extra_h;
-        prefix_query(1, 0, n, c, extra_v, extra_h, 0, 0, base, sum_v, sum_h, sum_both, a);
+        long long sum_v = 0, sum_h = 0, sum_both = 0, base = extra_v * extra_h, v = 0, h = 0;
+        int i = 1, l = 0, r = n;
+        while (i < n) {
+            push(i);
+
+            int m = l + (r - l) / 2;
+            auto cost = sum_both + ST[i << 1].sums[6] +
+                        extra_v * (sum_h + ST[i << 1].sums[1]) +
+                        extra_h * (sum_v + ST[i << 1].sums[0]) +
+                        (base - (ST[i << 1 | 1].sums[4] + extra_v) * (ST[i << 1 | 1].sums[5] + extra_h)) * m;
+
+            if (c < cost) {
+                v = ST[i << 1 | 1].sums[4];
+                h = ST[i << 1 | 1].sums[5];
+                i <<= 1;
+                r = m;
+            } else {
+                sum_v += ST[i << 1].sums[0];
+                sum_h += ST[i << 1].sums[1];
+                sum_both += ST[i << 1].sums[6];
+                a = {m, cost, ST[i << 1 | 1].sums[4], ST[i << 1 | 1].sums[5]};
+                (i <<= 1) |= 1;
+                l = m;
+            }
+        }
+
+        auto cost = sum_both + ST[i].sums[6] +
+                    extra_v * (sum_h + ST[i].sums[1]) +
+                    extra_h * (sum_v + ST[i].sums[0]) +
+                    (base - (v + extra_v) * (h + extra_h)) * (l + 1);
+
+        if (c >= cost) a = {l + 1, cost, v, h};
         return a;
     }
 
-    void prefix_query(int i, int l, int r, long long c, long long extra_v, long long extra_h, long long v, long long h, long long base,
-                      long long &sum_v, long long &sum_h, long long &sum_both, array<long long, 4> &a) {
-        if (l + 1 == r) {
-            auto cost = sum_both + ST[i].sums[6] + extra_v * (sum_h + ST[i].sums[1]) + extra_h * (sum_v + ST[i].sums[0]) +
-                        (base - (v + extra_v) * (h + extra_h)) * (l + 1);
-            if (c >= cost) {
-                sum_v += ST[i].sums[0];
-                sum_h += ST[i].sums[1];
-                sum_both += ST[i].sums[6];
-                a = {l + 1, cost, v, h};
-            }
-            return;
+    RURQSegmentTree(int n) : n(n), h(__lg(n)), ST(2 * n), lazy(n, {0, 0, 0, 0}) {
+        for (int i = 0; i < n; i++) {
+            ST[i + n].count = 1;
+            ST[i + n].sum_i = ST[i + n].first = i + 1;
+            ST[i + n].sum_sq = (long long) (i + 1) * (i + 1);
         }
-
-        push(i, l, r);
-
-        int m = midpoint(l, r);
-        auto cost = sum_both + ST[i << 1].sums[6] + extra_v * (sum_h + ST[i << 1].sums[1]) + extra_h * (sum_v + ST[i << 1].sums[0]) +
-                    (base - (ST[i << 1 | 1].sums[4] + extra_v) * (ST[i << 1 | 1].sums[5] + extra_h)) * m;
-
-        if (c < cost) prefix_query(i << 1, l, m, c, extra_v, extra_h, ST[i << 1 | 1].sums[4], ST[i << 1 | 1].sums[5], base, sum_v, sum_h, sum_both, a);
-        else {
-            sum_v += ST[i << 1].sums[0];
-            sum_h += ST[i << 1].sums[1];
-            sum_both += ST[i << 1].sums[6];
-            a = {m, cost, ST[i << 1 | 1].sums[4], ST[i << 1 | 1].sums[5]};
-            prefix_query(i << 1 | 1, m, r, c, extra_v, extra_h, v, h, base, sum_v, sum_h, sum_both, a);
-        }
+        build();
     }
 };
 
@@ -153,8 +177,8 @@ int main() {
     cin >> n >> m >> z >> s >> t;
 
     auto total = (long long) n * m;
-    int k = max(n, m) + 1;
-    SegmentTree st(k);
+    int k = bit_ceil((unsigned) max(n, m) + 1);
+    RURQSegmentTree st(k);
 
     auto handle_boundary = [&](int len, int sgn, bool b) {
         if (len) st.range_update(0, len, sgn * (len + 1), -sgn, b);
@@ -243,12 +267,12 @@ int main() {
             cin >> c;
 
             long long extra_v = v.empty() ? n : 0, extra_h = h.empty() ? m : 0;
-
             auto [i, cost, v1, h1] = st.prefix_query(c, extra_v, extra_h);
             long long a1 = (v1 + extra_v) * (h1 + extra_h), a2;
             if (i + 2 > k) a2 = extra_v * extra_h;
             else {
-                auto [v2, h2] = st.point_query(i + 1);
+                auto monoid = st.point_query(i + 1);
+                auto v2 = monoid.sums[0], h2 = monoid.sums[1];
                 a2 = (v2 + extra_v) * (h2 + extra_h);
             }
             cout << total - a1 + min(a1 - a2, (c - cost) / (i + 1)) << "\n";
