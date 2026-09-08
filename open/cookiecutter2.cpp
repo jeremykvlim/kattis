d95713edfd4e0e25c7a41904dcac9182f0587b64
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2026-09-08 12:57:51 +0800

    Update

--- open/cookiecutter2.cpp
+++ open/cookiecutter2.cpp
@@ -171,6 +171,10 @@ int main() {
         exit(0);
     }
 
+    auto halfplane = [](const auto &p) {
+        return (p.y > 0 || (!p.y && p.x > 0)) ? 0 : 1;
+    };
+
     for (int i = 0; i < m; i++) {
         int k = m - 1;
         vector<pair<Point<int>, int>> v;
@@ -178,14 +182,11 @@ int main() {
             if (i != j) v.emplace_back(points[j] - points[i], j);
 
         sort(v.begin(), v.end(), [&](auto p1, auto p2) {
-            auto halfplane = [](const auto &p) -> int {
-                if ((p.x > 0 && !p.y) || p.y > 0) return 0;
-                return 1;
-            };
-
             auto a = p1.first, b = p2.first;
             int ha = halfplane(a), hb = halfplane(b);
-            return ha != hb ? ha < hb : cross(a, b) > 0;
+            if (ha != hb) return ha < hb;
+            if (auto c = cross(a, b)) return c > 0;
+            return p1.second < p2.second;
         });
 
         auto cookie = [&](auto &a, auto &b) -> double {
