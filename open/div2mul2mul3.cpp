8e31ad61d3b9f81c5f761c56aa9da1a004cc9b8e
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2026-09-08 14:27:26 +0800

    Update div2mul2mul3.cpp
    standardized hierholzer

--- open/div2mul2mul3.cpp
+++ open/div2mul2mul3.cpp
@@ -5,193 +5,100 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    vector<long long> p3_table(38, 1);
+    for (int i = 1; i < 38; i++) p3_table[i] = 3 * p3_table[i - 1];
+
     int n;
+    vector<vector<int>> count(38, vector<int>(60)), prev(38, vector<int>(60));
+    vector<int> p2_l(38), p2_r(38), starts(39);
     while (cin >> n && n) {
-        vector<vector<int>> count(40, vector<int>(64, 0));
-        int max_p3 = 0, max_p2 = 0;
-        while (n--) {
+        bool possible = true;
+        int max_p3 = 0;
+        for (auto &row : count)
+            fill(row.begin(), row.end(), 0);
+        fill(p2_l.begin(), p2_l.end(), 60);
+        fill(p2_r.begin(), p2_r.end(), -1);
+        for (int i = 0; i < n; i++) {
             long long x;
             cin >> x;
 
-            int p3 = 0, p2 = 0;
-            while (!(x % 3)) {
-                x /= 3;
-                p3++;
+            int p2 = countr_zero((unsigned long long) x);
+            x >>= p2;
+
+            int p3 = lower_bound(p3_table.begin(), p3_table.end(), x) - p3_table.begin();
+            if (p3 == 38 || p3_table[p3] != x) {
+                possible = false;
+                continue;
             }
-            while (!(x & 1)) {
-                x >>= 1;
-                p2++;
-            }
-            if (x > 1) break;
 
             count[p3][p2]++;
+            p2_l[p3] = min(p2_l[p3], p2);
+            p2_r[p3] = max(p2_r[p3], p2);
             max_p3 = max(max_p3, p3);
-            max_p2 = max(max_p2, p2);
         }
 
-        if (n != -1) {
-            while (n--) {
-                long long x;
-                cin >> x;
-            }
+        if (!possible) {
             cout << "NO\n";
             continue;
         }
 
-        vector<int> p2_l(max_p3 + 1), p2_r(max_p3 + 1);
-        int max_r = 0;
+        auto dp = 1LL;
+        for (int p3 = 0; p3 <= max_p3 && dp; p3++) {
+            fill(prev[p3].begin(), prev[p3].end(), -1);
+            auto temp = 0LL;
+            for (int p2 = 0; p2 < 60; p2++)
+                if ((dp >> p2) & 1 && count[p3][p2])
+                    for (int i = p2_l[p3]; i <= p2_r[p3]; i++)
+                        if (count[p3][i] && !~prev[p3][i]) {
+                            int j = p2_l[p3], e = 0;
+                            for (; j <= p2_r[p3]; j++) {
+                                e = 2 * count[p3][j] - (j == p2) - (j == i) - e;
+                                if (e < (j < p2_r[p3])) break;
+                            }
+
+                            if (j > p2_r[p3] && !e) {
+                                prev[p3][i] = p2;
+                                temp |= 1LL << i;
+                            }
+                        }
+
+            dp = temp;
+        }
+
+        if (!dp) {
+            cout << "NO\n";
+            continue;
+        }
+
+        starts[max_p3 + 1] = countr_zero((unsigned long long) dp);
+        for (int p3 = max_p3; ~p3; p3--) starts[p3] = prev[p3][starts[p3 + 1]];
+
+        cout << "YES\n";
         for (int p3 = 0; p3 <= max_p3; p3++) {
-            int l = 63, r = -1;
-            for (int p2 = 0; p2 <= max_p2; p2++)
-                if (count[p3][p2]) {
-                    l = min(l, p2);
-                    r = max(r, p2);
+            vector<int> edge(60, 0);
+            for (int p2 = p2_l[p3], e = 0; p2 < p2_r[p3]; p2++) edge[p2] = e = 2 * count[p3][p2] - (p2 == starts[p3]) - (p2 == starts[p3 + 1]) - e;
+
+            vector<int> path;
+            stack<int> s;
+            s.emplace(starts[p3]);
+            while (!s.empty()) {
+                int p2 = s.top();
+
+                if (p2 > p2_l[p3] && edge[p2 - 1]) {
+                    edge[p2 - 1]--;
+                    s.emplace(p2 - 1);
+                } else if (p2 < p2_r[p3] && edge[p2]) {
+                    edge[p2]--;
+                    s.emplace(p2 + 1);
+                } else {
+                    path.emplace_back(p2);
+                    s.pop();
                 }
-            p2_l[p3] = l;
-            p2_r[p3] = r;
-            max_r = max(max_r, r);
-        }
-
-        vector<bool> dp((max_p3 + 1) * (max_r + 1) * (max_r + 1), false);
-        auto dp_index = [&](int p3, int p2_i, int p2_j) -> int {
-            return p3 * (max_r + 1) * (max_r + 1) + p2_i * (max_r + 1) + p2_j;
-        };
-
-        vector<int> temp(max_r + 1, 0);
-        for (int p3 = 0; p3 <= max_p3; p3++)
-            for (int p2_i = p2_l[p3]; p2_i <= p2_r[p3]; p2_i++)
-                for (int p2_j = p2_l[p3]; p2_j <= p2_r[p3]; p2_j++) {
-                    int l = p2_l[p3], r = p2_r[p3];
-                    if (!count[p3][p2_i] || !count[p3][p2_j] ||
-                        !(l <= p2_i && p2_i <= r) ||
-                        !(l <= p2_j && p2_j <= r)) continue;
-
-                    for (int p2 = l; p2 <= r; p2++) temp[p2] = count[p3][p2];
-
-                    bool rev = p2_i > p2_j;
-                    auto check_left = [&]() {
-                        for (int p2 = l; p2 < (!rev ? p2_i : p2_j); p2++)
-                            if (temp[p2]) {
-                                if (temp[p2 + 1] > temp[p2]) temp[p2 + 1] -= temp[p2];
-                                else return false;
-                            }
-                        return true;
-                    };
-
-                    auto check_middle = [&]() {
-                        for (int p2 = p2_i; (!rev ? p2 < p2_j : p2 > p2_j); (!rev ? p2++ : p2--)) {
-                            if (temp[p2] > 0) temp[(!rev ? p2 + 1 : p2 - 1)] -= temp[p2] - 1;
-                            else return false;
-                        }
-                        return true;
-                    };
-
-                    auto check_right = [&]() {
-                        for (int p2 = r; p2 > (!rev ? p2_j : p2_i); p2--)
-                            if (temp[p2]) {
-                                if (temp[p2 - 1] > temp[p2]) temp[p2 - 1] -= temp[p2];
-                                else return false;
-                            }
-                        return true;
-                    };
-
-                    if (!rev) {
-                        if (!check_left() || !check_middle() || !check_right()) goto done;
-                    } else {
-                        if (!check_right() || !check_middle() || !check_left()) goto done;
-                    }
-
-                    dp[dp_index(p3, p2_i, p2_j)] = temp[p2_j] == 1;
-                    done:;
-                }
-
-        vector<int> prev((max_p3 + 1) * (max_p2 + 1), -1);
-        auto prev_index = [&](int p3, int p2) {
-            return p3 * (max_p2 + 1) + p2;
-        };
-
-        for (int i = 0; i <= max_p2; i++)
-            if (dp[dp_index(0, 0, i)]) prev[prev_index(0, i)]++;
-
-        for (int i = 1; i <= max_p3; i++)
-            for (int j = 0; j <= max_p2; j++)
-                for (int k = 0; k <= max_p2; k++)
-                    if (~prev[prev_index(i - 1, k)] && dp[dp_index(i, k, j)]) {
-                        prev[prev_index(i, j)] = k;
-                        break;
-                    }
-
-        for (int p2_j = 0; p2_j <= max_p2; p2_j++) {
-            if (~prev[prev_index(max_p3, p2_j)]) {
-                cout << "YES\n";
-                vector<long long> a;
-                for (int p3 = max_p3; ~p3; p3--) {
-                    int p2_i = prev[prev_index(p3, p2_j)], l = p2_l[p3], r = p2_r[p3];
-                    for (int p2 = l; p2 <= r; p2++) temp[p2] = count[p3][p2];
-
-                    vector<long long> seq;
-                    auto calc = [&](int p3, int p2) {
-                        auto a = 1LL;
-                        while (p2--) a *= 2;
-                        while (p3--) a *= 3;
-                        return a;
-                    };
-
-                    bool rev = p2_i > p2_j;
-                    auto add_left = [&]() {
-                        for (int p2 = !rev ? p2_i : p2_j; p2 > l; p2--) {
-                            seq.emplace_back(calc(p3, p2));
-                            temp[p2]--;
-                        }
-
-                        for (int p2 = l; p2 < p2_j; p2++) {
-                            while (temp[p2] > 1) {
-                                seq.emplace_back(calc(p3, p2));
-                                seq.emplace_back(calc(p3, p2 + 1));
-                                temp[p2]--;
-                                temp[p2 + 1]--;
-                            }
-                            seq.emplace_back(calc(p3, p2));
-                        }
-                    };
-
-                    auto add_right = [&]() {
-                        for (int p2 = !rev ? p2_j : p2_i; p2 < r; p2++) {
-                            seq.emplace_back(calc(p3, p2));
-                            temp[p2]--;
-                        }
-
-                        for (int p2 = r; p2 > p2_j; p2--) {
-                            while (temp[p2] > 1) {
-                                seq.emplace_back(calc(p3, p2));
-                                seq.emplace_back(calc(p3, p2 - 1));
-                                temp[p2]--;
-                                temp[p2 - 1]--;
-                            }
-                            seq.emplace_back(calc(p3, p2));
-                        }
-                    };
-
-                    if (!rev) {
-                        add_left();
-                        add_right();
-                    } else {
-                        add_right();
-                        add_left();
-                    }
-                    seq.emplace_back(calc(p3, p2_j));
-                    reverse(seq.begin(), seq.end());
-                    for (auto ai : seq) a.emplace_back(ai);
-                    p2_j = p2_i;
-                }
-                reverse(a.begin(), a.end());
-                for (auto ai : a) cout << ai << " ";
-                cout << "\n";
-                goto next;
             }
-        }
 
-        cout << "NO\n";
-        next:;
+            reverse(path.begin(), path.end());
+            for (int p2 : path) cout << p3_table[p3] * (1LL << p2) << " ";
+        }
+        cout << "\n";
     }
 }
