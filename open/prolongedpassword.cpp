d2c0aca160d83043845ec7f778ac83ae6fd5ff3b
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2026-09-17 02:58:31 +0800

    Update prolongedpassword.cpp

--- open/prolongedpassword.cpp
+++ open/prolongedpassword.cpp
@@ -89,7 +89,8 @@ int main() {
     for (int c = 0; c < 26; c++) next[c] = T[c][0] - 'a';
 
     JumpTable<int> jt(K + 1, 26, next, vector<int>(26, 0));
-    auto letter = [&](int c1, long long k, long long p) {
+    auto letter = [&](int c1, long long p) {
+        auto k = K;
         if (k > p2) {
             c1 = jt.jump_up(c1, k - p2).second;
             k = p2;
@@ -109,14 +110,14 @@ int main() {
     };
 
     if (K >= p2)
-        for (auto mi : m) cout << letter(s[0] - 'a', K, mi) << "\n";
+        for (auto mi : m) cout << letter(s[0] - 'a', mi) << "\n";
     else {
         vector<long long> pref(s.size() + 1, 0);
         for (int i = 0; i < s.size(); i++) pref[i + 1] = min((long long) 1e15, pref[i] + len[K][s[i] - 'a']);
 
         for (auto mi : m) {
             int i = lower_bound(pref.begin() + 1, pref.end(), mi) - pref.begin() - 1;
-            cout << letter(s[i] - 'a', K, mi - pref[i]) << "\n";
+            cout << letter(s[i] - 'a', mi - pref[i]) << "\n";
         }
     }
 }
