c5923a4c28e96a0432307c385d394df1bc6823c7
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2026-09-19 01:17:46 +0800

    Update hillclimbing.cpp

--- nus/cs3233/hillclimbing.cpp
+++ nus/cs3233/hillclimbing.cpp
@@ -16,7 +16,7 @@ int main() {
         vector<int> a(m + 1);
         for (int i = 1; i <= m; i++) {
             cin >> a[i];
-            
+
             total += a[i];
         }
 
@@ -34,17 +34,13 @@ int main() {
             continue;
         }
         total -= (n + 1) / 2;
+        for (int i = 1; i <= m && total; i++) {
+            int d = min(a[i], total);
+            a[i] -= d;
+            total -= d;
+        }
 
-        if (total)
-            for (int i = 1; i <= m && total; i++) {
-                int d = min(a[i], total);
-                a[i] -= d;
-                total -= d;
-            }
-
-        vector<int> parent_e(n + 1, 0);
-        vector<vector<int>> children(n + 1);
-        vector<vector<pair<int, int>>> adj_list_tree(n + 1);
+        vector<int> parent_e(n + 1, 0), order{1};
         queue<int> q;
         q.emplace(1);
         while (!q.empty()) {
@@ -54,103 +50,39 @@ int main() {
             for (auto [u, i] : adj_list[v])
                 if (!parent_e[u] && u != 1) {
                     parent_e[u] = i;
-                    children[v].emplace_back(u);
-                    adj_list_tree[v].emplace_back(u, i);
-                    adj_list_tree[u].emplace_back(v, i);
+                    order.emplace_back(u);
                     q.emplace(u);
                 }
         }
 
+        vector<int> c(m + 1);
+        vector<vector<int>> edge_indices(n + 1);
         vector<pair<int, int>> colors;
         for (int i = 1; i <= m; i++)
             if (a[i]) colors.emplace_back(a[i], i);
-        sort(colors.begin(), colors.end());
-
-        vector<int> c(m + 1, 0);
-        vector<bool> active_e(m + 1, false);
-        for (int v = 2; v <= n; v++)
-            if (parent_e[v]) active_e[parent_e[v]] = true;
-
-        auto color_edge = [&](int i, int ci) {
-            if (!i) return;
-            c[i] = ci;
-            active_e[i] = false;
-        };
-
-        auto dfs1 = [&](auto &&self, int v, int p, int &r, int ci) {
-            if (r <= 0) return;
-            for (auto [u, i] : adj_list_tree[v])
-                if (u != p && active_e[i]) {
-                    color_edge(i, ci);
-                    r--;
-                    self(self, u, v, r, ci);
-                    if (r <= 0) return;
-                }
-        };
-
-        int k = 0;
-        vector<int> dp(n + 1, 0);
-        auto dfs2 = [&](auto &&self, int v = 1) -> void {
-            for (int u : children[v]) self(self, u);
-
-            int sum = 0;
-            set<pair<int, int>> active_v;
-            for (int u : children[v]) {
-                int i = parent_e[u];
-                if (i && active_e[i]) {
-                    active_v.emplace(dp[u] + 1, u);
-                    sum += dp[u] + 1;
-                }
-            }
-
-            for (; k < colors.size(); k++) {
-                auto [remaining, ci] = colors[k];
-                if (remaining > sum) break;
-
-                auto color = [&](int u, int r) {
-                    sum -= dp[u] + 1;
-                    color_edge(parent_e[u], ci);
-                    if (r) dfs1(dfs1, u, v, r, ci);
-                };
-
-                auto it = active_v.lower_bound({remaining, -1});
-                if (it != active_v.end()) {
-                    int u = it->second;
-                    active_v.erase(it);
-                    color(u, remaining - 1);
-                } else {
-                    int temp = remaining;
-                    vector<int> remove;
-                    for (it = active_v.begin(); it != active_v.end() && it->first < temp; it++) {
-                        remove.emplace_back(it->second);
-                        temp -= it->first;
+        sort(colors.rbegin(), colors.rend());
+        reverse(order.begin(), order.end());
+        for (int v : order)
+            for (auto [u, i] : adj_list[v])
+                if (parent_e[u] == i) {
+                    edge_indices[u].emplace_back(i);
+                    if (edge_indices[v].size() < edge_indices[u].size()) swap(edge_indices[v], edge_indices[u]);
+                    edge_indices[v].insert(edge_indices[v].end(), edge_indices[u].begin(), edge_indices[u].end());
+                    if (edge_indices[v].size() >= colors.back().first) {
+                        auto [count, color] = colors.back();
+                        colors.pop_back();
+                        while (count--) {
+                            c[edge_indices[v].back()] = color;
+                            edge_indices[v].pop_back();
+                        }
+                        edge_indices[v].clear();
                     }
-
-                    for (int u : remove) {
-                        it = active_v.find({dp[u] + 1, u});
-                        active_v.erase(it);
-                        color(u, dp[u]);
-                    }
-
-                    it = active_v.lower_bound({temp, -1});
-                    int u = it->second;
-                    active_v.erase(it);
-                    color(u, temp - 1);
+                    if (colors.empty()) goto done;
                 }
-            }
 
-            for (int u : children[v]) {
-                int i = parent_e[u];
-                if (i && active_e[i]) dp[v] += dp[u] + 1;
-            }
-        };
-        dfs2(dfs2);
-
-        if (k < colors.size()) cout << "No\n";
-        else {
-            cout << "Yes\n";
-            for (int i = 1; i <= m; i++) cout << c[i] << " ";
-            cout << "\n";
-        }
+        done:;
+        cout << "Yes\n";
+        for (int i = 1; i <= m; i++) cout << c[i] << " ";
+        cout << "\n";
     }
 }
