0aa37bd5fd7dfe22d7349d316d38052ce9dc4aa5
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2026-09-20 05:07:48 +0800

    Update

--- open/kingdomofhamsters.cpp
+++ open/kingdomofhamsters.cpp
@@ -124,6 +124,63 @@ double euclidean_dist(const Point<T> &a, const Point<T> &b = {0, 0}) {
     return sqrt((double) (a.x - b.x) * (a.x - b.x) + (double) (a.y - b.y) * (a.y - b.y));
 }
 
+template <typename T>
+vector<pair<T, int>> smawk(int n, int m, auto &&get, auto &&cmp) {
+    int lg = __lg(n);
+    vector<pair<T, int>> dp(n);
+    vector<int> cols(n), offset(lg + 1, 0);
+    for (int b = 0; b < lg; b++) {
+        int size = 0;
+        auto push = [&](int col, int limit = 0) {
+            int temp = size;
+            for (; size > limit; size--) {
+                int row = (size << (b + 1)) - 1;
+                pair<T, int> p{get(row, col), col};
+                if (!cmp(dp[row], p)) break;
+                dp[row] = p;
+            }
+            if (size == n >> (b + 1)) return;
+
+            if (size == temp) {
+                int row = ((size + 1) << (b + 1)) - 1;
+                dp[row] = {get(row, col), col};
+            }
+            cols[offset[b] + size++] = col;
+        };
+
+        if (!b)
+            for (int col = 0; col < m; col++) push(col);
+        else
+            for (int i = offset[b - 1]; i < offset[b]; i++) push(cols[i], (i - offset[b - 1]) >> 1);
+        offset[b + 1] = offset[b] + size;
+    }
+
+    for (int b = lg; b; b--)
+        for (int row = (1 << b) - 1, i = offset[b - 1]; row < n; row += 2 << b) {
+            int stop = row + (1 << b) < n ? dp[row + (1 << b)].second : -1, col = cols[i];
+            dp[row] = {get(row, col), col};
+            if (col == stop) continue;
+            for (i++; i < offset[b]; i++) {
+                col = cols[i];
+                pair<T, int> p{get(row, col), col};
+                if (cmp(dp[row], p)) dp[row] = p;
+                if (col == stop) break;
+            }
+        }
+
+    for (int row = 0, col = 0; row < n; row += 2) {
+        int stop = row + 1 < n ? dp[row + 1].second : -1;
+        dp[row] = {get(row, col), col};
+        if (col == stop) continue;
+        for (col++; col < m; col++) {
+            pair<T, int> p{get(row, col), col};
+            if (cmp(dp[row], p)) dp[row] = p;
+            if (col == stop) break;
+        }
+    }
+    return dp;
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -134,34 +191,41 @@ int main() {
     vector<Point<double>> points(n);
     for (auto &[x, y] : points) cin >> x >> y;
 
-    vector<vector<double>> dist(n, vector<double>(n));
+    vector<vector<double>> dist(n, vector<double>(n, 0)), dp(n, vector<double>(n, 0));
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
             if (i != j) dist[i][j] = euclidean_dist(points[i], points[j]);
-    
-    vector<vector<double>> dp(n, vector<double>(n, 0));
-    auto dnc = [&](auto &&self, int l, int r, int ql, int qr, int u, bool b) -> void {
-        int m = l + (r - l) / 2, qm = 0;
-        for (int i = ql; i <= qr && i < m - b; i++) {
-            auto d = dist[u][i % n] + dist[i % n][m % n];
-            if (dp[u][m % n] < d) {
-                dp[u][m % n] = d;
-                qm = i;
-            }
-        }
 
-        if (l < m) self(self, l, m - 1, ql, qm, u, b);
-        if (r > m) self(self, m + 1, r, qm, qr, u, b);
+    auto cmp = [&](const auto &p1, const auto &p2) {
+        return p1.first < p2.first;
     };
-    for (int i = 0; i < n; i++) dnc(dnc, i + 2, i + n - 1, i + 1, i + n - 2, i, false);
 
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++) {
-            dist[i][j] = dp[i][j];
-            dp[i][j] = 0;
-        }
+    for (int i = 0; i < n; i++) {
+        auto get = [&](int row, int col) {
+            if (col > row) return -1e20 * (2 * (col - row) - 1);
+            int j = (i + 1 + col) % n, k = (i + 2 + row) % n;
+            return dist[i][j] + dist[j][k];
+        };
 
-    for (int i = 0; i < n; i++) dnc(dnc, i + 4, i + n - 1, i + 2, i + n - 3, i, true);
+        int m = n - 2;
+        auto rows = smawk<double>(m, m, get, cmp);
+        for (int row = 0; row < m; row++) dp[i][(i + 2 + row) % n] = rows[row].first;
+    }
+
+    swap(dist, dp);
+    for (auto &row : dp) fill(row.begin(), row.end(), 0);
+
+    for (int i = 0; i < n; i++) {
+        auto get = [&](int row, int col) {
+            if (col > row) return -1e20 * (2 * (col - row) - 1);
+            int j = (i + 2 + col) % n, k = (i + 4 + row) % n;
+            return dist[i][j] + dist[j][k];
+        };
+
+        int m = n - 4;
+        auto rows = smawk<double>(m, m, get, cmp);
+        for (int row = 0; row < m; row++) dp[i][(i + 4 + row) % n] = rows[row].first;
+    }
 
     for (int i = 0; i < n; i++) {
         double d = 0;
--- open/transitcard.cpp
+++ open/transitcard.cpp
@@ -1,6 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+template <typename T>
+vector<pair<T, int>> smawk(int n, int m, auto &&get, auto &&cmp) {
+    int lg = __lg(n);
+    vector<pair<T, int>> dp(n);
+    vector<int> cols(n), offset(lg + 1, 0);
+    for (int b = 0; b < lg; b++) {
+        int size = 0;
+        auto push = [&](int col, int limit = 0) {
+            int temp = size;
+            for (; size > limit; size--) {
+                int row = (size << (b + 1)) - 1;
+                pair<T, int> p{get(row, col), col};
+                if (!cmp(dp[row], p)) break;
+                dp[row] = p;
+            }
+            if (size == n >> (b + 1)) return;
+
+            if (size == temp) {
+                int row = ((size + 1) << (b + 1)) - 1;
+                dp[row] = {get(row, col), col};
+            }
+            cols[offset[b] + size++] = col;
+        };
+
+        if (!b)
+            for (int col = 0; col < m; col++) push(col);
+        else
+            for (int i = offset[b - 1]; i < offset[b]; i++) push(cols[i], (i - offset[b - 1]) >> 1);
+        offset[b + 1] = offset[b] + size;
+    }
+
+    for (int b = lg; b; b--)
+        for (int row = (1 << b) - 1, i = offset[b - 1]; row < n; row += 2 << b) {
+            int stop = row + (1 << b) < n ? dp[row + (1 << b)].second : -1, col = cols[i];
+            dp[row] = {get(row, col), col};
+            if (col == stop) continue;
+            for (i++; i < offset[b]; i++) {
+                col = cols[i];
+                pair<T, int> p{get(row, col), col};
+                if (cmp(dp[row], p)) dp[row] = p;
+                if (col == stop) break;
+            }
+        }
+
+    for (int row = 0, col = 0; row < n; row += 2) {
+        int stop = row + 1 < n ? dp[row + 1].second : -1;
+        dp[row] = {get(row, col), col};
+        if (col == stop) continue;
+        for (col++; col < m; col++) {
+            pair<T, int> p{get(row, col), col};
+            if (cmp(dp[row], p)) dp[row] = p;
+            if (col == stop) break;
+        }
+    }
+    return dp;
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -11,29 +68,49 @@ int main() {
     vector<int> p(l), d(l - 1);
     for (int &pi : p) cin >> pi;
     for (int &di : d) cin >> di;
-    d.emplace_back(1e6 + 1);
 
     int t, n;
     cin >> t >> n;
 
-    vector<int> totals(t + 1, 0);
-    for (int i = 1, j = 0, s = 0, days = d[0]; i <= t; i++) {
-        totals[i] = s += p[j];
-        if (!--days)
-            if (j < l - 1) {
-                j++;
-                days = d[j];
-            }
+    d.emplace_back(t + 1);
+    vector<int> pref(t + 1, 0);
+    for (int i = 1, j = 0, days = d[0]; i <= t; i++) {
+        pref[i] = pref[i - 1] + p[j];
+
+        if (!--days && j + 1 < l) {
+            j++;
+            days = d[j];
+        }
     }
 
     vector<int> a(n), b(n), dp(n);
     for (int i = 0; i < n; i++) {
         cin >> a[i] >> b[i];
 
-        dp[i] = totals[a[i] - 1];
-        for (int j = 0; j < i; j++) dp[i] = min(dp[i], dp[j] + totals[a[i] - b[j] - 1]);
+        dp[i] = pref[a[i] - 1];
     }
 
-    for (int i = 0; i < n; i++) totals[t] = min(totals[t], dp[i] + totals[t - b[i]]);
-    cout << totals[t];
+    auto dnc = [&](auto &&self, int l, int r) -> void {
+        if (l + 1 == r) return;
+
+        int m = l + (r - l) / 2;
+        self(self, l, m);
+
+        auto get = [&](int row, int col) {
+            int i = m + row, j = m - 1 - col;
+            return dp[j] + pref[a[i] - b[j] - 1];
+        };
+
+        auto cmp = [&](const auto &p1, const auto &p2) {
+            return p1.first > p2.first;
+        };
+        auto rows = smawk<int>(r - m, m - l, get, cmp);
+        for (int i = m; i < r; i++) dp[i] = min(dp[i], rows[i - m].first);
+
+        self(self, m, r);
+    };
+    if (n) dnc(dnc, 0, n);
+
+    for (int i = 0; i < n; i++) pref[t] = min(pref[t], dp[i] + pref[t - b[i]]);
+    cout << pref[t];
 }
