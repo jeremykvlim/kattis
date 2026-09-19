0560e466ccc53c8457f67feb7cdc3519a8208e23
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2026-09-20 07:18:16 +0800

    Update

--- open/foodreplicator.cpp
+++ open/foodreplicator.cpp
@@ -1,6 +1,82 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+template <typename T>
+vector<pair<T, int>> smawk(int n, int m, auto &&get, auto &&cmp) {
+    int lg = __lg(n);
+    vector<pair<T, int>> dp(n);
+    vector<int> cols(n), offset(lg + 1, 0);
+    for (int b = 0; b < lg; b++) {
+        int size = 0;
+        auto push = [&](int col, int limit = 0) {
+            int temp = size;
+            for (; size > limit; size--) {
+                int row = (size << (b + 1)) - 1;
+                pair<T, int> p{get(row, col), col};
+                if (!cmp(dp[row], p)) break;
+                dp[row] = p;
+            }
+            if (size == n >> (b + 1)) return;
+
+            if (size == temp) {
+                int row = ((size + 1) << (b + 1)) - 1;
+                dp[row] = {get(row, col), col};
+            }
+            cols[offset[b] + size++] = col;
+        };
+
+        if (!b)
+            for (int col = 0; col < m; col++) push(col);
+        else
+            for (int i = offset[b - 1]; i < offset[b]; i++) push(cols[i], (i - offset[b - 1]) >> 1);
+        offset[b + 1] = offset[b] + size;
+    }
+
+    for (int b = lg; b; b--)
+        for (int row = (1 << b) - 1, i = offset[b - 1]; row < n; row += 2 << b) {
+            int stop = row + (1 << b) < n ? dp[row + (1 << b)].second : -1, col = cols[i];
+            dp[row] = {get(row, col), col};
+            if (col == stop) continue;
+            for (i++; i < offset[b]; i++) {
+                col = cols[i];
+                pair<T, int> p{get(row, col), col};
+                if (cmp(dp[row], p)) dp[row] = p;
+                if (col == stop) break;
+            }
+        }
+
+    for (int row = 0, col = 0; row < n; row += 2) {
+        int stop = row + 1 < n ? dp[row + 1].second : -1;
+        dp[row] = {get(row, col), col};
+        if (col == stop) continue;
+        for (col++; col < m; col++) {
+            pair<T, int> p{get(row, col), col};
+            if (cmp(dp[row], p)) dp[row] = p;
+            if (col == stop) break;
+        }
+    }
+    return dp;
+}
+
+template <typename T>
+vector<T> max_plus_convolve(const vector<T> &concave, const vector<T> &arbitrary) {
+    int da = concave.size(), db = arbitrary.size(), n = da + db - 1;
+
+    auto get = [&](int row, int col) -> T {
+        if (!(col <= row && row < col + da)) return numeric_limits<T>::lowest();
+        return arbitrary[col] + concave[row - col];
+    };
+
+    auto cmp = [&](const auto &p1, const auto &p2) {
+        return p1.first <= p2.first;
+    };
+    auto dp = smawk<T>(n, db, get, cmp);
+
+    vector<T> c(n);
+    for (int i = 0; i < n; i++) c[i] = dp[i].first;
+    return c;
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -8,8 +84,8 @@ int main() {
     int m, n, f;
     cin >> m >> n >> f;
 
-    vector<int> c(n);
-    for (int &ci : c) cin >> ci;
+    vector<int> C(n);
+    for (int &ci : C) cin >> ci;
 
     vector<vector<int>> tokens(n);
     while (f--) {
@@ -19,33 +95,26 @@ int main() {
         tokens[p].emplace_back(t);
     }
 
-    vector<array<int, 3>> batches;
-    for (int i = 0; i < n; i++)
-        if (!tokens[i].empty()) {
-            sort(tokens[i].begin(), tokens[i].end());
-            for (int j = 0, prev = 0; j < tokens[i].size(); j++) {
-                int len = tokens[i][j] - prev;
-                if (len > 0) {
-                    int count = min(len, m / c[i]);
-                    if (c[i] <= m && count) batches.push_back({c[i], (int) tokens[i].size() - j, count});
-                }
-                prev = tokens[i][j];
-            }
+    vector<int> dp(m + 1, 0);
+    for (int p = 0; p < n; p++) {
+        if (tokens[p].empty() || C[p] > m) continue;
+
+        sort(tokens[p].begin(), tokens[p].end());
+        int cp = C[p], rounds = min(m / cp, tokens[p].back());
+        vector<int> concave(rounds + 1, 0);
+        for (int k = 1, i = 0; k <= rounds; k++) {
+            for (; i < tokens[p].size() && tokens[p][i] < k; i++);
+            concave[k] = concave[k - 1] + tokens[p].size() - i;
         }
 
-    vector<int> dp(m + 1, 0);
-    for (auto [ci, h, count] : batches) {
-        auto temp = dp;
-        for (int r = 0; r <= min(m, ci - 1); r++) {
-            deque<pair<int, int>> dq;
-            for (int q = 0; q <= (m - r) / ci; q++) {
-                int i = r + q * ci, bound = temp[i] - q * h;
-                while (!dq.empty() && dq.back().second <= bound) dq.pop_back();
-                dq.emplace_back(q, bound);
-                while (!dq.empty() && dq.front().first < q - count) dq.pop_front();
-                dp[i] = dq.front().second + q * h;
-            }
+        vector<int> arbitrary;
+        for (int r = 0; r < cp && r <= m; r++) {
+            int len = (m - r) / cp + 1;
+            arbitrary.resize(len);
+            for (int i = 0, j = r; i < len; i++, j += cp) arbitrary[i] = dp[j];
+            auto c = max_plus_convolve(concave, arbitrary);
+            for (int i = 0, j = r; i < len; i++, j += cp) dp[j] = c[i];
         }
     }
-    cout << *max_element(dp.begin(), dp.end());
+    cout << dp[m];
 }
