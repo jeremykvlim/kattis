22ef7a250c67ac70fd16b58e1ce6e78ae8ebe708
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2026-09-22 02:38:06 +0800

    Update

--- nus/cs2040/lostmap.java
+++ nus/cs2040/lostmap.java
@@ -10,34 +10,31 @@ public class lostmap {
         var adjMatrix = new int[n][n];
         for (int i = 0; i < n; i++) adjMatrix[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
-        for (var e : prim_dense(n, adjMatrix).first) pw.println(e.first + " " + e.second);
+        for (var e : prim_dense(n, adjMatrix).second) pw.println((e.first + 1) + " " + (e.second + 1));
         pw.flush();
     }
 
-    static Tuple<ArrayList<Pair<Integer, Integer>>, Integer> prim_dense(int n, int[][] adjMatrix) {
+    static Tuple<Integer, Pair<Integer, Integer>[]> prim_dense(int n, int[][] adjMatrix) {
         var dist = new int[n];
         Arrays.fill(dist, Integer.MAX_VALUE);
         dist[0] = 0;
         var visited = new boolean[n];
-        var mst = new ArrayList<Pair<Integer, Integer>>(n - 1);
-        for (int i = 0; i < n - 1; i++) mst.add(new Pair<>(-1, -1));
-
-        int weight = 0;
+        int len = 0;
+        var mst = new Pair[n - 1];
         for (int __ = 0; __ < n; __++) {
             int u = -1;
             for (int v = 0; v < n; v++)
                 if (!visited[v] && (u < 0 || dist[v] < dist[u])) u = v;
 
             visited[u] = true;
-            weight += dist[u];
-            for (int k = 0; k < n; k++)
-                if (!visited[k] && dist[k] > adjMatrix[u][k]) {
-                    dist[k] = adjMatrix[u][k];
-                    mst.set(k - 1, new Pair<>(Math.min(u, k) + 1, Math.max(u, k) + 1));
+            len += dist[u];
+            for (int v = 1; v < n; v++)
+                if (!visited[v] && dist[v] > adjMatrix[u][v]) {
+                    dist[v] = adjMatrix[u][v];
+                    mst[v - 1] = new Pair<>(Math.min(u, v), Math.max(u, v));
                 }
         }
-
-        return new Tuple<>(mst, weight);
+        return new Tuple<>(len, mst);
     }
 
     record Tuple<T, U>(T first, U second) {}
--- open/fastestspeedrun.cpp
+++ open/fastestspeedrun.cpp
@@ -1,179 +1,234 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct DisjointSets {
+inline char readchar() {
+    static const int size = 1 << 20;
+    static array<char, size> buf;
+    static int pos = 0, len = 0;
+    if (pos >= len) {
+        pos = 0;
+        len = fread(buf.data(), 1, size, stdin);
+        if (!len) return EOF;
+    }
+    return buf[pos++];
+}
+
+template <typename T>
+inline bool read(T &v) {
+    char c;
+    do {
+        c = readchar();
+        if (c == EOF) return false;
+    } while (c == ' ' || c == '\n' || c == '\r' || c == '\t');
+
+    if constexpr (is_integral_v<T> && !is_same_v<T, char>) {
+        bool neg = false;
+        if (c == '+' || c == '-') {
+            neg = (c == '-');
+            c = readchar();
+        }
+
+        v = 0;
+        for (; '0' <= c && c <= '9'; c = readchar()) v = v * 10 + (c - '0');
+        if (neg) v = -v;
+        return true;
+    } else if constexpr (is_floating_point_v<T>) {
+        bool neg = false;
+        if (c == '+' || c == '-') {
+            neg = (c == '-');
+            c = readchar();
+        }
+
+        v = 0;
+        for (; '0' <= c && c <= '9'; c = readchar()) v = v * 10 + (c - '0');
+        if (c == '.') {
+            T place = 1;
+            for (c = readchar(); '0' <= c && c <= '9'; c = readchar()) {
+                place *= 0.1;
+                v += (c - '0') * place;
+            }
+        }
+        if (neg) v = -v;
+        return true;
+    } else if constexpr (is_same_v<T, char>) {
+        v = c;
+        return true;
+    } else if constexpr (is_same_v<T, string>) {
+        v.clear();
+        do {
+            v += c;
+            c = readchar();
+        } while (c != EOF && c != ' ' && c != '\n' && c != '\r' && c != '\t');
+        return true;
+    }
+
+    return false;
+}
+
+template <typename... T>
+inline bool read(T &... xs) requires (sizeof...(T) > 1) {
+    return (read(xs) && ...);
+}
+
+struct RollbackDisjointSets {
     vector<int> sets;
+    vector<pair<int, int>> history;
 
     int find(int v) {
-        while (sets[v] >= 0) {
-            int p = sets[v];
-            if (sets[p] >= 0) sets[v] = sets[p];
-            v = p;
-        }
+        while (sets[v] >= 0) v = sets[v];
         return v;
     }
 
-    pair<int, int> unite(int u, int v) {
+    bool unite(int u, int v) {
         int u_set = find(u), v_set = find(v);
-        if (u_set == v_set) return {u_set, -1};
+        if (u_set == v_set) return false;
 
         if (sets[u_set] > sets[v_set]) swap(u_set, v_set);
+        history.emplace_back(v_set, sets[v_set]);
         sets[u_set] += sets[v_set];
         sets[v_set] = u_set;
-        return {u_set, v_set};
+        return true;
     }
 
     int size(int v) {
         return -sets[find(v)];
     }
 
-    DisjointSets(int n) : sets(n, -1) {}
+    int record() {
+        return history.size();
+    }
+
+    void rollback(int version) {
+        while (record() > version) {
+            auto [v_set, s] = history.back();
+            history.pop_back();
+
+            int u_set = sets[v_set];
+            sets[u_set] -= s;
+            sets[v_set] = s;
+        }
+    }
+
+    void delete_history(int version = 0) {
+        history.resize(version);
+    }
+
+    RollbackDisjointSets(int n) : sets(n, -1) {}
 };
 
 template <typename T>
-pair<T, vector<int>> edmonds_dense(int n, vector<vector<pair<int, T>>> adj_list_transpose, int root = 0) {
-    for (auto &neighbors : adj_list_transpose) sort(neighbors.begin(), neighbors.end(), [&](const auto &p1, const auto &p2) { return p1.second > p2.second; });
+pair<T, vector<int>> edmonds_dense(int n, vector<vector<T>> &adj_matrix_transpose, int root = 0) {
+    vector<vector<pair<int, int>>> edge(n, vector<pair<int, int>>(n, {-1, -1}));
+    for (int u = 0; u < n; u++)
+        for (int v = 0; v < n; v++) edge[u][v] = {v, u};
 
-    DisjointSets dsu(n);
-    vector<T> cost(n, 0), loss(n, 0);
+    RollbackDisjointSets rdsu(n);
     vector<pair<int, int>> chosen_edge(n, {-1, -1});
-    vector<int> parent(n, -1), visited(n, 0), rep(n);
-    iota(rep.begin(), rep.end(), 0);
+    stack<tuple<int, int, vector<pair<int, int>>>> history;
     vector<bool> cycle(n, false);
+    vector<int> cycle_nodes, rep(n), active(n), seen(n, -1);
+    iota(rep.begin(), rep.end(), 0);
+    iota(active.begin(), active.end(), 0);
+    seen[root] = root;
+    T len = 0;
+    for (int s = 0; s < n; s++) {
+        int v = rep[rdsu.find(s)];
+        while (!~seen[v]) {
+            T w = numeric_limits<T>::max();
+            for (int u : active)
+                if (w > adj_matrix_transpose[v][u]) {
+                    w = adj_matrix_transpose[v][u];
+                    chosen_edge[v] = edge[v][u];
+                }
+            if (w == numeric_limits<T>::max()) return {0, {}};
 
-    stack<pair<vector<int>, vector<pair<int, int>>>> history;
-    for (T len = 0;;) {
-        int root_set = rep[dsu.find(root)];
-        for (int v = 0; v < n; v++)
-            if (v == rep[dsu.find(v)]) {
-                cost[v] = v != root_set ? numeric_limits<T>::max() : 0;
-                chosen_edge[v] = {-1, -1};
+            seen[v] = s;
+            len += w;
+            for (int u : active)
+                if (u != v) adj_matrix_transpose[v][u] -= w;
+
+            int r = rep[rdsu.find(chosen_edge[v].first)];
+            if (seen[r] != s) {
+                v = r;
+                continue;
             }
 
-        for (int v = 0; v < n; v++) {
-            while (!adj_list_transpose[v].empty() && dsu.find(v) == dsu.find(adj_list_transpose[v].back().first)) adj_list_transpose[v].pop_back();
-            if (!adj_list_transpose[v].empty()) {
-                auto [u, w] = adj_list_transpose[v].back();
-                T c = w - loss[v];
-                int v_set = rep[dsu.find(v)];
-                if (cost[v_set] > c) {
-                    cost[v_set] = c;
-                    chosen_edge[v_set] = {u, v};
-                }
+            cycle[r] = true;
+            cycle_nodes.emplace_back(r);
+            for (int u = rep[rdsu.find(chosen_edge[r].first)]; u != r; u = rep[rdsu.find(chosen_edge[u].first)]) {
+                cycle[u] = true;
+                cycle_nodes.emplace_back(u);
             }
-        }
 
-        for (int v = 0; v < n; v++)
-            if (v == rep[dsu.find(v)] && cost[v] == numeric_limits<T>::max()) return {-1, {}};
-
-        int count = 1;
-        vector<vector<int>> cycles;
-        fill(cycle.begin(), cycle.end(), false);
-        fill(visited.begin(), visited.end(), 0);
-        visited[root_set] = count++;
-        for (int v = 0; v < n; v++)
-            if (v == rep[dsu.find(v)] && !visited[v]) {
-                int u = v;
-                stack<int> s;
-                while (!visited[u]) {
-                    visited[u] = count;
-                    s.emplace(u);
-                    u = rep[dsu.find(chosen_edge[u].first)];
-                }
-
-                if (visited[u] == count) {
-                    cycle[u] = true;
-                    cycles.emplace_back(vector{u});
-                    while (s.top() != u) {
-                        cycle[s.top()] = true;
-                        cycles.back().emplace_back(s.top());
-                        s.pop();
+            for (int u : active)
+                if (!cycle[u])
+                    for (int i = 1; i < cycle_nodes.size(); i++) {
+                        int t = cycle_nodes[i];
+                        if (adj_matrix_transpose[r][u] > adj_matrix_transpose[t][u]) {
+                            adj_matrix_transpose[r][u] = adj_matrix_transpose[t][u];
+                            edge[r][u] = edge[t][u];
+                        }
+                        if (adj_matrix_transpose[u][r] > adj_matrix_transpose[u][t]) {
+                            adj_matrix_transpose[u][r] = adj_matrix_transpose[u][t];
+                            edge[u][r] = edge[u][t];
+                        }
                     }
-                }
-                count++;
-            }
 
-        if (!cycles.empty()) {
-            for (int v = 0; v < n; v++) {
-                int v_set = rep[dsu.find(v)];
-                if (cycle[v_set]) {
-                    if (v == v_set) len += cost[v_set];
-                    loss[v] += cost[v_set];
-                }
-            }
+            int version = rdsu.record();
+            vector<pair<int, int>> cycle_edges;
+            for (int u : cycle_nodes) cycle_edges.emplace_back(chosen_edge[u]);
+            history.emplace(version, r, cycle_edges);
+            for (int i = 1; i < cycle_nodes.size(); i++) rdsu.unite(r, cycle_nodes[i]);
+            rep[rdsu.find(r)] = r;
 
-            for (auto &c : cycles) {
-                vector<pair<int, int>> edges;
-                for (int ci : c) {
-                    if (ci != c[0]) {
-                        parent[ci] = c[0];
-                        auto [big, small] = dsu.unite(c[0], ci);
-                        if (small != -1) rep[big] = c[0];
-                    }
-                    edges.emplace_back(chosen_edge[ci]);
-                }
-                history.emplace(c, edges);
+            active.erase(remove_if(active.begin(), active.end(), [&](int u) { return cycle[u] && u != r; }), active.end());
+            while (!cycle_nodes.empty()) {
+                cycle[cycle_nodes.back()] = false;
+                cycle_nodes.pop_back();
             }
-            continue;
+            chosen_edge[r] = {-1, -1};
+            seen[r] = -1;
+            v = r;
         }
-
-        for (int v = 0; v < n; v++)
-            if (v == rep[dsu.find(v)]) len += cost[v];
-
-        vector<int> mst(n, -1);
-        root_set = rep[dsu.find(root)];
-        for (int v = 0; v < n; v++)
-            if (v == rep[dsu.find(v)]) {
-                if (v == root_set) chosen_edge[v] = {-1, root};
-                else mst[chosen_edge[v].second] = chosen_edge[v].first;
-            }
-
-        while (!history.empty()) {
-            auto [c, edges] = history.top();
-            history.pop();
-
-            auto [u, v] = chosen_edge[c[0]];
-            for (int ci : c) visited[ci] = count;
-            int t = v;
-            while (t != -1 && visited[t] != count) t = parent[t];
-            count++;
-
-            for (int i = 0; i < c.size(); i++)
-                if (c[i] == t) {
-                    mst[v] = u;
-                    chosen_edge[c[i]] = {u, v};
-                } else {
-                    auto [x, y] = edges[i];
-                    mst[y] = x;
-                    chosen_edge[c[i]] = {x, y};
-                }
-        }
-        mst[root] = root;
-        return {len, mst};
     }
+
+    vector<pair<int, int>> edges(n, {-1, -1});
+    for (int v : active)
+        if (v != root) edges[rdsu.find(chosen_edge[v].second)] = chosen_edge[v];
+
+    while (!history.empty()) {
+        auto [version, r, cycle_edges] = history.top();
+        history.pop();
+
+        auto e = edges[rdsu.find(r)];
+        rdsu.rollback(version);
+        for (auto f : cycle_edges) edges[rdsu.find(f.second)] = f;
+        edges[rdsu.find(e.second)] = e;
+    }
+
+    vector<int> mst(n);
+    for (int v = 0; v < n; v++) mst[v] = edges[v].first;
+    mst[root] = root;
+    return {len, mst};
 }
 
 int main() {
     ios::sync_with_stdio(false);
-    cin.tie(nullptr);
 
     int n;
-    cin >> n;
+    read(n);
 
-    vector<vector<pair<int, long long>>> adj_list_transpose(n + 1);
+    vector<vector<long long>> adj_matrix_transpose(n + 1, vector<long long>(n + 1, LLONG_MAX));
     for (int i = 1; i <= n; i++) {
-        int x;
-        long long s;
-        cin >> x >> s;
+        int x, s;
+        read(x, s);
 
         for (int j = 0; j <= n; j++) {
-            long long a;
-            cin >> a;
+            int a;
+            read(a);
 
-            if (i != j) adj_list_transpose[i].emplace_back(j, j == x ? min(a, s) : a);
+            if (i != j) adj_matrix_transpose[i][j] = j == x ? min(a, s) : a;
         }
     }
-
-    auto [len, mst] = edmonds_dense(n + 1, adj_list_transpose);
-    cout << len;
+    cout << edmonds_dense(n + 1, adj_matrix_transpose).first;
 }
