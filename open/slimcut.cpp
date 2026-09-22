39338f03980b41fcc1f6cadb665ead134566bae2
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2026-09-22 23:24:32 +0800

    Create slimcut.cpp

---
+++ open/slimcut.cpp
@@ -0,0 +1,677 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Hash {
+    template <typename T>
+    static inline void combine(size_t &h, const T &v) {
+        h ^= Hash{}(v) + 0x9e3779b9 + (h << 6) + (h >> 2);
+    }
+
+    template <typename T>
+    size_t operator()(const T &v) const {
+        if constexpr (requires { tuple_size<T>::value; })
+            return apply([](const auto &...e) {
+                size_t h = 0;
+                (combine(h, e), ...);
+                return h;
+            }, v);
+        else if constexpr (requires { declval<T>().begin(); declval<T>().end(); } && !is_same_v<T, string>) {
+            size_t h = 0;
+            for (const auto &e : v) combine(h, e);
+            return h;
+        } else return hash<T>{}(v);
+    }
+};
+
+template <typename T, typename U, typename V>
+T mul(U x, V y, T mod) {
+    return (unsigned __int128) x * y % mod;
+}
+
+template <typename T, typename U>
+T pow(T base, U exponent, T mod) {
+    T value = 1;
+    while (exponent) {
+        if (exponent & 1) value = mul(base, value, mod);
+        base = mul(base, base, mod);
+        exponent >>= 1;
+    }
+    return value;
+}
+
+bool isprime(unsigned long long n) {
+    if (n < 2) return false;
+    if (n == 2 || n == 5 || n == 11) return true;
+    if (n % 6 % 4 != 1) return (n | 1) == 3;
+
+    auto miller_rabin = [&](int a) {
+        int s = countr_zero(n - 1);
+        auto d = n >> s, x = pow(a % n, d, n);
+        if (x == 1 || x == n - 1) return true;
+
+        while (s--) {
+            x = mul(x, x, n);
+            if (x == n - 1) return true;
+        }
+        return false;
+    };
+    if (!miller_rabin(2) || !miller_rabin(3)) return false;
+
+    auto lucas_pseudoprime = [&]() {
+        auto normalize = [&](__int128 &x) {
+            if (x < 0) x += ((-x / n) + 1) * n;
+        };
+
+        __int128 D = -3;
+        for (;;) {
+            D += D > 0 ? 2 : -2;
+            D *= -1;
+
+            int jacobi = 1;
+            auto jacobi_symbol = [&](__int128 n) {
+                auto a = D;
+                normalize(a);
+
+                while (a) {
+                    while (!(a & 1)) {
+                        a >>= 1;
+                        if ((n & 7) == 3 || (n & 7) == 5) jacobi = -jacobi;
+                    }
+                    if ((a & 3) == 3 && (n & 3) == 3) jacobi = -jacobi;
+
+                    swap(a, n);
+                    a %= n;
+                }
+                return n == 1;
+            };
+            if (!jacobi_symbol(n)) return false;
+            if (jacobi == -1) break;
+        }
+
+        string bits;
+        auto temp = n + 1;
+        while (temp) {
+            bits += (temp & 1) ? '1' : '0';
+            temp >>= 1;
+        }
+        bits.pop_back();
+        reverse(bits.begin(), bits.end());
+
+        auto div2mod = [&](__int128 x) -> unsigned long long {
+            if (x & 1) x += n;
+            normalize(x >>= 1);
+
+            return x % n;
+        };
+
+        __int128 U = 1, V = 1;
+        for (char b : bits) {
+            auto U_2k = mul(U, V, n), V_2k = div2mod(mul(V, V, n) + D * mul(U, U, n));
+
+            if (b == '0') {
+                U = U_2k;
+                V = V_2k;
+            } else {
+                U = div2mod(U_2k + V_2k);
+                V = div2mod(D * U_2k + V_2k);
+            }
+        }
+
+        return !U;
+    };
+    return lucas_pseudoprime();
+}
+
+template <typename T>
+T brent(T n) {
+    if (!(n & 1)) return 2;
+
+    static mt19937_64 rng(random_device{}());
+    for (;;) {
+        T x = 2, y = 2, g = 1, q = 1, xs = 1, c = rng() % (n - 1) + 1;
+        for (int i = 1; g == 1; i <<= 1, y = x) {
+            for (int j = 1; j < i; j++) x = mul(x, x, n) + c;
+            for (int j = 0; j < i && g == 1; j += 128) {
+                xs = x;
+                for (int k = 0; k < min(128, i - j); k++) {
+                    x = mul(x, x, n) + c;
+                    q = mul(q, max(x, y) - min(x, y), n);
+                }
+                g = __gcd(q, n);
+            }
+        }
+
+        if (g == n) g = 1;
+        while (g == 1) {
+            xs = mul(xs, xs, n) + c;
+            g = __gcd(max(xs, y) - min(xs, y), n);
+        }
+        if (g != n) return isprime(g) ? g : brent(g);
+    }
+}
+
+template <typename T>
+vector<T> factorize(T n) {
+    vector<T> pfs;
+
+    auto dfs = [&](auto &&self, T m) -> void {
+        if (m < 2) return;
+        if (isprime(m)) {
+            pfs.emplace_back(m);
+            return;
+        }
+
+        T pf = brent(m);
+        pfs.emplace_back(pf);
+        self(self, m / pf);
+    };
+    dfs(dfs, n);
+
+    return pfs;
+}
+
+template <typename T>
+T primitive_root_mod_m(T m) {
+    if (m == 1 || m == 2 || m == 4) return m - 1;
+    if (!(m & 3)) return m;
+
+    auto pfs = factorize(m);
+    sort(pfs.begin(), pfs.end());
+    pfs.erase(unique(pfs.begin(), pfs.end()), pfs.end());
+    if (pfs.size() > 2 || (pfs.size() == 2 && (m & 1))) return m;
+
+    auto phi = !(m & 1) ? m / 2 / pfs[1] * (pfs[1] - 1) : m / pfs[0] * (pfs[0] - 1);
+    pfs = factorize(phi);
+    sort(pfs.begin(), pfs.end());
+    pfs.erase(unique(pfs.begin(), pfs.end()), pfs.end());
+    for (auto g = 2LL; g < m; g++)
+        if (gcd(g, m) == 1 && all_of(pfs.begin(), pfs.end(), [&](auto pf) { return pow((T) g, phi / pf, m) != 1; })) return g;
+
+    return m;
+}
+
+template <typename M>
+struct MontgomeryModInt {
+    using T = typename decay<decltype(M::value)>::type;
+    using U = typename conditional<is_same<T, unsigned int>::value, unsigned long long, typename conditional<is_same<T, unsigned long long>::value, unsigned __int128, void>::type>::type;
+    using I = typename conditional<is_same<T, unsigned int>::value, int, typename conditional<is_same<T, unsigned long long>::value, long long, void>::type>::type;
+    using J = typename conditional<is_same<T, unsigned int>::value, long long, typename conditional<is_same<T, unsigned long long>::value, __int128, void>::type>::type;
+
+    T value;
+    static inline int p2;
+    static inline T g;
+    static inline pair<T, U> r;
+    static inline bool prime_mod;
+    static constexpr int bit_length = sizeof(T) * 8;
+
+    static void init() {
+        prime_mod = mod() == 998244353 || mod() == (unsigned long long) 1e9 + 7 || mod() == (unsigned long long) 1e9 + 9 || mod() == (unsigned long long) 1e6 + 69 || mod() == 2524775926340780033 || mod() == 39582418599937 || mod() == 79164837199873 || isprime(mod());
+        r = {mod(), - (U) mod() % mod()};
+        while (mod() * r.first != 1) r.first *= (T) 2 - mod() * r.first;
+        g = primitive_root_mod_m(mod());
+        p2 = 0;
+        for (T t = mod() - 1; !(t & 1); t >>= 1) p2++;
+    }
+
+    constexpr MontgomeryModInt() : value() {}
+
+    MontgomeryModInt(const J &x) {
+        value = reduce((U) x * r.second);
+    }
+
+    template <typename N, typename = enable_if_t<!is_same<M, N>::value && is_same<typename MontgomeryModInt<N>::T, T>::value>>
+    MontgomeryModInt(const MontgomeryModInt<N> &x) {
+        value = reduce((U) x() * r.second);
+    }
+
+    static T reduce(const U &x) {
+        T q = (U) x * r.first, v = (x >> bit_length) + mod() - (((U) q * mod()) >> bit_length);
+        return v >= mod() ? v - mod() : v;
+    }
+
+    T operator()() const {
+        return reduce((U) value);
+    }
+
+    template <typename V>
+    explicit operator V() const {
+        return (V) value;
+    }
+
+    I recover() const {
+        T v = reduce((U) value);
+        return v > mod() / 2 ? v - mod() : v;
+    }
+
+    constexpr static T mod() {
+        return M::value;
+    }
+
+    constexpr static T primitive_root() {
+        return g;
+    }
+
+    constexpr static bool ntt_viable(int n) {
+        if (!prime_mod || (n & (n - 1)) || g == mod()) return false;
+        return __lg(n) <= p2;
+    }
+
+    inline auto & operator+=(const MontgomeryModInt &v) {
+        if ((I) (value += v.value) >= mod()) value -= mod();
+        return *this;
+    }
+
+    inline auto & operator-=(const MontgomeryModInt &v) {
+        if ((I) (value -= v.value) < 0) value += mod();
+        return *this;
+    }
+
+    template <typename U>
+    inline auto & operator+=(const U &v) {
+        return *this += (MontgomeryModInt) v;
+    }
+
+    template <typename U>
+    inline auto & operator-=(const U &v) {
+        return *this -= (MontgomeryModInt) v;
+    }
+
+    auto & operator++() {
+        return *this += 1;
+    }
+
+    auto & operator--() {
+        return *this -= 1;
+    }
+
+    auto operator++(int) {
+        return *this += 1;
+    }
+
+    auto operator--(int) {
+        return *this -= 1;
+    }
+
+    auto operator-() const {
+        return (MontgomeryModInt) 0 - *this;
+    }
+
+    MontgomeryModInt & operator*=(const MontgomeryModInt &v) {
+        if constexpr (is_same_v<T, unsigned int>) value = reduce((unsigned long long) value * v.value);
+        else value = reduce((unsigned __int128) value * v.value);
+        return *this;
+    }
+
+    auto & operator/=(const MontgomeryModInt &v) {
+        return *this *= inv(v);
+    }
+
+    static MontgomeryModInt pow(MontgomeryModInt base, T exponent) {
+        MontgomeryModInt v = 1;
+        while (exponent) {
+            if (exponent & 1) v *= base;
+            base *= base;
+            exponent >>= 1;
+        }
+        return v;
+    }
+
+    static MontgomeryModInt inv(const MontgomeryModInt &v) {
+        if (prime_mod) return pow(v, mod() - 2);
+
+        T x = 0, y = 1, a = v.value, m = mod();
+        while (a) {
+            T t = m / a;
+            m -= t * a;
+            swap(a, m);
+            x -= t * y;
+            swap(x, y);
+        }
+
+        return (MontgomeryModInt) x;
+    }
+};
+
+template <typename T>
+bool operator==(const MontgomeryModInt<T> &lhs, const MontgomeryModInt<T> &rhs) {
+    return lhs.value == rhs.value;
+}
+
+template <typename T, typename U>
+bool operator==(const MontgomeryModInt<T> &lhs, U rhs) {
+    return lhs == MontgomeryModInt<T>(rhs);
+}
+
+template <typename T, typename U>
+bool operator==(U lhs, const MontgomeryModInt<T> &rhs) {
+    return MontgomeryModInt<T>(lhs) == rhs;
+}
+
+template <typename T>
+bool operator!=(const MontgomeryModInt<T> &lhs, const MontgomeryModInt<T> &rhs) {
+    return !(lhs == rhs);
+}
+
+template <typename T, typename U>
+bool operator!=(const MontgomeryModInt<T> &lhs, U rhs) {
+    return !(lhs == rhs);
+}
+
+template <typename T, typename U>
+bool operator!=(U lhs, const MontgomeryModInt<T> &rhs) {
+    return !(lhs == rhs);
+}
+
+template <typename T>
+bool operator>(const MontgomeryModInt<T> &lhs, const MontgomeryModInt<T> &rhs) {
+    return lhs() > rhs();
+}
+
+template <typename T>
+bool operator<(const MontgomeryModInt<T> &lhs, const MontgomeryModInt<T> &rhs) {
+    return lhs() < rhs();
+}
+
+template <typename T>
+bool operator>=(const MontgomeryModInt<T> &lhs, const MontgomeryModInt<T> &rhs) {
+    return lhs > rhs || lhs == rhs;
+}
+
+template <typename T>
+bool operator<=(const MontgomeryModInt<T> &lhs, const MontgomeryModInt<T> &rhs) {
+    return lhs < rhs || lhs == rhs;
+}
+
+template <typename T>
+MontgomeryModInt<T> operator+(const MontgomeryModInt<T> &lhs, const MontgomeryModInt<T> &rhs) {
+    return MontgomeryModInt<T>(lhs) += rhs;
+}
+
+template <typename T, typename U>
+MontgomeryModInt<T> operator+(const MontgomeryModInt<T> &lhs, U rhs) {
+    return MontgomeryModInt<T>(lhs) += rhs;
+}
+
+template <typename T, typename U>
+MontgomeryModInt<T> operator+(U lhs, const MontgomeryModInt<T> &rhs) {
+    return MontgomeryModInt<T>(lhs) += rhs;
+}
+
+template <typename T>
+MontgomeryModInt<T> operator-(const MontgomeryModInt<T> &lhs, const MontgomeryModInt<T> &rhs) {
+    return MontgomeryModInt<T>(lhs) -= rhs;
+}
+
+template <typename T, typename U>
+MontgomeryModInt<T> operator-(const MontgomeryModInt<T> &lhs, U rhs) {
+    return MontgomeryModInt<T>(lhs) -= rhs;
+}
+
+template <typename T, typename U>
+MontgomeryModInt<T> operator-(U lhs, const MontgomeryModInt<T> &rhs) {
+    return MontgomeryModInt<T>(lhs) -= rhs;
+}
+
+template <typename T>
+MontgomeryModInt<T> operator*(const MontgomeryModInt<T> &lhs, const MontgomeryModInt<T> &rhs) {
+    return MontgomeryModInt<T>(lhs) *= rhs;
+}
+
+template <typename T, typename U>
+MontgomeryModInt<T> operator*(const MontgomeryModInt<T> &lhs, U rhs) {
+    return MontgomeryModInt<T>(lhs) *= rhs;
+}
+
+template <typename T, typename U>
+MontgomeryModInt<T> operator*(U lhs, const MontgomeryModInt<T> &rhs) {
+    return MontgomeryModInt<T>(lhs) *= rhs;
+}
+
+template <typename T>
+MontgomeryModInt<T> operator/(const MontgomeryModInt<T> &lhs, const MontgomeryModInt<T> &rhs) {
+    return MontgomeryModInt<T>(lhs) /= rhs;
+}
+
+template <typename T, typename U>
+MontgomeryModInt<T> operator/(const MontgomeryModInt<T> &lhs, U rhs) {
+    return MontgomeryModInt<T>(lhs) /= rhs;
+}
+
+template <typename T, typename U>
+MontgomeryModInt<T> operator/(U lhs, const MontgomeryModInt<T> &rhs) {
+    return MontgomeryModInt<T>(lhs) /= rhs;
+}
+
+template <typename T, typename U>
+U & operator<<(U &stream, const MontgomeryModInt<T> &v) {
+    return stream << v();
+}
+
+template <typename T, typename U>
+U & operator>>(U &stream, MontgomeryModInt<T> &v) {
+    typename make_signed<typename MontgomeryModInt<T>::T>::type x;
+    stream >> x;
+    v = MontgomeryModInt<T>(x);
+    return stream;
+}
+
+constexpr unsigned long long MOD = 1e9 + 7;
+using modint = MontgomeryModInt<integral_constant<decay<decltype(MOD)>::type, MOD>>;
+
+template <typename T>
+struct AntiMonopolyTree {
+    vector<int> parent, size;
+    vector<T> weight;
+    long long sum;
+
+    AntiMonopolyTree(int n) : parent(n, -1), size(n, 1), weight(n, numeric_limits<T>::max()), sum(0) {}
+
+    pair<T, int> path_max(int u, int v) {
+        upward_maintain(u);
+        upward_maintain(v);
+
+        T max_w = numeric_limits<T>::min();
+        int t = -1;
+        while (u != v) {
+            if (size[u] > size[v]) swap(u, v);
+            if (weight[u] == numeric_limits<T>::max()) return {numeric_limits<T>::max(), -1};
+            if (max_w < weight[u]) {
+                max_w = weight[u];
+                t = u;
+            }
+            u = parent[u];
+        }
+        return {max_w, t};
+    }
+
+    bool connected(int u, int v) {
+        return u == v || path_max(u, v).second != -1;
+    }
+
+    void upward_maintain(int v) {
+        while (~parent[v]) {
+            int p = parent[v];
+            if (3 * size[v] <= 2 * size[p]) {
+                v = p;
+                continue;
+            }
+
+            size[p] -= size[v];
+            parent[v] = parent[p];
+            if (weight[v] < weight[p]) {
+                size[v] += size[p];
+                swap(weight[v], weight[p]);
+                parent[p] = v;
+            }
+        }
+    }
+
+    int root(int v) {
+        while (~parent[v]) v = parent[v];
+        return v;
+    }
+
+    void cut(int v) {
+        for (int p = parent[v]; ~p; p = parent[p]) size[p] -= size[v];
+        parent[v] = -1;
+        weight[v] = numeric_limits<T>::max();
+    }
+
+    bool add(int u, int v, T w) {
+        if (u == v) return false;
+
+        auto [max_w, t] = path_max(u, v);
+        bool merged = max_w == numeric_limits<T>::max();
+        if (!merged) {
+            if (w >= max_w) return false;
+            cut(t);
+        } else sum += (long long) size[root(u)] * size[root(v)];
+
+        int du = 0, dv = 0;
+        while (~u && ~v) {
+            if (w >= weight[u]) {
+                int p = parent[u];
+                if (~p) size[p] += du;
+                u = p;
+            } else if (w >= weight[v]) {
+                int p = parent[v];
+                if (~p) size[p] += dv;
+                v = p;
+            } else {
+                if (size[u] > size[v]) {
+                    swap(u, v);
+                    swap(du, dv);
+                }
+
+                du -= size[u];
+                dv += size[u];
+                size[v] += size[u];
+                w = exchange(weight[u], w);
+                u = exchange(parent[u], v);
+                if (~u) size[u] += du;
+            }
+        }
+
+        if (~v)
+            for (v = parent[v]; ~v; v = parent[v]) size[v] += dv;
+        return merged;
+    }
+
+    bool remove(int u, int v, T w) {
+        auto [max_w, t] = path_max(u, v);
+        if (max_w != w) return false;
+
+        sum -= (long long) size[t] * (size[root(t)] - size[t]);
+        cut(t);
+        return true;
+    }
+};
+
+struct OfflineDynamicGraph {
+    AntiMonopolyTree<int> amt;
+    vector<array<int, 3>> events;
+    vector<pair<int, function<void(AntiMonopolyTree<int> &)>>> queries;
+    unordered_map<pair<int, int>, int, Hash> active;
+
+    OfflineDynamicGraph(int n) : amt(n) {}
+
+    bool add_edge(int u, int v) {
+        if (u > v) swap(u, v);
+
+        auto [it, inserted] = active.try_emplace({u, v}, events.size());
+        if (!inserted) return false;
+        events.push_back({u, v, 0});
+        return true;
+    }
+
+    bool delete_edge(int u, int v) {
+        if (u > v) swap(u, v);
+
+        auto it = active.find({u, v});
+        if (it == active.end()) return false;
+        events.push_back({u, v, 1});
+        events[it->second][2] = 1 - events.size();
+        active.erase(it);
+        return true;
+    }
+
+    template <typename F>
+    void query(F &&f) {
+        queries.emplace_back(events.size(), f);
+    }
+
+    void process() {
+        int q = 0;
+        for (int i = 0; i < events.size(); i++) {
+            for (; q < queries.size() && queries[q].first == i; q++) queries[q].second(amt);
+
+            auto [u, v, w] = events[i];
+            if (w == 1) amt.remove(u, v, -i);
+            else amt.add(u, v, w ? w : -events.size() - 1);
+        }
+        for (; q < queries.size(); q++) queries[q].second(amt);
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    modint::init();
+
+    int n, m;
+    cin >> n >> m;
+
+    OfflineDynamicGraph odg(n);
+    vector<array<int, 3>> edges(m);
+    for (auto &[w, u, v] : edges) {
+        cin >> u >> v >> w;
+
+        odg.add_edge(u, v);
+    }
+    sort(edges.begin(), edges.end());
+
+    int d = n / 2;
+    vector<modint> poly(d + 1, 0);
+    poly[0] = 1;
+
+    auto add = [&](int s) {
+        for (int i = d; i >= s; i--) poly[i] += poly[i - s];
+    };
+
+    auto remove = [&](int s) {
+        for (int i = s; i <= d; i++) poly[i] -= poly[i - s];
+    };
+
+    int weight = 0, size = 0, s = 0;
+    for (int i = 0, j = 0; i < m; i = j) {
+        int w = edges[i][0];
+        for (; j < m && edges[j][0] == w; j++) {
+            auto [_, u, v] = edges[j];
+            odg.delete_edge(u, v);
+            odg.query([&, u, v](auto &amt) {
+                int ru = amt.root(u), rv = amt.root(v);
+                if (ru == rv) return;
+
+                int su = amt.size[ru], sv = amt.size[rv];
+                remove(su + sv);
+                add(su);
+                add(sv);
+                for (int k = d; k > s; k--)
+                    if (poly[k]) {
+                        s = k;
+                        break;
+                    }
+            });
+        }
+
+        odg.query([&, w](auto &) {
+            if (!size || (double) weight / size > (double) w / s) {
+                weight = w;
+                size = s;
+            }
+        });
+    }
+    odg.process();
+    cout << fixed << setprecision(8) << (double) weight / size;
+}
