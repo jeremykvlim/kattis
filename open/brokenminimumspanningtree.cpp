1f759b45179cb00100082e696972b192ca614771
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2026-09-23 13:39:18 +0800

    Update brokenminimumspanningtree.cpp

--- open/brokenminimumspanningtree.cpp
+++ open/brokenminimumspanningtree.cpp
@@ -1,44 +1,111 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct DisjointSets {
-    int t;
-    vector<int> sets, seen;
+template <typename T>
+struct AntiMonopolyTree {
+    vector<int> parent, size;
+    vector<T> weight;
 
-    int find(int v) {
-        if (seen[v] != t) {
-            seen[v] = t;
-            sets[v] = -1;
-            return v;
-        }
+    AntiMonopolyTree(int n) : parent(n, -1), size(n, 1), weight(n, numeric_limits<T>::max()) {}
 
-        while (sets[v] >= 0) {
-            int p = sets[v];
-            if (sets[p] >= 0) sets[v] = sets[p];
-            v = p;
+    pair<T, int> path_max(int u, int v) {
+        upward_maintain(u);
+        upward_maintain(v);
+
+        T max_w = numeric_limits<T>::min();
+        int t = -1;
+        while (u != v) {
+            if (size[u] > size[v]) swap(u, v);
+            if (weight[u] == numeric_limits<T>::max()) return {numeric_limits<T>::max(), -1};
+            if (max_w < weight[u]) {
+                max_w = weight[u];
+                t = u;
+            }
+            u = parent[u];
         }
+        return {max_w, t};
+    }
+
+    bool connected(int u, int v) {
+        return u == v || path_max(u, v).second != -1;
+    }
+
+    void upward_maintain(int v) {
+        while (~parent[v]) {
+            int p = parent[v];
+            if (3 * size[v] <= 2 * size[p]) {
+                v = p;
+                continue;
+            }
+
+            size[p] -= size[v];
+            parent[v] = parent[p];
+            if (weight[v] < weight[p]) {
+                size[v] += size[p];
+                swap(weight[v], weight[p]);
+                parent[p] = v;
+            }
+        }
+    }
+
+    int root(int v) {
+        while (~parent[v]) v = parent[v];
         return v;
     }
 
-    bool unite(int u, int v) {
-        int u_set = find(u), v_set = find(v);
-        if (u_set == v_set) return false;
+    void cut(int v) {
+        for (int p = parent[v]; ~p; p = parent[p]) size[p] -= size[v];
+        parent[v] = -1;
+        weight[v] = numeric_limits<T>::max();
+    }
 
-        if (sets[u_set] > sets[v_set]) swap(u_set, v_set);
-        sets[u_set] += sets[v_set];
-        sets[v_set] = u_set;
+    bool add(int u, int v, T w) {
+        if (u == v) return false;
+
+        auto [max_w, t] = path_max(u, v);
+        bool merged = max_w == numeric_limits<T>::max();
+        if (!merged) {
+            if (w >= max_w) return false;
+            cut(t);
+        }
+
+        int du = 0, dv = 0;
+        while (~u && ~v) {
+            if (w >= weight[u]) {
+                int p = parent[u];
+                if (~p) size[p] += du;
+                u = p;
+            } else if (w >= weight[v]) {
+                int p = parent[v];
+                if (~p) size[p] += dv;
+                v = p;
+            } else {
+                if (size[u] > size[v]) {
+                    swap(u, v);
+                    swap(du, dv);
+                }
+
+                du -= size[u];
+                dv += size[u];
+                size[v] += size[u];
+                w = exchange(weight[u], w);
+                u = exchange(parent[u], v);
+                if (~u) size[u] += du;
+            }
+        }
+
+        if (~v)
+            for (v = parent[v]; ~v; v = parent[v]) size[v] += dv;
+        return merged;
+    }
+
+    bool remove(int u, int v, T w) {
+        auto [max_w, t] = path_max(u, v);
+        if (max_w != w) return false;
+
+        cut(t);
         return true;
     }
-
-    int size(int v) {
-        return -sets[find(v)];
-    }
-
-    void reset() {
-        t++;
-    }
-
-    DisjointSets(int n) : t(1), sets(n, -1), seen(n, 0) {}
 };
 
 int main() {
@@ -48,50 +115,29 @@ int main() {
     int n, m;
     cin >> n >> m;
 
-    vector<array<int, 4>> edges(m), tree_edges(n - 1);
+    vector<int> u(m), v(m);
+    vector<long long> w(m);
     for (int i = 0; i < m; i++) {
-        int u, v, w;
-        cin >> u >> v >> w;
-
-        edges[i] = {w, i, u, v};
-        if (i < n - 1) tree_edges[i] = {w, i, u, v};
+        cin >> u[i] >> v[i] >> w[i];
+        u[i]--;
+        v[i]--;
+        w[i] = w[i] * m + i;
     }
-    sort(edges.begin(), edges.end());
 
-    DisjointSets dsu(n + 1);
-    vector<bool> removed(m, true);
-    vector<pair<int, int>> mst_edges;
-    vector<array<int, 4>> add, remove;
-    for (auto [w, i, u, v] : edges)
-        if (dsu.unite(u, v)) {
-            if (i < n - 1) {
-                removed[i] = false;
-                mst_edges.emplace_back(u, v);
-            } else add.push_back({w, i, u, v});
-        }
+    AntiMonopolyTree<long long> amt(n);
+    for (int i = 0; i < n - 1; i++) amt.add(u[i], v[i], w[i]);
 
-    for (int i = 0; i < n - 1; i++)
-        if (removed[i]) remove.emplace_back(tree_edges[i]);
+    vector<int> order(m - n + 1);
+    iota(order.begin(), order.end(), n - 1);
+    sort(order.begin(), order.end(), [&](int i, int j) { return w[i] < w[j]; });
 
     vector<pair<int, int>> swaps;
-    for (int i = remove.size() - 1; ~i; i--) {
-        dsu.reset();
-        for (auto [u, v] : mst_edges) dsu.unite(u, v);
+    for (int i : order) {
+        auto [max_w, t] = amt.path_max(u[i], v[i]);
 
-        for (int j = 0; j < i; j++) {
-            auto [_, __, u, v] = remove[j];
-            dsu.unite(u, v);
-        }
-
-        auto [_, a, __, ___] = remove[i];
-        for (int j = 0; j < add.size(); j++) {
-            auto [w, b, u, v] = add[j];
-            if (dsu.unite(u, v)) {
-                mst_edges.emplace_back(u, v);
-                swaps.emplace_back(a, b);
-                add.erase(add.begin() + j);
-                break;
-            }
+        if (w[i] < max_w) {
+            swaps.emplace_back(max_w % m, i);
+            amt.add(u[i], v[i], w[i]);
         }
     }
 
