e89aa7065a18ed63eacf61bc763b4c3886b66c84
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2026-09-26 23:05:32 +0800

    Update pipes.cpp

--- open/pipes.cpp
+++ open/pipes.cpp
@@ -1,153 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Hash {
-    template <typename T>
-    static inline void combine(size_t &h, const T &v) {
-        h ^= Hash{}(v) + 0x9e3779b9 + (h << 6) + (h >> 2);
-    }
-
-    template <typename T>
-    size_t operator()(const T &v) const {
-        if constexpr (requires { tuple_size<T>::value; })
-            return apply([](const auto &...e) {
-                size_t h = 0;
-                (combine(h, e), ...);
-                return h;
-            }, v);
-        else if constexpr (requires { declval<T>().begin(); declval<T>().end(); } && !is_same_v<T, string>) {
-            size_t h = 0;
-            for (const auto &e : v) combine(h, e);
-            return h;
-        } else return hash<T>{}(v);
-    }
-};
-
-template <typename T>
-struct Point {
-    T x, y;
-
-    Point() {}
-    Point(T x, T y) : x(x), y(y) {}
-
-    template <typename U>
-    Point(U x, U y) : x(x), y(y) {}
-
-    template <typename U>
-    Point(const Point<U> &p) : x((T) p.x), y((T) p.y) {}
-
-    const auto begin() const {
-        return &x;
-    }
-
-    const auto end() const {
-        return &y + 1;
-    }
-
-    Point operator-() const {
-        return {-x, -y};
-    }
-
-    Point operator!() const {
-        return {y, x};
-    }
-
-    Point operator~() const {
-        return {-y, x};
-    }
-
-    bool operator<(const Point &p) const {
-        return x != p.x ? x < p.x : y < p.y;
-    }
-
-    bool operator>(const Point &p) const {
-        return x != p.x ? x > p.x : y > p.y;
-    }
-
-    bool operator==(const Point &p) const {
-        return x == p.x && y == p.y;
-    }
-
-    bool operator!=(const Point &p) const {
-        return x != p.x || y != p.y;
-    }
-
-    bool operator<=(const Point &p) const {
-        return *this < p || *this == p;
-    }
-
-    bool operator>=(const Point &p) const {
-        return *this > p || *this == p;
-    }
-
-    Point operator+(const Point &p) const {
-        return {x + p.x, y + p.y};
-    }
-
-    Point operator+(const T &v) const {
-        return {x + v, y + v};
-    }
-
-    Point & operator+=(const Point &p) {
-        x += p.x;
-        y += p.y;
-        return *this;
-    }
-
-    Point & operator+=(const T &v) {
-        x += v;
-        y += v;
-        return *this;
-    }
-
-    Point operator-(const Point &p) const {
-        return {x - p.x, y - p.y};
-    }
-
-    Point operator-(const T &v) const {
-        return {x - v, y - v};
-    }
-
-    Point & operator-=(const Point &p) {
-        x -= p.x;
-        y -= p.y;
-        return *this;
-    }
-
-    Point & operator-=(const T &v) {
-        x -= v;
-        y -= v;
-        return *this;
-    }
-
-    Point operator*(const T &v) const {
-        return {x * v, y * v};
-    }
-
-    Point & operator*=(const T &v) {
-        x *= v;
-        y *= v;
-        return *this;
-    }
-
-    Point operator/(const T &v) const {
-        return {x / v, y / v};
-    }
-
-    Point & operator/=(const T &v) {
-        x /= v;
-        y /= v;
-        return *this;
-    }
-};
-
-Point<int> tile(int l, int m, int n) {
-    if (l <= m) return {l - 1, 0};
-    if (l <= m + n) return {m - 1, l - m - 1};
-    if (l <= 2 * m + n) return {2 * m + n - l, n - 1};
-    return {0, 2 * (m + n) - l};
-}
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -159,116 +12,62 @@ int main() {
         int m, n, l;
         cin >> m >> n >> l;
 
-        vector<int> indices(2 * (m + n) + 1, -1);
-        deque<int> locations(2 * l);
-        vector<Point<int>> connect(l);
-        for (int i = 0; i < l; i++) {
+        vector<int> connect(2 * (m + n), -1);
+        while (l--) {
             int s, t;
             cin >> s >> t;
 
-            connect[i] = {s, t};
-            locations[2 * i] = s;
-            locations[2 * i + 1] = t;
-            indices[s] = indices[t] = i;
+            connect[s] = t;
+            connect[t] = s;
         }
-        sort(locations.begin(), locations.end());
 
-        vector<int> order;
-        while (!locations.empty()) {
-            if (indices[locations[0]] == indices[locations.back()]) {
-                order.emplace_back(indices[locations[0]]);
-                locations.pop_front();
-                locations.pop_back();
-                continue;
-            }
 
-            bool change = false;
-            for (int i = 1; i < locations.size(); i++)
-                if (indices[locations[i - 1]] == indices[locations[i]]) {
-                    order.emplace_back(indices[locations[i]]);
-                    locations.erase(locations.begin() + i - 1);
-                    locations.erase(locations.begin() + i - 1);
-                    change = true;
-                    break;
+        vector<int> dr{0, 1, 0, -1}, dc{-1, 0, 1, 0};
+        vector<bool> visited(m * n, false);
+        auto dfs = [&](auto &&self, int v, int in, int t, int out) -> bool {
+            visited[v] = true;
+            if (v == t && in != out) return true;
+
+            int r = v / n, c = v % n;
+            for (int k = 1; k < 4; k++) {
+                int d = (in + k) & 3, x = r + dr[d], y = c + dc[d];
+                if (0 <= x && x < m && 0 <= y && y < n) {
+                    int u = x * n + y;
+                    if (!visited[u] && self(self, u, d ^ 2, t, out)) return true;
                 }
+            }
+            return false;
+        };
 
-            if (!change) break;
-        }
-
-        if (!locations.empty()) {
-            no:;
-            cout << "NO\n";
-            continue;
-        }
-
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
-        for (int i : order) {
-            auto [r1, c1] = tile(connect[i].x, m, n);
-            auto [r2, c2] = tile(connect[i].y, m, n);
-
-            if (visited[r1][c1] || visited[r2][c2]) goto no;
-
-            visited[r1][c1] = visited[r2][c2] = true;
-        }
-
-        vector<vector<bool>> border(m, vector<bool>(n, false));
-        for (int i = 0; i < m; i++) border[i][0] = border[i][n - 1] = true;
-        for (int i = 0; i < n; i++) border[0][i] = border[m - 1][i] = true;
-
-        auto valid = [&](auto p) -> bool { return 0 <= p.x && p.x < m && 0 <= p.y && p.y < n; };
-        vector<Point<int>> drdc1{{1, 0}, {-1, 0}, {0, 1}, {0, -1}}, drdc2{{1, 1}, {1, 0}, {1, -1}, {0, 1}, {0, -1}, {-1, 1}, {-1, 0}, {-1, -1}};
-        for (int i : order) {
-            auto s = tile(connect[i].x, m, n), d = tile(connect[i].y, m, n);
-
-            unordered_map<Point<int>, Point<int>, Hash> prev;
-            unordered_map<Point<int>, int, Hash> dist;
-            dist[s] = 0;
-            stack<Point<int>> st;
-            st.emplace(s);
-            while (!st.empty()) {
-                auto v = st.top();
-                st.pop();
-
-                if (v == d) break;
-
-                for (int j = 0; j < 4; j++) {
-                    auto u = v + drdc1[j];
-
-                    if (u == d) {
-                        dist[u] = dist[v] + 1;
-                        prev[u] = v;
-                        goto done;
+        stack<int> st;
+        bool possible = true;
+        for (int i = 0; i < 2 * (m + n) && possible; i++)
+            if (~connect[i]) {
+                if (i < connect[i]) st.emplace(i);
+                else {
+                    if (st.empty() || st.top() != connect[i]) {
+                        possible = false;
+                        break;
                     }
 
-                    if (!valid(u) || visited[u.x][u.y] || !border[u.x][u.y]) continue;
+                    int j = st.top();
+                    st.pop();
 
-                    if (!dist.count(u)) {
-                        dist[u] = dist[v] + 1;
-                        prev[u] = v;
-                        st.emplace(u);
-                    }
+                    auto pos = [&](int v) -> pair<int, int> {
+                        if (v < m) return {v * n, 0};
+                        v -= m;
+                        if (v < n) return {(m - 1) * n + v, 1};
+                        v -= n;
+                        if (v < m) return {(m - 1 - v) * n + n - 1, 2};
+                        v -= m;
+                        return {n - 1 - v, 3};
+                    };
+                    auto [s, a] = pos(j);
+                    auto [t, b] = pos(i);
+                    if (!visited[s] || !visited[t] || !dfs(dfs, s, a, t, b)) possible = false;
                 }
             }
-            done:;
 
-            if (!dist.count(d)) goto no;
-
-            for (int j = 0; j < 8; j++) {
-                auto v = d + drdc2[j];
-                if (valid(v)) border[v.x][v.y] = true;
-            }
-
-            while (s != d) {
-                d = prev[d];
-                visited[d.x][d.y] = true;
-
-                for (int j = 0; j < 8; j++) {
-                    auto v = d + drdc2[j];
-                    if (valid(v)) border[v.x][v.y] = true;
-                }
-            }
-        }
-
-        cout << "YES\n";
+        cout << (possible ? "YES\n" : "NO\n");
     }
 }
