d9653fb6d796b0e49f86d7e4f5f3c7d1060c17b3
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2026-09-26 19:48:24 +0800

    Update allergy.cpp

--- open/allergy.cpp
+++ open/allergy.cpp
@@ -1,20 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int shortest(int mask, int d, vector<int> &days, vector<vector<int>> &dp) {
-    if (!mask) return d;
-    if (dp[mask][d] >= 0) return dp[mask][d];
-    dp[mask][d] = INT_MAX;
-
-    int prev = -1;
-    for (int i = 0; i < days.size(); i++) {
-        if (!((mask >> i) & 1) || days[i] == prev) continue;
-        prev = days[i];
-        dp[mask][d] = min(dp[mask][d], d + 1 + shortest(mask - (1 << i), max(0, d + 1 - days[i]) + days[i] - (d + 1), days, dp));
-    }
-    return dp[mask][d];
-}
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -22,10 +8,30 @@ int main() {
     int k;
     cin >> k;
 
-    vector<int> days(k);
-    for (int &d : days) cin >> d;
-    sort(days.begin(), days.end());
+    vector<int> count(8, 0);
+    while (k--) {
+        int d;
+        cin >> d;
 
-    vector<vector<int>> dp(1 << k, vector<int>(days.back(), -1));
-    cout << shortest((1 << k) - 1, 0, days, dp);
+        count[d]++;
+    }
+
+    int total = 1;
+    vector<int> base(8, 1);
+    for (int d = 1; d <= 7; d++) {
+        base[d] = total;
+        total *= count[d] + 1;
+    }
+
+    vector<vector<int>> dp(total, vector<int>(8, 1e9));
+    dp[0][1] = 0;
+    for (int i = 0; i < total; i++)
+        for (int d1 = 1; d1 <= 7; d1++)
+            if ((i / base[d1]) % (count[d1] + 1) != count[d1])
+                for (int j = i + base[d1], d2 = 1; d2 <= 7; d2++)
+                    if (dp[i][d2] != 1e9) {
+                        int d3 = max(1, d1 - d2 + 1);
+                        dp[j][d3] = min(dp[j][d3], dp[i][d2] + d3);
+                    }
+    cout << *min_element(dp.back().begin(), dp.back().end());
 }
