40b9b171fd0a599c852970332e06933d5edd03ca
Jeremy Lim <128276968+jeremykvlim@users.noreply.github.com>
2026-09-26 19:41:03 +0800

    Update jugglingtroupe.cpp

--- open/jugglingtroupe.cpp
+++ open/jugglingtroupe.cpp
@@ -1,40 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void perform(vector<int> &jugglers, stack<int> &zeroes, int i) {
-    if (!jugglers[i]) zeroes.emplace(i);
-
-    while (jugglers[i] > 1) {
-        int zero = zeroes.empty() ? -1 : zeroes.top();
-        if (!zeroes.empty()) zeroes.pop();
-
-        int dist1 = i - zero, dist2 = jugglers[i] - 1;
-        if (dist1 < jugglers[i]) {
-            jugglers[i] -= dist1 + 1;
-            if (i < jugglers.size() - 1) jugglers[i + 1] += dist1;
-            if (~zero) jugglers[zero] = 1;
-            if (!jugglers[i]) zeroes.emplace(i);
-        } else {
-            jugglers[i] = 1;
-            if (i < jugglers.size() - 1) jugglers[i + 1] += dist2;
-            if (~zero) jugglers[zero] = 1;
-            jugglers[zero + dist2] = 0;
-            zeroes.emplace(zero + dist2);
-        }
-    }
-}
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     string s;
     cin >> s;
+    int n = s.size();
 
-    vector<int> jugglers(s.size());
-    for (int i = 0; i < s.size(); i++) jugglers[i] = s[i] - '0';
+    deque<int> left, right;
+    for (int i = 1; i <= n; i++)
+        if (s[i - 1] == '0') right.emplace_back(i);
 
-    stack<int> zeroes;
-    for (int i = 0; i < s.size(); i++) perform(jugglers, zeroes, i);
-    for (int i : jugglers) cout << i;
+    for (int i = 1; i <= n; i++) {
+        if (s[i - 1] == '2') {
+            while (!right.empty() && right.front() < i) {
+                left.emplace_back(right.front());
+                right.pop_front();
+            }
+
+            if (!right.empty() && right.front() == i) {
+                right.pop_front();
+                continue;
+            }
+
+            int l = 0, r = n + 1;
+            if (!left.empty()) {
+                l = left.back();
+                left.pop_back();
+            }
+
+            if (!right.empty()) {
+                r = right.front();
+                right.pop_front();
+            }
+
+            int j = l + r - i;
+            if (i >= j) left.emplace_back(j);
+            else right.emplace_front(j);
+        }
+    }
+
+    fill(s.begin(), s.end(), '1');
+    for (int i : left) s[i - 1] = '0';
+    for (int i : right) s[i - 1] = '0';
+    cout << s;
 }
